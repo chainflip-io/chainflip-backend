44f216934e34e39e7cea3d4ba06c3a953138c4d3
Mikunj <mikunj@live.com.au>
2020-08-06 14:12:53 +1000

    Renamed enum and struct

--- src/common/coins.rs
+++ src/common/coins.rs
@@ -1,33 +1,37 @@
 use strum_macros::{EnumIter, EnumString, ToString};
 
-pub struct Coin {
+pub struct CoinInfo {
     pub name: String,
-    pub symbol: CoinSymbol,
+    pub symbol: Coin,
     pub decimals: u32,
     pub requires_return_address: bool,
 }
 
 #[derive(Debug, Copy, Clone, EnumString, ToString, EnumIter)]
-pub enum CoinSymbol {
+pub enum Coin {
     ETH,
     LOKI,
 }
 
-impl CoinSymbol {
-    pub fn get_coin(&self) -> Coin {
+impl Coin {
+    pub fn get_info(&self) -> CoinInfo {
         match self {
-            CoinSymbol::LOKI => Coin {
+            Coin::LOKI => CoinInfo {
                 name: String::from("Loki Network"),
                 symbol: (*self),
                 decimals: 8,
                 requires_return_address: true,
             },
-            CoinSymbol::ETH => Coin {
+            Coin::ETH => CoinInfo {
                 name: String::from("Ethereum"),
                 symbol: (*self),
                 decimals: 18,
                 requires_return_address: true,
             },
         }
+    }
+
+    pub fn get_decimals(&self) -> u32 {
+        self.get_info().decimals
     }
 }
