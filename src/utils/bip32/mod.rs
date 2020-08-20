0f79bd8ca1f6038b4465ddd57b8344e28311ea39
Mikunj <mikunj@live.com.au>
2020-08-20 12:34:43 +1000

    Added extended key decoding

---
+++ src/utils/bip32/mod.rs
@@ -0,0 +1,5 @@
+mod raw_key;
+pub use raw_key::RawKey;
+
+/// Type alias for an error which returns a static string
+pub type StaticError = &'static str;
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,31 @@
+use std::convert::AsMut;
+
 /// The test utils
 pub mod test_utils;
 
 /// Loki utils
 pub mod loki;
+
+/// BIP32/BIP44 utils
+pub mod bip32;
+
+/// Clone slice values into an array
+///
+/// # Example
+///
+/// ```
+/// use blockswap::utils::clone_into_array;
+///
+/// let original = [1, 2, 3, 4, 5];
+/// let cloned: [u8; 4] = clone_into_array(&original[..4]);
+/// assert_eq!(cloned, [1, 2, 3, 4]);
+/// ```
+pub fn clone_into_array<A, T>(slice: &[T]) -> A
+where
+    A: Sized + Default + AsMut<[T]>,
+    T: Clone,
+{
+    let mut a = Default::default();
+    <A as AsMut<[T]>>::as_mut(&mut a).clone_from_slice(slice);
+    a
+}
