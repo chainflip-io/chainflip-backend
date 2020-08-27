0ef3db50ac1ff7fd4666ee65479a3b7c3ed5ec3a
Maxim Shishmarev <msgmaxim@gmail.com>
2020-08-27 18:47:22 +1000

    Move loki types into common/loki.rs

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -11,6 +11,11 @@ pub mod api;
 /// Definitions for Ethereum
 pub mod ethereum;
 
+/// Definitions for Loki
+pub mod loki;
+
+pub use loki::{LokiPaymentId, LokiWalletAddress};
+
 /// Key value store definitions
 pub mod store;
 
@@ -56,86 +61,6 @@ impl WalletAddress {
         WalletAddress {
             0: address.to_owned(),
         }
-    }
-}
-
-/// Represents regular and integrated wallet addresses for Loki
-#[derive(Debug)]
-pub struct LokiWalletAddress {
-    /// base58 (monero flavor) representation
-    address: String,
-}
-
-impl std::str::FromStr for LokiWalletAddress {
-    type Err = String;
-
-    /// Construct from string, validating address length
-    fn from_str(addr: &str) -> Result<Self, Self::Err> {
-        match addr.len() {
-            97 | 108 => Ok(LokiWalletAddress {
-                address: addr.to_owned(),
-            }),
-            x @ _ => Err(format!("Invalid address length: {}", x)),
-        }
-    }
-}
-
-impl LokiWalletAddress {
-    /// Get internal string representation
-    pub fn to_str(&self) -> &str {
-        &self.address
-    }
-}
-/// Payment id that can be used to identify loki transactions
-#[derive(Debug)]
-pub struct LokiPaymentId {
-    // String representation of payment id with trailing zeros added at construction time.
-    // We might consider using a constant size array/string on the stack for this
-    long_pid: String,
-}
-
-impl LokiPaymentId {
-    /// Get payment id as a string slice
-    pub fn to_str(&self) -> &str {
-        &self.long_pid
-    }
-}
-
-impl std::str::FromStr for LokiPaymentId {
-    type Err = String;
-
-    /// Construct loki payment id from a short (long) 16 (64) hex character string
-    fn from_str(pid: &str) -> Result<Self, String> {
-        match pid.len() {
-            16 => {
-                // There is a bug in the wallet that requires trailing zero. Apparently
-                // there is a fix for that on some branch, but for now let's just
-                // defencively add zeros on our side
-                let long_pid = format!("{}000000000000000000000000000000000000000000000000", pid);
-
-                Ok(LokiPaymentId { long_pid })
-            }
-            64 => Ok(LokiPaymentId {
-                long_pid: pid.to_owned(),
-            }),
-            x @ _ => Err(format!("Incorect size for short payment id: {}", x)),
-        }
-    }
-}
-
-impl std::fmt::Display for LokiPaymentId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", &self.long_pid[0..16])
-    }
-}
-
-/// Serialize LokiPaymentId as a simple string
-impl serde::Serialize for LokiPaymentId {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        serializer.serialize_str(&self.long_pid)
     }
 }
 
