a6fa2bd24355d1b77359607b4831294d627b6a9e
Mikunj <mikunj@live.com.au>
2020-10-30 10:28:35 +1100

    Fix get coins. Fix get estimate. Fix get pools.

--- src/quoter/api/v1/get_estimate.rs
+++ src/quoter/api/v1/get_estimate.rs
@@ -17,7 +17,7 @@ pub struct EstimateParams {
     /// The input coin symbol
     pub input_coin: String,
     /// The input amount in atomic value (actual * decimal)
-    pub input_amount: u128,
+    pub input_amount: String,
     /// The output coin symbol
     pub output_coin: String,
 }
@@ -44,25 +44,16 @@ pub async fn get_estimate<S>(
 where
     S: StateProvider,
 {
-    let input_coin = match Coin::from_str(&params.input_coin) {
-        Ok(coin) => coin,
-        Err(_) => {
-            return Err(ResponseError::new(
-                StatusCode::BAD_REQUEST,
-                "Invalid input coin",
-            ))
-        }
-    };
+    let input_coin = Coin::from_str(&params.input_coin)
+        .map_err(|_| ResponseError::new(StatusCode::BAD_REQUEST, "Invalid input coin"))?;
 
-    let output_coin = match Coin::from_str(&params.output_coin) {
-        Ok(coin) => coin,
-        Err(_) => {
-            return Err(ResponseError::new(
-                StatusCode::BAD_REQUEST,
-                "Invalid output coin",
-            ))
-        }
-    };
+    let input_amount = params
+        .input_amount
+        .parse::<u128>()
+        .map_err(|_| ResponseError::new(StatusCode::BAD_REQUEST, "Invalid input amount"))?;
+
+    let output_coin = Coin::from_str(&params.output_coin)
+        .map_err(|_| ResponseError::new(StatusCode::BAD_REQUEST, "Invalid output coin"))?;
 
     if input_coin == output_coin {
         return Err(ResponseError::new(
@@ -71,7 +62,7 @@ where
         ));
     }
 
-    if params.input_amount == 0 {
+    if input_amount == 0 {
         return Err(ResponseError::new(
             StatusCode::BAD_REQUEST,
             "Input amount must be greater than 0",
@@ -81,7 +72,7 @@ where
     let calculation = {
         let state = state.lock().unwrap();
 
-        match price::get_output(&*state, input_coin, params.input_amount, output_coin) {
+        match price::get_output(&*state, input_coin, input_amount, output_coin) {
             Ok(calculation) => calculation,
             Err(err) => {
                 error!(
@@ -110,15 +101,13 @@ mod test {
     use crate::quoter::api::v1::test::setup_memory_db;
 
     #[tokio::test]
-    pub async fn get_estimate_validates_params() {
+    pub async fn get_estimate_returns_invalid_input_coin() {
         let db = setup_memory_db();
         let state = Arc::new(Mutex::new(db));
 
-        // =============
-
         let invalid_input_coin = EstimateParams {
             input_coin: "invalid".to_owned(),
-            input_amount: 1000000,
+            input_amount: "1000000".to_owned(),
             output_coin: "loki".to_owned(),
         };
 
@@ -127,12 +116,16 @@ mod test {
             .expect_err("Expected an error");
 
         assert_eq!(error.message, "Invalid input coin");
+    }
 
-        // =============
+    #[tokio::test]
+    pub async fn get_estimate_returns_invalid_output_coin() {
+        let db = setup_memory_db();
+        let state = Arc::new(Mutex::new(db));
 
         let invalid_output_coin = EstimateParams {
             input_coin: "loki".to_owned(),
-            input_amount: 1000000,
+            input_amount: "1000000".to_owned(),
             output_coin: "invalid".to_owned(),
         };
 
@@ -141,12 +134,58 @@ mod test {
             .expect_err("Expected an error");
 
         assert_eq!(error.message, "Invalid output coin");
+    }
 
-        // =============
+    #[tokio::test]
+    pub async fn get_estimate_returns_invalid_input_amount() {
+        let db = setup_memory_db();
+        let state = Arc::new(Mutex::new(db));
+
+        let invalid_input_amount = EstimateParams {
+            input_coin: "btc".to_owned(),
+            input_amount: "-123".to_owned(),
+            output_coin: "loki".to_owned(),
+        };
+
+        let error = get_estimate(invalid_input_amount, state.clone())
+            .await
+            .expect_err("Expected an error");
+
+        assert_eq!(error.message, "Invalid input amount");
+
+        let invalid_input_amount = EstimateParams {
+            input_coin: "btc".to_owned(),
+            input_amount: "abc".to_owned(),
+            output_coin: "loki".to_owned(),
+        };
+
+        let error = get_estimate(invalid_input_amount, state.clone())
+            .await
+            .expect_err("Expected an error");
+
+        assert_eq!(error.message, "Invalid input amount");
+
+        let invalid_input_amount = EstimateParams {
+            input_coin: "btc".to_owned(),
+            input_amount: "123.0123".to_owned(),
+            output_coin: "loki".to_owned(),
+        };
+
+        let error = get_estimate(invalid_input_amount, state.clone())
+            .await
+            .expect_err("Expected an error");
+
+        assert_eq!(error.message, "Invalid input amount");
+    }
+
+    #[tokio::test]
+    pub async fn get_estimate_returns_coins_must_be_different() {
+        let db = setup_memory_db();
+        let state = Arc::new(Mutex::new(db));
 
         let same_coins = EstimateParams {
             input_coin: "loki".to_owned(),
-            input_amount: 1000000,
+            input_amount: "1000000".to_owned(),
             output_coin: "loki".to_owned(),
         };
 
@@ -158,12 +197,16 @@ mod test {
             error.message,
             "Input coin must be different from output coin"
         );
+    }
 
-        // =============
+    #[tokio::test]
+    pub async fn get_estimate_returns_input_amount_gt_0() {
+        let db = setup_memory_db();
+        let state = Arc::new(Mutex::new(db));
 
         let invalid_input_amount = EstimateParams {
             input_coin: "btc".to_owned(),
-            input_amount: 0,
+            input_amount: "0".to_owned(),
             output_coin: "loki".to_owned(),
         };
 
@@ -172,12 +215,16 @@ mod test {
             .expect_err("Expected an error");
 
         assert_eq!(error.message, "Input amount must be greater than 0");
+    }
 
-        // =============
+    #[tokio::test]
+    pub async fn get_estimate_returns_valid_result() {
+        let db = setup_memory_db();
+        let state = Arc::new(Mutex::new(db));
 
         let valid_params = EstimateParams {
             input_coin: "btc".to_owned(),
-            input_amount: 100,
+            input_amount: "1000000".to_owned(),
             output_coin: "loki".to_owned(),
         };
 
