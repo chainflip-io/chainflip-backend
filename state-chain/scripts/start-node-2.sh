e1ab67533ec7f179606b82f920e8138395cda0c1
Andy Bell <andy.bell@barkingmad.io>
2021-04-06 20:35:12 +0200

    launch some nodes

---
+++ state-chain/scripts/start-node-2.sh
@@ -0,0 +1,11 @@
+# start node01
+../target/release/state-chain-node \
+  --base-path /tmp/node02 \
+  --chain ./customSpecRaw.json \
+  --port 30334 \
+  --ws-port 9946 \
+  --rpc-port 9934 \
+  --telemetry-url 'wss://telemetry.polkadot.io/submit/ 0' \
+  --validator \
+  --rpc-methods Unsafe \
+  --name MyNode02
