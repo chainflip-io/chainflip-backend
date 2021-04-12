434672219ec8a2bf78e2a05d4c2876e0cd8579ee
dlennon <3168260+dandanlen@users.noreply.github.com>
2021-04-12 18:35:30 +0200

    Filled in some gaps...
    - Add explicit witness extrinsics.
    - Fill out `claim` extrinsic.
    - Document assumptions in README

--- state-chain/pallets/cf-staking/README.md
+++ state-chain/pallets/cf-staking/README.md
@@ -1,10 +1,37 @@
 # Chainflip staking pallet
 
-This is a work-in-progress implementation of Chainflip staking functionality. 
+This is a work-in-progress implementation of Chainflip staking functionality.
 
 ## Purpose
 
-Receive notifications of events occurring in Chainflip's `StakeManager` Ethereum contract and update validator's stakes accordingly. 
+This pallet manages staking and claiming of stakes, including:
+
+- Receiving witnesses of events occurring in Chainflip's `StakeManager` Ethereum contract and updating validator's stakes accordingly.
+- Processing claim requests
+
+### Staking
+
+In order to stake, a prospective validator must:
+
+- Have a running state chain node and associated account ID.
+- Stake `FLIP` token through the `StakeManager` contract on Ethereum, specifying:
+    1. the amount they wish to stake
+    2. their account ID on the state chain network
+    3. an address on the ethereum network to which their stake can be returned in the event that they specify an invalid account ID.
+
+### Claiming
+
+Claiming is a bit more involved. In order to claim available stake, active validator nodes must generate a valid threshold signature over the claim arguments and a nonce value.
+
+This then allows anyone to craft a valid `StakeManager::claim` smart contract call.
+
+When called, `StakeManager::claim` will emit a `Claimed` event. This event is then witnessed on the state chain to update the validator's balance.
+
+A validator can have at most one open claim at any given time. If a validator submits a new claim this replaces any existing claim.
+
+### Signatures
+
+This pallet has no knowledge of signatures generated for submission to the `StakeManager::claim` smart contract call. The chainflip engine should store this and make it available to the validators so it can be posted to an Eth node.
 
 ### Genesis Configuration
 
@@ -14,6 +41,6 @@ This pallet does not have any genesis configuration.
 
 You can view the reference docs for this pallet by running:
 
-```
+```sh
 cargo doc --open
 ```
--- state-chain/pallets/cf-staking/src/lib.rs
+++ state-chain/pallets/cf-staking/src/lib.rs
@@ -14,20 +14,9 @@ pub mod pallet {
     use frame_support::pallet_prelude::*;
     use frame_system::pallet_prelude::*;
     use frame_system::pallet::Account;
-    use sp_runtime::{app_crypto::RuntimePublic, traits::{AtLeast32BitUnsigned, CheckedSub, Zero}};
-    use sp_std::{fmt::Debug, ops::Add};
+    use sp_runtime::{app_crypto::RuntimePublic, traits::{AtLeast32BitUnsigned, CheckedSub, One, Zero}};
 
     type AccountId<T> = <T as frame_system::Config>::AccountId;
-
-    struct ClaimState<T: Config> {
-        claim_nonce: u32,
-        pending_claim: Option<Claim<T>>,
-    }
-
-    #[derive(Clone, Debug, Default, PartialEq, Eq, Encode, Decode)]
-    struct Claim<T: Config> {
-        amount: T::StakedAmount,
-    }
 
     #[pallet::config]
     pub trait Config: frame_system::Config
@@ -43,11 +32,17 @@ pub mod pallet {
             + Default
             + AtLeast32BitUnsigned
             + MaybeSerializeDeserialize
-            + Zero
-            + Add
             + CheckedSub;
         
-		type EthereumPubKey: Member + FullCodec + RuntimePublic;
+        type EthereumPubKey: Member + FullCodec + RuntimePublic;
+
+        type Nonce: Member
+            + FullCodec
+            + Copy
+            + Default
+            + AtLeast32BitUnsigned
+            + MaybeSerializeDeserialize
+            + CheckedSub;
     }
 
     #[pallet::pallet]
@@ -55,12 +50,18 @@ pub mod pallet {
     pub struct Pallet<T>(PhantomData<T>);
 
     #[pallet::storage]
-    #[pallet::getter(fn get_stakes)]
     pub type Stakes<T: Config> = StorageMap<_, Identity, AccountId<T>, T::StakedAmount, ValueQuery>;
 
     #[pallet::storage]
-    #[pallet::getter(fn get_claim_states)]
-    pub type ClaimStates<T: Config> = StorageMap<_, Identity, AccountId<T>, ClaimState<T>, ValueQuery>;
+    pub type PendingClaims<T: Config> = StorageMap<
+        _, 
+        Identity, 
+        AccountId<T>, 
+        (T::Nonce, T::StakedAmount), 
+        ValueQuery>;
+
+    #[pallet::storage]
+    pub type Nonces<T: Config> = StorageMap<_, Identity, AccountId<T>, T::Nonce, ValueQuery>;
 
     #[pallet::hooks]
     impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T>
@@ -70,10 +71,9 @@ pub mod pallet {
     #[pallet::call]
     impl<T: Config> Pallet<T>
     {
-        /// Called as a witness for a new stake submitted through the StakeManager contract.
-        /// 
+        /// Witness that a `Staked` event was emitted by the `StakeManager` smart contract.
         #[pallet::weight(10_000)]
-        pub fn witness_staked_event(
+        pub fn witness_staked(
             origin: OriginFor<T>,
             staker_account_id: AccountId<T>,
             amount: T::StakedAmount,
@@ -86,7 +86,8 @@ pub mod pallet {
             if Account::<T>::contains_key(who) {
                 // Vote to call `stake` through the multisig. 
             } else {
-                // Vote to call `refund` through multisig
+                // Account doesn't exist.
+                // Vote to call `refund` through multisig.
             }
 
             Ok(().into())
@@ -114,22 +115,63 @@ pub mod pallet {
 
             Self::deposit_event(Event::Staked(account_id, amount, total_stake));
 
-			todo!()
+            Ok(().into())
 		}
 
-        /// Get FLIP that is held for me by the system, signed by a validator key.
+        /// Get FLIP that is held for me by the system, signed by my validator key.
+        ///
+        /// *QUESTION: should we burn a small amount of FLIP here to disincentivize spam?*
         #[pallet::weight(10_000)]
         pub fn claim(
             origin: OriginFor<T>,
             amount: T::StakedAmount,
+            eth_address: T::EthereumPubKey,
         ) -> DispatchResultWithPostInfo {
-            let who = ensure_signed(origin);
+            let who = ensure_signed(origin)?;
 
-            // TODO: 
-            // Is enough balance available? 
-            // Are any unexpired claims pending? If so, return the pending claim instead.
-            // Reserve the balance so it can't be re-claimed. 
-            // Emit ClaimSigRequested(nonce)
+            // If a claim already exists, return an error. The validator must either redeem their claim voucher
+            // or wait until expiry before creating a new claim.
+            ensure!(!PendingClaims::<T>::contains_key(&who), Error::<T>::PendingClaim);
+
+            // Throw an error if the validator tries to claim too much. Otherwise decrement the stake by the 
+            // amount claimed.
+            Stakes::<T>::try_mutate::<_,_,Error::<T>,_>(&who, |stake| {
+                *stake = stake.checked_sub(&amount).ok_or(Error::<T>::InsufficientStake)?;
+                Ok(())
+            })?;
+
+            // Don't check for overflow here - we don't expect more than 2^32 claims.
+            let nonce = Nonces::<T>::mutate(&who, |nonce| {
+                *nonce += T::Nonce::one();
+                *nonce
+            });
+            
+            // Emit the event requesting that the CFE to generate the claim voucher.
+            Self::deposit_event(Event::<T>::ClaimSigRequested(eth_address, nonce, amount));
+
+            // Assume for now that the siging process is successful and simply insert this claim into
+            // the pending claims. 
+            //
+            // TODO: This should be inserted by the CFE signer process including a valid signature.
+            PendingClaims::<T>::insert(&who, (nonce, amount));
+
+            Ok(().into())
+        }
+
+        /// Witness that a `Claimed` event was emitted by the `StakeManager` smart contract. 
+        ///
+        /// This implies that a valid claim has been 
+        #[pallet::weight(10_000)]
+        pub fn witness_claimed(
+            origin: OriginFor<T>,
+            account_id: AccountId<T>,
+            claimed_amount: T::StakedAmount,
+        ) -> DispatchResultWithPostInfo {
+            let who = ensure_signed(origin)?;
+            debug::info!("Witnessed `claimed` event!");
+
+            // If a claim exists, remove it.
+            // If it doesn't exist, something bad has happened.
 
             Ok(().into())
         }
@@ -149,7 +191,8 @@ pub mod pallet {
             claimed_amount: T::StakedAmount,
         ) -> DispatchResultWithPostInfo {
             let who = ensure_signed(origin)?;
-            debug::info!("Witnessed `claimed` event!");
+            
+            // TODO: Assert that the calling origin is the MultiSig origin.
 
             let (remaining_stake, overflow) = Stakes::<T>::try_mutate_exists::<_,_,Error::<T>,_>(&account_id, |storage| {
                 let mut overflow = false;
@@ -157,8 +200,15 @@ pub mod pallet {
                 *storage = match storage {
                     Some(staked_amount) => {
                         match staked_amount.checked_sub(&claimed_amount) {
+
+                            // If the validator has no more balance, remove the storage entry.
                             Some(balance) if balance == T::StakedAmount::zero() => Ok(None),
+
+                            // Otherwise update the storage entry with the new stake balance. 
                             Some(balance) => Ok(Some(balance)),
+
+                            // `None` indicates overflow occurred. This should never happen, however, if it does, 
+                            // we set an overflow flag and delete the record.
                             None => {
                                 overflow = true;
                                 Ok(None)
@@ -172,9 +222,7 @@ pub mod pallet {
             })?;
 
             // QUESTION: Is it ok to do this, ie. raise an error *after* changing the state? 
-            if overflow {
-                Err(Error::<T>::ExcessFundsClaimed)?;
-            }
+            ensure!(!overflow, Error::<T>::ClaimOverflow);
 
             Self::deposit_event(Event::Claimed(account_id, claimed_amount, remaining_stake));
             Ok(().into())
@@ -187,22 +235,32 @@ pub mod pallet {
     {
         /// A validator has staked some FLIP on the Ethereum chain. [validator_id, stake_added, total_stake]
         Staked(AccountId<T>, T::StakedAmount, T::StakedAmount),
+
         /// A validator has claimed their FLIP on the Ethereum chain. [validator_id, claimed_amount, remaining_stake]
         Claimed(AccountId<T>, T::StakedAmount, T::StakedAmount),
+
         /// The staked amount should be refunded to the provided Ethereum address. [refund_amount, address]
         Refund(T::StakedAmount, T::EthereumPubKey),
+
+        /// A claim request has been made to provided Ethereum address. [address, nonce, amount]
+        ClaimSigRequested(T::EthereumPubKey, T::Nonce, T::StakedAmount),
     }
 
     #[pallet::error]
     pub enum Error<T> {
         /// The account to be staked is not known.
         UnknownAccount,
-        /// The claimant doesn't exist
+
+        /// The claimant doesn't exist.
         UnknownClaimant,
-        /// The claimant tried to claim more funds than were available
-        ExcessFundsClaimed,
+
+        /// The claimant doesn't exist.
+        InsufficientStake,
+
+        /// The claimant tried to claim despite having a claim already pending.
+        PendingClaim,
+
+        /// The claimant tried to claim more funds than were available.
+        ClaimOverflow,
     }
 }
-
-
-
