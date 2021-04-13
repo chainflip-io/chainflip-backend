ea58115350d2b534ae3221d195383ca4f5325b29
Andy Bell <andyjsbell@gmail.com>
2021-04-13 13:40:58 +0200

    boilerplate stuff

---
+++ state-chain/pallets/cf-validator/src/tests.rs
@@ -0,0 +1,30 @@
+use crate::{mock::*, Error};
+use frame_support::{assert_err, assert_ok, storage::StorageMap};
+
+#[test]
+fn bring_forward_era() {
+    new_test_ext().execute_with(|| {
+
+    });
+}
+
+#[test]
+fn push_back_era() {
+    new_test_ext().execute_with(|| {
+
+    });
+}
+
+#[test]
+fn limit_validator_set_size() {
+    new_test_ext().execute_with(|| {
+
+    });
+}
+
+#[test]
+fn force_rotation() {
+    new_test_ext().execute_with(|| {
+
+    });
+}
