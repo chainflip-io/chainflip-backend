3e4848e4c3763c1df33fc3497b8c678dfe31be1a
dlennon <3168260+dandanlen@users.noreply.github.com>
2021-04-14 15:14:15 +0200

    Convert all indentation to tabs (parity style)

--- state-chain/pallets/cf-staking/src/lib.rs
+++ state-chain/pallets/cf-staking/src/lib.rs
@@ -15,83 +15,83 @@ use sp_runtime::{traits::{AtLeast32BitUnsigned, CheckedAdd, CheckedSub, One}};
 
 #[frame_support::pallet]
 pub mod pallet {
-    use super::*;
-    use frame_support::pallet_prelude::*;
-    use frame_system::pallet_prelude::*;
-    use frame_system::pallet::Account;
+	use super::*;
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+	use frame_system::pallet::Account;
 
-    type AccountId<T> = <T as frame_system::Config>::AccountId;
+	type AccountId<T> = <T as frame_system::Config>::AccountId;
 
-    #[pallet::config]
-    pub trait Config: frame_system::Config
-    {
-        /// Standard Event type.
-        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-    
-        /// Numeric type based on the `Balance` type from `Currency` trait. Defined inline for now, but we
-        /// might want to consider using the `Balances` pallet in future.
-        type StakedAmount: Member
-            + FullCodec
-            + Copy
-            + Default
-            + AtLeast32BitUnsigned
-            + MaybeSerializeDeserialize
-            + CheckedSub;
-        
-        type EthereumAddress: Member + FullCodec;
+	#[pallet::config]
+	pub trait Config: frame_system::Config
+	{
+		/// Standard Event type.
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+	
+		/// Numeric type based on the `Balance` type from `Currency` trait. Defined inline for now, but we
+		/// might want to consider using the `Balances` pallet in future.
+		type StakedAmount: Member
+			+ FullCodec
+			+ Copy
+			+ Default
+			+ AtLeast32BitUnsigned
+			+ MaybeSerializeDeserialize
+			+ CheckedSub;
+		
+		type EthereumAddress: Member + FullCodec;
 
-        type Nonce: Member
-            + FullCodec
-            + Copy
-            + Default
-            + AtLeast32BitUnsigned
-            + MaybeSerializeDeserialize
-            + CheckedSub;
-    }
+		type Nonce: Member
+			+ FullCodec
+			+ Copy
+			+ Default
+			+ AtLeast32BitUnsigned
+			+ MaybeSerializeDeserialize
+			+ CheckedSub;
+	}
 
-    #[pallet::pallet]
-    #[pallet::generate_store(pub(super) trait Store)]
-    pub struct Pallet<T>(PhantomData<T>);
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(PhantomData<T>);
 
-    #[pallet::storage]
-    pub type Stakes<T: Config> = StorageMap<_, Identity, AccountId<T>, T::StakedAmount, ValueQuery>;
+	#[pallet::storage]
+	pub type Stakes<T: Config> = StorageMap<_, Identity, AccountId<T>, T::StakedAmount, ValueQuery>;
 
-    #[pallet::storage]
-    pub type PendingClaims<T: Config> = StorageMap<
-        _, 
-        Identity, 
-        AccountId<T>, 
-        T::StakedAmount, 
-        OptionQuery>;
+	#[pallet::storage]
+	pub type PendingClaims<T: Config> = StorageMap<
+		_, 
+		Identity, 
+		AccountId<T>, 
+		T::StakedAmount, 
+		OptionQuery>;
 
-    #[pallet::storage]
-    pub type Nonces<T: Config> = StorageMap<_, Identity, AccountId<T>, T::Nonce, ValueQuery>;
+	#[pallet::storage]
+	pub type Nonces<T: Config> = StorageMap<_, Identity, AccountId<T>, T::Nonce, ValueQuery>;
 
-    #[pallet::hooks]
-    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T>
-    {
-    }
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T>
+	{
+	}
 
-    #[pallet::call]
-    impl<T: Config> Pallet<T>
-    {
-        /// Witness that a `Staked` event was emitted by the `StakeManager` smart contract.
-        #[pallet::weight(10_000)]
-        pub fn witness_staked(
-            origin: OriginFor<T>,
-            staker_account_id: AccountId<T>,
-            amount: T::StakedAmount,
+	#[pallet::call]
+	impl<T: Config> Pallet<T>
+	{
+		/// Witness that a `Staked` event was emitted by the `StakeManager` smart contract.
+		#[pallet::weight(10_000)]
+		pub fn witness_staked(
+			origin: OriginFor<T>,
+			staker_account_id: AccountId<T>,
+			amount: T::StakedAmount,
 			refund_address: T::EthereumAddress,
-        ) -> DispatchResultWithPostInfo {
-            let who = ensure_signed(origin)?;
+		) -> DispatchResultWithPostInfo {
+			let who = ensure_signed(origin)?;
 
-            debug::info!("Witnessed `staked` event!");
-            Ok(().into())
-        }
+			debug::info!("Witnessed `staked` event!");
+			Ok(().into())
+		}
 
-        /// Funds have been staked to an account via the StakeManager smart contract. 
-        ///
-        /// **This is a MultiSig call**
+		/// Funds have been staked to an account via the StakeManager smart contract. 
+		///
+		/// **This is a MultiSig call**
 		#[pallet::weight(10_000)]
 		pub fn staked(
 			origin: OriginFor<T>,
@@ -99,165 +99,165 @@ pub mod pallet {
 			amount: T::StakedAmount,
 			refund_address: T::EthereumAddress,
 		) -> DispatchResultWithPostInfo {
-            Self::ensure_multi(origin)?;
+			Self::ensure_multi(origin)?;
 
-            if Account::<T>::contains_key(&account_id) {
-                let total_stake = Self::add_stake(&account_id, amount)?;
-                Self::deposit_event(Event::Staked(account_id, amount, total_stake));
-            } else {
-                // Account doesn't exist.
-                debug::info!("Unknown staking account id {:?}, proceeding to refund.", account_id);
-                Self::deposit_event(Event::Refund(amount, refund_address));
-            }
-            
-            Ok(().into())
+			if Account::<T>::contains_key(&account_id) {
+				let total_stake = Self::add_stake(&account_id, amount)?;
+				Self::deposit_event(Event::Staked(account_id, amount, total_stake));
+			} else {
+				// Account doesn't exist.
+				debug::info!("Unknown staking account id {:?}, proceeding to refund.", account_id);
+				Self::deposit_event(Event::Refund(amount, refund_address));
+			}
+			
+			Ok(().into())
 		}
 
-        /// Get FLIP that is held for me by the system, signed by my validator key.
-        ///
-        /// *QUESTION: should we burn a small amount of FLIP here to disincentivize spam?*
-        #[pallet::weight(10_000)]
-        pub fn claim(
-            origin: OriginFor<T>,
-            amount: T::StakedAmount,
-            claim_address: T::EthereumAddress,
-        ) -> DispatchResultWithPostInfo {
-            let who = ensure_signed(origin)?;
+		/// Get FLIP that is held for me by the system, signed by my validator key.
+		///
+		/// *QUESTION: should we burn a small amount of FLIP here to disincentivize spam?*
+		#[pallet::weight(10_000)]
+		pub fn claim(
+			origin: OriginFor<T>,
+			amount: T::StakedAmount,
+			claim_address: T::EthereumAddress,
+		) -> DispatchResultWithPostInfo {
+			let who = ensure_signed(origin)?;
 
-            // If a claim already exists, return an error. The validator must either redeem their claim voucher
-            // or wait until expiry before creating a new claim.
-            ensure!(!PendingClaims::<T>::contains_key(&who), Error::<T>::PendingClaim);
+			// If a claim already exists, return an error. The validator must either redeem their claim voucher
+			// or wait until expiry before creating a new claim.
+			ensure!(!PendingClaims::<T>::contains_key(&who), Error::<T>::PendingClaim);
 
-            // Throw an error if the validator tries to claim too much. Otherwise decrement the stake by the 
-            // amount claimed.
-            Stakes::<T>::try_mutate::<_,_,Error::<T>,_>(&who, |stake| {
-                *stake = stake.checked_sub(&amount).ok_or(Error::<T>::InsufficientStake)?;
-                Ok(())
-            })?;
+			// Throw an error if the validator tries to claim too much. Otherwise decrement the stake by the 
+			// amount claimed.
+			Stakes::<T>::try_mutate::<_,_,Error::<T>,_>(&who, |stake| {
+				*stake = stake.checked_sub(&amount).ok_or(Error::<T>::InsufficientStake)?;
+				Ok(())
+			})?;
 
-            // Don't check for overflow here - we don't expect more than 2^32 claims.
-            let nonce = Nonces::<T>::mutate(&who, |nonce| {
-                *nonce += T::Nonce::one();
-                *nonce
-            });
-            
-            // Emit the event requesting that the CFE to generate the claim voucher.
-            Self::deposit_event(Event::<T>::ClaimSigRequested(claim_address, nonce, amount));
+			// Don't check for overflow here - we don't expect more than 2^32 claims.
+			let nonce = Nonces::<T>::mutate(&who, |nonce| {
+				*nonce += T::Nonce::one();
+				*nonce
+			});
+			
+			// Emit the event requesting that the CFE to generate the claim voucher.
+			Self::deposit_event(Event::<T>::ClaimSigRequested(claim_address, nonce, amount));
 
-            // Assume for now that the siging process is successful and simply insert this claim into
-            // the pending claims. 
-            //
-            // TODO: This should be inserted by the CFE signer process including a valid signature.
-            PendingClaims::<T>::insert(&who, amount);
+			// Assume for now that the siging process is successful and simply insert this claim into
+			// the pending claims. 
+			//
+			// TODO: This should be inserted by the CFE signer process including a valid signature.
+			PendingClaims::<T>::insert(&who, amount);
 
-            Ok(().into())
-        }
+			Ok(().into())
+		}
 
-        /// Witness that a `Claimed` event was emitted by the `StakeManager` smart contract. 
-        ///
-        /// This implies that a valid claim has been 
-        #[pallet::weight(10_000)]
-        pub fn witness_claimed(
-            origin: OriginFor<T>,
-            account_id: AccountId<T>,
-            claimed_amount: T::StakedAmount,
-        ) -> DispatchResultWithPostInfo {
-            let who = ensure_signed(origin)?;
-            debug::info!("Witnessed `claimed` event!");
+		/// Witness that a `Claimed` event was emitted by the `StakeManager` smart contract. 
+		///
+		/// This implies that a valid claim has been 
+		#[pallet::weight(10_000)]
+		pub fn witness_claimed(
+			origin: OriginFor<T>,
+			account_id: AccountId<T>,
+			claimed_amount: T::StakedAmount,
+		) -> DispatchResultWithPostInfo {
+			let who = ensure_signed(origin)?;
+			debug::info!("Witnessed `claimed` event!");
 
-            // If a claim exists, remove it.
-            // If it doesn't exist, something bad has happened.
+			// If a claim exists, remove it.
+			// If it doesn't exist, something bad has happened.
 
-            Ok(().into())
-        }
+			Ok(().into())
+		}
 
-        /// Previously staked funds have been reclaimed.
-        ///
-        /// Note that calling this doesn't initiate any protocol changes - the `claim` has already been authorised
-        /// by validator multisig. This merely signals that the claimant has in fact redeemed their funds via the 
-        /// `StakeManager` contract. 
-        ///
-        /// If the claimant tries to claim more funds than are available, we set the claimant's balance to 
-        /// zero and raise an error. 
-        ///
-        /// **This is a MultiSig call**
-        #[pallet::weight(10_000)]
-        pub fn claimed(
-            origin: OriginFor<T>,
-            account_id: AccountId<T>,
-            claimed_amount: T::StakedAmount,
-        ) -> DispatchResultWithPostInfo {
-            Self::ensure_multi(origin)?;
+		/// Previously staked funds have been reclaimed.
+		///
+		/// Note that calling this doesn't initiate any protocol changes - the `claim` has already been authorised
+		/// by validator multisig. This merely signals that the claimant has in fact redeemed their funds via the 
+		/// `StakeManager` contract. 
+		///
+		/// If the claimant tries to claim more funds than are available, we set the claimant's balance to 
+		/// zero and raise an error. 
+		///
+		/// **This is a MultiSig call**
+		#[pallet::weight(10_000)]
+		pub fn claimed(
+			origin: OriginFor<T>,
+			account_id: AccountId<T>,
+			claimed_amount: T::StakedAmount,
+		) -> DispatchResultWithPostInfo {
+			Self::ensure_multi(origin)?;
 
-            let pending_claim_amount = PendingClaims::<T>::get(&account_id).ok_or(Error::<T>::NoPendingClaim)?;
-            
-            ensure!(claimed_amount == pending_claim_amount, Error::<T>::InvalidClaimAmount);
+			let pending_claim_amount = PendingClaims::<T>::get(&account_id).ok_or(Error::<T>::NoPendingClaim)?;
+			
+			ensure!(claimed_amount == pending_claim_amount, Error::<T>::InvalidClaimAmount);
 
-            PendingClaims::<T>::remove(&account_id);
+			PendingClaims::<T>::remove(&account_id);
 
-            Self::deposit_event(Event::Claimed(account_id, claimed_amount));
+			Self::deposit_event(Event::Claimed(account_id, claimed_amount));
 
-            Ok(().into())
-        }
-    }
+			Ok(().into())
+		}
+	}
 
-    #[pallet::event]
-    #[pallet::generate_deposit(pub(super) fn deposit_event)]
-    pub enum Event<T: Config>
-    {
-        /// A validator has staked some FLIP on the Ethereum chain. [validator_id, stake_added, total_stake]
-        Staked(AccountId<T>, T::StakedAmount, T::StakedAmount),
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config>
+	{
+		/// A validator has staked some FLIP on the Ethereum chain. [validator_id, stake_added, total_stake]
+		Staked(AccountId<T>, T::StakedAmount, T::StakedAmount),
 
-        /// A validator has claimed their FLIP on the Ethereum chain. [validator_id, claimed_amount]
-        Claimed(AccountId<T>, T::StakedAmount),
+		/// A validator has claimed their FLIP on the Ethereum chain. [validator_id, claimed_amount]
+		Claimed(AccountId<T>, T::StakedAmount),
 
-        /// The staked amount should be refunded to the provided Ethereum address. [refund_amount, address]
-        Refund(T::StakedAmount, T::EthereumAddress),
+		/// The staked amount should be refunded to the provided Ethereum address. [refund_amount, address]
+		Refund(T::StakedAmount, T::EthereumAddress),
 
-        /// A claim request has been made to provided Ethereum address. [address, nonce, amount]
-        ClaimSigRequested(T::EthereumAddress, T::Nonce, T::StakedAmount),
-    }
+		/// A claim request has been made to provided Ethereum address. [address, nonce, amount]
+		ClaimSigRequested(T::EthereumAddress, T::Nonce, T::StakedAmount),
+	}
 
-    #[pallet::error]
-    pub enum Error<T> {
-        /// The account to be staked is not known.
-        UnknownAccount,
+	#[pallet::error]
+	pub enum Error<T> {
+		/// The account to be staked is not known.
+		UnknownAccount,
 
-        /// An invalid claim has been witnessed: the account has no pending claims.
-        NoPendingClaim,
+		/// An invalid claim has been witnessed: the account has no pending claims.
+		NoPendingClaim,
 
-        /// An invalid claim has been witnessed: the amount claimed does not match the pending claim amount.
-        InvalidClaimAmount,
+		/// An invalid claim has been witnessed: the amount claimed does not match the pending claim amount.
+		InvalidClaimAmount,
 
-        /// The claimant doesn't exist.
-        InsufficientStake,
+		/// The claimant doesn't exist.
+		InsufficientStake,
 
-        /// The claimant tried to claim despite having a claim already pending.
-        PendingClaim,
+		/// The claimant tried to claim despite having a claim already pending.
+		PendingClaim,
 
-        /// The claimant tried to claim more funds than were available. 
-        ClaimOverflow,
+		/// The claimant tried to claim more funds than were available. 
+		ClaimOverflow,
 
-        /// Stake amount caused overflow on addition. Should never happen.
-        StakeOverflow,
-    }
+		/// Stake amount caused overflow on addition. Should never happen.
+		StakeOverflow,
+	}
 }
 
 impl<T: Config> Module<T> {
-    fn add_stake(account_id: &T::AccountId, amount: T::StakedAmount) -> Result<T::StakedAmount, Error<T>> {
-        Stakes::<T>::try_mutate(
-            account_id, 
-            |stake| {
-                *stake = stake
-                    .checked_add(&amount)
-                    .ok_or(Error::<T>::StakeOverflow)?;
-                
-                Ok(*stake)
-            })
-    }
+	fn add_stake(account_id: &T::AccountId, amount: T::StakedAmount) -> Result<T::StakedAmount, Error<T>> {
+		Stakes::<T>::try_mutate(
+			account_id, 
+			|stake| {
+				*stake = stake
+					.checked_add(&amount)
+					.ok_or(Error::<T>::StakeOverflow)?;
+				
+				Ok(*stake)
+			})
+	}
 
-    fn ensure_multi(origin: OriginFor<T>) -> Result<(), BadOrigin> {
-        // TODO: replace this with a dedicated MULTISIG user instead of root.
-        ensure_root(origin)
-    }
+	fn ensure_multi(origin: OriginFor<T>) -> Result<(), BadOrigin> {
+		// TODO: replace this with a dedicated MULTISIG user instead of root.
+		ensure_root(origin)
+	}
 }
--- state-chain/pallets/cf-staking/src/tests.rs
+++ state-chain/pallets/cf-staking/src/tests.rs
@@ -5,129 +5,129 @@ const ETH_DUMMY_ADDR: <Test as Config>::EthereumAddress = 0u64;
 
 #[test]
 fn staked_amount_is_added_and_subtracted() {
-    new_test_ext().execute_with(|| {
-        let (stake_a1, stake_a2) = (45u128, 21u128);
-        let claim_a = 44u128;
-        let stake_b = 78u128;
-        let claim_b = 78u128;
+	new_test_ext().execute_with(|| {
+		let (stake_a1, stake_a2) = (45u128, 21u128);
+		let claim_a = 44u128;
+		let stake_b = 78u128;
+		let claim_b = 78u128;
 
-        // Dispatch a signed extrinsic to stake some FLIP.
-        assert_ok!(StakeManager::staked(Origin::root(), ALICE, stake_a1, ETH_DUMMY_ADDR));
-        // Read pallet storage and assert the balance was added.
-        assert_eq!(Stakes::<Test>::get(ALICE), stake_a1);
+		// Dispatch a signed extrinsic to stake some FLIP.
+		assert_ok!(StakeManager::staked(Origin::root(), ALICE, stake_a1, ETH_DUMMY_ADDR));
+		// Read pallet storage and assert the balance was added.
+		assert_eq!(Stakes::<Test>::get(ALICE), stake_a1);
 
-        // Add some more
-        assert_ok!(StakeManager::staked(Origin::root(), ALICE, stake_a2, ETH_DUMMY_ADDR));
-        assert_ok!(StakeManager::staked(Origin::root(), BOB, stake_b, ETH_DUMMY_ADDR));
+		// Add some more
+		assert_ok!(StakeManager::staked(Origin::root(), ALICE, stake_a2, ETH_DUMMY_ADDR));
+		assert_ok!(StakeManager::staked(Origin::root(), BOB, stake_b, ETH_DUMMY_ADDR));
 
-        // Check storage again.
-        assert_eq!(Stakes::<Test>::get(ALICE), stake_a1 + stake_a2);
-        assert_eq!(Stakes::<Test>::get(BOB), stake_b);
+		// Check storage again.
+		assert_eq!(Stakes::<Test>::get(ALICE), stake_a1 + stake_a2);
+		assert_eq!(Stakes::<Test>::get(BOB), stake_b);
 
-        // Now claim some FLIP.
-        assert_ok!(StakeManager::claim(Origin::signed(ALICE), claim_a, ETH_DUMMY_ADDR));
-        assert_ok!(StakeManager::claim(Origin::signed(BOB), claim_b, ETH_DUMMY_ADDR));
+		// Now claim some FLIP.
+		assert_ok!(StakeManager::claim(Origin::signed(ALICE), claim_a, ETH_DUMMY_ADDR));
+		assert_ok!(StakeManager::claim(Origin::signed(BOB), claim_b, ETH_DUMMY_ADDR));
 
-        // Make sure it was subtracted.
-        assert_eq!(Stakes::<Test>::get(ALICE), stake_a1 + stake_a2 - claim_a);
-        assert_eq!(Stakes::<Test>::get(BOB), stake_b - claim_b);
+		// Make sure it was subtracted.
+		assert_eq!(Stakes::<Test>::get(ALICE), stake_a1 + stake_a2 - claim_a);
+		assert_eq!(Stakes::<Test>::get(BOB), stake_b - claim_b);
 
-        // Check the pending claims
-        assert_eq!(PendingClaims::<Test>::get(ALICE), Some(claim_a));
-        assert_eq!(PendingClaims::<Test>::get(BOB), Some(claim_b));
-    });
+		// Check the pending claims
+		assert_eq!(PendingClaims::<Test>::get(ALICE), Some(claim_a));
+		assert_eq!(PendingClaims::<Test>::get(BOB), Some(claim_b));
+	});
 }
 
 #[test]
 fn claiming_unclaimable_is_err() {
-    new_test_ext().execute_with(|| {
-        let stake = 100_000u128;
+	new_test_ext().execute_with(|| {
+		let stake = 100_000u128;
 
-        // Claim FLIP before it is staked.
-        assert_err!(
-            StakeManager::claim(Origin::signed(ALICE), stake, ETH_DUMMY_ADDR), 
-            <Error<Test>>::InsufficientStake
-        );
+		// Claim FLIP before it is staked.
+		assert_err!(
+			StakeManager::claim(Origin::signed(ALICE), stake, ETH_DUMMY_ADDR), 
+			<Error<Test>>::InsufficientStake
+		);
 
-        // Make sure storage hasn't been touched.
-        assert_eq!(Stakes::<Test>::contains_key(ALICE), false);
+		// Make sure storage hasn't been touched.
+		assert_eq!(Stakes::<Test>::contains_key(ALICE), false);
 
-        // Stake some FLIP.
-        assert_ok!(StakeManager::staked(Origin::root(), ALICE, stake, ETH_DUMMY_ADDR));
+		// Stake some FLIP.
+		assert_ok!(StakeManager::staked(Origin::root(), ALICE, stake, ETH_DUMMY_ADDR));
 
-        // Claim FLIP from another account.
-        assert_err!(
-            StakeManager::claim(Origin::signed(BOB), stake, ETH_DUMMY_ADDR), 
-            <Error<Test>>::InsufficientStake
-        );
-        
-        // Make sure storage hasn't been touched.
-        assert_eq!(Stakes::<Test>::get(ALICE), stake);
-    });
+		// Claim FLIP from another account.
+		assert_err!(
+			StakeManager::claim(Origin::signed(BOB), stake, ETH_DUMMY_ADDR), 
+			<Error<Test>>::InsufficientStake
+		);
+		
+		// Make sure storage hasn't been touched.
+		assert_eq!(Stakes::<Test>::get(ALICE), stake);
+	});
 }
 
 #[test]
 fn cannot_double_claim() {
-    new_test_ext().execute_with(|| {
-        let (stake_a1, stake_a2) = (45u128, 21u128);
+	new_test_ext().execute_with(|| {
+		let (stake_a1, stake_a2) = (45u128, 21u128);
 
-        // Stake some FLIP.
-        assert_ok!(StakeManager::staked(Origin::root(), ALICE, stake_a1 + stake_a2, ETH_DUMMY_ADDR));
+		// Stake some FLIP.
+		assert_ok!(StakeManager::staked(Origin::root(), ALICE, stake_a1 + stake_a2, ETH_DUMMY_ADDR));
 
-        // Claim a portion.
-        assert_ok!(StakeManager::claim(Origin::signed(ALICE), stake_a1, ETH_DUMMY_ADDR));
+		// Claim a portion.
+		assert_ok!(StakeManager::claim(Origin::signed(ALICE), stake_a1, ETH_DUMMY_ADDR));
 
-        // Claiming the rest should not be possible yet.
-        assert_err!(
-            StakeManager::claim(Origin::signed(ALICE), stake_a2, ETH_DUMMY_ADDR),
-            <Error<Test>>::PendingClaim
-        );
+		// Claiming the rest should not be possible yet.
+		assert_err!(
+			StakeManager::claim(Origin::signed(ALICE), stake_a2, ETH_DUMMY_ADDR),
+			<Error<Test>>::PendingClaim
+		);
 
-        // Redeem the first claim.
-        assert_ok!(StakeManager::claimed(Origin::root(), ALICE, stake_a1));
+		// Redeem the first claim.
+		assert_ok!(StakeManager::claimed(Origin::root(), ALICE, stake_a1));
 
-        // Should now be able to claim the rest.
-        assert_ok!(StakeManager::claim(Origin::signed(ALICE), stake_a2, ETH_DUMMY_ADDR));
+		// Should now be able to claim the rest.
+		assert_ok!(StakeManager::claim(Origin::signed(ALICE), stake_a2, ETH_DUMMY_ADDR));
 
-        // Redeem the rest.
-        assert_ok!(StakeManager::claimed(Origin::root(), ALICE, stake_a2));
+		// Redeem the rest.
+		assert_ok!(StakeManager::claimed(Origin::root(), ALICE, stake_a2));
 
-        // Remaining stake should be zero
-        assert_eq!(Stakes::<Test>::get(ALICE), 0u128);
-    });
+		// Remaining stake should be zero
+		assert_eq!(Stakes::<Test>::get(ALICE), 0u128);
+	});
 }
 
 #[test]
 fn staked_and_claimed_events_must_match() {
-    new_test_ext().execute_with(|| {
-        let stake = 45u128;
+	new_test_ext().execute_with(|| {
+		let stake = 45u128;
 
-        // Stake some FLIP.
-        assert_ok!(StakeManager::staked(Origin::root(), ALICE, stake, ETH_DUMMY_ADDR));
+		// Stake some FLIP.
+		assert_ok!(StakeManager::staked(Origin::root(), ALICE, stake, ETH_DUMMY_ADDR));
 
-        // Claim it.
-        assert_ok!(StakeManager::claim(Origin::signed(ALICE), stake, ETH_DUMMY_ADDR));
+		// Claim it.
+		assert_ok!(StakeManager::claim(Origin::signed(ALICE), stake, ETH_DUMMY_ADDR));
 
-        // Invalid Claimed Event from Ethereum: wrong account.
-        assert_err!(StakeManager::claimed(Origin::root(), BOB, stake), <Error<Test>>::NoPendingClaim);
+		// Invalid Claimed Event from Ethereum: wrong account.
+		assert_err!(StakeManager::claimed(Origin::root(), BOB, stake), <Error<Test>>::NoPendingClaim);
 
-        // Invalid Claimed Event from Ethereum: wrong amount.
-        assert_err!(StakeManager::claimed(Origin::root(), ALICE, stake - 1), <Error<Test>>::InvalidClaimAmount);
+		// Invalid Claimed Event from Ethereum: wrong amount.
+		assert_err!(StakeManager::claimed(Origin::root(), ALICE, stake - 1), <Error<Test>>::InvalidClaimAmount);
 
-        // Valid Claimed Event from Ethereum.
-        assert_ok!(StakeManager::claimed(Origin::root(), ALICE, stake));
-    });
+		// Valid Claimed Event from Ethereum.
+		assert_ok!(StakeManager::claimed(Origin::root(), ALICE, stake));
+	});
 }
 
 #[test]
 fn multisig_endpoints_cant_be_called_from_invalid_origins() {
-    new_test_ext().execute_with(|| {
-        let stake = 1u128;
+	new_test_ext().execute_with(|| {
+		let stake = 1u128;
 
-        assert_err!(StakeManager::staked(Origin::none(), ALICE, stake, ETH_DUMMY_ADDR), BadOrigin);
-        assert_err!(StakeManager::staked(Origin::signed(Default::default()), ALICE, stake, ETH_DUMMY_ADDR), BadOrigin);
+		assert_err!(StakeManager::staked(Origin::none(), ALICE, stake, ETH_DUMMY_ADDR), BadOrigin);
+		assert_err!(StakeManager::staked(Origin::signed(Default::default()), ALICE, stake, ETH_DUMMY_ADDR), BadOrigin);
 
-        assert_err!(StakeManager::claimed(Origin::none(), ALICE, stake), BadOrigin);
-        assert_err!(StakeManager::claimed(Origin::signed(Default::default()), ALICE, stake), BadOrigin);
-    });
+		assert_err!(StakeManager::claimed(Origin::none(), ALICE, stake), BadOrigin);
+		assert_err!(StakeManager::claimed(Origin::signed(Default::default()), ALICE, stake), BadOrigin);
+	});
 }
