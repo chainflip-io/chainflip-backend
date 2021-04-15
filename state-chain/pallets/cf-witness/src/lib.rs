0762bf8f003e085979c7f6b46a9ce1e5e6dde8fe
dlennon <3168260+dandanlen@users.noreply.github.com>
2021-04-15 18:21:09 +0200

    Add some comments to outline functionality

--- state-chain/pallets/cf-witness/src/lib.rs
+++ state-chain/pallets/cf-witness/src/lib.rs
@@ -14,7 +14,9 @@ mod tests;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
+	use super::*;
+	use codec::FullCodec;
+    use frame_support::{dispatch::{Dispatchable, GetDispatchInfo, PostDispatchInfo}, pallet_prelude::*};
 	use frame_system::pallet_prelude::*;
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
@@ -22,19 +24,34 @@ pub mod pallet {
 	pub trait Config: frame_system::Config {
 		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+
+		/// The overarching call type.
+		type Call: Member + FullCodec
+			+ Dispatchable<Origin=Self::Origin, PostInfo=PostDispatchInfo>
+			+ GetDispatchInfo 
+			+ From<frame_system::Call<Self>>;
+
+		type Validator: ProvideValidators<Self>;
 	}
+
+	/// Just a bunch of bytes, but they should decode to a valid `Call`.
+	type OpaqueCall = Vec<u8>;
+
+	/// A hash to index the call by.
+	type CallHash = [u8; 20];
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
-	// The pallet's runtime storage items.
-	// https://substrate.dev/docs/en/knowledgebase/runtime/storage
 	#[pallet::storage]
 	#[pallet::getter(fn something)]
-	// Learn more about declaring storage items:
-	// https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
-	pub type Something<T> = StorageValue<_, u32>;
+	pub type Calls<T> = StorageMap<
+		_, 
+		Identity,
+		CallHash, 
+		OpaqueCall, 
+		OptionQuery>;
 
 	// Pallets use events to inform users when important changes are made.
 	// https://substrate.dev/docs/en/knowledgebase/runtime/events
@@ -42,9 +59,8 @@ pub mod pallet {
 	#[pallet::metadata(T::AccountId = "AccountId")]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
-		SomethingStored(u32, T::AccountId),
+		/// Some external event has been witnessed [who, what]
+		WitnessReceived(T::AccountId, OpaqueCall),
 	}
 
 	// Errors inform users that something went wrong.
@@ -57,48 +73,40 @@ pub mod pallet {
 	}
 
 	#[pallet::hooks]
-	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+		// TODO: 
+		//   - think about using a hook to apply voted-on extrinsics.
+		//   - check the era and maybe update the validator set: store validator set as an IndexSet
+		//			(see: https://substrate.dev/rustdocs/v3.0.0/indexmap/set/struct.IndexSet.html)
+		// 			This way, the set of approvals can be stored in a BitVec which should be the quite memory-efficient.
+		
+	}
 
-	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-	// These functions materialize as "extrinsics", which are often compared to transactions.
-	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
-	impl<T:Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
+	impl<T: Config> Pallet<T> {
+		/// Called as a witness of some external event. The call parameter is the resultant extrinsic. This can be 
+		/// thought of as a vote for the encoded [`Call`](crate::Pallet::Call) value. 
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResultWithPostInfo {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
+		pub fn witness(
+			origin: OriginFor<T>, 
+			event: <T as Config>::Call) -> DispatchResultWithPostInfo {
 			let who = ensure_signed(origin)?;
 
-			// Update storage.
-			<Something<T>>::put(something);
+			// TODO:
+			// - look up the signer in the list of validators
+			// - register the vote in storage
+			// - Maybe (?) check if threshold is reached and apply the voted-on Call.
 
-			// Emit an event.
-			Self::deposit_event(Event::SomethingStored(something, who));
 			// Return a successful DispatchResultWithPostInfo
 			Ok(().into())
 		}
-
-		/// An example dispatchable that may throw a custom error.
-		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResultWithPostInfo {
-			let _who = ensure_signed(origin)?;
-
-			// Read a value from storage.
-			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					<Something<T>>::put(new);
-					Ok(().into())
-				},
-			}
-		}
 	}
+}
+
+pub trait ProvideValidators<T: Config> {
+	type Era;
+
+	fn validators(&self) -> Vec<<T as frame_system::Config>::AccountId>;
+
+	fn current_era(&self) -> Self::Era;
 }
