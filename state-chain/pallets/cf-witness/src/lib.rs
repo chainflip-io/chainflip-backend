b40565abdee0e77582728af8cd324e2f7c95a89a
dlennon <3168260+dandanlen@users.noreply.github.com>
2021-04-20 20:46:18 +0200

    Implementation of witness extrinsic

--- state-chain/pallets/cf-witness/src/lib.rs
+++ state-chain/pallets/cf-witness/src/lib.rs
@@ -1,9 +1,4 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-
-/// Edit this file to define custom logic or remove it if it is not needed.
-/// Learn more about FRAME and the core library of Substrate FRAME pallets:
-/// <https://substrate.dev/docs/en/knowledgebase/runtime/frame>
-
 pub use pallet::*;
 
 #[cfg(test)]
@@ -12,12 +7,17 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
+use codec::FullCodec;
+use frame_support::pallet_prelude::Member;
+use sp_runtime::traits::AtLeast32BitUnsigned;
+
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
-	use codec::FullCodec;
+	use bitvec::prelude::*;
     use frame_support::{dispatch::{Dispatchable, GetDispatchInfo, PostDispatchInfo}, pallet_prelude::*};
 	use frame_system::pallet_prelude::*;
+    use sp_core::blake2_256;
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
@@ -31,55 +31,84 @@ pub mod pallet {
 			+ GetDispatchInfo 
 			+ From<frame_system::Call<Self>>;
 
-		type Validator: ProvideValidators<Self>;
+		// TODO: Investigate frame_support::ValidatorSet incase this already provides the required functionality.
+		type ValidatorProvider: ValidatorProvider<Self>;
+
+		type ValidatorId: Member + FullCodec + From<<Self as frame_system::Config>::AccountId>;
 	}
+
+	/// Alias for the `Epoch` type defined by the `ValidatorProvider`.
+	type Epoch<T> = <<T as Config>::ValidatorProvider as ValidatorProvider<T>>::Epoch;
 
 	/// Just a bunch of bytes, but they should decode to a valid `Call`.
 	type OpaqueCall = Vec<u8>;
 
 	/// A hash to index the call by.
-	type CallHash = [u8; 20];
+	type CallHash = [u8; 32];
+
+	/// Convenience alias for a collection of bits representing the votes of each validator.
+	type VoteMask = BitSlice<Msb0, u8>;
 
 	#[pallet::pallet]
-	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
 	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	pub type Calls<T> = StorageMap<
-		_, 
-		Identity,
-		CallHash, 
-		OpaqueCall, 
-		OptionQuery>;
+	pub type Calls<T: Config> = StorageDoubleMap<
+		_,
+		Blake2_128Concat,
+		Epoch<T>,
+		Blake2_128Concat,
+		<T as Config>::Call, 
+		Vec<u8>
+	>;
 
-	// Pallets use events to inform users when important changes are made.
-	// https://substrate.dev/docs/en/knowledgebase/runtime/events
+	#[pallet::storage]
+	pub(super) type ValidatorIndex<T: Config> = StorageDoubleMap<
+		_, 
+		Blake2_128Concat,
+		Epoch<T>,
+		Identity,
+		<T as frame_system::Config>::AccountId,
+		u64
+	>;
+
+	// QUESTION: Should this be managed here or in the sessions pallet, for example? (The *active* validator set and 
+	// therefore the threshold might change due to unavailable nodes, slashing etc.)
+	#[pallet::storage]
+	pub(super) type ConsensusThreshold<T> = StorageValue<_, u32, ValueQuery>;
+	
 	#[pallet::event]
 	#[pallet::metadata(T::AccountId = "AccountId")]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		/// Some external event has been witnessed [who, what]
-		WitnessReceived(T::AccountId, OpaqueCall),
+		/// A new call has been registered for voting [call, call_sig]
+		NewEvent(OpaqueCall, CallHash),
+
+		/// Some external event has been witnessed [call_sig, who]
+		WitnessReceived(CallHash, <T as Config>::ValidatorId),
+
+		/// The witness threshold has been reached [call_sig, num_votes]
+		ThresholdReached(CallHash),
 	}
 
 	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
-		/// Error names should be descriptive.
-		NoneValue,
-		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
+		/// CRITICAL: The validator index is out of bounds. This should never happen. 
+		ValidatorIndexOutOfBounds,
+		/// Witness is not a validator.
+		UnauthorizedWitness,
+		/// A witness vote was cast twice by the same validator.
+		DuplicateWitness
 	}
 
 	#[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
 		// TODO: 
-		//   - think about using a hook to apply voted-on extrinsics.
+		//   - think about using a hook to apply voted-on extrinsics in batches instead of inline in the witness fn.
 		//   - check the era and maybe update the validator set: store validator set as an IndexSet
 		//			(see: https://substrate.dev/rustdocs/v3.0.0/indexmap/set/struct.IndexSet.html)
 		// 			This way, the set of approvals can be stored in a BitVec which should be the quite memory-efficient.
-		
 	}
 
 	#[pallet::call]
@@ -89,24 +118,66 @@ pub mod pallet {
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
 		pub fn witness(
 			origin: OriginFor<T>, 
-			event: <T as Config>::Call) -> DispatchResultWithPostInfo {
+			call: <T as Config>::Call) -> DispatchResultWithPostInfo {
 			let who = ensure_signed(origin)?;
 
-			// TODO:
-			// - look up the signer in the list of validators
-			// - register the vote in storage
-			// - Maybe (?) check if threshold is reached and apply the voted-on Call.
+			let epoch: Epoch<T> = T::ValidatorProvider::current_epoch();
+			let num_validators = T::ValidatorProvider::num_validators() as usize;
+			
+			// Look up the signer in the list of validators
+			let index = ValidatorIndex::<T>::get(&epoch, &who)
+				.ok_or(Error::<T>::UnauthorizedWitness)? as usize;
+			
+			// Register the vote
+			let num_votes = Calls::<T>::try_mutate::<_, _, _, Error::<T>, _>(&epoch, &call, |buffer| {
+				// If there is no storage item, create an empty one.
+				if buffer.is_none() {
+					let empty_mask = bits![Msb0, u8; 0].repeat(num_validators);
+					*buffer = Some(empty_mask.into_vec())
+				}
 
-			// Return a successful DispatchResultWithPostInfo
+				let bytes = buffer.as_mut().expect("Checked for none condition above, this will never panic;");
+
+				// Convert to an addressable bitmask
+				let bits = VoteMask::from_slice_mut(bytes)
+					.expect("Only panics if the slice size exceeds the max; The number of validators should never exceed this;");
+
+				// Return an error if already voted, otherwise set the indexed bit to `true` to indicate a vote.
+				if bits[index] {
+					Err(Error::<T>::DuplicateWitness)?
+				} else {
+					let mut vote = bits.get_mut(index).ok_or(Error::<T>::ValidatorIndexOutOfBounds)?;
+					*vote = true;
+				}
+
+				Ok(bits.count_ones())
+			})?;
+
+			let call_hash = call.using_encoded(|bytes| blake2_256(bytes));
+			let threshold = ConsensusThreshold::<T>::get() as usize;
+
+			Self::deposit_event(Event::<T>::WitnessReceived(call_hash, who.into()));
+
+			// Check if threshold is reached and, if so, apply the voted-on Call.
+			if num_votes == threshold {
+				Self::deposit_event(Event::<T>::ThresholdReached(call_hash));
+			}
+
+			// QUESTION: Do we want to allow voting to continue *after* the call has been made? Might be useful for 
+			// slashing?
+
 			Ok(().into())
 		}
 	}
 }
 
-pub trait ProvideValidators<T: Config> {
-	type Era;
+pub trait ValidatorProvider<T: Config> {
+	type Epoch: Member + FullCodec + AtLeast32BitUnsigned;
+	type Validatorid: From<<T as frame_system::Config>::AccountId>;
 
-	fn validators(&self) -> Vec<<T as frame_system::Config>::AccountId>;
+	fn num_validators() -> u32;
 
-	fn current_era(&self) -> Self::Era;
+	fn validators() -> Vec<Self::Validatorid>;
+
+	fn current_epoch() -> Self::Epoch;
 }
