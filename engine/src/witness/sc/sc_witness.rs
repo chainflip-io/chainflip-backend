826aa33856233f844a7a45377eb9481ab6e14f4c
Kyle Zsembery <zsembery.kyle@gmail.com>
2021-04-22 10:41:37 +1000

    wip test

--- engine/src/witness/sc/sc_witness.rs
+++ engine/src/witness/sc/sc_witness.rs
@@ -2,3 +2,13 @@
 pub fn start() {
     println!("Start the state chain witness");
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn testing_stuff() {
+        start();
+    }
+}
