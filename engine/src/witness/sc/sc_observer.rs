f4a8e0399e290c53732528068b196cfbd0ed5123
Kyle Zsembery <zsembery.kyle@gmail.com>
2021-04-28 16:09:08 +1000

    clean up and decoding event example in tests

--- engine/src/witness/sc/sc_observer.rs
+++ engine/src/witness/sc/sc_observer.rs
@@ -20,24 +20,12 @@ use crate::{
 
 use super::{runtime::StateChainRuntime, staking::ClaimSigRequested};
 
-/// TODO: Make this sync
-/// Kick of the state chain observer process
+/// Kick off the state chain observer process
 pub async fn start<M: 'static + IMQClient + Send + Sync>(mq_client: Arc<Mutex<M>>) {
     println!("Start the state chain witness with subxt");
 
     subscribe_to_events(mq_client).await;
 }
-
-// can't borrow decoder and then return the EventSub object :(
-// async fn create_subscription<'a, E: substrate_subxt::Event<StateChainRuntime>>(
-//     client: Client<StateChainRuntime>,
-// ) -> Result<EventSubscription<'a, StateChainRuntime>> {
-//     let sub = client.subscribe_finalized_events().await?;
-//     let decoder = client.events_decoder();
-//     let mut sub = EventSubscription::new(sub, decoder);
-//     sub.filter_event::<E>();
-//     Ok(sub)
-// }
 
 /// Create a substrate subxt client over the StateChainRuntime
 async fn create_subxt_client() -> Result<Client<StateChainRuntime>> {
@@ -52,49 +40,42 @@ async fn create_subxt_client() -> Result<Client<StateChainRuntime>> {
 }
 
 async fn subscribe_to_events<M: 'static + IMQClient + Send + Sync>(mq_client: Arc<Mutex<M>>) {
-    let client = create_subxt_client().await.unwrap();
-
-    // TODO: subscribe_events -> finalised events
-
-    // ===== DataAddedEvents - for easy testing ====
-    let client = client.clone();
-    let sub = client.subscribe_finalized_events().await.unwrap();
-    let decoder = client.events_decoder();
-    let mut sub = EventSubscription::new(sub, decoder);
-
-    // TOOD: Spawn a thread. For each? or for all subscriptions? atm I think the latter, not much to gain for extra threads here
+    let mq_c = mq_client.clone();
 
     loop {
-        let raw_event = sub.next().await.unwrap().unwrap();
-        // let raw_sig_claim_requested = sig_claim_requested_events.next().await.unwrap().unwrap();
-        let mq_c = mq_client.clone();
-
+        let mq_c = mq_c.clone();
         tokio::spawn(async move {
-            println!("Raw event:\n{:#?}", raw_event);
+            let client = create_subxt_client().await.unwrap();
 
-            let subject: Option<Subject> = subject_from_raw_event(&raw_event);
+            let client = client.clone();
+            let sub = client.subscribe_finalized_events().await.unwrap();
+            let decoder = client.events_decoder();
+            let mut sub = EventSubscription::new(sub, decoder);
 
-            // Have some example consumer somewhere, of how to do this, but I think the raw bytes should be sent straight to the message queue
-            // why serialize / deserialize when we can just decode?
-            // let event =
-            //     DataAddedEvent::<StateChainRuntime>::decode(&mut &raw_data_added.data[..]).unwrap();
+            loop {
+                let raw_event = sub.next().await.unwrap().unwrap();
+                println!("Raw event:\n{:#?}", raw_event);
 
-            if let Some(subject) = subject {
-                mq_c.lock()
-                    .await
-                    .publish(subject, &raw_event.data)
-                    .await
-                    .unwrap();
-            } else {
-                println!(
-                    "Unable to resolve event: {:#?} to a known event type",
-                    raw_event
-                )
+                let subject: Option<Subject> = subject_from_raw_event(&raw_event);
+
+                if let Some(subject) = subject {
+                    mq_c.lock()
+                        .await
+                        .publish(subject, &raw_event.data)
+                        .await
+                        .unwrap();
+                } else {
+                    println!(
+                        "Unable to resolve event: {:#?} to a known event type",
+                        raw_event
+                    )
+                }
             }
         });
     }
 }
 
+/// Returns the subject to publish the data of a raw event to
 fn subject_from_raw_event(event: &RawEvent) -> Option<Subject> {
     let subject = match event.module.as_str() {
         "System" => None,
@@ -114,19 +95,19 @@ fn subject_from_raw_event(event: &RawEvent) -> Option<Subject> {
 #[cfg(test)]
 mod tests {
 
+    use std::marker::PhantomData;
+
     use nats_test_server::NatsTestServer;
+    use substrate_subxt::system::ExtrinsicSuccessEvent;
 
     use crate::mq::mq_mock::MockMQ;
+
+    use frame_support::weights::{DispatchClass, DispatchInfo, Pays};
 
     use super::*;
 
     #[tokio::test]
     async fn run_test() {
-        // let event = substrate_subxt::RawEvent {
-        //     module: "Transactions".to_string(),
-        //     variant: "DataAdded".to_string(),
-        //     data: "Hello".as_bytes().to_owned(),
-        // };
         let server = NatsTestServer::build().spawn();
         let test_mq_client = MockMQ::new(&server).await;
         let test_mq_client = Arc::new(Mutex::new(test_mq_client));
@@ -158,25 +139,30 @@ mod tests {
         assert_eq!(subject, None);
     }
 
-    // TODO: Test decodinng of each of the custom events using some raw data
+    // This test can probably go elsewhere later, but for now this works
+    // TOOD: Add all The CF specific events here
+    #[test]
+    fn example_event_decoding() {
+        let raw_event = RawEvent {
+            module: "System".to_string(),
+            variant: "ExtrinsicSuccess".to_string(),
+            // This is not random data, it decodes to the ExtrinsicSuccessEvent below
+            data: hex::decode("482d7c09000000000200").unwrap(),
+        };
+        let event =
+            ExtrinsicSuccessEvent::<StateChainRuntime>::decode(&mut &raw_event.data[..]).unwrap();
+
+        println!("Here's the event: {:#?}", event);
+
+        let success_event: ExtrinsicSuccessEvent<StateChainRuntime> = ExtrinsicSuccessEvent {
+            _runtime: PhantomData,
+            info: DispatchInfo {
+                weight: 159133000,
+                class: DispatchClass::Mandatory,
+                pays_fee: Pays::Yes,
+            },
+        };
+
+        assert_eq!(event, success_event);
+    }
 }
-
-// RawEvent {
-//     module: "Transactions",
-//     variant: "DataAdded",
-//     data: "8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a480c617364",
-// }
-
-// RawEvent {
-//     module: "System",
-//     variant: "ExtrinsicSuccess",
-//     data: "482d7c09000000000200",
-// }
-// Here's the event to be added: ExtrinsicSuccessEvent {
-//     _runtime: PhantomData,
-//     info: DispatchInfo {
-//         weight: 159133000,
-//         class: DispatchClass::Mandatory,
-//         pays_fee: Pays::Yes,
-//     },
-// }
