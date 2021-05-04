2dd88331afe0d0e8e50fcddee15bf97bcabe8a9b
Kyle Zsembery <zsembery.kyle@gmail.com>
2021-05-04 12:04:07 +1000

    WIP config

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,6 +1,20 @@
+use std::collections::HashMap;
+
+extern crate config;
+
 mod mq;
 mod p2p;
 
 fn main() {
-    println!("Hello");
+    let mut config_options = config::Config::default();
+    config_options
+        .merge(config::File::with_name("../config/Default"))
+        .expect("Could load default config");
+
+    println!(
+        "{:?}",
+        config_options
+            .try_into::<HashMap<String, String>>()
+            .unwrap()
+    );
 }
