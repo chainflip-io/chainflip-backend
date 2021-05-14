88a796d45934fcafab78dbb3f4935b7429c84e25
Andy Bell <andyjsbell@gmail.com>
2021-05-14 14:52:17 +0200

    Update some scripts for launching nodes

--- state-chain/scripts/start-node-1.sh
+++ state-chain/scripts/start-node-1.sh
@@ -1,10 +1,10 @@
 # start node01
-../target/release/state-chain-node \
+cargo run -p state-chain-node -- \
   --base-path /tmp/node01 \
-  --chain ./customSpecRaw.json \
-  --port 30333 \
-  --ws-port 9945 \
-  --rpc-port 9933 \
+  --chain ./cf-chainspec-raw.json \
+  --port 30335 \
+  --ws-port 9946 \
+  --rpc-port 9935 \
   --telemetry-url 'wss://telemetry.polkadot.io/submit/ 0' \
   --validator \
   --rpc-methods Unsafe \
