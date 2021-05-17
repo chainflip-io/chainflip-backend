7e98edb64a07786cf81a992b0c310d5fb083255e
Kyle Zsembery <zsembery.kyle@gmail.com>
2021-05-17 11:24:03 +1000

    ClaimRegistered event parsing and tests

--- engine/src/eth/stake_manager/stake_manager_sink.rs
+++ engine/src/eth/stake_manager/stake_manager_sink.rs
@@ -27,9 +27,13 @@ impl<M: IMQClient + Send + Sync> StakeManagerSink<M> {
 impl<M: IMQClient + Send + Sync> EventSink<StakingEvent> for StakeManagerSink<M> {
     async fn process_event(&self, event: StakingEvent) -> anyhow::Result<()> {
         let subject: Option<Subject> = match event {
-            StakingEvent::Claimed(_, _) | StakingEvent::Staked(_, _) => Some(Subject::StakeManager),
+            StakingEvent::ClaimExecuted(_, _) | StakingEvent::Staked(_, _) => {
+                Some(Subject::StakeManager)
+            }
             StakingEvent::EmissionChanged(_, _) => None,
             StakingEvent::MinStakeChanged(_, _) => None,
+            // TODO: Where does this go??
+            StakingEvent::ClaimRegistered(_, _, _, _, _) => None,
         };
         if subject.is_none() {
             log::trace!("Not publishing event: {:?} to MQ", &event);
