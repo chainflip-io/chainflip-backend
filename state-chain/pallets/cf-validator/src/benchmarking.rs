af707dd764b0630cec840a96fff6edae88b2e692
Andy Bell <andy.bell@displaynote.com>
2021-05-18 12:43:08 +0200

    As simple set of benchmarks

--- state-chain/pallets/cf-validator/src/benchmarking.rs
+++ state-chain/pallets/cf-validator/src/benchmarking.rs
@@ -3,7 +3,7 @@
 use super::*;
 
 use frame_system::RawOrigin;
-use frame_benchmarking::{benchmarks, whitelisted_caller, impl_benchmark_test_suite};
+use frame_benchmarking::{benchmarks, account, whitelisted_caller, impl_benchmark_test_suite};
 use sp_std::{vec, vec::Vec, boxed::Box};
 
 #[allow(unused)]
@@ -16,6 +16,26 @@ benchmarks! {
 	verify {
 		assert_eq!(Pallet::<T>::epoch_number_of_blocks(), s.into())
 	}
+
+	set_validator_target_size {
+		let s in 2 .. 100;
+	}: _(RawOrigin::Root, s.into())
+	verify {
+		assert_eq!(Pallet::<T>::max_validators(), s)
+	}
+
+	force_auction {
+		let a in 1 .. 100;
+	}: _(RawOrigin::Root)
+	verify {
+		assert_eq!(Pallet::<T>::force(), true)
+	}
+
+	confirm_auction {
+		let a in 1 .. 100;
+		let e  = EpochIndex(1);
+		AuctionToConfirm::<T>::set(Some(e));
+	}: _(RawOrigin::Signed(whitelisted_caller()), e)
 }
 
 impl_benchmark_test_suite!(
@@ -23,3 +43,7 @@ impl_benchmark_test_suite!(
 	crate::mock::new_test_ext(),
 	crate::mock::Test,
 );
+
+
+
+// confirm_auction
