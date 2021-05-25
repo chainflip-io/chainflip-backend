cda9b79ce0853538cc199e091b329fdbee5e5ea9
dlennon <3168260+dandanlen@users.noreply.github.com>
2021-05-25 15:24:39 +0200

    Formatting

--- state-chain/pallets/cf-witness/src/lib.rs
+++ state-chain/pallets/cf-witness/src/lib.rs
@@ -10,18 +10,23 @@ mod tests;
 
 use bitvec::prelude::*;
 use codec::FullCodec;
-use frame_support::{Hashable, dispatch::{DispatchResultWithPostInfo, Dispatchable}, pallet_prelude::Member, traits::EnsureOrigin};
+use frame_support::{
+	dispatch::{DispatchResultWithPostInfo, Dispatchable},
+	pallet_prelude::Member,
+	traits::EnsureOrigin,
+	Hashable,
+};
 use pallet_session::SessionHandler;
 use sp_runtime::traits::{AtLeast32BitUnsigned, Zero};
-use sp_std::{
-	prelude::*,
-	ops::AddAssign
-};
+use sp_std::{ops::AddAssign, prelude::*};
 
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
-	use frame_support::{dispatch::{DispatchResult, Dispatchable, GetDispatchInfo, PostDispatchInfo}, pallet_prelude::*};
+	use frame_support::{
+		dispatch::{DispatchResult, Dispatchable, GetDispatchInfo, PostDispatchInfo},
+		pallet_prelude::*,
+	};
 	use frame_system::pallet_prelude::*;
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
@@ -34,15 +39,17 @@ pub mod pallet {
 		type Origin: From<RawOrigin>;
 
 		/// The overarching call type.
-		type Call: Member + FullCodec
-			+ Dispatchable<Origin=<Self as Config>::Origin, PostInfo=PostDispatchInfo>
-			+ GetDispatchInfo 
+		type Call: Member
+			+ FullCodec
+			+ Dispatchable<Origin = <Self as Config>::Origin, PostInfo = PostDispatchInfo>
+			+ GetDispatchInfo
 			+ From<frame_system::Call<Self>>;
 
 		type Epoch: Member + FullCodec + AtLeast32BitUnsigned + Default;
 
-		type ValidatorId: Member + FullCodec 
-			+ From<<Self as frame_system::Config>::AccountId> 
+		type ValidatorId: Member
+			+ FullCodec
+			+ From<<Self as frame_system::Config>::AccountId>
 			+ Into<<Self as frame_system::Config>::AccountId>;
 	}
 
@@ -62,26 +69,20 @@ pub mod pallet {
 	pub struct Pallet<T>(_);
 
 	#[pallet::storage]
-	pub type Calls<T: Config> = StorageDoubleMap<
+	pub type Calls<T: Config> =
+		StorageDoubleMap<_, Blake2_128Concat, Epoch<T>, Identity, CallHash, Vec<u8>>;
+
+	#[pallet::storage]
+	pub(super) type ValidatorIndex<T: Config> = StorageDoubleMap<
 		_,
 		Blake2_128Concat,
 		Epoch<T>,
 		Identity,
-		CallHash, 
-		Vec<u8>
-	>;
-
-	#[pallet::storage]
-	pub(super) type ValidatorIndex<T: Config> = StorageDoubleMap<
-		_, 
-		Blake2_128Concat,
-		Epoch<T>,
-		Identity,
 		<T as frame_system::Config>::AccountId,
-		u16
+		u16,
 	>;
 
-	// TODO: This param should probably be managed in the sessions pallet. (The *active* validator set and 
+	// TODO: This param should probably be managed in the sessions pallet. (The *active* validator set and
 	// therefore the threshold might change due to unavailable nodes, slashing etc.)
 	#[pallet::storage]
 	pub(super) type ConsensusThreshold<T> = StorageValue<_, u32, ValueQuery>;
@@ -93,8 +94,8 @@ pub mod pallet {
 	pub(super) type CurrentEpoch<T: Config> = StorageValue<_, Epoch<T>, ValueQuery>;
 
 	#[pallet::hooks]
-	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> { }
-	
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+
 	#[pallet::event]
 	#[pallet::metadata(T::AccountId = "AccountId")]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
@@ -106,30 +107,30 @@ pub mod pallet {
 		ThresholdReached(CallHash, VoteCount),
 
 		/// A witness call has been executed [call_sig, result].
-		WitnessExecuted(CallHash, DispatchResult)
+		WitnessExecuted(CallHash, DispatchResult),
 	}
 
-	
 	#[pallet::error]
 	pub enum Error<T> {
-		/// CRITICAL: The validator index is out of bounds. This should never happen. 
+		/// CRITICAL: The validator index is out of bounds. This should never happen.
 		ValidatorIndexOutOfBounds,
 
 		/// Witness is not a validator.
 		UnauthorizedWitness,
 
 		/// A witness vote was cast twice by the same validator.
-		DuplicateWitness
+		DuplicateWitness,
 	}
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/// Called as a witness of some external event. The call parameter is the resultant extrinsic. This can be 
-		/// thought of as a vote for the encoded [`Call`](crate::Pallet::Call) value. 
+		/// Called as a witness of some external event. The call parameter is the resultant extrinsic. This can be
+		/// thought of as a vote for the encoded [`Call`](crate::Pallet::Call) value.
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
 		pub fn witness(
-			origin: OriginFor<T>, 
-			call: Box<<T as Config>::Call>) -> DispatchResultWithPostInfo {
+			origin: OriginFor<T>,
+			call: Box<<T as Config>::Call>,
+		) -> DispatchResultWithPostInfo {
 			let who = ensure_signed(origin)?;
 
 			Self::do_witness(who, *call)
@@ -143,74 +144,90 @@ pub mod pallet {
 	/// The raw origin enum for this pallet.
 	#[derive(PartialEq, Eq, Clone, RuntimeDebug, Encode, Decode)]
 	pub enum RawOrigin {
-		WitnessThreshold
+		WitnessThreshold,
 	}
 }
 
 impl<T: Config> Pallet<T> {
 	/// Do the actual witnessing.
-	/// 
+	///
 	/// Think of this a vote for some action (represented by a runtime `call`) to be taken. At a high level:
 	///
-	/// 1. Look up the account id in the list of validators. 
+	/// 1. Look up the account id in the list of validators.
 	/// 2. Get the list of votes for the call, or an empty list if this is the first vote.
 	/// 3. Add the account's vote to the list.
 	/// 4. Check the number of votes against the reuquired threshold.
-	/// 5. If the threshold is exceeded, execute the voted-on `call`. 
+	/// 5. If the threshold is exceeded, execute the voted-on `call`.
 	///
-	/// This implementation uses a bitmask whereby each index to the bitmask represents a validator account ID in the 
+	/// This implementation uses a bitmask whereby each index to the bitmask represents a validator account ID in the
 	/// current Epoch.
 	///
 	/// **Note:**
-	/// This implementation currently allows voting to continue even after the vote threshold is reached. 
+	/// This implementation currently allows voting to continue even after the vote threshold is reached.
 	///
 	fn do_witness(
-		who: <T as frame_system::Config>::AccountId, 
-		call: <T as Config>::Call) -> DispatchResultWithPostInfo 
-	{
+		who: <T as frame_system::Config>::AccountId,
+		call: <T as Config>::Call,
+	) -> DispatchResultWithPostInfo {
 		let epoch: Epoch<T> = CurrentEpoch::<T>::get();
 		let num_validators = NumValidators::<T>::get() as usize;
-		
+
 		// Look up the signer in the list of validators
-		let index = ValidatorIndex::<T>::get(&epoch, &who)
-			.ok_or(Error::<T>::UnauthorizedWitness)? as usize;
-		
+		let index =
+			ValidatorIndex::<T>::get(&epoch, &who).ok_or(Error::<T>::UnauthorizedWitness)? as usize;
+
 		// Register the vote
 		let call_hash = Hashable::blake2_256(&call);
-		let num_votes = Calls::<T>::try_mutate::<_, _, _, Error::<T>, _>(&epoch, &call_hash, |buffer| {
-			// If there is no storage item, create an empty one.
-			if buffer.is_none() {
-				let empty_mask = BitVec::<Msb0, u8>::repeat(false, num_validators);
-				*buffer = Some(empty_mask.into_vec())
-			}
+		let num_votes = Calls::<T>::try_mutate::<_, _, _, Error<T>, _>(
+			&epoch,
+			&call_hash,
+			|buffer| {
+				// If there is no storage item, create an empty one.
+				if buffer.is_none() {
+					let empty_mask = BitVec::<Msb0, u8>::repeat(false, num_validators);
+					*buffer = Some(empty_mask.into_vec())
+				}
 
-			let bytes = buffer.as_mut().expect("Checked for none condition above, this will never panic;");
+				let bytes = buffer
+					.as_mut()
+					.expect("Checked for none condition above, this will never panic;");
 
-			// Convert to an addressable bitmask
-			let bits = VoteMask::from_slice_mut(bytes)
+				// Convert to an addressable bitmask
+				let bits = VoteMask::from_slice_mut(bytes)
 				.expect("Only panics if the slice size exceeds the max; The number of validators should never exceed this;");
 
-			// Return an error if already voted, otherwise set the indexed bit to `true` to indicate a vote.
-			if bits[index] {
-				Err(Error::<T>::DuplicateWitness)?
-			} else {
-				let mut vote = bits.get_mut(index).ok_or(Error::<T>::ValidatorIndexOutOfBounds)?;
-				*vote = true;
-			}
+				// Return an error if already voted, otherwise set the indexed bit to `true` to indicate a vote.
+				if bits[index] {
+					Err(Error::<T>::DuplicateWitness)?
+				} else {
+					let mut vote = bits
+						.get_mut(index)
+						.ok_or(Error::<T>::ValidatorIndexOutOfBounds)?;
+					*vote = true;
+				}
 
-			Ok(bits.count_ones())
-		})?;
+				Ok(bits.count_ones())
+			},
+		)?;
 
 		let threshold = ConsensusThreshold::<T>::get() as usize;
 
-		Self::deposit_event(Event::<T>::WitnessReceived(call_hash, who.into(), num_votes as VoteCount));
+		Self::deposit_event(Event::<T>::WitnessReceived(
+			call_hash,
+			who.into(),
+			num_votes as VoteCount,
+		));
 
 		// Check if threshold is reached and, if so, apply the voted-on Call.
 		if num_votes == threshold {
-			Self::deposit_event(Event::<T>::ThresholdReached(call_hash, num_votes as VoteCount));
+			Self::deposit_event(Event::<T>::ThresholdReached(
+				call_hash,
+				num_votes as VoteCount,
+			));
 			let result = call.dispatch((RawOrigin::WitnessThreshold).into());
 			Self::deposit_event(Event::<T>::WitnessExecuted(
-				call_hash, result.map(|_| ()).map_err(|e| e.error)
+				call_hash,
+				result.map(|_| ()).map_err(|e| e.error),
 			));
 		}
 
@@ -227,7 +244,7 @@ impl<T: pallet::Config> cf_traits::Witnesser for Pallet<T> {
 	}
 }
 
-/// Simple struct on which to implement EnsureOrigin for our pallet's custom origin type. 
+/// Simple struct on which to implement EnsureOrigin for our pallet's custom origin type.
 ///
 /// # Example:
 ///
@@ -238,7 +255,8 @@ impl<T: pallet::Config> cf_traits::Witnesser for Pallet<T> {
 /// ```
 pub struct EnsureWitnessed;
 
-impl<OuterOrigin> EnsureOrigin<OuterOrigin> for EnsureWitnessed where 
+impl<OuterOrigin> EnsureOrigin<OuterOrigin> for EnsureWitnessed
+where
 	OuterOrigin: Into<Result<RawOrigin, OuterOrigin>> + From<RawOrigin>,
 {
 	type Success = ();
@@ -247,8 +265,8 @@ impl<OuterOrigin> EnsureOrigin<OuterOrigin> for EnsureWitnessed where
 		match o.into() {
 			Ok(o) => match o {
 				RawOrigin::WitnessThreshold => Ok(()),
-			}
-			Err(o) => Err(o)
+			},
+			Err(o) => Err(o),
 		}
 	}
 
@@ -259,11 +277,12 @@ impl<OuterOrigin> EnsureOrigin<OuterOrigin> for EnsureWitnessed where
 }
 
 /// Implementation of [SessionHandler](pallet_session::SessionHandler) to update
-/// the current list of validators and the current epoch. 
-impl<T, ValidatorId> SessionHandler<ValidatorId> for Pallet<T> where
+/// the current list of validators and the current epoch.
+impl<T, ValidatorId> SessionHandler<ValidatorId> for Pallet<T>
+where
 	T: Config,
-	ValidatorId: Clone + Into<<T as frame_system::Config>::AccountId> {
-		
+	ValidatorId: Clone + Into<<T as frame_system::Config>::AccountId>,
+{
 	const KEY_TYPE_IDS: &'static [sp_runtime::KeyTypeId] = &[];
 
 	fn on_genesis_session<Ks: sp_runtime::traits::OpaqueKeys>(validators: &[(ValidatorId, Ks)]) {
