4cc459d69938dca16cbddc71e99fb30474bb78ee
Kyle Zsembery <zsembery.kyle@gmail.com>
2021-05-26 10:23:01 +1000

    Message queue to use settings instead of Options refactor

--- engine/src/settings.rs
+++ engine/src/settings.rs
@@ -44,6 +44,21 @@ impl Settings {
 }
 
 #[cfg(test)]
+pub mod test_utils {
+    use super::*;
+
+    pub fn new_test_settings() -> Result<Settings, ConfigError> {
+        let mut s = Config::new();
+
+        // Start off by merging in the "testing" configuration file
+        s.merge(File::with_name("config/testing.toml"))?;
+
+        // You can deserialize (and thus freeze) the entire configuration as
+        s.try_into()
+    }
+}
+
+#[cfg(test)]
 mod tests {
 
     use super::*;
