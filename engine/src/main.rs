76aa039a86443b9efea835e807f0b87451c41498
Kyle Zsembery <zsembery.kyle@gmail.com>
2021-06-16 12:20:28 +0200

    Add health check endpoint to CFE

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -5,15 +5,34 @@ use chainflip_engine::{
     settings::Settings,
     signing::{self, crypto::Parameters},
 };
+use std::{io::Write, net::TcpListener};
+
+async fn health_check(port: u16) {
+    let bind_address = format!("127.0.0.1:{}", port);
+    let listener = TcpListener::bind(bind_address)
+        .expect(format!("Could not bind TCP listener to port {}", port).as_str());
+
+    // just return a 200 response on a request
+    for stream in listener.incoming() {
+        let mut stream = stream.expect("Could not open health TCP stream");
+        let http_200_response = "HTTP/1.1 200 OK\r\n\r\n";
+        stream
+            .write(http_200_response.as_bytes())
+            .expect("Error writing to health check stream");
+        log::trace!("Responding to health check: CFE is healthy :heart: ");
+        stream.flush().expect("Error flushing health check stream");
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    // init the logger
     env_logger::init();
+
+    log::info!("Start the engines! :broom: :broom: ");
 
     let settings = Settings::new().expect("Failed to initialise settings");
 
-    log::info!("Start the engines! :broom: :broom: ");
+    health_check(settings.engine.health_check_port).await;
 
     sc_observer::sc_observer::start(settings.clone()).await;
 
@@ -34,4 +53,16 @@ async fn main() {
     let signing_client = signing::MultisigClient::new(mq_factory, signer_idx, params);
 
     signing_client.run().await;
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[tokio::test]
+    #[ignore = "runs forever"]
+    async fn health_check_test() {
+        health_check(5555u16).await;
+    }
 }
