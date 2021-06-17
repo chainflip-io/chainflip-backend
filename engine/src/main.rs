2258034fe5cead448e13d435bb58f2aff064773d
Kyle Zsembery <zsembery.kyle@gmail.com>
2021-06-17 11:16:38 +0200

    use path /health and use tokio spawn

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -8,7 +8,7 @@ use chainflip_engine::{
 // use std::{io::Write, net::TcpListener};
 
 use async_std::net::TcpListener;
-use futures::{AsyncWriteExt, StreamExt};
+use futures::{AsyncReadExt, AsyncWriteExt, StreamExt};
 
 async fn health_check(port: u16) {
     let bind_address = format!("127.0.0.1:{}", port);
@@ -16,21 +16,42 @@ async fn health_check(port: u16) {
         .await
         .expect(format!("Could not bind TCP listener to port {}", port).as_str());
 
-    let mut incoming = listener.incoming();
+    tokio::spawn(async move {
+        let mut incoming = listener.incoming();
+        while let Some(stream) = incoming.next().await {
+            let mut stream = stream.expect("Could not open CFE health check TCP stream");
+            let mut buffer = [0; 1024];
+            // read the stream into the buffer
+            stream
+                .read(&mut buffer)
+                .await
+                .expect("Couldn't read stream into buffer");
 
-    while let Some(stream) = incoming.next().await {
-        let mut stream = stream.expect("Could not open CFE health check TCP stream");
-        let http_200_response = "HTTP/1.1 200 OK\r\n\r\n";
-        stream
-            .write(http_200_response.as_bytes())
-            .await
-            .expect("Could not write to health check stream");
-        log::trace!("Responded to health check: CFE is healthy :heart: ");
-        stream
-            .flush()
-            .await
-            .expect("Could not flush health check TCP stream");
-    }
+            // parse the http request
+            let mut headers = [httparse::EMPTY_HEADER; 16];
+            let mut req = httparse::Request::new(&mut headers);
+            let result = req.parse(&buffer);
+            if let Err(e) = result {
+                log::warn!("Invalid health check request, could not parse: {}", e);
+                continue;
+            }
+
+            if req.path.eq(&Some("/health")) {
+                let http_200_response = "HTTP/1.1 200 OK\r\n\r\n";
+                stream
+                    .write(http_200_response.as_bytes())
+                    .await
+                    .expect("Could not write to health check stream");
+                log::trace!("Responded to health check: CFE is healthy :heart: ");
+                stream
+                    .flush()
+                    .await
+                    .expect("Could not flush health check TCP stream");
+            } else {
+                log::warn!("Requested health at invalid path: {:?}", req.path);
+            }
+        }
+    });
 }
 
 #[tokio::main]
