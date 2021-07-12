98e95c7e2c3cdf8718aa1f98b15bc559daf812f9
Kyle Zsembery <zsembery.kyle@gmail.com>
2021-07-12 16:18:05 +0200

    WIP try slog

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -2,11 +2,28 @@ use chainflip_engine::{
     eth, health::health_check, mq::nats_client::NatsMQClientFactory, settings::Settings, signing,
     state_chain, temp_event_mapper::TempEventMapper,
 };
+use slog::Drain;
+
+#[macro_use]
+extern crate slog;
+extern crate slog_async;
+extern crate slog_json;
 
 #[tokio::main]
 async fn main() {
-    log4rs::init_file("./config/log4rs.yml", Default::default())
-        .expect("Should have logging configuration at config/log4rs.yml");
+    // log4rs::init_file("./config/log4rs.yml", Default::default())
+    //     .expect("Should have logging configuration at config/log4rs.yml");
+
+    let drain = slog_json::Json::new(std::io::stdout())
+        .add_default_keys()
+        .build()
+        .fuse();
+
+    let drain = slog_async::Async::new(drain).build().fuse();
+    let root = slog::Logger::root(drain, o!());
+
+    slog::error!(root, "Hello bro"; o!("my key" => "my value"));
+    std::thread::sleep(std::time::Duration::from_secs(5));
 
     log::info!("Start the engines! :broom: :broom: ");
 
