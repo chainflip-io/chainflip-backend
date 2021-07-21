9f5b831a3c77afa40c99ec78dd53da63f0bf52d2
Andy Bell <andyjsbell@gmail.com>
2021-07-21 17:31:41 +0200

    A nonce from timesource

---
+++ state-chain/pallets/cf-vaults/src/nonce.rs
@@ -0,0 +1,19 @@
+use sp_runtime::traits::{UniqueSaturatedInto, Bounded};
+use crate::NonceProvider;
+use frame_support::traits::UnixTime;
+use std::marker::PhantomData;
+
+pub struct NonceUnixTime<N, T> {_marker1: PhantomData<N>, _marker2: PhantomData<T>}
+
+impl<N, T> NonceProvider for NonceUnixTime<N, T> where
+	T : UnixTime,
+	N: From<u64> + Bounded,{
+	type Nonce = N;
+
+	fn generate_nonce() -> Self::Nonce {
+		// For now, we expect the nonce to be an u64 to stay compatible with the CFE
+		let u64_nonce = T::now().as_nanos();
+		let u : u64 = u64_nonce as u64;
+		u.unique_saturated_into()
+	}
+}
