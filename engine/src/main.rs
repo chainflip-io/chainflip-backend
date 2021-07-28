9a5307283cb784b575c9e4ca16932a4da67444ee
Kyle Zsembery <zsembery.kyle@gmail.com>
2021-07-28 10:59:37 +0200

    clean up main

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -23,49 +23,38 @@ async fn main() {
 
     let mq_factory = NatsMQClientFactory::new(&settings.message_queue);
 
-    // This can be the same filepath as the p2p key --node-key-file <file> on the state chain
-    // which won't necessarily always be the case, i.e. if we no longer have PeerId == ValidatorId
-    let signer = state_chain::get_signer_from_privkey_file(&settings.state_chain.p2p_priv_key_file);
-    let my_pubkey = signer.signer().public();
-    let my_validator_id = ValidatorId(my_pubkey.0);
-    let sc_o_fut = state_chain::sc_observer::start(settings.clone());
-    let sc_b_fut = state_chain::sc_broadcaster::start(&settings, signer, mq_factory.clone());
-
-    let eth_fut = eth::start(settings.clone());
-
-    let (_, shutdown_rx) = tokio::sync::oneshot::channel::<()>();
-
-    let substrate_node_endpoint = url::Url::parse(settings.state_chain.ws_endpoint.as_str())
-        .expect(&format!(
-            "Should be valid ws endpoint: {}",
-            settings.state_chain.ws_endpoint
-        ));
-    let p2p_client = RpcP2PClient::new(substrate_node_endpoint);
     let mq_client = *mq_factory
         .create()
         .await
         .expect("Could not connect MQ client");
-    let p2p_conductor_fut = P2PConductor::new(mq_client, p2p_client)
-        .await
-        .start(shutdown_rx);
+
+    // This can be the same filepath as the p2p key --node-key-file <file> on the state chain
+    // which won't necessarily always be the case, i.e. if we no longer have PeerId == ValidatorId
+    let signer = state_chain::get_signer_from_privkey_file(&settings.state_chain.p2p_priv_key_file);
+    let my_validator_id = ValidatorId(signer.signer().public().0);
 
     // TODO: Investigate whether we want to encrypt it on disk
     let db = PersistentKeyDB::new("data.db");
 
-    let signing_client = signing::MultisigClient::new(db, mq_factory, my_validator_id);
-
-    let temp_event_map_fut = TempEventMapper::run(&settings);
-
+    let (_, p2p_shutdown_rx) = tokio::sync::oneshot::channel::<()>();
     let (_, shutdown_client_rx) = tokio::sync::oneshot::channel::<()>();
 
-    let signing_client_fut = signing_client.run(shutdown_client_rx);
-
     futures::join!(
-        sc_o_fut,
-        sc_b_fut,
-        eth_fut,
-        temp_event_map_fut,
-        p2p_conductor_fut,
-        signing_client_fut,
+        state_chain::sc_observer::start(settings.clone()),
+        state_chain::sc_broadcaster::start(&settings, signer, mq_factory.clone()),
+        eth::start(settings.clone()),
+        TempEventMapper::run(&settings),
+        P2PConductor::new(
+            mq_client,
+            RpcP2PClient::new(
+                url::Url::parse(settings.state_chain.ws_endpoint.as_str()).expect(&format!(
+                    "Should be valid ws endpoint: {}",
+                    settings.state_chain.ws_endpoint
+                ))
+            )
+        )
+        .await
+        .start(p2p_shutdown_rx),
+        signing::MultisigClient::new(db, mq_factory, my_validator_id).run(shutdown_client_rx),
     );
 }
