d1c988e3c8ace1f80162328f01e4117c59ebecd7
Kyle Zsembery <zsembery.kyle@gmail.com>
2021-08-03 14:25:31 +0200

    Rename WitnessApi to WitnesserApi to match state chain metadata

--- engine/src/state_chain/witness_api.rs
+++ engine/src/state_chain/witness_api.rs
@@ -6,23 +6,23 @@ use substrate_subxt::{module, system::System, Call};
 type EthTransactionHash = [u8; 32];
 
 #[module]
-pub trait WitnessApi: System + Staking + Auction {}
+pub trait WitnesserApi: System + Staking + Auction {}
 
 #[derive(Clone, Debug, PartialEq, Call, Encode)]
-pub struct WitnessStakedCall<T: WitnessApi> {
+pub struct WitnessStakedCall<T: WitnesserApi> {
     staker_account_id: <T as System>::AccountId,
     amount: <T as Staking>::TokenAmount,
     tx_hash: EthTransactionHash,
 }
 
 #[derive(Clone, Debug, PartialEq, Call, Encode)]
-pub struct WitnessClaimedCall<T: WitnessApi> {
+pub struct WitnessClaimedCall<T: WitnesserApi> {
     account_id: <T as System>::AccountId,
     amount: <T as Staking>::TokenAmount,
     tx_hash: [u8; 32],
 }
 
 #[derive(Clone, Debug, PartialEq, Call, Encode)]
-pub struct WitnessAuctionConfirmationCall<T: WitnessApi> {
+pub struct WitnessAuctionConfirmationCall<T: WitnesserApi> {
     auction_index: <T as Auction>::AuctionIndex,
 }
