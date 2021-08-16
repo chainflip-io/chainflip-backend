f0674dd459d4d4a280ad520e84ee90bbae61aaae
dlennon <3168260+dandanlen@users.noreply.github.com>
2021-08-16 13:45:04 +0200

    Split serde tests into two.

--- state-chain/client/cf-p2p/rpc/src/p2p_serde.rs
+++ state-chain/client/cf-p2p/rpc/src/p2p_serde.rs
@@ -36,22 +36,30 @@ pub mod bs58_fixed_size {
 }
 
 #[test]
-fn test_serde() {
-	use super::{MessageBs58, ValidatorIdBs58};
+fn test_validator_id_bs58() {
+	use super::{ValidatorIdBs58};
 	use serde_json;
 
 	let validator_id_raw = [0xCF; 32];
-	let original_message = b"super interesting".to_vec();
 
 	serde_json::to_string(&ValidatorIdBs58(validator_id_raw))
 		.expect("Encoding validator Id should work.");
+
+	serde_json::from_str::<ValidatorIdBs58>(r#""5G""#).expect_err("Length is invalid.");
+	serde_json::from_str::<ValidatorIdBs58>(r#""5G9NWJ5P9uk7am24yCKeLZJqXWW6hjuMyRJDmw4ofqx""#)
+		.expect("Valid Id.");
+}
+
+#[test]
+fn test_message_bs58() {
+	use super::{MessageBs58};
+	use serde_json;
+
+	let original_message = b"super interesting".to_vec();
+
 	let encoded_message = serde_json::to_string(&MessageBs58(original_message.clone())).unwrap();
 
 	let decoded_message: MessageBs58 =
 		serde_json::from_str(encoded_message.as_str()).expect("Encoded should decode.");
 	assert_eq!(decoded_message.0, original_message);
-
-	serde_json::from_str::<ValidatorIdBs58>(r#""5G""#).expect_err("Length is invalid.");
-	serde_json::from_str::<ValidatorIdBs58>(r#""5G9NWJ5P9uk7am24yCKeLZJqXWW6hjuMyRJDmw4ofqx""#)
-		.expect("Valid Id.");
 }
