63f18bb1df48e453ee9b2232c9a5e41a7717e3a3
dlennon <3168260+dandanlen@users.noreply.github.com>
2021-08-17 18:03:50 +0200

    Instantiable pallet

--- state-chain/pallets/cf-reqrep/src/lib.rs
+++ state-chain/pallets/cf-reqrep/src/lib.rs
@@ -1,10 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 //! Request-Reply Pallet
-
-use codec::{Encode, Decode};
-use frame_support::dispatch::DispatchResult;
-pub use pallet::*;
-
 #[cfg(test)]
 mod mock;
 
@@ -14,70 +9,65 @@ mod tests;
 #[cfg(feature = "runtime-benchmarks")]
 mod benchmarking;
 
-trait ReqRep {
-	type Reply;
+use frame_support::{Parameter, dispatch::DispatchResult};
+pub use pallet::*;
 
-	fn on_reply(&self, reply: Self::Reply) -> DispatchResult { todo!() }
+pub trait ReqRep<T> : Parameter {
+	type Reply: Parameter;
+
+	fn on_reply(&self, _reply: Self::Reply) -> DispatchResult { todo!() }
 }
 
-// Requests
+mod reqreps {
+	pub use super::*;
+	use codec::{Decode, Encode};
+	use frame_support::Parameter;
 
-#[derive(Clone, Debug, PartialEq, Eq, Encode, Decode)]
-struct SignatureRequest<KeyId, ValidatorId> { signing_key: KeyId, payload: Vec<u8>, signatories: Vec<ValidatorId> }
+	pub trait BaseConfig: frame_system::Config {
+		type KeyId: Parameter;
+		type ValidatorId: Parameter;
+		type ChainId: Parameter;
+	}
 
-impl<KeyId, ValidatorId> ReqRep for SignatureRequest<KeyId, ValidatorId> {
-	type Reply = SignatureReply<ValidatorId>;
-}
+	pub mod signature {
+		use super::*;
 
-#[derive(Clone, Debug, PartialEq, Eq, Encode, Decode)]
-struct BroadcastRequest<Chain> { chain: Chain }
+		#[derive(Clone, Debug, PartialEq, Eq, Encode, Decode)]
+		pub struct Request<KeyId, ValidatorId> {
+			signing_key: KeyId,
+			payload: Vec<u8>,
+			signatories: Vec<ValidatorId>,
+		}
 
-impl<Chain> ReqRep for BroadcastRequest<Chain> {
-	type Reply = BroadcastReply;
-}
+		#[derive(Clone, Debug, PartialEq, Eq, Encode, Decode)]
+		pub enum Reply<ValidatorId> {
+			Success { sig: Vec<u8> },
+			Failure { bad_nodes: Vec<ValidatorId> },
+		}
 
-#[derive(Clone, Debug, PartialEq, Eq, Encode, Decode)]
-pub enum Request<
-	KeyId: Clone + PartialEq + Eq + Encode + Decode, 
-	ValidatorId: Clone + PartialEq + Eq + Encode + Decode, 
-	Chain: Clone + PartialEq + Eq + Encode + Decode
-	> 
-{
-	SignatureRequest(SignatureRequest<KeyId, ValidatorId>),
-	BroadcastRequest(BroadcastRequest<Chain>),
-}
-
-impl<
-		KeyId: Clone + PartialEq + Eq + Encode + Decode,
-		ValidatorId: Clone + PartialEq + Eq + Encode + Decode,
-		Chain: Clone + PartialEq + Eq + Encode + Decode,
-	> Request<KeyId, ValidatorId, Chain>
-{
-	fn upcast(self) -> Box<dyn ReqRep> {
-		match self {
-			Request::SignatureRequest(req) => Box::new(req),
-			Request::BroadcastRequest(req) => Box::new(req),
+		impl<T: BaseConfig> ReqRep<T> for Request<T::KeyId, T::ValidatorId> {
+			type Reply = Reply<T::ValidatorId>;
 		}
 	}
-}
 
-/// Replies
+	pub mod broadcast {
+		use super::*;
 
-enum SignatureReply<ValidatorId> {
-	Success{ sig: Vec<u8> },
-	Failure { bad_nodes: Vec<ValidatorId> },
-}
+		#[derive(Clone, Debug, PartialEq, Eq, Encode, Decode)]
+		pub struct Request<ChainId> {
+			chain: ChainId,
+		}
 
-struct BroadcastReply;
+		#[derive(Clone, Debug, PartialEq, Eq, Encode, Decode)]
+		pub enum Reply {
+			Success,
+			Failure,
+			Timeout,
+		}
 
-pub enum Reply<ValidatorId> {
-	SignatureReply(SignatureReply<ValidatorId>),
-	BroadcastReply(BroadcastReply),
-}
-
-impl<ValidatorId> Reply<ValidatorId> {
-	fn try_downcast_reply<T>(self) {
-		todo!()
+		impl<T: BaseConfig> ReqRep<T> for Request<T::ChainId> {
+			type Reply = Reply;
+		}
 	}
 }
 
@@ -86,84 +76,79 @@ pub type RequestId = u64;
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
-	use frame_support::dispatch::DispatchResultWithPostInfo;
+	use std::marker::PhantomData;
+	use frame_support::{Twox64Concat, dispatch::DispatchResultWithPostInfo};
 	use frame_system::pallet_prelude::*;
 	use frame_support::pallet_prelude::*;
 
-	/// Configure the pallet by specifying the parameters and types on which it depends.
+	type ReplyFor<T, I> = <<T as Config<I>>::Request as ReqRep<T>>::Reply;
+
 	#[pallet::config]
-	pub trait Config: frame_system::Config {
+	#[pallet::disable_frame_system_supertrait_check]
+	pub trait Config<I: 'static = ()>: reqreps::BaseConfig {
 		/// Because this pallet emits events, it depends on the runtime's definition of an event.
-		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+		type Event: From<Event<Self, I>> + IsType<<Self as frame_system::Config>::Event>;
 
-		type KeyId: Encode + Decode;
-
-		/// The overarching request type.
-		type Request: Encode + Decode;
-
-		/// The overarching reply type.
-		type Reply: Encode + Decode;
+		/// The request-reply definition for this instance.
+		type Request: ReqRep<Self>;
 	}
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
-	pub struct Pallet<T>(_);
+	pub struct Pallet<T, I = ()>(PhantomData<(T, I)>);
 
 	#[pallet::storage]
 	#[pallet::getter(fn request_id_counter)]
-	pub type RequestIdCounter<T> = StorageValue<_, RequestId, ValueQuery>;
+	pub type RequestIdCounter<T, I = ()> = StorageValue<_, RequestId, ValueQuery>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn pending_request)]
-	pub type PendingRequests<T> = StorageMap<_, RequestId, <T as Config>::Request, OptionQuery>;
+	pub type PendingRequests<T: Config<I>, I: 'static = ()> = StorageMap<_, Twox64Concat, RequestId, T::Request, OptionQuery>;
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config> {
+	pub enum Event<T: Config<I>, I: 'static = ()> {
 		/// An outgoing request. [id, request]
 		Request(RequestId, T::Request),
 	}
 
-	// Errors inform users that something went wrong.
 	#[pallet::error]
-	pub enum Error<T> {
+	pub enum Error<T, I = ()> {
 		/// The provided request id is invalid.
 		InvalidRequestId,
 	}
 
 	#[pallet::hooks]
-	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+	impl<T: Config<I>, I: 'static> Hooks<BlockNumberFor<T>> for Pallet<T, I> {}
 
 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
 	// These functions materialize as "extrinsics", which are often compared to transactions.
 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
-	impl<T: Config> Pallet<T> {
+	impl<T: Config<I>, I: 'static> Pallet<T, I> {
 		/// Reply.
 		#[pallet::weight(10_000)]
-		pub fn reply(origin: OriginFor<T>, id: RequestId, reply: T::Reply) -> DispatchResultWithPostInfo {
+		pub fn reply(origin: OriginFor<T>, id: RequestId, reply: ReplyFor<T, I>) -> DispatchResultWithPostInfo {
 			// Probably needs to be witnessed.
 			let who = ensure_signed(origin)?;
 			
 			// 1. Pull the request type out of storage.
-			let request = PendingRequests::<T>::get(id).ok_or(Error::<T>::InvalidRequestId)?;
+			let request = PendingRequests::<T, I>::get(id).ok_or(Error::<T, I>::InvalidRequestId)?;
 
-			// 2. Figure out what the reply type is. (Try this? https://crates.io/crates/downcast-rs)
-			// 3. Decode the reply.
-			// 4. Dispatch the callback.
-			let reply = request.upcast().on_reply(reply)?;
+			// 2. Dispatch the callback.
+			let _ = request.on_reply(reply)?;
 
 			Ok(().into())
 		}
 	}
 }
 
-impl<T: Config> Pallet<T> {
-	pub fn request(request: impl ReqRep) {
+impl<T: Config<I>, I: 'static> Pallet<T, I> {
+	pub fn request(request: T::Request) {
 		// Get a new id.
-		let id = RequestIdCounter::<T>::mutate(|id| { *id += 1; *id });
+		let id = RequestIdCounter::<T, I>::mutate(|id| { *id += 1; *id });
 
 		// Emit the request to the CFE.
-		Self::deposit_event(Event::<T>::Request(id, Encode::encode(request)));
+		Self::deposit_event(Event::<T, I>::Request(id, request));
 	}
 }
