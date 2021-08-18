66f451f1b595315274d73fa683cc495c9264828b
Kyle Zsembery <zsembery.kyle@gmail.com>
2021-08-18 17:18:35 +0200

    WIP - heartbeat is submitted

--- engine/src/heartbeat.rs
+++ engine/src/heartbeat.rs
@@ -1,15 +1,50 @@
-use substrate_subxt::Client;
+use substrate_subxt::{Client, PairSigner};
 
 use crate::state_chain::runtime::StateChainRuntime;
+
+use crate::state_chain::pallets::reputation::HeartbeatCallExt;
+
+use sp_keyring::AccountKeyring;
 
 /// Starts the CFE heartbeat. Submits an extrinsic to the SC every HeartbeatIntervalPeriod / 2 blocks
 pub async fn start_heartbeat(subxt_client: Client<StateChainRuntime>) {
     println!("Starting heartbeat");
-    // First get the HeartbeatIntervalPeriod
 
-    let props = subxt_client.properties();
+    let alice = AccountKeyring::Alice.pair();
+    let pair_signer = PairSigner::new(alice);
 
-    println!("Here are the props: {:?}", props);
+    // submit a heartbeat on startup
+    let heartbeat_result = subxt_client.heartbeat(&pair_signer).await;
+
+    println!("Here's the heartbeat result: {:?}", heartbeat_result);
+
+    // // TODO: Handle errors
+    // let heartbeat_block_interval = subxt_client
+    //     .metadata()
+    //     .module("Reputation")
+    //     .unwrap()
+    //     .constant("HeartbeatBlockInterval")
+    //     .unwrap()
+    //     .value::<i32>()
+    //     .unwrap();
+    // // println!("The heartbeat interval is: {:?}", heartbeat_block_interval);
+
+    // // temp set it to 2 for testing
+    // let heartbeat_block_interval: i32 = 4;
+    // let send_heartbeat_interval: i32 = heartbeat_block_interval / 2;
+
+    // let mut blocks = subxt_client
+    //     .subscribe_finalized_blocks()
+    //     .await
+    //     .expect("Should subscribe to finalised blocks");
+
+    // let mut count: i32 = 0;
+    // while let Some(_) = blocks.next().await {
+    //     count += 1;
+    //     if count % send_heartbeat_interval == 0 {
+    //         // send a heartbeat
+    //     }
+    // }
 }
 
 #[cfg(test)]
--- engine/src/state_chain/pallets/reputation.rs
+++ engine/src/state_chain/pallets/reputation.rs
@@ -2,10 +2,7 @@
 
 use std::marker::PhantomData;
 
-use cf_traits::EpochInfo;
 use codec::Encode;
-use frame_support::{pallet_prelude::Member, traits::IsType, Parameter};
-use frame_system::Event;
 use substrate_subxt::{module, system::System, Call};
 
 #[module]
