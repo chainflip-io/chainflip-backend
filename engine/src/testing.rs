baad6929f5b1f977d23fafe004823aaa6bcd0be6
Kyle Zsembery <zsembery.kyle@gmail.com>
2021-08-20 15:11:40 +0200

    rename to assert_ok

--- engine/src/testing.rs
+++ engine/src/testing.rs
@@ -2,7 +2,7 @@
 
 /// Simply unwraps the value. Advantage of this is to make it clear in tests
 /// what we are testing
-macro_rules! assert_ok_unwrap {
+macro_rules! assert_ok {
     ($result:expr) => {
         $result.unwrap()
     };
@@ -14,16 +14,16 @@ mod tests {
         fn works() -> Result<i32, i32> {
             Ok(1)
         }
-        let result = assert_ok_unwrap!(works());
+        let result = assert_ok!(works());
         assert_eq!(result, 1);
     }
 
     #[test]
     #[should_panic]
-    fn test_assert_ok_unwrap_err() {
+    fn test_assert_ok_err() {
         fn works() -> Result<i32, i32> {
             Err(0)
         }
-        assert_ok_unwrap!(works());
+        assert_ok!(works());
     }
 }
