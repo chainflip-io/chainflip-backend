1058455ccbc193750fa140ea6de64bb403df3e26
Alastair Holmes <holmesa31415@gmail.com>
2021-08-24 12:19:56 +0200

    Fix Integration Tests

--- engine/src/eth/eth_event_streamer.rs
+++ engine/src/eth/eth_event_streamer.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use futures::{stream, Stream, StreamExt};
+use std::fmt::Debug;
 use web3::{
     ethabi::RawLog,
     transports::WebSocket,
@@ -8,12 +9,16 @@ use web3::{
 };
 
 /// Creates a stream that outputs the (signature, transaction hash, raw log) of events from a contract.
-pub async fn new_eth_event_stream(
+pub async fn new_eth_event_stream<
+    Event: Debug,
+    LogDecoder: Fn(H256, H256, RawLog) -> Result<Event>,
+>(
     web3: Web3<WebSocket>,
     deployed_address: H160,
+    decode_log: LogDecoder,
     from_block: u64,
     logger: slog::Logger,
-) -> Result<impl Stream<Item = Result<(H256, H256, RawLog)>>> {
+) -> Result<impl Stream<Item = Result<Event>>> {
     // The `fromBlock` parameter doesn't seem to work reliably with subscription streams, so
     // request past block via http and prepend them to the stream manually.
     let past_logs = web3
@@ -39,61 +44,64 @@ pub async fn new_eth_event_stream(
     Ok(stream::iter(past_logs)
         .map(|log| Ok(log))
         .chain(future_logs)
-        .map(
-            move |result_unparsed_log| -> Result<(H256, H256, RawLog), anyhow::Error> {
-                let result_extracted_log_details = result_unparsed_log
-                    .map_err(|error| anyhow::Error::new(error))
-                    .and_then(|log| {
-                        Ok((
-                            /*signature*/
-                            *log.topics.first().ok_or_else(|| {
-                                anyhow::Error::msg("Could not get signature from ETH log")
-                            })?,
-                            /*tx hash*/
-                            log.transaction_hash.ok_or_else(|| {
-                                anyhow::Error::msg("Could not get transaction hash from ETH log")
-                            })?,
-                            RawLog {
-                                topics: log.topics,
-                                data: log.data.0,
-                            },
-                        ))
-                    });
+        .map(move |result_unparsed_log| -> Result<Event, anyhow::Error> {
+            let result_event = result_unparsed_log
+                .map_err(|error| anyhow::Error::new(error))
+                .and_then(|log| {
+                    decode_log(
+                        /*signature*/
+                        *log.topics.first().ok_or_else(|| {
+                            anyhow::Error::msg("Could not get signature from ETH log")
+                        })?,
+                        /*tx hash*/
+                        log.transaction_hash.ok_or_else(|| {
+                            anyhow::Error::msg("Could not get transaction hash from ETH log")
+                        })?,
+                        RawLog {
+                            topics: log.topics,
+                            data: log.data.0,
+                        },
+                    )
+                });
 
-                slog::debug!(
-                    logger,
-                    "Received ETH log: {:?}",
-                    result_extracted_log_details
-                );
+            slog::debug!(
+                logger,
+                "Received ETH log, parsing result: {:?}",
+                result_event
+            );
 
-                result_extracted_log_details
-            },
-        ))
+            result_event
+        }))
 }
 
 #[cfg(test)]
 mod tests {
 
-    use crate::{eth::new_synced_web3_client, logging, settings};
+    use crate::{
+        eth::{key_manager::KeyManager, new_synced_web3_client},
+        logging, settings,
+    };
 
     use super::*;
 
     #[tokio::test]
     #[ignore = "Depends on a running ganache instance, runs forever, useful for manually testing / observing incoming events"]
-    async fn subscribe_to_stake_manager_events() {
+    async fn subscribe_to_key_manager_events() {
         let logger = logging::test_utils::create_test_logger();
 
         let settings = settings::test_utils::new_test_settings().unwrap();
 
-        new_eth_event_stream(
-            new_synced_web3_client(&settings, &logger).await.unwrap(),
-            settings.eth.key_manager_eth_address,
-            0,
-            logger,
-        )
-        .await
-        .unwrap()
-        .collect::<Vec<_>>()
-        .await;
+        let key_manager = KeyManager::new(&settings).unwrap();
+
+        key_manager
+            .event_stream(
+                &new_synced_web3_client(&settings, &logger).await.unwrap(),
+                settings.eth.from_block,
+                &logger,
+            )
+            .await
+            .unwrap()
+            .collect::<Vec<_>>()
+            .await;
     }
 }
