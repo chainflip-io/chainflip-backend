b8f117e5b8e2b36ed1077186f8b0fee5aa97f58e
kylezs <zsembery.kyle@gmail.com>
2021-08-26 11:03:50 +0200

    try not running the script

--- engine/tests/scripts/setup.sh
+++ engine/tests/scripts/setup.sh
@@ -2,7 +2,7 @@
 
 set -ex
 # =============================================================
-# Script to setup for integration tests of the StakeManager witness
+# Script to setup for integration tests for the ETH witnesses
 # - Run from /engine/tests (important for the relative paths to work)
 #
 # =============================================================
@@ -27,7 +27,7 @@ cd eth-contracts
 poetry run poetry install
 
 # run the brownie script to generate events for the cfe to read
-# poetry run brownie networks add development hardhat cmd='npx hardhat node' host=http://127.0.0.1 port=8545
+poetry run brownie networks add development hardhat cmd='npx hardhat node' host=http://127.0.0.1 port=8545
 poetry run brownie run deploy_and all_events --network hardhat
 
 echo "Ready to run StakeManager and KeyManager witness integration tests"
