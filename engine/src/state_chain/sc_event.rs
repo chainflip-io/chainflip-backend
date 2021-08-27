191defb1f63db17e5865c670030db77b41be827a
kylezs <zsembery.kyle@gmail.com>
2021-08-27 14:30:52 +0200

    WIP

--- engine/src/state_chain/sc_event.rs
+++ engine/src/state_chain/sc_event.rs
@@ -1,12 +1,10 @@
 use codec::Decode;
 use substrate_subxt::RawEvent;
 
-use crate::mq::Subject;
-
 use anyhow::Result;
 
 use super::{
-    pallets::auction::{AuctionCompletedEvent, AuctionEvent},
+    pallets::auction::AuctionEvent,
     pallets::staking::StakingEvent,
     pallets::validator::ValidatorEvent,
     pallets::vaults::{KeygenRequestEvent, VaultsEvent},
@@ -22,27 +20,17 @@ pub enum SCEvent {
     VaultsEvent(VaultsEvent<StateChainRuntime>),
 }
 
-/// Raw substrate event to Subject and SCEvent
-pub fn raw_event_to_subject_and_sc_event(
-    raw_event: &RawEvent,
-) -> Result<Option<(Subject, SCEvent)>> {
-    let event = match raw_event.module.as_str() {
-        "Auction" => match raw_event.variant.as_str() {
-            "AuctionCompleted" => Ok(Some((
-                Subject::AuctionCompleted,
-                AuctionCompletedEvent::<StateChainRuntime>::decode(&mut &raw_event.data[..])?
-                    .into(),
-            ))),
-            _ => Ok(None),
-        },
+/// Convert raw Substrate event to `SCEvent`
+/// Supported events are:
+/// - Vaults::KeygenRequest
+pub fn raw_event_to_sc_event(raw_event: &RawEvent) -> Result<Option<SCEvent>> {
+    match raw_event.module.as_str() {
         "Vaults" => match raw_event.variant.as_str() {
-            "KeygenRequest" => Ok(Some((
-                Subject::AuctionCompleted,
+            "KeygenRequest" => Ok(Some(
                 KeygenRequestEvent::<StateChainRuntime>::decode(&mut &raw_event.data[..])?.into(),
-            ))),
+            )),
             _ => Ok(None),
         },
         _ => Ok(None),
-    };
-    event
+    }
 }
--- engine/src/state_chain/sc_observer.rs
+++ engine/src/state_chain/sc_observer.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use futures::Future;
 use pallet_cf_vaults::rotation::{ChainParams, VaultRotationResponse};
 use slog::o;
 use sp_core::Hasher;
@@ -22,7 +23,7 @@ use crate::{
 
 use sp_keyring::AccountKeyring;
 
-use super::{runtime::StateChainRuntime, sc_event::raw_event_to_subject_and_sc_event};
+use super::{runtime::StateChainRuntime, sc_event::raw_event_to_sc_event};
 
 pub async fn start(
     subxt_client: Client<StateChainRuntime>,
@@ -30,176 +31,133 @@ pub async fn start(
     multisig_instruction_sender: UnboundedSender<MultisigInstruction>,
     logger: &slog::Logger,
 ) {
-    SCObserver::new(
-        subxt_client,
-        eth_broadcaster,
-        multisig_instruction_sender,
-        logger,
-    )
-    .await
-    .run()
-    .await
-    .expect("SC Observer has died!");
-}
+    let logger = logger.new(o!(COMPONENT_KEY => "SCObserver"));
+    // subscribe to all finalised events, and then redirect them
+    let sub = subxt_client
+        .subscribe_finalized_events()
+        .await
+        .expect("Could not subscribe to state chain events");
 
-pub struct SCObserver {
-    subxt_client: Client<StateChainRuntime>,
-    eth_broadcaster: EthBroadcaster,
-    multisig_instruction_sender: UnboundedSender<MultisigInstruction>,
-    logger: slog::Logger,
-}
-
-impl SCObserver {
-    pub async fn new(
-        subxt_client: Client<StateChainRuntime>,
-        eth_broadcaster: EthBroadcaster,
-        multisig_instruction_sender: UnboundedSender<MultisigInstruction>,
-        logger: &slog::Logger,
-    ) -> Self {
-        Self {
-            subxt_client,
-            eth_broadcaster,
-            multisig_instruction_sender,
-            logger: logger.new(o!(COMPONENT_KEY => "SCObserver")),
-        }
-    }
-
-    pub async fn run(&self) -> Result<()> {
-        // subscribe to all finalised events, and then redirect them
-        let sub = self
-            .subxt_client
-            .subscribe_finalized_events()
-            .await
-            .expect("Could not subscribe to state chain events");
-        let decoder = self.subxt_client.events_decoder();
-        let mut sub = EventSubscription::new(sub, decoder);
-
-        while let Some(res_event) = sub.next().await {
-            let raw_event = match res_event {
-                Ok(raw_event) => raw_event,
-                Err(e) => {
-                    slog::error!(self.logger, "Next event could not be read: {}", e);
-                    continue;
-                }
-            };
-
-            let subject_and_sc_event = raw_event_to_subject_and_sc_event(&raw_event)?;
-
-            if let None = subject_and_sc_event {
-                slog::trace!(self.logger, "Discarding {:?}", raw_event);
+    let decoder = subxt_client.events_decoder();
+    let mut sub = EventSubscription::new(sub, decoder);
+    while let Some(res_event) = sub.next().await {
+        let raw_event = match res_event {
+            Ok(raw_event) => raw_event,
+            Err(e) => {
+                slog::error!(logger, "Next event could not be read: {}", e);
                 continue;
             }
+        };
 
-            let (_subject, sc_event) =
-                subject_and_sc_event.expect("Must be Some due to condition above");
-
-            match sc_event {
-                AuctionEvent(_) => todo!(),
-                ValidatorEvent(_) => todo!(),
-                StakingEvent(_) => todo!(),
-                VaultsEvent(event) => match event {
-                    KeygenRequestEvent(keygen_request_event) => {
-                        let validators: Vec<_> = keygen_request_event
-                            .keygen_request
-                            .validator_candidates
-                            .iter()
-                            .map(|v| p2p::ValidatorId(v.clone().into()))
-                            .collect();
-                        // TODO: Should this be request index? @andy
-                        let key_gen_info =
-                            KeygenInfo::new(KeyId(keygen_request_event.request_index), validators);
-                        let gen_new_key_event = MultisigInstruction::KeyGen(key_gen_info);
-                        // We need the Sender for Multisig instructions channel here
-                        self.multisig_instruction_sender
-                            .send(gen_new_key_event)
-                            .map_err(|_| "Receiver should exist")
-                            .unwrap();
-                    }
-                    EthSignTxRequestEvent(eth_sign_tx_request) => {
-                        let validators: Vec<_> = eth_sign_tx_request
-                            .eth_signing_tx_request
-                            .validators
-                            .iter()
-                            .map(|v| p2p::ValidatorId(v.clone().into()))
-                            .collect();
-
-                        // TODO: Should this hash be on the state chain or the signing module?
-                        // https://github.com/chainflip-io/chainflip-backend/issues/446
-                        let hash = Keccak256::hash(
-                            &eth_sign_tx_request.eth_signing_tx_request.payload[..],
-                        );
-                        let message_hash = MessageHash(hash.0);
-
-                        // TODO: we want to use some notion of "KeyId"
-                        // https://github.com/chainflip-io/chainflip-backend/issues/442
-                        let signing_info =
-                            SigningInfo::new(KeyId(eth_sign_tx_request.request_index), validators);
-
-                        let sign_tx = MultisigInstruction::Sign(message_hash, signing_info);
-
-                        self.multisig_instruction_sender
-                            .send(sign_tx)
-                            .map_err(|_| "Receiver should exist")
-                            .unwrap();
-                    }
-                    VaultRotationRequestEvent(vault_rotation_request_event) => {
-                        let alice = AccountKeyring::Alice.pair();
-                        let pair_signer = PairSigner::new(alice);
-                        match vault_rotation_request_event.vault_rotation_request.chain {
-                            ChainParams::Ethereum(tx) => {
-                                slog::debug!(self.logger, "Broadcasting to ETH: {:?}", tx);
-                                match self
-                                    .eth_broadcaster
-                                    .sign_and_broadcast_to(tx.clone(), CFContract::KeyManager)
-                                    .await
-                                {
-                                    Ok(tx_hash) => {
-                                        slog::debug!(
-                                            self.logger,
-                                            "Broadcast set_agg_key_with_agg_key tx, tx_hash: {}",
-                                            tx_hash
-                                        );
-                                        self.subxt_client
-                                            .vault_rotation_response(
-                                                &pair_signer,
-                                                vault_rotation_request_event.request_index,
-                                                VaultRotationResponse::Success {
-                                                    // TODO: Add the actual keys here
-                                                    // why are these being added here? The SC should know these already? right?
-                                                    old_key: Vec::default(),
-                                                    new_key: Vec::default(),
-                                                    tx,
-                                                },
-                                            )
-                                            .await?;
-                                    }
-                                    Err(e) => {
-                                        slog::error!(
-                                            self.logger,
-                                            "Failed to broadcast set_agg_key_with_agg_key tx: {}",
-                                            e
-                                        );
-                                        self.subxt_client
-                                            .vault_rotation_response(
-                                                &pair_signer,
-                                                vault_rotation_request_event.request_index,
-                                                VaultRotationResponse::Failure,
-                                            )
-                                            .await?;
-                                    }
-                                }
-                            }
-                            // Leave this to be explicit about future chains being added
-                            ChainParams::Other(_) => todo!("Chain::Other does not exist"),
-                        }
-                    }
-                },
-            }
+        if let None =
+            raw_event_to_sc_event(&raw_event).expect("Could not convert substrate event to SCEvent")
+        {
+            slog::trace!(logger, "No action for raw event: {:?}", raw_event);
+            continue;
         }
 
-        let err_msg = "State Chain Observer stopped subscribing to events!";
-        slog::error!(self.logger, "{}", err_msg);
-        Err(anyhow::Error::msg(err_msg))
+        match sc_event.expect("Not None, checked above") {
+            AuctionEvent(_) => todo!(),
+            ValidatorEvent(_) => todo!(),
+            StakingEvent(_) => todo!(),
+            VaultsEvent(event) => match event {
+                KeygenRequestEvent(keygen_request_event) => {
+                    let validators: Vec<_> = keygen_request_event
+                        .keygen_request
+                        .validator_candidates
+                        .iter()
+                        .map(|v| p2p::ValidatorId(v.clone().into()))
+                        .collect();
+                    // TODO: Should this be request index? @andy
+                    let key_gen_info =
+                        KeygenInfo::new(KeyId(keygen_request_event.request_index), validators);
+                    let gen_new_key_event = MultisigInstruction::KeyGen(key_gen_info);
+                    // We need the Sender for Multisig instructions channel here
+                    multisig_instruction_sender
+                        .send(gen_new_key_event)
+                        .map_err(|_| "Receiver should exist")
+                        .unwrap();
+                }
+                EthSignTxRequestEvent(eth_sign_tx_request) => {
+                    let validators: Vec<_> = eth_sign_tx_request
+                        .eth_signing_tx_request
+                        .validators
+                        .iter()
+                        .map(|v| p2p::ValidatorId(v.clone().into()))
+                        .collect();
+
+                    // TODO: Should this hash be on the state chain or the signing module?
+                    // https://github.com/chainflip-io/chainflip-backend/issues/446
+                    let hash =
+                        Keccak256::hash(&eth_sign_tx_request.eth_signing_tx_request.payload[..]);
+                    let message_hash = MessageHash(hash.0);
+
+                    // TODO: we want to use some notion of "KeyId"
+                    // https://github.com/chainflip-io/chainflip-backend/issues/442
+                    let signing_info =
+                        SigningInfo::new(KeyId(eth_sign_tx_request.request_index), validators);
+
+                    let sign_tx = MultisigInstruction::Sign(message_hash, signing_info);
+
+                    multisig_instruction_sender
+                        .send(sign_tx)
+                        .map_err(|_| "Receiver should exist")
+                        .unwrap();
+                }
+                VaultRotationRequestEvent(vault_rotation_request_event) => {
+                    let alice = AccountKeyring::Alice.pair();
+                    let pair_signer = PairSigner::new(alice);
+                    match vault_rotation_request_event.vault_rotation_request.chain {
+                        ChainParams::Ethereum(tx) => {
+                            slog::debug!(logger, "Broadcasting to ETH: {:?}", tx);
+                            match eth_broadcaster
+                                .sign_and_broadcast_to(tx.clone(), CFContract::KeyManager)
+                                .await
+                            {
+                                Ok(tx_hash) => {
+                                    slog::debug!(
+                                        logger,
+                                        "Broadcast set_agg_key_with_agg_key tx, tx_hash: {}",
+                                        tx_hash
+                                    );
+                                    subxt_client
+                                        .vault_rotation_response(
+                                            &pair_signer,
+                                            vault_rotation_request_event.request_index,
+                                            VaultRotationResponse::Success {
+                                                // TODO: Add the actual keys here
+                                                // why are these being added here? The SC should know these already? right?
+                                                old_key: Vec::default(),
+                                                new_key: Vec::default(),
+                                                tx,
+                                            },
+                                        )
+                                        .await
+                                        .unwrap(); // TODO: Handle error
+                                }
+                                Err(e) => {
+                                    slog::error!(
+                                        logger,
+                                        "Failed to broadcast set_agg_key_with_agg_key tx: {}",
+                                        e
+                                    );
+                                    subxt_client
+                                        .vault_rotation_response(
+                                            &pair_signer,
+                                            vault_rotation_request_event.request_index,
+                                            VaultRotationResponse::Failure,
+                                        )
+                                        .await
+                                        .unwrap(); // TODO: Handle error
+                                }
+                            }
+                        }
+                        // Leave this to be explicit about future chains being added
+                        ChainParams::Other(_) => todo!("Chain::Other does not exist"),
+                    }
+                }
+            },
+        }
     }
 }
 
