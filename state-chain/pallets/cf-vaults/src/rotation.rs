c3b5d5246c3d535b6041d9eb5ff6ca02c5c4966a
Andy Bell <andyjsbell@gmail.com>
2021-09-01 09:16:02 +0200

    Small doc change

--- state-chain/pallets/cf-vaults/src/rotation.rs
+++ state-chain/pallets/cf-vaults/src/rotation.rs
@@ -121,7 +121,7 @@ pub enum VaultRotationResponse<PublicKey: Into<Vec<u8>>, Transaction: Into<Vec<u
 /// A signing request
 #[derive(PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug)]
 pub struct ThresholdSignatureRequest<PublicKey, ValidatorId> {
-	/// Payload to be signed by the existing aggregate key
+	/// Payload to be signed over
 	pub(crate) payload: Vec<u8>,
 	/// The public key of the key to be used to sign with
 	pub(crate) public_key: PublicKey,
