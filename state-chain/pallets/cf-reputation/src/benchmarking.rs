f2ae502fba4914b04f7a093f7144ead8650015b0
Jan Börner <jan@chainflip.io>
2021-09-24 16:16:47 +0200

    * updated benchmarks * added weights for pallets reputation

--- state-chain/pallets/cf-reputation/src/benchmarking.rs
+++ state-chain/pallets/cf-reputation/src/benchmarking.rs
@@ -37,18 +37,17 @@ fn expected_reputation_penalty<T: Config>() -> ReputationPoints {
 }
 
 benchmarks! {
+	// heartbeat {
+	// 	let b in 0 .. VALIDATOR_COUNT;
+	// 	let accounts = initialise_pallet::<T>(VALIDATOR_COUNT);
+	// }: _(RawOrigin::Signed(accounts[b as usize].clone()))
+	// verify {
+	// 	let validator_id: T::ValidatorId = accounts[b as usize].clone().into();
+	// 	let expected_credits = Pallet::<T>::online_credit_reward();
+	// 	let reputation_for_validator = Pallet::<T>::reputation(&validator_id);
+	// 	assert_eq!(reputation_for_validator.online_credits, expected_credits);
+	// }
 	heartbeat {
-		let b in 0 .. VALIDATOR_COUNT;
-		let accounts = initialise_pallet::<T>(VALIDATOR_COUNT);
-	}: _(RawOrigin::Signed(accounts[b as usize].clone()))
-	verify {
-		let validator_id: T::ValidatorId = accounts[b as usize].clone().into();
-		let expected_credits = Pallet::<T>::online_credit_reward();
-		let reputation_for_validator = Pallet::<T>::reputation(&validator_id);
-		assert_eq!(reputation_for_validator.online_credits, expected_credits);
-	}
-
-	next_heartbeat {
 		let b in 0 .. VALIDATOR_COUNT;
 		let accounts = initialise_pallet::<T>(VALIDATOR_COUNT);
 
@@ -59,7 +58,7 @@ benchmarks! {
 				reputation_points: 1,
 			});
 		}
-	}: heartbeat(RawOrigin::Signed(accounts[b as usize].clone()))
+	}: _(RawOrigin::Signed(accounts[b as usize].clone()))
 	verify {
 		let validator_id: T::ValidatorId = accounts[b as usize].clone().into();
 		let expected_credits_after_two_heartbeat_intervals =
@@ -71,48 +70,74 @@ benchmarks! {
 		);
 	}
 
-	check_liveness {
-		let accounts = initialise_pallet::<T>(VALIDATOR_COUNT);
-		// All of the VALIDATOR_COUNT will be penalised, this wouldn't occur but for benchmarking
-		// here it is
-	} : {
-		Pallet::<T>::check_liveness();
-	}
-	verify {
-		for validator_id in &as_validators::<T>(&accounts) {
-			let reputation_for_validator = Pallet::<T>::reputation(&validator_id);
-			assert_eq!(
-				reputation_for_validator.reputation_points,
-				0 - expected_reputation_penalty::<T>()
-			);
-		}
-	}
+	on_initialize {
+		let x in 1 .. 1_000;
+		let max_block = x as u64;
 
-	check_liveness_at_eighty_percent {
-		let accounts = initialise_pallet::<T>(VALIDATOR_COUNT);
-
-		let mut eighty_percent_of_validators : i32 = (VALIDATOR_COUNT as i32 * 8) / 10;
+		let mut eighty_percent_of_validators : u32 = (VALIDATOR_COUNT as u32 * 8) / 10;
 		let mut dead_validators: Vec<T::ValidatorId> = vec![];
-		<Pallet<T> as Store>::AwaitingHeartbeats::translate(|validator_id, _awaiting: bool| {
-			eighty_percent_of_validators -= 1;
-			if eighty_percent_of_validators < 0 {
-				dead_validators.push(validator_id);
-			}
-			Some(eighty_percent_of_validators < 0)
-		});
+		let accounts = initialise_pallet::<T>(eighty_percent_of_validators);
 
-	} : {
-		Pallet::<T>::check_liveness();
-	}
-	verify {
-		for validator_id in dead_validators {
-			let reputation_for_validator = Pallet::<T>::reputation(&validator_id);
-			assert_eq!(
-				reputation_for_validator.reputation_points,
-				0 - expected_reputation_penalty::<T>()
-			);
+		// Give the validators reputation, this is equivalent to a heartbeat interval having passed
+		for validator_id in &as_validators::<T>(&accounts) {
+			<Pallet<T> as Store>::Reputations::insert(&validator_id, Reputation {
+				online_credits: Pallet::<T>::online_credit_reward(),
+				reputation_points: 1,
+			});
+		}
+	}: {
+		for c in 0..max_block {
+			Pallet::<T>::on_initialize((c as u32).into());
 		}
 	}
+
+	update_accrual_ratio {
+		let accounts = initialise_pallet::<T>(VALIDATOR_COUNT);
+		let b in 200 .. 1_000;
+	} : _(RawOrigin::Root, 2, b.into())
+
+	// check_liveness {
+	// 	let accounts = initialise_pallet::<T>(VALIDATOR_COUNT);
+	// 	// All of the VALIDATOR_COUNT will be penalised, this wouldn't occur but for benchmarking
+	// 	// here it is
+	// } : {
+	// 	Pallet::<T>::check_liveness();
+	// }
+	// verify {
+	// 	for validator_id in &as_validators::<T>(&accounts) {
+	// 		let reputation_for_validator = Pallet::<T>::reputation(&validator_id);
+	// 		assert_eq!(
+	// 			reputation_for_validator.reputation_points,
+	// 			0 - expected_reputation_penalty::<T>()
+	// 		);
+	// 	}
+	// }
+
+	// check_liveness_at_eighty_percent {
+	// 	let accounts = initialise_pallet::<T>(VALIDATOR_COUNT);
+
+	// 	let mut eighty_percent_of_validators : i32 = (VALIDATOR_COUNT as i32 * 8) / 10;
+	// 	let mut dead_validators: Vec<T::ValidatorId> = vec![];
+	// 	<Pallet<T> as Store>::AwaitingHeartbeats::translate(|validator_id, _awaiting: bool| {
+	// 		eighty_percent_of_validators -= 1;
+	// 		if eighty_percent_of_validators < 0 {
+	// 			dead_validators.push(validator_id);
+	// 		}
+	// 		Some(eighty_percent_of_validators < 0)
+	// 	});
+
+	// } : {
+	// 	Pallet::<T>::check_liveness();
+	// }
+	// verify {
+	// 	for validator_id in dead_validators {
+	// 		let reputation_for_validator = Pallet::<T>::reputation(&validator_id);
+	// 		assert_eq!(
+	// 			reputation_for_validator.reputation_points,
+	// 			0 - expected_reputation_penalty::<T>()
+	// 		);
+	// 	}
+	// }
 }
 
 #[cfg(test)]
