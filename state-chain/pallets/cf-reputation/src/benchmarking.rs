bb7bfad4238d2f859d1fffd90fcd503162839da8
Jan Börner <jan@chainflip.io>
2021-09-27 11:04:47 +0200

    * removed old code

--- state-chain/pallets/cf-reputation/src/benchmarking.rs
+++ state-chain/pallets/cf-reputation/src/benchmarking.rs
@@ -37,16 +37,6 @@ fn expected_reputation_penalty<T: Config>() -> ReputationPoints {
 }
 
 benchmarks! {
-	// heartbeat {
-	// 	let b in 0 .. VALIDATOR_COUNT;
-	// 	let accounts = initialise_pallet::<T>(VALIDATOR_COUNT);
-	// }: _(RawOrigin::Signed(accounts[b as usize].clone()))
-	// verify {
-	// 	let validator_id: T::ValidatorId = accounts[b as usize].clone().into();
-	// 	let expected_credits = Pallet::<T>::online_credit_reward();
-	// 	let reputation_for_validator = Pallet::<T>::reputation(&validator_id);
-	// 	assert_eq!(reputation_for_validator.online_credits, expected_credits);
-	// }
 	heartbeat {
 		let b in 0 .. VALIDATOR_COUNT;
 		let accounts = initialise_pallet::<T>(VALIDATOR_COUNT);
@@ -95,79 +85,6 @@ benchmarks! {
 		let accounts = initialise_pallet::<T>(VALIDATOR_COUNT);
 		let b in 200 .. 1_000;
 	} : _(RawOrigin::Root, 2, b.into())
-
-	// check_liveness {
-	// 	let accounts = initialise_pallet::<T>(VALIDATOR_COUNT);
-	// 	// All of the VALIDATOR_COUNT will be penalised, this wouldn't occur but for benchmarking
-	// 	// here it is
-	// } : {
-	// 	Pallet::<T>::check_liveness();
-	// }
-	// verify {
-	// 	for validator_id in &as_validators::<T>(&accounts) {
-	// 		let reputation_for_validator = Pallet::<T>::reputation(&validator_id);
-	// 		assert_eq!(
-	// 			reputation_for_validator.reputation_points,
-	// 			0 - expected_reputation_penalty::<T>()
-	// 		);
-	// 	}
-	// }
-
-	// check_liveness_at_eighty_percent {
-	// 	let accounts = initialise_pallet::<T>(VALIDATOR_COUNT);
-
-	// 	let mut eighty_percent_of_validators : i32 = (VALIDATOR_COUNT as i32 * 8) / 10;
-	// 	let mut dead_validators: Vec<T::ValidatorId> = vec![];
-	// 	<Pallet<T> as Store>::AwaitingHeartbeats::translate(|validator_id, _awaiting: bool| {
-	// 		eighty_percent_of_validators -= 1;
-	// 		if eighty_percent_of_validators < 0 {
-	// 			dead_validators.push(validator_id);
-	// 		}
-	// 		Some(eighty_percent_of_validators < 0)
-	// 	});
-
-	// } : {
-	// 	Pallet::<T>::check_liveness();
-	// }
-	// verify {
-	// 	for validator_id in dead_validators {
-	// 		let reputation_for_validator = Pallet::<T>::reputation(&validator_id);
-	// 		assert_eq!(
-	// 			reputation_for_validator.reputation_points,
-	// 			0 - expected_reputation_penalty::<T>()
-	// 		);
-	// 	}
-	// }
 }
-
-// #[cfg(test)]
-// mod tests {
-// 	use super::*;
-// 	use crate::mock::Test;
-// 	use frame_support::assert_ok;
-// 	use sp_io::TestExternalities;
-
-// 	pub fn new_test_ext() -> TestExternalities {
-// 		let t = frame_system::GenesisConfig::default()
-// 			.build_storage::<Test>()
-// 			.unwrap();
-// 		TestExternalities::new(t)
-// 	}
-
-// 	#[test]
-// 	fn bench_heartbeat() {
-// 		new_test_ext().execute_with(|| {
-// 			assert_ok!(test_benchmark_heartbeat::<Test>());
-// 			assert_ok!(test_benchmark_next_heartbeat::<Test>());
-// 		});
-// 	}
-
-// 	#[test]
-// 	fn bench_check_liveness() {
-// 		new_test_ext().execute_with(|| {
-// 			assert_ok!(test_benchmark_check_liveness::<Test>());
-// 		});
-// 	}
-// }
 
 impl_benchmark_test_suite!(Pallet, crate::mock::new_test_ext(), crate::mock::Test,);
