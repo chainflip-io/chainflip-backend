e0f659643bff9bdf26a7142b5554942107522b03
Jan Börner <jan@chainflip.io>
2021-09-28 11:02:13 +0200

    * run benchmark * updated comment

--- state-chain/scripts/benchmark.sh
+++ state-chain/scripts/benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# build the node with debug features enabled
+# build the node with benchmark features enabled
 cargo build --release --features runtime-benchmarks
 # execute the benchmark for pallet auction
 ./target/release/state-chain-node benchmark --extrinsic '*' --pallet pallet_cf_auction --output state-chain/runtime/src/weights --execution=wasm --steps=50 --repeat=20
