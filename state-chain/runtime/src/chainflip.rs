e16eecff27d33065f43acc42230d25724034adea
Andy Bell <andyjsbell@gmail.com>
2021-10-04 12:30:10 +0200

    Small rename

--- state-chain/runtime/src/chainflip.rs
+++ state-chain/runtime/src/chainflip.rs
@@ -12,19 +12,19 @@ impl EpochTransitionHandler for ChainflipEpochTransitions {
 	type ValidatorId = AccountId;
 	type Amount = FlipBalance;
 
-	fn on_new_epoch(_new_validators: &[Self::ValidatorId], _new_bond: Self::Amount) {
+	fn on_new_epoch(new_validators: &[Self::ValidatorId], new_bond: Self::Amount) {
 		// Process any outstanding emissions.
 		<Emissions as EmissionsTrigger>::trigger_emissions();
 		// Rollover the rewards.
-		Rewards::rollover(_new_validators).unwrap_or_else(|err| {
+		Rewards::rollover(new_validators).unwrap_or_else(|err| {
 			debug::error!("Unable to process rewards rollover: {:?}!", err);
 		});
 		// Update the the bond of all validators for the new epoch
-		<Flip as BondRotation>::update_validator_bonds(_new_validators, _new_bond);
+		<Flip as BondRotation>::update_validator_bonds(new_validators, new_bond);
 		// Update the list of validators in reputation
-		<Reputation as EpochTransitionHandler>::on_new_epoch(_new_validators, _new_bond);
+		<Reputation as EpochTransitionHandler>::on_new_epoch(new_validators, new_bond);
 		// Update the list of validators in the witnesser.
-		<Witnesser as EpochTransitionHandler>::on_new_epoch(_new_validators, _new_bond)
+		<Witnesser as EpochTransitionHandler>::on_new_epoch(new_validators, new_bond)
 	}
 }
 
