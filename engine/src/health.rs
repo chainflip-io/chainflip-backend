2d852813e96983a5667800d44537c7356893d871
Jamie Ford <jamie@chainflip.io>
2021-10-08 16:09:07 +1100

    fix unwrap_or_else error

--- engine/src/health.rs
+++ engine/src/health.rs
@@ -34,7 +34,9 @@ impl HealthMonitor {
         slog::info!(self.logger, "Starting");
         let listener = TcpListener::bind(self.bind_address.clone())
             .await
-            .unwrap_or_else(|_| panic!("Could not bind TCP listener to {}", self.bind_address));
+            .unwrap_or_else(|e| {
+                panic!("Could not bind TCP listener to {}: ", self.bind_address, e)
+            });
 
         let (shutdown_sender, mut shutdown_receiver) = tokio::sync::oneshot::channel::<()>();
         let logger = self.logger.clone();
