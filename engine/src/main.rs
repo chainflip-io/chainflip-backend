77f6276a88d469df74480f548f163a28f32f38ab
kylezs <zsembery.kyle@gmail.com>
2021-10-25 15:17:33 +0200

    remove stuff for easier compile

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -55,9 +55,7 @@ async fn main() {
     let (p2p_message_command_sender, p2p_message_command_receiver) =
         tokio::sync::mpsc::unbounded_channel::<P2PMessageCommand>();
 
-    let duty_manager = Arc::new(RwLock::new(DutyManager::new(AccountId(
-        key_pair.public().0,
-    ))));
+    let duty_manager = Arc::new(RwLock::new(DutyManager::new(account_id.clone())));
 
     let web3 = eth::new_synced_web3_client(&settings, &root_logger)
         .await
