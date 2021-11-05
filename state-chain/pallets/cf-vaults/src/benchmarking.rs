53286dd3e4b0aa5e1e8a5eb72399906dd639583a
Jan Börner <jan@chainflip.io>
2021-11-05 09:35:58 +0100

    * updated vaults benchmark

--- state-chain/pallets/cf-vaults/src/benchmarking.rs
+++ state-chain/pallets/cf-vaults/src/benchmarking.rs
@@ -12,7 +12,6 @@ use sp_std::{boxed::Box, vec, vec::Vec};
 use crate::Pallet;
 
 benchmarks! {
-	// TODO: implement benchmark
 	keygen_success {
 		let chain_id = ChainId::Ethereum;
 		let caller: T::AccountId = whitelisted_caller();
@@ -22,30 +21,37 @@ benchmarks! {
 			chain_id,
 			VaultRotationStatus::<T>::AwaitingRotation {  new_public_key: new_public_key.to_vec() },
 		);
-		let call = Call::<T>::keygen_success(1, chain_id, new_public_key.to_vec());
+		let ceremony_id = Pallet::<T>::keygen_ceremony_id_counter();
+		let call = Call::<T>::keygen_success(ceremony_id, chain_id, new_public_key.to_vec());
 		let origin = T::EnsureWitnessed::successful_origin();
 		// 2. Pass invalid rotations status check
 	} : { call.dispatch_bypass_filter(origin)? }
-	// TODO: implement benchmark
-	keygen_failure {} : {}
-	// TODO: implement benchmark
-	vault_key_rotated {} : {}
-	// TODO: implement benchmark
-	// threshold_signature_response {
-	// 	// let caller: T::AccountId = whitelisted_caller();
-	// 	// let ceremony_id = Pallet::<T>::next_ceremony_id();
-	// 	// // let keygen_request = KeygenRequest {
-	// 	// // 	chain: Chain::Ethereum,
-	// 	// // 	validator_candidates: vec![caller.clone().into()],
-	// 	// // };
-	// 	// // KeygenRequestResponse::<T>::make_request(ceremony_id, keygen_request);
-	// 	// let origin = T::EnsureWitnessed::successful_origin();
-	// 	// // TODO: fails on InvalidCeremonyId - no idea were this comes from
-	// 	// let call = Call::<T>::threshold_signature_response(ceremony_id, ThresholdSignatureResponse::Success {
-	// 	// 	message_hash: [0; 32],
-	// 	// 	signature: SchnorrSigTruncPubkey::default(),
-	// 	// });
-	// } : { }
+	keygen_failure {
+		let chain_id = ChainId::Ethereum;
+		let caller: T::AccountId = whitelisted_caller();
+		let new_public_key: [u8; 33] = [0x02; 33];
+		// 1. Pass the active rotation check
+		PendingVaultRotations::<T>::insert(
+			chain_id,
+			VaultRotationStatus::<T>::AwaitingRotation {  new_public_key: new_public_key.to_vec() },
+		);
+		let ceremony_id = Pallet::<T>::keygen_ceremony_id_counter();
+		let call = Call::<T>::keygen_failure(ceremony_id, chain_id, vec![]);
+		let origin = T::EnsureWitnessed::successful_origin();
+	} : { call.dispatch_bypass_filter(origin)? }
+	vault_key_rotated {
+		let chain_id = ChainId::Ethereum;
+		let caller: T::AccountId = whitelisted_caller();
+		let new_public_key: [u8; 33] = [0x02; 33];
+		let tx_hash: [u8; 32] = [0xab; 32];
+
+		PendingVaultRotations::<T>::insert(
+			chain_id,
+			VaultRotationStatus::<T>::AwaitingRotation {  new_public_key: new_public_key.to_vec() },
+		);
+		let call = Call::<T>::vault_key_rotated(chain_id, new_public_key.to_vec(), 5 as u64, tx_hash.to_vec());
+		let origin = T::EnsureWitnessed::successful_origin();
+	} : { call.dispatch_bypass_filter(origin)? }
 }
 
 impl_benchmark_test_suite!(Pallet, crate::mock::new_test_ext(), crate::mock::Test,);
