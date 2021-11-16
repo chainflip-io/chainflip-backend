bcb57aa771d8d8ccb11de6dd84aa9c23523bc010
dandanlen <3168260+dandanlen@users.noreply.github.com>
2021-11-16 09:29:57 +0000

    Add TODO reminders to verification code.

--- state-chain/chains/src/eth.rs
+++ state-chain/chains/src/eth.rs
@@ -229,6 +229,8 @@ impl AggKey {
 	}
 
 	/// Verify a signature against a given message hash for this public key.
+	/// 
+	/// **TODO: In-depth review to ensure correctness.**
 	pub fn verify(
 		&self,
 		msg_hash: &[u8; 32],
@@ -549,6 +551,8 @@ impl UnsignedTransaction {
 pub type RawSignedTransaction = Vec<u8>;
 
 /// Checks that the raw transaction is a valid rlp-encoded transaction.
+/// 
+/// **TODO: In-depth review to ensure correctness.**
 pub fn verify_transaction(
 	unsigned: &UnsignedTransaction,
 	signed: &RawSignedTransaction,
