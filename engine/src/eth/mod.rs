498586d6d67a21ff3f058d3be1766220232b21ca
dandanlen <3168260+dandanlen@users.noreply.github.com>
2021-11-16 10:33:30 +0100

    Update engine/src/eth/mod.rs
    Co-authored-by: Alastair Holmes <42404303+AlastairHolmes@users.noreply.github.com>

--- engine/src/eth/mod.rs
+++ engine/src/eth/mod.rs
@@ -116,7 +116,7 @@ impl EthBroadcaster {
             to: Some(unsigned_tx.contract),
             data: unsigned_tx.data.into(),
             chain_id: Some(unsigned_tx.chain_id),
-            value: web3::types::U256(unsigned_tx.value.0),
+            value: unsigned_tx.value,
             transaction_type: Some(web3::types::U64::from(2)),
             ..Default::default()
         };
