f5fa71068a9cf73e7001b8bbd254241cfcb9ede4
Andy Bell <andyjsbell@gmail.com>
2021-11-23 13:14:09 +0100

    fix benchmark

--- state-chain/pallets/cf-online/src/benchmarking.rs
+++ state-chain/pallets/cf-online/src/benchmarking.rs
@@ -16,15 +16,13 @@ benchmarks! {
 	heartbeat {
 		let caller: T::AccountId = whitelisted_caller();
 		let validator_id: T::ValidatorId = caller.clone().into();
-		let block_number: T::BlockNumber = 1u32.into();
-		Nodes::<T>::insert(&validator_id, block_number);
+		Nodes::<T>::insert(&validator_id, Node::<T>::default());
 	} : _(RawOrigin::Signed(caller))
 	submit_network_state {
 		for b in 1 .. MAX_VALIDATOR_AMOUNT {
 			let caller: T::AccountId  = account("doogle", b, b);
 			let validator_id: T::ValidatorId = caller.into();
-			let block_number: T::BlockNumber = 1u32.into();
-			Nodes::<T>::insert(&validator_id, block_number);
+			Nodes::<T>::insert(&validator_id, Node::<T>::default());
 		}
 		// TODO: set the generated validators as active validators
 	} : {
