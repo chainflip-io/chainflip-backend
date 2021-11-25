3740929f07fa1e0f8c080e108d71346315924121
Andy Bell <andyjsbell@gmail.com>
2021-11-25 10:08:56 +0100

    fmt!

--- state-chain/pallets/cf-flip/src/lib.rs
+++ state-chain/pallets/cf-flip/src/lib.rs
@@ -484,7 +484,7 @@ where
 		let slashing_rate: T::Balance = SlashingRate::<T>::get();
 		// Check that the slashing rate is not zero, no need to slash if this is set to zero, right
 		if slashing_rate == Zero::zero() {
-			return T::DbWeight::get().reads(1);
+			return T::DbWeight::get().reads(1)
 		}
 		// Get the MBA aka the bond
 		let bond = Account::<T>::get(account_id).validator_bond;
