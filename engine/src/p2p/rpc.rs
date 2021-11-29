dd77d6b4ddc7e54c1085c4d5fa6cf309ca85f74d
Alastair Holmes <holmes.alastair@outlook.com>
2021-11-29 17:50:07 +0100

    rpc_error_into_anyhow_error uses Debug formatting instead of Display

--- engine/src/p2p/rpc.rs
+++ engine/src/p2p/rpc.rs
@@ -1,33 +1,21 @@
-use crate::common::into_anyhow_error;
+use crate::common::rpc_error_into_anyhow_error;
 use crate::p2p::{AccountId, P2PNetworkClient, StatusCode};
-use anyhow::Result;
+use anyhow::{Result, Context};
 use async_trait::async_trait;
 use cf_p2p::{AccountIdBs58, MessageBs58, P2PEvent, P2PRpcClient};
 use futures::{stream::BoxStream, TryStreamExt};
-use jsonrpc_core_client::RpcError;
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub enum RpcClientError {
-    #[error("Could not connect to {0:?}: {1:?}")]
-    ConnectionError(url::Url, RpcError),
-    #[error("Rpc error calling method {0:?}: {1:?}")]
-    CallError(String, RpcError),
-    #[error("Rpc subscription notified an error: {0:?}")]
-    SubscriptionError(RpcError),
-}
 
 pub async fn connect(url: &url::Url, validator_id: AccountId) -> Result<P2PRpcClient> {
     let client = jsonrpc_core_client::transports::ws::connect::<P2PRpcClient>(url)
         .await
-        .map_err(|e| into_anyhow_error(RpcClientError::ConnectionError(url.clone(), e)))?;
+        .map_err(rpc_error_into_anyhow_error)
+        .context("connect")?;
 
     client
         .self_identify(AccountIdBs58(validator_id.0))
         .await
-        .map_err(|e| {
-            into_anyhow_error(RpcClientError::CallError(String::from("self_identify"), e))
-        })?;
+        .map_err(rpc_error_into_anyhow_error)
+        .context("self_identify")?;
 
     Ok(client)
 }
@@ -39,25 +27,23 @@ impl P2PNetworkClient for P2PRpcClient {
     async fn broadcast(&self, data: &[u8]) -> Result<StatusCode> {
         P2PRpcClient::broadcast(self, MessageBs58(data.into()))
             .await
-            .map_err(|e| into_anyhow_error(RpcClientError::CallError(String::from("broadcast"), e)))
+            .map_err(rpc_error_into_anyhow_error)
+            .context("broadcast")
     }
 
     async fn send(&self, to: &AccountId, data: &[u8]) -> Result<StatusCode> {
         P2PRpcClient::send(self, AccountIdBs58(to.0), MessageBs58(data.into()))
             .await
-            .map_err(|e| into_anyhow_error(RpcClientError::CallError(String::from("send"), e)))
+            .map_err(rpc_error_into_anyhow_error)
+            .context("send")
     }
 
     async fn take_stream(&self) -> Result<BoxStream<Self::NetworkEvent>> {
         let stream = self
             .subscribe_notifications()
-            .map_err(|e| {
-                into_anyhow_error(RpcClientError::CallError(
-                    String::from("subscribe_notifications"),
-                    e,
-                ))
-            })?
-            .map_err(|e| into_anyhow_error(RpcClientError::SubscriptionError(e)));
+            .map_err(rpc_error_into_anyhow_error)
+            .context("subscribe_notifications")?
+            .map_err(rpc_error_into_anyhow_error);
 
         Ok(Box::pin(stream))
     }
--- engine/src/state_chain/client.rs
+++ engine/src/state_chain/client.rs
@@ -31,7 +31,7 @@ use substrate_subxt::{
     Runtime, SignedExtension, SignedExtra,
 };
 
-use crate::common::into_anyhow_error;
+use crate::common::rpc_error_into_anyhow_error;
 use crate::settings;
 
 #[cfg(test)]
@@ -187,7 +187,7 @@ impl StateChainRpcApi for StateChainRpcClient {
         self.chain_rpc_client
             .block(Some(block_hash))
             .await
-            .map_err(into_anyhow_error)
+            .map_err(rpc_error_into_anyhow_error)
     }
 
     async fn storage_events_at(
@@ -198,7 +198,7 @@ impl StateChainRpcApi for StateChainRpcClient {
         self.state_rpc_client
             .query_storage_at(vec![storage_key], block_hash)
             .await
-            .map_err(into_anyhow_error)
+            .map_err(rpc_error_into_anyhow_error)
     }
 }
 
@@ -266,9 +266,10 @@ impl<RpcClient: StateChainRpcApi> StateChainClient<RpcClient> {
                         slog::error!(logger, "Extrinsic submission failed with nonce: {}", nonce);
                     }
                     err => {
-                        slog::error!(logger, "Error: {:?}", err);
+                        let err = rpc_error_into_anyhow_error(err);
+                        slog::error!(logger, "{}", err);
                         self.nonce.fetch_sub(1, Ordering::Relaxed);
-                        return Err(into_anyhow_error(err));
+                        return Err(err);
                     }
                 },
             }
@@ -297,6 +298,7 @@ impl<RpcClient: StateChainRpcApi> StateChainClient<RpcClient> {
                 state_chain_runtime::Call::from(extrinsic.clone()).encode(),
             )))
             .await
+            .map_err(rpc_error_into_anyhow_error)
         {
             Ok(tx_hash) => {
                 slog::trace!(
@@ -308,8 +310,8 @@ impl<RpcClient: StateChainRpcApi> StateChainClient<RpcClient> {
                 return Ok(tx_hash);
             }
             Err(err) => {
-                slog::error!(logger, "Error: {}", err);
-                return Err(into_anyhow_error(err));
+                slog::error!(logger, "{}", err);
+                return Err(err);
             }
         }
     }
@@ -534,7 +536,7 @@ pub async fn connect_to_state_chain(
         state_chain_settings.ws_endpoint.as_str(),
     )?)
     .await
-    .map_err(into_anyhow_error)
+    .map_err(rpc_error_into_anyhow_error)
     .context("Failed to establish rpc connection to substrate node")?;
 
     let author_rpc_client: AuthorRpcClient = rpc_client.clone().into();
@@ -543,8 +545,8 @@ pub async fn connect_to_state_chain(
 
     let mut block_header_stream = chain_rpc_client
         .subscribe_finalized_heads()
-        .map_err(into_anyhow_error)?
-        .map_err(into_anyhow_error);
+        .map_err(rpc_error_into_anyhow_error)?
+        .map_err(rpc_error_into_anyhow_error);
 
     // TODO It is possible to avoid this wait, but somewhat complex and probably not needed
     if let Some(Ok(block_header)) = block_header_stream.next().await {
@@ -554,7 +556,7 @@ pub async fn connect_to_state_chain(
                     sp_rpc::number::NumberOrHex::from(block_header.number).into(),
                 ))
                 .await
-                .map_err(into_anyhow_error)?,
+                .map_err(rpc_error_into_anyhow_error)?,
             anyhow::Error::msg("Failed to get latest block hash"),
         )?;
 
@@ -562,7 +564,7 @@ pub async fn connect_to_state_chain(
             &mut &state_rpc_client
                 .metadata(Some(latest_block_hash))
                 .await
-                .map_err(into_anyhow_error)?[..],
+                .map_err(rpc_error_into_anyhow_error)?[..],
         )?)?;
 
         let system_pallet_metadata = metadata.module("System")?.clone();
@@ -593,7 +595,7 @@ pub async fn connect_to_state_chain(
                             .state_rpc_client
                             .storage(account_storage_key.clone(), Some(latest_block_hash))
                             .await
-                            .map_err(into_anyhow_error)?
+                            .map_err(rpc_error_into_anyhow_error)?
                             .ok_or_else(|| {
                                 anyhow::format_err!(
                                     "AccountId {:?} doesn't exist on the state chain.",
@@ -608,13 +610,13 @@ pub async fn connect_to_state_chain(
                     .state_rpc_client
                     .runtime_version(Some(latest_block_hash))
                     .await
-                    .map_err(into_anyhow_error)?,
+                    .map_err(rpc_error_into_anyhow_error)?,
                 genesis_hash: try_unwrap_value(
                     state_chain_rpc_client
                         .chain_rpc_client
                         .block_hash(Some(sp_rpc::number::NumberOrHex::from(0u64).into()))
                         .await
-                        .map_err(into_anyhow_error)?,
+                        .map_err(rpc_error_into_anyhow_error)?,
                     anyhow::Error::msg("Genesis block doesn't exist?"),
                 )?,
                 signer: signer.clone(),
