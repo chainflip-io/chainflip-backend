50cd13c2fe97ca78e23c1ae746108add6f4e16b2
Andy Bell <andyjsbell@gmail.com>
2021-12-09 13:58:54 +0100

    Centralise mock and include Banned

--- state-chain/traits/src/mocks/offline_conditions.rs
+++ state-chain/traits/src/mocks/offline_conditions.rs
@@ -2,7 +2,8 @@
 macro_rules! impl_mock_offline_conditions {
 	($account_id:ty) => {
 		thread_local! {
-			pub static REPORTED: std::cell::RefCell<Vec<$account_id>> = Default::default()
+			pub static REPORTED: std::cell::RefCell<Vec<$account_id>> = Default::default();
+			pub static BANNED_VALIDATORS: std::cell::RefCell<std::collections::HashMap<$account_id, ()>> = Default::default();
 		}
 
 		pub struct MockOfflineReporter;
@@ -16,15 +17,39 @@ macro_rules! impl_mock_offline_conditions {
 		pub struct MockOfflinePenalty;
 		impl $crate::offline_conditions::OfflinePenalty for MockOfflinePenalty {
 			fn penalty(
-				_condition: &$crate::offline_conditions::OfflineCondition,
+				condition: &$crate::offline_conditions::OfflineCondition,
 			) -> $crate::offline_conditions::ReputationPoints {
-				0
+				match condition {
+					$crate::offline_conditions::OfflineCondition::BroadcastOutputFailed => 10,
+					$crate::offline_conditions::OfflineCondition::ParticipateSigningFailed => 100,
+					$crate::offline_conditions::OfflineCondition::NotEnoughPerformanceCredits => 1000,
+				}
+			}
+		}
+
+		pub struct MockBanned;
+		impl $crate::offline_conditions::Banned for MockBanned {
+			type ValidatorId = $account_id;
+			fn ban(validator_id: &Self::ValidatorId) {
+				BANNED_VALIDATORS.with(|cell| {
+					(*(cell.borrow_mut())).insert(validator_id.clone(), ());
+				});
+			}
+		}
+
+		impl MockBanned {
+			pub fn is_banned(validator_id: &$account_id) -> bool {
+				BANNED_VALIDATORS.with(|cell| {
+					(*(cell.borrow())).contains_key(validator_id)
+				})
 			}
 		}
 
 		impl $crate::offline_conditions::OfflineReporter for MockOfflineReporter {
 			type ValidatorId = $account_id;
 			type Penalty = MockOfflinePenalty;
+			type Banned = MockBanned;
+
 			fn report(
 				_condition: $crate::offline_conditions::OfflineCondition,
 				validator_id: &Self::ValidatorId,
