e268f4640e1b2e6a4acc73c8561779c140ae41fa
Jan Börner <jan@chainflip.io>
2021-12-09 17:41:00 +0100

    * moved unit tests to tests file

--- state-chain/runtime/src/chainflip.rs
+++ state-chain/runtime/src/chainflip.rs
@@ -232,7 +232,7 @@ impl Heartbeat for ChainflipHeartbeat {
 }
 
 /// Returns a scaled index based on an input seed
-fn get_random_id_by_seed_in_range(seed: Vec<u8>, max: usize) -> usize {
+pub fn get_random_id_by_seed_in_range(seed: Vec<u8>, max: usize) -> usize {
 	let seed = twox_128(&seed)
 		.iter()
 		.fold(0, |acc: u64, x| acc.saturating_add(x.clone() as u64));
@@ -241,7 +241,7 @@ fn get_random_id_by_seed_in_range(seed: Vec<u8>, max: usize) -> usize {
 }
 
 /// Select the next signer
-fn select_signer<SignerId: Clone, T: IsOnline<ValidatorId = SignerId>>(
+pub fn select_signer<SignerId: Clone, T: IsOnline<ValidatorId = SignerId>>(
 	validators: Vec<(SignerId, ())>,
 	seed: Vec<u8>,
 ) -> Option<SignerId> {
@@ -444,91 +444,5 @@ impl cf_traits::offline_conditions::OfflinePenalty for OfflinePenalty {
 			OfflineCondition::ParticipateSigningFailed => 15,
 			OfflineCondition::NotEnoughPerformanceCredits => 100,
 		}
-	}
-}
-
-// Unit tests for runtime code
-mod test {
-	use cf_traits::IsOnline;
-	use sp_std::cell::RefCell;
-
-	use crate::chainflip::{get_random_id_by_seed_in_range, select_signer};
-	#[test]
-	fn test_generate_id() {
-		assert!(get_random_id_by_seed_in_range(vec![1, 6, 7, 4, 6, 7, 8], 5) < 5);
-		assert!(get_random_id_by_seed_in_range(vec![0, 0, 0], 5) < 5);
-		assert!(get_random_id_by_seed_in_range(vec![180, 200, 240], 10) < 10);
-	}
-
-	#[test]
-	fn test_select_signer() {
-		/// Generates a set of validators with the SignerId = index + 1
-		fn validator_set(len: usize) -> Vec<(u64, ())> {
-			let mut id: u64 = 0;
-			vec![(0, ()); len]
-				.iter()
-				.map(|_| {
-					id += 1;
-					(id, ())
-				})
-				.collect::<Vec<_>>()
-		}
-		thread_local! {
-			// Switch to control the mock
-			pub static ONLINE: RefCell<bool>  = RefCell::new(true);
-		}
-		struct MockIsOnline;
-		impl IsOnline for MockIsOnline {
-			type ValidatorId = u64;
-
-			fn is_online(_validator_id: &Self::ValidatorId) -> bool {
-				ONLINE.with(|cell| cell.borrow().clone())
-			}
-		}
-		// Expect Some validator
-		assert_eq!(
-			select_signer::<u64, MockIsOnline>(
-				vec![(4, ()), (6, ()), (7, ()), (9, ())],
-				vec![2, 5, 7, 3]
-			),
-			Some(6)
-		);
-		// Expect a validator in a set of 150 validators
-		assert_eq!(
-			select_signer::<u64, MockIsOnline>(
-				validator_set(150),
-				String::from("seed").into_bytes()
-			),
-			Some(75)
-		);
-		// Expect an comparable big change in the value
-		// distribution for an small input seed change
-		assert_eq!(
-			select_signer::<u64, MockIsOnline>(
-				validator_set(150),
-				String::from("seeed").into_bytes()
-			),
-			Some(147)
-		);
-		// Expect an reasonable SignerId for an bigger input seed
-		assert_eq!(
-			select_signer::<u64, MockIsOnline>(
-				validator_set(150),
-				String::from("west1_north_south_east:_berlin_zonk").into_bytes(),
-			),
-			Some(48)
-		);
-		// Switch the mock to simulate an situation where all
-		// validators are offline
-		ONLINE.with(|cell| *cell.borrow_mut() = false);
-		// Expect the select_signer function to return None
-		// if there is currently no online validator
-		assert_eq!(
-			select_signer::<u64, MockIsOnline>(
-				vec![(14, ()), (3, ()), (2, ()), (6, ())],
-				vec![2, 5, 9, 3]
-			),
-			None
-		);
 	}
 }
---
+++ state-chain/runtime/src/tests.rs
@@ -0,0 +1,79 @@
+use crate::chainflip::{get_random_id_by_seed_in_range, select_signer};
+use cf_traits::IsOnline;
+use sp_std::cell::RefCell;
+
+/// Generates a set of validators with the SignerId = index + 1
+fn validator_set(len: usize) -> Vec<(u64, ())> {
+	let mut id: u64 = 0;
+	vec![(0, ()); len]
+		.iter()
+		.map(|_| {
+			id += 1;
+			(id, ())
+		})
+		.collect::<Vec<_>>()
+}
+
+thread_local! {
+	// Switch to control the mock
+	pub static ONLINE: RefCell<bool>  = RefCell::new(true);
+}
+
+struct MockIsOnline;
+impl IsOnline for MockIsOnline {
+	type ValidatorId = u64;
+
+	fn is_online(_validator_id: &Self::ValidatorId) -> bool {
+		ONLINE.with(|cell| cell.borrow().clone())
+	}
+}
+
+#[test]
+fn test_generate_id() {
+	assert!(get_random_id_by_seed_in_range(vec![1, 6, 7, 4, 6, 7, 8], 5) < 5);
+	assert!(get_random_id_by_seed_in_range(vec![0, 0, 0], 5) < 5);
+	assert!(get_random_id_by_seed_in_range(vec![180, 200, 240], 10) < 10);
+}
+
+#[test]
+fn test_select_signer() {
+	// Expect Some validator
+	assert_eq!(
+		select_signer::<u64, MockIsOnline>(
+			vec![(4, ()), (6, ()), (7, ()), (9, ())],
+			vec![2, 5, 7, 3]
+		),
+		Some(6)
+	);
+	// Expect a validator in a set of 150 validators
+	assert_eq!(
+		select_signer::<u64, MockIsOnline>(validator_set(150), String::from("seed").into_bytes()),
+		Some(75)
+	);
+	// Expect an comparable big change in the value
+	// distribution for an small input seed change
+	assert_eq!(
+		select_signer::<u64, MockIsOnline>(validator_set(150), String::from("seeed").into_bytes()),
+		Some(147)
+	);
+	// Expect an reasonable SignerId for an bigger input seed
+	assert_eq!(
+		select_signer::<u64, MockIsOnline>(
+			validator_set(150),
+			String::from("west1_north_south_east:_berlin_zonk").into_bytes(),
+		),
+		Some(48)
+	);
+	// Switch the mock to simulate an situation where all
+	// validators are offline
+	ONLINE.with(|cell| *cell.borrow_mut() = false);
+	// Expect the select_signer function to return None
+	// if there is currently no online validator
+	assert_eq!(
+		select_signer::<u64, MockIsOnline>(
+			vec![(14, ()), (3, ()), (2, ()), (6, ())],
+			vec![2, 5, 9, 3]
+		),
+		None
+	);
+}
