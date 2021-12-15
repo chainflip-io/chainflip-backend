610703e86e9d4e887103f9ebd194e26975679186
dlennon <daniel@chainflip.io>
2021-12-15 16:00:56 +0100

    Add correct stakeman/keyman addresses

--- state-chain/node/src/chain_spec/network_env.rs
+++ state-chain/node/src/chain_spec/network_env.rs
@@ -3,8 +3,8 @@ use super::*;
 /// Environment variables for SOUNDCHECK public testnet.
 ///
 pub const SOUNDCHECK: StateChainEnvironment = StateChainEnvironment {
-	stake_manager_address: hex_literal::hex!("9Dfaa29bEc7d22ee01D533Ebe8faA2be5799C77F"),
-	key_manager_address: hex_literal::hex!("36fB9E46D6cBC14600D9089FD7Ce95bCf664179f"),
+	stake_manager_address: hex_literal::hex!("42fDb8254192AcE0C01B6f82212C37265a964d06"),
+	key_manager_address: hex_literal::hex!("076cf86E7156e50339fB5D34963676f9aBfB99A9"),
 	ethereum_chain_id: 4, // RINKEBY
 	eth_init_agg_key: hex_literal::hex!("02e61afd677cdfbec838c6f309deff0b2c6056f8a27f2c783b68bba6b30f667be6"),
 	genesis_stake_amount: 10_000 * 10u128.pow(18),
