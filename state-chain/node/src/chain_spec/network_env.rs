1bd59a7bb9092ea08a6606224c563b8f13e0b431
dlennon <daniel@chainflip.io>
2021-12-15 16:34:55 +0100

    Add Soundcheck aggkey and stake amount.

--- state-chain/node/src/chain_spec/network_env.rs
+++ state-chain/node/src/chain_spec/network_env.rs
@@ -6,8 +6,8 @@ pub const SOUNDCHECK: StateChainEnvironment = StateChainEnvironment {
 	key_manager_address: hex_literal::hex!("076cf86E7156e50339fB5D34963676f9aBfB99A9"),
 	ethereum_chain_id: 4, // RINKEBY
 	eth_init_agg_key: hex_literal::hex!(
-		"02e61afd677cdfbec838c6f309deff0b2c6056f8a27f2c783b68bba6b30f667be6"
+		"02555a2fcda57ae29bff6fd54f87e78279c76830a28511c3eccef998e96521f4b6"
 	),
-	genesis_stake_amount: 10_000 * 10u128.pow(18),
+	genesis_stake_amount: 15_000 * 10u128.pow(18),
 	ethereum_deployment_block: 9818850,
 };
