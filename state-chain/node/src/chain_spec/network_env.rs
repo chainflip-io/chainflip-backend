313faa997e1c9c3ba1ec86670ad2e7ff35faf424
dlennon <daniel@chainflip.io>
2021-12-15 17:09:01 +0100

    Update soundcheck contracts again.

--- state-chain/node/src/chain_spec/network_env.rs
+++ state-chain/node/src/chain_spec/network_env.rs
@@ -2,12 +2,12 @@ use super::*;
 
 /// Environment variables for SOUNDCHECK public testnet.
 pub const SOUNDCHECK: StateChainEnvironment = StateChainEnvironment {
-	stake_manager_address: hex_literal::hex!("6f1031c27017669C75dd8516A167Ec157f692407"),
-	key_manager_address: hex_literal::hex!("f7e1F09B84983fcc8479a908546f7Df1c9282b8B"),
+	stake_manager_address: hex_literal::hex!("3A96a2D552356E17F97e98FF55f69fDFb3545892"),
+	key_manager_address: hex_literal::hex!("70d15CD89a551Bcf90fFC72bc006E633c2e4F828"),
 	ethereum_chain_id: 4, // RINKEBY
 	eth_init_agg_key: hex_literal::hex!(
 		"02555a2fcda57ae29bff6fd54f87e78279c76830a28511c3eccef998e96521f4b6"
 	),
 	genesis_stake_amount: 50_000 * 10u128.pow(18),
-	ethereum_deployment_block: 9819279,
+	ethereum_deployment_block: 9819300,
 };
