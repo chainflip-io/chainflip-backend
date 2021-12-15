1e6f9bcdcf9532a42ac61643dc9f864c3a4656c1
dlennon <daniel@chainflip.io>
2021-12-15 18:10:18 +0100

    Tests missing.

--- state-chain/runtime/src/chainflip/signer_nomination.rs
+++ state-chain/runtime/src/chainflip/signer_nomination.rs
@@ -3,6 +3,7 @@ use cf_traits::{Chainflip, IsOnline};
 use frame_support::{Hashable, storage::IterableStorageMap};
 use nanorand::{Rng, WyRand};
 use sp_std::vec::Vec;
+use codec::{Encode, Decode};
 
 // /// Returns a scaled index based on an input seed
 // fn get_random_index(seed: Vec<u8>, max: usize) -> usize {
@@ -58,7 +59,7 @@ fn select_one<T>(seed: u64, things: Vec<T>) -> Option<T> {
 
 /// Takes something `Hashable` and hashes it to generate a `u64` seed value.
 fn seed_from_hashable<H: Hashable>(value: H) -> u64 {
-	let bytes = [0u8; 8];
+	let mut bytes = [0u8; 8];
 	bytes.copy_from_slice(&value.twox_128()[0..8]);
 	u64::from_be_bytes(bytes)
 }
@@ -66,9 +67,9 @@ fn seed_from_hashable<H: Hashable>(value: H) -> u64 {
 /// Nominates pseudo-random signers based on the provided seed.
 pub struct RandomSignerNomination;
 
-fn get_online_validators<T: Chainflip + pallet_session::Config>(
-) -> Vec<<T as Chainflip>::ValidatorId> {
-	pallet_cf_validator::ValidatorLookup::<T>::iter_keys()
+fn get_online_validators(
+) -> Vec<<Runtime as Chainflip>::ValidatorId> {
+	pallet_cf_validator::ValidatorLookup::<Runtime>::iter_keys()
 		.filter(|id| <Online as cf_traits::IsOnline>::is_online(&id))
 		.collect()
 }
@@ -77,14 +78,14 @@ impl cf_traits::SignerNomination for RandomSignerNomination {
 	type SignerId = <Runtime as Chainflip>::ValidatorId;
 
 	fn nomination_with_seed<H: Hashable>(seed: H) -> Option<Self::SignerId> {
-		let online_validators = get_online_validators::<Runtime>();
+		let online_validators = get_online_validators();
 		select_one(seed_from_hashable(seed), online_validators)
 	}
 
 	fn threshold_nomination_with_seed<H: Hashable>(seed: H) -> Option<Vec<Self::SignerId>> {
 		// TODO: get this from `EpochInfo` instead.
 		let threshold = pallet_cf_witnesser::ConsensusThreshold::<Runtime>::get();
-		let mut online_validators = get_online_validators::<Runtime>();
+		let mut online_validators = get_online_validators();
 		select_random_subset(
 			seed_from_hashable(seed),
 			threshold as usize,
@@ -104,82 +105,73 @@ mod tests {
 	// use std::ops::Range;
 
 	/// Generates a set of validators with the SignerId = index + 1
-	fn validator_set(len: usize) -> Vec<(u64, ())> {
-		(0..len).map(|id| (id, ())).collect::<Vec<_>>()
+	fn validator_set(len: usize) -> Vec<u64> {
+		(0..len as u64).collect::<Vec<_>>()
 	}
 
-	thread_local! {
-		// Switch to control the mock
-		pub static ONLINE: RefCell<bool>  = RefCell::new(true);
-	}
+	// thread_local! {
+	// 	// Switch to control the mock
+	// 	pub static ONLINE: RefCell<bool>  = RefCell::new(true);
+	// }
 
-	struct MockIsOnline;
-	impl IsOnline for MockIsOnline {
-		type ValidatorId = u64;
+	// struct MockIsOnline;
+	// impl IsOnline for MockIsOnline {
+	// 	type ValidatorId = u64;
 
-		fn is_online(_validator_id: &Self::ValidatorId) -> bool {
-			ONLINE.with(|cell| cell.borrow().clone())
-		}
-	}
+	// 	fn is_online(_validator_id: &Self::ValidatorId) -> bool {
+	// 		ONLINE.with(|cell| cell.borrow().clone())
+	// 	}
+	// }
 
-	#[test]
-	fn test_get_random_index() {
-		assert!(get_random_index(vec![1, 6, 7, 4, 6, 7, 8], 5) < 5);
-		assert!(get_random_index(vec![0, 0, 0], 5) < 5);
-		assert!(get_random_index(vec![180, 200, 240], 10) < 10);
-	}
+	// #[test]
+	// fn test_get_random_index() {
+	// 	assert!(get_random_index(vec![1, 6, 7, 4, 6, 7, 8], 5) < 5);
+	// 	assert!(get_random_index(vec![0, 0, 0], 5) < 5);
+	// 	assert!(get_random_index(vec![180, 200, 240], 10) < 10);
+	// }
 
 	#[test]
 	fn test_select_signer() {
 		// Expect Some validator
-		assert_eq!(
-			select_signer::<u64, MockIsOnline>(
+		assert!(
+			select_one(
+				seed_from_hashable(vec![2, 5, 7, 3]),
 				vec![(4, ()), (6, ()), (7, ()), (9, ())],
-				vec![2, 5, 7, 3]
-			),
-			Some(9)
+			).is_some()
 		);
 		// Expect a validator in a set of 150 validators
-		assert_eq!(
-			select_signer::<u64, MockIsOnline>(
+		assert!(
+			select_one(
+				seed_from_hashable(String::from("seed").into_bytes()),
 				validator_set(150),
-				String::from("seed").into_bytes()
-			),
-			Some(45)
+			).is_some()
 		);
 		// Expect an comparable big change in the value
 		// distribution for an small input seed change
-		assert_eq!(
-			select_signer::<u64, MockIsOnline>(
+		assert!(
+			select_one(
+				seed_from_hashable(("seedy", "seed")),
 				validator_set(150),
-				String::from("seeed").into_bytes()
-			),
-			Some(53)
+			).is_some()
 		);
 		// Expect an reasonable SignerId for an bigger input seed
-		assert_eq!(
-			select_signer::<u64, MockIsOnline>(
+		assert!(
+			select_one(
+				seed_from_hashable(("west1_north_south_east:_berlin_zonk", 1, 2, 3, 4u128))
 				validator_set(150),
-				String::from("west1_north_south_east:_berlin_zonk").into_bytes(),
-			),
-			Some(97)
+			).is_some()
 		);
-		// Switch the mock to simulate an situation where all
-		// validators are offline
-		ONLINE.with(|cell| *cell.borrow_mut() = false);
 		// Expect the select_signer function to return None
 		// if there is currently no online validator
-		assert_eq!(
-			select_signer::<u64, MockIsOnline>(
-				vec![(14, ()), (3, ()), (2, ()), (6, ())],
-				vec![2, 5, 9, 3]
-			),
-			None
+		assert!(
+			select_one(
+				seed_from_hashable("seed")
+				vec![],
+			).is_none()
 		);
 	}
 
-	fn test_subset_with<T>(threshold: u64, set: Vec<T>) {
-		const SEED: u64 = 0;
+	fn test_subset_with<T>(seed: u64, threshold: u64, set: Vec<T>) {
 		let source = BTreeSet::from_iter(set.clone());
 		let result = BTreeSet::from_iter(select_random_subset(SEED, threshold, set));
 		assert!(result.len() == threshold);
@@ -188,9 +180,10 @@ mod tests {
 
 	#[test]
 	fn test_random_subset_selection() {
-		test_subset_with(0, 2, (0..5).collect());
-		test_subset_with(0, 3, (0..5).collect());
-		test_subset_with(0, 4, (0..5).collect());
-		test_subset_with(0, 5, (0..5).collect());
+		const SEED: u64 = 0;
+		test_subset_with(SEED, 2, (0..5).collect());
+		test_subset_with(SEED, 3, (0..5).collect());
+		test_subset_with(SEED, 4, (0..5).collect());
+		test_subset_with(SEED, 5, (0..5).collect());
 	}
 }
