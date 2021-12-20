dafb1e17c79d3970d3fb5e9734789e7ed19da77a
Andy Bell <andyjsbell@gmail.com>
2021-12-20 10:20:31 +0100

    Bump runtime version to 102

--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -101,7 +101,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("chainflip-node"),
 	impl_name: create_runtime_str!("chainflip-node"),
 	authoring_version: 1,
-	spec_version: 101,
+	spec_version: 102,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
