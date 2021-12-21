8b912ae0be57d57a1c91fe9b503e6e502dbaee62
kylezs <zsembery.kyle@gmail.com>
2021-12-21 10:41:04 +0100

    compiling with some generics for tests

--- engine/src/eth/safe_stream.rs
+++ engine/src/eth/safe_stream.rs
@@ -1,12 +1,6 @@
-use std::{
-    collections::{HashMap, VecDeque},
-    ops::Range,
-    task::Poll,
-};
+use std::collections::HashMap;
 
-use futures::{future::join_all, stream, FutureExt, Stream};
-use futures_lite::future::block_on;
-use slog::o;
+use futures::{stream, Stream};
 use sp_core::H160;
 use web3::{
     api::SubscriptionStream,
@@ -17,49 +11,34 @@ use web3::{
 
 use ethbloom::{Bloom, Input};
 
-use hex_literal::hex;
-
-/// Contains a blocks worth of logs for a particular contract
-type LogBlock = Vec<Log>;
-
 use futures::StreamExt;
 
 use crate::{eth, logging::COMPONENT_KEY, settings};
 
-pub struct StreamAndBlocks {
-    next_block_to_pop: U64,
-    stream: SubscriptionStream<WebSocket, BlockHeader>,
-    head_eth_stream: U64,
-    web3: Web3<WebSocket>,
-    interesting_past_blocks: HashMap<U64, ()>,
-    logs: Vec<Log>,
-}
-
-pub async fn create_safe_eth_log_stream(
-    // init_data: StreamAndBlocks,
-    settings: settings::Eth,
+pub fn inner_safe_eth_log_stream<BlockHeaderStream>(
+    header_stream: BlockHeaderStream,
     web3: Web3<WebSocket>,
     contract_address: H160,
-    logger: &slog::Logger,
-) -> impl Stream<Item = Log> {
-    const BLOCKS_AWAITED: u64 = 3;
-
-    let eth_head_stream = web3
-        .clone()
-        .eth_subscribe()
-        .subscribe_new_heads()
-        .await
-        .expect("should create head stream");
-
+    safety_margin: u64,
+) -> impl Stream<Item = Log>
+where
+    BlockHeaderStream: Stream<Item = Result<BlockHeader, web3::Error>>,
+{
+    pub struct StreamAndBlocks<BlockHeaderStream>
+    where
+        BlockHeaderStream: Stream<Item = Result<BlockHeader, web3::Error>>,
+    {
+        stream: BlockHeaderStream,
+        head_eth_stream: U64,
+        web3: Web3<WebSocket>,
+        interesting_past_blocks: HashMap<U64, ()>,
+    }
     let init_data = StreamAndBlocks {
-        next_block_to_pop: U64::from(0),
-        stream: eth_head_stream,
+        stream: Box::pin(header_stream),
         head_eth_stream: U64::from(0),
         interesting_past_blocks: Default::default(),
         web3,
-        logs: Vec::default(),
     };
-
     let stream = Box::pin(
         stream::unfold(init_data, move |mut state| async move {
             if let Some(header) = state.stream.next().await {
@@ -80,7 +59,7 @@ pub async fn create_safe_eth_log_stream(
 
             let safe_block = state
                 .head_eth_stream
-                .saturating_sub(U64::from(BLOCKS_AWAITED));
+                .saturating_sub(U64::from(safety_margin));
             if let Some(_) = state.interesting_past_blocks.get(&safe_block) {
                 println!("Getting a safe block");
                 let logs = state
@@ -109,29 +88,76 @@ pub async fn create_safe_eth_log_stream(
     stream
 }
 
+async fn create_safe_eth_log_stream(
+    // init_data: StreamAndBlocks,
+    web3: Web3<WebSocket>,
+    contract_address: H160,
+    logger: &slog::Logger,
+) -> impl Stream<Item = Log> {
+    const BLOCKS_AWAITED: u64 = 3;
+
+    let eth_head_stream = web3
+        .clone()
+        .eth_subscribe()
+        .subscribe_new_heads()
+        .await
+        .expect("should create head stream");
+
+    inner_safe_eth_log_stream(eth_head_stream, web3, contract_address, BLOCKS_AWAITED)
+}
+
 #[cfg(test)]
 mod tests {
-    use std::{collections::VecDeque, sync::Arc, task::Context};
-
-    use futures::stream;
-    use generic_array::typenum::U6;
 
     use crate::{eth, logging::utils::new_discard_logger, settings::Settings};
 
+    use web3::types::H2048;
+
+    // dev dep?
+    use hex_literal::hex;
+    use sp_core::H256;
+
     use super::*;
+
+    fn block_header(hash: u8, block_number: u64, bloom: H2048) {
+        let block_header = BlockHeader {
+            // fields that matter
+            hash: Some(H256::from([hash; 32])),
+            number: Some(U64::from(block_number)),
+            logs_bloom: H2048::default(),
+
+            // defaults
+            parent_hash: H256::default(),
+            uncles_hash: H256::default(),
+            author: H160::default(),
+            state_root: H256::default(),
+            transactions_root: H256::default(),
+            receipts_root: H256::default(),
+            gas_used: sp_core::U256::default(),
+            gas_limit: sp_core::U256::default(),
+            base_fee_per_gas: Default::default(),
+            extra_data: Default::default(),
+            timestamp: Default::default(),
+            difficulty: sp_core::U256::default(),
+            mix_hash: Default::default(),
+            nonce: Default::default(),
+        };
+    }
 
     #[tokio::test]
     async fn gets_block_heads() {
         let settings = Settings::from_file("config/Local.toml").unwrap();
         let logger = new_discard_logger();
+
         let web3 = eth::new_synced_web3_client(&settings.eth, &logger)
             .await
             .expect("Failed to create Web3 WebSocket");
 
         let contract_address = H160::from(hex!("01BE23585060835E02B77ef475b0Cc51aA1e0709"));
 
-        let mut stream =
-            create_safe_eth_log_stream(settings.eth, web3, contract_address, &logger).await;
+        let header_stream = stream::iter::<Vec<Result<BlockHeader, web3::Error>>>(vec![]);
+
+        let mut stream = inner_safe_eth_log_stream(header_stream, web3, contract_address, 2);
 
         while let Some(item) = stream.next().await {
             println!("Item in block: {:?}", item.block_number);
