54a679e5afb27b9a1f360f2a6336049c5a84d9e5
Jan Börner <jan@chainflip.io>
2021-12-21 12:14:05 +0100

    * refactored vaults benchmark * integrated benchmark into pallet * generated weights

--- state-chain/pallets/cf-vaults/src/benchmarking.rs
+++ state-chain/pallets/cf-vaults/src/benchmarking.rs
@@ -6,11 +6,19 @@ use super::*;
 use frame_benchmarking::{account, benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_support::dispatch::UnfilteredDispatchable;
 use frame_system::RawOrigin;
-// use sp_std::vec;
 
 #[allow(unused)]
 use crate::Pallet;
 
+// Note: Currently we only have one chain (ETH) - as soon we've
+// another chain we've to take this in account in our weight calculation benchmark.
+
+const CEREMONY_ID: u64 = 1;
+const CHAIN_ID: ChainId = ChainId::Ethereum;
+const NEW_PUBLIC_KEY: [u8; 33] = [0x02; 33];
+const TX_HASH: [u8; 32] = [0xab; 32];
+
+/// Generate a validator set
 fn generate_validator_set<T: Config>(
 	amount: u32,
 	caller: T::ValidatorId,
@@ -26,145 +34,85 @@ fn generate_validator_set<T: Config>(
 
 benchmarks! {
 	on_initialize_failure {
-		let chain_id = ChainId::Ethereum;
 		let current_block: T::BlockNumber = (0 as u32).into();
 			KeygenResolutionPending::<T>::append((
-				chain_id,
+				CHAIN_ID,
 				current_block
 		));
 		let caller: T::AccountId = whitelisted_caller();
-		let ceremony_id = 1;
-		let new_public_key: [u8; 33] = [0x02; 33];
-
-		let mut candidates: BTreeSet<T::ValidatorId> = BTreeSet::new();
-		let mut blamed: BTreeSet<T::ValidatorId> = BTreeSet::new();
-
-		for i in 0..150 {
-			let validator_id = account("doogle", i, 0);
-			blamed.insert(validator_id);
-		}
-
-		for i in 0..150 {
-			let validator_id = account("doogle", i, 0);
-			candidates.insert(validator_id);
-		}
-
-		candidates.insert(caller.clone().into());
-
+		let candidates: BTreeSet<T::ValidatorId> = generate_validator_set::<T>(150, caller.clone().into());
+		let blamed: BTreeSet<T::ValidatorId> = generate_validator_set::<T>(150, caller.clone().into());
 		let mut keygen_response_status = KeygenResponseStatus::<T>::new(candidates);
 
 		for i in 0..120 {
 			let validator_id = account("doogle", i, 0);
-			keygen_response_status.add_failure_vote(&validator_id, blamed.clone());
+			let _ = keygen_response_status.add_failure_vote(&validator_id, blamed.clone());
 		}
 
 		PendingVaultRotations::<T>::insert(
-			chain_id,
-			VaultRotationStatus::<T>::AwaitingKeygen {  keygen_ceremony_id: ceremony_id, response_status: keygen_response_status},
+			CHAIN_ID,
+			VaultRotationStatus::<T>::AwaitingKeygen {  keygen_ceremony_id: CEREMONY_ID, response_status: keygen_response_status},
 		);
 	} : {
 		Pallet::<T>::on_initialize((5 as u32).into());
 	}
 	on_initialize_success {
-		let chain_id = ChainId::Ethereum;
 		let current_block: T::BlockNumber = (0 as u32).into();
 			KeygenResolutionPending::<T>::append((
-				chain_id,
+				CHAIN_ID,
 				current_block
 		));
 		let caller: T::AccountId = whitelisted_caller();
-		let ceremony_id = 1;
-		let new_public_key: [u8; 33] = [0x02; 33];
-
-		let mut candidates: BTreeSet<T::ValidatorId> = BTreeSet::new();
-
-		for i in 0..150 {
-			let validator_id = account("doogle", i, 0);
-			candidates.insert(validator_id);
-		}
-
-		candidates.insert(caller.clone().into());
-
+		let candidates: BTreeSet<T::ValidatorId> = generate_validator_set::<T>(150, caller.clone().into());
 		let mut keygen_response_status = KeygenResponseStatus::<T>::new(candidates);
 
 		for i in 0..120 {
 			let validator_id = account("doogle", i, 0);
-			keygen_response_status.add_success_vote(&validator_id, new_public_key.to_vec());
+			let _ = keygen_response_status.add_success_vote(&validator_id, NEW_PUBLIC_KEY.to_vec());
 		}
 
 		PendingVaultRotations::<T>::insert(
-			chain_id,
-			VaultRotationStatus::<T>::AwaitingKeygen {  keygen_ceremony_id: ceremony_id, response_status: keygen_response_status},
+			CHAIN_ID,
+			VaultRotationStatus::<T>::AwaitingKeygen {  keygen_ceremony_id: CEREMONY_ID, response_status: keygen_response_status},
 		);
 	} : {
 		Pallet::<T>::on_initialize((5 as u32).into());
 	}
 	on_initialize_none {
-		let chain_id = ChainId::Ethereum;
 		let current_block: T::BlockNumber = (0 as u32).into();
 		KeygenResolutionPending::<T>::append((
-			chain_id,
+			CHAIN_ID,
 			current_block
 		));
 		let caller: T::AccountId = whitelisted_caller();
-		let ceremony_id = 1;
-		let new_public_key: [u8; 33] = [0x02; 33];
-
-		let mut candidates: BTreeSet<T::ValidatorId> = BTreeSet::new();
-
-		for i in 0..150 {
-			let validator_id = account("doogle", i, 0);
-			candidates.insert(validator_id);
-		}
-
-		candidates.insert(caller.clone().into());
-
+		let candidates: BTreeSet<T::ValidatorId> = generate_validator_set::<T>(150, caller.clone().into());
 		let keygen_response_status = KeygenResponseStatus::<T>::new(candidates);
 
 		PendingVaultRotations::<T>::insert(
-			chain_id,
-			VaultRotationStatus::<T>::AwaitingKeygen {  keygen_ceremony_id: ceremony_id, response_status: keygen_response_status},
+			CHAIN_ID,
+			VaultRotationStatus::<T>::AwaitingKeygen {  keygen_ceremony_id: CEREMONY_ID, response_status: keygen_response_status},
 		);
 	} : {
 		Pallet::<T>::on_initialize((11 as u32).into());
 	}
 	report_keygen_outcome {
 		let caller: T::AccountId = whitelisted_caller();
-		let ceremony_id = 1;
-		let chain_id = ChainId::Ethereum;
-		let new_public_key: [u8; 33] = [0x02; 33];
-
-		let mut candidates: BTreeSet<T::ValidatorId> = generate_validator_set::<T>(150, caller.clone().into());
-
-		// for i in 0..150 {
-		// 	let validator_id = account("doogle", i, 0);
-		// 	candidates.insert(validator_id);
-		// }
-
-		// candidates.insert(caller.clone().into());
-
+		let candidates: BTreeSet<T::ValidatorId> = generate_validator_set::<T>(150, caller.clone().into());
 		let keygen_response_status = KeygenResponseStatus::<T>::new(candidates);
+
 		PendingVaultRotations::<T>::insert(
-			chain_id,
-			VaultRotationStatus::<T>::AwaitingKeygen {  keygen_ceremony_id: ceremony_id, response_status: keygen_response_status},
+			CHAIN_ID,
+			VaultRotationStatus::<T>::AwaitingKeygen {  keygen_ceremony_id: CEREMONY_ID, response_status: keygen_response_status},
 		);
 		let reported_outcome = KeygenOutcome::Success(Default::default());
-	} : _(RawOrigin::Signed(caller), ceremony_id, chain_id, reported_outcome)
-	verify {
-		let rotation = PendingVaultRotations::<T>::get(chain_id);
-		// TODO: Figure out something to proof that this benchmark reaches the most expensive part
-	}
+	} : _(RawOrigin::Signed(caller), CEREMONY_ID, CHAIN_ID, reported_outcome)
 	vault_key_rotated {
-		let chain_id = ChainId::Ethereum;
 		let caller: T::AccountId = whitelisted_caller();
-		let new_public_key: [u8; 33] = [0x02; 33];
-		let tx_hash: [u8; 32] = [0xab; 32];
-
 		PendingVaultRotations::<T>::insert(
-			chain_id,
-			VaultRotationStatus::<T>::AwaitingRotation {  new_public_key: new_public_key.to_vec() },
+			CHAIN_ID,
+			VaultRotationStatus::<T>::AwaitingRotation {  new_public_key: NEW_PUBLIC_KEY.to_vec() },
 		);
-		let call = Call::<T>::vault_key_rotated(chain_id, new_public_key.to_vec(), 5 as u64, tx_hash.to_vec());
+		let call = Call::<T>::vault_key_rotated(CHAIN_ID, NEW_PUBLIC_KEY.to_vec(), 5 as u64, TX_HASH.to_vec());
 		let origin = T::EnsureWitnessed::successful_origin();
 	} : { call.dispatch_bypass_filter(origin)? }
 }
