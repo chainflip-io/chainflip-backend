530cfc7579ec6bd575327ebf276323e79bc73cae
Andy Bell <andyjsbell@gmail.com>
2021-12-29 12:50:12 +0100

    fix node build

--- state-chain/node/src/command.rs
+++ state-chain/node/src/command.rs
@@ -122,7 +122,7 @@ pub fn run() -> sc_cli::Result<()> {
 					sc_service::TaskManager::new(config.task_executor.clone(), registry)
 						.map_err(|e| sc_cli::Error::Service(sc_service::Error::Prometheus(e)))?;
 
-				Ok((cmd.run::<Block, Executor>(config), task_manager))
+				Ok((cmd.run::<Block, service::ExecutorDispatch>(config), task_manager))
 			})
 		},
 		#[cfg(not(feature = "try-runtime"))]
