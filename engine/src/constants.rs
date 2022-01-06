d3a8a935f7dc36543a477f27bb0177360a3e141a
kylezs <zsembery.kyle@gmail.com>
2022-01-06 11:59:19 +0100

    better comment

--- engine/src/constants.rs
+++ engine/src/constants.rs
@@ -5,7 +5,8 @@ pub const ETH_BLOCK_SAFETY_MARGIN: u64 = 4;
 
 // ======= Keygen and signing =======
 //
-/// Defines how long a signing ceremony remains pending
+/// Defines how long a signing ceremony remains pending. i.e. how long it waits for the key that is supposed to sign this message
+/// to be generated. (Since we can receive requests to sign for the next key, if other nodes are ahead of us)
 pub const PENDING_SIGN_DURATION: Duration = Duration::from_secs(500); // TODO Look at this value
 
 /// Maximum duration a ceremony stage can last
