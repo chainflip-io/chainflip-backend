a74e7ea9107c046a8a86f2d05f27d952cfe6e3e3
Alastair Holmes <holmes.alastair@outlook.com>
2022-01-06 09:24:59 +0100

    assert share_count isnt zero

--- utilities/src/lib.rs
+++ utilities/src/lib.rs
@@ -9,11 +9,8 @@
 /// For the *success* threshold, use [success_threshold_from_share_count].
 
 pub fn threshold_from_share_count(share_count: u32) -> u32 {
-    if share_count == 0 {
-        0
-    } else {
-        (share_count.checked_mul(2).unwrap() - 1) / 3
-    }
+    assert_ne!(0, share_count);
+    (share_count.checked_mul(2).unwrap() - 1) / 3
 }
 
 /// Returns the number of parties required for a threshold signature
