79cd95cad8463b0ef1e1aab7242de3950bb1e692
kylezs <zsembery.kyle@gmail.com>
2022-01-11 10:53:04 +0100

    test for backup but not outgoing

--- engine/src/state_chain/tests/sc_observer_tests.rs
+++ engine/src/state_chain/tests/sc_observer_tests.rs
@@ -275,6 +275,103 @@ async fn sends_initial_extrinsics_and_starts_witnessing_when_outgoing_on_startup
 }
 
 #[tokio::test]
+async fn sends_initial_extrinsics_when_backup_but_not_outgoing_on_startup() {
+    // Current epoch is set to 3. Our last_active_epoch is set to 2.
+    // So we should be deemed outgoing, and submit the block height windows as expected to the nodes
+    // even though we are passive
+    let logger = new_test_logger();
+
+    let eth_rpc_mock = MockEthRpcApi::new();
+
+    let eth_broadcaster = EthBroadcaster::new_test(eth_rpc_mock, &logger);
+
+    let (multisig_instruction_sender, _multisig_instruction_receiver) =
+        tokio::sync::mpsc::unbounded_channel::<MultisigInstruction>();
+    let (account_peer_mapping_change_sender, _account_peer_mapping_change_receiver) =
+        tokio::sync::mpsc::unbounded_channel();
+    let (_multisig_outcome_sender, multisig_outcome_receiver) =
+        tokio::sync::mpsc::unbounded_channel::<MultisigOutcome>();
+
+    let (sm_window_sender, mut sm_window_receiver) =
+        tokio::sync::mpsc::unbounded_channel::<BlockHeightWindow>();
+    let (km_window_sender, mut km_window_receiver) =
+        tokio::sync::mpsc::unbounded_channel::<BlockHeightWindow>();
+
+    // Submits only one extrinsic when no events, the heartbeat
+    let mut mock_state_chain_rpc_client = MockStateChainRpcApi::new();
+    mock_state_chain_rpc_client
+        .expect_submit_extrinsic_rpc()
+        .times(1)
+        .returning(move |_| Ok(H256::default()));
+
+    let latest_block_hash = H256::default();
+
+    // get account info
+    let our_account_id = AccountId32::new([0u8; 32]);
+
+    let account_info_storage_key = StorageKey(
+        frame_system::Account::<state_chain_runtime::Runtime>::hashed_key_for(&our_account_id),
+    );
+
+    mock_state_chain_rpc_client
+        .expect_storage_events_at()
+        .with(eq(Some(latest_block_hash)), eq(account_info_storage_key))
+        .times(1)
+        .returning(move |_, _| {
+            Ok(vec![storage_change_set_from(
+                AccountInfo {
+                    nonce: 0,
+                    consumers: 0,
+                    providers: 0,
+                    sufficients: 0,
+                    data: ChainflipAccountData {
+                        // NB: We are Passive and last active is *two* less than current epoch (3)
+                        state: ChainflipAccountState::Backup,
+                        last_active_epoch: Some(1),
+                    },
+                },
+                latest_block_hash,
+            )])
+        });
+
+    // get the current epoch, which is 3
+    let epoch_key = StorageKey(
+        pallet_cf_validator::CurrentEpoch::<state_chain_runtime::Runtime>::hashed_key().into(),
+    );
+    mock_state_chain_rpc_client
+        .expect_storage_events_at()
+        .with(eq(Some(latest_block_hash)), eq(epoch_key))
+        .times(1)
+        .returning(move |_, _| Ok(vec![storage_change_set_from(3, latest_block_hash)]));
+
+    let state_chain_client = Arc::new(StateChainClient::create_test_sc_client(
+        mock_state_chain_rpc_client,
+        our_account_id,
+    ));
+
+    // No blocks in the stream
+    let sc_block_stream = tokio_stream::iter(vec![]);
+
+    start(
+        state_chain_client,
+        sc_block_stream,
+        eth_broadcaster,
+        multisig_instruction_sender,
+        account_peer_mapping_change_sender,
+        multisig_outcome_receiver,
+        sm_window_sender,
+        km_window_sender,
+        latest_block_hash,
+        &logger,
+    )
+    .await;
+
+    // ensure we did NOT kick off the witness processes - as we are *only* backup, not outgoing
+    assert!(km_window_receiver.recv().await.is_none());
+    assert!(sm_window_receiver.recv().await.is_none());
+}
+
+#[tokio::test]
 #[ignore = "runs forever, useful for testing without having to start the whole CFE"]
 async fn run_the_sc_observer() {
     let settings = new_test_settings().unwrap();
