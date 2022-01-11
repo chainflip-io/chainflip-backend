dbf6991c3b01896787a41fb1f16448b657ff73f0
kylezs <zsembery.kyle@gmail.com>
2022-01-11 16:22:47 +0100

    remove debug printlns from sco

--- engine/src/state_chain/sc_observer.rs
+++ engine/src/state_chain/sc_observer.rs
@@ -61,13 +61,11 @@ pub async fn start<BlockStream, RpcClient, EthRpc>(
         state_chain_client: Arc<StateChainClient<RpcClient>>,
         block_hash: H256,
     ) -> (ChainflipAccountData, bool) {
-        println!("Getting account data for block hash: {:?}", block_hash);
         let new_account_data = state_chain_client
             .get_account_data(block_hash)
             .await
             .expect("Could not get account data");
 
-        println!("Getting epoch");
         let current_epoch = state_chain_client
             .epoch_at_block(block_hash)
             .await
@@ -89,10 +87,6 @@ pub async fn start<BlockStream, RpcClient, EthRpc>(
         sm_window_sender: &UnboundedSender<BlockHeightWindow>,
         km_window_sender: &UnboundedSender<BlockHeightWindow>,
     ) -> anyhow::Result<()> {
-        println!(
-            "Getting vault, accountdata: {:?}, block_hash: {:?}",
-            account_data, block_hash
-        );
         let eth_vault = state_chain_client
             .get_vault(
                 block_hash,
@@ -140,7 +134,6 @@ pub async fn start<BlockStream, RpcClient, EthRpc>(
                 let mut received_new_epoch = false;
 
                 // Process this block's events
-                println!("Getting events");
                 match state_chain_client.get_events(current_block_hash).await {
                     Ok(events) => {
                         for (_phase, event, _topics) in events {
