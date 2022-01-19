e6f494132f1c03152c1f27e7b78a45f9e677ed76
Jan Börner <jan@chainflip.io>
2022-01-19 14:22:23 +0100

    * added run step for performance test * added performance script * added benchmarking folder

---
+++ state-chain/scripts/performance-test.sh
@@ -0,0 +1,2 @@
+./target/release/chainflip-node benchmark --extrinsic '*' --pallet pallet_cf_auction --output state-chain/benchmarking/auction.rs --execution=wasm --steps=1 --repeat=1 --template=state-chain/chainflip-weight-template.hbs
+./target/release/chainflip-node benchmark --extrinsic '*' --pallet pallet_cf_emissions --output state-chain/benchmarking/emissions.rs --execution=wasm --steps=1 --repeat=1 --template=state-chain/chainflip-weight-template.hbs
