1157964e8f7f4fbef1a72b4da7a4579d6d2a5152
Andy Bell <andyjsbell@gmail.com>
2022-01-24 11:41:15 +0100

    If we are in an auction during migration then reset the auction

--- state-chain/pallets/cf-auction/src/migrations.rs
+++ state-chain/pallets/cf-auction/src/migrations.rs
@@ -1,4 +1,3 @@
-use cf_traits::Auctioneer;
 use frame_support::traits::Get;
 
 use super::*;
@@ -10,12 +9,6 @@ pub(crate) mod v1 {
 	pub(crate) fn pre_migrate<T: Config, P: GetStorageVersion>() -> Result<(), &'static str> {
 		assert!(P::on_chain_storage_version() == releases::V0, "Storage version too high.");
 
-		// We don't want to run the migration during an auction
-		assert!(
-			Pallet::<T>::phase() == AuctionPhase::WaitingForBids,
-			"Migration should be run out of auction."
-		);
-
 		log::info!(
 			target: "runtime::cf_auction",
 			"migration: Auction storage version v1 PRE migration checks successful!"
@@ -26,16 +19,18 @@ pub(crate) mod v1 {
 
 	pub fn migrate<T: Config>() -> frame_support::weights::Weight {
 		// Current version is is genesis, upgrade to version 1
-		// With version 2 we have changed the type for CurrentPhase, there is no need to do anything
-		// here as our try-runtime checks will assert if we are in an auction.
-		// As we don't want to panic here we will log the case we have run the migration and that
-		// we are in an auction, we can't really do much more than that.
-		if Pallet::<T>::phase() != AuctionPhase::WaitingForBids {
-			log::error!(
+		// With version 2 we have changed the type for CurrentPhase. If we were in an auction, where
+		// `CurrentPhase != AuctionPhase::WaitingForBids`, then we state back to
+		// `AuctionPhase::WaitingForBids`
+		if CurrentPhase::<T>::get() != AuctionPhase::WaitingForBids {
+			CurrentPhase::<T>::set(AuctionPhase::WaitingForBids);
+			log::info!(
 				target: "runtime::cf_auction",
-				"migration: Migration failed, this was not to be run during an auction."
+				"migration: Auction migration setting phase to `WaitingForBids`"
 			);
+			return T::DbWeight::get().reads_writes(1, 1)
 		}
+
 		T::DbWeight::get().reads(1)
 	}
 
@@ -43,10 +38,10 @@ pub(crate) mod v1 {
 	pub(crate) fn post_migrate<T: Config, P: GetStorageVersion>() -> Result<(), &'static str> {
 		assert_eq!(P::on_chain_storage_version(), releases::V1);
 
-		// We should see no changes to the auction phase post migration
+		// We should see the auction state set to `WaitingForBids` in migration
 		assert!(
 			Pallet::<T>::phase() == AuctionPhase::WaitingForBids,
-			"The migration should not have updated auction state."
+			"The migration should have reset the auction phase"
 		);
 
 		log::info!(
