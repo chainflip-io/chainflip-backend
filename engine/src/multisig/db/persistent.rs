350209862009bc2f7a2b76bd8f1fd0e11091eed4
Jamie Ford <jamie@chainflip.io>
2022-01-25 17:24:49 +1100

    Cleanup

--- engine/src/multisig/db/persistent.rs
+++ engine/src/multisig/db/persistent.rs
@@ -23,7 +23,7 @@ const DATA_VERSION: u32 = 1;
 /// Key used to store the `DATA_VERSION` value in the `METADATA_COLUMN`
 pub const DATA_VERSION_KEY: &[u8; 12] = b"data_version";
 
-// Prefixes for the `DATA_COLUMN`
+/// Prefixes for the `DATA_COLUMN`
 const PREFIX_SIZE: usize = 4;
 const KEYGEN_DATA_PREFIX: &[u8; PREFIX_SIZE] = b"key_";
 
@@ -33,6 +33,175 @@ const DATA_COLUMN: &str = "data";
 // This column is just for data version info. No prefix is used.
 const METADATA_COLUMN: &str = "metadata";
 const COLUMN_FAMILIES: &[&str] = &[DATA_COLUMN, METADATA_COLUMN];
+
+/// Database for keys and persistent metadata
+pub struct PersistentKeyDB {
+    /// Rocksdb database instance
+    db: DB,
+    logger: slog::Logger,
+}
+impl PersistentKeyDB {
+    pub fn new(path: &Path, logger: &slog::Logger) -> Result<Self> {
+        let logger = logger.new(o!(COMPONENT_KEY => "PersistentKeyDB"));
+
+        // Build a list of column families with default descriptors
+        let mut cfs_check: HashSet<String> = HashSet::new();
+        let mut cfs: HashMap<String, ColumnFamilyDescriptor> = COLUMN_FAMILIES
+            .iter()
+            .map(|cf_name| {
+                (
+                    cf_name.to_string(),
+                    ColumnFamilyDescriptor::new((*cf_name).to_string(), Options::default()),
+                )
+            })
+            .collect();
+
+        let has_existing_db = path.exists();
+        if has_existing_db {
+            // Add the column families found in the existing db, they might be needed for migration.
+            cfs = cfs
+                .into_iter()
+                .chain(
+                    DB::list_cf(&Options::default(), path)
+                        .expect("Should get list of column families")
+                        .into_iter()
+                        .filter_map(|cf_name| {
+                            // Filter out the `default` column because we don't use it
+                            if cf_name == "default" {
+                                None
+                            } else {
+                                cfs_check.insert(cf_name.clone());
+                                Some((
+                                    cf_name.clone(),
+                                    ColumnFamilyDescriptor::new(cf_name, Options::default()),
+                                ))
+                            }
+                        }),
+                )
+                .collect();
+        }
+
+        // Override the default descriptor for the data column so it can use a prefix extractor
+        let mut cfopts_for_prefix = Options::default();
+        cfopts_for_prefix
+            .set_prefix_extractor(rocksdb::SliceTransform::create_fixed_prefix(PREFIX_SIZE));
+        cfs.insert(
+            DATA_COLUMN.to_string(),
+            ColumnFamilyDescriptor::new(DATA_COLUMN, cfopts_for_prefix),
+        );
+
+        let mut create_missing_db_and_cols_opts = Options::default();
+        create_missing_db_and_cols_opts.create_missing_column_families(true);
+        create_missing_db_and_cols_opts.create_if_missing(true);
+
+        let cf_descriptors: Vec<ColumnFamilyDescriptor> =
+            cfs.into_iter().map(|(_, cf_desc)| cf_desc).collect();
+
+        // Open the db or create a new one if it doesn't exist
+        let mut db =
+            DB::open_cf_descriptors(&create_missing_db_and_cols_opts, &path, cf_descriptors)
+                .map_err(anyhow::Error::msg)
+                .context(format!("Failed to open database at: {}", path.display()))?;
+
+        // We must check if the database is new or not, so we don't try and migrate from version 0.
+        // Because version 0 had no metadata, we cant tell the difference between version 0 and a new db.
+        let data_version = if has_existing_db {
+            read_data_version(&db, &logger)
+        } else {
+            write_latest_data_version(&db)?
+        };
+
+        if data_version != DATA_VERSION {
+            if data_version < DATA_VERSION {
+                // TODO: backup the database before migrating it. #1182
+
+                slog::info!(
+                    logger,
+                    "Database is migrating from version {} to {}",
+                    data_version,
+                    DATA_VERSION
+                );
+                // Preform migrations
+                migrate_db_to_latest(&mut db, data_version)
+                    .expect(&format!("Failed to migrate database at {}. Manual restoration of a backup or purging of the file is required.", path.display()));
+            } else {
+                // Automatic backwards migration is not supported
+                return Err(anyhow::Error::msg(
+                    format!("Database is at data version {} but needs to be {}. Manual backwards migration is required",
+                    data_version,
+                    DATA_VERSION)
+                ));
+            }
+        } else {
+            // Check for unused data columns
+            let junk_columns: HashSet<String> = cfs_check
+                .iter()
+                .filter(|cf_name| !COLUMN_FAMILIES.iter().any(|s| s == cf_name))
+                .cloned()
+                .collect();
+            if junk_columns.len() > 0 {
+                // Just a warning for now. We can delete the columns if this becomes a problem in the future.
+                slog::warn!(logger, "Unknown columns found in db: {:?}", junk_columns);
+            }
+        }
+
+        Ok(PersistentKeyDB { db, logger })
+    }
+}
+
+impl KeyDB for PersistentKeyDB {
+    fn update_key(&mut self, key_id: &KeyId, keygen_result_info: &KeygenResultInfo) {
+        // TODO: this error should be handled better
+        let keygen_result_info_encoded =
+            bincode::serialize(keygen_result_info).expect("Could not serialize keygen_result_info");
+
+        let key_id_with_prefix = [KEYGEN_DATA_PREFIX.to_vec(), key_id.0.clone()].concat();
+
+        self.db
+            .put_cf(
+                get_data_column_handle(&self.db),
+                key_id_with_prefix,
+                &keygen_result_info_encoded,
+            )
+            .unwrap_or_else(|_| {
+                panic!(
+                    "Could not write key share for key_id `{}` to database",
+                    &key_id
+                )
+            });
+    }
+
+    fn load_keys(&self) -> HashMap<KeyId, KeygenResultInfo> {
+        self.db
+            .prefix_iterator_cf(get_data_column_handle(&self.db), KEYGEN_DATA_PREFIX)
+            .filter_map(|(key_id, key_info)| {
+                // Strip the prefix off the key_id
+                let key_id: KeyId = KeyId(key_id[PREFIX_SIZE..].into());
+
+                // deserialize the `KeygenResultInfo`
+                match bincode::deserialize::<KeygenResultInfo>(&*key_info) {
+                    Ok(keygen_info) => {
+                        slog::info!(
+                            self.logger,
+                            "Loaded key_info (key_id: {}) from database",
+                            key_id
+                        );
+                        Some((key_id, keygen_info))
+                    }
+                    Err(err) => {
+                        slog::error!(
+                            self.logger,
+                            "Could not deserialize key_info (key_id: {}) from database: {}",
+                            key_id,
+                            err
+                        );
+                        None
+                    }
+                }
+            })
+            .collect()
+    }
+}
 
 fn get_metadata_column_handle(db: &DB) -> &ColumnFamily {
     get_column_handle(db, METADATA_COLUMN)
@@ -45,62 +214,6 @@ fn get_data_column_handle(db: &DB) -> &ColumnFamily {
 fn get_column_handle<'a>(db: &'a DB, column_name: &str) -> &'a ColumnFamily {
     db.cf_handle(column_name)
         .unwrap_or_else(|| panic!("Should get column family handle for {}", column_name))
-}
-
-/// Migrates the db forward one version migration at a time
-fn migrate_db_to_latest(db: &mut DB, from_version: u32) -> Result<(), anyhow::Error> {
-    assert!(from_version < DATA_VERSION, "Invalid migration");
-
-    for version in (from_version + 1)..=DATA_VERSION {
-        match version {
-            1 => {
-                migration_0_to_1(db)?;
-            }
-            _ => {
-                return Err(anyhow::Error::msg(format!(
-                    "Invalid migration to data version {}",
-                    version
-                )))
-            }
-        }
-    }
-    Ok(())
-}
-
-// Moving the keygen data from column `col0` to `data` and adding a prefix
-// Also adding data version to the metadata column
-fn migration_0_to_1(db: &mut DB) -> Result<(), anyhow::Error> {
-    // Update version data
-    let mut batch = WriteBatch::default();
-    add_version_to_batch_write(db, 1, &mut batch);
-
-    // Get column handles
-    let old_cf_name = "col0";
-
-    let old_cf = db
-        .cf_handle(old_cf_name)
-        .unwrap_or_else(|| panic!("Should get column {}", &old_cf_name));
-
-    let new_cf = db.cf_handle("data").expect("Should get column data");
-
-    // Read the data from the old column and add it to the new column via the batch write
-    for (k, v) in db.iterator_cf(old_cf, IteratorMode::Start) {
-        // Add the prefix to the key
-        let key = [KEYGEN_DATA_PREFIX.iter().cloned().collect(), k.clone()].concat();
-        batch.put_cf(new_cf, &key, v);
-        batch.delete_cf(old_cf, k);
-    }
-
-    // Write the batch
-    db.write(batch).map_err(|e| {
-        anyhow::Error::msg(format!("Failed to write to db during migration: {}", e))
-    })?;
-
-    // Delete the old column family
-    db.drop_cf(old_cf_name)
-        .unwrap_or_else(|_| panic!("Should drop old column family {}", old_cf_name));
-
-    Ok(())
 }
 
 /// Used is every migration to update the db data version in the same batch write as the migration operation
@@ -147,173 +260,64 @@ fn read_data_version(db: &DB, logger: &slog::Logger) -> u32 {
     }
 }
 
-/// Database for keys and persistent metadata
-pub struct PersistentKeyDB {
-    /// Rocksdb database instance
-    db: DB,
-    logger: slog::Logger,
-}
-impl PersistentKeyDB {
-    pub fn new(path: &Path, logger: &slog::Logger) -> Result<Self> {
-        let logger = logger.new(o!(COMPONENT_KEY => "PersistentKeyDB"));
+/// Migrates the db forward one version migration at a time
+fn migrate_db_to_latest(db: &mut DB, from_version: u32) -> Result<(), anyhow::Error> {
+    assert!(from_version < DATA_VERSION, "Invalid migration");
 
-        // Build a list of column families with default descriptors
-        let mut cfs_check: HashSet<String> = HashSet::new();
-        let mut cfs: HashMap<String, ColumnFamilyDescriptor> = COLUMN_FAMILIES
-            .iter()
-            .map(|s| {
-                (
-                    s.to_string(),
-                    ColumnFamilyDescriptor::new((*s).to_string(), Options::default()),
-                )
-            })
-            .collect();
-        let has_existing_db = path.exists();
-        if has_existing_db {
-            // Add the column families found in the existing db, they might be needed for migration.
-            cfs = cfs
-                .into_iter()
-                .chain(
-                    DB::list_cf(&Options::default(), path)
-                        .expect("Should get list of column families")
-                        .into_iter()
-                        .filter_map(|cf| {
-                            // Filter out the `default` column because we don't use it
-                            if cf == "default" {
-                                None
-                            } else {
-                                cfs_check.insert(cf.clone());
-                                Some((
-                                    cf.clone(),
-                                    ColumnFamilyDescriptor::new(cf, Options::default()),
-                                ))
-                            }
-                        }),
-                )
-                .collect();
-        }
-
-        // Override the default descriptors and create the columns that we need
-        let mut cfopts_for_prefix = Options::default();
-        cfopts_for_prefix
-            .set_prefix_extractor(rocksdb::SliceTransform::create_fixed_prefix(PREFIX_SIZE));
-        cfs.insert(
-            DATA_COLUMN.to_string(),
-            // The DATA_COLUMN must use a descriptor with a prefix extractor
-            ColumnFamilyDescriptor::new(DATA_COLUMN, cfopts_for_prefix),
-        );
-        cfs.insert(
-            METADATA_COLUMN.to_string(),
-            ColumnFamilyDescriptor::new(METADATA_COLUMN, Options::default()),
-        );
-
-        let mut create_missing_db_and_cols_opts = Options::default();
-        create_missing_db_and_cols_opts.create_missing_column_families(true);
-        create_missing_db_and_cols_opts.create_if_missing(true);
-
-        let cf_descriptors: Vec<ColumnFamilyDescriptor> =
-            cfs.into_iter().map(|(_, cf_desc)| cf_desc).collect();
-
-        // Open the db or create a new one if it doesn't exist
-        let mut db =
-            DB::open_cf_descriptors(&create_missing_db_and_cols_opts, &path, cf_descriptors)
-                .map_err(anyhow::Error::msg)
-                .context(format!("Failed to open database at: {}", path.display()))?;
-
-        // We must check if the database is new or not, so we don't try and migrate from version 0.
-        // Because version 0 had no metadata, we cant tell the difference between version 0 and a new db.
-        let data_version = if has_existing_db {
-            read_data_version(&db, &logger)
-        } else {
-            write_latest_data_version(&db)?
-        };
-
-        if data_version != DATA_VERSION {
-            if data_version < DATA_VERSION {
-                // TODO: backup the database before migrating it. #1182
-
-                slog::info!(
-                    logger,
-                    "Database is migrating from version {} to {}",
-                    data_version,
-                    DATA_VERSION
-                );
-                // Preform migrations
-                migrate_db_to_latest(&mut db, data_version).expect("Failed to migrate database");
-            } else {
-                // Automatic backwards migration is not supported
-                return Err(anyhow::Error::msg(
-                    format!("Database is at data version {} but needs to be {}. Manual backwards migration is required",
-                    data_version,
-                    DATA_VERSION)
-                ));
+    for version in (from_version + 1)..=DATA_VERSION {
+        match version {
+            1 => {
+                migration_0_to_1(db)?;
             }
-        } else {
-            // Check for unused data columns
-            let junk_columns: HashSet<String> = cfs_check
-                .iter()
-                .filter(|column| !COLUMN_FAMILIES.iter().any(|s| s == column))
-                .cloned()
-                .collect();
-            if junk_columns.len() > 0 {
-                // Just a warning for now. We can delete the columns if this becomes a problem in the future.
-                slog::warn!(logger, "Unknown columns found in db: {:?}", junk_columns);
+            _ => {
+                return Err(anyhow::Error::msg(format!(
+                    "Invalid migration to data version {}",
+                    version
+                )))
             }
         }
-
-        Ok(PersistentKeyDB { db, logger })
     }
+    Ok(())
 }
 
-impl KeyDB for PersistentKeyDB {
-    fn update_key(&mut self, key_id: &KeyId, keygen_result_info: &KeygenResultInfo) {
-        // TODO: this error should be handled better
-        let keygen_result_info_encoded =
-            bincode::serialize(keygen_result_info).expect("Could not serialize keygen_result_info");
+// Moving the keygen data from column `col0` to `data` and adding a prefix
+// Also adding data version to the metadata column
+fn migration_0_to_1(db: &mut DB) -> Result<(), anyhow::Error> {
+    // Update version data
+    let mut batch = WriteBatch::default();
+    add_version_to_batch_write(db, 1, &mut batch);
 
-        let key_with_prefix = [KEYGEN_DATA_PREFIX.to_vec(), key_id.0.clone()].concat();
+    // Get column handles
+    let old_cf_name = "col0";
 
-        self.db
-            .put_cf(
-                get_data_column_handle(&self.db),
-                key_with_prefix,
-                &keygen_result_info_encoded,
-            )
-            .unwrap_or_else(|_| {
-                panic!(
-                    "Could not write key share for key_id `{}` to database",
-                    &key_id
-                )
-            });
+    let old_cf = db
+        .cf_handle(old_cf_name)
+        .unwrap_or_else(|| panic!("Should get column {}", &old_cf_name));
+
+    let new_cf = db.cf_handle("data").expect("Should get column data");
+
+    // Read the data from the old column and add it to the new column via the batch write
+    for (old_key, old_value) in db.iterator_cf(old_cf, IteratorMode::Start) {
+        // Add the prefix to the key
+        let key_with_prefix = [
+            KEYGEN_DATA_PREFIX.iter().cloned().collect(),
+            old_key.clone(),
+        ]
+        .concat();
+        batch.put_cf(new_cf, &key_with_prefix, old_value);
+        batch.delete_cf(old_cf, old_key);
     }
 
-    fn load_keys(&self) -> HashMap<KeyId, KeygenResultInfo> {
-        self.db
-            .prefix_iterator_cf(get_data_column_handle(&self.db), KEYGEN_DATA_PREFIX)
-            .filter_map(|(key_id, key_info)| {
-                let key_id: KeyId = KeyId(key_id[PREFIX_SIZE..].into());
-                match bincode::deserialize::<KeygenResultInfo>(&*key_info) {
-                    Ok(keygen_info) => {
-                        slog::info!(
-                            self.logger,
-                            "Loaded key_info (key_id: {}) from database",
-                            key_id
-                        );
-                        Some((key_id, keygen_info))
-                    }
-                    Err(err) => {
-                        slog::error!(
-                            self.logger,
-                            "Could not deserialize key_info (key_id: {}) from database: {}",
-                            key_id,
-                            err
-                        );
-                        None
-                    }
-                }
-            })
-            .collect()
-    }
+    // Write the batch
+    db.write(batch).map_err(|e| {
+        anyhow::Error::msg(format!("Failed to write to db during migration: {}", e))
+    })?;
+
+    // Delete the old column family
+    db.drop_cf(old_cf_name)
+        .unwrap_or_else(|_| panic!("Should drop old column family {}", old_cf_name));
+
+    Ok(())
 }
 
 #[cfg(test)]
