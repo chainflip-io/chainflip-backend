921a7c5d4d18e8f831b1fd19648d9a93b1b7a402
kylezs <zsembery.kyle@gmail.com>
2022-01-25 11:52:38 +0100

    ok_or_else client

--- engine/src/state_chain/client.rs
+++ engine/src/state_chain/client.rs
@@ -228,7 +228,7 @@ impl StateChainRpcApi for StateChainRpcClient {
             .await
             .map_err(rpc_error_into_anyhow_error)
             .context("latest_block_hash RPC API failed")?
-            .ok_or(anyhow::Error::msg("Latest block hash could not be fetched"))?
+            .ok_or_else(|| anyhow::Error::msg("Latest block hash could not be fetched"))?
             .hash())
     }
 
@@ -496,11 +496,11 @@ impl<RpcClient: StateChainRpcApi> StateChainClient<RpcClient> {
                     extrinsic,
                     tx_hash
                 );
-                return Ok(tx_hash);
+                Ok(tx_hash)
             }
             Err(err) => {
                 slog::error!(logger, "Failed to submit unsigned extrinsic: {:?}", err);
-                return Err(err);
+                Err(err)
             }
         }
     }
