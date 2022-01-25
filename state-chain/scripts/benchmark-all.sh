56618ce94136ccc50dd540164a4dd89d31d1c422
Jan Börner <jan@chainflip.io>
2022-01-25 17:19:45 +0100

    * fixed paths

--- state-chain/scripts/benchmark-all.sh
+++ state-chain/scripts/benchmark-all.sh
@@ -4,33 +4,33 @@ echo "Build the state-chain node with benchmark features enabled..."
 cargo build --release --features runtime-benchmarks
 echo "Executing benchmarks..."
 # benchmark the auction pallet
-source benchmark.sh auction
+source ./state-chain/scripts/benchmark.sh auction
 # benchmark the broadcast pallet
-source benchmark.sh broadcast
+source ./state-chain/scripts/benchmark.sh broadcast
 # benchmark the emissions pallet
-source benchmark.sh emissions
+source ./state-chain/scripts/benchmark.sh emissions
 # benchmark the environment pallet
-source benchmark.sh environment
+source ./state-chain/scripts/benchmark.sh environment
 # benchmark the flip pallet
-source benchmark.sh flip
+source ./state-chain/scripts/benchmark.sh flip
 # benchmark the governance pallet
-source benchmark.sh governance
+source ./state-chain/scripts/benchmark.sh governance
 # benchmark the online pallet
-source benchmark.sh online
+source ./state-chain/scripts/benchmark.sh online
 # benchmark the reputation pallet
-source benchmark.sh reputation
+source ./state-chain/scripts/benchmark.sh reputation
 # benchmark the rewards pallet
-source benchmark.sh rewards
+source ./state-chain/scripts/benchmark.sh rewards
 # benchmark the staking pallet
-source benchmark.sh staking
+source ./state-chain/scripts/benchmark.sh staking
 # benchmark the threshold-signature pallet
 # source benchmark.sh threshold-signature
 # benchmark the validator pallet
-source benchmark.sh validator
+source ./state-chain/scripts/benchmark.sh validator
 # benchmark the vaults pallet
-source benchmark.sh vaults
+source ./state-chain/scripts/benchmark.sh vaults
 # benchmark the witnesser pallet
-source benchmark.sh witnesser
+source ./state-chain/scripts/benchmark.sh witnesser
 # benchmark the witnesser-api pallet
 # source benchmark.sh witnesser-api
 echo "Checking benhmark diff..."
