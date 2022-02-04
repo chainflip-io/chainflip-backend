75d93727330e26ddd8a3b1d808d4ac9c1242b42e
kylezs <zsembery.kyle@gmail.com>
2022-02-04 12:17:24 +0100

    WIP tests

--- engine/src/eth/http_observer.rs
+++ engine/src/eth/http_observer.rs
@@ -19,6 +19,7 @@ const HTTP_POLL_INTERVAL: Duration = Duration::from_secs(4);
 // polls the HTTP endpoint every x seconds, returning the next head when it progresses
 pub async fn polling_http_head_stream<EthHttpRpc: EthHttpRpcApi>(
     eth_http_rpc: EthHttpRpc,
+    poll_interval: Duration,
 ) -> impl Stream<Item = Block<H256>> {
     struct StreamState<EthHttpRpc> {
         last_block_fetched: U64,
@@ -35,7 +36,7 @@ pub async fn polling_http_head_stream<EthHttpRpc: EthHttpRpcApi>(
     Box::pin(stream::unfold(init_data, move |mut state| async move {
         'block_safety_loop: loop {
             // TODO: Check this is the correct sleep
-            std::thread::sleep(HTTP_POLL_INTERVAL);
+            std::thread::sleep(poll_interval);
 
             let unsafe_block_number = state.eth_http_rpc.block_number().await.unwrap();
             println!("Got eth block number {}", unsafe_block_number);
@@ -44,10 +45,17 @@ pub async fn polling_http_head_stream<EthHttpRpc: EthHttpRpcApi>(
                 panic!("NOOO, we shouldn't ever go backwards on HTTP (I think, waiting for rivet to get back to me)");
             } else if unsafe_block_number == state.last_block_fetched {
                 println!("Our unsafe block number is the same as the last fetched block");
-                // ignore
-            } else if unsafe_block_number == state.last_block_fetched + U64::from(1) {
+                // ignore - we will wait until next poll to go again
+            } else if (state.last_block_yielded == U64::from(0)
+                && state.last_block_fetched == U64::from(0))
+                && unsafe_block_number == state.last_block_fetched + U64::from(1)
+            {
+                // We enter this when we have progressed, or if this is the first iteration
                 // we should progress to the next block
-                println!("The unsafe block number is one above the last fetched");
+                println!(
+                    "The unsafe block number is {} and last fetched is: {}",
+                    unsafe_block_number, state.last_block_fetched
+                );
 
                 // TODO: Protect against overflows + check this is in aligment with the other observer
                 // We should do this in a test
@@ -63,13 +71,20 @@ pub async fn polling_http_head_stream<EthHttpRpc: EthHttpRpcApi>(
                     .unwrap();
                 state.last_block_yielded = block.number.unwrap();
                 break Some((block, state));
-            } else if unsafe_block_number > state.last_block_fetched + 1 {
+                // we want to skip the first block
+            } else if state.last_block_fetched != U64::from(0)
+                && unsafe_block_number > state.last_block_fetched + 1
+            {
                 // we skipped a block
                 // if our *head* is now at N, and we are assuming N - 5 is safe
                 // Then (N - 1) - 5 must be safe
                 let last_block_yielded_u64 = state.last_block_yielded.as_u64();
                 for return_block_number in last_block_yielded_u64 + 1..unsafe_block_number.as_u64()
                 {
+                    println!(
+                        "Yielding block with return_block_number: {}",
+                        return_block_number
+                    );
                     let block = state
                         .eth_http_rpc
                         .block(U64::from(return_block_number).into())
@@ -80,6 +95,8 @@ pub async fn polling_http_head_stream<EthHttpRpc: EthHttpRpcApi>(
                     state.last_block_yielded = block.number.unwrap();
                     break 'block_safety_loop Some((block, state));
                 }
+            } else {
+                println!("Entered else, do nothing");
             }
             state.last_block_fetched = unsafe_block_number;
         }
@@ -89,22 +106,88 @@ pub async fn polling_http_head_stream<EthHttpRpc: EthHttpRpcApi>(
 #[cfg(test)]
 mod tests {
 
-    use mockall::mock;
+    use mockall::{mock, predicate::eq, Sequence};
 
     use crate::eth::{EthHttpRpcApi, EthRpcApi};
 
     use super::*;
 
+    // in tests, this can be instant
+    const TEST_HTTP_POLL_INTERVAL: Duration = Duration::from_millis(1);
+
     use crate::eth::mocks::MockEthHttpRpc;
     use async_trait::async_trait;
 
-    #[tokio::test]
-    async fn test_http_stream() {
-        let mock_eth_http_rpc = MockEthHttpRpc::new();
+    use anyhow::Result;
 
-        let mut stream = polling_http_head_stream(mock_eth_http_rpc).await;
-        while let Some(block) = stream.next().await {
-            println!("Here's the block: {}", block.number.unwrap());
+    fn dummy_block(block_number: u64) -> Result<Option<Block<H256>>> {
+        Ok(Some(Block {
+            hash: Some(H256([(block_number % 256) as u8; 32])),
+            number: Some(U64::from(block_number)),
+            logs_bloom: Default::default(),
+            ..Default::default()
+        }))
+    }
+
+    #[tokio::test]
+    async fn returns_nothing_on_initial_block_read() {
+        let mut mock_eth_http_rpc = MockEthHttpRpc::new();
+
+        // ensure these looped calls occur in order
+        let mut seq_numbers = Sequence::new();
+
+        for n in 10..=11 {
+            mock_eth_http_rpc
+                .expect_block_number()
+                .times(1)
+                .in_sequence(&mut seq_numbers)
+                .returning(move || Ok(U64::from(n)));
+        }
+
+        mock_eth_http_rpc
+            .expect_block()
+            .times(1)
+            .returning(move |n| dummy_block(n.as_u64()));
+
+        let mut stream = polling_http_head_stream(mock_eth_http_rpc, TEST_HTTP_POLL_INTERVAL).await;
+        assert_eq!(stream.next().await.unwrap().number.unwrap(), U64::from(10));
+    }
+
+    #[tokio::test]
+    async fn if_block_numbers_increment_by_one_progresses_at_block_margin() {
+        let mut mock_eth_http_rpc = MockEthHttpRpc::new();
+
+        // ensure these looped calls occur in order
+        let mut seq_numbers = Sequence::new();
+
+        // TODO: Try unify these
+        let mut seq_blocks = Sequence::new();
+
+        let block_range = 10..20;
+
+        for block_number in block_range.clone() {
+            mock_eth_http_rpc
+                .expect_block_number()
+                .times(1)
+                .in_sequence(&mut seq_numbers)
+                .returning(move || Ok(U64::from(block_number)));
+
+            mock_eth_http_rpc
+                .expect_block()
+                .times(1)
+                .in_sequence(&mut seq_blocks)
+                .returning(move |number| dummy_block(number.as_u64()));
+        }
+
+        let mut stream = polling_http_head_stream(mock_eth_http_rpc, TEST_HTTP_POLL_INTERVAL).await;
+        for block_number in block_range {
+            println!("Testing block_number: {}", block_number);
+            if let Some(block) = stream.next().await {
+                assert_eq!(
+                    block.number.unwrap(),
+                    U64::from(block_number - ETH_BLOCK_SAFETY_MARGIN)
+                );
+            };
         }
     }
 
