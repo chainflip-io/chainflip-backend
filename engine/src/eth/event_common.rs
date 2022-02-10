023397b2f845ab0f75d49fa9370029d0e675f599
kylezs <zsembery.kyle@gmail.com>
2022-02-10 15:03:06 +0000

    Move and update common event info decoded test to include log index

--- engine/src/eth/event_common.rs
+++ engine/src/eth/event_common.rs
@@ -56,3 +56,44 @@ impl<EventParameters: Debug> EventWithCommon<EventParameters> {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use std::str::FromStr;
+
+    use sp_core::H160;
+
+    use crate::eth::{key_manager::KeyManager, EthObserver};
+
+    use super::*;
+
+    #[tokio::test]
+    async fn common_event_info_decoded_correctly() {
+        let key_manager = KeyManager::new(H160::default()).unwrap();
+
+        let transaction_hash =
+            H256::from_str("0x6320cfd702415644192bf57702ceccc0d6de0ddc54fe9aa53f9b1a5d9035fe52")
+                .unwrap();
+
+        let event = EventWithCommon::decode(
+            &key_manager.decode_log_closure().unwrap(),
+             web3::types::Log {
+                address: H160::zero(),
+                topics: vec![H256::from_str("0x19389c59b816d8b0ec43f2d5ed9b41bddc63d66dac1ecd808efe35b86b9ee0bf")
+                .unwrap()],
+                data: web3::types::Bytes(hex::decode("00000000000000000000000000000000000000000000000000000000000000001742daacd4dbfbe66d4c8965550295873c683cb3b65019d3a53975ba553cc31d000000000000000000000000000000000000000000000000000000000000000131b2ba4b46201610901c5164f42edd1f64ce88076fde2e2c544f9dc3d7b350ae0000000000000000000000000000000000000000000000000000000000000001").unwrap()),
+                block_hash: None,
+                block_number: Some(web3::types::U64::zero()),
+                transaction_hash: Some(transaction_hash),
+                transaction_index: None,
+                log_index: Some(U256::from(0)),
+                transaction_log_index: None,
+                log_type: None,
+                removed: None,
+            }
+        ).unwrap();
+
+        assert_eq!(event.tx_hash, transaction_hash);
+    }
+}
