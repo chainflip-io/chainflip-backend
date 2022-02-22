b6eff84869ea2b17d823b946c0bc9a875a5cc83e
Andy Bell <andyjsbell@gmail.com>
2022-02-22 11:19:30 +0100

    kill storage item in migration

--- state-chain/pallets/cf-auction/src/migrations.rs
+++ state-chain/pallets/cf-auction/src/migrations.rs
@@ -6,6 +6,7 @@ pub(crate) mod v1 {
 
 	generate_storage_alias!(Auction, CurrentPhase => Value<()>);
 	generate_storage_alias!(Auction, LastAuctionResult => Value<()>);
+	generate_storage_alias!(Auction, CurrentAuctionIndex => Value<()>);
 
 	#[cfg(feature = "try-runtime")]
 	pub(crate) fn pre_migrate<T: Config, P: GetStorageVersion>() -> Result<(), &'static str> {
@@ -14,6 +15,7 @@ pub(crate) mod v1 {
 		// For my sanity
 		log::info!(target: "runtime:cf_auction", "AuctionPhase.exists()? {:?}", CurrentPhase::exists());
 		log::info!(target: "runtime:cf_auction", "LastAuctionResult.exits()? {:?}", LastAuctionResult::exists());
+		log::info!(target: "runtime:cf_auction", "CurrentAuctionIndex.exits()? {:?}", CurrentAuctionIndex::exists());
 
 		log::info!(
 			target: "runtime::cf_auction",
@@ -25,7 +27,9 @@ pub(crate) mod v1 {
 
 	pub fn migrate<T: Config>() -> frame_support::weights::Weight {
 		// Current version is is genesis, upgrade to version 1
-		// Changes are the removal of two storage items: `CurrentPhase` and `LastAuctionResult`
+		// Changes are the removal of two storage items: `CurrentPhase`, `CurrentAuctionIndex` and
+		// `LastAuctionResult`
+		CurrentAuctionIndex::kill();
 		CurrentPhase::kill();
 		LastAuctionResult::kill();
 
@@ -46,6 +50,7 @@ pub(crate) mod v1 {
 		// We should expect no values for these items
 		assert_err!(CurrentPhase::try_get(), ());
 		assert_err!(LastAuctionResult::try_get(), ());
+		assert_err!(CurrentAuctionIndex::try_get(), ());
 
 		log::info!(
 			target: "runtime::cf_auction",
