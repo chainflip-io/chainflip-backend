f7abc8bbeb074d05b334c023d86b685c24a043cd
Daniel <daniel@chainflip.io>
2022-02-24 17:51:20 +0100

    feat: use CfeSettings default values in chainspec.

--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -16,6 +16,7 @@ pub use frame_support::{
 	StorageValue,
 };
 use frame_system::offchain::SendTransactionTypes;
+pub use pallet_cf_environment::cfe::CfeSettings;
 use pallet_grandpa::{
 	fg_primitives, AuthorityId as GrandpaId, AuthorityList as GrandpaAuthorityList,
 };
