244b4c7e1f14870a1e52c0b95515686ee40c4299
Daniel <daniel@chainflip.io>
2022-02-24 14:55:19 +0100

    feat: working benchmarks for threshold pallet

--- state-chain/pallets/cf-threshold-signature/src/benchmarking.rs
+++ state-chain/pallets/cf-threshold-signature/src/benchmarking.rs
@@ -4,8 +4,10 @@
 use super::*;
 
 use cf_runtime_benchmark_utilities::BenchmarkDefault;
-use frame_benchmarking::{account, benchmarks_instance_pallet};
+use frame_benchmarking::{account, benchmarks_instance_pallet, whitelist_account};
+use frame_support::dispatch::UnfilteredDispatchable;
 use frame_system::RawOrigin;
+use pallet_cf_online::Call as OnlineCall;
 use pallet_cf_validator::{ValidatorLookup, Validators};
 use sp_std::convert::TryInto;
 
@@ -13,40 +15,52 @@ const SEED: u32 = 0;
 
 type SignatureFor<T, I> = <<T as Config<I>>::TargetChain as ChainCrypto>::ThresholdSignature;
 
+fn add_online_validators<T, I>(accounts: I)
+where
+	T: frame_system::Config + pallet_cf_validator::Config + pallet_cf_online::Config,
+	I: Clone + Iterator<Item = <T as frame_system::Config>::AccountId>,
+{
+	Validators::<T>::put(accounts.clone().collect::<Vec<_>>());
+	for account in accounts {
+		whitelist_account!(account);
+		ValidatorLookup::<T>::insert(&account, ());
+		OnlineCall::<T>::heartbeat()
+			.dispatch_bypass_filter(RawOrigin::Signed(account).into())
+			.unwrap();
+	}
+}
+
 benchmarks_instance_pallet! {
 	where_clause {
 		where
 			T: frame_system::Config
 			+ pallet_cf_validator::Config
+			+ pallet_cf_online::Config
 	}
 
 	signature_success {
 		let all_accounts = (0..150).map(|i| account::<T::AccountId>("signers", i, SEED));
 
-		Validators::<T>::put(all_accounts.clone().collect::<Vec<_>>());
-		for account in all_accounts.clone() {
-			ValidatorLookup::<T>::insert(account, ());
-		}
+		add_online_validators::<T, _>(all_accounts);
 
 		let ceremony_id = Pallet::<T, I>::request_signature(<T::SigningContext as BenchmarkDefault>::benchmark_default());
 		let signature = <SignatureFor<T, I> as BenchmarkDefault>::benchmark_default();
 	} : _(RawOrigin::None, ceremony_id, signature)
 	report_signature_failed {
 		let a in 1 .. 100;
-		let mut all_accounts = (0..150).map(|i| account::<T::AccountId>("signers", i, SEED));
+		let all_accounts = (0..150).map(|i| account::<T::AccountId>("signers", i, SEED));
 
-		Validators::<T>::put(all_accounts.clone().collect::<Vec<_>>());
-		for account in all_accounts.clone() {
-			ValidatorLookup::<T>::insert(account, ());
-		}
-		let all_validator_ids = all_accounts.clone().map(<T as Chainflip>::ValidatorId::from);
-		let offenders = BTreeSet::from_iter(all_validator_ids.take(a as usize))
-			.try_into()
-			.expect("Benchmark threshold should not exceed BTreeSet bounds");
-		let signer = all_accounts.nth(a as usize).unwrap();
+		add_online_validators::<T, _>(all_accounts);
 
 		let ceremony_id = Pallet::<T, I>::request_signature(<T::SigningContext as BenchmarkDefault>::benchmark_default());
-	} : _(RawOrigin::Signed(signer), ceremony_id, offenders)
+
+		let mut threshold_set = PendingRequests::<T, I>::get(ceremony_id).unwrap().remaining_respondents.into_iter();
+
+		let reporter = threshold_set.next().unwrap();
+		let offenders = BTreeSet::from_iter(threshold_set.take(a as usize))
+			.try_into()
+			.expect("Benchmark threshold should not exceed BTreeSet bounds");
+	} : _(RawOrigin::Signed(reporter.into()), ceremony_id, offenders)
 	on_initialize {} : {}
 	determine_offenders {
 		let a in 1 .. 200;
