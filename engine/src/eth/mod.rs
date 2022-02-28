2e94fe4c8b0dd6cf3556bdeba865630c587f0b08
kylezs <zsembery.kyle@gmail.com>
2022-02-28 17:33:50 +0100

    Remove TODO tests already done

--- engine/src/eth/mod.rs
+++ engine/src/eth/mod.rs
@@ -1857,12 +1857,6 @@ mod merged_stream_tests {
 
         assert!(stream.next().await.is_none());
     }
-
-    // merged stream does not return blocks ahead of the failed block
-
-    // merged stream ignores when the stream receives a failed block
-
-    // test interleaving errors - the other tests should cover this, but it's cool to see that it can happen
 }
 
 #[cfg(test)]
