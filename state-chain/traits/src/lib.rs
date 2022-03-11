37c73b2f15ac671f53be01a63f3bc2c4d60a9c7a
dandanlen <3168260+dandanlen@users.noreply.github.com>
2022-03-11 16:46:22 +0100

    feat: remove AuctionError (#1414)

--- state-chain/traits/src/lib.rs
+++ state-chain/traits/src/lib.rs
@@ -172,9 +172,11 @@ pub type ActiveValidatorRange = (u32, u32);
 pub trait Auctioneer {
 	type ValidatorId;
 	type Amount;
+	type Error: Into<DispatchError>;
 
 	/// Run an auction by qualifying a validator
-	fn resolve_auction() -> Result<AuctionResult<Self::ValidatorId, Self::Amount>, AuctionError>;
+	fn resolve_auction() -> Result<AuctionResult<Self::ValidatorId, Self::Amount>, Self::Error>;
+
 	/// Update validator status for the winners
 	fn update_validator_status(winners: &[Self::ValidatorId]);
 }
@@ -204,12 +206,6 @@ pub trait VaultRotator {
 	fn get_keygen_status() -> Option<KeygenStatus>;
 }
 
-/// An error has occurred during an auction
-#[derive(Encode, Decode, Clone, Copy, RuntimeDebug, PartialEq, Eq)]
-pub enum AuctionError {
-	/// Insufficient number of bidders
-	NotEnoughBidders,
-}
 /// Handler for Epoch life cycle events.
 pub trait EpochTransitionHandler {
 	/// The id type used for the validators.
