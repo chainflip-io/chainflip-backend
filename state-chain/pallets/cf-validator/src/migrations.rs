5be5609b47c8395e9bfeb3fb49c9709981f2f416
dandanlen <3168260+dandanlen@users.noreply.github.com>
2022-03-29 15:49:42 +0200

    fix: correct migration bounds for validator pallet (#1475)

--- state-chain/pallets/cf-validator/src/migrations.rs
+++ state-chain/pallets/cf-validator/src/migrations.rs
@@ -2,4 +2,4 @@ pub mod v2;
 
 use cf_runtime_upgrade_utilities::VersionedMigration;
 
-pub type PalletMigration<T> = (VersionedMigration<crate::Pallet<T>, v2::Migration<T>, 0, 2>,);
+pub type PalletMigration<T> = (VersionedMigration<crate::Pallet<T>, v2::Migration<T>, 1, 2>,);
