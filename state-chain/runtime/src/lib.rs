985d35ad8da7b05ee50c1d700e38cb061008fba6
dandanlen <3168260+dandanlen@users.noreply.github.com>
2022-04-12 13:25:57 +0200

    fix: apply transaction fees. (#1507)
    * fix: apply transaction fees.
    
    * fix: remove needless pub use

--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -536,6 +536,7 @@ pub type SignedExtra = (
 	frame_system::CheckEra<Runtime>,
 	frame_system::CheckNonce<Runtime>,
 	frame_system::CheckWeight<Runtime>,
+	pallet_transaction_payment::ChargeTransactionPayment<Runtime>,
 );
 /// Unchecked extrinsic type as expected by this runtime.
 pub type UncheckedExtrinsic = generic::UncheckedExtrinsic<Address, Call, Signature, SignedExtra>;
