339812f79026d3c486a48fbbfb0cf60407c38484
kylezs <zsembery.kyle@gmail.com>
2022-04-28 15:40:44 +0200

    chore: use T for consistency

--- state-chain/test-utilities/src/lib.rs
+++ state-chain/test-utilities/src/lib.rs
@@ -1,13 +1,13 @@
 use frame_system::Config;
 
-pub fn last_event<Runtime: Config>() -> <Runtime as Config>::Event {
-	frame_system::Pallet::<Runtime>::events().pop().expect("Event expected").event
+pub fn last_event<T: Config>() -> <T as Config>::Event {
+	frame_system::Pallet::<T>::events().pop().expect("Event expected").event
 }
 
 /// Checks the deposited events in the order they occur
 #[macro_export]
 macro_rules! assert_event_sequence {
-	($runtime:ty, $($evt:expr $( => $test:block )? ),*) => {
+	($runtime:ty, $($evt:expr),*) => {
 		let mut events = frame_system::Pallet::<$runtime>::events()
 		.into_iter()
 		// We want to be able to input the events into this macro in the order they occurred.
