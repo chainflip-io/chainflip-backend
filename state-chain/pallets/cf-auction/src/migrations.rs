408d08245d49d19200594fef874c21a215395762
Daniel <daniel@chainflip.io>
2022-05-02 14:24:51 +0200

    fix auction miration

--- state-chain/pallets/cf-auction/src/migrations.rs
+++ state-chain/pallets/cf-auction/src/migrations.rs
@@ -3,5 +3,7 @@ pub mod v2;
 
 use cf_runtime_upgrade_utilities::VersionedMigration;
 
-pub type PalletMigration<T> = (VersionedMigration<crate::Pallet<T>, v1::Migration<T>, 0, 1>,);
-pub type PalletMigration<T> = (VersionedMigration<crate::Pallet<T>, v2::Migration<T>, 1, 2>,);
+pub type PalletMigration<T> = (
+	VersionedMigration<crate::Pallet<T>, v1::Migration<T>, 0, 1>,
+	VersionedMigration<crate::Pallet<T>, v2::Migration<T>, 1, 2>,
+);
