6609f57ad97a8b4c191464787d6dc52edd7699a7
Tom Nash <tom@chainflip.io>
2022-05-24 14:40:55 +0200

    chore: useless runtime upgrade for soundcheck two (#1720)

--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -102,7 +102,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("chainflip-node"),
 	impl_name: create_runtime_str!("chainflip-node"),
 	authoring_version: 1,
-	spec_version: 111,
+	spec_version: 112,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
