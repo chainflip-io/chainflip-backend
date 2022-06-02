d9d5136d7c146acc20115e329ec75c036bab2a59
morelazers <tom@chainflip.io>
2022-06-02 16:30:24 +0200

    fix: forgot script updates

--- state-chain/scripts/benchmark-all.sh
+++ state-chain/scripts/benchmark-all.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
-[[ -z $DEBUG ]] && set -x
+# Binary location
+if [[ -z "$1" ]];
+then
+  BINARY=./target/release/chainflip-node
+else
+  BINARY=$1
+fi
 # Number of steps across component ranges
 STEPS=20
 # Number of times we repeat a benchmark
 REPEAT=10
 # Template file by which we genreate the weight files
 TEMPLATE=state-chain/chainflip-weight-template.hbs
-echo "Build the state-chain node with benchmark features enabled..."
-cargo build --release --features runtime-benchmarks
 echo "Executing benchmarks..."
 
 # TODO: implement benchmarking for threshold
@@ -16,7 +20,7 @@ pallets=$(ls state-chain/pallets | grep -v threshold-signature)
 for pallet in $pallets ; do
   echo "Running benchmark for: $pallet"
   pallet_fmt="pallet_$(echo $pallet|tr "-" "_")"
-  ./target/release/chainflip-node benchmark pallet \
+  $BINARY benchmark pallet \
     --pallet "$pallet_fmt" \
     --extrinsic '*' \
     --output "state-chain/pallets/$pallet/src/weights.rs" \
@@ -24,7 +28,6 @@ for pallet in $pallets ; do
     --steps="$STEPS" \
     --repeat="$REPEAT" \
     --template="$TEMPLATE"
-  git add -p "state-chain/pallets/$pallet/src/weights.rs"
 done
 
 echo "Benchmarking was succesful! - Don't forget to commit your accepted changes ;-)"
