8ee2e4732ba84e6dfc821732918e464c2d3f2232
Assem Hasna <asem.hasna@gmail.com>
2022-06-11 10:48:09 +0200

    chore: add docker to runner 🐳

--- benchmarks-runner/ec2.tf
+++ benchmarks-runner/ec2.tf
@@ -14,6 +14,19 @@ module "ec2_instance" {
   user_data                   = <<-EOT
     #!/bin/bash
     sudo apt-get -y update
+    sudo apt-get -y install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get -y update
+    sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    sudo usermod -aG docker ubuntu
+    newgrp docker
     cd /home/ubuntu
     mkdir actions-runner && cd actions-runner
     curl -o actions-runner-linux-x64-2.292.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.292.0/actions-runner-linux-x64-2.292.0.tar.gz
