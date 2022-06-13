07c3943f344a0202357eab6fee98207f7c7b71c1
Assem Hasna <asem.hasna@gmail.com>
2022-06-13 08:26:11 +0200

    chore: move terraform files to platform repo 🧹

--- benchmarks-runner/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "chainflip-terraform-workloads-state"
-    dynamodb_table = "chainflip-terraform-workloads-lock"
-    key            = "benchmark-github-runners/terraform.tfstate"
-    region         = "eu-central-1"
-    encrypt        = true
-  }
-}
--- benchmarks-runner/output.tf
+++
@@ -1,6 +0,0 @@
-output "ec2_public_ip" {
-  value = module.ec2_instance.public_ip
-}
-output "ec2_id" {
-  value = module.ec2_instance.id
-}
