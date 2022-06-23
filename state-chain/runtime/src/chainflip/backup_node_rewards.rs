e5c0093cffff2e41327adf558c990a9e0da7f3da
Ramiz Siddiqui <ramiz@chainflip.io>
2022-06-23 15:51:44 +0200

    feat: test for backup rewards calculation

--- state-chain/runtime/src/chainflip/backup_node_rewards.rs
+++ state-chain/runtime/src/chainflip/backup_node_rewards.rs
@@ -66,3 +66,108 @@ where
 	}
 	rewards
 }
+
+fn abs_difff(a: u128, b: u128) -> u128 {
+	if a > b {
+		a - b
+	} else {
+		b - a
+	}
+}
+
+#[test]
+fn test_example_calculations() {
+	let test_backup_nodes: Vec<(u128, u128)> = vec![
+		(1, 15000000),
+		(2, 12000000),
+		(3, 11760000),
+		(4, 11524800),
+		(5, 11294304),
+		(6, 11068418),
+		(7, 10847050),
+		(8, 10630109),
+		(9, 10417506),
+		(10, 10209156),
+		(11, 10004973),
+		(12, 9804874),
+		(13, 9608776),
+		(14, 9416601),
+		(15, 9228269),
+		(16, 9043703),
+		(17, 8862829),
+		(18, 8685573),
+		(19, 8511861),
+		(20, 8341624),
+		(21, 8174791),
+		(22, 8011296),
+		(23, 7851070),
+		(24, 7694048),
+		(25, 7540167),
+		(26, 7389364),
+		(27, 7241577),
+		(28, 7096745),
+		(29, 6954810),
+		(30, 6815714),
+		(31, 6679400),
+		(32, 6545812),
+		(33, 6414896),
+		(34, 6286598),
+		(35, 6160866),
+		(36, 6037648),
+		(37, 5916895),
+		(38, 5798558),
+		(39, 5682586),
+		(40, 5568935),
+		(41, 5457556),
+		(42, 5348405),
+		(43, 5241437),
+		(44, 5136608),
+		(45, 5033876),
+		(46, 4933198),
+		(47, 4834534),
+		(48, 4737844),
+		(49, 4643087),
+		(50, 4550225),
+	];
+
+	let mut backup_rewards: Vec<u128> = vec![
+		3408412, 3408412, 3408412, 3408412, 3408412, 3408412, 3314286, 3183040, 3056992, 2935935,
+		2819672, 2708013, 2600776, 2497785, 2398873, 2303877, 2212644, 2125023, 2040872, 1960054,
+		1882435, 1807891, 1736298, 1667541, 1601506, 1538087, 1477179, 1418682, 1362502, 1308547,
+		1256729, 1206962, 1159167, 1113264, 1069178, 1026839, 986176, 947124, 909618, 873597,
+		839002, 805778, 773869, 743224, 713792, 685526, 658379, 632307, 607268, 583220,
+	];
+
+	const MUL_FACTOR: u128 = 100000;
+
+	let test_backup_nodes = test_backup_nodes
+		.into_iter()
+		.map(|(node, reward)| (node, reward * MUL_FACTOR))
+		.collect();
+
+	const MAB: u128 = 11000000 * MUL_FACTOR;
+	const BLOCKSPERYEAR: u128 = 14400 * 356;
+	const BACKUP_EMISSIONS_CAP_PER_BLOCK: u128 = 90_000_000 * MUL_FACTOR / BLOCKSPERYEAR;
+	const AUTHORITY_EMISSIONS_PER_BLOCK: u128 = 900_000_000 * MUL_FACTOR / BLOCKSPERYEAR;
+	const AUTHORITY_COUNT: u128 = 150;
+
+	let mut calculated_rewards = calculate_backup_rewards(
+		test_backup_nodes,
+		MAB,
+		BLOCKSPERYEAR,
+		BACKUP_EMISSIONS_CAP_PER_BLOCK,
+		AUTHORITY_EMISSIONS_PER_BLOCK,
+		AUTHORITY_COUNT,
+	);
+
+	for _ in 0..backup_rewards.len() {
+		assert!(
+			abs_difff(
+				calculated_rewards.pop().unwrap().1 / MUL_FACTOR,
+				backup_rewards.pop().unwrap()
+			) <= 3_u128
+		)
+	}
+	assert!(calculated_rewards.pop().is_none());
+	assert!(backup_rewards.pop().is_none());
+}
