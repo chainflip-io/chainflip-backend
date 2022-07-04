f39c0809bb1334f7aff00604e4c040f139edea13
Daniel <daniel@chainflip.io>
2022-07-04 15:35:18 +0200

    fix: use expect for bond amount

--- state-chain/pallets/cf-auction/src/auction_resolver.rs
+++ state-chain/pallets/cf-auction/src/auction_resolver.rs
@@ -79,7 +79,10 @@ impl SetSizeMaximisingAuctionResolver {
 			.into_iter()
 			.map(Into::into)
 			.collect();
-		let bond = auction_candidates.last().map(|&(_, amount)| amount).unwrap_or_else(Zero::zero);
+		let bond = auction_candidates
+			.last()
+			.map(|&(_, amount)| amount)
+			.expect("Can't run auction with no candidates, and candidates must be staked > 0.");
 		let winners = auction_candidates.into_iter().map(|(id, _)| id).collect();
 
 		Ok(AuctionOutcome { winners, losers, bond })
