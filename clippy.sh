1b4f421f1776adc09b569300d81ea09a71a02d07
Lars Lubkoll <lars@chainflip.io>
2022-07-13 10:22:03 +0000

    chore: Reuse clippy.sh in pre-commit hook

--- clippy.sh
+++ clippy.sh
@@ -1,2 +1,6 @@
+cat <<\EOF
+Running: cargo clippy --all-targets -- -D warnings -A clippy::boxed_local -A clippy::nonstandard_macro_braces
+EOF
+
 set -e
 cargo clippy --all-targets -- -D warnings -A clippy::boxed_local -A clippy::nonstandard_macro_braces
