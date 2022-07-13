33a69c45940ffdbd20dd2fea851135f1ce622f75
Jan Börner <jan@chainflip.io>
2022-07-13 14:53:25 +0200

    chore: updated test to proof happy path behaviour

--- state-chain/pallets/cf-tokenholder-governance/src/tests.rs
+++ state-chain/pallets/cf-tokenholder-governance/src/tests.rs
@@ -13,9 +13,8 @@ const COMM_KEY_PROPOSAL: [u8; 32] = [1u8; 32];
 #[test]
 fn update_gov_key_via_onchain_proposal() {
     new_test_ext().execute_with(|| {
-        // Try to bake a not existing proposal
-        // assert_noop!(TokenholderGovernance::back_proposal(Origin::signed(ALICE), Proposal::SetGovernanceKey(GOV_KEY_PROPOSAL)), Error::<Test>::ProposalDosentExists);
-        // Submit a proposal
+        VotingPeriod::<Test>::set(10);
+        EnactmentDelay::<Test>::set(20);
         assert_ok!(TokenholderGovernance::submit_proposal(Origin::signed(ALICE), Proposal::SetGovernanceKey(GOV_KEY_PROPOSAL)));
         assert!(Proposals::<Test>::contains_key(<frame_system::Pallet<Test>>::block_number() + VotingPeriod::<Test>::get()));
         // Back a proposal
@@ -27,6 +26,9 @@ fn update_gov_key_via_onchain_proposal() {
         assert_ok!(TokenholderGovernance::back_proposal(Origin::signed(CHARLES), Proposal::SetGovernanceKey(GOV_KEY_PROPOSAL)));
         // Jump to the block in which we expect the proposal
         TokenholderGovernance::on_initialize(<frame_system::Pallet<Test>>::block_number() + VotingPeriod::<Test>::get());
+        // Expect the proposal to be moved to the enactment stage
         assert!(GovKeyUpdateAwaitingEnactment::<Test>::get().is_some());
+        TokenholderGovernance::on_initialize(<frame_system::Pallet<Test>>::block_number() + EnactmentDelay::<Test>::get());
+        assert!(GovKeyUpdateAwaitingEnactment::<Test>::get().is_none());
     });
 }
