76bdf7e66b22916e034b503e6bacd6b58bcc58da
Thomas Burton <tomjohnburton@gmail.com>
2022-07-20 18:31:33 +0200

    chore: update values

--- state-chain/node/src/chain_spec/network_env.rs
+++ state-chain/node/src/chain_spec/network_env.rs
@@ -2,15 +2,15 @@ use super::*;
 
 /// Environment variables for PARADISE public testnet.
 pub const PARADISE: StateChainEnvironment = StateChainEnvironment {
-	flip_token_address: hex_literal::hex!("Cc36D1fe201d656b6d1733c16B62AD75265dDe9E"),
-	stake_manager_address: hex_literal::hex!("4351C1BbF333DeB6589972407aAc9f4cD59609eF"),
-	key_manager_address: hex_literal::hex!("cf2975b417fFA6f341e77811248D732A29b498d1"),
+	flip_token_address: hex_literal::hex!("9F690f3B12700538f3872AfF504016a546E85c78"),
+	stake_manager_address: hex_literal::hex!("41f00272Ac87fFc4c1fd4B3D6E8A932FC423cF80"),
+	key_manager_address: hex_literal::hex!("4DBAf1eE163Cfe78F544D00Ce6AEB2bFb590dA75"),
 	ethereum_chain_id: 5, // GOERLI
 	eth_init_agg_key: hex_literal::hex!(
-		"032bf2c3d3308f53437c84be18ac3966ded07e50a599c4698dc7ca8dbe8f1116bc"
+		"0273984947f2a25bab820524aca9e71ffda6e928d731ae308e725feddeeb7c5123"
 	),
 	genesis_stake_amount: 50_000 * 10u128.pow(18),
-	ethereum_deployment_block: 7260132,
+	ethereum_deployment_block: 7260444,
 	eth_block_safety_margin: 6,
 	max_ceremony_stage_duration: 300,
 	min_stake: 1_000_000_000_000_000_000_000,
