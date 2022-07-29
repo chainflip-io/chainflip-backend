8c78ed3f9ea682cb65ab90e7803e7bf5c0419dd2
Daniel <daniel@chainflip.io>
2022-07-29 16:36:29 +0200

    test: witness decomposition

--- state-chain/runtime/src/chainflip/decompose_recompose.rs
+++ state-chain/runtime/src/chainflip/decompose_recompose.rs
@@ -52,8 +52,83 @@ impl WitnessDataExtraction for Call {
 		}
 	}
 }
-				log::trace!("No witness data injection for call {:?}", self);
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::{Origin, Validator, Witnesser};
+	use cf_chains::{eth::TrackedData, Chain, Ethereum};
+	use cf_traits::EpochInfo;
+	use frame_support::{assert_ok, Hashable};
+	use pallet_cf_witnesser::CallHash;
+	use sp_std::iter;
+
+	fn eth_chain_tracking_call_with_fee(priority_fee: <Ethereum as Chain>::ChainAmount) -> Call {
+		Call::EthereumChainTracking(
+			pallet_cf_chain_tracking::Call::<Runtime, EthereumInstance>::update_chain_state {
+				state: TrackedData { block_height: 1_000, base_fee: 40, priority_fee },
 			},
-		}
+		)
+	}
+
+	#[test]
+	fn test_priority_fee_median() {
+		let [mut threshold_call, mut calls @ ..] =
+			[1, 100, 10, 10, 10, 10].map(eth_chain_tracking_call_with_fee);
+
+		let call_hashes = calls.iter().map(|call| CallHash(call.blake2_256())).collect::<Vec<_>>();
+		assert!(
+			!iter::zip(call_hashes.iter(), call_hashes.iter().skip(1)).all(|(a, b)| a == b),
+			"Call hashes should be different before extraction."
+		);
+
+		let mut extracted_data =
+			calls.iter_mut().map(|call| call.extract().unwrap()).collect::<Vec<_>>();
+
+		let call_hashes = calls.iter().map(|call| CallHash(call.blake2_256())).collect::<Vec<_>>();
+		assert!(
+			iter::zip(call_hashes.iter(), call_hashes.iter().skip(1)).all(|(a, b)| a == b),
+			"Call hashes should all be equal after extraction."
+		);
+
+		threshold_call.combine_and_inject(&mut extracted_data[..]);
+
+		assert_eq!(threshold_call, eth_chain_tracking_call_with_fee(10));
+	}
+
+	#[test]
+	fn test_priority_fee_witnessing() {
+		frame_support::sp_io::TestExternalities::new_empty().execute_with(|| {
+			assert!(
+				pallet_cf_chain_tracking::ChainState::<Runtime, EthereumInstance>::get().is_none()
+			);
+
+			let calls = [1, 100, 10, 10, 10, 10].map(eth_chain_tracking_call_with_fee);
+
+			let authorities = (0..calls.len()).map(|i| [i as u8; 32].into()).collect::<Vec<_>>();
+			pallet_cf_validator::CurrentEpoch::<Runtime>::put(1);
+			pallet_cf_validator::CurrentAuthorities::<Runtime>::put(&authorities);
+			pallet_cf_validator::EpochAuthorityCount::<Runtime>::insert(
+				<Validator as EpochInfo>::epoch_index(),
+				authorities.len() as u32,
+			);
+
+			for (index, authority_id) in authorities.into_iter().enumerate() {
+				pallet_cf_validator::AuthorityIndex::<Runtime>::insert(
+					<Validator as EpochInfo>::epoch_index(),
+					&authority_id,
+					index as u32,
+				);
+				assert_ok!(Witnesser::witness(
+					Origin::signed(authority_id),
+					Box::new(calls[index].clone())
+				));
+			}
+
+			assert_eq!(
+				pallet_cf_chain_tracking::ChainState::<Runtime, EthereumInstance>::get().unwrap(),
+				TrackedData { block_height: 1_000, base_fee: 40, priority_fee: 10 }
+			);
+		})
 	}
 }
