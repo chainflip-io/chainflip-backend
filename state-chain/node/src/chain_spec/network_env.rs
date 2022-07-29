e8fad21647fd3ec2cbd72474cbef25086f5fa293
kylezs <kyle@chainflip.io>
2022-07-29 18:25:04 +0200

    Release/0.4.0 rc4 (#1937)
    Co-authored-by: morelazers <tom@chainflip.io>
    Co-authored-by: Thomas Burton <tomjohnburton@gmail.com>
    Co-authored-by: dandanlen <3168260+dandanlen@users.noreply.github.com>

--- state-chain/node/src/chain_spec/network_env.rs
+++ state-chain/node/src/chain_spec/network_env.rs
@@ -2,15 +2,15 @@ use super::*;
 
 /// Environment variables for PARADISE public testnet.
 pub const PARADISE: StateChainEnvironment = StateChainEnvironment {
-	flip_token_address: hex_literal::hex!("Aa07486C20F73fF4309495411927E6AE7C884DBa"),
-	stake_manager_address: hex_literal::hex!("D4185915BD9533575207DCfdEb6FDeF798B095d3"),
-	key_manager_address: hex_literal::hex!("6699A372477f62caA0B0e3465CDA30E789a8F815"),
+	flip_token_address: hex_literal::hex!("9F690f3B12700538f3872AfF504016a546E85c78"),
+	stake_manager_address: hex_literal::hex!("41f00272Ac87fFc4c1fd4B3D6E8A932FC423cF80"),
+	key_manager_address: hex_literal::hex!("4DBAf1eE163Cfe78F544D00Ce6AEB2bFb590dA75"),
 	ethereum_chain_id: 5, // GOERLI
 	eth_init_agg_key: hex_literal::hex!(
-		"02071915b34b466951fa08709724a40cc4ad69fbdf3503b372d218654eb0cff592"
+		"0273984947f2a25bab820524aca9e71ffda6e928d731ae308e725feddeeb7c5123"
 	),
 	genesis_stake_amount: 50_000 * 10u128.pow(18),
-	ethereum_deployment_block: 7230032,
+	ethereum_deployment_block: 7260444,
 	eth_block_safety_margin: 6,
 	max_ceremony_stage_duration: 300,
 	min_stake: 1_000_000_000_000_000_000_000,
