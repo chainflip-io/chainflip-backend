32891f032cbd89bc723defdbcae25b18c8962b8f
Alastair Holmes <holmes.alastair@outlook.com>
2022-08-02 18:03:15 +0200

    cargo fmt --all

--- utilities/src/with_std.rs
+++ utilities/src/with_std.rs
@@ -21,7 +21,7 @@ pub fn make_periodic_tick(duration: Duration, yield_immediately: bool) -> tokio:
 
 #[cfg(test)]
 mod tests_make_periodic_tick {
-    use crate::{assert_ok, assert_err};
+    use crate::{assert_err, assert_ok};
 
     use super::*;
 
