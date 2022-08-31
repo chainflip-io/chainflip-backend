cca09cbaebd5f9cd963932c794b8d233f95a8e82
Daniel <daniel@chainflip.io>
2022-08-31 13:40:41 +0200

    fix: remove unused mod declaration

--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -2,9 +2,6 @@
 // `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
 #![recursion_limit = "256"]
 
-#[cfg(test)]
-mod tests;
-
 pub mod chainflip;
 pub mod constants;
 pub mod runtime_apis;
