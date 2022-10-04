5ae78a4bf6bd6f6470e3bb27ca9b979758114e9c
kylezs <zsembery.kyle@gmail.com>
2022-10-04 13:24:23 +0200

    chore: move genesis tests to own file

--- state-chain/cf-integration-tests/src/lib.rs
+++ state-chain/cf-integration-tests/src/lib.rs
@@ -7,7 +7,7 @@ mod signer_nomination;
 mod mock_runtime;
 
 mod authorities;
-
+mod genesis;
 mod staking;
 
 use frame_support::{assert_noop, assert_ok, traits::OnInitialize};
@@ -46,112 +46,6 @@ pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Pu
 	TPublic::Pair::from_string(&format!("//{}", seed), None)
 		.expect("static values are valid; qed")
 		.public()
-}
-
-mod genesis {
-	use sp_std::collections::btree_set::BTreeSet;
-
-	use crate::mock_runtime::ExtBuilder;
-
-	use super::*;
-	use cf_primitives::ChainflipAccountState;
-	use cf_traits::{
-		ChainflipAccount, ChainflipAccountStore, EpochInfo, QualifyNode, StakeTransfer,
-	};
-	pub const GENESIS_BALANCE: FlipBalance = TOTAL_ISSUANCE / 100;
-
-	pub fn default() -> ExtBuilder {
-		ExtBuilder::default()
-			.accounts(vec![
-				(AccountId::from(ALICE), GENESIS_BALANCE),
-				(AccountId::from(BOB), GENESIS_BALANCE),
-				(AccountId::from(CHARLIE), GENESIS_BALANCE),
-			])
-			.root(AccountId::from(ERIN))
-	}
-
-	#[test]
-	fn state_of_genesis_is_as_expected() {
-		default().build().execute_with(|| {
-			// Confirmation that we have our assumed state at block 1
-			assert_eq!(Flip::total_issuance(), TOTAL_ISSUANCE, "we have issued the total issuance");
-
-			let accounts = [AccountId::from(CHARLIE), AccountId::from(BOB), AccountId::from(ALICE)];
-
-			for account in accounts.iter() {
-				assert_eq!(
-					Flip::staked_balance(account),
-					GENESIS_BALANCE,
-					"the account has its stake"
-				);
-			}
-
-			assert_eq!(Validator::bond(), GENESIS_BALANCE);
-			assert_eq!(
-				Validator::current_authorities().iter().collect::<BTreeSet<_>>(),
-				accounts.iter().collect::<BTreeSet<_>>(),
-				"the validators are those expected at genesis"
-			);
-
-			assert_eq!(
-				Validator::blocks_per_epoch(),
-				0,
-				"epochs will not rotate automatically from genesis"
-			);
-
-			let current_epoch = Validator::current_epoch();
-
-			for account in accounts.iter() {
-				assert!(
-					Validator::authority_index(current_epoch, account).is_some(),
-					"authority is present in lookup"
-				);
-			}
-
-			for account in accounts.iter() {
-				assert!(Reputation::is_qualified(account), "Genesis nodes start online");
-			}
-
-			assert_eq!(Emissions::last_supply_update_block(), 0, "no emissions");
-
-			assert_eq!(Validator::ceremony_id_counter(), 0, "no key generation requests");
-
-			assert_eq!(
-				pallet_cf_environment::GlobalSignatureNonce::<Runtime>::get(),
-				0,
-				"Global signature nonce should be 0"
-			);
-
-			assert!(Governance::members().contains(&AccountId::from(ERIN)), "expected governor");
-			assert_eq!(Governance::proposal_id_counter(), 0, "no proposal for governance");
-
-			assert_eq!(
-				Emissions::current_authority_emission_inflation(),
-				CURRENT_AUTHORITY_EMISSION_INFLATION_PERBILL,
-				"invalid emission inflation for authorities"
-			);
-
-			assert_eq!(
-				Emissions::backup_node_emission_inflation(),
-				BACKUP_NODE_EMISSION_INFLATION_PERBILL,
-				"invalid emission inflation for backup authorities"
-			);
-
-			for account in accounts.iter() {
-				assert_eq!(
-					Reputation::reputation(account),
-					pallet_cf_reputation::ReputationTracker::<Runtime>::default(),
-					"authority shouldn't have reputation points"
-				);
-			}
-
-			for account in accounts.iter() {
-				let account_data = ChainflipAccountStore::<Runtime>::get(account);
-				// TODO: Check historical epochs
-				assert_eq!(ChainflipAccountState::CurrentAuthority, account_data.state);
-			}
-		});
-	}
 }
 
 // The minimum number of blocks a vault rotation should last
