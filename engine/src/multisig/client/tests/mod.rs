659aff56092518e0f873970f2afe1b42eadbf813
Jamie Ford <Jamie@chainflip.io>
2022-10-10 14:52:33 +1100

    refactor: show default ceremony ids as incremental

--- engine/src/multisig/client/tests/mod.rs
+++ engine/src/multisig/client/tests/mod.rs
@@ -22,12 +22,14 @@ use state_chain_runtime::AccountId;
 pub const DEFAULT_KEYGEN_SEED: [u8; 32] = [8; 32];
 pub const DEFAULT_SIGNING_SEED: [u8; 32] = [4; 32];
 
-/// Default ceremony ids
-/// We start at id 1 because the latests ceremony id starts at 0 for tests (making 0 invalid),
+// Default ceremony ids used many unit tests.
+/// The initial latest ceremony id starts at 0,
+/// so the first ceremony request must have a ceremony id of 1.
 /// Also the SC will never send a ceremony request at id 0.
 pub const INITIAL_LATEST_CEREMONY_ID: u64 = 0;
-pub const DEFAULT_KEYGEN_CEREMONY_ID: u64 = 1;
-pub const DEFAULT_SIGNING_CEREMONY_ID: u64 = 2;
+// Ceremony ids must be consecutive.
+pub const DEFAULT_KEYGEN_CEREMONY_ID: u64 = INITIAL_LATEST_CEREMONY_ID + 1;
+pub const DEFAULT_SIGNING_CEREMONY_ID: u64 = DEFAULT_KEYGEN_CEREMONY_ID + 1;
 
 lazy_static! {
     pub static ref ACCOUNT_IDS: Vec<AccountId> = [1, 2, 3, 4]
