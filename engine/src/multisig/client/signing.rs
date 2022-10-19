02f400810ee388f7730f29a5365f198e536d0027
Jamie Ford <jamie@chainflip.io>
2022-10-19 12:36:09 +1100

    refactor: move tests to be children (#2301)

--- engine/src/multisig/client/signing.rs
+++ engine/src/multisig/client/signing.rs
@@ -1,11 +1,29 @@
-pub mod frost;
-pub mod frost_stages;
+mod signing_data;
+mod signing_detail;
+mod signing_stages;
+
+#[cfg(test)]
+mod tests;
 
 use std::sync::Arc;
 
 use crate::multisig::{crypto::ECPoint, MessageHash};
 
 use super::common::KeygenResult;
+
+pub use signing_data::{
+	Comm1, LocalSig3, SigningCommitment, SigningData, VerifyComm2, VerifyLocalSig4,
+};
+
+pub use signing_detail::generate_schnorr_response;
+
+pub use signing_stages::AwaitCommitments1;
+
+#[cfg(test)]
+pub use signing_data::{gen_signing_data_stage1, gen_signing_data_stage4};
+
+#[cfg(test)]
+pub use signing_detail::get_lagrange_coeff;
 
 /// Data common for signing stages
 #[derive(Clone)]
