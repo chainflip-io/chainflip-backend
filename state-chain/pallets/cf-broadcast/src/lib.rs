6ee8726ec38c4bae5667caa4a4843803dc570501
Daniel <daniel@chainflip.io>
2022-10-28 12:04:41 +0200

    hack: DO NOT MERGE

--- state-chain/pallets/cf-broadcast/src/lib.rs
+++ state-chain/pallets/cf-broadcast/src/lib.rs
@@ -71,7 +71,7 @@ pub mod pallet {
 	use cf_chains::benchmarking_value::BenchmarkValue;
 	use cf_traits::{AccountRoleRegistry, KeyProvider};
 	use frame_support::{ensure, pallet_prelude::*, traits::EnsureOrigin};
-	use frame_system::pallet_prelude::*;
+	use frame_system::{ensure_root, pallet_prelude::*};
 
 	/// Type alias for the instance's configured SignedTransaction.
 	pub type SignedTransactionFor<T, I> =
@@ -588,6 +588,19 @@ pub mod pallet {
 			}
 			Self::deposit_event(Event::<T, I>::BroadcastSuccess(broadcast_id));
 			Ok(().into())
+		}
+
+		#[pallet::weight(0)]
+		pub fn stress_test(origin: OriginFor<T>, how_many: u32) -> DispatchResult {
+			ensure_root(origin)?;
+
+			let payload = PayloadFor::<T, I>::decode(&mut &[0xcf; 32][..])
+				.map_err(|_| Error::<T, I>::InvalidPayload)?;
+			for _ in 0..how_many {
+				T::ThresholdSigner::request_signature(payload.clone());
+			}
+
+			Ok(())
 		}
 	}
 }
--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -126,10 +126,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("chainflip-node"),
 	impl_name: create_runtime_str!("chainflip-node"),
 	authoring_version: 1,
-	spec_version: 2,
+	spec_version: 3,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 1,
+	transaction_version: 2,
 	state_version: 1,
 };
 
