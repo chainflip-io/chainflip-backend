c92d7e69c51148ff357c1a62f14c200d5b971de9
kylezs <kyle@chainflip.io>
2022-11-09 22:40:54 +1100

    refactor: initial threshold sig refactors for multi-vault (#2426)
    * chore: remove dead VaultsRotated event
    
    * feat: allow nominee lookup via vault epoch index
    
    * feat: use vault epoch instead of current epoch
    
    * doc: fix comment
    
    * chore: remove unused errors
    
    * chore: remove unused KeygenAborted
    
    * refactor: remove redundant option in inner_request_signature
    
    * refactor: tie current_key_id and epoch index together
    
    This was a source of potential bugs. Namely, it was possible to say "sign with key X, but the current authorities", which doesn't make sense. The key should always be tied to the authorities that hold that key.
    
    * refactor: flatten RequestContext into CeremonyContext
    
    * refactor: use explicit enum rather than implicit Option
    
    * refactor: remove LiveCeremonies storage item :rocket:
    
    * doc: rephrase doc comment on CurrentKeyholdersEpoch
    
    * refactor: deduplicate set vault for epoch

--- state-chain/pallets/cf-threshold-signature/src/mock.rs
+++ state-chain/pallets/cf-threshold-signature/src/mock.rs
@@ -1,13 +1,14 @@
 use std::{collections::BTreeSet, marker::PhantomData};
 
 use crate::{
-	self as pallet_cf_threshold_signature, CeremonyContext, CeremonyId, EnsureThresholdSigned,
-	LiveCeremonies, PalletOffence, PendingCeremonies, RequestContext, RequestId, RetryQueues,
+	self as pallet_cf_threshold_signature, CeremonyId, EnsureThresholdSigned, PalletOffence,
+	PendingCeremonies, RequestId, RetryQueues,
 };
 use cf_chains::{
 	mocks::{MockEthereum, MockThresholdSignature},
 	ChainCrypto,
 };
+use cf_primitives::EpochIndex;
 use cf_traits::{
 	mocks::{
 		ceremony_id_provider::MockCeremonyIdProvider, signer_nomination::MockNominator,
@@ -144,8 +145,8 @@ pub struct MockKeyProvider;
 impl cf_traits::KeyProvider<MockEthereum> for MockKeyProvider {
 	type KeyId = Vec<u8>;
 
-	fn current_key_id() -> Self::KeyId {
-		MOCK_AGG_KEY.into()
+	fn current_key_id_epoch_index() -> (Self::KeyId, EpochIndex) {
+		(MOCK_AGG_KEY.into(), Default::default())
 	}
 
 	fn current_key() -> <MockEthereum as ChainCrypto>::AggKey {
@@ -218,12 +219,9 @@ impl ExtBuilder {
 	pub fn with_request(mut self, message: &<MockEthereum as ChainCrypto>::Payload) -> Self {
 		self.ext.execute_with(|| {
 			// Initiate request
-			let request_id =
+			let (request_id, ceremony_id) =
 				<EthereumThresholdSigner as ThresholdSigner<_>>::request_signature(*message);
-			let (ceremony_id, attempt) =
-				EthereumThresholdSigner::live_ceremonies(request_id).unwrap();
 			let pending = EthereumThresholdSigner::pending_ceremonies(ceremony_id).unwrap();
-			assert_eq!(attempt, 0);
 			assert_eq!(
 				pending.remaining_respondents,
 				BTreeSet::from_iter(MockNominator::get_nominees().unwrap_or_default())
@@ -240,12 +238,9 @@ impl ExtBuilder {
 	) -> Self {
 		self.ext.execute_with(|| {
 			// Initiate request
-			let request_id =
+			let (request_id, ceremony_id) =
 				EthereumThresholdSigner::request_signature_with_callback(*message, callback_gen);
-			let (ceremony_id, attempt) =
-				EthereumThresholdSigner::live_ceremonies(request_id).unwrap();
 			let pending = EthereumThresholdSigner::pending_ceremonies(ceremony_id).unwrap();
-			assert_eq!(attempt, 0);
 			assert_eq!(
 				pending.remaining_respondents,
 				BTreeSet::from_iter(MockNominator::get_nominees().unwrap_or_default())
@@ -282,31 +277,9 @@ impl TestExternalitiesWithCheck {
 	/// Every ceremony should also have at least one retry scheduled.
 	pub fn do_consistency_check() {
 		let retries = BTreeSet::<_>::from_iter(RetryQueues::<Test, _>::iter_values().flatten());
-		PendingCeremonies::<Test, _>::iter().for_each(
-			|(
-				ceremony_id,
-				CeremonyContext {
-					request_context: RequestContext { request_id, attempt_count, .. },
-					..
-				},
-			)| {
-				assert_eq!(
-					LiveCeremonies::<Test, _>::get(request_id).unwrap(),
-					(ceremony_id, attempt_count)
-				);
-				assert!(retries.contains(&ceremony_id));
-			},
-		);
-		LiveCeremonies::<Test, _>::iter().for_each(
-			|(live_request_id, (live_ceremony_id, live_attempt_count))| {
-				let CeremonyContext::<Test, Instance1> {
-					request_context:
-						RequestContext::<Test, Instance1> { request_id, attempt_count, .. },
-					..
-				} = EthereumThresholdSigner::pending_ceremonies(live_ceremony_id).unwrap();
-				assert!((request_id, attempt_count) == (live_request_id, live_attempt_count));
-			},
-		);
+		PendingCeremonies::<Test, _>::iter().for_each(|(ceremony_id, _)| {
+			assert!(retries.contains(&ceremony_id));
+		});
 	}
 }
 
