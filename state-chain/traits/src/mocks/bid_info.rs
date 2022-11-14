f1f4e752df03614b36a829a7e8659935720f8153
Jan Börner <jan@chainflip.io>
2022-11-14 09:46:26 +0100

    fix: :bug: fixed clippy warnings

--- state-chain/traits/src/mocks/bid_info.rs
+++ state-chain/traits/src/mocks/bid_info.rs
@@ -10,7 +10,7 @@ pub struct MockBidInfo;
 impl BidInfo for MockBidInfo {
 	type Balance = u128;
 	fn get_min_backup_bid() -> Self::Balance {
-		Self::Balance::from(MIN_BID.with(|cell| cell.borrow().clone()))
+		MIN_BID.with(|cell| *cell.borrow())
 	}
 }
 
