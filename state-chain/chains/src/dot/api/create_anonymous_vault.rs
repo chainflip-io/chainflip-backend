da41a334c49ec8c2664fdd7965c134e68b38b6eb
Ramiz Siddiqui <97471826+ramizhasan111@users.noreply.github.com>
2022-11-21 11:31:13 +0100

    feat: add apicall for polkadot anonymous vault creation (#2483)

---
+++ state-chain/chains/src/dot/api/create_anonymous_vault.rs
@@ -0,0 +1,127 @@
+use codec::{Decode, Encode};
+use scale_info::TypeInfo;
+
+use crate::dot::{
+	Polkadot, PolkadotExtrinsicBuilder, PolkadotProxyType, PolkadotPublicKey,
+	PolkadotReplayProtection, PolkadotRuntimeCall, ProxyCall,
+};
+
+use crate::{ApiCall, ChainCrypto};
+use sp_std::vec::Vec;
+
+use sp_runtime::{traits::IdentifyAccount, MultiSigner, RuntimeDebug};
+
+/// Represents all the arguments required to build the call to fetch assets for all given intent
+/// ids.
+#[derive(Encode, Decode, TypeInfo, Clone, RuntimeDebug, PartialEq, Eq)]
+pub struct CreateAnonymousVault {
+	/// The handler for creating and signing polkadot extrinsics
+	pub extrinsic_handler: PolkadotExtrinsicBuilder,
+	/// The proxy account public key that control the anonymous vault
+	pub proxy_key: PolkadotPublicKey,
+}
+
+impl CreateAnonymousVault {
+	pub fn new_unsigned(
+		replay_protection: PolkadotReplayProtection,
+		proxy_key: PolkadotPublicKey,
+	) -> Self {
+		let mut calldata = Self {
+			extrinsic_handler: PolkadotExtrinsicBuilder::new_empty(
+				replay_protection,
+				MultiSigner::Sr25519(proxy_key.0).into_account(),
+			),
+			proxy_key,
+		};
+		// create and insert polkadot runtime call
+		calldata
+			.extrinsic_handler
+			.insert_extrinsic_call(calldata.extrinsic_call_polkadot());
+		// compute and insert the threshold signature payload
+		calldata.extrinsic_handler.insert_threshold_signature_payload().expect(
+			"This should not fail since SignedExtension of the SignedExtra type is implemented",
+		);
+
+		calldata
+	}
+
+	fn extrinsic_call_polkadot(&self) -> PolkadotRuntimeCall {
+		PolkadotRuntimeCall::Proxy(ProxyCall::create_pure {
+			proxy_type: PolkadotProxyType::Any,
+			delay: 0,
+			index: 0,
+		})
+	}
+}
+
+impl ApiCall<Polkadot> for CreateAnonymousVault {
+	fn threshold_signature_payload(&self) -> <Polkadot as ChainCrypto>::Payload {
+		self
+		.extrinsic_handler
+		.signature_payload
+		.clone()
+		.expect("This should never fail since the apicall created above with new_unsigned() ensures it exists")
+	}
+
+	fn signed(mut self, signature: &<Polkadot as ChainCrypto>::ThresholdSignature) -> Self {
+		self.extrinsic_handler
+			.insert_signature_and_get_signed_unchecked_extrinsic(signature.clone());
+		self
+	}
+
+	fn chain_encoded(&self) -> Vec<u8> {
+		self.extrinsic_handler.signed_extrinsic.clone().unwrap().encode()
+	}
+
+	fn is_signed(&self) -> bool {
+		self.extrinsic_handler.is_signed().unwrap_or(false)
+	}
+}
+
+#[cfg(test)]
+mod test_create_anonymous_vault {
+
+	use super::*;
+	use crate::dot::{sr25519::Pair, NONCE_2, RAW_SEED_2, WESTEND_CONFIG};
+	use sp_core::{crypto::Pair as TraitPair, Hasher};
+	use sp_runtime::traits::BlakeTwo256;
+
+	#[ignore]
+	#[test]
+	fn create_test_api_call() {
+		let keypair_proxy: Pair = <Pair as TraitPair>::from_seed(&RAW_SEED_2);
+
+		let create_anonymous_vault = CreateAnonymousVault::new_unsigned(
+			PolkadotReplayProtection::new(NONCE_2, 0, WESTEND_CONFIG),
+			PolkadotPublicKey(keypair_proxy.public()),
+		);
+
+		println!(
+			"CallHash: 0x{}",
+			create_anonymous_vault
+				.extrinsic_handler
+				.extrinsic_call
+				.clone()
+				.unwrap()
+				.using_encoded(|encoded| hex::encode(BlakeTwo256::hash(encoded)))
+		);
+		println!(
+			"Encoded Call: 0x{}",
+			hex::encode(
+				create_anonymous_vault
+					.extrinsic_handler
+					.extrinsic_call
+					.clone()
+					.unwrap()
+					.encode()
+			)
+		);
+
+		let create_anonymous_vault = create_anonymous_vault
+			.clone()
+			.signed(&keypair_proxy.sign(&create_anonymous_vault.threshold_signature_payload().0));
+		assert!(create_anonymous_vault.is_signed());
+
+		println!("encoded extrinsic: 0x{}", hex::encode(create_anonymous_vault.chain_encoded()));
+	}
+}
