4ce3afaed4582ebf11303569212d2c3655630c2b
kylezs <zsembery.kyle@gmail.com>
2022-11-22 11:39:07 +0100

    feat: all_vault_rotator to take a second vault

--- state-chain/runtime/src/chainflip/all_vaults_rotator.rs
+++ state-chain/runtime/src/chainflip/all_vaults_rotator.rs
@@ -5,45 +5,45 @@ use core::marker::PhantomData;
 use cf_traits::{AsyncResult, VaultRotator, VaultStatus};
 use sp_std::collections::btree_set::BTreeSet;
 
-pub struct AllVaultRotator<A> {
-	_phantom: PhantomData<A>,
+pub struct AllVaultRotator<A, B> {
+	_phantom: PhantomData<(A, B)>,
 }
 
-impl<A> VaultRotator for AllVaultRotator<A>
+impl<A, B> VaultRotator for AllVaultRotator<A, B>
 where
 	A: VaultRotator,
+	B: VaultRotator<ValidatorId = A::ValidatorId>,
 {
 	type ValidatorId = A::ValidatorId;
 
 	/// Start all vault rotations with the provided `candidates`.
 	fn start(candidates: BTreeSet<Self::ValidatorId>) {
-		A::start(candidates)
+		A::start(candidates.clone());
+		B::start(candidates);
 	}
 
 	fn status() -> AsyncResult<VaultStatus<Self::ValidatorId>> {
-		let a_async_result = A::status();
+		let async_results = [A::status(), B::status()];
 
 		// if any of the inner rotations are void, then the overall vault rotation result is void.
-		if matches!(a_async_result, AsyncResult::Void) {
+		if async_results.iter().any(|item| matches!(item, AsyncResult::Void)) {
 			return AsyncResult::Void
 		}
 
-		let all_ready = a_async_result.is_ready();
-
 		// We must wait until all of these are ready before we do any action
-		if all_ready {
-			let all_results = [a_async_result.unwrap()];
-			if all_results.iter().all(|x| matches!(x, VaultStatus::KeygenComplete)) {
+		if async_results.iter().all(|item| matches!(item, AsyncResult::Ready(..))) {
+			let mut statuses = async_results.into_iter().map(AsyncResult::unwrap);
+
+			if statuses.all(|x| matches!(x, VaultStatus::KeygenComplete)) {
 				AsyncResult::Ready(VaultStatus::KeygenComplete)
-			} else if all_results.iter().all(|x| matches!(x, VaultStatus::RotationComplete)) {
+			} else if statuses.all(|x| matches!(x, VaultStatus::RotationComplete)) {
 				AsyncResult::Ready(VaultStatus::RotationComplete)
 			} else {
 				// We currently treat an offence in one vault rotation as bad as in all rotations.
 				// We may want to change it, but this is simplest for now.
 
 				AsyncResult::Ready(VaultStatus::Failed(
-					all_results
-						.into_iter()
+					statuses
 						.filter_map(|r| {
 							if let VaultStatus::Failed(offenders) = r {
 								Some(offenders)
@@ -62,6 +62,7 @@ where
 	}
 
 	fn activate() {
-		A::activate()
+		A::activate();
+		B::activate();
 	}
 }
