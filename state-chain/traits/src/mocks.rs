c42285d867a1e8dfff03a835fd0d5d1b32ca0248
Daniel <daniel@chainflip.io>
2022-11-22 14:56:08 +0100

    feat: ingress and egress mocks

--- state-chain/traits/src/mocks.rs
+++ state-chain/traits/src/mocks.rs
@@ -1,17 +1,19 @@
 #![cfg(feature = "std")]
 
-use codec::{Decode, Encode};
+use codec::{Decode, Encode, EncodeLike};
 use frame_support::{storage, StorageHasher, Twox64Concat};
 
 // pub mod broadcaster;
 pub mod account_role_registry;
 pub mod bid_info;
 pub mod ceremony_id_provider;
+pub mod egress_handler;
 pub mod ensure_origin_mock;
 pub mod ensure_witnessed;
 pub mod epoch_info;
 pub mod eth_environment_provider;
 pub mod eth_replay_protection_provider;
+pub mod ingress_handler;
 pub mod key_provider;
 pub mod offence_reporting;
 pub mod qualify_node;
@@ -35,6 +37,35 @@ trait MockPalletStorage {
 	fn take_storage<K: Encode, V: Decode + Sized>(store: &[u8], k: K) -> Option<V>;
 	fn put_value<V: Encode>(store: &[u8], v: V);
 	fn get_value<V: Decode + Sized>(store: &[u8]) -> Option<V>;
+	fn mutate_storage<
+		K: Encode,
+		E: EncodeLike<K>,
+		V: Encode + Decode + Sized,
+		R,
+		F: FnOnce(&mut Option<V>) -> R,
+	>(
+		store: &[u8],
+		k: &E,
+		f: F,
+	) -> R {
+		let mut storage = Self::get_storage(store, k);
+		let result = f(&mut storage);
+		if let Some(v) = storage {
+			Self::put_storage(store, k, v);
+		}
+		result
+	}
+	fn mutate_value<V: Encode + Decode + Sized, R, F: FnOnce(&mut Option<V>) -> R>(
+		store: &[u8],
+		f: F,
+	) -> R {
+		let mut storage = Self::get_value(store);
+		let result = f(&mut storage);
+		if let Some(v) = storage {
+			Self::put_value(store, v);
+		}
+		result
+	}
 }
 
 fn storage_key<K: Encode>(prefix: &[u8], store: &[u8], k: K) -> Vec<u8> {
---
+++ state-chain/traits/src/mocks/ingress_handler.rs
@@ -0,0 +1,118 @@
+use crate::{Chainflip, IngressApi};
+use cf_chains::{eth::assets::any, Chain, ForeignChain};
+use cf_primitives::{ForeignChainAddress, IntentId};
+use codec::{Decode, Encode};
+use scale_info::TypeInfo;
+use sp_std::marker::PhantomData;
+
+use super::{MockPallet, MockPalletStorage};
+
+pub struct MockIngressHandler<C, T>(PhantomData<(C, T)>);
+
+impl<C, T> MockPallet for MockIngressHandler<C, T> {
+	const PREFIX: &'static [u8] = b"MockIngressHandler";
+}
+
+enum SwapOrLp {
+	Swap,
+	Lp,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Encode, Decode, TypeInfo)]
+pub struct SwapIntent<C: Chain, T: Chainflip> {
+	ingress_address: ForeignChainAddress,
+	ingress_asset: <C as Chain>::ChainAsset,
+	egress_asset: any::Asset,
+	egress_address: ForeignChainAddress,
+	relayer_commission_bps: u16,
+	relayer_id: <T as frame_system::Config>::AccountId,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Encode, Decode, TypeInfo)]
+pub struct LpIntent<C: Chain, T: Chainflip> {
+	ingress_address: ForeignChainAddress,
+	ingress_asset: <C as Chain>::ChainAsset,
+	lp_account: <T as frame_system::Config>::AccountId,
+}
+
+impl<C: Chain, T: Chainflip> MockIngressHandler<C, T> {
+	fn get_new_intent(
+		swap_or_lp: SwapOrLp,
+		asset: <C as Chain>::ChainAsset,
+	) -> (IntentId, ForeignChainAddress) {
+		let intent_id = <Self as MockPalletStorage>::mutate_value(
+			match swap_or_lp {
+				SwapOrLp::Swap => b"SWAP_INTENT_ID",
+				SwapOrLp::Lp => b"LP_INTENT_ID",
+			},
+			|storage| {
+				let intent_id: IntentId = storage.unwrap_or_default();
+				let _ = storage.insert(intent_id + 1);
+				intent_id
+			},
+		);
+		(
+			intent_id,
+			match asset.into() {
+				ForeignChain::Ethereum => ForeignChainAddress::Eth([intent_id as u8; 20]),
+				ForeignChain::Polkadot => ForeignChainAddress::Dot([intent_id as u8; 32]),
+			},
+		)
+	}
+
+	pub fn get_liquidity_intents(
+	) -> Vec<(ForeignChainAddress, <C as Chain>::ChainAsset, <T as frame_system::Config>::AccountId)>
+	{
+		<Self as MockPalletStorage>::get_value(b"LP_INGRESS_INTENTS").unwrap_or_default()
+	}
+
+	pub fn get_swap_intents() -> Vec<SwapIntent<C, T>> {
+		<Self as MockPalletStorage>::get_value(b"SWAP_INGRESS_INTENTS").unwrap_or_default()
+	}
+}
+
+impl<C: Chain, T: Chainflip> IngressApi<C> for MockIngressHandler<C, T> {
+	type AccountId = <T as frame_system::Config>::AccountId;
+
+	fn register_liquidity_ingress_intent(
+		lp_account: Self::AccountId,
+		ingress_asset: <C as cf_chains::Chain>::ChainAsset,
+	) -> Result<
+		(cf_primitives::IntentId, cf_primitives::ForeignChainAddress),
+		sp_runtime::DispatchError,
+	> {
+		let (intent_id, ingress_address) = Self::get_new_intent(SwapOrLp::Lp, ingress_asset);
+		<Self as MockPalletStorage>::mutate_value(b"LP_INGRESS_INTENTS", |storage| {
+			storage.as_mut().unwrap_or(&mut vec![]).push(LpIntent::<C, T> {
+				ingress_address,
+				ingress_asset,
+				lp_account,
+			});
+		});
+		Ok((intent_id, ingress_address))
+	}
+
+	fn register_swap_intent(
+		ingress_asset: <C as Chain>::ChainAsset,
+		egress_asset: cf_primitives::Asset,
+		egress_address: cf_primitives::ForeignChainAddress,
+		relayer_commission_bps: u16,
+		relayer_id: Self::AccountId,
+	) -> Result<
+		(cf_primitives::IntentId, cf_primitives::ForeignChainAddress),
+		sp_runtime::DispatchError,
+	> {
+		let (intent_id, ingress_address) = Self::get_new_intent(SwapOrLp::Swap, ingress_asset);
+		<Self as MockPalletStorage>::mutate_value(b"SWAP_INGRESS_INTENTS", |storage| {
+			storage.as_mut().unwrap_or(&mut vec![]).push(SwapIntent::<C, T> {
+				ingress_address,
+				ingress_asset,
+				egress_asset,
+				egress_address,
+				relayer_commission_bps,
+				relayer_id,
+			})
+		});
+		Ok((intent_id, ingress_address))
+	}
+}
