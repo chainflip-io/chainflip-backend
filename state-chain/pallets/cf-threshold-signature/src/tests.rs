c5f3fd059393bcfa399f90c8b95fb11a4425e879
kylezs <kyle@chainflip.io>
2022-11-24 02:08:58 +1100

    feat: Multi Vaults - Decoupling of EthVault from Validator pallet (#2462)
    * feat: introduce key states
    
    * feat: initial multi vault rotator
    
    * feat: compiling without tests
    
    * feat: tests compiling
    
    * chore: remove unused mock
    
    * test: fix tests
    
    * chore: KeygenVerificationComplete -> KeygenComplete
    
    * refactor: simplify key states
    
    * chore: remove completed TODOs
    
    * format
    
    * fix: any void should return void
    
    * chore: clean up validator state transitions
    
    * chore: add todo
    
    * chore: create TODO
    
    * doc: clean up some comments
    
    * chore: get_vault_rotation_outcome -> vault_rotation_outcome
    
    * refactor: log event rather than return log message
    
    * chore: rotate externally -> activate key
    
    * chore: inline event
    
    * refactor: remove KeygensComplete state
    
    * chore: NewKeyActivated -> NewKeysActivated
    
    * doc: activate key doc
    
    * refactor: remove multi-vault-rotator
    
    * feat: all_vault_rotator to take a second vault
    
    * fix: keygen in transition state
    
    * chore: InTransition -> Unavailable
    
    * feat: introduce Unavailable key state
    
    * refactor: remove key_id function from KeyProvider
    
    * chore: rebase issues
    
    * chore: key_id -> key
    
    * chore: start -> keygen
    
    * doc: fix comment
    
    * chore: update expect message

--- state-chain/pallets/cf-threshold-signature/src/tests.rs
+++ state-chain/pallets/cf-threshold-signature/src/tests.rs
@@ -249,7 +249,7 @@ fn keygen_verification_ceremony_calls_callback_on_failure() {
 		.build()
 		.execute_with(|| {
 			const PAYLOAD: &[u8; 4] = b"OHAI";
-			let current_key_id = MockKeyProvider::current_key_id_epoch_index().0;
+			let current_key_id = MockKeyProvider::current_key_epoch_index().unwrap_key().into();
 			let (request_id, _) = EthereumThresholdSigner::request_keygen_verification_signature(
 				*PAYLOAD,
 				current_key_id,
@@ -483,7 +483,7 @@ mod unsigned_validation {
 
 				let (_request_id, ceremony_id) =
 					<EthereumThresholdSigner as ThresholdSigner<_>>::request_signature(PAYLOAD);
-				let (current_key_id, _) = MockKeyProvider::current_key_id_epoch_index();
+				let current_key = MockKeyProvider::current_key_epoch_index().unwrap_key();
 
 				assert!(
 					Test::validate_unsigned(
@@ -493,9 +493,9 @@ mod unsigned_validation {
 					)
 					.is_ok(),
 					"Validation Failed: {:?} / {:?} / {:?}",
-					MockKeyProvider::current_key(),
-					current_key_id.clone(),
-					<[u8; 4]>::try_from(current_key_id).unwrap()
+					MockKeyProvider::current_key_epoch_index(),
+					current_key.clone(),
+					current_key
 				);
 			});
 	}
@@ -571,11 +571,11 @@ mod failure_reporting {
 	) -> CeremonyContext<Test, Instance1> {
 		const PAYLOAD: <MockEthereum as ChainCrypto>::Payload = *b"OHAI";
 		MockEpochInfo::set_authorities(Vec::from_iter(validator_set));
-		let current_key_id = MockKeyProvider::current_key_id_epoch_index().0;
+		let current_key_id = MockKeyProvider::current_key_epoch_index().unwrap_key();
 		CeremonyContext::<Test, Instance1> {
 			request_context: RequestContext { request_id: 1, attempt_count: 0, payload: PAYLOAD },
 			threshold_ceremony_type: ThresholdCeremonyType::Standard,
-			key_id: current_key_id,
+			key_id: current_key_id.into(),
 			remaining_respondents: BTreeSet::from_iter(validator_set),
 			blame_counts: Default::default(),
 			participant_count: 5,
