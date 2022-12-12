54742369649bcd3336c8376c33fed1ed014c4293
kylezs <kyle@chainflip.io>
2022-12-13 02:44:18 +1100

    Fix/broadcast retry (#2594)
    * fix: always attempt to verify signature on broadcast retries
    
    remove unwrap impl
    
    * refactor: remove VaultState
    
    * refactor: inline EpochKey destructure
    
    * chore: current_key_epoch_index -> current_epoch_key

--- state-chain/traits/src/lib.rs
+++ state-chain/traits/src/lib.rs
@@ -370,29 +370,25 @@ pub trait ThresholdSignerNomination {
 }
 
 #[derive(Default, Debug, TypeInfo, Decode, Encode, Clone, Copy, PartialEq, Eq)]
-pub enum KeyState<Key> {
-	Active {
-		key: Key,
-		epoch_index: EpochIndex,
-	},
+pub enum KeyState {
+	Active,
 	// We are currently transitioning to a new key or the key doesn't yet exist.
 	#[default]
 	Unavailable,
 }
 
-impl<Key> KeyState<Key> {
-	pub fn unwrap_key(self) -> Key {
-		match self {
-			Self::Active { key: key_id, epoch_index: _ } => key_id,
-			Self::Unavailable => panic!("KeyState is Unavailable!"),
-		}
-	}
+#[derive(Default, Debug, TypeInfo, Decode, Encode, Clone, Copy, PartialEq, Eq)]
+pub struct EpochKey<Key> {
+	pub key: Key,
+	pub epoch_index: EpochIndex,
+	pub key_state: KeyState,
 }
 
 /// Provides the currently valid key for multisig ceremonies.
 pub trait KeyProvider<C: ChainCrypto> {
-	/// Get the chain's current agg key and the epoch index for the current key.
-	fn current_key_epoch_index() -> KeyState<C::AggKey>;
+	/// Get the chain's current agg key, the epoch index for the current key and the state of that
+	/// key.
+	fn current_epoch_key() -> EpochKey<C::AggKey>;
 
 	#[cfg(feature = "runtime-benchmarks")]
 	fn set_key(_key: C::AggKey) {
--- state-chain/traits/src/mocks/key_provider.rs
+++ state-chain/traits/src/mocks/key_provider.rs
@@ -1,12 +1,12 @@
 use std::marker::PhantomData;
 
-use crate::KeyState;
+use crate::EpochKey;
 
 #[derive(Default)]
 pub struct MockKeyProvider<Chain: cf_chains::Chain>(PhantomData<Chain>);
 
 impl<C: cf_chains::ChainCrypto> crate::KeyProvider<C> for MockKeyProvider<C> {
-	fn current_key_epoch_index() -> KeyState<C::AggKey> {
-		KeyState::default()
+	fn current_epoch_key() -> EpochKey<C::AggKey> {
+		EpochKey::default()
 	}
 }
