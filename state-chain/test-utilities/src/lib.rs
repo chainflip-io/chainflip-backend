098d0e6d2f45f138a7116edd7cbebc3c9a7a7832
kylezs <zsembery.kyle@gmail.com>
2022-12-23 11:56:53 +0100

    chore: dedup last_event code

--- state-chain/test-utilities/src/lib.rs
+++ state-chain/test-utilities/src/lib.rs
@@ -1,7 +1,7 @@
 use frame_system::Config;
 
 pub fn last_event<T: Config>() -> <T as Config>::Event {
-	frame_system::Pallet::<T>::events().pop().expect("Event expected").event
+	maybe_last_event::<T>().expect("Event expected")
 }
 
 pub fn maybe_last_event<T: Config>() -> Option<<T as Config>::Event> {
