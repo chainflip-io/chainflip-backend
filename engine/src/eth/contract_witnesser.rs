e20e7aa5728e07266bfd2df4244e93354299629d
Maxim Shishmarev <maxim@chainflip.io>
2022-12-23 12:38:21 +1100

    fix: remember history of witnessing

--- engine/src/eth/contract_witnesser.rs
+++ engine/src/eth/contract_witnesser.rs
@@ -1,6 +1,7 @@
-use std::sync::Arc;
+use std::{io::Write, sync::Arc, time::Duration};
 
 use cf_chains::eth::Ethereum;
+use cf_primitives::EpochIndex;
 use futures::StreamExt;
 
 use crate::{
@@ -30,6 +31,14 @@ where
 	ContractWitnesser: 'static + EthContractWitnesser + Sync + Send,
 	StateChainClient: ExtrinsicApi + 'static + Send + Sync,
 {
+	use serde::{Deserialize, Serialize};
+
+	#[derive(Clone, Debug, Serialize, Deserialize)]
+	struct WitnessedUntil {
+		epoch_index: EpochIndex,
+		block_number: u64,
+	}
+
 	epoch_witnesser::start(
 		contract_witnesser.contract_name(),
 		epoch_starts_receiver,
@@ -40,33 +49,113 @@ where
 			let eth_dual_rpc = eth_dual_rpc.clone();
 
 			async move {
-				let mut block_stream = block_events_stream_for_contract_from(
-					epoch_start.block_number,
-					&contract_witnesser,
-					eth_dual_rpc.clone(),
-					&logger,
-				)
-				.await?;
+				let contract_name = contract_witnesser.contract_name();
 
-				while let Some(block) = block_stream.next().await {
-					if should_end_witnessing::<Ethereum>(
-						end_witnessing_signal.clone(),
-						block.block_number,
-						&logger,
-					) {
-						break
+				let mut file_path = std::env::current_dir().unwrap();
+				file_path.push(contract_name);
+
+				let witnessed_until = tokio::task::spawn_blocking({
+					let file_path = file_path.clone();
+					move || match std::fs::read_to_string(&file_path)
+						.map_err(anyhow::Error::new)
+						.and_then(|string| {
+							serde_json::from_str::<WitnessedUntil>(&string)
+								.map_err(anyhow::Error::new)
+						}) {
+						Ok(witnessed_record) => witnessed_record,
+						Err(_) => WitnessedUntil { epoch_index: 0, block_number: 0 },
 					}
+				})
+				.await
+				.unwrap();
 
-					contract_witnesser
-						.handle_block_events(
-							epoch_start.epoch_index,
+				slog::info!(logger, "WitnessingUntil: {:?}", witnessed_until);
+
+				let (witnessed_until_sender, witnessed_until_receiver) =
+					tokio::sync::watch::channel(witnessed_until.clone());
+
+				tokio::task::spawn_blocking({
+					let file_path = file_path.clone();
+					let logger = logger.clone();
+					move || loop {
+						std::thread::sleep(Duration::from_secs(4));
+						if let Ok(changed) = witnessed_until_receiver.has_changed() {
+							if changed {
+								let witnessed_until = witnessed_until_receiver.borrow().clone();
+
+								if let Err(error) = atomicwrites::AtomicFile::new(
+									&file_path,
+									atomicwrites::OverwriteBehavior::AllowOverwrite,
+								)
+								.write(|file| {
+									write!(
+										file,
+										"{}",
+										serde_json::to_string::<WitnessedUntil>(&witnessed_until)
+											.unwrap()
+									)
+								}) {
+									slog::info!(
+										logger,
+										"Failed to record WitnessingUntil: {:?}",
+										error
+									);
+								} else {
+									slog::info!(
+										logger,
+										"Recorded WitnessingUntil: {:?}",
+										witnessed_until
+									);
+								}
+							}
+						} else {
+							break
+						}
+					}
+				});
+
+				if epoch_start.epoch_index >= witnessed_until.epoch_index {
+					let mut block_stream = block_events_stream_for_contract_from(
+						if witnessed_until.epoch_index == epoch_start.epoch_index {
+							std::cmp::max(epoch_start.block_number, witnessed_until.block_number)
+						} else {
+							epoch_start.block_number
+						},
+						&contract_witnesser,
+						eth_dual_rpc.clone(),
+						&logger,
+					)
+					.await?;
+
+					while let Some(block) = block_stream.next().await {
+						if should_end_witnessing::<Ethereum>(
+							end_witnessing_signal.clone(),
 							block.block_number,
-							block,
-							state_chain_client.clone(),
-							&eth_dual_rpc,
 							&logger,
-						)
-						.await?;
+						) {
+							break
+						}
+
+						let block_number = block.block_number;
+
+						contract_witnesser
+							.handle_block_events(
+								epoch_start.epoch_index,
+								block_number,
+								block,
+								state_chain_client.clone(),
+								&eth_dual_rpc,
+								&logger,
+							)
+							.await?;
+
+						witnessed_until_sender
+							.send(WitnessedUntil {
+								epoch_index: epoch_start.epoch_index,
+								block_number,
+							})
+							.unwrap();
+					}
 				}
 				Ok(contract_witnesser)
 			}
