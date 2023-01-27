be724016d1811490499de0bf6d07146cf85209a4
Daniel <daniel@chainflip.io>
2023-01-27 14:00:12 +0100

    feat: migrate claim delay buffer

--- state-chain/pallets/cf-staking/src/migrations/v1.rs
+++ state-chain/pallets/cf-staking/src/migrations/v1.rs
@@ -1,11 +1,17 @@
 use crate::*;
-use frame_support::traits::OnRuntimeUpgrade;
+use frame_support::{parameter_types, traits::OnRuntimeUpgrade};
+use sp_core::ConstU64;
 use sp_std::marker::PhantomData;
 
 #[cfg(feature = "try-runtime")]
 use codec::{Decode, Encode};
 
+/// Runtime Migration for migrating from V0 to V1 based on perseverance at commit `3f3d1ea0` (branch
+/// `release/0.6`).
 pub struct Migration<T: Config>(PhantomData<T>);
+
+// Note this is valid only perseverance.
+const CLAIM_DELAY_BUFFER_SECONDS: u64 = 80;
 
 impl<T: Config> OnRuntimeUpgrade for Migration<T> {
 	fn on_runtime_upgrade() -> frame_support::weights::Weight {
@@ -13,6 +19,7 @@ impl<T: Config> OnRuntimeUpgrade for Migration<T> {
 		for account in accounts {
 			PendingClaims::<T>::insert(account, ());
 		}
+		ClaimDelayBufferSeconds::<T>::put(CLAIM_DELAY_BUFFER_SECONDS);
 		Weight::zero()
 	}
 
