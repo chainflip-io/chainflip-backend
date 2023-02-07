19d2339e505de08dd3b5c52656630baa46aa1b79
kylezs <zsembery.kyle@gmail.com>
2023-02-07 15:36:59 +0100

    fix: use correct init state

--- engine/src/dot/runtime_version_updater.rs
+++ engine/src/dot/runtime_version_updater.rs
@@ -22,52 +22,35 @@ where
 	StateChainClient: ExtrinsicApi + StorageApi + 'static + Send + Sync,
 	DotRpc: DotRpcApi + 'static + Send + Sync + Clone,
 {
+	// When this witnesser starts up, we should check that the runtime version is up to
+	// date with the chain. This is in case we missed a Polkadot runtime upgrade when
+	// we were down.
+	let on_chain_runtime_version = match state_chain_client
+		.storage_value::<pallet_cf_environment::PolkadotRuntimeVersion<state_chain_runtime::Runtime>>(
+			latest_block_hash,
+		)
+		.await
+	{
+		Ok(version) => version,
+		Err(e) =>
+			return Err((
+				epoch_starts_receiver,
+				anyhow::anyhow!("Failed to get PolkadotRuntimeVersion from SC: {:?}", e),
+			)),
+	};
+
 	epoch_witnesser::start(
 		"DOT-Runtime-Version".to_string(),
 		epoch_starts_receiver,
 		|epoch_start| epoch_start.current,
-		(),
-		move |end_witnessing_signal, epoch_start, (), logger| {
+		on_chain_runtime_version,
+		move |end_witnessing_signal, epoch_start, mut last_version_witnessed, logger| {
 			let state_chain_client = state_chain_client.clone();
 			let dot_client = dot_client.clone();
 			async move {
-				// When this witnesser starts up, we should check that the runtime version is up to
-				// date with the chain. This is in case we missed a Polkadot runtime upgrade when
-				// we were down.
-				let on_chain_runtime_version = state_chain_client
-					.storage_value::<pallet_cf_environment::PolkadotRuntimeVersion<state_chain_runtime::Runtime>>(
-						latest_block_hash,
-					)
-					.await
-					.context("Failed to get PolkadotRuntimeVersion from SC")?;
-
-				// NB: We start the subscrption *before* we query for the latest polkadot runtime
-				// version. If we don't do this, there is a race condition whereby we could miss a
-				// runtime upgrade.
+				// NB: The first item of this stream is the current runtime version.
 				let mut runtime_version_subscription =
 					dot_client.subscribe_runtime_version().await?;
-
-				let runtime_version = dot_client.runtime_version(None).await?;
-
-				// If the `transaction_version` is bumped, the `spec_version` must also be bumped.
-				// So we only need to check the `spec_version` here.
-				// https://paritytech.github.io/substrate/master/sp_version/struct.RuntimeVersion.html#structfield.transaction_version
-				if on_chain_runtime_version.spec_version < runtime_version.spec_version {
-					let _result = state_chain_client
-						.submit_signed_extrinsic(
-							pallet_cf_witnesser::Call::witness_at_epoch {
-								call: Box::new(
-									pallet_cf_environment::Call::update_polkadot_runtime_version {
-										runtime_version,
-									}
-									.into(),
-								),
-								epoch_index: epoch_start.epoch_index,
-							},
-							&logger,
-						)
-						.await;
-				}
 
 				while let Some(result_runtime_version) = runtime_version_subscription.next().await {
 					// TODO: Change end_witnessing_signal to a oneshot channel and tokio::select!
@@ -81,25 +64,35 @@ where
 
 					match result_runtime_version {
 						Ok(new_runtime_version) => {
-							let _result = state_chain_client
-								.submit_signed_extrinsic(
-									pallet_cf_witnesser::Call::witness_at_epoch {
-										call: Box::new(
-											pallet_cf_environment::Call::update_polkadot_runtime_version {
-												runtime_version,
-											}
-											.into(),
-										),
-										epoch_index: epoch_start.epoch_index,
-									},
-									&logger,
-								)
-								.await;
-							slog::info!(
-                            logger,
-                            "Polkadot runtime version update submitted, version witnessed: {:?}",
-                            new_runtime_version
-                        );
+							if new_runtime_version.spec_version >
+								last_version_witnessed.spec_version
+							{
+								assert!(
+									new_runtime_version.transaction_version >=
+										last_version_witnessed.transaction_version,
+									"Polkadot spec version must be bumped when the transaction version is bumped"
+								);
+								let _result = state_chain_client
+									.submit_signed_extrinsic(
+										pallet_cf_witnesser::Call::witness_at_epoch {
+											call: Box::new(
+												pallet_cf_environment::Call::update_polkadot_runtime_version {
+													runtime_version: new_runtime_version,
+												}
+												.into(),
+											),
+											epoch_index: epoch_start.epoch_index,
+										},
+										&logger,
+									)
+									.await;
+								slog::info!(
+									logger,
+									"Polkadot runtime version update submitted, version witnessed: {:?}",
+									new_runtime_version
+								);
+								last_version_witnessed = new_runtime_version;
+							}
 						},
 						Err(e) => {
 							slog::error!(logger, "Error receiving runtime version update: {:?}", e);
@@ -107,7 +100,7 @@ where
 					}
 				}
 
-				Ok(())
+				Ok(last_version_witnessed)
 			}
 		},
 		&logger,
