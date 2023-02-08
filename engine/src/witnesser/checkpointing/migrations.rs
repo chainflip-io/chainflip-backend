1b3b42237f6a9499f2b1d02fe5f4d47adf376732
Jamie Ford <Jamie@chainflip.io>
2023-02-08 15:09:24 +1100

    refactor: changes from PR review

--- engine/src/witnesser/checkpointing/migrations.rs
+++ engine/src/witnesser/checkpointing/migrations.rs
@@ -10,6 +10,8 @@ use crate::multisig::{ChainTag, PersistentKeyDB};
 
 use super::WitnessedUntil;
 
+const LEGACY_FILE_NAMES: [&str; 2] = ["StakeManager", "KeyManager"];
+
 /// Attempt to migrate from legacy witnesser checkpointing files to db if no checkpoint is found in
 /// the db
 pub async fn run_migrations(
@@ -22,7 +24,19 @@ pub async fn run_migrations(
 	// Only go ahead with the migration if no checkpoint is found in the db.
 	if matches!(chain_tag, ChainTag::Ethereum) && db.load_checkpoint(chain_tag)?.is_none() {
 		// Check for a legacy checkpoint and save it to the db
-		if let Some(legacy_witness_until) = get_legacy_checkpoint(legacy_files_path, logger).await {
+		if let Some(legacy_witness_until) =
+			futures::future::join_all(LEGACY_FILE_NAMES.iter().map(|name| {
+				Box::pin(load_from_legacy_checkpoint_file(legacy_files_path.join(name), logger))
+			}))
+			.await
+			.into_iter()
+			.collect::<Option<Vec<WitnessedUntil>>>()
+			.and_then(|checkpoints| {
+				checkpoints
+					.into_iter()
+					.sorted_by_key(|witnessed_until| witnessed_until.block_number)
+					.next()
+			}) {
 			slog::info!(
 				logger,
 				"Migrating legacy witnesser {chain_tag} checkpoint of {:?} to db",
@@ -30,42 +44,13 @@ pub async fn run_migrations(
 			);
 			db.update_checkpoint(chain_tag, &legacy_witness_until);
 
-			delete_legacy_checkpointing_files(legacy_files_path)
-				.with_context(|| "Removing legacy witnesser checkpointing files")?
+			if let Err(e) = delete_legacy_checkpointing_files(legacy_files_path) {
+				slog::error!(logger, "Failed to delete legacy checkpointing files: {e}");
+			}
 		}
 	}
 
 	Ok(())
-}
-
-const LEGACY_FILE_NAMES: [&str; 5] =
-	["eth-ingress", "StakeManager", "KeyManager", "ERC20-Flip", "ERC20-Usdc"];
-
-/// Loads all 5 Eth witnesser checkpointing files and returns the `WitnessedUntil` with the lowest
-/// block number
-async fn get_legacy_checkpoint(
-	legacy_files_path: &Path,
-	logger: &slog::Logger,
-) -> Option<WitnessedUntil> {
-	let load_checkpoint_futures: Vec<_> = LEGACY_FILE_NAMES
-		.iter()
-		.map(|name| {
-			let file_path = legacy_files_path.join(name);
-			Box::pin(load_from_legacy_checkpoint_file(file_path, logger))
-		})
-		.collect();
-
-	let loaded_checkpoints: Option<Vec<WitnessedUntil>> =
-		futures::future::join_all(load_checkpoint_futures).await.into_iter().collect();
-
-	if let Some(checkpoints) = loaded_checkpoints {
-		checkpoints
-			.into_iter()
-			.sorted_by_key(|witnessed_until| witnessed_until.block_number)
-			.next()
-	} else {
-		None
-	}
 }
 
 async fn load_from_legacy_checkpoint_file(
@@ -76,16 +61,20 @@ async fn load_from_legacy_checkpoint_file(
 		tokio::task::spawn_blocking({
 			let file_path = file_path.clone();
 			let logger = logger.clone();
-			move || match std::fs::read_to_string(file_path).map_err(anyhow::Error::new).and_then(
-				|string| {
+			move || {
+				std::fs::read_to_string(file_path)
+					.map_err(anyhow::Error::new)
+					.and_then(|string| {
 					serde_json::from_str::<WitnessedUntil>(&string).map_err(anyhow::Error::new)
-				},
-			) {
-				Ok(witnessed_record) => Some(witnessed_record),
-				Err(e) => {
-					slog::error!(logger, "Failed to read legacy witnesser checkpoint file: {e}");
-					None
-				},
+					})
+					.map_err(|e| {
+						slog::error!(
+							logger,
+							"Failed to read legacy witnesser checkpoint file: {e}"
+						);
+						e
+					})
+					.ok()
 			}
 		})
 		.await
@@ -138,13 +127,11 @@ mod tests {
 		// Checkpoints to save to the legacy files
 		let expected_witness_until_list: HashMap<&str, WitnessedUntil> = HashMap::from_iter(vec![
 			(LEGACY_FILE_NAMES[0], WitnessedUntil { epoch_index: 1, block_number: 6 }),
-			(LEGACY_FILE_NAMES[1], WitnessedUntil { epoch_index: 9, block_number: 1 }), /* Has the lowest block number */
-			(LEGACY_FILE_NAMES[2], WitnessedUntil { epoch_index: 9, block_number: 2 }),
-			(LEGACY_FILE_NAMES[3], WitnessedUntil { epoch_index: 10, block_number: 4 }),
-			(LEGACY_FILE_NAMES[4], WitnessedUntil { epoch_index: 5, block_number: 5 }),
+			// Has the lowest block number
+			(LEGACY_FILE_NAMES[1], WitnessedUntil { epoch_index: 9, block_number: 1 }),
 		]);
 
-		// Create all 5 eth witnesser legacy checkpointing files
+		// Create both witnesser legacy checkpointing files
 		for (name, witness_until) in expected_witness_until_list.clone() {
 			let file_path = temp_path.join(name);
 			write_legacy_checkpoint(file_path, witness_until);
