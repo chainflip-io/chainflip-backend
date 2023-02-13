5f96c275fd788be916a2559ce62b8920d94a4679
Thomas Burton <tomjohnburton@gmail.com>
2023-02-13 17:04:57 +0200

    chore: simplify script

--- engine/start-engine.sh
+++ engine/start-engine.sh
@@ -1,11 +1,28 @@
 #!/bin/sh
 
-RUNTIME_SPEC_VERSION=$(curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "state_getRuntimeVersion", "params": []}' http://localhost:9933 | grep -o "\"specVersion\":[^,}]*" | awk -F ':' '{print $2}')
+getruntimeversion() {
+  curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "state_getRuntimeVersion", "params": []}' http://localhost:9933
+}
 
-if [ $RUNTIME_SPEC_VERSION -le 3 ]; then
-    CMD=/usr/local/bin/0.5/chainflip-engine
-else
-    CMD=chainflip-engine
+runoldversion() {
+  /usr/local/bin/chainflip/0.5/chainflip-engine --config-path /etc/chainflip/config/Default.toml
+}
+
+runnewversion() {
+  chainflip-engine --config-root /etc/chainflip/
+}
+
+until getruntimeversion 2>/dev/null; do
+  echo "Node unavailable at localhost:9933. Perhaps you need to start up the node?"
+  sleep 10
+done
+
+if getruntimeversion > /dev/null; then
+  RUNTIME_SPEC_VERSION=$(getruntimeversion | grep -o "\"specVersion\":[^,}]*" | awk -F ':' '{print $2}')
 fi
 
-$CMD "$@"
+if [ $RUNTIME_SPEC_VERSION -le 3 ]; then
+  runoldversion
+else
+  runnewversion
+fi
