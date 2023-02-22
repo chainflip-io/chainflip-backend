db63c879146dfc3140eb80f022bcb11a4e505bb5
Thomas Burton <tomjohnburton@gmail.com>
2023-02-22 13:50:58 +0200

    fix: port

--- engine/start-engine.sh
+++ engine/start-engine.sh
@@ -1,28 +1,35 @@
 #!/bin/sh
 
-getruntimeversion() {
+ENGINE_VERSION=0.5
+CURRENT_RUNTIME_VERSION=3
+
+_get_runtime_version() {
   curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "state_getRuntimeVersion", "params": []}' http://localhost:9933
 }
 
-runoldversion() {
-  /usr/local/bin/chainflip/0.5/chainflip-engine --config-path /etc/chainflip/config/Settings.toml
+_run_old_version() {
+  /usr/local/bin/chainflip/$ENGINE_VERSION/chainflip-engine --config-path /etc/chainflip/config/Settings.toml
 }
 
-runnewversion() {
+_run_new_version() {
   /usr/bin/chainflip-engine --config-root /etc/chainflip/
 }
 
-until getruntimeversion 2>/dev/null; do
+_check_old_engine_exists() {
+  ls /usr/local/bin/chainflip/$ENGINE_VERSION/chainflip-engine
+}
+
+until _get_runtime_version 2>/dev/null; do
   echo "Node unavailable at localhost:9933. Perhaps you need to start up the node?"
   sleep 10
 done
 
-if getruntimeversion > /dev/null; then
-  RUNTIME_SPEC_VERSION=$(getruntimeversion | grep -o "\"specVersion\":[^,}]*" | awk -F ':' '{print $2}')
+if _get_runtime_version > /dev/null; then
+  RUNTIME_SPEC_VERSION=$(_get_runtime_version | grep -o "\"specVersion\":[^,}]*" | awk -F ':' '{print $2}')
 fi
 
-if [ $RUNTIME_SPEC_VERSION -le 3 ]; then
-  runoldversion
+if [ $RUNTIME_SPEC_VERSION -le $CURRENT_RUNTIME_VERSION ] && _check_old_engine_exists; then
+  _run_old_version
 else
-  runnewversion
+  _run_new_version
 fi
