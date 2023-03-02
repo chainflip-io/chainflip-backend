f2d251a22fcfbc9b8b27579add27379d360ee0c9
Ramiz Siddiqui <ramiz@chainflip.io>
2023-03-02 10:11:03 +0100

    feat: utxo selection logic first version

---
+++ state-chain/chains/src/btc.rs
@@ -0,0 +1,42 @@
+use crate::{vec, Vec};
+
+#[derive(Clone)]
+pub struct UTXO {
+	pub amount: u64,
+}
+
+#[allow(clippy::dead_code)]
+fn select_utxos_from_pool(
+	mut available_utxos: Vec<UTXO>,
+	fee_per_utxo: u64,
+	amount_to_be_egressed: u64,
+) -> Vec<UTXO> {
+	if amount_to_be_egressed == 0 {
+		return vec![]
+	}
+
+	// Sort the utxos by the amounts they hold, in descending order
+	available_utxos.sort_by_key(|utxo| utxo.clone().amount);
+
+	let mut selected_utxos: Vec<UTXO> = vec![];
+
+	let mut cumulative_amount = 0;
+
+	// Start selecting the utxos from the smallest amount and keep on selecting the utxos until the
+	// cummulative amount of all selected utxos (plus the fees that need to be paid on spending
+	// them) just exceeds the total amount that needs to be spent (such that not selecting the last
+	// utxo would reduce the cummulative amount below the required amount).
+	while cumulative_amount < amount_to_be_egressed {
+		let current_smallest_utxo = available_utxos.pop().expect("The funds in vault should be greater than the amount requested to be egressed. This is made sure elsewhere and should be expected here");
+		cumulative_amount += current_smallest_utxo.clone().amount - fee_per_utxo;
+		selected_utxos.push(current_smallest_utxo);
+	}
+
+	// Select one more utxo that is next in line (smallest) among the remaining unselected utxos.
+	// Dont select any the utxo in case there is none remianing.
+	if let Some(utxo) = available_utxos.pop() {
+		selected_utxos.push(utxo);
+	}
+
+	selected_utxos
+}
--- state-chain/chains/src/lib.rs
+++ state-chain/chains/src/lib.rs
@@ -22,6 +22,7 @@ pub use cf_primitives::chains::*;
 pub mod benchmarking_value;
 
 pub mod any;
+pub mod btc;
 pub mod dot;
 pub mod eth;
 
