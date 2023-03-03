eedf0a350b6de728df15272615cd30cd2af704b5
kylezs <zsembery.kyle@gmail.com>
2023-03-03 09:59:28 +0100

    test: add hardcoded bytes test for safety

--- state-chain/primitives/src/lib.rs
+++ state-chain/primitives/src/lib.rs
@@ -164,6 +164,34 @@ fn test_key_id_to_and_from_bytes() {
 	for key_id in key_ids {
 		assert_eq!(key_id, KeyId::from_bytes(&key_id.to_bytes()));
 	}
+
+	let key_id = KeyId {
+		epoch_index: 29,
+		public_key_bytes: vec![
+			0xa,
+			93,
+			141,
+			u8::MAX,
+			0,
+			82,
+			2,
+			39,
+			144,
+			241,
+			29,
+			91,
+			3,
+			241,
+			120,
+			194,
+		],
+	};
+	// We check this because if this form changes then there will be an impact to how keys should be
+	// loaded from the db on the CFE. Thus, we want to be notified if this changes.
+	let expected_bytes =
+		vec![0, 0, 0, 29, 10, 93, 141, 255, 0, 82, 2, 39, 144, 241, 29, 91, 3, 241, 120, 194];
+	assert_eq!(expected_bytes, key_id.to_bytes());
+	assert_eq!(key_id, KeyId::from_bytes(&expected_bytes));
 }
 
 #[derive(
