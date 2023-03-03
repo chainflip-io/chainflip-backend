4515752913b90867a4f8b4ee5b643ed3ff24d577
Maxim Shishmarev <maxim@chainflip.io>
2023-03-03 16:59:20 +1100

    feat: key store fallback
    Due to migration some old keys may incorrectly stored as for epoch 0.
    This fallback allows us to load those keys.

--- engine/src/multisig/client/key_store.rs
+++ engine/src/multisig/client/key_store.rs
@@ -28,7 +28,13 @@ where
 
 	/// Get the key for the given key id
 	pub fn get_key(&self, key_id: &KeyId) -> Option<&KeygenResultInfo<C>> {
-		self.keys.get(key_id)
+		self.keys.get(key_id).or_else(|| {
+			// (Temporary) fallback: due to db migration (v0 to v1), some old
+			// keys may incorrectly be stored under epoch 0. Check if this is
+			// one of those keys and return it if so.
+			self.keys
+				.get(&KeyId { epoch_index: 0, public_key_bytes: key_id.public_key_bytes.clone() })
+		})
 	}
 
 	/// Save or update the key data and write it to persistent memory
