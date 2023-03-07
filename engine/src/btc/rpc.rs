77c4ab96b03543dc820c9c0390d940794a11ce91
kylezs <zsembery.kyle@gmail.com>
2023-03-07 13:53:26 +0100

    chore: move filter utxos to witnesser file

--- engine/src/btc/rpc.rs
+++ engine/src/btc/rpc.rs
@@ -1,7 +1,7 @@
-use std::{collections::BTreeSet, sync::Arc};
+use std::sync::Arc;
 
 use bitcoincore_rpc::{
-	bitcoin::{Block, BlockHash, Transaction, TxOut, Txid},
+	bitcoin::{Block, BlockHash, Txid},
 	Auth, Client, RpcApi,
 };
 
@@ -9,8 +9,6 @@ use anyhow::Result;
 use cf_chains::btc::BlockNumber;
 
 use crate::{settings, witnesser::LatestBlockNumber};
-
-use super::ScriptPubKey;
 
 #[derive(Clone)]
 pub struct BtcRpcClient {
@@ -62,94 +60,5 @@ impl LatestBlockNumber for BtcRpcClient {
 
 	async fn latest_block_number(&self) -> Result<BlockNumber> {
 		Ok(self.client.get_block_count()?)
-	}
-}
-
-// Takes txs and list of monitored addresses. Returns a list of txs that are relevant to the
-// monitored addresses.
-pub fn filter_interesting_utxos(
-	txs: Vec<Transaction>,
-	monitored_script_pubkeys: &BTreeSet<ScriptPubKey>,
-) -> Vec<TxOut> {
-	let mut interesting_utxos = vec![];
-	for tx in txs {
-		for tx_out in &tx.output {
-			if tx_out.value > 0 {
-				let script_pubkey_bytes = tx_out.script_pubkey.to_bytes();
-				if monitored_script_pubkeys.contains(&script_pubkey_bytes) {
-					interesting_utxos.push(tx_out.clone());
-				}
-			}
-		}
-	}
-	interesting_utxos
-}
-
-#[cfg(test)]
-mod tests {
-	use bitcoincore_rpc::bitcoin::{PackedLockTime, Script};
-
-	use super::*;
-
-	fn fake_transaction(tx_outs: Vec<TxOut>) -> Transaction {
-		Transaction { version: 2, lock_time: PackedLockTime(0), input: vec![], output: tx_outs }
-	}
-
-	#[test]
-	fn filter_interesting_utxos_no_utxos() {
-		let txs = vec![fake_transaction(vec![]), fake_transaction(vec![])];
-		let monitored_script_pubkeys = BTreeSet::new();
-		assert!(filter_interesting_utxos(txs, &monitored_script_pubkeys).is_empty());
-	}
-
-	#[test]
-	fn filter_interesting_utxos_several_same_tx() {
-		let monitored_pubkey = vec![0, 1, 2, 3];
-		let txs = vec![
-			fake_transaction(vec![
-				TxOut { value: 2324, script_pubkey: Script::from(monitored_pubkey.clone()) },
-				TxOut { value: 12223, script_pubkey: Script::from(vec![0, 32, 121, 9]) },
-				TxOut { value: 1234, script_pubkey: Script::from(monitored_pubkey.clone()) },
-			]),
-			fake_transaction(vec![]),
-		];
-		let monitored_script_pubkeys = BTreeSet::from([monitored_pubkey]);
-		let interesting_utxos = filter_interesting_utxos(txs, &monitored_script_pubkeys);
-		assert_eq!(interesting_utxos.len(), 2);
-		assert_eq!(interesting_utxos[0].value, 2324);
-		assert_eq!(interesting_utxos[1].value, 1234);
-	}
-
-	#[test]
-	fn filter_interesting_utxos_several_diff_tx() {
-		let monitored_pubkey = vec![0, 1, 2, 3];
-		let txs = vec![
-			fake_transaction(vec![
-				TxOut { value: 2324, script_pubkey: Script::from(monitored_pubkey.clone()) },
-				TxOut { value: 12223, script_pubkey: Script::from(vec![0, 32, 121, 9]) },
-			]),
-			fake_transaction(vec![TxOut {
-				value: 1234,
-				script_pubkey: Script::from(monitored_pubkey.clone()),
-			}]),
-		];
-		let monitored_script_pubkeys = BTreeSet::from([monitored_pubkey]);
-		let interesting_utxos = filter_interesting_utxos(txs, &monitored_script_pubkeys);
-		assert_eq!(interesting_utxos.len(), 2);
-		assert_eq!(interesting_utxos[0].value, 2324);
-		assert_eq!(interesting_utxos[1].value, 1234);
-	}
-
-	#[test]
-	fn filter_out_value_0() {
-		let monitored_pubkey = vec![0, 1, 2, 3];
-		let txs = vec![fake_transaction(vec![
-			TxOut { value: 2324, script_pubkey: Script::from(monitored_pubkey.clone()) },
-			TxOut { value: 0, script_pubkey: Script::from(monitored_pubkey.clone()) },
-		])];
-		let monitored_script_pubkeys = BTreeSet::from([monitored_pubkey]);
-		let interesting_utxos = filter_interesting_utxos(txs, &monitored_script_pubkeys);
-		assert_eq!(interesting_utxos.len(), 1);
-		assert_eq!(interesting_utxos[0].value, 2324);
 	}
 }
--- engine/src/btc/witnesser.rs
+++ engine/src/btc/witnesser.rs
@@ -1,6 +1,7 @@
 use std::{collections::BTreeSet, sync::Arc};
 
 use crate::constants::BTC_INGRESS_BLOCK_SAFETY_MARGIN;
+use bitcoincore_rpc::bitcoin::{Transaction, TxOut};
 use cf_chains::Bitcoin;
 use futures::StreamExt;
 use tokio::select;
@@ -20,9 +21,29 @@ use crate::{
 };
 
 use super::{
-	rpc::{filter_interesting_utxos, BtcRpcApi, BtcRpcClient},
+	rpc::{BtcRpcApi, BtcRpcClient},
 	ScriptPubKey,
 };
+
+// Takes txs and list of monitored addresses. Returns a list of txs that are relevant to the
+// monitored addresses.
+pub fn filter_interesting_utxos(
+	txs: Vec<Transaction>,
+	monitored_script_pubkeys: &BTreeSet<ScriptPubKey>,
+) -> Vec<TxOut> {
+	let mut interesting_utxos = vec![];
+	for tx in txs {
+		for tx_out in &tx.output {
+			if tx_out.value > 0 {
+				let script_pubkey_bytes = tx_out.script_pubkey.to_bytes();
+				if monitored_script_pubkeys.contains(&script_pubkey_bytes) {
+					interesting_utxos.push(tx_out.clone());
+				}
+			}
+		}
+	}
+	interesting_utxos
+}
 
 pub async fn start(
 	epoch_starts_receiver: async_broadcast::Receiver<EpochStart<Bitcoin>>,
@@ -174,5 +195,74 @@ mod tests {
 		start(epoch_starts_receiver, rpc, script_pubkeys_receiver, BTreeSet::new(), Arc::new(db))
 			.await
 			.unwrap();
+	}
+}
+
+#[cfg(test)]
+mod test_utxo_filtering {
+	use bitcoincore_rpc::bitcoin::{PackedLockTime, Script, Transaction, TxOut};
+
+	use super::*;
+
+	fn fake_transaction(tx_outs: Vec<TxOut>) -> Transaction {
+		Transaction { version: 2, lock_time: PackedLockTime(0), input: vec![], output: tx_outs }
+	}
+
+	#[test]
+	fn filter_interesting_utxos_no_utxos() {
+		let txs = vec![fake_transaction(vec![]), fake_transaction(vec![])];
+		let monitored_script_pubkeys = BTreeSet::new();
+		assert!(filter_interesting_utxos(txs, &monitored_script_pubkeys).is_empty());
+	}
+
+	#[test]
+	fn filter_interesting_utxos_several_same_tx() {
+		let monitored_pubkey = vec![0, 1, 2, 3];
+		let txs = vec![
+			fake_transaction(vec![
+				TxOut { value: 2324, script_pubkey: Script::from(monitored_pubkey.clone()) },
+				TxOut { value: 12223, script_pubkey: Script::from(vec![0, 32, 121, 9]) },
+				TxOut { value: 1234, script_pubkey: Script::from(monitored_pubkey.clone()) },
+			]),
+			fake_transaction(vec![]),
+		];
+		let monitored_script_pubkeys = BTreeSet::from([monitored_pubkey]);
+		let interesting_utxos = filter_interesting_utxos(txs, &monitored_script_pubkeys);
+		assert_eq!(interesting_utxos.len(), 2);
+		assert_eq!(interesting_utxos[0].value, 2324);
+		assert_eq!(interesting_utxos[1].value, 1234);
+	}
+
+	#[test]
+	fn filter_interesting_utxos_several_diff_tx() {
+		let monitored_pubkey = vec![0, 1, 2, 3];
+		let txs = vec![
+			fake_transaction(vec![
+				TxOut { value: 2324, script_pubkey: Script::from(monitored_pubkey.clone()) },
+				TxOut { value: 12223, script_pubkey: Script::from(vec![0, 32, 121, 9]) },
+			]),
+			fake_transaction(vec![TxOut {
+				value: 1234,
+				script_pubkey: Script::from(monitored_pubkey.clone()),
+			}]),
+		];
+		let monitored_script_pubkeys = BTreeSet::from([monitored_pubkey]);
+		let interesting_utxos = filter_interesting_utxos(txs, &monitored_script_pubkeys);
+		assert_eq!(interesting_utxos.len(), 2);
+		assert_eq!(interesting_utxos[0].value, 2324);
+		assert_eq!(interesting_utxos[1].value, 1234);
+	}
+
+	#[test]
+	fn filter_out_value_0() {
+		let monitored_pubkey = vec![0, 1, 2, 3];
+		let txs = vec![fake_transaction(vec![
+			TxOut { value: 2324, script_pubkey: Script::from(monitored_pubkey.clone()) },
+			TxOut { value: 0, script_pubkey: Script::from(monitored_pubkey.clone()) },
+		])];
+		let monitored_script_pubkeys = BTreeSet::from([monitored_pubkey]);
+		let interesting_utxos = filter_interesting_utxos(txs, &monitored_script_pubkeys);
+		assert_eq!(interesting_utxos.len(), 1);
+		assert_eq!(interesting_utxos[0].value, 2324);
 	}
 }
