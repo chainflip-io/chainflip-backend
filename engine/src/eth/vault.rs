3998178bd35800ecf2db951b34a333372ab6c7aa
kylezs <zsembery.kyle@gmail.com>
2023-03-15 16:24:42 +1100

    feat: decode log closure for vault events

--- engine/src/eth/vault.rs
+++ engine/src/eth/vault.rs
@@ -1,10 +1,28 @@
-use web3::{ethabi, types::H160};
+use std::sync::Arc;
+
+use async_trait::async_trait;
+use cf_primitives::EpochIndex;
+use web3::{
+	ethabi::{self, RawLog},
+	types::{H160, H256},
+};
+
+use crate::{eth::EventParseError, state_chain_observer::client::extrinsic_api::ExtrinsicApi};
+
+use super::{
+	event::Event, rpc::EthRpcApi, utils, BlockWithItems, EthContractWitnesser, SignatureAndEvent,
+};
+
+use anyhow::{bail, Result};
 
 pub struct Vault {
 	pub deployed_address: H160,
 	contract: ethabi::Contract,
 }
 
+// The following events need to reflect the events emitted in the staking contract:
+// https://github.com/chainflip-io/chainflip-eth-contracts/blob/master/contracts/Vault.sol
+#[derive(Debug)]
 pub enum VaultEvent {
 	CommunityGuardDisabled {
 		community_guard_disabled: bool,
@@ -17,14 +35,14 @@ pub enum VaultEvent {
 	},
 	SwapNative {
 		destination_chain: u32,
-		destination_address: ethabi::Address,
+		destination_address: web3::types::Bytes,
 		destination_token: u16,
 		amount: u128,
 		sender: ethabi::Address,
 	},
 	SwapToken {
 		destination_chain: u32,
-		destination_address: ethabi::Address,
+		destination_address: web3::types::Bytes,
 		destination_token: u16,
 		source_token: ethabi::Address,
 		amount: u128,
@@ -42,24 +60,24 @@ pub enum VaultEvent {
 	},
 	XCallNative {
 		destination_chain: u32,
-		destination_address: ethabi::Address,
+		destination_address: web3::types::Bytes,
 		destination_token: u16,
 		amount: u128,
 		sender: ethabi::Address,
 		message: web3::types::Bytes,
 		destination_native_budget: u128,
-		refund_address: ethabi::Address,
+		refund_address: web3::types::Bytes,
 	},
 	XCallToken {
 		destination_chain: u32,
-		destination_address: ethabi::Address,
+		destination_address: web3::types::Bytes,
 		destination_token: u16,
 		source_token: ethabi::Address,
 		amount: u128,
 		sender: ethabi::Address,
 		message: web3::types::Bytes,
 		destination_native_budget: u128,
-		refund_address: ethabi::Address,
+		refund_address: web3::types::Bytes,
 	},
 	AddGasNative {
 		swap_id: [u8; 32],
@@ -70,4 +88,163 @@ pub enum VaultEvent {
 		amount: u128,
 		token: ethabi::Address,
 	},
+}
+
+#[async_trait]
+impl EthContractWitnesser for Vault {
+	type EventParameters = VaultEvent;
+
+	fn contract_name(&self) -> String {
+		"Vault".to_string()
+	}
+
+	async fn handle_block_events<StateChainClient, EthRpcClient>(
+		&mut self,
+		_epoch: EpochIndex,
+		_block_number: u64,
+		_block: BlockWithItems<Event<Self::EventParameters>>,
+		_state_chain_client: Arc<StateChainClient>,
+		_eth_rpc: &EthRpcClient,
+	) -> Result<()>
+	where
+		EthRpcClient: EthRpcApi + Sync + Send,
+		StateChainClient: ExtrinsicApi + Send + Sync,
+	{
+		todo!("Handle the Vault events. Can't do this until the pallet exists.")
+	}
+
+	fn decode_log_closure(&self) -> Result<super::DecodeLogClosure<Self::EventParameters>> {
+		let community_guard_disabled =
+			SignatureAndEvent::new(&self.contract, "CommunityGuardDisabled")?;
+		let suspended = SignatureAndEvent::new(&self.contract, "Suspended")?;
+		let updated_key_manager = SignatureAndEvent::new(&self.contract, "UpdatedKeyManager")?;
+		let swap_native = SignatureAndEvent::new(&self.contract, "SwapNative")?;
+		let swap_token = SignatureAndEvent::new(&self.contract, "SwapToken")?;
+		let transfer_native_failed =
+			SignatureAndEvent::new(&self.contract, "TransferNativeFailed")?;
+		let transfer_token_failed = SignatureAndEvent::new(&self.contract, "TransferTokenFailed")?;
+		let xcall_native = SignatureAndEvent::new(&self.contract, "XCallNative")?;
+		let xcall_token = SignatureAndEvent::new(&self.contract, "XCallToken")?;
+		let add_gas_native = SignatureAndEvent::new(&self.contract, "AddGasNative")?;
+		let add_gas_token = SignatureAndEvent::new(&self.contract, "AddGasToken")?;
+
+		Ok(Box::new(
+			move |event_signature: H256, raw_log: RawLog| -> Result<Self::EventParameters> {
+				Ok(if event_signature == community_guard_disabled.signature {
+					let log = community_guard_disabled.event.parse_log(raw_log)?;
+					VaultEvent::CommunityGuardDisabled {
+						community_guard_disabled: utils::decode_log_param(
+							&log,
+							"communityGuardDisabled",
+						)?,
+					}
+				} else if event_signature == suspended.signature {
+					let log = suspended.event.parse_log(raw_log)?;
+					VaultEvent::Suspended { suspended: utils::decode_log_param(&log, "suspended")? }
+				} else if event_signature == updated_key_manager.signature {
+					let log = updated_key_manager.event.parse_log(raw_log)?;
+					VaultEvent::UpdatedKeyManager {
+						key_manager: utils::decode_log_param(&log, "keyManager")?,
+					}
+				} else if event_signature == swap_native.signature {
+					let log = swap_native.event.parse_log(raw_log)?;
+					VaultEvent::SwapNative {
+						destination_chain: utils::decode_log_param(&log, "dstChain")?,
+						destination_address: utils::decode_log_param(&log, "dstAddress")?,
+						destination_token: utils::decode_log_param(&log, "dstToken")?,
+						amount: utils::decode_log_param::<ethabi::Uint>(&log, "amount")?
+							.try_into()
+							.expect("SwapNative amount should fit into u128"),
+						sender: utils::decode_log_param(&log, "sender")?,
+					}
+				} else if event_signature == swap_token.signature {
+					let log = swap_token.event.parse_log(raw_log)?;
+					VaultEvent::SwapToken {
+						destination_chain: utils::decode_log_param(&log, "dstChain")?,
+						destination_address: utils::decode_log_param(&log, "dstAddress")?,
+						destination_token: utils::decode_log_param(&log, "dstToken")?,
+						source_token: utils::decode_log_param(&log, "srcToken")?,
+						amount: utils::decode_log_param::<ethabi::Uint>(&log, "amount")?
+							.try_into()
+							.expect("SwapToken amount should fit into u128"),
+						sender: utils::decode_log_param(&log, "sender")?,
+					}
+				} else if event_signature == transfer_native_failed.signature {
+					let log = transfer_native_failed.event.parse_log(raw_log)?;
+					VaultEvent::TransferNativeFailed {
+						recipient: utils::decode_log_param(&log, "recipient")?,
+						amount: utils::decode_log_param(&log, "amount")?,
+					}
+				} else if event_signature == transfer_token_failed.signature {
+					let log = transfer_token_failed.event.parse_log(raw_log)?;
+					VaultEvent::TransferTokenFailed {
+						recipient: utils::decode_log_param(&log, "recipient")?,
+						amount: utils::decode_log_param::<ethabi::Uint>(&log, "amount")?
+							.try_into()
+							.expect("TransferTokenFailed amount should fit into u128"),
+						token: utils::decode_log_param(&log, "token")?,
+						reason: utils::decode_log_param(&log, "reason")?,
+					}
+				} else if event_signature == xcall_native.signature {
+					let log = xcall_native.event.parse_log(raw_log)?;
+					VaultEvent::XCallNative {
+						destination_chain: utils::decode_log_param(&log, "dstChain")?,
+						destination_address: utils::decode_log_param(&log, "dstAddress")?,
+						destination_token: utils::decode_log_param(&log, "dstToken")?,
+						amount: utils::decode_log_param::<ethabi::Uint>(&log, "amount")?
+							.try_into()
+							.expect("XCallNative amount should fit into u128"),
+						sender: utils::decode_log_param(&log, "sender")?,
+						message: utils::decode_log_param(&log, "message")?,
+						destination_native_budget: utils::decode_log_param(
+							&log,
+							"dstNativeBudget",
+						)?,
+						refund_address: utils::decode_log_param(&log, "refundAddress")?,
+					}
+				} else if event_signature == xcall_token.signature {
+					let log = xcall_token.event.parse_log(raw_log)?;
+					VaultEvent::XCallToken {
+						destination_chain: utils::decode_log_param(&log, "dstChain")?,
+						destination_address: utils::decode_log_param(&log, "dstAddress")?,
+						destination_token: utils::decode_log_param(&log, "dstToken")?,
+						source_token: utils::decode_log_param(&log, "srcToken")?,
+						amount: utils::decode_log_param::<ethabi::Uint>(&log, "amount")?
+							.try_into()
+							.expect("XCallToken amount should fit into u128"),
+						sender: utils::decode_log_param(&log, "sender")?,
+						message: utils::decode_log_param(&log, "message")?,
+						destination_native_budget: utils::decode_log_param(
+							&log,
+							"dstNativeBudget",
+						)?,
+						refund_address: utils::decode_log_param(&log, "refundAddress")?,
+					}
+				} else if event_signature == add_gas_token.signature {
+					let log = add_gas_token.event.parse_log(raw_log)?;
+					VaultEvent::AddGasToken {
+						swap_id: utils::decode_log_param(&log, "swapID")?,
+						amount: utils::decode_log_param::<ethabi::Uint>(&log, "amount")?
+							.try_into()
+							.expect("AddGasToken amount should fit into u128"),
+						token: utils::decode_log_param(&log, "token")?,
+					}
+				} else if event_signature == add_gas_native.signature {
+					let log = add_gas_native.event.parse_log(raw_log)?;
+					VaultEvent::AddGasNative {
+						swap_id: utils::decode_log_param(&log, "swapID")?,
+						amount: utils::decode_log_param::<ethabi::Uint>(&log, "amount")?
+							.try_into()
+							.expect("AddGasNative amount should fit into u128"),
+					}
+				} else {
+					bail!(EventParseError::UnexpectedEvent(event_signature))
+				})
+			},
+		))
+	}
+
+	fn contract_address(&self) -> H160 {
+		self.deployed_address
+	}
 }
