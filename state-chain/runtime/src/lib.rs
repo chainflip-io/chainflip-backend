aa0e1fc2c3527f201ced0e106f42169b6400657c
Daniel <daniel@chainflip.io>
2023-03-24 11:40:40 +0100

    fix: emergency powers to force witness

--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -140,7 +140,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("chainflip-node"),
 	impl_name: create_runtime_str!("chainflip-node"),
 	authoring_version: 1,
-	spec_version: 4,
+	spec_version: 5,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
