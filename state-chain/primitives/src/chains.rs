fb5e40111c5bfd9e47efd73ca15573926d61fa37
kylezs <zsembery.kyle@gmail.com>
2023-04-13 14:02:21 +0200

    feat: include explicit chain id to ForeignChain conversion

--- state-chain/primitives/src/chains.rs
+++ state-chain/primitives/src/chains.rs
@@ -4,7 +4,7 @@ use frame_support::traits::Get;
 pub mod assets;
 
 macro_rules! chains {
-	( $( $chain:ident ),+ ) => {
+	( $( $chain:ident = $value:expr),+ ) => {
 		$(
 			#[derive(Copy, Clone, RuntimeDebug, Default, PartialEq, Eq, Encode, Decode, TypeInfo)]
 			pub struct $chain;
@@ -26,22 +26,22 @@ macro_rules! chains {
 		#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 		pub enum ForeignChain {
 			$(
-				$chain,
+				$chain = $value,
 			)+
 		}
 	}
+}
+
+chains! {
+	Ethereum = 1,
+	Polkadot = 2,
+	Bitcoin = 3
 }
 
 /// Can be any Chain.
 #[derive(Clone, Debug, PartialEq, Eq, Encode, Decode, TypeInfo, MaxEncodedLen, Copy)]
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 pub struct AnyChain;
-
-chains! {
-	Ethereum,
-	Polkadot,
-	Bitcoin
-}
 
 impl ForeignChain {
 	pub fn gas_asset(self) -> assets::any::Asset {
@@ -51,6 +51,27 @@ impl ForeignChain {
 			ForeignChain::Bitcoin => assets::any::Asset::Btc,
 		}
 	}
+}
+
+impl TryFrom<u32> for ForeignChain {
+	type Error = &'static str;
+
+	fn try_from(value: u32) -> Result<Self, Self::Error> {
+		match value {
+			x if x == Self::Ethereum as u32 => Ok(Self::Ethereum),
+			x if x == Self::Polkadot as u32 => Ok(Self::Polkadot),
+			x if x == Self::Bitcoin as u32 => Ok(Self::Bitcoin),
+			_ => Err("Invalid foreign chain"),
+		}
+	}
+}
+
+#[test]
+fn chain_id_to_chain() {
+	assert_eq!(ForeignChain::try_from(1), Ok(ForeignChain::Ethereum));
+	assert_eq!(ForeignChain::try_from(2), Ok(ForeignChain::Polkadot));
+	assert_eq!(ForeignChain::try_from(3), Ok(ForeignChain::Bitcoin));
+	assert!(ForeignChain::try_from(4).is_err());
 }
 
 #[test]
