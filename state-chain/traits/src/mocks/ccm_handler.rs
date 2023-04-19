9444dc9d5b4e74f325601e56cf7560a518ed0350
Roy Yang <roy@chainflip.io>
2023-04-19 17:11:49 +1200

    Fixed mock prefix

--- state-chain/traits/src/mocks/ccm_handler.rs
+++ state-chain/traits/src/mocks/ccm_handler.rs
@@ -9,9 +9,9 @@ use scale_info::TypeInfo;
 use super::{MockPallet, MockPalletStorage};
 
 pub struct MockCcmHandler;
-pub const STORAGE_NAME: &str = "MockCcmHandler";
+pub const CCM_HANDLER_PREFIX: &[u8] = b"MockCcmHandler";
 impl MockPallet for MockCcmHandler {
-	const PREFIX: &'static [u8] = STORAGE_NAME.as_bytes();
+	const PREFIX: &'static [u8] = CCM_HANDLER_PREFIX;
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Encode, Decode, TypeInfo)]
@@ -25,7 +25,7 @@ pub struct CcmRequest {
 
 impl MockCcmHandler {
 	pub fn get_ccm_requests() -> Vec<CcmRequest> {
-		<Self as MockPalletStorage>::get_value(b"MockCcmHandler").unwrap_or_default()
+		<Self as MockPalletStorage>::get_value(CCM_HANDLER_PREFIX).unwrap_or_default()
 	}
 }
 
@@ -37,7 +37,7 @@ impl CcmHandler for MockCcmHandler {
 		egress_address: ForeignChainAddress,
 		message_metadata: CcmIngressMetadata,
 	) -> DispatchResult {
-		<Self as MockPalletStorage>::mutate_value(b"MockCcmHandler", |ccm_requests| {
+		<Self as MockPalletStorage>::mutate_value(CCM_HANDLER_PREFIX, |ccm_requests| {
 			if ccm_requests.is_none() {
 				*ccm_requests = Some(vec![]);
 			}
