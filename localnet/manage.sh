812c7aea76c59059884b43c98a66d3bdedc88c04
Thomas Burton <tomjohnburton@gmail.com>
2023-04-20 11:14:52 +0200

    fix: ports and endpoints

--- localnet/manage.sh
+++ localnet/manage.sh
@@ -113,18 +113,18 @@ build-localnet-in-ci() {
   done
 
   echo "🪙 Waiting for Bitcoin node to start"
-  check_endpoint_health --user flip:flip -H 'Content-Type: text/plain;' --data '{"jsonrpc":"1.0", "id": "1", "method": "getblockchaininfo", "params" : []}' http://localhost:8332
-
-  echo "Replacing URL in toml file"
-  sed -i -e "s|localhost:8332|bitcoin:8332|g" ./localnet/init/config/Settings.toml
+  check_endpoint_health --user flip:flip -H 'Content-Type: text/plain;' --data '{"jsonrpc":"1.0", "id": "1", "method": "getblockchaininfo", "params" : []}' http://bitcoin:8332
 
   echo "💎 Waiting for ETH node to start"
-  check_endpoint_health -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"net_version","params":[],"id":67}' http://localhost:8545
-  sed -i -e "s|localhost:8545|geth:8545|g" ./localnet/init/config/Settings.toml
-  sed -i -e "s|localhost:8546|geth:8546|g" ./localnet/init/config/Settings.toml
+  check_endpoint_health -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"net_version","params":[],"id":67}' http://geth:8545
 
   echo "🚦 Waiting for polkadot node to start"
-  REPLY=$(check_endpoint_health -H "Content-Type: application/json" -s -d '{"id":1, "jsonrpc":"2.0", "method": "chain_getBlockHash", "params":[0]}' 'http://localhost:9945') || [ -z $(echo $REPLY | grep -o '\"result\":\"0x[^"]*' | grep -o '0x.*') ]
+  REPLY=$(check_endpoint_health -H "Content-Type: application/json" -s -d '{"id":1, "jsonrpc":"2.0", "method": "chain_getBlockHash", "params":[0]}' 'http://polkadot:9944') || [ -z $(echo $REPLY | grep -o '\"result\":\"0x[^"]*' | grep -o '0x.*') ]
+
+  echo "🎛️ Replacing URLs in Settings.toml"
+  sed -i -e "s|localhost:8332|bitcoin:8332|g" ./localnet/init/config/Settings.toml
+  sed -i -e "s|localhost:8545|geth:8545|g" ./localnet/init/config/Settings.toml
+  sed -i -e "s|localhost:8546|geth:8546|g" ./localnet/init/config/Settings.toml
   sed -i -e "s|localhost:9945|polkadot:9944|g" ./localnet/init/config/Settings.toml
 
   DOT_GENESIS_HASH=$(echo $REPLY | grep -o '\"result\":\"0x[^"]*' | grep -o '0x.*')
