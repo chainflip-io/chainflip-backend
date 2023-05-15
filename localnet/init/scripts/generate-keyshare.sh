d5506cb41855116e08bbd7751c2ee8cd5dc14b9c
tomjohnburton <39559415+tomjohnburton@users.noreply.github.com>
2023-05-15 14:51:47 +0200

    chore: generate new 3 node agg keys (#3241)

--- localnet/init/scripts/generate-keyshare.sh
+++ localnet/init/scripts/generate-keyshare.sh
@@ -4,13 +4,13 @@ if [ $1 == 1 ]; then
   mkdir -p localnet/init/keyshare/1-node
   rm -rf localnet/init/keyshare/1-node/bashful.db
   rm -rf ./*.db
-  GENESIS_NODE_IDS=localnet/init/keyshare/1-node.csv ./target/debug/generate-genesis-keys > localnet/init/keyshare/1-node/1_node_agg_keys.json
+  GENESIS_NODE_IDS=localnet/init/keyshare/1-node.csv ./target/debug/generate-genesis-keys > localnet/init/keyshare/1-node/agg_keys.json
   mv bashful.db localnet/init/keyshare/1-node
 elif [ $1 == 3 ]; then
   mkdir -p localnet/init/keyshare/3-node
   rm -rf localnet/init/keyshare/3-node/*.db
   rm -rf ./*.db
-  GENESIS_NODE_IDS=localnet/init/keyshare/3-node.csv ./target/debug/generate-genesis-keys > localnet/init/keyshare/3-node/3_node_agg_keys.json
+  GENESIS_NODE_IDS=localnet/init/keyshare/3-node.csv ./target/debug/generate-genesis-keys > localnet/init/keyshare/3-node/agg_keys.json
   mv *.db localnet/init/keyshare/3-node
 else
   echo "Incorrect option"
