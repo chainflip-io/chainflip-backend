378ae057d2c8ec662587774c4b40d81d902c7836
Assem <asem.hasna@gmail.com>
2023-05-31 13:40:40 +0200

    debug: versuch nummer 98755 🎬

--- bouncer/run_simple.sh
+++ bouncer/run_simple.sh
@@ -1,12 +1,13 @@
 /usr/bin/env
 echo "*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*"
-/usr/bin/env pnpm tsx
+echo "PATH: $PATH"
 echo "*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*"
 
 echo "Starting Simple Test ..."
 pnpm tsx ./commands/observe_block.ts 1 &&
 ./tests/stress_test.sh 3
 
+PATH=$PATH:$(which pnpm)
 echo "Starting Orginal Test ..."
 ./commands/observe_block.ts 1 &&
 ./tests/stress_test.sh 3
