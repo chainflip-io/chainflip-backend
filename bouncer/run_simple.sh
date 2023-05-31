7a002a08e131ca395f781019b12bf2848c4d3906
Assem <asem.hasna@gmail.com>
2023-05-31 12:21:15 +0200

    debug: runner env

--- bouncer/run_simple.sh
+++ bouncer/run_simple.sh
@@ -1,2 +1,12 @@
+/usr/bin/env
+echo "*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*"
+/usr/bin/env pnpm tsx
+echo "*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*"
+
+echo "Starting Simple Test ..."
 pnpm tsx ./commands/observe_block.ts 1 &&
+./tests/stress_test.sh 3
+
+echo "Starting Orginal Test ..."
+./commands/observe_block.ts 1 &&
 ./tests/stress_test.sh 3
