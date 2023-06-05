7934737eb44dee5b8faea8d8ff5aa92bb9bebd7b
kylezs <zsembery.kyle@gmail.com>
2023-06-05 18:53:59 +0200

    chore: clippy

--- utilities/src/with_std/futures_unordered_wait.rs
+++ utilities/src/with_std/futures_unordered_wait.rs
@@ -2,15 +2,13 @@ use futures::{Future, StreamExt};
 
 /// A wrapper around `futures::stream::FuturesUnordered` that waits instead of returning None when
 /// there are no futures to poll.
+
+#[derive(Default)]
 pub struct FuturesUnorderedWait<Fut> {
 	futures: futures::stream::FuturesUnordered<Fut>,
 }
 
 impl<Fut> FuturesUnorderedWait<Fut> {
-	pub fn new() -> Self {
-		Self { futures: futures::stream::FuturesUnordered::new() }
-	}
-
 	pub fn push(&mut self, future: Fut) {
 		self.futures.push(future);
 	}
@@ -36,6 +34,7 @@ mod tests {
 
 	use super::*;
 
+	#[derive(Default)]
 	pub struct TestFuture {
 		ready: bool,
 	}
@@ -59,14 +58,14 @@ mod tests {
 
 	#[tokio::test]
 	async fn no_futures_stays_pending() {
-		let mut stream = FuturesUnorderedWait::<TestFuture>::new();
+		let mut stream = FuturesUnorderedWait::<TestFuture>::default();
 
 		assert_eq!(stream.next().now_or_never(), None);
 	}
 
 	#[tokio::test]
 	async fn one_future_is_ready() {
-		let mut stream = FuturesUnorderedWait::<TestFuture>::new();
+		let mut stream = FuturesUnorderedWait::<TestFuture>::default();
 		stream.push(TestFuture { ready: true });
 		assert_eq!(stream.next().now_or_never(), Some(Some(())));
 
@@ -75,7 +74,7 @@ mod tests {
 
 	#[tokio::test]
 	async fn many_futures_are_ready() {
-		let mut stream = FuturesUnorderedWait::<TestFuture>::new();
+		let mut stream = FuturesUnorderedWait::<TestFuture>::default();
 
 		const READY_FUTURES: u32 = 4;
 
@@ -92,7 +91,7 @@ mod tests {
 
 	#[tokio::test]
 	async fn wait_until_ready() {
-		let mut stream = FuturesUnorderedWait::<TestFuture>::new();
+		let mut stream = FuturesUnorderedWait::<TestFuture>::default();
 		assert_eq!(stream.next().now_or_never(), None);
 
 		stream.push(TestFuture { ready: true });
