7ae7f097d2377c1877c5c8f38ab6fa36b0a114d3
Assem Hasna <asem.hasna@gmail.com>
2023-06-09 17:39:08 +0200

    chore: nocer, more readable code

--- ci/scripts/check_docker_image_tags.py
+++ ci/scripts/check_docker_image_tags.py
@@ -4,25 +4,16 @@ import yaml
 
 EXIT_CODE = 0
 
-services = {
-    "geth": "services.geth.image",
-    "polkadot": "services.polkadot.image",
-    "bitcoin": "services.bitcoin.image"
-}
-
+services = ["geth", "polkadot", "bitcoin"]
 with (
     open("./localnet/docker-compose.yml", 'r', encoding="utf-8") as docker_compose_file,
     open(".github/workflows/_40_post_check.yml", 'r', encoding="utf-8") as github_actions_file
 ):
     docker_compose = yaml.safe_load(docker_compose_file)
     github_actions = yaml.safe_load(github_actions_file)
-    for service, path in services.items():
-        path_parts = path.split('.')
-        docker_image = docker_compose
-        for part in path_parts:
-            docker_image = docker_image.get(part, None)
-        github_image = github_actions['jobs']['bouncer']['services'].get(
-            service, {}).get('image', None)
+    for service in services:
+        docker_image = docker_compose["services"][service]["image"]
+        github_image = github_actions["jobs"]["bouncer"]["services"][service]["image"]
         if docker_image != github_image:
             error_message = f"""🚨 \033[1;31m{service} docker image mismatch!\033[0m\n\033[1;33mLocal:\033[0m {docker_image}\n\033[1;33mGitHub:\033[0m {github_image}"""
             print(error_message)
