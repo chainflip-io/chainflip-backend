5f57bf65d33d12455600edc73056b0293722345a
Maxim Shishmarev <msgmaxim@gmail.com>
2023-06-15 10:56:32 +1000

    test: add swap native bouncer test (#3420)
    * test: add swap native bouncer test
    
    * test: add native contract swap to usdc
    
    * refactor: move chainFromToken into utils
    
    * chore: rename test
    
    * chore: fix import

---
+++ bouncer/tests/native_swap.ts
@@ -0,0 +1,35 @@
+import { executeNativeSwap } from '../commands/native_swap';
+import { Token, chainflipApi, getAddress as newAddress, getBalanceSync, observeBalanceIncrease, observeEvent, runWithTimeout } from '../shared/utils';
+
+async function testNativeSwap(destToken: Token) {
+
+    const api = await chainflipApi();
+    const addr = newAddress(destToken, 'never');
+    console.log("Destination address:", addr);
+
+    const oldBalance = getBalanceSync(destToken, addr);
+    // Note that we start observing events before executing
+    // the swap to avoid race conditions:
+    console.log(`Executing native contract swap to (${destToken}) ${addr}. Current balance: ${oldBalance}`)
+    const handle = observeEvent("swapping:SwapExecuted", api);
+    await executeNativeSwap(destToken, addr);
+    await handle;
+    console.log(`Successfully observed event: swapping:SwapExecuted`);
+
+    const newBalance = await observeBalanceIncrease(destToken, addr, oldBalance);
+    console.log(`Swap success! New balance: ${newBalance}`);
+}
+
+async function test() {
+    await testNativeSwap('DOT');
+    await testNativeSwap('USDC');
+}
+
+// A successful execution usually takes ~150 seconds
+runWithTimeout(test(), 180000).then(() => {
+    // Don't wait for the timeout future to finish:
+    process.exit(0);
+}).catch((error) => {
+    console.error(error);
+    process.exit(-1);
+});
