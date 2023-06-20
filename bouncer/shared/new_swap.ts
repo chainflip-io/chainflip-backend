91d73db83b913c45ce106d5a6f2ee62287ac3042
Maxim Shishmarev <maxim@chainflip.io>
2023-06-20 16:17:48 +1000

    refactor: make new_swap importable

---
+++ bouncer/shared/new_swap.ts
@@ -0,0 +1,33 @@
+import { Keyring } from "@polkadot/api";
+import { u8aToHex } from "@polkadot/util";
+import { cryptoWaitReady } from "@polkadot/util-crypto";
+import { Mutex } from "async-mutex";
+import { Token, chainflipApi } from "./utils";
+
+const mutex = new Mutex();
+
+export async function newSwap(sourceToken: Token, destToken: Token,
+    destAddress: string, fee: any): Promise<void> {
+    await cryptoWaitReady();
+    const keyring = new Keyring({ type: 'sr25519' });
+
+    const chainflip = await chainflipApi();
+    const destinationAddress =
+        destToken === 'DOT' ? u8aToHex(keyring.decodeAddress(destAddress)) : destAddress;
+
+    const brokerUri = process.env.BROKER_URI ?? '//BROKER_1';
+    const broker = keyring.createFromUri(brokerUri);
+
+    mutex.runExclusive(async () => {
+        await chainflip.tx.swapping
+            .requestSwapDepositAddress(
+                sourceToken,
+                destToken,
+                { [destToken === 'USDC' ? 'ETH' : destToken]: destinationAddress },
+                fee,
+                null,
+            )
+            .signAndSend(broker, { nonce: -1 });
+    })
+
+}
