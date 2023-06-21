1af3237da7f0a2189efe96344d9b910d86f04bd8
albert <allimos3@gmail.com>
2023-06-21 12:59:50 +0200

    chore: debug with wrong usdc address

--- bouncer/commands/fund_usdc.ts
+++ bouncer/commands/fund_usdc.ts
@@ -51,7 +51,9 @@ async function main(): Promise<void> {
   const web3 = new Web3(ethEndpoint);
   // const chainid = await web3.eth.getChainId();
   const usdcContractAddress =
-    process.env.ETH_USDC_ADDRESS ?? '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0';
+    process.env.ETH_USDC_ADDRESS ?? '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e1';
+  
+  console.log("Using USDC contract at " + usdcContractAddress)
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const usdcContract = new web3.eth.Contract(erc20TransferABI as any, usdcContractAddress);
   const txData = usdcContract.methods.transfer(ethereumAddress, microusdcAmount).encodeABI();
