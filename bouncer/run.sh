f7030154eb21a92e81567b0e7ed02ead6bd1818e
albert <allimos3@gmail.com>
2023-06-21 12:44:30 +0200

    chore: revert

--- bouncer/run.sh
+++ bouncer/run.sh
@@ -1,6 +1,5 @@
 pnpm tsx ./commands/observe_block.ts 5 &&
 pnpm tsx ./commands/setup_vaults.ts &&
-pnpm tsx ./commands/get_usdc_balance.ts 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 &&
 pnpm tsx ./commands/setup_swaps.ts &&
 ./tests/swapping.sh &&
 pnpm tsx ./tests/lp_deposit_expiry.ts &&
