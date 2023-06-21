7c7c796cfee462ac4f99a3395e069322929d8083
Maxim Shishmarev <maxim@chainflip.io>
2023-06-21 17:04:06 +1000

    refactor: rework observeEvent

--- bouncer/shared/utils.ts
+++ bouncer/shared/utils.ts
@@ -64,20 +64,32 @@ export function getBtcClient(btcEndpoint?: string): any {
   });
 }
 
-type EventQuery = (event: any) => boolean;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type EventQuery = (data: any) => boolean;
 
-export async function observeEventWithQuery(query: EventQuery, chainflip: ApiPromise): Promise<any> {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function observeEvent(eventName: string, chainflip: ApiPromise, eventQuery?: EventQuery): Promise<any> {
   let result;
   let waiting = true;
+
+  const query = eventQuery ?? (() => true);
+
+  const [expectedSection, expectedMethod] = eventName.split(':');
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const unsubscribe: any = await chainflip.query.system.events((events: any[]) => {
     events.forEach((record) => {
       const { event } = record;
 
-      if (query(event)) {
-        result = event.data;
-        waiting = false;
-        unsubscribe();
+      if (event.section === expectedSection && event.method === expectedMethod) {
+
+        const data = event.data.toJSON();
+
+        if (query(data)) {
+          result = event.data;
+          waiting = false;
+          unsubscribe();
+        }
+
       }
 
     });
