240e8b083686c3cac23bc0e360f9146a37b2f5a2
Assem Hasna <asem.hasna@gmail.com>
2023-06-22 09:33:01 +0200

    chore: sanity checks before publishing binaries 🫧

---
+++ ci/scripts/check_binary_version.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+binary_version_full=$($1 -V)
+github_tag=$2
+
+binary_semver=$(echo $binary_version_full | awk '{print $2}' | awk -F '-' '{print $1}')
+
+echo "Extracted binary version: $binary_semver"
+echo "Github tag: $github_tag"
+
+if [ "$binary_semver" != "$github_tag" ]; then
+    echo "Error: Binary version and Github tag do not match!"
+    exit 1
+fi
