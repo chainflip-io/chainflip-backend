d1447aa26436a7541e9737158417871506c9bc7b
kylezs <zsembery.kyle@gmail.com>
2023-06-22 14:57:13 +0200

    chore: update usdc address

--- bouncer/shared/fund_usdc.ts
+++ bouncer/shared/fund_usdc.ts
@@ -1,3 +1,4 @@
+import { Mutex } from "async-mutex";
 import Web3 from "web3";
 import { getNextEthNonce } from "./fund_eth";
 
@@ -41,7 +42,7 @@ export async function fundUsdc(ethereumAddress: string, usdcAmount: string) {
     const web3 = new Web3(ethEndpoint);
 
     const usdcContractAddress =
-        process.env.ETH_USDC_ADDRESS ?? '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
+        process.env.ETH_USDC_ADDRESS ?? '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0';
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const usdcContract = new web3.eth.Contract(erc20TransferABI as any, usdcContractAddress);
