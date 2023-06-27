dd37b50ea46badcdb7b25b5d612732a4d14190b0
Alastair Holmes <holmes.alastair@outlook.com>
2023-06-27 09:02:50 +0200

    refactor: cleanup

--- engine/src/witness/split_by_vault.rs
+++ engine/src/witness/split_by_vault.rs
@@ -1,9 +1,9 @@
 use cf_primitives::EpochIndex;
-use futures_util::{stream, StreamExt};
+use futures_util::StreamExt;
 
 use super::{
 	chain_source::{box_chain_stream, BoxChainStream, ChainSourceWithClient},
-	common::{BoxCurrentAndFuture, CurrentAndFuture, ExternalChainSource, RuntimeHasInstance},
+	common::{BoxCurrentAndFuture, ExternalChainSource, RuntimeHasInstance},
 	epoch_source::Vault,
 };
 
@@ -53,34 +53,6 @@ where
 	underlying_chain_source: &'a UnderlyingChainSource,
 	vaults: BoxCurrentAndFuture<'static, Vault<UnderlyingChainSource::Instance>>,
 }
-impl<'a, UnderlyingChainSource: ExternalChainSource> SplitByVault<'a, UnderlyingChainSource>
-where
-	state_chain_runtime::Runtime: RuntimeHasInstance<UnderlyingChainSource::Instance>,
-{
-	async fn into_item(
-		underlying_chain_source: &'a UnderlyingChainSource,
-		mut vault: Vault<UnderlyingChainSource::Instance>,
-	) -> Item<'a, UnderlyingChainSource> {
-		let (stream, client) = underlying_chain_source.stream_and_client().await;
-
-		(
-			vault.epoch,
-			vault.active_state.clone(),
-			box_chain_stream(stream.take_until(vault.expired_signal.wait()).filter(
-				move |header| {
-					futures::future::ready(
-						header.index >= vault.active_state.active_from_block &&
-							vault
-								.historic_signal
-								.get()
-								.map_or(true, |end_index| header.index < *end_index),
-					)
-				},
-			)),
-			client,
-		)
-	}
-}
 #[async_trait::async_trait]
 impl<'a, UnderlyingChainSource: ExternalChainSource> ChainSplitByVault<'a>
 	for SplitByVault<'a, UnderlyingChainSource>
@@ -90,19 +62,29 @@ where
 	type UnderlyingChainSource = UnderlyingChainSource;
 
 	async fn stream(self) -> BoxCurrentAndFuture<'a, Item<'a, Self::UnderlyingChainSource>> {
-		CurrentAndFuture {
-			current: Box::new(
-				stream::iter(self.vaults.current)
-					.then(|vault| Self::into_item(self.underlying_chain_source, vault))
-					.collect::<Vec<_>>()
-					.await
-					.into_iter(),
-			),
-			future: Box::pin(
-				self.vaults
-					.future
-					.then(|vault| Self::into_item(self.underlying_chain_source, vault)),
-			),
-		}
+		let underlying_chain_source = self.underlying_chain_source;
+		self.vaults
+			.then(move |mut vault| async move {
+				let (stream, client) = underlying_chain_source.stream_and_client().await;
+
+				(
+					vault.epoch,
+					vault.active_state.clone(),
+					box_chain_stream(stream.take_until(vault.expired_signal.wait()).filter(
+						move |header| {
+							futures::future::ready(
+								header.index >= vault.active_state.active_from_block &&
+									vault
+										.historic_signal
+										.get()
+										.map_or(true, |end_index| header.index < *end_index),
+							)
+						},
+					)),
+					client,
+				)
+			})
+			.await
+			.into_box()
 	}
 }
