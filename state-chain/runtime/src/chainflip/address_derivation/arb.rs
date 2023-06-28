1b10525037d5c70816d2653995c76db2370555d1
Ramiz Siddiqui <ramiz@chainflip.io>
2023-06-28 20:57:39 +0500

    feat: arb address derivation

--- state-chain/runtime/src/chainflip/address_derivation/arb.rs
+++ state-chain/runtime/src/chainflip/address_derivation/arb.rs
@@ -1,15 +1,25 @@
-use cf_chains::{Arbitrum, Chain};
+use cf_chains::{
+	eth::{api::EthEnvironmentProvider, deposit_address::get_create_2_address},
+	Arbitrum, Chain,
+};
 use cf_primitives::{chains::assets::arb, ChannelId};
 use cf_traits::AddressDerivationApi;
 use sp_runtime::DispatchError;
+
+use crate::{ArbEnvironment, Environment};
 
 use super::AddressDerivation;
 
 impl AddressDerivationApi<Arbitrum> for AddressDerivation {
 	fn generate_address(
-		_source_asset: arb::Asset,
-		_channel_id: ChannelId,
+		source_asset: arb::Asset,
+		channel_id: ChannelId,
 	) -> Result<<Arbitrum as Chain>::ChainAccount, DispatchError> {
-		todo!()
+		Ok(get_create_2_address(
+			Environment::arb_vault_address(),
+			ArbEnvironment::token_address(source_asset).map(|address| address.to_fixed_bytes()),
+			channel_id,
+		)
+		.into())
 	}
 }
