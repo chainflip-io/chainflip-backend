2cd43c9d7c5a68def855a0bd7100e2f20a953fa6
kylezs <zsembery.kyle@gmail.com>
2023-07-04 09:00:17 +0200

    feat: btc source ChainSourceWithClient

--- engine/src/witness/chain_source/btc_source.rs
+++ engine/src/witness/chain_source/btc_source.rs
@@ -3,45 +3,54 @@ use std::time::Duration;
 use bitcoin::BlockHash;
 use futures_util::stream;
 
-use super::{ChainSource, Header};
+use super::{ChainClient, ChainSourceWithClient, Header};
 use crate::{btc::retry_rpc::BtcRetryRpcApi, witness::chain_source::BoxChainStream};
 
 pub struct BtcBlockStream<C: BtcRetryRpcApi> {
-	rpc_client: C,
+	client: C,
 }
 
 const POLL_INTERVAL: Duration = Duration::from_secs(10);
 
 #[async_trait::async_trait]
-impl<C: BtcRetryRpcApi + Clone + Send + Sync> ChainSource for BtcBlockStream<C> {
-	type Index = u64;
-	type Hash = BlockHash;
-	type Data = ();
+impl<C> ChainSourceWithClient for BtcBlockStream<C>
+where
+	C: BtcRetryRpcApi + ChainClient<Index = u64, Hash = BlockHash, Data = ()> + Clone + Send + Sync,
+{
+	type Index = <C as ChainClient>::Index;
+	type Hash = <C as ChainClient>::Hash;
+	type Data = <C as ChainClient>::Data;
+	type Client = C;
 
-	async fn stream(&self) -> BoxChainStream<'_, Self::Index, Self::Hash, Self::Data> {
-		Box::pin(stream::unfold(
-			(self.rpc_client.clone(), None),
-			|(client, last_block_hash_yielded)| async move {
-				loop {
-					tokio::time::sleep(POLL_INTERVAL).await;
-					let best_block_hash = client.best_block_hash().await;
-					if last_block_hash_yielded.is_some_and(|hash| best_block_hash != hash) ||
-						last_block_hash_yielded.is_none()
-					{
-						let header = client.block_header(best_block_hash).await;
-						assert_eq!(header.hash, best_block_hash);
-						return Some((
-							Header {
-								index: header.height,
-								hash: header.hash,
-								parent_hash: header.previous_block_hash,
-								data: (),
-							},
-							(client, Some(best_block_hash)),
-						))
+	async fn stream_and_client(
+		&self,
+	) -> (BoxChainStream<'_, Self::Index, Self::Hash, Self::Data>, Self::Client) {
+		(
+			Box::pin(stream::unfold(
+				(self.client.clone(), None),
+				|(client, last_block_hash_yielded)| async move {
+					loop {
+						tokio::time::sleep(POLL_INTERVAL).await;
+						let best_block_hash = client.best_block_hash().await;
+						if last_block_hash_yielded.is_some_and(|hash| best_block_hash != hash) ||
+							last_block_hash_yielded.is_none()
+						{
+							let header = client.block_header(best_block_hash).await;
+							assert_eq!(header.hash, best_block_hash);
+							return Some((
+								Header {
+									index: header.height,
+									hash: header.hash,
+									parent_hash: header.previous_block_hash,
+									data: (),
+								},
+								(client, Some(best_block_hash)),
+							))
+						}
 					}
-				}
-			},
-		))
+				},
+			)),
+			self.client.clone(),
+		)
 	}
 }
