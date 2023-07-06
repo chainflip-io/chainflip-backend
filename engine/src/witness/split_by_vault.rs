5054c58ed4cc144ca1e483bf830aa49e4ac8fe60
Alastair Holmes <holmes.alastair@outlook.com>
2023-07-06 16:33:58 +0200

    chore: standardize split traits

--- engine/src/witness/split_by_vault.rs
+++ engine/src/witness/split_by_vault.rs
@@ -1,10 +1,10 @@
-use cf_primitives::EpochIndex;
+use cf_chains::Chain;
 use futures_util::StreamExt;
 
 use super::{
 	chain_source::{box_chain_stream, BoxChainStream, ChainSource},
 	common::{BoxActiveAndFuture, ExternalChainSource, RuntimeHasChain},
-	epoch_source::Vault,
+	epoch_source::{Epoch, Vault},
 };
 
 use utilities::assert_stream_send;
@@ -24,15 +24,17 @@ where
 			self.stream()
 				.await
 				.into_stream()
-				.flat_map_unordered(None, |(_, _, chain_stream, _)| chain_stream),
+				.flat_map_unordered(None, |(_, chain_stream, _)| chain_stream),
 		);
 		stream.for_each(|_| futures::future::ready(())).await;
 	}
 }
 
 type Item<'a, UnderlyingChainSource> = (
-	EpochIndex,
-	pallet_cf_vaults::Vault<<UnderlyingChainSource as ExternalChainSource>::Chain>,
+	Epoch<
+		pallet_cf_vaults::Vault<<UnderlyingChainSource as ExternalChainSource>::Chain>,
+		<<UnderlyingChainSource as ExternalChainSource>::Chain as Chain>::ChainBlockNumber,
+	>,
 	BoxChainStream<
 		'a,
 		<UnderlyingChainSource as ChainSource>::Index,
@@ -64,8 +66,7 @@ where
 				let (stream, client) = underlying_chain_source.stream_and_client().await;
 
 				(
-					vault.index,
-					vault.info.clone(),
+					vault.clone(),
 					box_chain_stream(stream.take_until(vault.expired_signal.wait()).filter(
 						move |header| {
 							futures::future::ready(
