132c48ba3a9a2c57325706eb2666fbc6db5d58cf
Albert Llimos <53186777+albert-llimos@users.noreply.github.com>
2023-07-06 10:27:22 +0200

    Test/bouncer ccm swaps (#3565)
    * chore: initial bouncer ccm tests
    
    * chore: improvements. debugging
    
    * chore: debugged event witnessing
    
    * chore: push missing files
    
    * chore: improvements
    
    * chore: cleanup
    
    * chore: cleanup
    
    * chore: change dsttokens
    
    * chore: use different ccm data
    
    * chore: add same-chain swaps
    
    * chore: pass non-abi encoded message
    
    * chore: address PR comments
    
    * chore: all addresses under utils
    
    * chore: pass valid srcAddress
    
    * chore: update comment
    
    * chore: small refactor
    
    * chore: address PR comments
    
    * chore: parallelize ccm swaps
    
    * chore: add missing return
    
    * chore: remove keyring parameter

---
+++ bouncer/shared/new_swap.ts
@@ -0,0 +1,39 @@
+import { cryptoWaitReady } from "@polkadot/util-crypto";
+import { Keyring } from "@polkadot/api";
+import { Mutex } from "async-mutex";
+import { Asset } from "@chainflip-io/cli/.";
+import { getChainflipApi, encodeDotAddressForContract } from "./utils";
+
+const mutex = new Mutex();
+
+export async function newSwap(sourceToken: Asset, destToken: Asset,
+    destAddress: string, fee: any, messageMetadata?: CcmDepositMetadata): Promise<void> {
+    await cryptoWaitReady();
+
+    const chainflip = await getChainflipApi();
+    const destinationAddress =
+        destToken === 'DOT' ? encodeDotAddressForContract(destAddress) : destAddress;
+    const keyring = new Keyring({ type: 'sr25519' });
+    const brokerUri = process.env.BROKER_URI ?? '//BROKER_1';
+    const broker = keyring.createFromUri(brokerUri);
+
+    await mutex.runExclusive(async () => {
+        await chainflip.tx.swapping
+            .requestSwapDepositAddress(
+                sourceToken,
+                destToken,
+                { [destToken === 'USDC' ? 'ETH' : destToken]: destinationAddress },
+                fee,
+                messageMetadata ?? null,
+            )
+            .signAndSend(broker, { nonce: -1 });
+    })
+
+}
+
+export interface CcmDepositMetadata {
+    message: string;
+    gas_budget: number;
+    cf_parameters: string;
+    source_address: any;
+  }
