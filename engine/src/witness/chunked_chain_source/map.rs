4d73190a9b25c0fd7cb169bcaa0071ab5d3ed482
Alastair Holmes <holmes.alastair@outlook.com>
2023-07-07 20:38:45 +0200

    refactor: Chunked Map takes epoch details

--- engine/src/witness/chunked_chain_source/map.rs
+++ engine/src/witness/chunked_chain_source/map.rs
@@ -1,8 +1,9 @@
+use cf_primitives::EpochIndex;
 use futures_core::Future;
 use futures_util::StreamExt;
 
 use crate::witness::{
-	chain_source::{aliases, box_chain_stream, map::MappedClient, Header},
+	chain_source::{aliases, box_chain_stream, ChainClient, Header},
 	common::BoxActiveAndFuture,
 };
 
@@ -24,7 +25,10 @@ where
 	Self: 'a,
 	MappedTo: aliases::Data,
 	FutMappedTo: Future<Output = MappedTo> + Send,
-	MapFn: Fn(Header<Inner::Index, Inner::Hash, Inner::Data>) -> FutMappedTo + Send + Sync + Clone,
+	MapFn: Fn(EpochIndex, Inner::Info, Header<Inner::Index, Inner::Hash, Inner::Data>) -> FutMappedTo
+		+ Send
+		+ Sync
+		+ Clone,
 {
 	type Info = Inner::Info;
 	type HistoricInfo = Inner::HistoricInfo;
@@ -33,7 +37,7 @@ where
 	type Hash = Inner::Hash;
 	type Data = MappedTo;
 
-	type Client = MappedClient<Inner::Client, MapFn>;
+	type Client = MappedClient<'a, Inner, MapFn>;
 
 	type Chain = Inner::Chain;
 
@@ -46,27 +50,82 @@ where
 			.then(move |(epoch, chain_stream, chain_client)| {
 				let map_fn = self.map_fn.clone();
 				async move {
+					let epoch_index = epoch.index;
+					let epoch_info = epoch.info.clone();
 					(
 						epoch,
 						box_chain_stream(chain_stream.then({
 							let map_fn = map_fn.clone();
+							let epoch_info = epoch_info.clone();
 							move |header| {
 								let map_fn = map_fn.clone();
+								let epoch_info = epoch_info.clone();
 								async move {
 									Header {
 										index: header.index,
 										hash: header.hash,
 										parent_hash: header.parent_hash,
-										data: (map_fn)(header).await,
+										data: (map_fn)(epoch_index, epoch_info, header).await,
 									}
 								}
 							}
 						})),
-						MappedClient::new(chain_client, map_fn.clone()),
+						MappedClient::new(chain_client, map_fn.clone(), epoch_index, epoch_info),
 					)
 				}
 			})
 			.await
 			.into_box()
+	}
+}
+
+pub struct MappedClient<'a, Inner: ChunkedChainSource<'a>, MapFn> {
+	inner_client: Inner::Client,
+	map_fn: MapFn,
+	index: EpochIndex,
+	info: Inner::Info,
+}
+impl<'a, Inner: ChunkedChainSource<'a>, MapFn> MappedClient<'a, Inner, MapFn> {
+	pub fn new(
+		inner_client: Inner::Client,
+		map_fn: MapFn,
+		index: EpochIndex,
+		info: Inner::Info,
+	) -> Self {
+		Self { inner_client, map_fn, index, info }
+	}
+}
+#[async_trait::async_trait]
+impl<
+		'a,
+		Inner: ChunkedChainSource<'a>,
+		MappedTo: aliases::Data,
+		FutMappedTo: Future<Output = MappedTo> + Send,
+		MapFn: Fn(
+				EpochIndex,
+				Inner::Info,
+				Header<Inner::Index, Inner::Hash, Inner::Data>,
+			) -> FutMappedTo
+			+ Send
+			+ Sync
+			+ Clone,
+	> ChainClient for MappedClient<'a, Inner, MapFn>
+{
+	type Index = Inner::Index;
+	type Hash = Inner::Hash;
+	type Data = MappedTo;
+
+	async fn header_at_index(
+		&self,
+		index: Self::Index,
+	) -> Header<Self::Index, Self::Hash, Self::Data> {
+		let header = self.inner_client.header_at_index(index).await;
+
+		Header {
+			index: header.index,
+			hash: header.hash,
+			parent_hash: header.parent_hash,
+			data: (self.map_fn)(self.index, self.info.clone(), header).await,
+		}
 	}
 }
