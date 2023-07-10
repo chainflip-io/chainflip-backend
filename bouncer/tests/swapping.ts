a449754d4ceebda5c2190ab799ed96f2bdfb4d56
Maxim Shishmarev <maxim@chainflip.io>
2023-07-10 16:26:13 +1000

    chore: more clear output

--- bouncer/tests/swapping.ts
+++ bouncer/tests/swapping.ts
@@ -10,18 +10,22 @@ import { performNativeSwap } from "../shared/native_swap";
 
 let swapCount = 1;
 
-async function testSwap(sourceToken: Asset, destToken: Asset, addressType?: BtcAddressType,  messageMetadata?: CcmDepositMetadata) {
+async function testSwap(sourceToken: Asset, destToken: Asset, addressType?: BtcAddressType, messageMetadata?: CcmDepositMetadata) {
     // Seed needs to be unique per swap:
     const seed = randomAsHex(32);
-    let address = await getAddress(destToken, seed, addressType);
-    
+
+    const tag = `[${swapCount++}: ${sourceToken}->${destToken}]`;
+
+    let address;
     // For swaps with a message force the address to be the CF Receiver Mock address.
-    if (messageMetadata && chainFromAsset(destToken) === chainFromAsset('ETH')){
+    if (messageMetadata && chainFromAsset(destToken) === chainFromAsset('ETH')) {
         address = getEthContractAddress('CFRECEIVER');
+        console.log(`${tag} Using CF Receiver Mock address: ${address}`);
+    } else {
+        address = await getAddress(destToken, seed, addressType);
+        console.log(`${tag} Created new ${destToken} address: ${address}`);
     }
 
-    console.log(`Created new ${destToken} address: ${address}`);
-    const tag = `[${swapCount++}: ${sourceToken}->${destToken}]`;
 
     await performSwap(sourceToken, destToken, address, tag, messageMetadata);
 }
