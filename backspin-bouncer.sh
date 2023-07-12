04135d2eff96f2e120d38bfb09fc340f2e333d8f
Thomas Burton <tomjohnburton@gmail.com>
2023-07-12 12:37:57 +0200

    force

--- backspin-bouncer.sh
+++
@@ -1,4 +0,0 @@
-export CF_NODE_ENDPOINT="wss://backspin-rpc.staging"
-export POLKADOT_ENDPOINT="wss://backspindot.staging"
-export BTC_ENDPOINT="https://backspin-btc.staging"
-export ETH_ENDPOINT="https://backspin-eth.staging"
