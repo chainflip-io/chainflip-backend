0758bddf1c767772046b4c71f5b59b5ddd309477
Martin Rieke <martin@chainflip.io>
2023-07-14 16:26:59 +0200

    lower amount in rotates_through_btc_swap test

--- bouncer/tests/rotates_through_btc_swap.sh
+++ bouncer/tests/rotates_through_btc_swap.sh
@@ -29,7 +29,7 @@ echo "Generated BTC address " $MY_ADDRESS &&
 pnpm tsx ./commands/new_swap.ts eth btc $MY_ADDRESS 100 &&
 SWAP_ADDRESS=`pnpm tsx ./commands/observe_events.ts --timeout 10000 --succeed_on swapping:SwapDepositAddressReady --fail_on foo:bar | jq  -r ".[0].eth"` &&
 OLD_BALANCE=`pnpm tsx ./commands/get_btc_balance.ts $MY_ADDRESS` &&
-pnpm tsx ./commands/fund_eth.ts $SWAP_ADDRESS 1000 &&
+pnpm tsx ./commands/fund_eth.ts $SWAP_ADDRESS 1 &&
 pnpm tsx ./commands/observe_events.ts --timeout 60000 --succeed_on swapping:SwapExecuted --fail_on foo:bar > /dev/null &&
 for i in `seq 60`; do
 	NEW_BALANCE=`pnpm tsx ./commands/get_btc_balance.ts $MY_ADDRESS`
