c28053c18ca7b353526c6f456ddc57618748af33
kylezs <zsembery.kyle@gmail.com>
2023-07-17 17:00:34 +0200

    test: add ignored epoch source test

--- engine/src/witness/start.rs
+++ engine/src/witness/start.rs
@@ -47,3 +47,53 @@ where
 
 	Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+
+	use crate::state_chain_observer;
+	use cf_primitives::AccountId;
+	use futures::{FutureExt, StreamExt};
+	use std::str::FromStr;
+	use utilities::task_scope::task_scope;
+
+	use super::*;
+
+	#[tokio::test]
+	#[ignore = "useful for testing the epoch source"]
+	async fn run_epoch_source() {
+		task_scope(|scope| {
+			async {
+				let (state_chain_stream, state_chain_client) =
+					state_chain_observer::client::StateChainClient::connect_without_account(
+						scope,
+						"ws://localhost:9944",
+					)
+					.await?;
+
+				let bashful =
+					AccountId::from_str("cFK7GTahm9qeX5Jjct3yfSvV4qLb8LJaArHL2SL6m9HAzc2sq")
+						.unwrap();
+
+				let mut epoch_source =
+					EpochSource::new(scope, state_chain_stream, state_chain_client.clone())
+						.await
+						.participating(bashful)
+						.await
+						.into_stream()
+						.await
+						.into_box()
+						.into_stream();
+
+				while let Some(epoch) = epoch_source.next().await {
+					println!("Epoch: {:?}", epoch.index);
+				}
+
+				Ok(())
+			}
+			.boxed()
+		})
+		.await
+		.unwrap()
+	}
+}
