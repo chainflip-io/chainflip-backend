0409f1cb519ef068c0995327f96d7e85bf690f7a
Alastair Holmes <42404303+AlastairHolmes@users.noreply.github.com>
2023-07-24 12:02:35 +0200

    Don't use roaring (#3715)
    * RLE Bitmap implementation
    
    * review changes
    
    * remove u64 bounds on Index

--- engine/src/witness/chunked_chain_source/chunked_by_vault/continuous.rs
+++ engine/src/witness/chunked_chain_source/chunked_by_vault/continuous.rs
@@ -3,7 +3,7 @@ use cf_primitives::EpochIndex;
 use core::iter::Step;
 use futures_util::stream;
 use std::sync::Arc;
-use utilities::{future_map::FutureMap, loop_select};
+use utilities::{future_map::FutureMap, loop_select, rle_bitmap::RleBitmap};
 
 use super::ChunkedByVault;
 use crate::{
@@ -11,18 +11,19 @@ use crate::{
 	witness::chain_source::{ChainClient, ChainStream},
 };
 use futures::{FutureExt, StreamExt};
+use serde::{de::DeserializeOwned, Serialize};
 use utilities::UnendingStream;
 
-pub trait Storage: Send + Sync {
-	fn load(&self, epoch: EpochIndex) -> Result<Option<roaring::RoaringTreemap>, anyhow::Error>;
-	fn store(&self, epoch: EpochIndex, map: &roaring::RoaringTreemap) -> Result<(), anyhow::Error>;
+pub trait Storage<Index: Ord>: Send + Sync {
+	fn load(&self, epoch: EpochIndex) -> Result<Option<RleBitmap<Index>>, anyhow::Error>;
+	fn store(&self, epoch: EpochIndex, map: &RleBitmap<Index>) -> Result<(), anyhow::Error>;
 }
 
-impl Storage for (String, Arc<PersistentKeyDB>) {
-	fn load(&self, epoch: EpochIndex) -> Result<Option<roaring::RoaringTreemap>, anyhow::Error> {
+impl<Index: Ord + DeserializeOwned + Serialize> Storage<Index> for (String, Arc<PersistentKeyDB>) {
+	fn load(&self, epoch: EpochIndex) -> Result<Option<RleBitmap<Index>>, anyhow::Error> {
 		self.1.load_processed_blocks(&self.0, epoch)
 	}
-	fn store(&self, epoch: EpochIndex, map: &roaring::RoaringTreemap) -> Result<(), anyhow::Error> {
+	fn store(&self, epoch: EpochIndex, map: &RleBitmap<Index>) -> Result<(), anyhow::Error> {
 		self.1.update_processed_blocks(&self.0, epoch, map)
 	}
 }
@@ -31,13 +32,14 @@ pub struct Continuous<Inner, Store> {
 	inner: Inner,
 	store: Store,
 }
-impl<Inner: ChunkedByVault, Store: Storage> Continuous<Inner, Store> {
+impl<Inner: ChunkedByVault, Store: Storage<Inner::Index>> Continuous<Inner, Store> {
 	pub fn new(inner: Inner, store: Store) -> Self {
 		Self { inner, store }
 	}
 }
 #[async_trait::async_trait]
-impl<Inner: ChunkedByVault, Store: Storage> ChunkedByVault for Continuous<Inner, Store>
+impl<Inner: ChunkedByVault, Store: Storage<Inner::Index>> ChunkedByVault
+	for Continuous<Inner, Store>
 where
 	Inner::Client: Clone,
 {
@@ -61,17 +63,18 @@ where
 			.then(move |(epoch, chain_stream, chain_client)| async move {
 				const MAXIMUM_CONCURRENT_INPROGRESS: usize = 32;
 
-				let processed_indices = self.store.load(epoch.index).map_or(roaring::RoaringTreemap::new(), |option_processed_indices| {
-					option_processed_indices.unwrap_or(roaring::RoaringTreemap::new())
+				let processed_indices = self.store.load(epoch.index).map_or(RleBitmap::new(false), |option_processed_indices| {
+					option_processed_indices.unwrap_or(RleBitmap::new(false))
 				});
 
 				let inprogress_indices = FutureMap::default();
 
 				let unprocessed_indices = {
-					let mut processed_inverse = roaring::RoaringTreemap::full() | &processed_indices;
-					processed_inverse.remove_range(0..epoch.info.active_from_block.into());
-					let highest_processed = processed_indices.iter().last().map(|i| i.try_into().map_err(|_| ()).unwrap()).map_or(epoch.info.active_from_block, |highest_processed| std::cmp::max(highest_processed, epoch.info.active_from_block));
-					processed_inverse.remove_range(highest_processed.into()..=u64::MAX);
+					let mut processed_inverse = processed_indices.clone();
+					processed_inverse.invert();
+					processed_inverse.set_range(..epoch.info.active_from_block, false);
+					let highest_processed = processed_indices.iter(true).last().map_or(epoch.info.active_from_block, |highest_processed| std::cmp::max(highest_processed, epoch.info.active_from_block));
+					processed_inverse.set_range(highest_processed.., false);
 					processed_inverse
 				};
 
@@ -82,7 +85,7 @@ where
 						move |(mut chain_stream, chain_client, mut epoch, mut unprocessed_indices, mut inprogress_indices, mut processed_indices)| async move {
 							loop_select!(
 								let header = chain_stream.next_or_pending() => {
-									let highest_processed = processed_indices.iter().last().map(|i| i.try_into().map_err(|_| ()).unwrap()).map_or(epoch.info.active_from_block, |highest_processed| std::cmp::max(highest_processed, epoch.info.active_from_block));
+									let highest_processed = processed_indices.iter(true).last().map_or(epoch.info.active_from_block, |highest_processed| std::cmp::max(highest_processed, epoch.info.active_from_block));
 									if highest_processed < header.index {
 										for unprocessed_index in Step::forward(highest_processed, 1)..header.index {
 											if inprogress_indices.len() < MAXIMUM_CONCURRENT_INPROGRESS {
@@ -94,36 +97,36 @@ where
 													}.boxed()
 												});
 											} else {
-												unprocessed_indices.insert(unprocessed_index.into());
+												unprocessed_indices.set(unprocessed_index, true);
 											}
 										}
 									}
 
-									unprocessed_indices.remove(header.index.into());
+									unprocessed_indices.set(header.index, false);
 									inprogress_indices.remove(header.index);
-									processed_indices.insert(header.index.into());
+									processed_indices.set(header.index, true);
 									let _result = self.store.store(epoch.index, &processed_indices);
 
 									break Some((header, (chain_stream, chain_client, epoch, unprocessed_indices, inprogress_indices, processed_indices)))
 								},
 								if epoch.historic_signal.get().is_some() && processed_indices.is_superset(&{
-									let mut bitmap = roaring::RoaringTreemap::full();
-									bitmap.remove_range(0..epoch.info.active_from_block.into());
-									bitmap.remove_range((*epoch.historic_signal.get().unwrap()).into()..=u64::MAX);
+									let mut bitmap = RleBitmap::new(true);
+									bitmap.set_range(..epoch.info.active_from_block, false);
+									bitmap.set_range((*epoch.historic_signal.get().unwrap()).., false);
 									bitmap
 								}) => break None,
 								let (_, header) = inprogress_indices.next_or_pending() => {
-									processed_indices.insert(header.index.into());
+									processed_indices.set(header.index, true);
 									let _result = self.store.store(epoch.index, &processed_indices);
 
-									if let Some(unprocessed_index) = unprocessed_indices.iter().next() {
-										unprocessed_indices.remove(unprocessed_index);
-										let unprocessed_into = unprocessed_index.try_into().map_err(|_| ()).unwrap();
-										inprogress_indices.insert(unprocessed_into, {
+									let next_unprocessed_indice = unprocessed_indices.iter(true).next();
+									if let Some(unprocessed_index) = next_unprocessed_indice {
+										unprocessed_indices.set(unprocessed_index, false);
+										inprogress_indices.insert(unprocessed_index, {
 											let chain_client = chain_client.clone();
 											#[allow(clippy::redundant_async_block)]
 											async move {
-												chain_client.header_at_index(unprocessed_into).await
+												chain_client.header_at_index(unprocessed_index).await
 											}.boxed()
 										});
 									}
