3633cec02ad2a511d6b9f37e2d5bd2f17097d6ec
Maxim Shishmarev <maxim@chainflip.io>
2023-07-27 11:41:37 +1000

    refactor: rename swapping.ts -> all_concurrent_tests.ts; remove swapping.sh

--- bouncer/run.sh
+++ bouncer/run.sh
@@ -2,6 +2,6 @@ set -e
 ./commands/observe_block.ts 5
 ./commands/setup_vaults.ts
 ./commands/setup_swaps.ts
-./tests/swapping.sh
+./tests/all_concurrent_tests.ts
 ./tests/lp_deposit_expiry.ts
 ./tests/rotates_through_btc_swap.ts
