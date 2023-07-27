9bfd29c5cb1048e919808c1e37a37f4bc2d32a2d
dandanlen <3168260+dandanlen@users.noreply.github.com>
2023-07-27 15:44:20 +0200

    feat: new extrinsic for deleting swap queue (#3741)

--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -139,10 +139,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("chainflip-node"),
 	impl_name: create_runtime_str!("chainflip-node"),
 	authoring_version: 1,
-	spec_version: 12,
+	spec_version: 13,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 3,
+	transaction_version: 4,
 	state_version: 1,
 };
 
