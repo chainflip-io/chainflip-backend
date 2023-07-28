707d7c2393be4e5aa6a6f14d4d0adc809c86eaf0
Maxim Shishmarev <maxim@chainflip.io>
2023-07-28 11:47:51 +1000

    refactor: swapping tests can be run separately

--- bouncer/tests/swapping.ts
+++ bouncer/tests/swapping.ts
@@ -1,13 +1,8 @@
 #!/usr/bin/env -S pnpm tsx
-import { testLpDepositExpiry } from '../shared/lp_deposit_expiry';
 import { testAllSwaps } from '../shared/swapping';
 import { runWithTimeout } from '../shared/utils';
 
-async function runAllConcurrentTests() {
-  await Promise.all([testAllSwaps(), testLpDepositExpiry()]);
-}
-
-runWithTimeout(runAllConcurrentTests(), 1800000)
+runWithTimeout(testAllSwaps(), 1800000)
   .then(() => {
     // there are some dangling resources that prevent the process from exiting
     process.exit(0);
