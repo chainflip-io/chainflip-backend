a1cde1e205835f7e44fa827c21622d3f2a1e878f
Martin Rieke <121793148+martin-chainflip@users.noreply.github.com>
2023-07-28 14:47:36 +0200

    test flip swaps (#3735)
    * test flip swaps

--- bouncer/tests/swapping.ts
+++ bouncer/tests/swapping.ts
@@ -122,9 +122,11 @@ async function testAll() {
   // otherwise in subsequent approvals the broker might not send the transaction confusing the eth nonce.
   await approveTokenVault(
     'USDC',
-    (
-      parseInt(amountToFineAmount(defaultAssetAmounts('USDC'), assetDecimals.USDC), 10) * 4
-    ).toString(),
+    (BigInt(amountToFineAmount(defaultAssetAmounts('USDC'), assetDecimals.USDC)) * 6n).toString(),
+  );
+  await approveTokenVault(
+    'FLIP',
+    (BigInt(amountToFineAmount(defaultAssetAmounts('FLIP'), assetDecimals.FLIP)) * 6n).toString(),
   );
 
   const ccmContractSwaps = Promise.all([
@@ -140,15 +142,27 @@ async function testAll() {
       cf_parameters: getAbiEncodedMessage(['bytes', 'uint256']),
       source_address: { ETH: await getAddress('ETH', randomAsHex(32)) },
     }),
+    testSwapViaContract('FLIP', 'ETH', {
+      message: getAbiEncodedMessage(),
+      gas_budget: 10000000000000000,
+      cf_parameters: getAbiEncodedMessage(['bytes', 'uint256']),
+      source_address: { ETH: await getAddress('ETH', randomAsHex(32)) },
+    }),
   ]);
 
   const contractSwaps = Promise.all([
     testSwapViaContract('ETH', 'DOT'),
     testSwapViaContract('ETH', 'USDC'),
     testSwapViaContract('ETH', 'BTC'),
+    testSwapViaContract('ETH', 'FLIP'),
     testSwapViaContract('USDC', 'DOT'),
     testSwapViaContract('USDC', 'ETH'),
     testSwapViaContract('USDC', 'BTC'),
+    testSwapViaContract('USDC', 'FLIP'),
+    testSwapViaContract('FLIP', 'DOT'),
+    testSwapViaContract('FLIP', 'ETH'),
+    testSwapViaContract('FLIP', 'BTC'),
+    testSwapViaContract('FLIP', 'USDC'),
   ]);
 
   const regularSwaps = Promise.all([
@@ -156,12 +170,15 @@ async function testAll() {
     testSwap('ETH', 'BTC', 'P2SH'),
     testSwap('USDC', 'BTC', 'P2WPKH'),
     testSwap('DOT', 'BTC', 'P2WSH'),
+    testSwap('FLIP', 'BTC', 'P2SH'),
     testSwap('BTC', 'DOT'),
     testSwap('DOT', 'USDC'),
     testSwap('DOT', 'ETH'),
     testSwap('BTC', 'ETH'),
     testSwap('BTC', 'USDC'),
     testSwap('ETH', 'USDC'),
+    testSwap('FLIP', 'DOT'),
+    testSwap('BTC', 'FLIP'),
   ]);
 
   // NOTE: Parallelized ccm swaps with the same sourceAsset and destAsset won't work because
@@ -202,7 +219,7 @@ async function testAll() {
         }),
       },
     }),
-    testSwap('DOT', 'USDC', undefined, {
+    testSwap('DOT', 'FLIP', undefined, {
       message: getAbiEncodedMessage(),
       gas_budget: 1000000,
       cf_parameters: getAbiEncodedMessage(['address', 'uint256']),
