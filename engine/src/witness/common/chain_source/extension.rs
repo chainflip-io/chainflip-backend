daddc033b2dff9a5759d68da97076f07e5f94dac
kylezs <zsembery.kyle@gmail.com>
2023-08-01 15:56:53 +0200

    doc: chain source extensions

--- engine/src/witness/common/chain_source/extension.rs
+++ engine/src/witness/common/chain_source/extension.rs
@@ -17,6 +17,7 @@ use super::{
 
 #[async_trait::async_trait]
 pub trait ChainSourceExt: ChainSource {
+	/// Map the data of each header when the data with an async closure.
 	fn then<Output, Fut, F>(self, f: F) -> Then<Self, F>
 	where
 		Self: Sized,
@@ -27,6 +28,7 @@ pub trait ChainSourceExt: ChainSource {
 		Then::new(self, f)
 	}
 
+	/// Map the data of each header when the data is Result::Ok with an async closure.
 	fn and_then<Input, Output, Error, Fut, F>(self, f: F) -> AndThen<Self, F>
 	where
 		Self: Sized + ChainSource<Data = Result<Input, Error>>,
@@ -39,6 +41,9 @@ pub trait ChainSourceExt: ChainSource {
 		AndThen::new(self, f)
 	}
 
+	/// Apply some safety margin to the chain source, such that the chain source will lag behind by
+	/// a set margin. This is specifically for chains that don't offer deterministic finality, such
+	/// as Ethereum or Bitcoin.
 	fn lag_safety(self, margin: usize) -> LagSafety<Self>
 	where
 		Self: Sized,
@@ -46,6 +51,8 @@ pub trait ChainSourceExt: ChainSource {
 		LagSafety::new(self, margin)
 	}
 
+	/// Allows sharing an underlying chain source between multiple consumers. This ensures that work
+	/// done in previous chain source adapters is not duplicated by downstream consumers.
 	fn shared<'env>(self, scope: &Scope<'env, anyhow::Error>) -> SharedSource<Self>
 	where
 		Self: 'env + Sized,
@@ -55,6 +62,9 @@ pub trait ChainSourceExt: ChainSource {
 		SharedSource::new(self, scope)
 	}
 
+	/// Ensures the stream is always increasing with respect to the header index (normally the block
+	/// number). We don't assume the root chain source is strictly increasing, since we could
+	/// encounter reorgs.
 	fn strictly_monotonic(self) -> StrictlyMonotonic<Self>
 	where
 		Self: Sized,
@@ -62,6 +72,9 @@ pub trait ChainSourceExt: ChainSource {
 		StrictlyMonotonic::new(self)
 	}
 
+	/// Chunk the chain source by time (in blocks). Some consumers do not care about the exact
+	/// external chain block number they start and end but we only want to run it for the epoch
+	/// duration (as measured approximately by the State Chain blocks we consume).
 	fn chunk_by_time<Epochs: Into<EpochSource<(), ()>>>(
 		self,
 		epochs: Epochs,
@@ -72,6 +85,9 @@ pub trait ChainSourceExt: ChainSource {
 		ChunkedByTimeBuilder::new(ChunkByTime::new(self), epochs.into())
 	}
 
+	/// Chunk the chain source by vault. We specifically want to chunk the chain source from the
+	/// block the epoch stars at for a particular chain. This ensures we don't miss witnesses, and
+	/// allows us to only run for those epochs we are interested in.
 	fn chunk_by_vault<
 		ExtraInfo,
 		ExtraHistoricInfo,
