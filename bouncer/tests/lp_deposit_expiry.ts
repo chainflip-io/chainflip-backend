b6707b0cd90edb1283c2cf0bdb2b2b3745b6aa73
Maxim Shishmarev <msgmaxim@gmail.com>
2023-08-01 20:05:17 +1000

    Feat: run lp expiry tests concurrently to swaps (#3670)
    * refactor: make lp deposit expiry test exportable
    
    * refactor: rename swapping.ts -> all_concurrent_tests.ts; remove swapping.sh
    
    * feat: run lp expiry tests concurrently to swaps
    
    * refactor: swapping tests can be run separately

--- bouncer/tests/lp_deposit_expiry.ts
+++ bouncer/tests/lp_deposit_expiry.ts
@@ -1,45 +1,9 @@
 #!/usr/bin/env -S pnpm tsx
-import { Keyring } from '@polkadot/keyring';
-import { cryptoWaitReady } from '@polkadot/util-crypto';
-import { runWithTimeout, observeEvent, getChainflipApi } from '../shared/utils';
-import { sendBtc } from '../shared/send_btc';
-import { submitGovernanceExtrinsic } from '../shared/cf_governance';
+import { testLpDepositExpiry } from '../shared/lp_deposit_expiry';
+import { runWithTimeout } from '../shared/utils';
 
 async function main(): Promise<void> {
-  await cryptoWaitReady();
-  const keyring = new Keyring({ type: 'sr25519' });
-  const lpUri = process.env.LP_URI ?? '//LP_1';
-  const lp = keyring.createFromUri(lpUri);
-
-  const chainflip = await getChainflipApi();
-
-  console.log('=== Testing expiry of funded LP deposit address ===');
-  const originalExpiryTime = Number(await chainflip.query.liquidityProvider.lpTTL());
-  console.log('Setting expiry time for LP addresses to 10 blocks');
-
-  await submitGovernanceExtrinsic(chainflip.tx.liquidityProvider.setLpTtl(10));
-  await observeEvent('liquidityProvider:LpTtlSet', chainflip);
-
-  console.log('Requesting new BTC LP deposit address');
-  await chainflip.tx.liquidityProvider
-    .requestLiquidityDepositAddress('Btc')
-    .signAndSend(lp, { nonce: -1 });
-
-  const depositEventResult = await observeEvent(
-    'liquidityProvider:LiquidityDepositAddressReady',
-    chainflip,
-  );
-  const ingressAddress = depositEventResult.data.depositAddress.Btc;
-
-  console.log('Funding BTC LP deposit address of ' + ingressAddress + ' with 1 BTC');
-
-  await sendBtc(ingressAddress, 1);
-  await observeEvent('liquidityProvider:LiquidityDepositAddressExpired', chainflip);
-
-  console.log('Restoring expiry time for LP addresses to ' + originalExpiryTime + ' blocks');
-  await submitGovernanceExtrinsic(chainflip.tx.liquidityProvider.setLpTtl(originalExpiryTime));
-
-  await observeEvent('liquidityProvider:LpTtlSet', chainflip);
+  await testLpDepositExpiry();
   console.log('=== Test complete ===');
   process.exit(0);
 }
