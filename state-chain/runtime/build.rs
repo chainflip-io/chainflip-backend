5105e8d061761cff905530536142b6cfc10c5ee4
Daniel <daniel@chainflip.io>
2023-08-02 17:45:54 +0200

    fix: dependency resolution

--- state-chain/runtime/build.rs
+++ state-chain/runtime/build.rs
@@ -1,7 +1,6 @@
 fn main() {
 	#[cfg(feature = "std")]
 	{
-		use substrate_wasm_builder::WasmBuilder;
 		substrate_wasm_builder::WasmBuilder::new()
 			.with_current_project()
 			.export_heap_base()
