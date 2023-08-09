f3a5c720af018904c5c65bfe0f4f01336228579b
Daniel <daniel@chainflip.io>
2023-08-09 12:07:20 +0200

    fix: only restrict registrations at max set size

--- state-chain/pallets/cf-validator/src/lib.rs
+++ state-chain/pallets/cf-validator/src/lib.rs
@@ -725,11 +725,15 @@ pub mod pallet {
 		#[pallet::weight(T::ValidatorWeightInfo::register_as_validator())]
 		pub fn register_as_validator(origin: OriginFor<T>) -> DispatchResult {
 			let account_id: T::AccountId = ensure_signed(origin)?;
-			ensure!(
-				T::FundingInfo::total_balance_of(&account_id) >=
-					RegistrationBondPercentage::<T>::get() * <Self as EpochInfo>::bond(),
-				Error::<T>::NotEnoughFunds
-			);
+			if <Self as EpochInfo>::current_authority_count() >=
+				AuctionParameters::<T>::get().max_size
+			{
+				ensure!(
+					T::FundingInfo::total_balance_of(&account_id) >=
+						RegistrationBondPercentage::<T>::get() * <Self as EpochInfo>::bond(),
+					Error::<T>::NotEnoughFunds
+				);
+			}
 			T::AccountRoleRegistry::register_as_validator(&account_id)
 		}
 	}
