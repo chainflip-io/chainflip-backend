2b44fdcdb84f0a6c040defb4f8062524866df7fb
Daniel <daniel@chainflip.io>
2023-08-10 13:38:25 +0200

    fix: clippy

--- state-chain/runtime/src/chainflip/missed_authorship_slots.rs
+++ state-chain/runtime/src/chainflip/missed_authorship_slots.rs
@@ -51,24 +51,16 @@ mod test_missed_authorship_slots {
 	use codec::Encode;
 	use frame_support::{
 		construct_runtime, parameter_types,
-		sp_runtime::{
-			testing::{Header, UintAuthorityId},
-			traits::IdentityLookup,
-			BuildStorage, Digest,
-		},
+		sp_runtime::{testing::UintAuthorityId, traits::IdentityLookup, BuildStorage, Digest},
 		traits::{ConstU32, ConstU64, OnInitialize},
 	};
 	use sp_consensus_aura::ed25519::AuthorityId;
 	use sp_core::ConstBool;
 
-	type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 	type Block = frame_system::mocking::MockBlock<Test>;
 
 	construct_runtime!(
-		pub enum Test where
-			Block = Block,
-			NodeBlock = Block,
-			UncheckedExtrinsic = UncheckedExtrinsic,
+		pub enum Test
 		{
 			System: frame_system,
 			Timestamp: pallet_timestamp,
