86583acf5ffb034f70ee147b19326f796a7aaad1
kylezs <kyle@chainflip.io>
2023-08-11 00:42:28 +1000

    refactor: generalise vault witnessing (#3827)
    * refactor: generalise vault_witnessing
    
    * chore: delete unused RPC
    
    * chore: use debug for hash
    
    * chore: rename to supported_erc20_tokens

--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -853,12 +853,6 @@ impl_runtime_apis! {
 		fn cf_eth_flip_token_address() -> EthereumAddress {
 			Environment::supported_eth_assets(cf_primitives::chains::assets::eth::Asset::Flip).expect("FLIP token address should exist")
 		}
-		fn cf_eth_asset(token_address: EthereumAddress) -> Option<Asset> {
-			use pallet_cf_environment::EthereumSupportedAssets;
-			EthereumSupportedAssets::<Runtime>::iter()
-				.find(|(_, address)| *address == token_address)
-				.map(|(asset, _)| asset.into())
-		}
 		fn cf_eth_state_chain_gateway_address() -> EthereumAddress {
 			Environment::state_chain_gateway_address()
 		}
