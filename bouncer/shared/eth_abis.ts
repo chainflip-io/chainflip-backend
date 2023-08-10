622de2b0b5f9c382a1ac9cf29b3b6b34879f3ac9
albert <allimos3@gmail.com>
2023-08-10 12:34:08 +0200

    chore: update to perseverance-0.9-rc4

--- bouncer/shared/eth_abis.ts
+++ bouncer/shared/eth_abis.ts
@@ -14,11 +14,11 @@ function loadContractCached(abiPath: string) {
     return cached;
   };
 }
-
+const CF_ETH_CONTRACT_ABI_TAG = 'perseverance-0.9-rc3';
 export const getErc20abi = loadContractCached('../eth-contract-abis/IERC20.json');
 export const getGatewayAbi = loadContractCached(
-  '../eth-contract-abis/perseverance-0.9-rc3/IStateChainGateway.json',
+  '../eth-contract-abis/' + CF_ETH_CONTRACT_ABI_TAG + '/IStateChainGateway.json',
 );
 export const getCFTesterAbi = loadContractCached(
-  '../eth-contract-abis/perseverance-0.9-rc3/CFTester.json',
+  '../eth-contract-abis/' + CF_ETH_CONTRACT_ABI_TAG + '/CFTester.json',
 );
