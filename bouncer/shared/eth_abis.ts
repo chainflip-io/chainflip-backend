6001bb85652a9cc98e6b92219e989cfd8391d75b
albert <allimos3@gmail.com>
2023-08-11 11:43:20 +0200

    chore: PR comment

--- bouncer/shared/eth_abis.ts
+++ bouncer/shared/eth_abis.ts
@@ -17,8 +17,8 @@ function loadContractCached(abiPath: string) {
 const CF_ETH_CONTRACT_ABI_TAG = 'perseverance-0.9-rc4';
 export const getErc20abi = loadContractCached('../eth-contract-abis/IERC20.json');
 export const getGatewayAbi = loadContractCached(
-  '../eth-contract-abis/' + CF_ETH_CONTRACT_ABI_TAG + '/IStateChainGateway.json',
+  `../eth-contract-abis/${CF_ETH_CONTRACT_ABI_TAG}/IStateChainGateway.json`,
 );
 export const getCFTesterAbi = loadContractCached(
-  '../eth-contract-abis/' + CF_ETH_CONTRACT_ABI_TAG + '/CFTester.json',
+  `../eth-contract-abis/${CF_ETH_CONTRACT_ABI_TAG}//CFTester.json`,
 );
