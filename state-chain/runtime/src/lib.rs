eb86cbdcc5908378d31055fae52f321a861f4459
Daniel <daniel@chainflip.io>
2023-08-17 12:30:14 +0200

    chore: version bump

--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -139,7 +139,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("chainflip-node"),
 	impl_name: create_runtime_str!("chainflip-node"),
 	authoring_version: 1,
-	spec_version: 13,
+	spec_version: 14,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 4,
