d98386db6471c703b1b5adadabe07f588ca2145a
Maxim Shishmarev <maxim@chainflip.io>
2023-08-18 16:24:32 +1000

    fix: only update available utxo on success

--- state-chain/chains/src/btc/utxo_selection.rs
+++ state-chain/chains/src/btc/utxo_selection.rs
@@ -17,13 +17,15 @@ use super::GetUtxoAmount;
 /// algorithm will skip the selection of that utxo and will keep it in the list of available utxos
 /// for future use when the fee possibly comes down so that it is feasable to select these utxos.
 pub fn select_utxos_from_pool<UTXO: GetUtxoAmount + Clone>(
-	available_utxos: &mut Vec<UTXO>,
+	available_utxos_out: &mut Vec<UTXO>,
 	fee_per_utxo: u64,
 	amount_to_be_spent: u64,
 ) -> Option<(Vec<UTXO>, u64)> {
-	if available_utxos.is_empty() {
+	if available_utxos_out.is_empty() {
 		return None
 	}
+
+	let mut available_utxos = available_utxos_out.clone();
 
 	available_utxos.sort_by_key(|utxo| sp_std::cmp::Reverse(utxo.amount()));
 
@@ -52,15 +54,17 @@ pub fn select_utxos_from_pool<UTXO: GetUtxoAmount + Clone>(
 
 	available_utxos.append(&mut skipped_utxos);
 
+	*available_utxos_out = available_utxos;
 	Some((selected_utxos, cumulative_amount))
 }
 
 #[test]
 fn test_utxo_selection() {
 	use super::GetUtxoAmount;
+	use std::collections::BTreeSet;
 
 	#[allow(clippy::upper_case_acronyms)]
-	#[derive(Clone, Debug, PartialEq, Eq)]
+	#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 	pub struct UTXO {
 		pub amount: u64,
 	}
@@ -72,7 +76,7 @@ fn test_utxo_selection() {
 
 	const FEE_PER_UTXO: u64 = 2;
 
-	let mut available_utxos = vec![
+	let available_utxos = vec![
 		UTXO { amount: 110 },
 		UTXO { amount: 25 },
 		UTXO { amount: 500 },
@@ -84,77 +88,91 @@ fn test_utxo_selection() {
 		UTXO { amount: 1000 },
 		UTXO { amount: 768 },
 	];
+
+	#[track_caller]
+	fn test_case(
+		initial_available_utxos: &Vec<UTXO>,
+		fee_per_utxo: u64,
+		amount_to_be_spent: u64,
+		expected_selection: Option<(Vec<UTXO>, u64)>,
+	) {
+		let mut utxos = initial_available_utxos.clone();
+		let selected = select_utxos_from_pool(&mut utxos, fee_per_utxo, amount_to_be_spent);
+
+		assert_eq!(selected, expected_selection);
+
+		// check remaining utxos:
+		let initial = initial_available_utxos.iter().collect::<BTreeSet<_>>();
+		let selected = selected.map(|x| x.0).unwrap_or_default();
+		let selected = selected.iter().collect::<BTreeSet<_>>();
+		let remaining = utxos.iter().collect::<BTreeSet<_>>();
+
+		assert_eq!(
+			initial.difference(&selected).copied().collect::<BTreeSet<_>>(),
+			remaining,
+			"remaining utxos do not match"
+		);
+	}
 
 	// empty utxo list as input should return Option::None.
-	assert_eq!(select_utxos_from_pool(&mut Vec::<UTXO>::new(), FEE_PER_UTXO, 0), None);
-	assert_eq!(
-		select_utxos_from_pool(&mut available_utxos.clone(), FEE_PER_UTXO, 1),
-		Some((vec![UTXO { amount: 7 }, UTXO { amount: 15 }], 18))
+	test_case(&Default::default(), 0, FEE_PER_UTXO, None);
+
+	test_case(
+		&available_utxos,
+		FEE_PER_UTXO,
+		1,
+		Some((vec![UTXO { amount: 7 }, UTXO { amount: 15 }], 18)),
 	);
-	assert_eq!(
-		select_utxos_from_pool(&mut available_utxos.clone(), FEE_PER_UTXO, 18),
-		Some((vec![UTXO { amount: 7 }, UTXO { amount: 15 }, UTXO { amount: 19 }], 35))
+
+	test_case(
+		&available_utxos,
+		FEE_PER_UTXO,
+		18,
+		Some((vec![UTXO { amount: 7 }, UTXO { amount: 15 }, UTXO { amount: 19 }], 35)),
 	);
-	assert_eq!(
-		select_utxos_from_pool(&mut available_utxos.clone(), FEE_PER_UTXO, 19),
+
+	test_case(
+		&available_utxos,
+		FEE_PER_UTXO,
+		19,
 		Some((
 			vec![UTXO { amount: 7 }, UTXO { amount: 15 }, UTXO { amount: 19 }, UTXO { amount: 20 }],
-			53
-		))
+			53,
+		)),
 	);
 
-	let all_selected_utxos = vec![
-		UTXO { amount: 7 },
-		UTXO { amount: 15 },
-		UTXO { amount: 19 },
-		UTXO { amount: 20 },
-		UTXO { amount: 25 },
-		UTXO { amount: 41 },
-		UTXO { amount: 110 },
-		UTXO { amount: 500 },
-		UTXO { amount: 768 },
-		UTXO { amount: 1000 },
-	];
+	let all_selected_utxos = {
+		let mut utxos = available_utxos.clone();
+		utxos.sort();
+		utxos
+	};
 
 	// The amount that will cause all utxos to be selected
-	assert_eq!(
-		select_utxos_from_pool(&mut available_utxos.clone(), FEE_PER_UTXO, 2000),
-		Some((all_selected_utxos.clone(), 2485))
-	);
-	// max amount that can be spent with the given utxos.
-	assert_eq!(
-		select_utxos_from_pool(&mut available_utxos.clone(), FEE_PER_UTXO, 2485),
-		Some((all_selected_utxos, 2485))
-	);
-	// entering the amount greater than the max spendable amount will
-	// cause the function to return no utxos
-	assert_eq!(select_utxos_from_pool(&mut available_utxos.clone(), FEE_PER_UTXO, 100000), None);
+	test_case(&available_utxos, FEE_PER_UTXO, 2000, Some((all_selected_utxos.clone(), 2485)));
 
-	// choosing the fee to spend the input utxo as greater than the amounts in the 2 smallest utxos
-	// will cause the algorithm to skip the selection of those 2 utxos and adding it to the list of
-	// available utxos for future use.
-	assert_eq!(
-		select_utxos_from_pool(&mut available_utxos, 16, 19),
+	// Max amount that can be spent with the given utxos.
+	test_case(&available_utxos, FEE_PER_UTXO, 2485, Some((all_selected_utxos, 2485)));
+
+	// Entering the amount greater than the max spendable amount will
+	// cause the function to return no utxos
+	test_case(&available_utxos, FEE_PER_UTXO, 100000, None);
+
+	// choosing the fee to spend the input utxo as greater than the amounts in the 2 smallest
+	// utxos will cause the algorithm to skip the selection of those 2 utxos and adding it to the
+	// list of available utxos for future use.
+	test_case(
+		&available_utxos,
+		16,
+		19,
 		Some((
 			vec![
 				UTXO { amount: 19 },
 				UTXO { amount: 20 },
 				UTXO { amount: 25 },
 				UTXO { amount: 41 },
-				UTXO { amount: 110 }
+				UTXO { amount: 110 },
 			],
-			135
-		))
+			135,
+		)),
 	);
-
-	assert_eq!(
-		available_utxos,
-		vec![
-			UTXO { amount: 1000 },
-			UTXO { amount: 768 },
-			UTXO { amount: 500 },
-			UTXO { amount: 7 },
-			UTXO { amount: 15 }
-		],
-	)
 }
