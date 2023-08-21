753f2e43d8c93a064b2777e6afe0cf822ecdb2eb
kylezs <kyle@chainflip.io>
2023-08-22 00:41:22 +1000

    chore: remove update asset (#3869)

--- state-chain/pallets/cf-environment/src/tests.rs
+++ state-chain/pallets/cf-environment/src/tests.rs
@@ -1,15 +1,12 @@
 #![cfg(test)]
-use cf_chains::{
-	btc::{api::UtxoSelectionType, deposit_address::DepositAddress, BtcAmount, Utxo},
-	eth::Address as EthereumAddress,
-};
-use cf_primitives::{chains::assets::eth::Asset, SemVer};
+use cf_chains::btc::{api::UtxoSelectionType, deposit_address::DepositAddress, Utxo};
+use cf_primitives::SemVer;
 use cf_traits::SafeMode;
-use frame_support::{assert_noop, assert_ok, traits::OriginTrait};
+use frame_support::{assert_ok, traits::OriginTrait};
 
-use crate::{EthereumSupportedAssets, RuntimeSafeMode, SafeModeUpdate};
+use crate::{RuntimeSafeMode, SafeModeUpdate};
 
-use crate::{mock::*, Error};
+use crate::mock::*;
 
 #[test]
 fn genesis_config() {
@@ -21,43 +18,8 @@ fn genesis_config() {
 }
 
 #[test]
-fn update_supported_eth_assets() {
-	new_test_ext().execute_with(|| {
-		const ADDRESS: EthereumAddress = EthereumAddress::repeat_byte(2);
-		// Expect the FLIP token address to be set after genesis
-		assert!(EthereumSupportedAssets::<Test>::contains_key(Asset::Flip));
-		// Update the address for Usdc
-		assert_ok!(Environment::update_supported_eth_assets(
-			RuntimeOrigin::root(),
-			Asset::Usdc,
-			ADDRESS
-		));
-		assert_eq!(EthereumSupportedAssets::<Test>::get(Asset::Usdc), Some(ADDRESS));
-		assert_eq!(
-			frame_system::Pallet::<Test>::events()
-				.pop()
-				.expect("Event should be emitted!")
-				.event,
-			crate::mock::RuntimeEvent::Environment(crate::Event::UpdatedEthAsset(
-				Asset::Usdc,
-				ADDRESS
-			),)
-		);
-		// Last but not least - verify we can not add an address for ETH
-		assert_noop!(
-			Environment::update_supported_eth_assets(
-				RuntimeOrigin::root(),
-				Asset::Eth,
-				EthereumAddress::repeat_byte(3)
-			),
-			<Error<Test>>::EthAddressNotUpdateable
-		);
-	});
-}
-
-#[test]
 fn test_btc_utxo_selection() {
-	fn add_utxo_amount(amount: BtcAmount) {
+	fn add_utxo_amount(amount: crate::BtcAmount) {
 		Environment::add_bitcoin_utxo_to_list(
 			amount,
 			Default::default(),
