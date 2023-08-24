6852a1d57199195bc2f382d3f7670173e76d4f08
Martin Rieke <martin@chainflip.io>
2023-08-24 13:06:22 +0200

    update bouncer for new lp-interface

--- bouncer/shared/create_lp_pool.ts
+++ bouncer/shared/create_lp_pool.ts
@@ -5,7 +5,7 @@ import { submitGovernanceExtrinsic } from './cf_governance';
 export async function createLpPool(ccy: Asset, initialPrice: number) {
   const chainflip = await getChainflipApi();
 
-  if ((await chainflip.query.liquidityPools.pools(ccy.toLowerCase())).toJSON()! === null) {
+  if ((await chainflip.query.liquidityPools.pools({assets: {one: 'usdc', zero: ccy.toLowerCase()}})).toJSON()! === null) {
     const price = BigInt(
       Math.round(
         Math.sqrt(initialPrice / 10 ** (assetDecimals[ccy] - assetDecimals.USDC)) * 2 ** 96,
@@ -17,9 +17,9 @@ export async function createLpPool(ccy: Asset, initialPrice: number) {
     const poolCreatedEvent = observeEvent(
       'liquidityPools:NewPoolCreated',
       chainflip,
-      (event) => event.data.unstableAsset.toUpperCase() === ccy,
+      (event) => event.data.pairAsset.toUpperCase() === ccy,
     );
-    const extrinsic = chainflip.tx.liquidityPools.newPool(ccy.toLowerCase(), 0, price);
+    const extrinsic = chainflip.tx.liquidityPools.newPool('usdc', ccy.toLowerCase(), 10, price);
     await submitGovernanceExtrinsic(extrinsic);
     await poolCreatedEvent;
   }
--- bouncer/shared/range_order.ts
+++ bouncer/shared/range_order.ts
@@ -19,18 +19,18 @@ export async function rangeOrder(ccy: Asset, amount: number) {
   const lpUri = process.env.LP_URI || '//LP_1';
   const lp = keyring.createFromUri(lpUri);
 
-  const currentSqrtPrice = (await chainflip.query.liquidityPools.pools(ccy.toLowerCase())).toJSON()!
+  const currentSqrtPrice = (await chainflip.query.liquidityPools.pools({assets: {one: 'usdc', zero: ccy.toLowerCase()}})).toJSON()!
     .poolState.rangeOrders.currentSqrtPrice;
   const liquidity = BigInt(Math.round((currentSqrtPrice / 2 ** 96) * Number(fineAmount)));
   console.log('Setting up ' + ccy + ' range order');
   const orderCreatedEvent = observeEvent(
-    'liquidityPools:RangeOrderMinted',
+    'liquidityPools:RangeOrderUpdated',
     chainflip,
-    (event) => event.data.lp === lp.address && event.data.unstableAsset.toUpperCase() === ccy,
+    (event) => event.data.lp === lp.address && event.data.pairAsset.toUpperCase() === ccy && event.data.id == 0,
   );
   await lpMutex.runExclusive(async () => {
     await chainflip.tx.liquidityPools
-      .collectAndMintRangeOrder(ccy.toLowerCase(), [-887272, 887272], { Liquidity: liquidity })
+      .setRangeOrder('usdc', ccy.toLowerCase(), 0, [-887272, 887272], {Liquidity:{ Liquidity: liquidity }})
       .signAndSend(lp, { nonce: -1 }, handleSubstrateError(chainflip));
   });
   await orderCreatedEvent;
