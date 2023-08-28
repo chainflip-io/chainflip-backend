376e0e588b18808729ac0711e359fe99936da9e5
kylezs <zsembery.kyle@gmail.com>
2023-08-28 14:02:26 +0200

    test: add lag safety tests

--- engine/src/witness/common/chain_source/lag_safety.rs
+++ engine/src/witness/common/chain_source/lag_safety.rs
@@ -55,7 +55,7 @@ where
 					utilities::loop_select!(
 						if let Some(header) = chain_stream.next() => {
 							let header_index = header.index;
-							if unsafe_cache.back().map_or(false, |last_header| Some(&last_header.hash) != header.parent_hash.as_ref() || Step::forward_checked(last_header.index, 1) != Some(header.index)) {
+							if unsafe_cache.back().map_or(false, |last_header| Some(&last_header.hash) != header.parent_hash.as_ref() || Step::forward_checked(last_header.index, 1) != Some(header_index)) {
 								unsafe_cache.clear();
 							}
 							unsafe_cache.push_back(header);
@@ -82,4 +82,229 @@ where
 	InnerSource::Client: Clone,
 {
 	type Chain = InnerSource::Chain;
+}
+
+#[cfg(test)]
+mod tests {
+	use std::{ops::Range, sync::Arc};
+
+	use crate::common::Mutex;
+
+	use super::*;
+
+	use futures::Stream;
+
+	#[derive(Clone)]
+	pub struct MockChainClient {
+		// These are the indices that have been queried, not got from the inner stream.
+		queried_indices: Arc<Mutex<Vec<u64>>>,
+	}
+
+	impl MockChainClient {
+		pub async fn queried_indices(&self) -> Vec<u64> {
+			let guard = self.queried_indices.lock().await;
+			guard.clone()
+		}
+	}
+
+	#[async_trait::async_trait]
+	impl ChainClient for MockChainClient {
+		type Index = u64;
+
+		type Hash = u64;
+
+		type Data = ();
+
+		async fn header_at_index(
+			&self,
+			index: Self::Index,
+		) -> Header<Self::Index, Self::Hash, Self::Data> {
+			let mut queried = self.queried_indices.lock().await;
+			queried.push(index);
+			Header { index, hash: index, parent_hash: Some(index - 1), data: () }
+		}
+	}
+
+	pub struct MockChainSource<HeaderStream: Stream<Item = Header<u64, u64, ()>> + Send + Sync> {
+		stream: Arc<Mutex<Option<HeaderStream>>>,
+		client: MockChainClient,
+	}
+
+	impl<HeaderStream: Stream<Item = Header<u64, u64, ()>> + Send + Sync>
+		MockChainSource<HeaderStream>
+	{
+		fn new(stream: HeaderStream) -> Self {
+			Self {
+				stream: Arc::new(Mutex::new(Some(stream))),
+				client: MockChainClient { queried_indices: Arc::new(Mutex::new(Vec::new())) },
+			}
+		}
+	}
+
+	#[async_trait::async_trait]
+	impl<HeaderStream: Stream<Item = Header<u64, u64, ()>> + Send + Sync> ChainSource
+		for MockChainSource<HeaderStream>
+	{
+		type Index = u64;
+		type Hash = u64;
+		type Data = ();
+
+		type Client = MockChainClient;
+
+		async fn stream_and_client(
+			&self,
+		) -> (BoxChainStream<'_, Self::Index, Self::Hash, Self::Data>, Self::Client) {
+			let mut guard = self.stream.lock().await;
+			let stream = guard.take().unwrap();
+			(Box::pin(stream), self.client.clone())
+		}
+	}
+
+	pub fn normal_header(index: u64) -> Header<u64, u64, ()> {
+		Header { index, hash: index, parent_hash: Some(index - 1), data: () }
+	}
+
+	#[tokio::test]
+	async fn empty_inner_stream_returns_empty_no_lag() {
+		let mock_chain_source = MockChainSource::new(stream::empty());
+
+		let lag_safety = LagSafety::new(mock_chain_source, 0);
+
+		let (mut chain_stream, client) = lag_safety.stream_and_client().await;
+
+		assert!(chain_stream.next().await.is_none());
+		assert!(client.queried_indices().await.is_empty())
+	}
+
+	#[tokio::test]
+	async fn empty_inner_stream_returns_empty_with_lag() {
+		let mock_chain_source = MockChainSource::new(stream::empty());
+
+		let lag_safety = LagSafety::new(mock_chain_source, 4);
+
+		let (mut chain_stream, client) = lag_safety.stream_and_client().await;
+
+		assert!(chain_stream.next().await.is_none());
+		assert!(client.queried_indices().await.is_empty())
+	}
+
+	#[tokio::test]
+	async fn no_margin_passes_through() {
+		const INDICES: Range<u64> = 5u64..10;
+		let mock_chain_source = MockChainSource::new(stream::iter(INDICES).map(normal_header));
+
+		let lag_safety = LagSafety::new(mock_chain_source, 0);
+
+		let (mut chain_stream, client) = lag_safety.stream_and_client().await;
+
+		for i in INDICES {
+			assert_eq!(chain_stream.next().await.unwrap().index, i);
+		}
+		assert!(chain_stream.next().await.is_none());
+		// If we pass the stream straight through, we don't need to query for any blocks.
+		assert!(client.queried_indices().await.is_empty())
+	}
+
+	#[tokio::test]
+	async fn margin_holds_up_blocks() {
+		const INDICES: Range<u64> = 5u64..10;
+		const MARGIN: usize = 2;
+		let mock_chain_source = MockChainSource::new(stream::iter(INDICES).map(normal_header));
+		let lag_safety = LagSafety::new(mock_chain_source, MARGIN);
+
+		let (mut chain_stream, client) = lag_safety.stream_and_client().await;
+
+		for i in (INDICES.start - MARGIN as u64)..(INDICES.end - MARGIN as u64) {
+			assert_eq!(chain_stream.next().await.unwrap().index, i);
+		}
+		assert!(chain_stream.next().await.is_none());
+		assert_eq!(client.queried_indices().await, vec![3, 4]);
+	}
+
+	fn test_header(index: u64, hash: u64, parent_hash: u64) -> Header<u64, u64, ()> {
+		Header { index, hash, parent_hash: Some(parent_hash), data: () }
+	}
+
+	// Normally this isn't going to occur because the strictly monotonic will be applied first.
+	#[tokio::test]
+	async fn duplicate_block_index_produces_duplicate_output_blocks() {
+		const MARGIN: usize = 1;
+
+		// one block fork
+		let mock_chain_source =
+			MockChainSource::new(stream::iter([test_header(5, 5, 4), test_header(5, 55, 4)]));
+
+		let lag_safety = LagSafety::new(mock_chain_source, MARGIN);
+
+		let (mut chain_stream, _) = lag_safety.stream_and_client().await;
+
+		assert_eq!(chain_stream.next().await, Some(test_header(4, 4, 3)));
+		assert_eq!(chain_stream.next().await, Some(test_header(4, 4, 3)));
+		assert!(chain_stream.next().await.is_none());
+	}
+
+	#[tokio::test]
+	async fn reorg_with_depth_equal_to_safety_margin_queries_for_correct_blocks() {
+		const MARGIN: usize = 3;
+
+		let mock_chain_source = MockChainSource::new(stream::iter([
+			// these three are on a bad fork
+			test_header(5, 55, 44),
+			test_header(6, 66, 55),
+			test_header(7, 77, 66),
+			// canonical chain
+			normal_header(8),
+			normal_header(9),
+			normal_header(10),
+			normal_header(11),
+			normal_header(12),
+			normal_header(13),
+		]));
+
+		let lag_safety = LagSafety::new(mock_chain_source, MARGIN);
+		let (mut chain_stream, client) = lag_safety.stream_and_client().await;
+
+		for i in (5 - MARGIN as u64)..=(13 - MARGIN as u64) {
+			assert_eq!(chain_stream.next().await, Some(normal_header(i)));
+		}
+
+		assert!(chain_stream.next().await.is_none());
+		assert_eq!(client.queried_indices().await, vec![2, 3, 4, 5, 6, 7]);
+	}
+
+	// This is not ideal, but it's an accepted risk. We test this to ensure that we don't crash or
+	// some other strange behaviour.
+	#[tokio::test]
+	async fn reorg_with_depth_less_than_safety_margin_passes_through_bad_block() {
+		const MARGIN: usize = 2;
+
+		let mock_chain_source = MockChainSource::new(stream::iter([
+			// these three are on a bad fork
+			test_header(5, 55, 44),
+			test_header(6, 66, 55),
+			test_header(7, 77, 66),
+			// canonical chain
+			normal_header(8),
+			normal_header(9),
+			normal_header(10),
+			normal_header(11),
+			normal_header(12),
+		]));
+
+		let lag_safety = LagSafety::new(mock_chain_source, MARGIN);
+		let (mut chain_stream, client) = lag_safety.stream_and_client().await;
+
+		for i in (5 - MARGIN as u64)..(5 as u64) {
+			assert_eq!(chain_stream.next().await, Some(normal_header(i)));
+		}
+		// Here's the bad block, we consider it safe now, because within the bad fork it's safe.
+		assert_eq!(chain_stream.next().await.unwrap(), test_header(5, 55, 44));
+
+		for i in (6 as u64)..(12 - MARGIN as u64) {
+			assert_eq!(chain_stream.next().await, Some(normal_header(i)));
+		}
+
+		// NB: No 5, since we returned as safe on the bad fork.
+		assert_eq!(client.queried_indices().await, vec![3, 4, 6, 7]);
+	}
 }
