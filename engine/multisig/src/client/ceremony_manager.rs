f57897a2e94f9ff6087445b89ea43024467f8aea
marcellorigotti <marcello@chainflip.io>
2023-08-29 16:51:54 +0200

    added gauge to keep track of the number of unauthorized ceremonies

--- engine/multisig/src/client/ceremony_manager.rs
+++ engine/multisig/src/client/ceremony_manager.rs
@@ -48,16 +48,23 @@ use super::{
 };
 
 use lazy_static;
-use prometheus::{IntCounterVec, Opts, Registry};
+use prometheus::{IntCounterVec, Opts, Registry, IntGauge};
 lazy_static::lazy_static! {
 	pub static ref CEREMONY_MANAGER_BAD_MSG: IntCounterVec = IntCounterVec::new(Opts::new("CEREMONY_MANAGER_BAD_MSG", "Count all the bad msgs received by the ceremony manager and labels them by the reason they got discarded and the sender Id"), &["reason", "senderId"]).expect("Metric succesfully created");
+	pub static ref UNAUTHORIZED_CEREMONY: IntGauge = IntGauge::new("unauthorized_ceremony", "Gauge keeping track of the number of unauthorized ceremony beeing run").expect("Metric succesfully created");
 	pub static ref CEREMONY_RUNNER_BAD_MSG: IntCounterVec = IntCounterVec::new(Opts::new("CEREMONY_RUNNER_BAD_MSG", "Count all the bad msgs received by the ceremony runner and labels them by the reason they got discarded and the sender Id"), &["reason", "senderId"]).expect("Metric succesfully created");
+
+	pub static ref CEREMONY_PROCESSED_MSG: IntCounterVec = IntCounterVec::new(Opts::new("ceremony_msg", "Count all the messages for a give ceremony"), &["ceremonyId", "fromAccountId"]).expect("Metric succesfully created");
 }
 
 fn register_metrics(reg: Registry) {
 	reg.register(Box::new(CEREMONY_MANAGER_BAD_MSG.clone()))
 		.expect("Metric succesfully register");
+	reg.register(Box::new(UNAUTHORIZED_CEREMONY.clone()))
+		.expect("Metric succesfully register");
 	reg.register(Box::new(CEREMONY_RUNNER_BAD_MSG.clone()))
+		.expect("Metric succesfully register");
+	reg.register(Box::new(CEREMONY_PROCESSED_MSG.clone()))
 		.expect("Metric succesfully register");
 }
 
@@ -732,7 +739,8 @@ impl<Ceremony: CeremonyTrait> CeremonyStates<Ceremony> {
 						matches!(handle.request_state, CeremonyRequestState::Unauthorised(_))
 					})
 					.count() + 1;
-
+				
+				UNAUTHORIZED_CEREMONY.set(total.try_into().unwrap());
 				trace!("Creating unauthorised ceremony {ceremony_id_string} (Total: {total})",);
 				self.ceremony_handles.insert(
 					ceremony_id,
