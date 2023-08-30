75979b8c6f6c176b6bc8e7222359dc6a97407964
marcellorigotti <marcello@chainflip.io>
2023-08-30 16:06:17 +0200

    cargo fmt

--- engine/multisig/src/client/ceremony_manager.rs
+++ engine/multisig/src/client/ceremony_manager.rs
@@ -48,7 +48,7 @@ use super::{
 };
 
 use lazy_static;
-use prometheus::{IntCounterVec, Opts, Registry, IntGaugeVec};
+use prometheus::{IntCounterVec, IntGaugeVec, Opts, Registry};
 lazy_static::lazy_static! {
 	pub static ref CEREMONY_MANAGER_BAD_MSG: IntCounterVec = IntCounterVec::new(Opts::new("CEREMONY_MANAGER_BAD_MSG", "Count all the bad msgs received by the ceremony manager and labels them by the reason they got discarded and the sender Id"), &["reason", "senderId"]).expect("Metric succesfully created");
 	pub static ref UNAUTHORIZED_CEREMONY: IntGaugeVec = IntGaugeVec::new(Opts::new("unauthorized_ceremony", "Gauge keeping track of the number of unauthorized ceremony beeing run"), &["chain"]).expect("Metric succesfully created");
@@ -740,7 +740,9 @@ impl<Ceremony: CeremonyTrait> CeremonyStates<Ceremony> {
 					})
 					.count() + 1;
 
-				UNAUTHORIZED_CEREMONY.with_label_values(&[Chain::NAME]).set(total.try_into().unwrap());
+				UNAUTHORIZED_CEREMONY
+					.with_label_values(&[Chain::NAME])
+					.set(total.try_into().unwrap());
 				trace!("Creating unauthorised ceremony {ceremony_id_string} (Total: {total})",);
 				self.ceremony_handles.insert(
 					ceremony_id,
