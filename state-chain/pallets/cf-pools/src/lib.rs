97918f06a25426e704eeba215cd11293eed0f026
Alastair Holmes <holmes.alastair@outlook.com>
2023-09-06 17:51:08 +0200

    fix: codec skip on phantom

--- state-chain/pallets/cf-pools/src/lib.rs
+++ state-chain/pallets/cf-pools/src/lib.rs
@@ -44,6 +44,8 @@ enum Stability {
 #[scale_info(skip_type_params(T))]
 pub struct CanonicalAssetPair<T: Config> {
 	assets: cf_amm::common::SideMap<Asset>,
+	#[doc(hidden)]
+	#[codec(skip)]
 	_phantom: core::marker::PhantomData<T>,
 }
 impl<T: Config> Copy for CanonicalAssetPair<T> {}
