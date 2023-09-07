cc18d04c33446c8dfb353a9560901a7b2eccd576
Jan Börner <jan@chainflip.io>
2023-09-07 16:25:55 +0200

    fix: :bug: fixed eslint

--- bouncer/shared/cf_governance.ts
+++ bouncer/shared/cf_governance.ts
@@ -17,7 +17,7 @@ export const snowWhite = keyring.createFromUri(snowWhiteUri);
 
 export async function submitGovernanceExtrinsic(
   extrinsic: SubmittableExtrinsic<'promise'>,
-  preAuthorise: boolean = false,
+  preAuthorise = false,
 ) {
   return snowWhiteMutex.runExclusive(async () =>
     chainflip.tx.governance
