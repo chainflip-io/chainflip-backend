f52083d08cd2fde9c178135dd3c6c3cc0b32ab3a
kylezs <zsembery.kyle@gmail.com>
2023-09-11 15:16:33 +0200

    test: add multi governance members test to concurrent

--- bouncer/tests/all_concurrent_tests.ts
+++ bouncer/tests/all_concurrent_tests.ts
@@ -4,12 +4,13 @@ import { testAllSwaps } from '../shared/swapping';
 import { testEthereumDeposits } from '../shared/ethereum_deposits';
 import { runWithTimeout, observeBadEvents } from '../shared/utils';
 import { testFundRedeem } from '../shared/fund_redeem';
+import { testMultipleMembersGovernance } from '../shared/multiple_members_governance';
 
 async function runAllConcurrentTests() {
   let stopObserving = false;
   const observingBadEvents = observeBadEvents(':BroadcastAborted', () => stopObserving);
 
-  await Promise.all([testAllSwaps(), testLpDepositExpiry(), testEthereumDeposits(), testFundRedeem("redeem")]);
+  await Promise.all([testAllSwaps(), testLpDepositExpiry(), testEthereumDeposits(), testFundRedeem("redeem"), testMultipleMembersGovernance()]);
 
   // Gracefully exit the broadcast abort observer
   stopObserving = true;
