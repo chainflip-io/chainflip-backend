1676b99fa07778cbdc640ea94e830168f865dca2
Jamie Ford <Jamie@chainflip.io>
2023-09-12 15:57:41 +1000

    feat: use SecretUrl type for endpoints

--- utilities/src/with_std/redact_endpoint_secret.rs
+++ utilities/src/with_std/redact_endpoint_secret.rs
@@ -1,15 +1,71 @@
-use anyhow::Context;
 use regex::Regex;
+use serde::Deserialize;
+use std::fmt::{Debug, Display};
 use url::Url;
 
 const MAX_SECRET_CHARACTERS_REVEALED: usize = 3;
 const SCHEMA_PADDING_LEN: usize = 3;
 
+/// A wrapper around a `String` that redacts a secret in the url when displayed. Used for node
+/// endpoints.
+#[derive(Clone, PartialEq, Eq, Deserialize, Default)]
+pub struct SecretUrl(String);
+
+impl Display for SecretUrl {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "{}", redact_secret_node_endpoint(&self.0))
+	}
+}
+
+// Only debug print the secret in debug mode
+#[cfg(debug_assertions)]
+impl Debug for SecretUrl {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "{:?}", self.0)
+	}
+}
+#[cfg(not(debug_assertions))]
+impl Debug for SecretUrl {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "{:?}", redact_secret_node_endpoint(&self.0))
+	}
+}
+
+impl From<String> for SecretUrl {
+	fn from(s: String) -> Self {
+		SecretUrl(s)
+	}
+}
+
+impl<'a> From<&'a str> for SecretUrl {
+	fn from(s: &'a str) -> Self {
+		SecretUrl(s.to_string())
+	}
+}
+
+impl From<SecretUrl> for String {
+	fn from(s: SecretUrl) -> Self {
+		s.0
+	}
+}
+
+impl<'a> From<&'a SecretUrl> for &'a str {
+	fn from(s: &'a SecretUrl) -> Self {
+		&s.0
+	}
+}
+
+impl AsRef<str> for SecretUrl {
+	fn as_ref(&self) -> &str {
+		&self.0
+	}
+}
+
 /// Partially redacts the secret in the url of the node endpoint.
 ///  eg: `wss://cdcd639308194d3f977a1a5a7ff0d545.rinkeby.ws.rivet.cloud/` ->
 /// `wss://cdc****.rinkeby.ws.rivet.cloud/`
 #[allow(unused)]
-pub fn redact_secret_eth_node_endpoint(endpoint: &str) -> Result<String, anyhow::Error> {
+pub fn redact_secret_node_endpoint(endpoint: &str) -> String {
 	let re = Regex::new(r"[0-9a-fA-F]{32}").unwrap();
 	if re.is_match(endpoint) {
 		// A 32 character hex string was found, redact it
@@ -23,19 +79,26 @@ pub fn redact_secret_eth_node_endpoint(endpoint: &str) -> Result<String, anyhow:
 				),
 			);
 		}
-		Ok(endpoint_redacted)
+		endpoint_redacted
 	} else {
 		// No secret was found, so just redact almost all of the url
-		let url = Url::parse(endpoint).context("Failed to parse node endpoint into a URL")?;
-		Ok(format!(
-			"{}****",
-			endpoint
-				.split_at(usize::min(
-					url.scheme().len() + SCHEMA_PADDING_LEN + MAX_SECRET_CHARACTERS_REVEALED,
-					endpoint.len()
-				))
-				.0
-		))
+		if let Ok(url) = Url::parse(endpoint) {
+			format!(
+				"{}****",
+				endpoint
+					.split_at(usize::min(
+						url.scheme().len() + SCHEMA_PADDING_LEN + MAX_SECRET_CHARACTERS_REVEALED,
+						endpoint.len()
+					))
+					.0
+			)
+		} else {
+			// Not a valid url, so just redact most of the string
+			format!(
+				"{}****",
+				endpoint.split_at(usize::min(MAX_SECRET_CHARACTERS_REVEALED, endpoint.len())).0
+			)
+		}
 	}
 }
 
@@ -46,35 +109,49 @@ mod tests {
 	#[test]
 	fn test_secret_web_addresses() {
 		assert_eq!(
-			redact_secret_eth_node_endpoint(
-				"wss://mainnet.infura.io/ws/v3/d52c362116b640b98a166d08d3170a42"
-			)
-			.unwrap(),
+			format!(
+				"{}",
+				SecretUrl(
+					"wss://mainnet.infura.io/ws/v3/d52c362116b640b98a166d08d3170a42".to_string()
+				)
+			),
 			"wss://mainnet.infura.io/ws/v3/d52****"
 		);
 		assert_eq!(
-			redact_secret_eth_node_endpoint(
-				"wss://cdcd639308194d3f977a1a5a7ff0d545.rinkeby.ws.rivet.cloud/"
-			)
-			.unwrap(),
+			format!(
+				"{}",
+				SecretUrl(
+					"wss://cdcd639308194d3f977a1a5a7ff0d545.rinkeby.ws.rivet.cloud/".to_string()
+				)
+			),
 			"wss://cdc****.rinkeby.ws.rivet.cloud/"
 		);
-		// same, but HTTP
 		assert_eq!(
-			redact_secret_eth_node_endpoint(
-				"https://cdcd639308194d3f977a1a5a7ff0d545.rinkeby.rpc.rivet.cloud/"
-			)
-			.unwrap(),
+			format!("{}", SecretUrl("wss://non_32hex_secret.rinkeby.ws.rivet.cloud/".to_string())),
+			"wss://non****"
+		);
+		assert_eq!(format!("{}", SecretUrl("wss://a".to_string())), "wss://a****");
+
+		// Same, but HTTPS
+		assert_eq!(
+			format!(
+				"{}",
+				SecretUrl(
+					"https://cdcd639308194d3f977a1a5a7ff0d545.rinkeby.rpc.rivet.cloud/".to_string()
+				)
+			),
 			"https://cdc****.rinkeby.rpc.rivet.cloud/"
 		);
 		assert_eq!(
-			redact_secret_eth_node_endpoint("wss://non_32hex_secret.rinkeby.ws.rivet.cloud/")
-				.unwrap(),
-			"wss://non****"
+			format!(
+				"{}",
+				SecretUrl("https://non_32hex_secret.rinkeby.ws.rivet.cloud/".to_string())
+			),
+			"https://non****"
 		);
-		assert_eq!(redact_secret_eth_node_endpoint("wss://a").unwrap(), "wss://a****");
-		// same, but HTTP
-		assert_eq!(redact_secret_eth_node_endpoint("http://a").unwrap(), "http://a****");
-		assert!(redact_secret_eth_node_endpoint("no.schema.com").is_err());
+		assert_eq!(format!("{}", SecretUrl("https://a".to_string())), "https://a****");
+
+		assert_eq!(format!("{}", SecretUrl("no.schema.com".to_string())), "no.****");
+		assert_eq!(format!("{:?}", SecretUrl("debug_print".to_string())), "\"debug_print\"");
 	}
 }
