d53611e4b68be0526b4d578862cfd9ee12c83b78
Ramiz Siddiqui <ramiz@chainflip.io>
2023-09-27 13:07:25 +0200

    fix: move resetting of the proxy account nonce to when constructed tx

--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -85,11 +85,10 @@ pub use cf_traits::{EpochInfo, QualifyNode, SessionKeysRegistered, SwappingApi};
 pub use chainflip::chain_instances::*;
 use chainflip::{
 	epoch_transition::ChainflipEpochTransitions, BroadcastReadyProvider, BtcEnvironment,
-	BtcVaultTransitionHandler, ChainAddressConverter, ChainflipHeartbeat, EthEnvironment,
-	EthVaultTransitionHandler, TokenholderGovernanceBroadcaster,
+	ChainAddressConverter, ChainflipHeartbeat, EthEnvironment, TokenholderGovernanceBroadcaster,
 };
 
-use chainflip::{all_vaults_rotator::AllVaultRotator, DotEnvironment, DotVaultTransitionHandler};
+use chainflip::{all_vaults_rotator::AllVaultRotator, DotEnvironment};
 use constants::common::*;
 use pallet_cf_flip::{Bonder, FlipSlasher};
 use pallet_cf_vaults::Vault;
@@ -230,7 +229,6 @@ impl pallet_cf_vaults::Config<EthereumInstance> for Runtime {
 	type Offence = chainflip::Offence;
 	type Chain = Ethereum;
 	type SetAggKeyWithAggKey = eth::api::EthereumApi<EthEnvironment>;
-	type VaultTransitionHandler = EthVaultTransitionHandler;
 	type Broadcaster = EthereumBroadcaster;
 	type OffenceReporter = Reputation;
 	type WeightInfo = pallet_cf_vaults::weights::PalletWeight<Runtime>;
@@ -248,7 +246,6 @@ impl pallet_cf_vaults::Config<PolkadotInstance> for Runtime {
 	type Offence = chainflip::Offence;
 	type Chain = Polkadot;
 	type SetAggKeyWithAggKey = dot::api::PolkadotApi<DotEnvironment>;
-	type VaultTransitionHandler = DotVaultTransitionHandler;
 	type Broadcaster = PolkadotBroadcaster;
 	type OffenceReporter = Reputation;
 	type WeightInfo = pallet_cf_vaults::weights::PalletWeight<Runtime>;
@@ -266,7 +263,6 @@ impl pallet_cf_vaults::Config<BitcoinInstance> for Runtime {
 	type Offence = chainflip::Offence;
 	type Chain = Bitcoin;
 	type SetAggKeyWithAggKey = cf_chains::btc::api::BitcoinApi<BtcEnvironment>;
-	type VaultTransitionHandler = BtcVaultTransitionHandler;
 	type Broadcaster = BitcoinBroadcaster;
 	type OffenceReporter = Reputation;
 	type WeightInfo = pallet_cf_vaults::weights::PalletWeight<Runtime>;
