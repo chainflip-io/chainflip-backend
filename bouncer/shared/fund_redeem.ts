a92b7389ad24460bc38ce9dfab85c890024014ca
Jamie Ford <Jamie@chainflip.io>
2023-10-04 15:13:06 +1100

    refactor: use redemptionTax in calculations

--- bouncer/shared/fund_redeem.ts
+++ bouncer/shared/fund_redeem.ts
@@ -1,38 +1,35 @@
 import assert from 'assert';
 import { randomBytes } from 'crypto';
 import { HexString } from '@polkadot/util/types';
-import { newAddress, observeBalanceIncrease } from '../shared/utils';
+import { assetDecimals } from '@chainflip-io/cli';
+import {
+  fineAmountToAmount,
+  newAddress,
+  observeBalanceIncrease,
+  getChainflipApi,
+} from '../shared/utils';
 import { getBalance } from '../shared/get_balance';
 import { fundFlip } from '../shared/fund_flip';
 import { redeemFlip, RedeemAmount } from '../shared/redeem_flip';
 import { newStatechainAddress } from '../shared/new_statechain_address';
 
-const maxRedemptionVariationPercent = 0.03;
+const expectedFeesFlip = 0.000025;
 
-/// Redeems the flip and checks that the balance increase is within the variation percent of the expected amount.
-async function redeemAndAssertBalanceIncrease(
+/// Redeems the flip and observed the balance increase
+async function redeemAndObserve(
   seed: string,
   redeemEthAddress: HexString,
   redeemAmount: RedeemAmount,
-  expectedBalanceIncrease: number,
 ): Promise<number> {
   const initBalance = await getBalance('FLIP', redeemEthAddress);
-  console.log(`Initial ERC20-FLIP balance: ${initBalance.toString()}`);
+  console.log(`Initial ERC20-FLIP balance: ${initBalance}`);
 
   await redeemFlip(seed, redeemEthAddress, redeemAmount);
 
   const newBalance = await observeBalanceIncrease('FLIP', redeemEthAddress, initBalance);
-  const balanceIncrease = newBalance - parseInt(initBalance.toString());
+  const balanceIncrease = newBalance - parseInt(initBalance);
   console.log(
     `Redemption success! New balance: ${newBalance.toString()}, Increase: ${balanceIncrease}`,
-  );
-
-  assert(
-    Math.abs(balanceIncrease - expectedBalanceIncrease) <
-      expectedBalanceIncrease * maxRedemptionVariationPercent,
-    `unexpected balance increase: ${balanceIncrease}. Expected: ${expectedBalanceIncrease} +- ${
-      maxRedemptionVariationPercent * 100
-    }%`,
   );
 
   return balanceIncrease;
@@ -43,6 +40,13 @@ async function redeemAndAssertBalanceIncrease(
 // checking that the balance has increased the expected amount.
 // If no seed is provided, a random one is generated.
 export async function testFundRedeem(providedSeed?: string) {
+  const chainflip = await getChainflipApi();
+  const redemptionTax = await chainflip.query.funding.redemptionTax();
+  const redemptionTaxAmount = parseInt(
+    fineAmountToAmount(redemptionTax.toString(), assetDecimals.FLIP),
+  );
+  console.log(`Redemption tax: ${redemptionTax} = ${redemptionTaxAmount} FLIP`);
+
   const seed = providedSeed ?? randomBytes(32).toString('hex');
   const fundAmount = 1000;
   const redeemSCAddress = await newStatechainAddress(seed);
@@ -54,23 +58,26 @@ export async function testFundRedeem(providedSeed?: string) {
   await fundFlip(redeemSCAddress, fundAmount.toString());
 
   // Test redeeming an exact amount with a portion of the funded flip
-  const exactAmount = fundAmount / 3;
+  const exactAmount = fundAmount / 4;
   const exactRedeemAmount = { Exact: exactAmount.toString() };
   console.log(`Testing redeem exact amount: ${exactRedeemAmount.Exact}`);
-  const redeemedExact = await redeemAndAssertBalanceIncrease(
+  const redeemedExact = await redeemAndObserve(
     seed,
     redeemEthAddress as HexString,
     exactRedeemAmount,
-    exactAmount,
   );
+  assert.strictEqual(redeemedExact, exactAmount, `Unexpected balance increase amount`);
 
   // Test redeeming the rest of the flip with a 'Max' redeem amount
   console.log(`Testing redeem all`);
-  const expectedRedeemAllAmount = fundAmount - redeemedExact;
-  await redeemAndAssertBalanceIncrease(
-    seed,
-    redeemEthAddress as HexString,
-    'Max',
-    expectedRedeemAllAmount,
+  const redeemedAll = await redeemAndObserve(seed, redeemEthAddress as HexString, 'Max');
+  // We expect to redeem the entire amount minus the exact amount redeemed above + tax & fees for both redemptions
+  const expectedRedeemAllAmount = fundAmount - redeemedExact - redemptionTaxAmount * 2;
+  assert(
+    redeemedAll >= expectedRedeemAllAmount - expectedFeesFlip &&
+      redeemedAll <= expectedRedeemAllAmount,
+    `Unexpected balance increase amount: ${redeemedAll}. Expected between: ${
+      expectedRedeemAllAmount - expectedFeesFlip
+    } - ${expectedRedeemAllAmount}. Did fees change?`,
   );
 }
