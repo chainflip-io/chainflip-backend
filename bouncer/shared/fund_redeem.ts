42dca015b27f7e37bdefc05179d0ea513c5cdd24
Jamie Ford <jamie@chainflip.io>
2023-10-09 16:34:09 +1100

    Bouncer test for redeeming all (#4067)
    * test: added redeem all to fund redeem test
    
    * refactor: use redemptionTax in calculations
    
    * chore: rename & document gas fee
    
    * refactor: assert instead of warning

--- bouncer/shared/fund_redeem.ts
+++ bouncer/shared/fund_redeem.ts
@@ -1,29 +1,85 @@
+import assert from 'assert';
+import { randomBytes } from 'crypto';
 import { HexString } from '@polkadot/util/types';
-import { newAddress, observeBalanceIncrease } from '../shared/utils';
+import { assetDecimals } from '@chainflip-io/cli';
+import {
+  fineAmountToAmount,
+  newAddress,
+  observeBalanceIncrease,
+  getChainflipApi,
+} from '../shared/utils';
 import { getBalance } from '../shared/get_balance';
 import { fundFlip } from '../shared/fund_flip';
-import { redeemFlip } from '../shared/redeem_flip';
+import { redeemFlip, RedeemAmount } from '../shared/redeem_flip';
 import { newStatechainAddress } from '../shared/new_statechain_address';
+
+// Submitting the `redeem` extrinsic will cost a small amount of gas
+// TODO: Send the redeem extrinsic from a different account to avoid compensating for this fee in the test.
+const expectedRedeemGasFeeFlip = 0.0000125;
+
+/// Redeems the flip and observed the balance increase
+async function redeemAndObserve(
+  seed: string,
+  redeemEthAddress: HexString,
+  redeemAmount: RedeemAmount,
+): Promise<number> {
+  const initBalance = await getBalance('FLIP', redeemEthAddress);
+  console.log(`Initial ERC20-FLIP balance: ${initBalance}`);
+
+  await redeemFlip(seed, redeemEthAddress, redeemAmount);
+
+  const newBalance = await observeBalanceIncrease('FLIP', redeemEthAddress, initBalance);
+  const balanceIncrease = newBalance - parseInt(initBalance);
+  console.log(
+    `Redemption success! New balance: ${newBalance.toString()}, Increase: ${balanceIncrease}`,
+  );
+
+  return balanceIncrease;
+}
 
 // Uses the seed to generate a new SC address and ETH address.
 // It then funds the SC address with FLIP, and redeems the FLIP to the ETH address
-// checking that the balance has increased.
-export async function testFundRedeem(seed: string) {
+// checking that the balance has increased the expected amount.
+// If no seed is provided, a random one is generated.
+export async function testFundRedeem(providedSeed?: string) {
+  const chainflip = await getChainflipApi();
+  const redemptionTax = await chainflip.query.funding.redemptionTax();
+  const redemptionTaxAmount = parseInt(
+    fineAmountToAmount(redemptionTax.toString(), assetDecimals.FLIP),
+  );
+  console.log(`Redemption tax: ${redemptionTax} = ${redemptionTaxAmount} FLIP`);
+
+  const seed = providedSeed ?? randomBytes(32).toString('hex');
+  const fundAmount = 1000;
   const redeemSCAddress = await newStatechainAddress(seed);
   const redeemEthAddress = await newAddress('ETH', seed);
   console.log(`FLIP Redeem address: ${redeemSCAddress}`);
   console.log(`ETH  Redeem address: ${redeemEthAddress}`);
-  const initBalance = await getBalance('FLIP', redeemEthAddress);
-  console.log(`Initial ERC20-FLIP balance: ${initBalance.toString()}`);
-  const amount = 1000;
-  // We fund to a specific SC address.
-  await fundFlip(redeemSCAddress, amount.toString());
 
-  // The ERC20 FLIP is sent back to an ETH address, and the registered redemption can only be executed by that address.
-  await redeemFlip(seed, redeemEthAddress as HexString, (amount / 2).toString());
-  console.log('Observed RedemptionSettled event');
-  const newBalance = await observeBalanceIncrease('FLIP', redeemEthAddress, initBalance);
-  console.log(`Redemption success! New balance: ${newBalance.toString()}`);
-  console.log('=== Fund/Redeem Test Success ===');
-  process.exit(0);
+  // Fund the SC address for the tests
+  await fundFlip(redeemSCAddress, fundAmount.toString());
+
+  // Test redeeming an exact amount with a portion of the funded flip
+  const exactAmount = fundAmount / 4;
+  const exactRedeemAmount = { Exact: exactAmount.toString() };
+  console.log(`Testing redeem exact amount: ${exactRedeemAmount.Exact}`);
+  const redeemedExact = await redeemAndObserve(
+    seed,
+    redeemEthAddress as HexString,
+    exactRedeemAmount,
+  );
+  assert.strictEqual(redeemedExact, exactAmount, `Unexpected balance increase amount`);
+
+  // Test redeeming the rest of the flip with a 'Max' redeem amount
+  console.log(`Testing redeem all`);
+  const redeemedAll = await redeemAndObserve(seed, redeemEthAddress as HexString, 'Max');
+  // We expect to redeem the entire amount minus the exact amount redeemed above + tax & gas for both redemptions
+  const expectedRedeemAllAmount = fundAmount - redeemedExact - redemptionTaxAmount * 2;
+  assert(
+    redeemedAll >= expectedRedeemAllAmount - expectedRedeemGasFeeFlip * 2 &&
+      redeemedAll <= expectedRedeemAllAmount,
+    `Unexpected balance increase amount: ${redeemedAll}. Expected between: ${
+      expectedRedeemAllAmount - expectedRedeemGasFeeFlip * 2
+    } - ${expectedRedeemAllAmount}. Did fees change?`,
+  );
 }
