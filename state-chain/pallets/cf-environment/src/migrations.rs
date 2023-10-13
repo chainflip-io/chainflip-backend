289b4e302af3f8b7a402e7c2cdc831ae81172c7c
kylezs <zsembery.kyle@gmail.com>
2023-10-13 14:17:18 +0200

    chore: fmt

--- state-chain/pallets/cf-environment/src/migrations.rs
+++ state-chain/pallets/cf-environment/src/migrations.rs
@@ -5,6 +5,6 @@ pub mod v5;
 use cf_runtime_upgrade_utilities::VersionedMigration;
 
 pub type PalletMigration<T> = (
-    VersionedMigration<crate::Pallet<T>, v4::Migration<T>, 3, 4>,
-    VersionedMigration<crate::Pallet<T>, v5::Migration<T>, 4, 5>
+	VersionedMigration<crate::Pallet<T>, v4::Migration<T>, 3, 4>,
+	VersionedMigration<crate::Pallet<T>, v5::Migration<T>, 4, 5>,
 );
