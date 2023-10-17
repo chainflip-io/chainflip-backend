4789131859df762dcfedc77304bfd007a3e33230
Marcello <marcello@chainflip.io>
2023-10-17 13:25:25 +0200

    added message when exiting (#4069)
    * added message when exiting
    
    * use general macro print_start_and_end
    
    * few changes from rebase
    
    * cargo fmt
    
    * addressed comments
    
    * added comment about panic handling for the node

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -87,8 +87,12 @@ async fn main() -> anyhow::Result<()> {
 
 	// Note: the greeting should only be printed in normal mode (i.e. not for short-lived commands
 	// like `--version`), so we execute it only after the settings have been parsed.
-	utilities::print_starting!();
+	utilities::print_start_and_end!(async run_main(settings));
 
+	Ok(())
+}
+
+async fn run_main(settings: Settings) -> anyhow::Result<()> {
 	task_scope(|scope| {
 		async move {
 			let mut start_logger_server_fn = Some(utilities::logging::init_json_logger(settings.logging.clone()).await);
