d31551c521adb393d4eaeddcb50593a9f0ad1066
albert <allimos3@gmail.com>
2023-10-26 15:04:37 +0200

    chore: reorder tests

--- bouncer/run.sh
+++ bouncer/run.sh
@@ -2,9 +2,8 @@ set -e
 ./commands/observe_block.ts 5
 ./commands/setup_vaults.ts
 ./commands/setup_swaps.ts
-# Gas limit tests to be executed first to have a deterministic pool state
-./tests/gaslimit_ccm.ts
 ./tests/swap_less_than_existential_deposit_dot.ts
+./tests/gaslimit_ccm.ts
 ./tests/all_concurrent_tests.ts
 ./tests/rotates_through_btc_swap.ts
 
