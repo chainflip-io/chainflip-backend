8c45a61ff61d2f6d35fe5918b6e2735a2570e865
kylezs <zsembery.kyle@gmail.com>
2023-10-26 13:05:25 +0200

    fix: use && between commands

--- bouncer/shared/utils/compile_binaries.ts
+++ bouncer/shared/utils/compile_binaries.ts
@@ -4,7 +4,7 @@ import { execSync } from 'child_process';
 export async function compileBinaries(type: 'runtime' | 'all', projectRoot: string) {
   if (type === 'all') {
     console.log('Building all the binaries...');
-    execSync(`cd ${projectRoot} cargo build --release`);
+    execSync(`cd ${projectRoot} && cargo build --release`);
   } else {
     console.log('Building the new runtime...');
     execSync(`cd ${projectRoot}/state-chain/runtime && cargo build --release`);
