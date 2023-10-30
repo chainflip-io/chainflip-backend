cbf300d9f42daf1739e2e16c2388c704d6951cc8
Daniel <daniel@chainflip.io>
2023-10-30 16:34:19 +0100

    fix: allow rotation witness on bootstrap

--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -150,7 +150,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("chainflip-node"),
 	impl_name: create_runtime_str!("chainflip-node"),
 	authoring_version: 1,
-	spec_version: 24,
+	spec_version: 25,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
