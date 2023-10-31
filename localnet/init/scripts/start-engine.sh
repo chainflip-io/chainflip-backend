ec9960de17705de04134b4f693bd88468f9c7f7e
kylezs <kyle@chainflip.io>
2023-11-01 01:20:47 +1100

    refactor: more parameterising of start-engine.sh (#4186)
    * refactor: pass in env vars to start-engine
    
    * refactor: allow for starting engines with different log file
    
    * chore: explicit binary root path
    
    * chore: comments

--- localnet/init/scripts/start-engine.sh
+++ localnet/init/scripts/start-engine.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
+
+# LOG_SUFFIX is optional. It can be used to differentiate between the logs of two engines, potentially with the same owner
+# e.g. in the case of an upgrade where we run two engines simultaneously.
+
 set -e
-BINARY_ROOT_PATH=$1
-NODE_NAME=$2
-PORT=$3
-HEALTH_PORT=$4
-RPC_PORT=$5
-LOG_PORT=$6
 source ./localnet/init/env/cfe.env
 $BINARY_ROOT_PATH/chainflip-engine \
   --config-root=./localnet/init/ \
   --eth.private_key_file=./localnet/init/keys/$NODE_NAME/eth_private_key_file \
   --state_chain.signing_key_file=./localnet/init/keys/$NODE_NAME/signing_key_file \
-  --state_chain.ws_endpoint=ws://localhost:$RPC_PORT \
+  --state_chain.ws_endpoint=ws://localhost:$SC_RPC_PORT \
   --p2p.node_key_file=./localnet/init/keys/$NODE_NAME/node_key_file \
-  --p2p.port=$PORT \
+  --p2p.port=$P2P_PORT \
   --logging.command_server_port=$LOG_PORT \
   --signing.db_file=/tmp/chainflip/$NODE_NAME/$NODE_NAME.db \
-  --health_check.port=$HEALTH_PORT > /tmp/chainflip/$NODE_NAME/chainflip-engine.log 2>&1 &
+  --health_check.port=$HEALTH_PORT > /tmp/chainflip/$NODE_NAME/chainflip-engine$LOG_SUFFIX.log 2>&1 &
