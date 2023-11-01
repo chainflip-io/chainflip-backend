d21557aadb5eeca8edb8634b487b546796f53a6b
dandanlen <3168260+dandanlen@users.noreply.github.com>
2023-11-01 11:04:34 +0100

    fix: handle relative path to db (#4164)

--- localnet/init/scripts/start-engine.sh
+++ localnet/init/scripts/start-engine.sh
@@ -7,10 +7,10 @@ set -e
 source ./localnet/init/env/cfe.env
 $BINARY_ROOT_PATH/chainflip-engine \
   --config-root=./localnet/init/ \
-  --eth.private_key_file=./localnet/init/keys/$NODE_NAME/eth_private_key_file \
-  --state_chain.signing_key_file=./localnet/init/keys/$NODE_NAME/signing_key_file \
+  --eth.private_key_file=./keys/$NODE_NAME/eth_private_key_file \
+  --state_chain.signing_key_file=./keys/$NODE_NAME/signing_key_file \
   --state_chain.ws_endpoint=ws://localhost:$SC_RPC_PORT \
-  --p2p.node_key_file=./localnet/init/keys/$NODE_NAME/node_key_file \
+  --p2p.node_key_file=./keys/$NODE_NAME/node_key_file \
   --p2p.port=$P2P_PORT \
   --logging.command_server_port=$LOG_PORT \
   --signing.db_file=/tmp/chainflip/$NODE_NAME/$NODE_NAME.db \
