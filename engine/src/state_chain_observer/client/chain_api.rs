22c713bc5d6d8c06df295c8c2606de21a1ecd2a3
Alastair Holmes <42404303+AlastairHolmes@users.noreply.github.com>
2023-11-07 17:45:20 +0100

    feat: get new block streams from scc after creation (#4217)
    * feat: get new block streams from scc
    
    * fix bad rebase

--- engine/src/state_chain_observer/client/chain_api.rs
+++ engine/src/state_chain_observer/client/chain_api.rs
@@ -1,4 +1,12 @@
+use async_trait::async_trait;
+
+use super::StateChainStreamApi;
+
+#[async_trait]
 pub trait ChainApi {
 	fn latest_finalized_block(&self) -> super::BlockInfo;
 	fn latest_unfinalized_block(&self) -> super::BlockInfo;
+
+	async fn finalized_block_stream(&self) -> Box<dyn StateChainStreamApi>;
+	async fn unfinalized_block_stream(&self) -> Box<dyn StateChainStreamApi<false>>;
 }
