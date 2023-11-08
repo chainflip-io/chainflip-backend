774c280ce036024035a247776593f134fc531e71
marcellorigotti <marcello@chainflip.io>
2023-11-08 11:18:15 +0100

    addressed comments

--- bouncer/shared/swap_less_than_existential_deposit_dot.ts
+++ bouncer/shared/swap_less_than_existential_deposit_dot.ts
@@ -1,25 +1,27 @@
 #!/usr/bin/env -S pnpm tsx
 import { getDotBalance } from './get_dot_balance';
 import { performAndTrackSwap } from './perform_swap';
-
 import { getSwapRate, newAddress } from './utils';
+
+const DOT_EXISTENTIAL_DEPOSIT = 1;
 
 export async function swapLessThanED() {
   console.log('=== Testing USDC -> DOT swaps obtaining less than ED ===');
   const tag = `USDC -> DOT (less than ED)`;
-  // The initial price is 10USDC = 1DOT,
-  // we will swap only 5 USDC and check that the swap is completed successfully with 0 output
+
+  // we will try to swap with 5 USDC and check if the expected output is low enough
+  // otherwise we'll keep reducing the amount
   let retry = true;
   let inputAmount = '5';
   while (retry) {
     let outputAmount = await getSwapRate('USDC', 'DOT', inputAmount);
 
-    while (parseFloat(outputAmount) >= 1) {
+    while (parseFloat(outputAmount) >= DOT_EXISTENTIAL_DEPOSIT) {
       inputAmount = (parseFloat(inputAmount) / 2).toString();
       outputAmount = await getSwapRate('USDC', 'DOT', inputAmount);
     }
     console.log(`${tag} Input amount: ${inputAmount} USDC`);
-    console.log(`${tag} Output amount: ${outputAmount} DOT`);
+    console.log(`${tag} Approximate expected output amount: ${outputAmount} DOT`);
 
     // we want to be sure to have an address with 0 balance, hence we create a new one every time
     const address = await newAddress(
@@ -29,6 +31,8 @@ export async function swapLessThanED() {
     console.log(`${tag} Generated DOT address: ${address}`);
 
     await performAndTrackSwap('USDC', 'DOT', address, inputAmount, tag);
+    // if for some reason the balance after swapping is > 0 it means that the output was larger than
+    // ED, so we'll retry the test with a lower input
     if (parseFloat(await getDotBalance(address)) > 0) {
       console.log(`${tag}, swap output was more than ED, retrying with less...`);
       inputAmount = (parseFloat(inputAmount) / 3).toString();
