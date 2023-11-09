b0ec31440a38c8f3013af3657f442635ffa69e9a
Daniel <daniel@chainflip.io>
2023-11-09 13:13:42 +0100

    doc: inflation helpers

--- state-chain/node/src/chain_spec/common.rs
+++ state-chain/node/src/chain_spec/common.rs
@@ -19,12 +19,29 @@ pub const REDEMPTION_PERIOD_AS_PERCENTAGE: u8 = 50;
 // Consider the equation (1 + x/1_000_000_000)^n = 1 + inf/100
 // where inf is the target yearly inflation (percent), n is the number of compundings that
 // we do in a year and x is the inflation rate (Perbill) for each compunding time period.
-
+//
 // The following values are calculated by solving the above equation for x using n =
-// (365*14400)/150 (since compunding is done every heartbeat which is every 150 blocks) and inf
-// is taken as 0.1 percent for authority emissions and 0.02 percent for backup node emissions.
+// (365*14400)/150 (since compunding is done every heartbeat which is every 150 blocks) and
+// inf is taken as 0.1 percent for authority emissions and 0.02 percent for backup node emissions.
+//
+// Can be generated using the following python code:
+//
+// ```
+// import math
+// def per_bill_inflation(pct):
+//      return (math.pow(1 + pct/100, 1/35040) - 1) * 1000000000
+//
+// # Testnet:
+// round(per_bill_inflation(0.1)) -> 28
+// round(per_bill_inflation(0.02)) -> 6
+// # Mainnet:
+// round(per_bill_inflation(7)) -> 1931
+// round(per_bill_inflation(1)) -> 284
+// ```
+//
 pub const CURRENT_AUTHORITY_EMISSION_INFLATION_PERBILL: u32 = 28;
 pub const BACKUP_NODE_EMISSION_INFLATION_PERBILL: u32 = 6;
+
 pub const SUPPLY_UPDATE_INTERVAL: u32 = 24 * HOURS;
 
 // This is equivalent to one reputation point for every minute of online time.
