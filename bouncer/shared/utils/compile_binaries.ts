8d15f57d2793dc0bacf369aea805ccc9197f0199
kylezs <zsembery.kyle@gmail.com>
2023-11-14 09:49:11 +0100

    feat: try-runtime feature option

--- bouncer/shared/utils/compile_binaries.ts
+++ bouncer/shared/utils/compile_binaries.ts
@@ -1,13 +1,21 @@
 import { execSync } from 'child_process';
 
 // Returns the expected next version of the runtime.
-export async function compileBinaries(type: 'runtime' | 'all', projectRoot: string) {
+export async function compileBinaries(type: 'runtime' | 'all', projectRoot: string, tryRuntime = false) {
   if (type === 'all') {
     console.log('Building all the binaries...');
-    execSync(`cd ${projectRoot} && cargo build --release`);
+    if (tryRuntime) {
+      execSync(`cd ${projectRoot} && cargo build --release --features try-runtime`);
+    } else {
+      execSync(`cd ${projectRoot} && cargo build --release`);
+    }
   } else {
     console.log('Building the new runtime...');
-    execSync(`cd ${projectRoot}/state-chain/runtime && cargo build --release`);
+    if (tryRuntime) {
+      execSync(`cd ${projectRoot}/state-chain/runtime && cargo build --release`);
+    } else {
+      execSync(`cd ${projectRoot}/state-chain/runtime && cargo build --release --features try-runtime`);
+    }
   }
 
   console.log('Build complete.');
