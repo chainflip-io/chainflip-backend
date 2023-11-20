93da707c71cf6c3ba6757ef4f8cec1c75a2622b2
kylezs <zsembery.kyle@gmail.com>
2023-11-20 15:23:07 +0100

    chore: remove stale comment

--- bouncer/shared/utils/compile_binaries.ts
+++ bouncer/shared/utils/compile_binaries.ts
@@ -1,6 +1,5 @@
 import { execSync } from 'child_process';
 
-// Returns the expected next version of the runtime.
 export async function compileBinaries(type: 'runtime' | 'all', projectRoot: string) {
   if (type === 'all') {
     console.log('Building all the binaries...');
