03cd5cd361c534495a6eb02d5cc9d65db7d0cd15
Assem Hasna <asem.hasna@gmail.com>
2023-12-06 17:13:14 +0100

    chore: address PR review comments

--- ci/scripts/check_engine_systemd_config.sh
+++ ci/scripts/check_engine_systemd_config.sh
@@ -17,21 +17,21 @@ subdirectories=("berghain" "sisyphos" "perseverance")
 version=$1
 IFS='.' read -r major_version minor_version patch_version <<< "$version"
 
-# Construct the filename to search for
+# Construct the service file to search for
 service_file="chainflip-engine${major_version}.${minor_version}.service"
-search_string="chainflip-engine${major_version}.${minor_version}"
+engine_binary_name="chainflip-engine${major_version}.${minor_version}"
 
 # Check each subdirectory for the specified file and content
 for subdir in "${subdirectories[@]}"; do
-    filepath="$base_directory/$subdir/$filename"
+    filepath="$base_directory/$subdir/$service_file"
     if [[ -f "$filepath" ]]; then
         echo "File exists: $filepath"
 
         # Check if the file contains the string 'chainflip-engine1.1'
         if grep -q "chainflip-engine${major_version}.${minor_version}" "$filepath"; then
-            echo "The string '$search_string' exists in $filepath"
+            echo "The string '$engine_binary_name' exists in $filepath"
         else
-            echo "The string '$search_string' does not exist in $filepath"
+            echo "The string '$engine_binary_name' does not exist in $filepath"
             exit 2
         fi
     else
@@ -42,10 +42,10 @@ done
 
 # Check if the string exists in engine/cargo.toml
 if [[ -f "$cargo_file" ]]; then
-    if grep -q "usr/bin/$search_string" $cargo_file; then
-        echo "The string '$search_string' exists in $cargo_file"
+    if grep -q "usr/bin/$engine_binary_name" $cargo_file; then
+        echo "The string '$engine_binary_name' exists in $cargo_file"
     else
-        echo "The string '$search_string' does not exist in $cargo_file"
+        echo "The string '$engine_binary_name' does not exist in $cargo_file"
         exit 2
     fi
 else
