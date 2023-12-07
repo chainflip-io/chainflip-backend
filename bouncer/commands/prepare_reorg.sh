72e76ca2b263cecfd7898c688fd3b34fa4c41147
kylezs <zsembery.kyle@gmail.com>
2023-12-07 10:20:52 +0100

    doc: fix typo

--- bouncer/commands/prepare_reorg.sh
+++ bouncer/commands/prepare_reorg.sh
@@ -3,7 +3,7 @@
 # This command prepares the localnet for a Bitcoin reorg. It will stop generating bitcoin blocks,
 # so commands that wait for the success of a bitcoin transaction need to be run asynchronously after
 # executing "prepare_reorg.sh". After running this command and submitting the BTC transactions to be included
-# in the reorg, run "execute_regorg.sh <REORG_DEPTH>"
+# in the reorg, run "execute_reorg.sh <REORG_DEPTH>"
 
 echo "Launching second BTC node"
 docker run --platform linux/amd64 -d --network=chainflip-localnet_default --name secondary_btc_node ghcr.io/chainflip-io/chainflip-bitcoin-regtest:v24.0.1 bash start.sh dont_mine > /dev/null
