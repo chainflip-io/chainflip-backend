fc83d5db6143eb9bd8f8b03d9d74e684408c9b6c
Roy Yang <roy@chainflip.io>
2023-12-11 19:16:11 +1300

    Added runtime storage migration for Broadcast pallet.

--- state-chain/pallets/cf-broadcast/src/migrations/v2.rs
+++ state-chain/pallets/cf-broadcast/src/migrations/v2.rs
@@ -2,9 +2,9 @@ use crate::*;
 #[cfg(feature = "try-runtime")]
 use frame_support::sp_runtime::DispatchError;
 use frame_support::{traits::OnRuntimeUpgrade, weights::Weight};
-use sp_std::marker::PhantomData;
 #[cfg(feature = "try-runtime")]
 use sp_std::prelude::Vec;
+use sp_std::{collections::btree_map::BTreeMap, marker::PhantomData};
 
 mod old {
 	use super::*;
@@ -18,6 +18,14 @@ mod old {
 		pub attempt_count: AttemptCount,
 	}
 
+	#[derive(Clone, RuntimeDebug, PartialEq, Eq, Encode, Decode, TypeInfo)]
+	pub struct BroadcastAttempt<T: Config<I>, I: 'static> {
+		pub broadcast_attempt_id: BroadcastAttemptId,
+		pub transaction_payload: TransactionFor<T, I>,
+		pub threshold_signature_payload: PayloadFor<T, I>,
+		pub transaction_out_id: TransactionOutIdFor<T, I>,
+	}
+
 	#[frame_support::storage_alias]
 	pub type RequestCallbacks<T: Config<I>, I: 'static> =
 		StorageMap<Pallet<T, I>, Twox64Concat, BroadcastId, <T as Config<I>>::BroadcastCallable>;
@@ -25,6 +33,10 @@ mod old {
 	#[frame_support::storage_alias]
 	pub type AwaitingBroadcast<T: Config<I>, I: 'static> =
 		StorageMap<Pallet<T, I>, Twox64Concat, BroadcastAttemptId, TransactionSigningAttempt<T, I>>;
+
+	#[frame_support::storage_alias]
+	pub type FailedBroadcasters<T: Config<I>, I: 'static> =
+		StorageMap<Pallet<T, I>, Twox64Concat, BroadcastId, Vec<<T as Chainflip>::ValidatorId>>;
 }
 
 pub struct Migration<T: Config<I>, I: 'static>(PhantomData<(T, I)>);
@@ -44,6 +56,53 @@ impl<T: Config<I>, I: 'static> OnRuntimeUpgrade for Migration<T, I> {
 			.map(|old::BroadcastAttemptId { broadcast_id, .. }| broadcast_id)
 			.collect::<BTreeSet<_>>();
 		PendingBroadcasts::<T, I>::put(pending_broadcasts);
+
+		// Awaiting broadcast: take the broadcast attempt with the highest attempt.
+		let mut latest_attempt = BTreeMap::new();
+		old::AwaitingBroadcast::<T, I>::iter_keys().for_each(
+			|old::BroadcastAttemptId { broadcast_id, attempt_count }| match latest_attempt
+				.get(&broadcast_id)
+			{
+				Some(attempt) =>
+					if attempt_count > *attempt {
+						latest_attempt.insert(broadcast_id, attempt_count);
+					},
+				None => {
+					latest_attempt.insert(broadcast_id, attempt_count);
+				},
+			},
+		);
+
+		// Migrate data from old storage into new storage.
+		latest_attempt.into_iter().for_each(|(broadcast_id, attempt_count)| {
+			if let Some(attempt) = old::AwaitingBroadcast::<T, I>::get(old::BroadcastAttemptId {
+				broadcast_id,
+				attempt_count,
+			}) {
+				AwaitingBroadcast::<T, I>::insert(
+					broadcast_id,
+					TransactionSigningAttempt {
+						broadcast_attempt: BroadcastAttempt {
+							broadcast_id,
+							transaction_payload: attempt.broadcast_attempt.transaction_payload,
+							threshold_signature_payload: attempt
+								.broadcast_attempt
+								.threshold_signature_payload,
+							transaction_out_id: attempt.broadcast_attempt.transaction_out_id,
+						},
+						nominee: attempt.nominee,
+					},
+				);
+			}
+		});
+
+		// Failed broadcaster storage: Vec<ValidatorId> -> BTreeSet<ValidatorId>
+		old::FailedBroadcasters::<T, I>::drain().for_each(|(broadcast_id, failed_broadcasters)| {
+			FailedBroadcasters::<T, I>::insert(
+				broadcast_id,
+				BTreeSet::from_iter(failed_broadcasters),
+			)
+		});
 
 		Weight::zero()
 	}
