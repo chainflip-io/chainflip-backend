ba89728c5add65d7a8ad93c3e2ec16ec18553dab
Jamie Ford <Jamie@chainflip.io>
2023-12-12 19:18:45 +1100

    chore: check for broadcast aborted events

--- bouncer/tests/polkadot_runtime_update.ts
+++ bouncer/tests/polkadot_runtime_update.ts
@@ -1,13 +1,28 @@
 #!/usr/bin/env -S pnpm tsx
 import { testPolkadotRuntimeUpdate } from '../shared/polkadot_runtime_update';
-import { runWithTimeout } from '../shared/utils';
+import { getChainflipApi, observeEvent, runWithTimeout } from '../shared/utils';
+
+let broadcastAbortedCount = 0;
+
+async function observeBroadcastAborted(): Promise<void> {
+  for (;;) {
+    await observeEvent(':BroadcastAborted', await getChainflipApi());
+    console.log('Broadcast aborted');
+    broadcastAbortedCount++;
+  }
+}
 
 async function main(): Promise<void> {
+  observeBroadcastAborted();
+
   await testPolkadotRuntimeUpdate();
+
+  console.log(`Broadcasts aborted: ${broadcastAbortedCount}`);
+
   process.exit(0);
 }
 
-runWithTimeout(main(), 1230000).catch((error) => {
+runWithTimeout(main(), 1300000).catch((error) => {
   console.error(error);
   process.exit(-1);
 });
