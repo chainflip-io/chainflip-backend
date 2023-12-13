cc72e2f0b1380264aa98003b4fd05da4b909bd71
Assem <asem.hasna@gmail.com>
2023-12-13 14:08:25 +0100

    chore: update docker tags 🐳 (#4347)
    * chore: update docker tags 🐳
    
    * chore: add version to network name
    
    * fix: typo

--- ci/scripts/extract_version.sh
+++ ci/scripts/extract_version.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
-# Remove any prefixes like "v" from the tag
-tag="${1#v}"
+# Input string
+input_string="$1"
 
-# Extract major and minor version using cut command
-version=$(echo "$tag" | cut -d. -f1,2)
+# Regular expression to match version pattern (X.Y.Z or X.Y)
+# This pattern looks for numbers separated by dots.
+# The -E flag is used for extended regex, and -o to output only the matched part.
+version=$(echo "$input_string" | grep -Eo '[0-9]+\.[0-9]+(\.[0-9]+)?')
+
+# Check if a version was found
+if [[ -z "$version" ]]; then
+   exit 0
+fi
+
+# Extract major and minor version
+major_minor=$(echo "$version" | cut -d. -f1,2)
 
 # Output the major and minor version
-echo "$version"
+echo "$major_minor"
