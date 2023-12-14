a6fd7e671928579f123d11f7b6a3d52361d510d0
Assem Hasna <asem.hasna@gmail.com>
2023-12-14 12:08:15 +0100

    chore: add conditional rpc endpoint

--- ci/scripts/check_runtime_spec_version.sh
+++ ci/scripts/check_runtime_spec_version.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+network=$1
+
 runtime_spec_version_file="state-chain/runtime/src/lib.rs"
 
 # Extract the spec_version from the Rust file
 spec_version=$(grep 'spec_version:' $runtime_spec_version_file | awk '{print $2}' | tr -d ',')
 
-live_runtime_version=$(curl -s -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "state_getRuntimeVersion", "params":[]}' https://mainnet-archive.chainflip.io | jq .result.specVersion)
+if [ -z $network ]; then
+    echo "Network not specified"
+    exit 1
+fi
+
+if [ $network == "berghain" ]; then
+    RPC_ENDPOINT="https://mainnet-archive.chainflip.io"
+else
+    RPC_ENDPOINT="https://perseverance.chainflip.xyz"
+fi
+
+live_runtime_version=$(curl -s -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "state_getRuntimeVersion", "params":[]}' $RPC_ENDPOINT | jq .result.specVersion)
 
 # Compare versions
 if [ $spec_version -gt $live_runtime_version ]; then
@@ -13,5 +26,5 @@ if [ $spec_version -gt $live_runtime_version ]; then
     exit 0
 else
     echo "Runtime version has not been incremented. Live runtime version: $live_runtime_version, Current Spec version: $spec_version"
-    exit 1
+    exit 2
 fi
