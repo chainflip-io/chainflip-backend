afa4e474536a95751cd1c083ae7003aaf29c15fc
Assem Hasna <asem.hasna@gmail.com>
2023-12-14 11:59:15 +0100

    chore: update runtime spec checks ⛓️

--- ci/scripts/check_runtime_spec_version.sh
+++ ci/scripts/check_runtime_spec_version.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-version=$1
 runtime_spec_version_file="state-chain/runtime/src/lib.rs"
-numeric_version=$(echo $version | tr -d '.')
 
 # Extract the spec_version from the Rust file
 spec_version=$(grep 'spec_version:' $runtime_spec_version_file | awk '{print $2}' | tr -d ',')
 
-runtime_version=$(cargo read-manifest --manifest-path state-chain/runtime/Cargo.toml | jq -r .version)
+live_runtime_version=$(curl -s -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "state_getRuntimeVersion", "params":[]}' https://mainnet-archive.chainflip.io | jq .result.specVersion)
 
 # Compare versions
-if [ "$spec_version" != "$numeric_version" ]; then
-    echo "Error: spec_version ($spec_version) does not match the expected version ($numeric_version)"
-    exit 1
-fi
-
-if [ "$runtime_version" != "$version" ]; then
-    echo "Error: runtime version ($runtime_version) does not match the expected version ($version)"
+if [ $spec_version -gt $live_runtime_version ]; then
+    echo "Runtime version has been incremented"
+    exit 0
+else
+    echo "Runtime version has not been incremented. Live runtime version: $live_runtime_version, Current Spec version: $spec_version"
     exit 1
 fi
