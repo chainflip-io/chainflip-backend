f59ae16ef35c092092ce89654c28d41179672641
kylezs <kyle@chainflip.io>
2023-12-16 03:34:05 +1100

    feat: end to end network upgrade github action (#4274)
    * feat: upgrade testing in CI
    
    * fix: ignore idempotency checks
    
    * fix: fund_redeem test erasing decimals
    
    * chore: clean up
    
    * chore: lint
    
    * chore: minor clean up

--- localnet/init/scripts/start-lp-api.sh
+++ localnet/init/scripts/start-lp-api.sh
@@ -4,4 +4,4 @@ binary_location=$1
 RUST_LOG=debug,jsonrpsee_types::params=trace $binary_location/chainflip-lp-api \
   --port=10589 \
   --state_chain.ws_endpoint=ws://localhost:9944 \
-  --state_chain.signing_key_file localnet/init/keys/LP_1 > /tmp/chainflip/chainflip-lp-api.log 2>&1 &
+  --state_chain.signing_key_file $KEYS_DIR/LP_1 > /tmp/chainflip/chainflip-lp-api.log 2>&1 &
