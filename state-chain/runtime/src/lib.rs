67075f12772c49e09264833de3f8ce6fb63402a0
kylezs <kyle@chainflip.io>
2024-01-03 20:23:21 +1100

    chore: bump spec version to match release (#4375)

--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -159,7 +159,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("chainflip-node"),
 	impl_name: create_runtime_str!("chainflip-node"),
 	authoring_version: 1,
-	spec_version: 112,
+	spec_version: 115,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 12,
