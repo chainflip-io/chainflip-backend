ebe2299044286222265e5597e47ccdee3f25dfd2
kylezs <kyle@chainflip.io>
2024-01-06 01:12:19 +1100

    chore: add reason to DepositIgnored event (#4385)
    * chore: add reason to DepositIgnored event
    
    * chore: bump versions
    
    * chore: remove stale comments
    
    * fix: upload broker and lp api logs (#4386)
    
    fix: upload broker and lp api logs 🕵🏻‍♂️
    
    ---------
    
    Co-authored-by: Assem <asem.hasna@gmail.com>

--- state-chain/pallets/cf-ingress-egress/src/lib.rs
+++ state-chain/pallets/cf-ingress-egress/src/lib.rs
@@ -64,6 +64,15 @@ impl<C: Chain> FetchOrTransfer<C> {
 	}
 }
 
+#[derive(RuntimeDebug, Eq, PartialEq, Clone, Encode, Decode, TypeInfo)]
+pub enum DepositIgnoredReason {
+	BelowMinimumDeposit,
+
+	/// The deposit was ignored because the amount provided was not high enough to pay for the fees
+	/// required to process the requisite transactions.
+	NotEnoughToPayFees,
+}
+
 /// Cross-chain messaging requests.
 #[derive(RuntimeDebug, Eq, PartialEq, Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
 pub(crate) struct CrossChainMessage<C: Chain> {
@@ -423,6 +432,7 @@ pub mod pallet {
 			asset: TargetChainAsset<T, I>,
 			amount: TargetChainAmount<T, I>,
 			deposit_details: <T::TargetChain as Chain>::DepositDetails,
+			reason: DepositIgnoredReason,
 		},
 		TransferFallbackRequested {
 			asset: TargetChainAsset<T, I>,
@@ -968,6 +978,7 @@ impl<T: Config<I>, I: 'static> Pallet<T, I> {
 				asset,
 				amount: deposit_amount,
 				deposit_details,
+				reason: DepositIgnoredReason::BelowMinimumDeposit,
 			});
 			return Ok(())
 		}
@@ -986,13 +997,13 @@ impl<T: Config<I>, I: 'static> Pallet<T, I> {
 			deposit_amount,
 		);
 
-		// TODO: Consider updating the event with a reason explaining why the deposit was ignored.
 		if net_deposit_amount.is_zero() {
 			Self::deposit_event(Event::<T, I>::DepositIgnored {
 				deposit_address,
 				asset,
 				amount: deposit_amount,
 				deposit_details,
+				reason: DepositIgnoredReason::NotEnoughToPayFees,
 			});
 			return Ok(())
 		}
--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -159,7 +159,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("chainflip-node"),
 	impl_name: create_runtime_str!("chainflip-node"),
 	authoring_version: 1,
-	spec_version: 116,
+	spec_version: 120,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 12,
