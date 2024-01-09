b8b91a18b6171d09ed4f94af2cbbafefe10a5e6f
Martin Rieke <martin@chainflip.io>
2024-01-09 13:12:45 +0100

    cargo fmt, tests

--- state-chain/pallets/cf-environment/src/tests.rs
+++ state-chain/pallets/cf-environment/src/tests.rs
@@ -54,15 +54,15 @@ fn test_btc_utxo_selection() {
 
 		// select some utxos for a tx
 
-		// the default fee is 10 satoshi per byte. 
+		// the default fee is 10 satoshi per byte.
 		// inputs are 78 bytes
 		// outputs are 51 bytes
 		// transactions have a 12 byte base size
 		// the fee for 3 inputs and 2 outputs is thus:
-		// 10*(12 + 3*78 + 2*51) = 3480 satoshi
+		// 10*(16 + 3*78 + 2*51) = 3520 satoshi
 		// the expected change is:
-		// 5000 + 10000 + 25000 - 12000 - 3480 = 24520 satoshi
-		const EXPECTED_CHANGE_AMOUNT: crate::BtcAmount = 24520;
+		// 5000 + 10000 + 25000 - 12000 - 3520 = 24480 satoshi
+		const EXPECTED_CHANGE_AMOUNT: crate::BtcAmount = 24480;
 		assert_eq!(
 			Environment::select_and_take_bitcoin_utxos(UtxoSelectionType::Some {
 				output_amount: 12000,
@@ -79,7 +79,7 @@ fn test_btc_utxo_selection() {
 		assert_eq!(
 			Environment::select_and_take_bitcoin_utxos(UtxoSelectionType::SelectAllForRotation)
 				.unwrap(),
-			(vec![utxo(5000000), utxo(100000), utxo(EXPECTED_CHANGE_AMOUNT),], 5121550)
+			(vec![utxo(5000000), utxo(100000), utxo(EXPECTED_CHANGE_AMOUNT),], 5121470)
 		);
 
 		// add some more utxos to the list
@@ -97,7 +97,7 @@ fn test_btc_utxo_selection() {
 		assert_eq!(
 			Environment::select_and_take_bitcoin_utxos(UtxoSelectionType::SelectAllForRotation)
 				.unwrap(),
-			(vec![utxo(5000), utxo(15000),], 17810)
+			(vec![utxo(5000), utxo(15000),], 17770)
 		);
 	});
 }
@@ -154,7 +154,7 @@ fn test_btc_utxo_consolidation() {
 		// Should select two UTXOs, with all funds (minus fees) going back to us as change
 		assert_eq!(
 			Environment::select_and_take_bitcoin_utxos(UtxoSelectionType::SelectForConsolidation),
-			Some((vec![utxo(10000), utxo(20000)], 27810))
+			Some((vec![utxo(10000), utxo(20000)], 27770))
 		);
 
 		// Any utxo that didn't get consolidated should still be available:
