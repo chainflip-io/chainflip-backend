e7d86dfb115a00c76dd3f2cc01870469e24aee1f
Roy Yang <roy@chainflip.io>
2024-01-23 12:41:51 +1300

    Fixed benchmarking for the Broadcast pallet (#4436)
    Fixed benchmarking for the Broadcast pallet.
    Re-write the on_initialize hook benchmarking
    Fixed the transaction_failed_benchmarking

--- state-chain/scripts/benchmark.sh
+++ state-chain/scripts/benchmark.sh
@@ -3,11 +3,12 @@
 # usage: state-chain/scripts/benchmark.sh palletname
 
 # execute the benchmark for $palletame
+# Use dev-3 to run the benchmarks (required by Broadcast pallet)
 ./target/${2:-release}/chainflip-node benchmark pallet \
     --extrinsic '*' \
     --pallet pallet_cf_$1 \
     --output state-chain/pallets/cf-$1/src/weights.rs \
-    --execution=wasm \
     --steps=20 \
     --repeat=20 \
-    --template=state-chain/chainflip-weight-template.hbs
+    --template=state-chain/chainflip-weight-template.hbs \
+    --chain=dev-3
