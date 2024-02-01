8071c9ffd3c0b49e6439d86d4c008d8ca4c54231
Daniel <daniel@chainflip.io>
2024-02-01 10:39:08 +0100

    fix: assert_has_matching_event test macro

--- state-chain/test-utilities/src/lib.rs
+++ state-chain/test-utilities/src/lib.rs
@@ -23,14 +23,16 @@ pub fn assert_has_event<T: frame_system::Config>(event: <T as frame_system::Conf
 
 #[macro_export]
 macro_rules! assert_has_matching_event {
-	(( $runtime:ty, $event:pat )) => {
-		let events = frame_system::Pallet::<T>::events()
+	( $runtime:ty, $event:pat $(if $guard:expr )? $(,)? ) => {
+		let events = frame_system::Pallet::<$runtime>::events()
 			.into_iter()
 			.map(|e| e.event)
 			.collect::<Vec<_>>();
 		assert!(
-			events.iter().any(|e| matches!(e, $event)),
-			"No event matching {stringify!($event):#?} found in {events:#?}",
+			events.iter().any(|e| matches!(e, $event $(if $guard)?)),
+			"No event matching {:#?} found in {:#?}",
+			stringify!($event $(if $guard)?),
+			events,
 		);
 	};
 }
