21343794e229dabcc71e4773c7612ced8d8478d6
albert <allimos3@gmail.com>
2024-02-05 08:24:45 +0100

    chore: add optional cu limit and priofee

--- bouncer/shared/send_sol.ts
+++ bouncer/shared/send_sol.ts
@@ -1,11 +1,44 @@
-import { Transaction, SystemProgram, PublicKey } from '@solana/web3.js';
+import {
+  Transaction,
+  SystemProgram,
+  PublicKey,
+  TransactionInstruction,
+  ComputeBudgetProgram,
+} from '@solana/web3.js';
 import { amountToFineAmount, getSolConnection, getSolWhaleKeyPair } from './utils';
 
-export async function signAndSendTxSol(tx: Transaction, /* gas = 2000000, */ log = true) {
+export async function signAndSendTxSol(
+  instructions: TransactionInstruction[],
+  prioFee = 0,
+  limitCU = 0,
+  log = true,
+) {
   const connection = getSolConnection();
   const whaleKeypair = getSolWhaleKeyPair();
 
-  const transaction = tx;
+  const transaction = new Transaction();
+
+  if (prioFee > 0) {
+    transaction.add(
+      ComputeBudgetProgram.setComputeUnitPrice({
+        microLamports: prioFee,
+      }),
+    );
+  }
+
+  if (limitCU > 0) {
+    transaction.add(
+      ComputeBudgetProgram.setComputeUnitLimit({
+        units: limitCU,
+      }),
+    );
+  }
+
+  // Add remaining instructions
+  instructions.forEach((item) => {
+    transaction.add(item);
+  });
+
   transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
   transaction.sign(whaleKeypair);
   const txHash = await connection.sendRawTransaction(transaction.serialize());
@@ -21,12 +54,12 @@ export async function signAndSendTxSol(tx: Transaction, /* gas = 2000000, */ log
 
 export async function sendSol(solAddress: string, solAmount: string, log = true) {
   const lamportsAmount = amountToFineAmount(solAmount, 9); // assetDecimals.SOL when available
-  const transaction = new Transaction().add(
+  const intruction = [
     SystemProgram.transfer({
       fromPubkey: getSolWhaleKeyPair().publicKey,
       toPubkey: new PublicKey(solAddress),
       lamports: BigInt(lamportsAmount),
     }),
-  );
-  await signAndSendTxSol(transaction, /* undefined, */ log);
+  ];
+  await signAndSendTxSol(intruction, undefined, undefined, log);
 }
