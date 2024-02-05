b61389782116f794bcf6783af8b1457ae5882461
albert <allimos3@gmail.com>
2024-02-05 09:12:07 +0100

    chore: add print

--- bouncer/run.sh
+++ bouncer/run.sh
@@ -1,6 +1,7 @@
 set -e
 ./commands/observe_block.ts 5
 ./commands/send_arb.ts 0x41aD2bc63A2059f9b623533d87fe99887D794847 1
+which solana-test-validator
 if which solana-test-validator > /dev/null; then
     ./commands/send_sol.ts 7QQGNm3ptwinipDCyaCF7jY5katgmFUu1ieP2f7nwLpE 1.2
 fi
