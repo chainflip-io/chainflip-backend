b8622e888054ff7591669dc1977dbc9c47072e95
albert <allimos3@gmail.com>
2024-02-09 14:00:56 +0100

    chore: restore run

--- bouncer/run.sh
+++ bouncer/run.sh
@@ -5,5 +5,5 @@ if lsof -Pi :8899 -sTCP:LISTEN -t >/dev/null ; then
     ./commands/send_sol.ts 7QQGNm3ptwinipDCyaCF7jY5katgmFUu1ieP2f7nwLpE 1.2
 fi
 ./setup_for_test.sh
-# ./tests/all_concurrent_tests.ts $1
-# ./tests/broker_fee_collection_test.ts
+./tests/all_concurrent_tests.ts $1
+./tests/broker_fee_collection_test.ts
