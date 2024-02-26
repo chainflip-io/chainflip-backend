c1271c094f115f3904cddf2a50953a0951322949
Marcello <marcello@chainflip.io>
2024-02-26 14:04:29 +0100

    fix: Wait for ThresholdSignature success before switching to NewKeysActivated (#4534)
    * track tss request id before completing rotation
    
    * add unit test to check the correct flow is followed when tss is required

--- state-chain/pallets/cf-vaults/src/vault_activator.rs
+++ state-chain/pallets/cf-vaults/src/vault_activator.rs
@@ -1,5 +1,6 @@
 use super::*;
 use cf_chains::SetAggKeyWithAggKeyError;
+use cf_primitives::ThresholdSignatureRequestId;
 use cf_runtime_utilities::{log_or_panic, StorageDecodeVariant};
 use cf_traits::{GetBlockHeight, VaultActivator};
 
@@ -18,26 +19,44 @@ impl<T: Config<I>, I: 'static> VaultActivator<<T::Chain as Chain>::ChainCrypto> 
 		}
 	}
 
-	fn activate(new_public_key: AggKeyFor<T, I>, maybe_old_public_key: Option<AggKeyFor<T, I>>) {
+	fn activate_key() {
+		Self::activate_new_key_for_chain(T::ChainTracking::get_block_height());
+	}
+
+	fn start_key_activation(
+		new_public_key: AggKeyFor<T, I>,
+		maybe_old_public_key: Option<AggKeyFor<T, I>>,
+	) -> Option<ThresholdSignatureRequestId> {
 		if let Some(old_key) = maybe_old_public_key {
 			match <T::SetAggKeyWithAggKey as SetAggKeyWithAggKey<_>>::new_unsigned(
 				Some(old_key),
 				new_public_key,
 			) {
 				Ok(activation_call) => {
-					T::Broadcaster::threshold_sign_and_broadcast_rotation_tx(activation_call);
-					Self::activate_new_key_for_chain(T::ChainTracking::get_block_height());
+					// we need to sign and submit the rotation call
+					// reporting back the request_id of the tss such that we can complete the
+					// rotation when that request is completed
+					let (_, tss_request_id) =
+						T::Broadcaster::threshold_sign_and_broadcast_rotation_tx(activation_call);
+					// since vaults are activated only when the tss completes we need to initiate
+					// the activation
+					PendingVaultActivation::<T, I>::put(
+						VaultActivationStatus::<T, I>::AwaitingActivation { new_public_key },
+					);
+					Some(tss_request_id)
 				},
 				Err(SetAggKeyWithAggKeyError::NotRequired) => {
 					// This can happen if, for example, on a utxo chain there are no funds that
 					// need to be swept.
 					Self::activate_new_key_for_chain(T::ChainTracking::get_block_height());
+					None
 				},
 				Err(SetAggKeyWithAggKeyError::Failed) => {
 					log_or_panic!(
 						"Unexpected failure during {} vault activation.",
 						<T::Chain as cf_chains::Chain>::NAME,
 					);
+					None
 				},
 			}
 		} else {
@@ -46,6 +65,7 @@ impl<T: Config<I>, I: 'static> VaultActivator<<T::Chain as Chain>::ChainCrypto> 
 				VaultActivationStatus::<T, I>::AwaitingActivation { new_public_key },
 			);
 			Self::deposit_event(Event::<T, I>::AwaitingGovernanceActivation { new_public_key });
+			None
 		}
 	}
 
