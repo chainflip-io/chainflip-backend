392ee04ead234249c00842774e6769e8981052d5
marcellorigotti <marcello@chainflip.io>
2024-02-26 16:06:16 +0100

    add comment on why we need big-endian values for tx hashes

--- state-chain/chains/src/btc.rs
+++ state-chain/chains/src/btc.rs
@@ -242,6 +242,8 @@ pub struct BitcoinTransactionMetadata {
 	pub tx_hash: Hash,
 }
 impl BitcoinTransactionMetadata {
+	/// It creates a tx_hash by reversing the provided hash
+	/// Btc softwares and explorers display blocks/txs hashes as big endian values, we need to convert it to obtain a valid tx hash
 	pub fn new(hash: Hash) -> Self {
 		BitcoinTransactionMetadata { tx_hash: hash.iter().rev().copied().collect_array() }
 	}
