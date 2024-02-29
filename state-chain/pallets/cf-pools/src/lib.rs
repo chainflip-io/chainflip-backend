22885100664c23110e8e8efd60880a89021364bf
Roy Yang <roy@chainflip.io>
2024-03-01 03:18:55 +1300

    Added Range order pool price to the pool_price_v2 rpc call (#4548)
    Co-authored-by: dandanlen <3168260+dandanlen@users.noreply.github.com>

--- state-chain/pallets/cf-pools/src/lib.rs
+++ state-chain/pallets/cf-pools/src/lib.rs
@@ -1193,6 +1193,7 @@ pub struct PoolPriceV1 {
 pub struct PoolPriceV2 {
 	pub sell: Option<SqrtPriceQ64F96>,
 	pub buy: Option<SqrtPriceQ64F96>,
+	pub range_order: SqrtPriceQ64F96,
 }
 
 impl<T: Config> Pallet<T> {
@@ -1558,6 +1559,7 @@ impl<T: Config> Pallet<T> {
 		Ok(PoolPriceV2 {
 			sell: pool.pool_state.current_price(Side::Sell).map(|(_, sqrt_price, _)| sqrt_price),
 			buy: pool.pool_state.current_price(Side::Buy).map(|(_, sqrt_price, _)| sqrt_price),
+			range_order: pool.pool_state.current_range_order_pool_price(),
 		})
 	}
 
