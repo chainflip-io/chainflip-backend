804aae9d5717a796d2c7a5cfbe9b5aedeba44e80
Martin Rieke <martin@chainflip.io>
2024-03-07 10:05:43 +0100

    migration

---
+++ state-chain/pallets/cf-environment/src/migrations/usdt.rs
@@ -0,0 +1,31 @@
+use crate::*;
+use cf_chains::eth::Address as EthereumAddress;
+use frame_support::traits::OnRuntimeUpgrade;
+
+pub struct Migration<T: Config>(PhantomData<T>);
+
+impl<T: Config> OnRuntimeUpgrade for Migration<T> {
+	fn on_runtime_upgrade() -> frame_support::weights::Weight {
+		let usdt_address: EthereumAddress = match ChainflipNetworkEnvironment::<T>::get() {
+			NetworkEnvironment::Mainnet =>
+				hex_literal::hex!("dAC17F958D2ee523a2206206994597C13D831ec7").into(),
+			NetworkEnvironment::Testnet =>
+				hex_literal::hex!("509Ee0d083DdF8AC028f2a56731412edD63223B9").into(),
+			NetworkEnvironment::Development =>
+				hex_literal::hex!("0DCd1Bf9A1b36cE34237eEaFef220932846BCD82").into(),
+		};
+		EthereumSupportedAssets::<T>::insert(EthAsset::Usdt, usdt_address);
+
+		Weight::zero()
+	}
+
+	#[cfg(feature = "try-runtime")]
+	fn pre_upgrade() -> Result<Vec<u8>, DispatchError> {
+		Ok(vec![])
+	}
+
+	#[cfg(feature = "try-runtime")]
+	fn post_upgrade(_state: Vec<u8>) -> Result<(), DispatchError> {
+		Ok(())
+	}
+}
