d69379bb9ab1c940539e5c836374e3aab0722e64
tomjohnburton <39559415+tomjohnburton@users.noreply.github.com>
2024-03-07 16:49:23 +0300

    fix: correct usdc address (#4621)
    Co-authored-by: Daniel <daniel@chainflip.io>

--- state-chain/node/src/chain_spec/perseverance.rs
+++ state-chain/node/src/chain_spec/perseverance.rs
@@ -19,7 +19,7 @@ pub const GENESIS_FUNDING_AMOUNT: FlipBalance = 1_000 * FLIPPERINOS_PER_FLIP;
 
 pub const ENV: StateChainEnvironment = StateChainEnvironment {
 	flip_token_address: hex_literal::hex!("dC27c60956cB065D19F08bb69a707E37b36d8086"),
-	eth_usdc_address: hex_literal::hex!("5fd84259d66Cd46123540766Be93DFE6D43130D7"),
+	eth_usdc_address: hex_literal::hex!("1c7D4B196Cb0C7B01d743Fbc6116a902379C7238"),
 	state_chain_gateway_address: hex_literal::hex!("A34a967197Ee90BB7fb28e928388a573c5CFd099"),
 	key_manager_address: hex_literal::hex!("4981b1329F29E720642266fc6e172C3f78159dff"),
 	eth_vault_address: hex_literal::hex!("36eaD71325604DC15d35FAE584D7b50646D81753"),
