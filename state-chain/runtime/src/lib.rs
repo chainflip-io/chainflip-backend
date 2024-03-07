9f1cdd086e2506a44c07620dfa3df20e807e97e3
kylezs <zsembery.kyle@gmail.com>
2024-03-07 08:50:15 +0100

    chore: spec version to 130

--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -163,7 +163,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("chainflip-node"),
 	impl_name: create_runtime_str!("chainflip-node"),
 	authoring_version: 1,
-	spec_version: 131,
+	spec_version: 130,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 12,
