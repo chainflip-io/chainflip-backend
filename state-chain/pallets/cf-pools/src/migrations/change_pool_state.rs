ba442502fff7a9b10e00dea6cd79c7ad46cfa27a
Jan Börner <jan@chainflip.io>
2024-03-12 13:50:01 +0100

    chore: added migrations directory

---
+++ state-chain/pallets/cf-pools/src/migrations/change_pool_state.rs
@@ -0,0 +1,29 @@
+use cf_chains::Chain;
+use frame_support::traits::OnRuntimeUpgrade;
+use sp_std::marker::PhantomData;
+
+use crate::ChainInitialized;
+#[cfg(feature = "try-runtime")]
+use frame_support::pallet_prelude::DispatchError;
+
+#[cfg(feature = "try-runtime")]
+use sp_std::vec::Vec;
+
+pub struct Migration<T, I>(PhantomData<(T, I)>);
+
+impl<T: crate::Config<I>, I: 'static> OnRuntimeUpgrade for Migration<T, I> {
+	fn on_runtime_upgrade() -> frame_support::weights::Weight {
+		Default::default()
+	}
+
+	#[cfg(feature = "try-runtime")]
+	fn pre_upgrade() -> Result<Vec<u8>, DispatchError> {
+		Ok(Default::default())
+	}
+
+	#[cfg(feature = "try-runtime")]
+	#[allow(clippy::bool_assert_comparison)]
+	fn post_upgrade(_state: Vec<u8>) -> Result<(), DispatchError> {
+		Ok(())
+	}
+}
