200c5d31fac6672c5191db12ddc32a6649856703
albert <allimos3@gmail.com>
2024-03-14 16:02:07 +0100

    chore: set connections to 500 dor node and broker api

--- localnet/init/scripts/start-broker-api.sh
+++ localnet/init/scripts/start-broker-api.sh
@@ -6,5 +6,6 @@ echo $($binary_location/chainflip-broker-api --version)
 
 RUST_LOG=debug,jsonrpsee_types::params=trace $binary_location/chainflip-broker-api \
   --port=10997 \
+  --max_connections=500 \
   --state_chain.ws_endpoint=ws://localhost:9944 \
   --state_chain.signing_key_file $KEYS_DIR/BROKER_1 > /tmp/chainflip/chainflip-broker-api.log 2>&1 &
