9d7a13efcb32de530053e7a63d72a320f39c6f56
kylezs <zsembery.kyle@gmail.com>
2024-03-18 15:27:42 +0100

    doc: clarifying comment for dylib build

--- engine-dylib/build.rs
+++ engine-dylib/build.rs
@@ -1,5 +1,8 @@
 use std::{fs, path::Path};
 
+// We want to enforce the fact that the package version, and the version suffix on the dylib
+// matches at compile time.
+// e.g. if version is `1.4.0` then the dylib lib name should be: `chainflip_engine_v1_4_0`
 fn main() {
 	let manifest_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("Cargo.toml");
 	let manifest_contents = fs::read_to_string(manifest_path).expect("Could not read Cargo.toml");
@@ -18,5 +21,9 @@ fn main() {
 
 	let lib_name = cargo_toml.get("lib").and_then(|l| l.get("name")).expect("Should be a lib");
 	let lib_name = lib_name.as_str().unwrap();
-	assert_eq!(lib_name, format!("chainflip_engine_v{}", version), "lib name should match version");
+	assert_eq!(
+		lib_name,
+		format!("chainflip_engine_v{}", version),
+		"lib name version suffix should match package version"
+	);
 }
