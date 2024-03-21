9634dc2b8ba6d50e6ee5444ae5ac8dd398a12bd9
kylezs <zsembery.kyle@gmail.com>
2024-03-21 10:03:37 +0100

    build improvements

--- engine-dylib/build.rs
+++ engine-dylib/build.rs
@@ -1,5 +1,10 @@
 use std::{fs, path::Path};
 
+use engine_upgrade_utils::NEW_VERSION;
+
+// We want to enforce the fact that the package version, and the version suffix on the dylib
+// matches at compile time.
+// e.g. if version is `1.4.0` then the dylib lib name should be: `chainflip_engine_v1_4_0`
 fn main() {
 	let manifest_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("Cargo.toml");
 	let manifest_contents = fs::read_to_string(manifest_path).expect("Could not read Cargo.toml");
@@ -13,10 +18,17 @@ fn main() {
 		.and_then(|p| p.get("version"))
 		.unwrap()
 		.as_str()
-		.unwrap()
-		.replace('.', "_");
+		.unwrap();
+
+	assert_eq!(version, NEW_VERSION);
+
+	let version_suffix = version.replace('.', "_");
 
 	let lib_name = cargo_toml.get("lib").and_then(|l| l.get("name")).expect("Should be a lib");
 	let lib_name = lib_name.as_str().unwrap();
-	assert_eq!(lib_name, format!("chainflip_engine_v{}", version), "lib name should match version");
+	assert_eq!(
+		lib_name,
+		format!("chainflip_engine_v{}", version_suffix),
+		"lib name version suffix should match package version"
+	);
 }
---
+++ engine-runner-bin/build.rs
@@ -0,0 +1,68 @@
+use std::{fs, path::Path};
+
+use engine_upgrade_utils::{NEW_VERSION, OLD_VERSION};
+
+fn main() {
+	// === Ensure the runner runs the linker checks at compile time ===
+
+	let out_dir = std::env::var("OUT_DIR").unwrap();
+
+	let build_dir = std::path::Path::new(&out_dir)
+		.parent()
+		.unwrap()
+		.parent()
+		.unwrap()
+		.parent()
+		.unwrap(); // target/debug or target/release
+
+	// ./old-engine-dylib from project root.
+	let old_version = build_dir.parent().unwrap().parent().unwrap().join("old-engine-dylib");
+
+	let old_version_str = old_version.to_str().unwrap();
+
+	let build_dir_str = build_dir.to_str().unwrap(); // target/debug or target/release
+
+	println!("cargo:rustc-link-search=native={old_version_str}");
+	println!("cargo:rustc-link-search=native={build_dir_str}");
+
+	let old_version_suffix = OLD_VERSION.replace('.', "_");
+	let new_version_suffix = NEW_VERSION.replace('.', "_");
+
+	println!("cargo:rustc-link-lib=dylib=chainflip_engine_v{}", old_version_suffix);
+	println!("cargo:rustc-link-lib=dylib=chainflip_engine_v{}", new_version_suffix);
+
+	// ===  Sanity check that the the assets have an item with the matching version. ===
+
+	let manifest_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("Cargo.toml");
+	let manifest_contents = fs::read_to_string(manifest_path).expect("Could not read Cargo.toml");
+
+	let cargo_toml: toml::Value =
+		toml::from_str(&manifest_contents).expect("Could not parse Cargo.toml");
+
+	// Get the version from the Cargo.toml
+	let package_version = cargo_toml
+		.get("package")
+		.and_then(|p| p.get("version"))
+		.unwrap()
+		.as_str()
+		.unwrap();
+
+	assert_eq!(package_version, NEW_VERSION);
+
+	let deb_assets: Vec<Vec<String>> = cargo_toml
+		.get("package")
+		.unwrap()
+		.get("metadata")
+		.unwrap()
+		.get("deb")
+		.unwrap()
+		.get("assets")
+		.unwrap()
+		.clone()
+		.try_into()
+		.unwrap();
+
+	let mut flat_deb_assets = deb_assets.iter().flatten();
+	assert!(flat_deb_assets.any(|item| item.contains(&new_version_suffix)));
+	assert!(flat_deb_assets.any(|item| item.contains(&old_version_suffix)));
+}
