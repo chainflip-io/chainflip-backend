75d24d8dda4113fecf5ec81a817b5d8c505eb21e
Daniel <daniel@chainflip.io>
2024-03-21 16:36:13 +0100

    fix: update usdt address for testnet

--- state-chain/pallets/cf-environment/src/migrations/usdt.rs
+++ state-chain/pallets/cf-environment/src/migrations/usdt.rs
@@ -10,7 +10,7 @@ impl<T: Config> OnRuntimeUpgrade for Migration<T> {
 			NetworkEnvironment::Mainnet =>
 				hex_literal::hex!("dAC17F958D2ee523a2206206994597C13D831ec7").into(),
 			NetworkEnvironment::Testnet =>
-				hex_literal::hex!("7169D38820dfd117C3FA1f22a697dBA58d90BA06").into(),
+				hex_literal::hex!("27cea6eb8a21aae05eb29c91c5ca10592892f584").into(),
 			NetworkEnvironment::Development =>
 				hex_literal::hex!("0DCd1Bf9A1b36cE34237eEaFef220932846BCD82").into(),
 		};
