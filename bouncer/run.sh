a4ca6f24b4345bb5a04e4a5e335bb6599911e9c0
albert <allimos3@gmail.com>
2024-03-25 13:45:38 +0100

    chore: restore run.sh

--- bouncer/run.sh
+++ bouncer/run.sh
@@ -4,5 +4,4 @@ if lsof -Pi :8899 -sTCP:LISTEN -t >/dev/null ; then
     ./commands/send_sol.ts 7QQGNm3ptwinipDCyaCF7jY5katgmFUu1ieP2f7nwLpE 1.2
 fi
 ./setup_for_test.sh
-./tests/gaslimit_ccm.ts
 ./tests/all_concurrent_tests.ts $1
