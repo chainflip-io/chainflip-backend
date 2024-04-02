418d609ef769b464274b19c1d568c25d839178f5
kylezs <zsembery.kyle@gmail.com>
2024-04-02 14:11:42 +0200

    chore: repr(C) CStrArray and pass it around

--- engine-dylib/src/lib.rs
+++ engine-dylib/src/lib.rs
@@ -1,11 +1,9 @@
 use chainflip_engine::settings_and_run_main;
 use engine_proc_macros::cfe_entrypoint;
-use engine_upgrade_utils::ExitStatus;
+use engine_upgrade_utils::{CStrArray, ExitStatus};
 
 // The `cfe_entrypoint` macro adds the required C parameters to the function signature
 #[cfe_entrypoint]
 fn cfe_entrypoint() {
-	let rust_string_args = engine_upgrade_utils::rust_string_args(args, n_args);
-
-	settings_and_run_main(rust_string_args, start_from)
+	settings_and_run_main(c_args.rust_string_args(), start_from)
 }
