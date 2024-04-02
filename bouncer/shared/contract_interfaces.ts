e44d76683ec5de8eccd873ec2f4085848a5536e1
albert <allimos3@gmail.com>
2024-04-02 17:21:35 +0200

    chore: upgrade to 0.1.5 for upgrade_manager abi

--- bouncer/shared/contract_interfaces.ts
+++ bouncer/shared/contract_interfaces.ts
@@ -15,7 +15,7 @@ function loadContractCached(abiPath: string) {
   };
 }
 const CF_ETH_CONTRACT_ABI_TAG = 'v1.1.2';
-const CF_SOL_PROGRAM_IDL_TAG = 'v0.1.4';
+const CF_SOL_PROGRAM_IDL_TAG = 'v0.1.5';
 export const getErc20abi = loadContractCached(
   '../contract-interfaces/eth-contract-abis/IERC20.json',
 );
