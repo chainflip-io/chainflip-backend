cafe7a76a3db1516683eab53cd189acc764b31dd
Maxim Shishmarev <maxim@chainflip.io>
2024-04-03 12:39:57 +1100

    fix: revert spec version change

--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -169,7 +169,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("chainflip-node"),
 	impl_name: create_runtime_str!("chainflip-node"),
 	authoring_version: 1,
-	spec_version: 141,
+	spec_version: 140,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 12,
