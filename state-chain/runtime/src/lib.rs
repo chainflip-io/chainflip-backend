cd1eca103e5a634ff45e63f6d70d8a5d624c11f9
kylezs <zsembery.kyle@gmail.com>
2024-04-10 18:24:55 +0200

    chore: bump spec version

--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -163,7 +163,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("chainflip-node"),
 	impl_name: create_runtime_str!("chainflip-node"),
 	authoring_version: 1,
-	spec_version: 133,
+	spec_version: 134,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 12,
