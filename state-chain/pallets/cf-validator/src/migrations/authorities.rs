f57fcc9c84cee05afcaa5dc8849afce49340d271
marcellorigotti <marcello@chainflip.io>
2024-04-10 14:09:29 +0200

    remove debug println

--- state-chain/pallets/cf-validator/src/migrations/authorities.rs
+++ state-chain/pallets/cf-validator/src/migrations/authorities.rs
@@ -7,12 +7,10 @@ pub struct Migration<T: Config>(PhantomData<T>);
 
 impl<T: Config> OnRuntimeUpgrade for Migration<T> {
 	fn on_runtime_upgrade() -> frame_support::weights::Weight {
-		let result = CurrentAuthorities::<T>::translate::<BTreeSet<ValidatorIdOf<T>>, _>(|btree| {
-			Some(btree.unwrap().into_iter().collect::<Vec<ValidatorIdOf<T>>>())
-		});
-		if result.is_err() {
-			println!("ERROR DURING MIGRATION");
-		}
+		let _result =
+			CurrentAuthorities::<T>::translate::<BTreeSet<ValidatorIdOf<T>>, _>(|btree| {
+				Some(btree.unwrap().into_iter().collect::<Vec<ValidatorIdOf<T>>>())
+			});
 
 		HistoricalAuthorities::<T>::translate::<BTreeSet<ValidatorIdOf<T>>, _>(
 			|_epoch_index, btree| Some(btree.into_iter().collect::<Vec<ValidatorIdOf<T>>>()),
