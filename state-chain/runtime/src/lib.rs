7ac565961a9924f4b6d9b7a367a3bc8c63046078
Daniel <daniel@chainflip.io>
2024-04-11 10:54:59 +0200

    feat: sisyphos re-broadcast

--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -871,14 +871,18 @@ pub struct ResignFailedEthereumBroadcast;
 
 const FAILED_BROADCAST_ID_BERGHAIN: u32 = 3348;
 const FAILED_BROADCAST_ID_PERSEVERANCE: u32 = 113;
+const FAILED_BROADCAST_ID_SISYPHOS: u32 = 93;
 
 pub const BERGHAIN: [u8; 32] =
 	hex_literal::hex!("8b8c140b0af9db70686583e3f6bf2a59052bfe9584b97d20c45068281e976eb9");
 pub const PERSEVERANCE: [u8; 32] =
 	hex_literal::hex!("7a5d4db858ada1d20ed6ded4933c33313fc9673e5fffab560d0ca714782f2080");
+pub const SISYPHOS: [u8; 32] =
+	hex_literal::hex!("7db0684f891ad10fa919c801f9a9f030c0f6831aafa105b1a68e47803f91f2b6");
 
 fn failed_broadcast_id() -> Option<u32> {
 	match frame_system::BlockHash::<Runtime>::get(0).to_fixed_bytes() {
+		SISYPHOS => Some(FAILED_BROADCAST_ID_SISYPHOS),
 		PERSEVERANCE => Some(FAILED_BROADCAST_ID_PERSEVERANCE),
 		BERGHAIN => Some(FAILED_BROADCAST_ID_BERGHAIN),
 		_ => None,
@@ -917,11 +921,7 @@ impl frame_support::traits::OnRuntimeUpgrade for ResignFailedEthereumBroadcast {
 
 	#[cfg(feature = "try-runtime")]
 	fn post_upgrade(_state: Vec<u8>) -> Result<(), DispatchError> {
-		if let Some(id) = match frame_system::BlockHash::<Runtime>::get(0).to_fixed_bytes() {
-			PERSEVERANCE => Some(FAILED_BROADCAST_ID_PERSEVERANCE),
-			BERGHAIN => Some(FAILED_BROADCAST_ID_BERGHAIN),
-			_ => None,
-		} {
+		if let Some(id) = failed_broadcast_id() {
 			frame_support::ensure!(
 				!pallet_cf_broadcast::ThresholdSignatureData::<Runtime, EthereumInstance>::contains_key(id),
 				"Broadcast {id} was not removed.",
