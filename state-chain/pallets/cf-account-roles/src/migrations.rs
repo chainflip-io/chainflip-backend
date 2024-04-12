4a962615b970c2f8fcca3d89f80e5182f0459973
Daniel <daniel@chainflip.io>
2024-04-12 13:26:19 +0200

    fix: vanity names migration version numbers

--- state-chain/pallets/cf-account-roles/src/migrations.rs
+++ state-chain/pallets/cf-account-roles/src/migrations.rs
@@ -1,7 +1,20 @@
-use cf_runtime_upgrade_utilities::VersionedMigration;
+use crate::Pallet;
+use cf_runtime_upgrade_utilities::{NoopRuntimeUpgrade, VersionedMigration};
 
 mod remove_swapping_enabled;
 
-// Migration 1->2 is in the runtime/src/lib.rs `VanityNamesMigration`
-pub type PalletMigration<T> =
-	(VersionedMigration<crate::Pallet<T>, remove_swapping_enabled::Migration<T>, 0, 1>,);
+pub type PalletMigration<T> = (
+	VersionedMigration<Pallet<T>, remove_swapping_enabled::Migration<T>, 0, 1>,
+	// Migration 1 -> 2 is in the runtime/src/lib.rs `VanityNamesMigration`
+	// This ensures the storage version bump.
+	VersionedMigration<
+		Pallet<T>,
+		NoopRuntimeUpgrade,
+		{ vanity_name_migration::APPLY_AT_ACCOUNT_ROLES_STORAGE_VERSION - 1 },
+		{ vanity_name_migration::APPLY_AT_ACCOUNT_ROLES_STORAGE_VERSION },
+	>,
+);
+
+pub mod vanity_name_migration {
+	pub const APPLY_AT_ACCOUNT_ROLES_STORAGE_VERSION: u16 = 2;
+}
--- state-chain/pallets/cf-validator/src/migrations.rs
+++ state-chain/pallets/cf-validator/src/migrations.rs
@@ -1,9 +1,23 @@
-use cf_runtime_upgrade_utilities::VersionedMigration;
+use crate::Pallet;
+use cf_runtime_upgrade_utilities::{NoopRuntimeUpgrade, VersionedMigration};
 
 mod authorities;
 
-pub type PalletMigration<T> =
-	(VersionedMigration<crate::Pallet<T>, authorities::Migration<T>, 0, 1>,);
+pub type PalletMigration<T> = (
+	VersionedMigration<Pallet<T>, authorities::Migration<T>, 0, 1>,
+	// Migration 1 -> 2 is in the runtime/src/lib.rs `VanityNamesMigration`
+	// This ensures the storage version bump.
+	VersionedMigration<
+		Pallet<T>,
+		NoopRuntimeUpgrade,
+		{ vanity_name_migration::APPLY_AT_VALIDATOR_STORAGE_VERSION - 1 },
+		{ vanity_name_migration::APPLY_AT_VALIDATOR_STORAGE_VERSION },
+	>,
+);
+
+pub mod vanity_name_migration {
+	pub const APPLY_AT_VALIDATOR_STORAGE_VERSION: u16 = 2;
+}
 
 #[cfg(feature = "try-runtime")]
 pub mod old {
@@ -11,7 +25,7 @@ pub mod old {
 	use cf_primitives::AccountId;
 	use frame_support::pallet_prelude::ValueQuery;
 
-	// Migration 0->1 is in the runtime/src/lib.rs `VanityNamesMigration`
+	// Migration 1 -> 2 is in the runtime/src/lib.rs `VanityNamesMigration`
 	#[frame_support::storage_alias]
 	pub type VanityNames<T: Config> =
 		StorageValue<Pallet<T>, BTreeMap<AccountId, Vec<u8>>, ValueQuery>;
