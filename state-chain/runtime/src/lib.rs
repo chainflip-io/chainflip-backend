ac3fa5c5d97f77ed0ba7023a5fa742ea1f6badb1
Andrew Dibble <andrew@chainflip.io>
2024-04-18 17:29:14 +0200

    remove transaction

--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -36,11 +36,7 @@ use cf_runtime_upgrade_utilities::VersionedMigration;
 use cf_traits::{AdjustedFeeEstimationApi, AssetConverter, LpBalanceApi};
 use codec::Encode;
 use core::ops::Range;
-use frame_support::{
-	instances::*,
-	sp_runtime::{AccountId32, TransactionOutcome},
-	storage::with_transaction,
-};
+use frame_support::{instances::*, sp_runtime::AccountId32};
 pub use frame_system::Call as SystemCall;
 use pallet_cf_governance::GovCallHash;
 use pallet_cf_ingress_egress::{ChannelAction, DepositWitness, OwedAmount, TargetChainAsset};
@@ -1282,23 +1278,19 @@ impl_runtime_apis! {
 
 			let (asset_pair, order) = AssetPair::from_swap(from, to).ok_or(DispatchErrorWithMessage::Other(DispatchError::Other("Invalid asset pair")))?;
 
-			with_transaction(|| {
-				let result = pallet_cf_pools::Pools::<Runtime>::try_mutate(asset_pair, |maybe_pool| {
-					let pool = maybe_pool.as_mut().ok_or(DispatchErrorWithMessage::Other(DispatchError::Other("Pool not found")))?;
+			let result = pallet_cf_pools::Pools::<Runtime>::try_mutate(asset_pair, |maybe_pool| {
+				let pool = maybe_pool.as_mut().ok_or(DispatchErrorWithMessage::Other(DispatchError::Other("Pool not found")))?;
 
-					for (id, (tick, amount)) in limit_orders.into_iter().enumerate() {
-						let account_id = AccountId32::new([0; 32]);
-						pool.pool_state.collect_and_mint_limit_order(&(account_id, id as u64), order, tick, amount)
-							.map_err(|_| DispatchErrorWithMessage::Other(DispatchError::Other("Failed to set limit order")))?;
-					}
+				for (id, (tick, amount)) in limit_orders.into_iter().enumerate() {
+					let account_id = AccountId32::new([0; 32]);
+					pool.pool_state.collect_and_mint_limit_order(&(account_id, id as u64), order, tick, amount)
+						.map_err(|_| DispatchErrorWithMessage::Other(DispatchError::Other("Failed to set limit order")))?;
+				}
 
-					Ok::<_, DispatchErrorWithMessage>(())
-				}).and_then(|_| {
-					LiquidityPools::swap_with_network_fee(from, to, amount).map_err(Into::into)
-				});
+				Ok::<_, DispatchErrorWithMessage>(())
+			})?;
 
-				TransactionOutcome::Rollback(result)
-			})
+			LiquidityPools::swap_with_network_fee(from, to, amount).map_err(Into::into)
 		}
 
 		fn cf_pool_info(base_asset: Asset, quote_asset: Asset) -> Result<PoolInfo, DispatchErrorWithMessage> {
