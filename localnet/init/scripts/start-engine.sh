36243881aeedc8d144e21040770ccf6a6d591d21
kylezs <zsembery.kyle@gmail.com>
2024-04-19 09:36:06 +0200

    fix: more robust script path

--- localnet/init/scripts/start-engine.sh
+++ localnet/init/scripts/start-engine.sh
@@ -3,16 +3,21 @@
 # LOG_SUFFIX is optional. It can be used to differentiate between the logs of two engines, potentially with the same owner
 # e.g. in the case of an upgrade where we run two engines simultaneously.
 
+
+SCRIPT_DIR=$(dirname "$(realpath "$BASH_SOURCE")")
+OLD_ENGINE_DYLIB_PATH="${SCRIPT_DIR}/../../../old-engine-dylib/"
+
 echo "Starting engine..."
 if [ -n "$DYLD_LIBRARY_PATH" ]; then
-    export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:old-engine-dylib"
+    export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$OLD_ENGINE_DYLIB_PATH"
 else
-    export DYLD_LIBRARY_PATH="old-engine-dylib"
+    export DYLD_LIBRARY_PATH="$OLD_ENGINE_DYLIB_PATH"
 fi
+
 if [ -n "$LD_LIBRARY_PATH" ]; then
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:old-engine-dylib"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$OLD_ENGINE_DYLIB_PATH"
 else
-    export LD_LIBRARY_PATH="old-engine-dylib"
+    export LD_LIBRARY_PATH="$OLD_ENGINE_DYLIB_PATH"
 fi
 
 echo "DYLD_LIBRARY_PATH/LD_LIBRARY_PATH to find the engine dylib: $DYLD_LIBRARY_PATH"
