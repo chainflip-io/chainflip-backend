2274a467f65e6ce5698fd8a4b1c2995ca62105ca
albert <allimos3@gmail.com>
2024-04-25 17:30:08 +0200

    chore: add prints

--- localnet/init/scripts/start-solana.sh
+++ localnet/init/scripts/start-solana.sh
@@ -3,4 +3,7 @@
 set -e
 DATETIME=$(date '+%Y-%m-%d_%H-%M-%S')
 export RUST_LOG=solana_runtime::system_instruction_processor=trace,solana_runtime::message_processor=info,solana_bpf_loader=debug,solana_rbpf=debug
-solana-test-validator --ledger /tmp/solana/test-ledger/snapshot > /tmp/solana/solana.$DATETIME.log 2>&1 &
+rustc --version
+solana --version
+solana-test-validator --version
+solana-test-validator --ledger /tmp/solana/test-ledger > /tmp/solana/solana.$DATETIME.log 2>&1 &
