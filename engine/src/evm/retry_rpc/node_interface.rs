4c52b53215490b60e324ca85f31f0c9e5c657a58
Albert Llimos <53186777+albert-llimos@users.noreply.github.com>
2024-04-30 15:26:28 +0200

    Arbitrum gas limit multiplier (#4781)
    * chore: add node_inderface and gas_estimate
    
    * chore: update wrong import
    
    * chore: implementation
    
    * chore: working impl
    
    * chore: small updates
    
    * chore: cleanup chain_tracking hardcoded values
    
    * chore: update estimations
    
    * chore: remove remaining comments
    
    * chore: remove unused line
    
    * chore: update values
    
    * chore: typo
    
    * chore: update function
    
    * chore: improve NODE_INTERFACE comment

---
+++ engine/src/evm/retry_rpc/node_interface.rs
@@ -0,0 +1,49 @@
+use ethers::prelude::*;
+
+use crate::evm::rpc::{node_interface::NodeInterfaceRpcApi, EvmRpcApi};
+
+use super::EvmRetryRpcClient;
+
+use crate::evm::retry_rpc::RequestLog;
+
+#[async_trait::async_trait]
+pub trait NodeInterfaceRetryRpcApi {
+	async fn gas_estimate_components(
+		&self,
+		destination_address: H160,
+		contract_creation: bool,
+		tx_data: Bytes,
+	) -> (u64, u64, U256, U256);
+}
+
+#[async_trait::async_trait]
+impl<Rpc: EvmRpcApi + NodeInterfaceRpcApi> NodeInterfaceRetryRpcApi for EvmRetryRpcClient<Rpc> {
+	async fn gas_estimate_components(
+		&self,
+		destination_address: H160,
+		contract_creation: bool,
+		tx_data: Bytes,
+	) -> (u64, u64, U256, U256) {
+		self.rpc_retry_client
+			.request(
+				RequestLog::new(
+					"gas_estimate_components".to_string(),
+					Some(format!("{destination_address:?}, {contract_creation:?}")),
+				),
+				Box::pin(move |client| {
+					let tx_data = tx_data.clone();
+					#[allow(clippy::redundant_async_block)]
+					Box::pin(async move {
+						client
+							.gas_estimate_components(
+								destination_address,
+								contract_creation,
+								tx_data,
+							)
+							.await
+					})
+				}),
+			)
+			.await
+	}
+}
--- state-chain/chains/src/arb.rs
+++ state-chain/chains/src/arb.rs
@@ -63,6 +63,7 @@ impl Chain for Arbitrum {
 #[codec(mel_bound())]
 pub struct ArbitrumTrackedData {
 	pub base_fee: <Arbitrum as Chain>::ChainAmount,
+	pub gas_limit_multiplier: FixedU64,
 }
 
 impl Default for ArbitrumTrackedData {
@@ -82,10 +83,10 @@ impl ArbitrumTrackedData {
 }
 
 pub mod fees {
-	pub const BASE_COST_PER_BATCH: u128 = 5_200_000;
-	pub const GAS_COST_PER_FETCH: u128 = 1_700_000;
-	pub const GAS_COST_PER_TRANSFER_NATIVE: u128 = 1_500_000;
-	pub const GAS_COST_PER_TRANSFER_TOKEN: u128 = 1_800_000;
+	pub const BASE_COST_PER_BATCH: u128 = 60_000;
+	pub const GAS_COST_PER_FETCH: u128 = 30_000;
+	pub const GAS_COST_PER_TRANSFER_NATIVE: u128 = 20_000;
+	pub const GAS_COST_PER_TRANSFER_TOKEN: u128 = 40_000;
 }
 
 impl FeeEstimationApi<Arbitrum> for ArbitrumTrackedData {
@@ -103,7 +104,8 @@ impl FeeEstimationApi<Arbitrum> for ArbitrumTrackedData {
 				assets::arb::Asset::ArbUsdc => GAS_COST_PER_FETCH,
 			};
 
-		self.base_fee.saturating_mul(gas_cost_per_fetch)
+		self.base_fee
+			.saturating_mul(self.gas_limit_multiplier.saturating_mul_int(gas_cost_per_fetch))
 	}
 
 	fn estimate_egress_fee(
@@ -118,7 +120,8 @@ impl FeeEstimationApi<Arbitrum> for ArbitrumTrackedData {
 				assets::arb::Asset::ArbUsdc => GAS_COST_PER_TRANSFER_TOKEN,
 			};
 
-		self.base_fee.saturating_mul(gas_cost_per_transfer)
+		self.base_fee
+			.saturating_mul(self.gas_limit_multiplier.saturating_mul_int(gas_cost_per_transfer))
 	}
 }
 
