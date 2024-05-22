545c94bd1236943a577c84aefff0e085da7712f3
albert <allimos3@gmail.com>
2024-05-22 11:48:05 +0200

    chore: checkpoint

--- engine/src/sol/rpc.rs
+++ engine/src/sol/rpc.rs
@@ -1,13 +1,13 @@
-use cf_chains::btc::BitcoinNetwork;
 use futures_core::Future;
-use subxt::ext::sp_runtime::print;
+// use subxt::ext::sp_runtime::print;
 use thiserror::Error;
 
 use reqwest::Client;
-use serde::{Deserialize, Deserializer, Serialize};
+use serde::{Deserialize, Serialize};
 
 use serde;
-use serde_json::{json, Map};
+use serde_json::json;
+use serde_bytes;
 
 use tracing::error;
 use utilities::make_periodic_tick;
@@ -18,7 +18,11 @@ use anyhow::{anyhow, Context, Result};
 use tracing::warn;
 
 use cf_chains::sol::SolHash;
+use cf_chains::sol::sol_tx_core::Pubkey;
+
 use std::str::FromStr;
+use super::rpc_client_api::*;
+use super::commitment_config::CommitmentConfig;
 
 // From jsonrpc crate
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -57,7 +61,7 @@ pub struct SolRpcClient {
 
 impl SolRpcClient {
 	pub fn new(
-		// TODO: We should probably use SecretUrl isntead, as in the http_sender.rs
+		// TODO: Should we use SecretUrl instead?
 		endpoint: HttpBasicAuthEndpoint,
 		expected_genesis_hash: Option<SolHash>,
 	) -> anyhow::Result<impl Future<Output = Self>> {
@@ -113,7 +117,7 @@ impl SolRpcClient {
 #[derive(Clone, Debug)]
 enum ReqParams {
 	Empty,
-	Batch(Vec<serde_json::Value>),
+	Single(serde_json::Value),
 }
 
 async fn call_rpc_raw(
@@ -129,19 +133,15 @@ async fn call_rpc_raw(
 			"method": method,
 			"params": []
 		})],
-		ReqParams::Batch(params) => params
-			.into_iter()
-			.enumerate()
-			.map(|(i, p)| {
-				json!({
-					"jsonrpc": "2.0",
-					"id": i,
-					"method": method,
-					"params": p
-				})
-			})
-			.collect::<Vec<serde_json::Value>>(),
+		ReqParams::Single(params) => vec![json!({
+            "jsonrpc": "2.0",
+            "id": 0,
+            "method": method,
+            "params": params
+        })],
 	};
+
+	println!("request_body: {:?}", request_body);
 
 	let response = client
 		.post(endpoint.http_endpoint.as_ref())
@@ -150,25 +150,14 @@ async fn call_rpc_raw(
 		.send()
 		.await
 		.map_err(Error::Transport)?;
+	
 
 	let response = response
 		.json::<Vec<serde_json::Value>>()
 		.await
-		.map_err(Error::Transport)
-		.and_then(|result| match params {
-			ReqParams::Batch(params) =>
-				if params.len() == result.len() {
-					// a bunch of json result values containing an error and a result field.
-					Ok(result)
-				} else {
-					Err(Error::Rpc(RpcError {
-						code: -1,
-						message: "Incorrect response number for batch request".to_string(),
-						data: None,
-					}))
-				},
-			ReqParams::Empty => Ok(result),
-		})?;
+		.map_err(Error::Transport)?;
+	println!("response: {:?}", response);
+
 
 	response
 		.into_iter()
@@ -180,6 +169,7 @@ async fn call_rpc_raw(
 				Ok(r["result"].to_owned())
 			}
 		})
+		.inspect(|r| println!("Value before collect: {:?}", r))
 		.collect::<Result<_, Error>>()
 }
 
@@ -196,75 +186,80 @@ async fn get_genesis_hash(
 		.next()
 		.ok_or(anyhow!("Missing response from getGenesisHash"))?;
 
-	println!("json_value: {:?}", json_value.as_str().unwrap());
 	let genesis_hash_str = json_value
 		.as_str()
 		.ok_or(anyhow!("Missing or empty `result` field in getGenesisHash response"))?;
-	println!("genesis_hash_str: {:?}", genesis_hash_str);
 
-	// TODO: This conversion is incorrect but the values look correct
 	Ok(SolHash::from_str(genesis_hash_str).context("Invalid genesis hash")?)
 }
 
-#[derive(Clone, PartialEq, Debug, Deserialize, Serialize)]
+/// An Account with data that is stored on chain
+#[repr(C)]
+// #[frozen_abi(digest = "HawRVHh7t4d3H3bitWHFt25WhhoDmbJMCfWdESQQoYEy")]
+#[derive(Deserialize, PartialEq, Eq, Clone, Default /* ,  AbiExample */ )]
 #[serde(rename_all = "camelCase")]
-pub struct BlockHeader {
-	// pub hash: bitcoin::BlockHash,
-	pub confirmations: i32,
-	// pub height: u64,
-	// pub version: Version,
-	// pub version_hex: Option<String>,
-	// // Don't care to write custom deserializer for this
-	// #[serde(rename = "merkleroot")]
-	// pub merkle_root: bitcoin::hash_types::TxMerkleNode,
-	// pub time: usize,
-	// #[serde(rename = "mediantime")]
-	// pub median_time: Option<usize>,
-	// pub nonce: u32,
-	// pub bits: String,
-	// pub difficulty: Difficulty,
-	// // Don't care to write custom deserializer for this
-	// pub chainwork: Option<String>,
-	// pub n_tx: usize,
-	// #[serde(rename = "previousblockhash")]
-	// pub previous_block_hash: Option<bitcoin::BlockHash>,
-	// #[serde(rename = "nextblockhash")]
-	// pub next_block_hash: Option<bitcoin::BlockHash>,
-
-	// pub strippedsize: Option<usize>,
-	// pub size: Option<usize>,
-	// pub weight: Option<usize>,
+pub struct Account {
+    /// lamports in the account
+    pub lamports: u64,
+    /// data held in this account
+    #[serde(with = "serde_bytes")]
+    pub data: Vec<u8>,
+    /// the program that owns this account. If executable, the program that loads this account.
+    pub owner: Pubkey,
+    /// this account's data contains a loaded program (and is now read-only)
+    pub executable: bool,
+    /// the epoch at which this account will next owe rent
+    pub rent_epoch: u64,
 }
 
 #[async_trait::async_trait]
 pub trait SolRpcApi {
-	// async fn block_header(&self, block_hash: BlockHash) -> anyhow::Result<BlockHeader>;
+	async fn get_recent_prioritization_fees(&self) -> anyhow::Result<Vec<RpcPrioritizationFee>>;
+	async fn get_multiple_accounts_with_config(
+		&self,
+		pubkeys: &[Pubkey],
+		config: RpcAccountInfoConfig,
+	) -> Result<Vec<Option<Account>>>;
 }
 
 #[async_trait::async_trait]
 impl SolRpcApi for SolRpcClient {
-	// async fn block_header(&self, block_hash: BlockHash) -> anyhow::Result<BlockHeader> {
-	// 	Ok(self
-	// 		.call_rpc("getblockheader", ReqParams::Batch(vec![json!([json!(block_hash)])]))
-	// 		.await?
-	// 		.into_iter()
-	// 		.next()
-	// 		.ok_or_else(|| anyhow!("Response missing block header"))?)
-	// }
+	async fn get_recent_prioritization_fees(&self) -> anyhow::Result<Vec<RpcPrioritizationFee>> {
+		Ok(self
+			.call_rpc("getRecentPrioritizationFees", ReqParams::Empty)
+			.await?
+			.into_iter()
+			.next()
+			.ok_or_else(|| anyhow!("Response missing prioritization fees"))?)
+	}
+	async fn get_multiple_accounts_with_config(
+			&self,
+			pubkeys: &[Pubkey],
+			config: RpcAccountInfoConfig,
+		) -> Result<Vec<Option<Account>>> {
+			// TODO: We might want to request a data slice => No data for Sol, only balance for tokens.
+			Ok(self
+				// .call_rpc("getMultipleAccounts", ReqParams::Batch(vec![json!([json!(pubkeys), json!(config)])]))
+				// .call_rpc("getMultipleAccounts", ReqParams::Batch(vec![json!([json!["vines1vzrYbzLMRdu58ou5XTby4qAqVRLmqo36NKPTg"], json!(config)])]))
+				.call_rpc("getMultipleAccounts", ReqParams::Single(json!([["vines1vzrYbzLMRdu58ou5XTby4qAqVRLmqo36NKPTg"], json!(config)])))
+				.await?
+				.into_iter()
+				.next()
+				.ok_or_else(|| anyhow!("Response missing getMultipleAccounts"))?)
+		}	
 }
 
 #[cfg(test)]
 mod tests {
+	// use utilities::testing::logging::init_test_logger;
 
 	use super::*;
 
 	#[tokio::test]
 	async fn test_sol_asyc() {
-		// 		init_test_logger();
-
-		let client = SolRpcClient::new(
+		let sol_rpc_client = SolRpcClient::new(
 			HttpBasicAuthEndpoint {
-				http_endpoint: "https://api.devnet.solana.com".into(),
+				http_endpoint: "https://api.testnet.solana.com".into(),
 				basic_auth_user: "flip".to_string(),
 				basic_auth_password: "flip".to_string(),
 			},
@@ -273,67 +268,41 @@ mod tests {
 		.unwrap()
 		.await;
 
-		let genesis_hash = get_genesis_hash(&client.client, &client.endpoint).await.unwrap();
-		println!("genesis_hash: {:?}", genesis_hash);
+		get_genesis_hash(&sol_rpc_client.client, &sol_rpc_client.endpoint).await.unwrap();
+	}
 
-		// 		let result: Result<VerboseBlock, _> = serde_path_to_error::deserialize(jd);
+	#[tokio::test]
+	async fn test_sol_devnet() {
+		// init_test_logger();
 
-		// 		match result {
-		// 			Ok(vb) => {
-		// 				println!("vb: {vb:?}");
-		// 				println!("Verbose block fee: {}", vb.txdata[1].fee.unwrap());
-		// 			},
-		// 			Err(e) => panic!("error: {e:?}"),
-		// 		}
+		let sol_rpc_client = SolRpcClient::new(
+			HttpBasicAuthEndpoint {
+				http_endpoint: "https://api.devnet.solana.com".into(),
+				basic_auth_user: "flip".to_string(),
+				basic_auth_password: "flip".to_string(),
+			},
+			Some(SolHash::from_str("EtWTRABZaYq6iMfeYKouRu166VU2xqa1wcaWoxPkrZBG").unwrap()),
+		)
+		.unwrap()
+		.await;
 
-		// 		let block_hash_zero = client.block_hash(0).await.unwrap();
 
-		// 		println!("block_hash_zero: {block_hash_zero:?}");
+		let priority_fees = sol_rpc_client.get_recent_prioritization_fees().await.unwrap();
+		// println!("priority_fees: {:?}", priority_fees);
 
-		// 		let block_zero = client.block(block_hash_zero).await.unwrap();
+		let accounts_info = sol_rpc_client.get_multiple_accounts_with_config(
+			&[Pubkey::from_str("vines1vzrYbzLMRdu58ou5XTby4qAqVRLmqo36NKPTg").unwrap()],
+			RpcAccountInfoConfig {
+				encoding: Some(UiAccountEncoding::JsonParsed),
+				data_slice: None,
+				commitment: Some(CommitmentConfig::finalized()),
+				min_context_slot: None,
+			},
+		).await.unwrap();
 
-		// 		println!("block_zero: {block_zero:?}");
+		// Print the first account's balance
+		println!("account_info: {:?}", accounts_info[0].as_ref().map(|a| a.lamports));
 
-		// 		let next_block_fee_rate = client.next_block_fee_rate().await.unwrap();
 
-		// 		println!("next_block_fee_rate: {next_block_fee_rate:?}");
-
-		// 		let best_block_hash = client.best_block_hash().await.unwrap();
-
-		// 		println!("best_block_hash: {best_block_hash:?}");
-
-		// 		let block_header = client.block_header(best_block_hash).await.unwrap();
-
-		// 		println!("block_header: {block_header:?}");
-
-		// 		let v_block = client.block(best_block_hash).await.unwrap();
-
-		// 		println!("verbose block: {v_block:?}");
-
-		// 		println!("number of txs: {}", v_block.txdata.len());
-
-		// 		let tx = &v_block.txdata[0];
-
-		// 		let raw_transaction =
-		// client.get_raw_transactions(vec![tx.txid]).await.unwrap()[0].txid(); 		assert_eq!
-		// (raw_transaction, tx.txid);
-
-		// 		// let average_block_fee_rate =
-		// 		// client.average_block_fee_rate(best_block_hash).await.unwrap();
-
-		// 		// println!("average_block_fee_rate: {average_block_fee_rate}");
-
-		// 		// Generate new hex bytes using ./bouncer/commands/create_raw_btc_tx.ts;
-		// 		// let hex_str =
-		// 		// "0200000000010133e287d3a464b226a1917303e1714af508a6bfe219265184a93c7f78851085a30000000000fdffffff0200e1f505000000001976a9149a1c78a507689f6f54b847ad1cef1e614ee23f1e88ac58d94a1f000000001600145baa7941ea1268fbd6279a0408a0419f8acd8245024730440220590dcc64661a362b54543f66d3cd24fdeae8a210643ad4f6fd39031281a9657902201f7a750d01f7cfc948ae4f8b76221b5c325f8ff82ac1b0d1d9a927632b40dd6001210386dc234ecbc4e677b927da260349cbd399c622507feb9dd2895a3537f6d4aa5d00000000"
-		// ;
-
-		// 		// let bytes = hex::decode(hex_str).unwrap();
-
-		// 		// let send_raw_transaction = client.send_raw_transaction(bytes).await.unwrap();
-
-		// 		// println!("tx_id: {:?}", send_raw_transaction);
-
-		// 		// let best_
 	}
 }
