84d685e660b6d03cdb6a29a81a247d01c2d66785
kylezs <zsembery.kyle@gmail.com>
2024-05-24 09:37:18 +0200

    tmp: deliberately error to test merge queue checks

--- bouncer/shared/upgrade_network.ts
+++ bouncer/shared/upgrade_network.ts
@@ -94,6 +94,8 @@ async function compatibleUpgrade(
   runtimePath: string,
   numberOfNodes: 1 | 3,
 ) {
+
+  throw new Error('Deliberate error, hopefully is removed from merge queue');
   await submitRuntimeUpgradeWithRestrictions(runtimePath, undefined, undefined, true);
 
   killOldNodes();
