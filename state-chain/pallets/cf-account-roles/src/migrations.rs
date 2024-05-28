a594ed0677d2690905c9933438e01b123eaad89c
Daniel <daniel@chainflip.io>
2024-05-28 12:46:03 +0200

    clippy

--- state-chain/pallets/cf-account-roles/src/migrations.rs
+++ state-chain/pallets/cf-account-roles/src/migrations.rs
@@ -1,4 +1,4 @@
 use crate::Pallet;
 use cf_runtime_upgrade_utilities::PlaceholderMigration;
 
-pub type PalletMigration<T> = (PlaceholderMigration<Pallet<T>, 2>);
+pub type PalletMigration<T> = PlaceholderMigration<Pallet<T>, 2>;
