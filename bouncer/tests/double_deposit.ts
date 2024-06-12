ae0d19ac204c3c1d483bb1d5caf4b9cf6e41d6d0
kylezs <kyle@chainflip.io>
2024-06-12 16:54:43 +0200

    refactor (bouncer): convert tests to new observeEvents (#4956)
    * refactor: use new observeEvent for dot runtime upgrade
    
    * refactor: convert redeem flip to new observeEvent
    
    * refactor: convert utxo consolidation test to new observeEvent
    
    * refactor: convert double_deposit to new observeEvent
    
    * chore: delete eth_witness_test
    
    * chore: clean up and fix double deposit test
    
    * chore: lint

--- bouncer/tests/double_deposit.ts
+++ bouncer/tests/double_deposit.ts
@@ -1,15 +1,15 @@
 #!/usr/bin/env -S pnpm tsx
-import { exec } from 'child_process';
 import { Keyring } from '../polkadot/keyring';
 import {
   runWithTimeout,
   sleep,
   hexStringToBytesArray,
   newAddress,
-  observeEvent,
   getChainflipApi,
   lpMutex,
 } from '../shared/utils';
+import { observeEvent } from '../shared/utils/substrate';
+import { sendEvmNative } from '../shared/send_evm';
 
 async function main(): Promise<void> {
   const keyring = new Keyring({ type: 'sr25519' });
@@ -33,39 +33,17 @@ async function main(): Promise<void> {
       .signAndSend(lp);
   });
   const ethIngressKey = (
-    await observeEvent(
-      'liquidityProvider:LiquidityDepositAddressReady',
-      chainflip,
-      (event) => event.data.depositAddress.Eth,
-    )
+    await observeEvent('liquidityProvider:LiquidityDepositAddressReady', {
+      test: (event) => event.data.depositAddress.Eth,
+    }).event
   ).data.depositAddress.Eth as string;
   console.log('Eth ingress address: ' + ethIngressKey);
   await sleep(8000); // sleep for 8 seconds to give the engine a chance to start witnessing
-  exec(
-    'pnpm tsx  ./commands/send_eth.ts ' + ethIngressKey + ' 10',
-    { timeout: 10000 },
-    (err, stdout, stderr) => {
-      if (stderr !== '') process.stdout.write(stderr);
-      if (err !== null) {
-        console.error(err);
-        process.exit(1);
-      }
-      if (stdout !== '') process.stdout.write(stdout);
-    },
-  );
-  await observeEvent('liquidityProvider:AccountCredited', chainflip);
-  exec(
-    'pnpm tsx  ./commands/send_eth.ts ' + ethIngressKey + ' 10',
-    { timeout: 10000 },
-    (err, stdout, stderr) => {
-      if (stderr !== '') process.stdout.write(stderr);
-      if (err !== null) {
-        console.error(err);
-        process.exit(1);
-      }
-      if (stdout !== '') process.stdout.write(stdout);
-    },
-  );
+  await sendEvmNative('Ethereum', ethIngressKey, '10');
+
+  await observeEvent('liquidityProvider:AccountCredited').event;
+  await sendEvmNative('Ethereum', ethIngressKey, '10');
+  await observeEvent('liquidityProvider:AccountCredited').event;
 }
 
 runWithTimeout(main(), 120000).catch((error) => {
