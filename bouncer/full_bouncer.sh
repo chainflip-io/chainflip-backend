ed224c6614006957fb3c7fc0dab5f982a6c57aca
kylezs <zsembery.kyle@gmail.com>
2024-06-17 15:59:58 +0200

    chore: fix full bouncer message

--- bouncer/full_bouncer.sh
+++ bouncer/full_bouncer.sh
@@ -1,9 +1,6 @@
 set -e
-echo "Running nightly tests 🧪"
+echo "Running full bouncer 🧪"
 ./setup_for_test.sh
-# gaslimit test has to run before all_concurrent tests
-# We might want to move this back into the standard CI run, but currently it's a little flaky
-# so is moved to nightly for now: PRO-1095
 ./tests/gaslimit_ccm.ts
 ./tests/all_concurrent_tests.ts $1
 ./tests/rotates_through_btc_swap.ts
