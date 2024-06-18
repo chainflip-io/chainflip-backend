d2afbd9750cf21b61bb0a0784a4d5f304bb0c8bc
Daniel <daniel@chainflip.io>
2024-06-18 12:22:41 +0200

    chore: bump migration version in env pallet

--- state-chain/pallets/cf-environment/src/migrations.rs
+++ state-chain/pallets/cf-environment/src/migrations.rs
@@ -32,7 +32,7 @@ impl<T: Config> OnRuntimeUpgrade for VersionUpdate<T> {
 	}
 }
 
-pub type PalletMigration<T> = PlaceholderMigration<Pallet<T>, 10>;
+pub type PalletMigration<T> = PlaceholderMigration<Pallet<T>, 11>;
 
 #[cfg(test)]
 mod tests {
