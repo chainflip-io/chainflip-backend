252fa3580e6f3232a20f3975b5da2ad8611fa669
kylezs <zsembery.kyle@gmail.com>
2024-06-20 17:01:06 +0200

    fix: remove prewitnessed deposits migration should migrate boost status

--- state-chain/pallets/cf-ingress-egress/src/migrations/remove_prewitnessed_deposits.rs
+++ state-chain/pallets/cf-ingress-egress/src/migrations/remove_prewitnessed_deposits.rs
@@ -3,7 +3,6 @@ use frame_support::traits::OnRuntimeUpgrade;
 
 mod old {
 	use super::*;
-
 	#[derive(Clone, Debug, PartialEq, Eq, Encode, Decode, TypeInfo)]
 	pub struct PrewitnessedDeposit<C: Chain> {
 		pub asset: C::ChainAsset,
@@ -22,6 +21,33 @@ mod old {
 		PrewitnessedDepositId,
 		PrewitnessedDeposit<<T as Config<I>>::TargetChain>,
 	>;
+
+	#[derive(CloneNoBound, PartialEq, Eq, Encode, Decode, TypeInfo)]
+	pub enum BoostStatus {
+		Boosted { prewitnessed_deposit_id: PrewitnessedDepositId, pools: Vec<BoostPoolTier> },
+		NotBoosted,
+	}
+
+	#[derive(CloneNoBound, PartialEq, Eq, Encode, Decode, TypeInfo)]
+	#[scale_info(skip_type_params(T, I))]
+	pub struct DepositChannelDetails<T: Config<I>, I: 'static> {
+		pub deposit_channel: DepositChannel<T::TargetChain>,
+		pub opened_at: TargetChainBlockNumber<T, I>,
+		pub expires_at: TargetChainBlockNumber<T, I>,
+		pub action: ChannelAction<T::AccountId>,
+		pub boost_fee: BasisPoints,
+		// Using the old BoostStatus here
+		pub boost_status: BoostStatus,
+	}
+
+	#[frame_support::storage_alias]
+	pub type DepositChannelLookup<T: Config<I>, I: 'static> = StorageMap<
+		Pallet<T, I>,
+		Twox64Concat,
+		TargetChainAccount<T, I>,
+		DepositChannelDetails<T, I>,
+		OptionQuery,
+	>;
 }
 
 pub struct Migration<T: Config<I>, I: 'static>(PhantomData<(T, I)>);
@@ -29,6 +55,34 @@ pub struct Migration<T: Config<I>, I: 'static>(PhantomData<(T, I)>);
 impl<T: Config<I>, I: 'static> OnRuntimeUpgrade for Migration<T, I> {
 	fn on_runtime_upgrade() -> Weight {
 		let _ = old::PrewitnessedDeposits::<T, I>::clear(u32::MAX, None);
+
+		// convert to new BoostStatus
+		DepositChannelLookup::<T, I>::translate_values::<old::DepositChannelDetails<T, I>, _>(
+			|old| {
+				let boost_status =
+					if let old::BoostStatus::Boosted { prewitnessed_deposit_id, pools } =
+						old.boost_status
+					{
+						BoostStatus::Boosted {
+							prewitnessed_deposit_id,
+							pools,
+							// we don't try and backfill these old values
+							amount: Zero::zero(),
+						}
+					} else {
+						BoostStatus::NotBoosted
+					};
+
+				Some(DepositChannelDetails::<T, I> {
+					deposit_channel: old.deposit_channel,
+					opened_at: old.opened_at,
+					expires_at: old.expires_at,
+					action: old.action,
+					boost_fee: old.boost_fee,
+					boost_status,
+				})
+			},
+		);
 
 		Weight::zero()
 	}
@@ -46,7 +100,8 @@ impl<T: Config<I>, I: 'static> OnRuntimeUpgrade for Migration<T, I> {
 
 #[cfg(test)]
 mod migration_tests {
-	use cf_chains::btc::UtxoId;
+	use super::*;
+	use cf_chains::btc::{deposit_address::DepositAddress, UtxoId};
 	use sp_core::H256;
 
 	#[test]
@@ -71,11 +126,55 @@ mod migration_tests {
 				},
 			);
 
+			let btc_deposit_script: ScriptPubkey = DepositAddress::new([0; 32], 9).script_pubkey();
+
+			let deposit_channel = DepositChannel::<Bitcoin> {
+				channel_id: Default::default(),
+				address: btc_deposit_script.clone(),
+				asset: cf_chains::assets::btc::Asset::Btc,
+				state: DepositAddress::new([0; 32], 1),
+			};
+
+			old::DepositChannelLookup::<Test, _>::insert(
+				btc_deposit_script.clone(),
+				old::DepositChannelDetails {
+					deposit_channel: deposit_channel.clone(),
+					opened_at: 420,
+					expires_at: 6969,
+					action: ChannelAction::<u64>::LiquidityProvision { lp_account: 22 },
+					boost_fee: 8,
+					boost_status: old::BoostStatus::Boosted {
+						prewitnessed_deposit_id: 2,
+						pools: vec![5, 10],
+					},
+				},
+			);
+
+			assert_eq!(DepositChannelLookup::<Test, _>::iter_keys().count(), 1);
+			assert_eq!(old::DepositChannelLookup::<Test, _>::iter_keys().count(), 1);
+
 			// Perform runtime migration.
 			super::Migration::<Test, _>::on_runtime_upgrade();
 
 			// Test that we delete all entries as part of the migration:
 			assert_eq!(old::PrewitnessedDeposits::<Test, _>::iter_keys().count(), 0);
+
+			assert_eq!(DepositChannelLookup::<Test, _>::iter_keys().count(), 1);
+			assert_eq!(
+				DepositChannelLookup::<Test, _>::get(btc_deposit_script),
+				Some(DepositChannelDetails {
+					deposit_channel: deposit_channel.clone(),
+					opened_at: 420,
+					expires_at: 6969,
+					action: ChannelAction::<u64>::LiquidityProvision { lp_account: 22 },
+					boost_fee: 8,
+					boost_status: BoostStatus::Boosted {
+						prewitnessed_deposit_id: 2,
+						pools: vec![5, 10],
+						amount: 0,
+					},
+				})
+			);
 		});
 	}
 }
