1bfc546ea46a02721bba577ffb34f90ea95fe5e3
Daniel <daniel@chainflip.io>
2024-07-02 17:40:59 +0200

    fix: apply broadcast re-signing for polkadot on all networks

--- state-chain/runtime/src/migrations/housekeeping.rs
+++ state-chain/runtime/src/migrations/housekeeping.rs
@@ -6,35 +6,36 @@ pub struct Migration;
 impl OnRuntimeUpgrade for Migration {
 	fn on_runtime_upgrade() -> Weight {
 		use cf_runtime_upgrade_utilities::genesis_hashes;
+		if crate::VERSION.spec_version == 145 {
+			// Re-sign polkadot broadcasts.
+			// Both pending and aborted broadcasts are re-signed because both would have
+			// been invalid.
+			for broadcast_id in [
+				pallet_cf_broadcast::AbortedBroadcasts::<Runtime, PolkadotInstance>::take(),
+				pallet_cf_broadcast::PendingBroadcasts::<Runtime, PolkadotInstance>::take()
+					.into_iter()
+					.collect(),
+			]
+			.concat()
+			{
+				if let Some((api_call, ..)) = pallet_cf_broadcast::ThresholdSignatureData::<
+					Runtime,
+					PolkadotInstance,
+				>::get(broadcast_id)
+				{
+					log::info!("✍️ Re-signing broadcast: {}", broadcast_id);
+					pallet_cf_broadcast::Pallet::<Runtime, PolkadotInstance>::clean_up_broadcast_storage(broadcast_id);
+					pallet_cf_broadcast::Pallet::<Runtime, PolkadotInstance>::threshold_sign(
+						api_call,
+						broadcast_id,
+						true,
+					);
+				}
+			}
+		}
 		match genesis_hashes::genesis_hash::<Runtime>() {
 			genesis_hashes::BERGHAIN => {
-				log::info!("🧹 Applying housekeeping chores for Berghain.");
-				if crate::VERSION.spec_version == 145 {
-					// Re-sign polkadot broadcasts.
-					// Both pending and aborted broadcasts are re-signed because both would have
-					// been invalid.
-					for broadcast_id in [
-						pallet_cf_broadcast::AbortedBroadcasts::<Runtime, PolkadotInstance>::take(),
-						pallet_cf_broadcast::PendingBroadcasts::<Runtime, PolkadotInstance>::take()
-							.into_iter()
-							.collect(),
-					]
-					.concat()
-					{
-						if let Some((api_call, ..)) = pallet_cf_broadcast::ThresholdSignatureData::<
-							Runtime,
-							PolkadotInstance,
-						>::get(broadcast_id)
-						{
-							pallet_cf_broadcast::Pallet::<Runtime, PolkadotInstance>::clean_up_broadcast_storage(broadcast_id);
-							pallet_cf_broadcast::Pallet::<Runtime, PolkadotInstance>::threshold_sign(
-								api_call,
-								broadcast_id,
-								true,
-							);
-						}
-					}
-				}
+				log::info!("🧹 No housekeeping required for Berghain.");
 			},
 			genesis_hashes::PERSEVERANCE => {
 				log::info!("🧹 No housekeeping required for Perseverance.");
