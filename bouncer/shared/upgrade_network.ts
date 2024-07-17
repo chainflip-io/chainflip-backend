08f04475047bbfb81923d04dd2dc5d2976d8d621
kylezs <zsembery.kyle@gmail.com>
2024-07-17 15:13:38 +0200

    tmp: version correct

--- bouncer/shared/upgrade_network.ts
+++ bouncer/shared/upgrade_network.ts
@@ -162,7 +162,7 @@ async function incompatibleUpgradeNoBuild(
 
   console.log('Engines started');
 
-  await submitRuntimeUpgradeWithRestrictions(runtimePath, undefined, undefined, true);
+  await submitRuntimeUpgradeWithRestrictions(runtimePath, undefined, undefined, false);
 
   console.log(
     'Check that the old engine has now shut down, and that the new engine is now running.',
