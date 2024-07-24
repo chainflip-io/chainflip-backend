543050cb18a3b71b3ae60a6ff176d30582b6d6e0
Assem Hasna <asem.hasna@gmail.com>
2024-07-24 17:11:03 +0200

    fix: set node count correctly

--- localnet/manage.sh
+++ localnet/manage.sh
@@ -20,7 +20,7 @@ CORE_CONTAINERS="bitcoin geth polkadot redis"
 ARB_CONTAINERS="sequencer staker-unsafe poster"
 SOLANA_BASE_PATH="/tmp/solana"
 CHAINFLIP_BASE_PATH="/tmp/chainflip"
-export NODE_COUNT="3-node"
+export NODE_COUNT="3"
 
 DEBUG_OUTPUT_DESTINATION=${DEBUG_OUTPUT_DESTINATION:-"$CHAINFLIP_BASE_PATH/debug.log"}
 
