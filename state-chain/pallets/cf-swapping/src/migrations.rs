7601bf4cfdc130f902990bae4b698ec6d48e6836
kylezs <zsembery.kyle@gmail.com>
2024-07-26 13:09:12 +0200

    fix: bad merge

--- state-chain/pallets/cf-swapping/src/migrations.rs
+++ state-chain/pallets/cf-swapping/src/migrations.rs
@@ -1,9 +1,8 @@
 use crate::Pallet;
 use cf_runtime_upgrade_utilities::{PlaceholderMigration, VersionedMigration};
-mod separate_swap_state;
 mod swapping_redesign;
 
 pub type PalletMigration<T> = (
 	VersionedMigration<Pallet<T>, swapping_redesign::Migration<T>, 5, 6>,
-    PlaceholderMigration<Pallet<T>, 5>
+	PlaceholderMigration<Pallet<T>, 6>,
 );
