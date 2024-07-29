909bb6bcb66f6eb585d24c0e1f73a1a42a1fde4a
Ramiz Siddiqui <ramiz@chainflip.io>
2024-07-29 15:10:47 +0200

    feat: upgrade test for threshold sig pallet

--- state-chain/pallets/cf-threshold-signature/src/migrations/migrate_signature_to_include_signer.rs
+++ state-chain/pallets/cf-threshold-signature/src/migrations/migrate_signature_to_include_signer.rs
@@ -4,7 +4,7 @@ use frame_support::traits::OnRuntimeUpgrade;
 #[cfg(feature = "try-runtime")]
 use frame_support::sp_runtime::DispatchError;
 #[cfg(feature = "try-runtime")]
-use sp_std::{vec, vec::Vec};
+use sp_std::vec::Vec;
 
 pub struct Migration<T, I>(sp_std::marker::PhantomData<(T, I)>);
 
@@ -39,11 +39,28 @@ impl<T: crate::Config<I>, I: 'static> OnRuntimeUpgrade for Migration<T, I> {
 
 	#[cfg(feature = "try-runtime")]
 	fn pre_upgrade() -> Result<Vec<u8>, DispatchError> {
-		Ok(vec![])
+		Ok(old::Signature::<T, I>::iter().collect::<Vec<_>>().encode())
 	}
 
 	#[cfg(feature = "try-runtime")]
-	fn post_upgrade(_state: Vec<u8>) -> Result<(), DispatchError> {
+	fn post_upgrade(state: Vec<u8>) -> Result<(), DispatchError> {
+		let signatures =
+			Vec::<(u32, AsyncResult<SignatureResultFor<T, I>>)>::decode(&mut &state[..])
+				.map_err(|_| DispatchError::Other("Failed to decode Signatures"))?;
+
+		let current_aggkey = Keys::<T, I>::get(CurrentKeyEpoch::<T, I>::get().unwrap()).unwrap();
+
+		assert_eq!(
+			signatures.len(),
+			SignerAndSignature::<T, I>::iter_keys().collect::<Vec<_>>().len()
+		);
+
+		for (request_id, signature) in signatures {
+			let SignerAndSignatureResult { signer, signature_result } =
+				SignerAndSignature::<T, I>::get(request_id).unwrap();
+			assert_eq!(signer, current_aggkey);
+			assert_eq!(signature, signature_result);
+		}
 		Ok(())
 	}
 }
