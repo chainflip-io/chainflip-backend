41711258eb5025d1fec6951c41e2005aecb8ae2f
albert <allimos3@gmail.com>
2024-08-02 07:16:07 +0200

    chore: install node dependencies

--- localnet/init/scripts/start-solana.sh
+++ localnet/init/scripts/start-solana.sh
@@ -4,5 +4,4 @@ set -e
 DATETIME=$(date '+%Y-%m-%d_%H-%M-%S')
 export RUST_LOG=solana_runtime::system_instruction_processor=trace,solana_runtime::message_processor=info,solana_bpf_loader=debug,solana_rbpf=debug
 export RUST_BACKTRACE=full
-which solana-test-validator
 solana-test-validator --ledger /tmp/solana/test-ledger > /tmp/solana/solana.$DATETIME.log 2>&1 &
