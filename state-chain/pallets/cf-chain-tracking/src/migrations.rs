843475096ff9d4b8b4533248a8f5b70e79d664fe
Ramiz Siddiqui <ramiz@chainflip.io>
2024-08-07 15:50:32 +0200

    chore: bump placeholder migration

--- state-chain/pallets/cf-chain-tracking/src/migrations.rs
+++ state-chain/pallets/cf-chain-tracking/src/migrations.rs
@@ -1,4 +1,4 @@
 use crate::Pallet;
 use cf_runtime_upgrade_utilities::PlaceholderMigration;
 
-pub type PalletMigration<T, I> = PlaceholderMigration<Pallet<T, I>, 3>;
+pub type PalletMigration<T, I> = PlaceholderMigration<Pallet<T, I>, 4>;
