06f4895bd835c2215fa40abbccc858e6f9713612
Daniel <daniel@chainflip.io>
2024-08-09 17:02:06 +0200

    chore rename and deduplicate

--- state-chain/traits/src/mocks/qualify_node.rs
+++ state-chain/traits/src/mocks/qualify_node.rs
@@ -17,7 +17,7 @@ impl<Id: Encode + Decode> QualifyAll<Id> {
 	}
 }
 
-impl<Id: Ord + Encode + Decode> QualifyNode<Id> for QualifyAll<Id> {
+impl<Id: Ord + Clone + Encode + Decode> QualifyNode<Id> for QualifyAll<Id> {
 	fn is_qualified(id: &Id) -> bool {
 		!Self::get_storage::<_, Vec<Id>>(b"EXCEPT", b"").unwrap_or_default().contains(id)
 	}
