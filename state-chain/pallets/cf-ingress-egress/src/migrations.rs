52155ff74f7a96975150fb7540b4f3d65559f0fe
kylezs <kyle@chainflip.io>
2024-08-12 16:05:01 +0200

    fix: pallet versions should match release + bump old dylib version to 1.5.1 (#5142)
    * fix: pallet versions should match release
    
    * chore: bump old dylib version
    
    * fix: ccm transfers should charge broker fee
    
    ---------
    
    Co-authored-by: Daniel <daniel@chainflip.io>

--- state-chain/pallets/cf-ingress-egress/src/migrations.rs
+++ state-chain/pallets/cf-ingress-egress/src/migrations.rs
@@ -8,5 +8,6 @@ pub type PalletMigration<T, I> = (
 	VersionedMigration<Pallet<T, I>, remove_prewitnessed_deposits::Migration<T, I>, 8, 9>,
 	VersionedMigration<Pallet<T, I>, add_refund_params::Migration<T, I>, 9, 10>,
 	VersionedMigration<Pallet<T, I>, withheld_transaction_fees::Migration<T, I>, 10, 11>,
-	PlaceholderMigration<Pallet<T, I>, 11>,
+	VersionedMigration<Pallet<T, I>, (), 11, 12>,
+	PlaceholderMigration<Pallet<T, I>, 12>,
 );
