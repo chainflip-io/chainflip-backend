830e9fc2da20259b282786c92a1a4eb0370236ad
Alastair Holmes <holmes.alastair@outlook.com>
2024-08-13 14:07:39 +0200

    chore: naming consistency

--- state-chain/pallets/cf-elections/src/vote_storage/individual/composite.rs
+++ state-chain/pallets/cf-elections/src/vote_storage/individual/composite.rs
@@ -1,4 +1,4 @@
-macro_rules! generate_simple_vote_storage_tuple_impls {
+macro_rules! generate_individual_vote_storage_tuple_impls {
     ($module:ident: ($($t:ident),*$(,)?)) => {
         mod $module {
             #[allow(unused_imports)]
@@ -10,7 +10,7 @@ macro_rules! generate_simple_vote_storage_tuple_impls {
                 SharedDataHash,
             };
 
-            use super::super::{private, SimpleVoteStorage};
+            use super::super::{private, IndividualVoteStorage};
 
             use codec::{Encode, Decode};
             use scale_info::TypeInfo;
@@ -22,15 +22,15 @@ macro_rules! generate_simple_vote_storage_tuple_impls {
 
             #[allow(non_snake_case)]
             #[allow(unused_variables)]
-            impl<$($t: SimpleVoteStorage),*> SimpleVoteStorage for ($($t,)*) {
-                type Vote = ($(<$t as SimpleVoteStorage>::Vote,)*);
-                type PartialVote = ($(<$t as SimpleVoteStorage>::PartialVote,)*);
-                type SharedData = SharedDataEnum<$(<$t as SimpleVoteStorage>::SharedData,)*>;
+            impl<$($t: IndividualVoteStorage),*> IndividualVoteStorage for ($($t,)*) {
+                type Vote = ($(<$t as IndividualVoteStorage>::Vote,)*);
+                type PartialVote = ($(<$t as IndividualVoteStorage>::PartialVote,)*);
+                type SharedData = SharedDataEnum<$(<$t as IndividualVoteStorage>::SharedData,)*>;
 
                 #[allow(clippy::unused_unit)]
                 fn vote_into_partial_vote<H: Fn(Self::SharedData) -> SharedDataHash>(($($t,)*): &Self::Vote, h: H) -> Self::PartialVote {
                     ($(
-                        <$t as SimpleVoteStorage>::vote_into_partial_vote($t, |shared_data| {
+                        <$t as IndividualVoteStorage>::vote_into_partial_vote($t, |shared_data| {
                             (&h)(SharedDataEnum::$t(shared_data))
                         })
                     ,)*)
@@ -39,7 +39,7 @@ macro_rules! generate_simple_vote_storage_tuple_impls {
                 #[allow(unused_mut)]
                 fn partial_vote_into_vote<GetSharedData: FnMut(SharedDataHash) -> Result<Option<Self::SharedData>, CorruptStorageError>>(($($t,)*): &Self::PartialVote, mut get_shared_data: GetSharedData) -> Result<Option<Self::Vote>, CorruptStorageError> {
                     Ok(Some(($(
-                        if let Some(vote) = <$t as SimpleVoteStorage>::partial_vote_into_vote(
+                        if let Some(vote) = <$t as IndividualVoteStorage>::partial_vote_into_vote(
                             $t,
                             |shared_data_hash| {
                                 Ok(match get_shared_data(shared_data_hash)? {
@@ -57,7 +57,7 @@ macro_rules! generate_simple_vote_storage_tuple_impls {
 
                 fn visit_shared_data_in_vote<E, F: Fn(Self::SharedData) -> Result<(), E>>(($($t,)*): Self::Vote, f: F) -> Result<(), E> {
                     $(
-                        <$t as SimpleVoteStorage>::visit_shared_data_in_vote($t, |shared_data| {
+                        <$t as IndividualVoteStorage>::visit_shared_data_in_vote($t, |shared_data| {
                             f(SharedDataEnum::$t(shared_data))
                         })?;
                     )*
@@ -65,17 +65,17 @@ macro_rules! generate_simple_vote_storage_tuple_impls {
                 }
                 fn visit_shared_data_references_in_partial_vote<F: Fn(SharedDataHash)>(($($t,)*): &Self::PartialVote, f: F) {
                     $(
-                        <$t as SimpleVoteStorage>::visit_shared_data_references_in_partial_vote($t, &f);
+                        <$t as IndividualVoteStorage>::visit_shared_data_references_in_partial_vote($t, &f);
                     )*
                 }
             }
-            impl<$($t: SimpleVoteStorage),*> private::Sealed for ($($t,)*) {}
+            impl<$($t: IndividualVoteStorage),*> private::Sealed for ($($t,)*) {}
         }
     }
 }
 
-generate_simple_vote_storage_tuple_impls!(tuple_0_impls: ());
-generate_simple_vote_storage_tuple_impls!(tuple_1_impls: (A));
-generate_simple_vote_storage_tuple_impls!(tuple_2_impls: (A, B));
-generate_simple_vote_storage_tuple_impls!(tuple_3_impls: (A, B, C));
-generate_simple_vote_storage_tuple_impls!(tuple_4_impls: (A, B, C, D));
+generate_individual_vote_storage_tuple_impls!(tuple_0_impls: ());
+generate_individual_vote_storage_tuple_impls!(tuple_1_impls: (A));
+generate_individual_vote_storage_tuple_impls!(tuple_2_impls: (A, B));
+generate_individual_vote_storage_tuple_impls!(tuple_3_impls: (A, B, C));
+generate_individual_vote_storage_tuple_impls!(tuple_4_impls: (A, B, C, D));
