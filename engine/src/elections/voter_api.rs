92cc535450774b16223c4bce99493e68cd76e886
Alastair Holmes <holmes.alastair@outlook.com>
2024-08-19 15:08:00 +0200

    Use async_trait macro in VoterApi

--- engine/src/elections/voter_api.rs
+++ engine/src/elections/voter_api.rs
@@ -4,15 +4,13 @@ use pallet_cf_elections::{
 	vote_storage::{self, VoteStorage},
 };
 
+#[async_trait::async_trait]
 pub trait VoterApi<E: ElectoralSystem> {
-	fn vote<'a>(
-		&'a self,
+	async fn vote(
+		&self,
 		settings: <E as ElectoralSystem>::ElectoralSettings,
 		properties: <E as ElectoralSystem>::ElectionProperties,
-	) -> impl std::future::Future<
-		Output = Result<<<E as ElectoralSystem>::Vote as VoteStorage>::Vote, anyhow::Error>,
-	> + Send
-	       + 'a;
+	) -> Result<<<E as ElectoralSystem>::Vote as VoteStorage>::Vote, anyhow::Error>;
 }
 
 pub struct CompositeVoter<ElectoralSystem, Voters> {
@@ -34,34 +32,30 @@ impl<ElectoralSystem, Voters> CompositeVoter<ElectoralSystem, Voters> {
 macro_rules! generate_voter_api_tuple_impls {
     ($module:ident: ($(($electoral_system:ident, $voter:ident)),*$(,)?)) => {
         #[allow(non_snake_case)]
+        #[async_trait::async_trait]
         impl<$($voter: VoterApi<$electoral_system> + Send + Sync),*, $($electoral_system : ElectoralSystem + Send + Sync + 'static),*, Hooks: Send + Sync + 'static + composite::$module::Hooks<$($electoral_system,)*>> VoterApi<Composite<($($electoral_system,)*), Hooks>> for CompositeVoter<Composite<($($electoral_system,)*), Hooks>, ($($voter,)*)> {
-            fn vote<'a>(
-                &'a self,
+            async fn vote(
+                &self,
                 settings: <Composite<($($electoral_system,)*), Hooks> as ElectoralSystem>::ElectoralSettings,
                 properties: <Composite<($($electoral_system,)*), Hooks> as ElectoralSystem>::ElectionProperties,
-            ) -> impl std::future::Future<
-                Output = Result<
-                    <<Composite<($($electoral_system,)*), Hooks> as ElectoralSystem>::Vote as VoteStorage>::Vote,
-                    anyhow::Error,
-                >,
-            > + Send
-                   + 'a {
-                async {
-                    use vote_storage::composite::$module::CompositeVoteStorageEnum;
-                    use composite::$module::CompositeElectoralSystemEnum;
+            ) -> Result<
+                <<Composite<($($electoral_system,)*), Hooks> as ElectoralSystem>::Vote as VoteStorage>::Vote,
+                anyhow::Error,
+            > {
+                use vote_storage::composite::$module::CompositeVoteStorageEnum;
+                use composite::$module::CompositeElectoralSystemEnum;
 
-                    let ($($voter,)*) = &self.voters;
-                    let ($($electoral_system,)*) = settings;
-                    match properties {
-                        $(
-                            CompositeElectoralSystemEnum::$electoral_system(properties) => {
-                                $voter.vote(
-                                    $electoral_system,
-                                    properties,
-                                ).await.map(CompositeVoteStorageEnum::$electoral_system)
-                            },
-                        )*
-                    }
+                let ($($voter,)*) = &self.voters;
+                let ($($electoral_system,)*) = settings;
+                match properties {
+                    $(
+                        CompositeElectoralSystemEnum::$electoral_system(properties) => {
+                            $voter.vote(
+                                $electoral_system,
+                                properties,
+                            ).await.map(CompositeVoteStorageEnum::$electoral_system)
+                        },
+                    )*
                 }
             }
         }
