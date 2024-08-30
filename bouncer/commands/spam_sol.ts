220dd800955e3ce3781472aa807922a287968cf9
albert <allimos3@gmail.com>
2024-08-30 09:03:07 +0200

    chore: add solusdc support

--- bouncer/commands/spam_sol.ts
+++ bouncer/commands/spam_sol.ts
@@ -1,36 +1,56 @@
 #!/usr/bin/env -S pnpm tsx
 // INSTRUCTIONS
 //
-// This command takes three arguments.
-// It will fund the solana address provided as the first argument with the amount
-// provided in the second argument. The third is the number of successive deposits.
-// The asset amount is interpreted in Sol
+// It will repeatedly send Sol or SolUsdc to the provided address concurrently and as
+// fast as possible, with many transactions potentially being included in the same slot.
+// <asset> <destAsset> <amount> <numberOfTransfers>
+// The asset amount is interpreted in Sol/SolUsdc.
 //
-// For example: ./commands/spam_sol.ts 7QQGNm3ptwinipDCyaCF7jY5katgmFUu1ieP2f7nwLpE 0.01 100
+// For example: ./commands/spam_sol.ts Sol 7QQGNm3ptwinipDCyaCF7jY5katgmFUu1ieP2f7nwLpE 0.01 100
 // will send 0.01 * 100 Sol to account 7QQGNm3ptwinipDCyaCF7jY5katgmFUu1ieP2f7nwLpE
 // It also accepts non-encoded bs58 address representations:
-// ./commands/spam_sol.ts 0x2f3fcadf740018f6037513959bab60d0dbef26888d264d54fc4d3d36c8cf5c91 0.01 100
+// ./commands/spam_sol.ts Sol 0x2f3fcadf740018f6037513959bab60d0dbef26888d264d54fc4d3d36c8cf5c91 0.01 100
 
 import BigNumber from 'bignumber.js';
 import { sendSol } from '../shared/send_sol';
-import { executeWithTimeout } from '../shared/utils';
+import { assetDecimals, executeWithTimeout } from '../shared/utils';
+import { sendSolUsdc } from '../shared/send_solusdc';
 
 async function main() {
-  const solanaAddress = process.argv[2];
-  let solAmount = new BigNumber(process.argv[3].trim());
-  const numberOfDeposits = Number(process.argv[4].trim());
+  const asset = process.argv[2].trim() as 'Sol' | 'SolUsdc';
+  const solanaAddress = process.argv[3];
+  let solAmount = new BigNumber(process.argv[4].trim());
+  const numberOfDeposits = Number(process.argv[5].trim());
 
   console.log(
-    'Transferring ' + solAmount + ' Sol to ' + solanaAddress + ' ' + numberOfDeposits + ' times',
+    'Transferring ' +
+      solAmount +
+      ' ' +
+      asset +
+      ' to ' +
+      solanaAddress +
+      ' ' +
+      numberOfDeposits +
+      ' times',
   );
 
   const txPromises = [];
+  const decimals = assetDecimals(asset);
 
   for (let i = 0; i < numberOfDeposits; i++) {
     // Add a minimal amount so they are not the same transaction and end up silently failing
     // in the background given that the underneath client might use the same PoH hash.
-    solAmount = solAmount.plus(new BigNumber('0.000000001'));
-    txPromises.push(sendSol(solanaAddress, solAmount.toString(), false));
+    solAmount = solAmount.plus(new BigNumber(1).div(10 ** decimals));
+    switch (asset) {
+      case 'Sol':
+        txPromises.push(sendSol(solanaAddress, solAmount.toString(), false));
+        break;
+      case 'SolUsdc':
+        txPromises.push(sendSolUsdc(solanaAddress, solAmount.toString(), false));
+        break;
+      default:
+        throw new Error('Unsupported asset');
+    }
   }
 
   const txs = await Promise.all(txPromises);
