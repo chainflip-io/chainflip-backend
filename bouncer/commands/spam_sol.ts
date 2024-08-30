dd582fd011550fcf3a318735a13e3d16e79263e7
albert <allimos3@gmail.com>
2024-08-30 10:04:16 +0200

    chore: restore run.sh

--- bouncer/commands/spam_sol.ts
+++ bouncer/commands/spam_sol.ts
@@ -38,7 +38,7 @@ async function main() {
   const decimals = assetDecimals(asset);
 
   for (let i = 0; i < numberOfDeposits; i++) {
-    // Add a minimal amount so they are not the same transaction and end up silently failing
+    // Add a minimal amount so it doesn't result in the same transaction silently failing
     // in the background given that the underneath client might use the same PoH hash.
     solAmount = solAmount.plus(new BigNumber(1).div(10 ** decimals));
     switch (asset) {
--- bouncer/run.sh
+++ bouncer/run.sh
@@ -1,6 +1,4 @@
 set -e
 ./commands/observe_block.ts 5
 ./setup_for_test.sh
-# ./tests/all_concurrent_tests.ts $1
-./commands/spam_sol.ts Sol 7QQGNm3ptwinipDCyaCF7jY5katgmFUu1ieP2f7nwLpE 0.01 20
-./commands/spam_sol.ts SolUsdc 7QQGNm3ptwinipDCyaCF7jY5katgmFUu1ieP2f7nwLpE 0.01 20
+./tests/all_concurrent_tests.ts $1
