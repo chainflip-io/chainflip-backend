5a6fea8c199fc70e3231229536a29c40b7d52964
Daniel <daniel@chainflip.io>
2024-08-30 14:13:11 +0200

    chore: version bumps to 1.6.2

--- engine-upgrade-utils/src/lib.rs
+++ engine-upgrade-utils/src/lib.rs
@@ -11,7 +11,7 @@ pub mod build_helpers;
 // relevant crates.
 // Should also check that the compatibility function below `args_compatible_with_old` is correct.
 pub const OLD_VERSION: &str = "1.5.1";
-pub const NEW_VERSION: &str = "1.6.1";
+pub const NEW_VERSION: &str = "1.6.2";
 
 pub const ENGINE_LIB_PREFIX: &str = "chainflip_engine_v";
 pub const ENGINE_ENTRYPOINT_PREFIX: &str = "cfe_entrypoint_v";
