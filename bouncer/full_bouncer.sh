2feb7a59469e60ae1200c765d7cd996945a26297
Jamie Ford <Jamie@chainflip.io>
2024-09-18 15:31:33 +1000

    fix: missed renaming run_test in full bouncer script

--- bouncer/full_bouncer.sh
+++ bouncer/full_bouncer.sh
@@ -1,16 +1,16 @@
 set -e
 echo "Running full bouncer 🧪"
 ./setup_for_test.sh
-./commands/execute_test.ts "Gas-Limit-Ccm-Swaps"
+./commands/run_test.ts "Gas-Limit-Ccm-Swaps"
 ./tests/all_concurrent_tests.ts $1
-./commands/execute_test.ts "Rotates-Through-BTC-Swap"
-./commands/execute_test.ts "BTC-UTXO-Consolidation"
-./commands/execute_test.ts "Rotation-Barrier"
-./commands/execute_test.ts "Minimum-Deposit"
+./commands/run_test.ts "Rotates-Through-BTC-Swap"
+./commands/run_test.ts "BTC-UTXO-Consolidation"
+./commands/run_test.ts "Rotation-Barrier"
+./commands/run_test.ts "Minimum-Deposit"
 
 if [[ $LOCALNET == false ]]; then
   echo "🤫 Skipping tests that require localnet"
 else
   echo "🚀 Running tests that require localnet"
-  ./commands/execute_test.ts "Swap-After-Disconnection"
+  ./commands/run_test.ts "Swap-After-Disconnection"
 fi
