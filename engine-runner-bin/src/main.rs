89b112c45f381b2ba1294dec67ed52f979dff7f0
Daniel <daniel@chainflip.io>
2024-10-02 14:09:51 +0200

    chore: version bumps and changelog 1.6.8

--- engine-runner-bin/src/main.rs
+++ engine-runner-bin/src/main.rs
@@ -12,7 +12,7 @@ mod old {
 }
 
 mod new {
-	#[engine_proc_macros::link_engine_library_version("1.6.7")]
+	#[engine_proc_macros::link_engine_library_version("1.6.8")]
 	extern "C" {
 		fn cfe_entrypoint(
 			c_args: engine_upgrade_utils::CStrArray,
