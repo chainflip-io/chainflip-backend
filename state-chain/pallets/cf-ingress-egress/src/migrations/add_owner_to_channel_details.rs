a7ad89e97bbf04f41f9c6cd4760261b6a8ec115f
Jan Börner <jan@chainflip.io>
2024-10-14 15:20:53 +0200

    test: Added migration test 🧪

--- state-chain/pallets/cf-ingress-egress/src/migrations/add_owner_to_channel_details.rs
+++ state-chain/pallets/cf-ingress-egress/src/migrations/add_owner_to_channel_details.rs
@@ -61,3 +61,68 @@ impl<T: Config<I>, I: 'static> OnRuntimeUpgrade for Migration<T, I> {
 		Ok(())
 	}
 }
+
+#[cfg(test)]
+mod migration_tests {
+	use super::*;
+	use crate::mock_eth::*;
+
+	#[test]
+	fn test_migration() {
+		use cf_chains::{evm::DeploymentStatus, ForeignChainAddress::Eth};
+		new_test_ext().execute_with(|| {
+			let channel_id = 1u64;
+			let address = sp_core::H160([1u8; 20]);
+			let asset = cf_chains::assets::eth::Asset::Eth;
+			let deployment_state = DeploymentStatus::Deployed;
+			let lp_account = 5u64;
+			let refund_address = Eth([1u8; 20].into());
+			let opened_at = 1u64;
+			let expires_at = 2u64;
+			let action = ChannelAction::LiquidityProvision { lp_account, refund_address };
+			let boost_fee = 1;
+			let boost_status = BoostStatus::NotBoosted;
+
+			old::DepositChannelLookup::<Test, _>::insert(
+				address,
+				old::OldDepositChannelDetails {
+					deposit_channel: DepositChannel {
+						asset,
+						channel_id,
+						address,
+						state: deployment_state,
+					},
+					opened_at,
+					expires_at,
+					action: action.clone(),
+					boost_fee,
+					boost_status,
+				},
+			);
+			assert_eq!(old::DepositChannelLookup::<Test, _>::iter().count(), 1);
+
+			#[cfg(feature = "try-runtime")]
+			let state = super::Migration::<Test, _>::pre_upgrade().unwrap();
+			super::Migration::<Test, _>::on_runtime_upgrade();
+
+			#[cfg(feature = "try-runtime")]
+			super::Migration::<Test, _>::post_upgrade(state).unwrap();
+
+			assert_eq!(DepositChannelLookup::<Test, _>::iter().count(), 1);
+
+			let migrated_deposit_channel = DepositChannelLookup::<Test, _>::get(address)
+				.expect("to have a channel in storage");
+
+			assert_eq!(migrated_deposit_channel.owner, 0);
+			assert_eq!(old::DepositChannelLookup::<Test, _>::iter().count(), 0);
+
+			assert_eq!(migrated_deposit_channel.deposit_channel.asset, asset);
+			assert_eq!(migrated_deposit_channel.deposit_channel.channel_id, channel_id);
+			assert_eq!(migrated_deposit_channel.deposit_channel.address, address);
+			assert_eq!(migrated_deposit_channel.deposit_channel.state, deployment_state);
+			assert_eq!(migrated_deposit_channel.opened_at, opened_at);
+			assert_eq!(migrated_deposit_channel.expires_at, expires_at);
+			assert_eq!(migrated_deposit_channel.action, action);
+		});
+	}
+}
