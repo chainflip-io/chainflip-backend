b75c29ca0d5d0764b7a4daa486dc0d7a6b9b1112
Jan Börner <jan@chainflip.io>
2024-10-14 16:14:45 +0200

    chore: changed pallet storage version 📀

--- state-chain/pallets/cf-ingress-egress/src/migrations.rs
+++ state-chain/pallets/cf-ingress-egress/src/migrations.rs
@@ -4,5 +4,5 @@ use cf_runtime_upgrade_utilities::{PlaceholderMigration, VersionedMigration};
 mod add_owner_to_channel_details;
 pub type PalletMigration<T, I> = (
 	VersionedMigration<Pallet<T, I>, add_owner_to_channel_details::Migration<T, I>, 15, 16>,
-	PlaceholderMigration<Pallet<T, I>, 17>,
+	PlaceholderMigration<Pallet<T, I>, 16>,
 );
