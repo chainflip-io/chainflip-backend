bb3a729e91000dae05bb8167b00706fd660222f5
dandanlen <3168260+dandanlen@users.noreply.github.com>
2024-10-24 18:09:56 +0200

    Update state-chain/pallets/cf-validator/src/migrations.rs

--- state-chain/pallets/cf-validator/src/migrations.rs
+++ state-chain/pallets/cf-validator/src/migrations.rs
@@ -6,6 +6,6 @@ mod rename_blocks_per_epoch;
 
 pub type PalletMigration<T> = (
 	VersionedMigration<Pallet<T>, delete_old_epoch_data::Migration<T>, 3, 4>,
-	VersionedMigration<Pallet<T>, rename_blocks_per_epoch::BlocksPerEpochMigration<T>, 3, 5>,
+	VersionedMigration<Pallet<T>, rename_blocks_per_epoch::BlocksPerEpochMigration<T>, 4, 5>,
 	PlaceholderMigration<Pallet<T>, 5>,
 );
