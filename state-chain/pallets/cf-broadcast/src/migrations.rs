f8d38212164a99b6a2930baabb7f41c882407810
Roy Yang <roy@chainflip.io>
2024-10-29 18:07:16 +1300

    Corrected placeholder migration version for the broadcaster pallet

--- state-chain/pallets/cf-broadcast/src/migrations.rs
+++ state-chain/pallets/cf-broadcast/src/migrations.rs
@@ -8,7 +8,7 @@ pub mod remove_aborted_broadcasts;
 pub type PalletMigration<T, I> = (
 	VersionedMigration<Pallet<T, I>, initialize_broadcast_timeout_storage::Migration<T, I>, 6, 7>,
 	VersionedMigration<Pallet<T, I>, migrate_timeouts::Migration<T, I>, 7, 8>,
-	PlaceholderMigration<Pallet<T, I>, 8>,
 	// Migration 8->9 is SerializeSolanaBroadcastMigration in runtime lib.
 	// Migration 9->10 is SolanaEgressSuccessWitnessMigration in runtime lib.
+	PlaceholderMigration<Pallet<T, I>, 10>,
 );
