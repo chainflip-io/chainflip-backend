46bcbd095d1a1df3f3add2caab0b1bc2929019e0
kylezs <kyle@chainflip.io>
2024-10-30 11:18:39 +0100

    fix: separate lp account for lp api (#5354)
    * fix: separate LP API test from other bouncer tests
    
    * chore: ensure migration tests run

--- localnet/init/scripts/start-lp-api.sh
+++ localnet/init/scripts/start-lp-api.sh
@@ -5,4 +5,4 @@ binary_location=$1
 RUST_LOG=debug,jsonrpsee_types::params=trace $binary_location/chainflip-lp-api \
   --port=10589 \
   --state_chain.ws_endpoint=ws://localhost:9944 \
-  --state_chain.signing_key_file $KEYS_DIR/LP_1 > /tmp/chainflip/chainflip-lp-api.$DATETIME.log 2>&1 &
+  --state_chain.signing_key_file $KEYS_DIR/LP_API > /tmp/chainflip/chainflip-lp-api.$DATETIME.log 2>&1 &
