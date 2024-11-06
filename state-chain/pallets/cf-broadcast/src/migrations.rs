1d032f98da779762569f72dd3b1ce13f5f86b898
kylezs <zsembery.kyle@gmail.com>
2024-11-06 14:54:34 +0100

    chore: remove delete old epoch data

--- state-chain/pallets/cf-broadcast/src/migrations.rs
+++ state-chain/pallets/cf-broadcast/src/migrations.rs
@@ -1,7 +1,4 @@
 use crate::Pallet;
 use cf_runtime_utilities::PlaceholderMigration;
 
-pub type PalletMigration<T, I> = (
-	// Migration 9->10 is SolanaEgressSuccessWitnessMigration in runtime lib.
-	PlaceholderMigration<10, Pallet<T, I>>,
-);
+pub type PalletMigration<T, I> = (PlaceholderMigration<10, Pallet<T, I>>,);
--- state-chain/pallets/cf-validator/src/migrations.rs
+++ state-chain/pallets/cf-validator/src/migrations.rs
@@ -1,16 +1,4 @@
 use crate::Pallet;
 use cf_runtime_utilities::PlaceholderMigration;
-use frame_support::migrations::VersionedMigration;
 
-mod delete_old_epoch_data;
-
-pub type PalletMigration<T> = (
-	VersionedMigration<
-		3,
-		4,
-		delete_old_epoch_data::Migration<T>,
-		Pallet<T>,
-		<T as frame_system::Config>::DbWeight,
-	>,
-	PlaceholderMigration<4, Pallet<T>>,
-);
+pub type PalletMigration<T> = (PlaceholderMigration<4, Pallet<T>>,);
