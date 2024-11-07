dd180b771d3a356745e37c352c789b79650844da
Daniel <daniel@chainflip.io>
2024-11-07 13:50:51 +0100

    chore: fmt

--- state-chain/custom-rpc/src/monitoring.rs
+++ state-chain/custom-rpc/src/monitoring.rs
@@ -5,11 +5,16 @@ use jsonrpsee::proc_macros::rpc;
 use sc_client_api::{BlockchainEvents, HeaderBackend};
 use sp_api::{ApiExt, Core};
 use sp_core::{bounded_vec::BoundedVec, ConstU32};
-use state_chain_runtime::{self, Block, chainflip::Offence, monitoring_apis::{
-	ActivateKeysBroadcastIds, AuthoritiesInfo, BtcUtxos, EpochState, ExternalChainsBlockHeight,
-	LastRuntimeUpgradeInfo, MonitoringRuntimeApi, OpenDepositChannels, PendingBroadcasts,
-	PendingTssCeremonies, RedemptionsInfo, SolanaNonces,
-}};
+use state_chain_runtime::{
+	self,
+	chainflip::Offence,
+	monitoring_apis::{
+		ActivateKeysBroadcastIds, AuthoritiesInfo, BtcUtxos, EpochState, ExternalChainsBlockHeight,
+		LastRuntimeUpgradeInfo, MonitoringRuntimeApi, OpenDepositChannels, PendingBroadcasts,
+		PendingTssCeremonies, RedemptionsInfo, SolanaNonces,
+	},
+	Block,
+};
 
 #[rpc(server, client, namespace = "cf_monitoring")]
 pub trait MonitoringApi {
@@ -125,7 +130,7 @@ where
 		at: Option<state_chain_runtime::Hash>,
 	) -> RpcResult<RpcMonitoringData> {
 		self.with_runtime_api(at, |api, hash| {
-			if api.api_version::< dyn Core<Block> >(hash).unwrap().unwrap() < 2 {
+			if api.api_version::<dyn Core<Block>>(hash).unwrap().unwrap() < 2 {
 				let old_result = api.cf_monitoring_data_before_version_2(hash)?;
 				Ok(old_result.into())
 			} else {
