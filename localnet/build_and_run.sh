ba87bd2255a7cb8051c7d387b2102873cfb8f890
Jamie Ford <Jamie@chainflip.io>
2024-11-08 15:59:43 +1100

    chore: build and run script for localnet

---
+++ localnet/build_and_run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Does a cargo build and then recreates the localnet using the new binaries.
+# Usage: ./localnet/build_and_run.sh
+
+cargo build && ./localnet/recreate.sh -d && cd bouncer && ./setup_for_test.sh
