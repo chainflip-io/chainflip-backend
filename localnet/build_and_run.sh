06e0a5817912005417732a2624bf9be95a46d40b
Jamie Ford <jamie@chainflip.io>
2024-11-11 10:09:53 +1100

    chore: brackets on cd
    Co-authored-by: kylezs <kyle@chainflip.io>

--- localnet/build_and_run.sh
+++ localnet/build_and_run.sh
@@ -2,4 +2,4 @@
 # Does a cargo build and then recreates the localnet using the new binaries.
 # Usage: ./localnet/build_and_run.sh
 
-cargo build && ./localnet/recreate.sh -d && cd bouncer && ./setup_for_test.sh
+cargo build && ./localnet/recreate.sh -d && (cd bouncer && ./setup_for_test.sh)
