1e7c7d887f1d700e87eb5aaac8f96ccd4160b9d7
Jan Börner <jan@chainflip.io>
2024-11-18 13:18:33 +0100

    fix: fixed clippy

--- state-chain/pallets/cf-swapping/src/mock.rs
+++ state-chain/pallets/cf-swapping/src/mock.rs
@@ -149,6 +149,10 @@ impl WeightInfo for MockWeightInfo {
 	fn register_affiliate() -> Weight {
 		Weight::from_parts(100, 0)
 	}
+
+	fn update_broker_bond() -> Weight {
+		Weight::from_parts(100, 0)
+	}
 }
 
 pub struct AlwaysValid;
