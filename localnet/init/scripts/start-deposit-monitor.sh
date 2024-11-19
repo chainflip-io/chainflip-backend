43d2ce20bf6381f4544c9e56b6d5cf0141ed784e
Maxim Urschumzew <MxmUrw@users.noreply.github.com>
2024-11-19 09:50:55 +0400

    Add bouncer tests involving the full deposit monitor pipeline. (#5388)
    * Add deposit monitor test.
    
    * Include deposit-monitor in the localnet docker-compose config.
    
    * Start deposit-monitor after broker API, add healthcheck.
    
    * Add new poll time configuration.
    
    * The broker API url depends on whether we run locally or in CI.
    
    * Use variable to denote env file location.
    
    * Use environment var instead of file for setting up deposit-monitor.
    
    * The deposit monitor tests now don't depend on being run locally,
    
    since we don't need to stop the bitcoin node anymore.
    
    * Fix prettier and eslint warnings.
    
    * Use manual extrinsic submission for "tx marked too late as tainted" test.
    
    * Run prettier.
    
    * Add new timeout configuration value.
    
    * Use new `all_processors` field for healthcheck.
    
    * Address PR comments:
    
     - Use axios for sending requests.
     - Use a simpler health check.
     - Smaller fixes.
    
    Additional changes:
     - Integrate new "/mockmode" feature of deposit monitor which controls the behaviour of the mock analysis provider.
    
    * Better naming & add env var required for newest CFDM.
    
    * Add type for parameter to `setMockmode`.
    
    * Pin image of deposit-monitor to current commit on main.
    
    We don't want to break the bouncer tests if we change the deposit-monitor
    configuration for example.
    
    * Start deposit-monitor during upgrade-test.
    
    This change is supported by moving the start code for
    the deposit-monitor to its own file.
    
    * Fixes required for upgrade-test to run successfully.
    
    * Use format string instead of string concatenation for messages.

---
+++ localnet/init/scripts/start-deposit-monitor.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+DATETIME=$(date '+%Y-%m-%d_%H-%M-%S')
+
+source $LOCALNET_INIT_DIR/../helper.sh
+
+# On some machines (e.g. MacOS), 172.17.0.1 is not accessible from inside the container, so we need to use host.docker.internal
+if [[ $CI == true ]]; then
+  export CFDM_BROKER_API_URL='ws://172.17.0.1:10997'
+else
+  export CFDM_BROKER_API_URL='ws://host.docker.internal:10997'
+fi
+$DOCKER_COMPOSE_CMD -f $LOCALNET_INIT_DIR/../docker-compose.yml -p "chainflip-localnet" up $DEPOSIT_MONITOR_CONTAINER $additional_docker_compose_up_args -d \
+  > /tmp/chainflip/chainflip-deposit-monitor.$DATETIME.log 2>&1
+
+while true; do
+  echo "🩺 Checking deposit-monitor's health ..."
+  REPLY=$(check_endpoint_health 'http://localhost:6060/health')
+  starting=$(echo $REPLY | jq .starting)
+  all_healthy=$(echo $REPLY | jq .all_processors)
+  if test "$starting" == "false" && test "$all_healthy" == "true" ; then
+    echo "💚 deposit-monitor is running!"
+    break
+  fi
+  sleep 1
+done
