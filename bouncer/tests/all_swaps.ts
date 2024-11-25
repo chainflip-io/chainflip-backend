54e908ca538f81a72e9663e4c7bff41796beb9d7
albert <allimos3@gmail.com>
2024-11-25 19:57:56 +0100

    chore: manually skip solana vault swaps

--- bouncer/tests/all_swaps.ts
+++ bouncer/tests/all_swaps.ts
@@ -6,7 +6,7 @@ import { btcAddressTypes } from '../shared/new_btc_address';
 import { ccmSupportedChains, chainFromAsset, VaultSwapParams } from '../shared/utils';
 
 /* eslint-disable @typescript-eslint/no-use-before-define */
-export const testAllSwaps = new ExecutableTest('All-Swaps', main, 1000);
+export const testAllSwaps = new ExecutableTest('All-Swaps', main, 3000);
 
 async function main() {
   const allSwaps: Promise<SwapParams | VaultSwapParams>[] = [];
@@ -41,42 +41,40 @@ async function main() {
     }
   }
 
-  // Object.values(Assets).forEach((sourceAsset) => {
-  //   Object.values(Assets)
-  //     .filter((destAsset) => sourceAsset !== destAsset)
-  //     .forEach((destAsset) => {
-  //       // Regular swaps
-  //       appendSwap(sourceAsset, destAsset, testSwap);
+  Object.values(Assets).forEach((sourceAsset) => {
+    Object.values(Assets)
+      .filter((destAsset) => sourceAsset !== destAsset)
+      .forEach((destAsset) => {
+        // Regular swaps
+        appendSwap(sourceAsset, destAsset, testSwap);
 
-  //       const sourceChain = chainFromAsset(sourceAsset);
-  //       const destChain = chainFromAsset(destAsset);
-  //       if (sourceChain === 'Ethereum' || sourceChain === 'Arbitrum') {
-  //         // Vault Swaps
-  //         appendSwap(sourceAsset, destAsset, testVaultSwap);
+        const sourceChain = chainFromAsset(sourceAsset);
+        const destChain = chainFromAsset(destAsset);
+        if (sourceChain === 'Ethereum' || sourceChain === 'Arbitrum') {
+          // Vault Swaps
+          appendSwap(sourceAsset, destAsset, testVaultSwap);
 
-  //         if (ccmSupportedChains.includes(destChain)) {
-  //           // CCM Vault swaps
-  //           appendSwap(sourceAsset, destAsset, testVaultSwap, true);
-  //         }
-  //       }
+          if (ccmSupportedChains.includes(destChain)) {
+            // CCM Vault swaps
+            appendSwap(sourceAsset, destAsset, testVaultSwap, true);
+          }
+        }
 
-  //       if (ccmSupportedChains.includes(destChain)) {
-  //         // CCM swaps
-  //         appendSwap(sourceAsset, destAsset, testSwap, true);
-  //       }
-  //     });
-  // });
+        if (ccmSupportedChains.includes(destChain)) {
+          // CCM swaps
+          appendSwap(sourceAsset, destAsset, testSwap, true);
+        }
+      });
+  });
 
   // Not doing BTC due to encoding complexity in vault_swap. Will be fixed once SDK supports it.
   appendSwap('Sol', 'Eth', testVaultSwap);
-  // appendSwap('Sol', 'Usdc', testVaultSwap, true);
-  // appendSwap('Sol', 'ArbEth', testVaultSwap);
-  // appendSwap('Sol', 'ArbEth', testVaultSwap, true);
-  // appendSwap('Sol', 'Dot', testVaultSwap);
-  // appendSwap('SolUsdc', 'Eth', testVaultSwap);
-  // appendSwap('SolUsdc', 'Flip', testVaultSwap, true);
-
-  appendSwap('ArbEth', 'Eth', testVaultSwap, true);
+  appendSwap('Sol', 'Usdc', testVaultSwap, true);
+  appendSwap('Sol', 'ArbEth', testVaultSwap);
+  appendSwap('Sol', 'ArbEth', testVaultSwap, true);
+  appendSwap('Sol', 'Dot', testVaultSwap);
+  appendSwap('SolUsdc', 'Eth', testVaultSwap);
+  appendSwap('SolUsdc', 'Flip', testVaultSwap, true);
 
   await Promise.all(allSwaps);
 }
