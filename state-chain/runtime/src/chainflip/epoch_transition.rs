494b54c4d302a4155d458c1eb46cbdb1c7cfa2a8
nmammeri <nmammeri@gmail.com>
2024-11-26 16:36:09 +0100

    feat: cleanup VaultStartBlockNumbers upon epoch expiry

--- state-chain/runtime/src/chainflip/epoch_transition.rs
+++ state-chain/runtime/src/chainflip/epoch_transition.rs
@@ -2,13 +2,18 @@ use crate::AssetBalances;
 use cf_primitives::EpochIndex;
 use cf_traits::EpochTransitionHandler;
 
-use crate::Witnesser;
+use crate::{ArbitrumVault, BitcoinVault, EthereumVault, PolkadotVault, SolanaVault, Witnesser};
 
 pub struct ChainflipEpochTransitions;
 
 impl EpochTransitionHandler for ChainflipEpochTransitions {
 	fn on_expired_epoch(expired: EpochIndex) {
 		<Witnesser as EpochTransitionHandler>::on_expired_epoch(expired);
+		<EthereumVault as EpochTransitionHandler>::on_expired_epoch(expired);
+		<PolkadotVault as EpochTransitionHandler>::on_expired_epoch(expired);
+		<BitcoinVault as EpochTransitionHandler>::on_expired_epoch(expired);
+		<ArbitrumVault as EpochTransitionHandler>::on_expired_epoch(expired);
+		<SolanaVault as EpochTransitionHandler>::on_expired_epoch(expired);
 	}
 	fn on_new_epoch(_new: EpochIndex) {
 		AssetBalances::trigger_reconciliation();
