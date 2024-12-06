65260f523939388f82140e9f44b203a0ef7263e1
Maxim Urschumzew <maxu@chainflip.io>
2024-12-06 13:11:11 +0100

    Integrate block height witnesser in btc electoral system.

--- engine/src/elections/voter_api.rs
+++ engine/src/elections/voter_api.rs
@@ -81,4 +81,5 @@ macro_rules! generate_voter_api_tuple_impls {
 }
 
 generate_voter_api_tuple_impls!(tuple_1_impls: ((A, A0)));
+generate_voter_api_tuple_impls!(tuple_2_impls: ((A, A0), (B, B0)));
 generate_voter_api_tuple_impls!(tuple_7_impls: ((A, A0), (B, B0), (C, C0), (D, D0), (EE, E0), (FF, F0), (GG, G0)));
