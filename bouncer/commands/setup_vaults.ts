f2106e48d6140afe1fcf88e6df21f1d7f350008f
Martin Rieke <martin@chainflip.io>
2024-12-09 13:33:57 +0100

    batch assethub setup in bouncer

--- bouncer/commands/setup_vaults.ts
+++ bouncer/commands/setup_vaults.ts
@@ -185,14 +185,15 @@ async function main(): Promise<void> {
 
   // Step 8
   console.log('Creating USDC and USDT tokens on Assethub');
-  let create_usdc = assethub.tx.assets.forceCreate(1337, alice.address, true, 10000);
-  let create_usdt = assethub.tx.assets.forceCreate(1984, alice.address, true, 10000);
-  await assethub.tx.sudo.sudo(create_usdc).signAndSend(alice, {nonce: -1});
-  await assethub.tx.sudo.sudo(create_usdt).signAndSend(alice, {nonce: -1});
-  await assethub.tx.assets.setMetadata(1337, "USD Coin", "USDC", 6).signAndSend(alice, {nonce: -1});
-  await assethub.tx.assets.setMetadata(1984, "Tether USD", "USDT", 6).signAndSend(alice, {nonce: -1});
-  await assethub.tx.assets.mint(1337, alice.address, 100000000000000).signAndSend(alice, {nonce: -1});
-  await assethub.tx.assets.mint(1984, alice.address, 100000000000000).signAndSend(alice, {nonce: -1});
+  let batch = [
+    assethub.tx.sudo.sudo(assethub.tx.assets.forceCreate(1337, alice.address, true, 10000)),
+    assethub.tx.sudo.sudo(assethub.tx.assets.forceCreate(1984, alice.address, true, 10000)),
+    assethub.tx.assets.setMetadata(1337, "USD Coin", "USDC", 6),
+    assethub.tx.assets.setMetadata(1984, "Tether USD", "USDT", 6),
+    assethub.tx.assets.mint(1337, alice.address, 100000000000000),
+    assethub.tx.assets.mint(1984, alice.address, 100000000000000)
+  ];
+  await assethub.tx.utility.batchAll(batch).signAndSend(alice, {nonce: -1});
 
   // Confirmation
   console.log('Waiting for new epoch...');
