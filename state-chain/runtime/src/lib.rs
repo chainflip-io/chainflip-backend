763e63fdc1b31272aa2c67f9f2449adbebcd06a8
Jan Börner <jan@chainflip.io>
2024-12-12 13:16:37 +0100

    chore: Removed upgrade condition for pending swaps

--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -730,7 +730,7 @@ impl pallet_cf_governance::Config for Runtime {
 	type WeightInfo = pallet_cf_governance::weights::PalletWeight<Runtime>;
 	type UpgradeCondition = (
 		pallet_cf_validator::NotDuringRotation<Runtime>,
-		(pallet_cf_swapping::NoPendingSwaps<Runtime>, pallet_cf_environment::NoUsedNonce<Runtime>),
+		pallet_cf_environment::NoUsedNonce<Runtime>,
 	);
 	type RuntimeUpgrade = chainflip::RuntimeUpgradeManager;
 	type CompatibleCfeVersions = Environment;
