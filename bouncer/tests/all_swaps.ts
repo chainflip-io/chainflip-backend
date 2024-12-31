0ce5be4d99c6bf4bd75833730bb9564300fcff1b
Albert Llimos <53186777+albert-llimos@users.noreply.github.com>
2024-12-31 15:45:53 +0100

    chore: increase ccm message max to 15k (#5503)
    * chore: increase ccm message max to 15k
    
    * chore: fix benchmark value
    
    * chore: add vault swap specific ccm metadata lengths
    
    * chore: incrase gas
    
    * chore: update SDK to 15k
    
    * chore: decrease gasDiv
    
    * chore: improve engine print
    
    * chore: decrease Arbitrum ccm max length
    
    * chore: limit ARB_LENGTH on vault swaps
    
    * chore: decrease length further
    
    * chore: decrease ARB_LENGTH further
    
    * chore: add workaround!
    
    * chore: increase patch to 1.5M
    
    * feat: modify arbitrum ccm_calculations and add bouncer gas estimation
    
    * chore: update gas limit test and run full bouncer
    
    * chore: update test
    
    * chore: lint bouncer
    
    * chore: improve gas limit test
    
    * chore: add missing await and nit
    
    * chore: restore flag

--- bouncer/tests/all_swaps.ts
+++ bouncer/tests/all_swaps.ts
@@ -1,12 +1,44 @@
 import { InternalAsset as Asset, InternalAssets as Assets } from '@chainflip/cli';
 import { ExecutableTest } from '../shared/executable_test';
 import { SwapParams } from '../shared/perform_swap';
-import { newCcmMetadata, testSwap, testVaultSwap } from '../shared/swapping';
+import {
+  newCcmMetadata,
+  newVaultSwapCcmMetadata,
+  testSwap,
+  testVaultSwap,
+} from '../shared/swapping';
 import { btcAddressTypes } from '../shared/new_btc_address';
 import { ccmSupportedChains, chainFromAsset, VaultSwapParams } from '../shared/utils';
 
 /* eslint-disable @typescript-eslint/no-use-before-define */
 export const testAllSwaps = new ExecutableTest('All-Swaps', main, 3000);
+
+export async function initiateSwap(
+  sourceAsset: Asset,
+  destAsset: Asset,
+  functionCall: typeof testSwap | typeof testVaultSwap,
+  ccmSwap: boolean = false,
+): Promise<SwapParams | VaultSwapParams> {
+  let ccmSwapMetadata;
+  if (ccmSwap) {
+    ccmSwapMetadata =
+      functionCall === testSwap
+        ? await newCcmMetadata(destAsset)
+        : await newVaultSwapCcmMetadata(sourceAsset, destAsset);
+  }
+
+  if (destAsset === 'Btc') {
+    const btcAddressTypesArray = Object.values(btcAddressTypes);
+    return functionCall(
+      sourceAsset,
+      destAsset,
+      btcAddressTypesArray[Math.floor(Math.random() * btcAddressTypesArray.length)],
+      ccmSwapMetadata,
+      testAllSwaps.swapContext,
+    );
+  }
+  return functionCall(sourceAsset, destAsset, undefined, ccmSwapMetadata, testAllSwaps.swapContext);
+}
 
 async function main() {
   const allSwaps: Promise<SwapParams | VaultSwapParams>[] = [];
@@ -17,28 +49,7 @@ async function main() {
     functionCall: typeof testSwap | typeof testVaultSwap,
     ccmSwap: boolean = false,
   ) {
-    if (destAsset === 'Btc') {
-      const btcAddressTypesArray = Object.values(btcAddressTypes);
-      allSwaps.push(
-        functionCall(
-          sourceAsset,
-          destAsset,
-          btcAddressTypesArray[Math.floor(Math.random() * btcAddressTypesArray.length)],
-          ccmSwap ? newCcmMetadata(destAsset) : undefined,
-          testAllSwaps.swapContext,
-        ),
-      );
-    } else {
-      allSwaps.push(
-        functionCall(
-          sourceAsset,
-          destAsset,
-          undefined,
-          ccmSwap ? newCcmMetadata(destAsset) : undefined,
-          testAllSwaps.swapContext,
-        ),
-      );
-    }
+    allSwaps.push(initiateSwap(sourceAsset, destAsset, functionCall, ccmSwap));
   }
 
   Object.values(Assets).forEach((sourceAsset) => {
