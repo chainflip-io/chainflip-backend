4b26c6de371ba8f454ab2742ce2abf68c9ff8871
albert <allimos3@gmail.com>
2025-01-07 12:23:22 +0100

    chore: add workaround for swap event accounts

--- bouncer/shared/sol_vault_swap.ts
+++ bouncer/shared/sol_vault_swap.ts
@@ -11,6 +11,7 @@ import {
 } from '@solana/web3.js';
 import { getAssociatedTokenAddressSync } from '@solana/spl-token';
 import BigNumber from 'bignumber.js';
+import { randomBytes } from 'crypto';
 import {
   getContractAddress,
   amountToFineAmount,
@@ -24,6 +25,7 @@ import {
   stateChainAssetFromAsset,
   decodeSolAddress,
   decodeDotAddressForContract,
+  newAddress,
 } from './utils';
 import { CcmDepositMetadata, DcaParams, FillOrKillParamsX128 } from './new_swap';
 
@@ -187,30 +189,49 @@ export async function checkSolEventAccountsClosure(
     getContractAddress('Solana', 'SWAP_ENDPOINT_DATA_ACCOUNT'),
   );
 
-  const maxRetries = 50; // 300 seconds
+  async function checkAccounts(swapEventAccounts: PublicKey[]): Promise<boolean> {
+    const maxRetries = 20; // 120 seconds
 
-  for (let attempt = 0; attempt < maxRetries; attempt++) {
-    const swapEndpointDataAccount =
-      await cfSwapEndpointProgram.account.swapEndpointDataAccount.fetch(
-        swapEndpointDataAccountAddress,
-      );
+    for (let attempt = 0; attempt < maxRetries; attempt++) {
+      const swapEndpointDataAccount =
+        await cfSwapEndpointProgram.account.swapEndpointDataAccount.fetch(
+          swapEndpointDataAccountAddress,
+        );
 
-    if (swapEndpointDataAccount.openEventAccounts.length >= 10) {
-      await sleep(6000);
-    } else {
-      const onChainOpenedAccounts = swapEndpointDataAccount.openEventAccounts.map((element) =>
-        element.toString(),
-      );
-      for (const eventAccount of eventAccounts) {
-        if (!onChainOpenedAccounts.includes(eventAccount.toString())) {
-          const accountInfo = await getSolConnection().getAccountInfo(eventAccount);
-          if (accountInfo !== null) {
-            throw new Error('Event account still exists, should have been closed');
+      if (swapEndpointDataAccount.openEventAccounts.length >= 10) {
+        await sleep(6000);
+      } else {
+        const onChainOpenedAccounts = swapEndpointDataAccount.openEventAccounts.map((element) =>
+          element.toString(),
+        );
+        for (const eventAccount of swapEventAccounts) {
+          if (!onChainOpenedAccounts.includes(eventAccount.toString())) {
+            const accountInfo = await getSolConnection().getAccountInfo(eventAccount);
+            if (accountInfo !== null) {
+              throw new Error('Event account still exists, should have been closed');
+            }
           }
         }
+        return true;
       }
-      return;
     }
+    return false;
   }
-  throw new Error('Timed out waiting for event accounts to be closed');
+
+  let success = await checkAccounts(eventAccounts);
+  if (!success) {
+    // Due to implementation details on the SC the accounts won't necessarily be closed
+    // immediately and the timeout won't be executed until one extra Vault swap is witnessed.
+    // We manually trigger a new one to ensure the timeout is executed.
+    await executeSolVaultSwap(
+      'Sol',
+      'ArbEth',
+      await newAddress('ArbEth', randomBytes(32).toString('hex')),
+    );
+    success = await checkAccounts(eventAccounts);
+  }
+
+  if (!success) {
+    throw new Error('Timed out waiting for event accounts to be closed');
+  }
 }
