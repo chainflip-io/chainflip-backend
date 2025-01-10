1d58d05786ff1e7c1a1a381b80b106c7b3c64a95
Maxim Urschumzew <maxu@chainflip.io>
2025-01-10 18:38:19 +0100

    Fix `get_hubasset_balance` command (proper comment and type checking).

--- bouncer/commands/get_hubasset_balance.ts
+++ bouncer/commands/get_hubasset_balance.ts
@@ -4,14 +4,22 @@
 // This command takes one argument.
 // It will print the Dot balance of the address provided as the first argument.
 //
-// For example: ./commands/get_dot_balance.ts 5Dd1drBHuBzHK7qGWzGQ2iR2KnbYZJbYuUfc88v5Cv4juWci
+// For example: ./commands/get_hubasset_balance.ts HubUsdc 5Dd1drBHuBzHK7qGWzGQ2iR2KnbYZJbYuUfc88v5Cv4juWci
 // might print: 1.2
 
-import { runWithTimeoutAndExit } from '../shared/utils';
+import { HubAsset, runWithTimeoutAndExit } from '../shared/utils';
 import { getHubAssetBalance } from '../shared/get_hub_balance';
 
+function stringToHubAsset(asset: string): HubAsset {
+  if (asset === 'HubUsdc' || asset === 'HubUsdt') {
+    return asset;
+  } else {
+      throw new Error(`Unsupported Assethub asset: ${asset}`);
+  }
+}
+
 async function getHubAssetBalanceCommand(asset: string, address: string) {
-  console.log(await getHubAssetBalance(asset, address));
+  console.log(await getHubAssetBalance(stringToHubAsset(asset), address));
 }
 
 const asset = process.argv[2];
