149721ef342036cc8bd67bcfb56a263dc7ada6f8
Daniel <daniel@chainflip.io>
2025-01-16 17:44:20 +0100

    fix: use chain-specific account type for refund params

--- engine/src/witness/arb.rs
+++ engine/src/witness/arb.rs
@@ -192,7 +192,9 @@ impl super::evm::vault::IngressCallBuilder for ArbCallBuilder {
 		destination_address: EncodedAddress,
 		deposit_metadata: Option<CcmDepositMetadata>,
 		tx_id: H256,
-		vault_swap_parameters: Option<VaultSwapParameters>,
+		vault_swap_parameters: Option<
+			VaultSwapParameters<<Self::Chain as cf_chains::Chain>::ChainAccount>,
+		>,
 	) -> state_chain_runtime::RuntimeCall {
 		let deposit = vault_deposit_witness!(
 			source_asset,
