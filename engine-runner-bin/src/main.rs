7262772dc949f7913dda429e3e7a153fbf73c09b
kylezs <kyle@chainflip.io>
2025-01-29 16:35:44 +0100

    chore: bump old engine version (#5594)

--- engine-runner-bin/src/main.rs
+++ engine-runner-bin/src/main.rs
@@ -3,7 +3,7 @@ use engine_upgrade_utils::{CStrArray, NEW_VERSION, OLD_VERSION};
 
 // Declare the entrypoints into each version of the engine
 mod old {
-	#[engine_proc_macros::link_engine_library_version("1.7.5")]
+	#[engine_proc_macros::link_engine_library_version("1.7.8")]
 	extern "C" {
 		pub fn cfe_entrypoint(
 			c_args: engine_upgrade_utils::CStrArray,
