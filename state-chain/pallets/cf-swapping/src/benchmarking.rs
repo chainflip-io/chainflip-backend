a8b09426355b95495bdbeca75c85997600b51a6b
Maxim Shishmarev <maxim@chainflip.io>
2025-02-03 16:18:17 +1100

    chore: fix benchmark

--- state-chain/pallets/cf-swapping/src/benchmarking.rs
+++ state-chain/pallets/cf-swapping/src/benchmarking.rs
@@ -204,6 +204,12 @@ mod benchmarks {
 		let lp_id =
 			T::AccountRoleRegistry::whitelisted_caller_with_role(AccountRole::LiquidityProvider)
 				.unwrap();
+
+		T::LpRegistrationApi::register_liquidity_refund_address(
+			&lp_id,
+			ForeignChainAddress::Eth(Default::default()),
+		);
+
 		let caller = OriginFor::<T>::signed(lp_id.clone());
 
 		T::BalanceApi::credit_account(&lp_id, Asset::Eth, 1000);
