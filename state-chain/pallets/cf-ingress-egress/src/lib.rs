254f35a37e48f00c6cb02956e68ec4679c4d2682
Jan Börner <jan@chainflip.io>
2025-02-03 16:56:56 +0100

    test: Added test for checking refund of cancelled VaultSwaps

--- state-chain/pallets/cf-ingress-egress/src/lib.rs
+++ state-chain/pallets/cf-ingress-egress/src/lib.rs
@@ -1508,6 +1508,8 @@ impl<T: Config<I>, I: 'static> IngressSink for Pallet<T, I> {
 	}
 }
 
+use cf_chains::TransferFallback;
+
 impl<T: Config<I>, I: 'static> Pallet<T, I> {
 	fn mark_transaction_for_rejection_inner(
 		account_id: T::AccountId,
@@ -2485,8 +2487,6 @@ impl<T: Config<I>, I: 'static> Pallet<T, I> {
 			dca_params,
 			boost_fee,
 		} = vault_deposit_witness.clone();
-
-		use cf_chains::TransferFallback;
 
 		if AbortedVaultTransaction::<T, I>::take(&tx_id).is_some() {
 			log::info!("Ignoring deposit since transcation was aborted.");
