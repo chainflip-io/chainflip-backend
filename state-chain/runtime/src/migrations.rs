d89b29050b94b0509c091e3aceb1e5bce3d2b6d2
Daniel <daniel@chainflip.io>
2025-02-04 17:04:58 +0100

    fix: recover boost funds

--- state-chain/runtime/src/migrations.rs
+++ state-chain/runtime/src/migrations.rs
@@ -1,6 +1,7 @@
 //! Chainflip runtime storage migrations.
 
 pub mod add_liveness_electoral_system_solana;
+pub mod boost_recovery;
 pub mod housekeeping;
 pub mod reap_old_accounts;
 pub mod serialize_solana_broadcast;
