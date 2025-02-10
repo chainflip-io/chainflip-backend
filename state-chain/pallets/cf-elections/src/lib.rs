422a7760a7f0e3f8c9a2a18efdbd6286d5acddcf
kylezs <zsembery.kyle@gmail.com>
2025-02-10 16:30:49 +0100

    chore: bump elections version

--- state-chain/pallets/cf-elections/src/lib.rs
+++ state-chain/pallets/cf-elections/src/lib.rs
@@ -124,7 +124,7 @@ use frame_system::pallet_prelude::*;
 
 pub use pallet::*;
 
-pub const PALLET_VERSION: StorageVersion = StorageVersion::new(4);
+pub const PALLET_VERSION: StorageVersion = StorageVersion::new(5);
 
 pub use pallet::UniqueMonotonicIdentifier;
 
