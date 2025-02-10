87754840fe1b3c50229aa620259c5ddb41fc55fe
Daniel <daniel@chainflip.io>
2025-02-10 11:35:18 +0100

    fix: delete Solana channel state when closed

--- state-chain/chains/src/sol.rs
+++ state-chain/chains/src/sol.rs
@@ -473,7 +473,7 @@ impl DepositDetailsToTransactionInId<SolanaCrypto> for () {}
 #[cfg(test)]
 mod test {
 	use super::*;
-	use crate::sol::compute_units_costs::*;
+	use crate::{sol::compute_units_costs::*, ChannelLifecycleHooks};
 
 	#[test]
 	fn can_calculate_gas_limit() {
@@ -509,5 +509,14 @@ mod test {
 			let tx_compute_limit = SolTrackedData::calculate_ccm_compute_limit(0, *asset);
 			assert_eq!(tx_compute_limit, default_compute_limit);
 		}
+	}
+
+	#[test]
+	fn solana_channel_recycling_is_assumed_to_be_deactivated() {
+		assert!(
+			<<Solana as Chain>::DepositChannelState as ChannelLifecycleHooks>::maybe_recycle(0).is_none(),
+			"It looks like Solana channel recycling is active. If this is intentional, ensure that the corresponding
+			unsynchronised state map in the delta_based_ingress election is not deleted when channels are closed."
+		);
 	}
 }
