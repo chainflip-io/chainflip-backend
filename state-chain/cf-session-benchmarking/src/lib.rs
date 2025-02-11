6b6bbb784bb684a71505bbbd19d0cd09e2560251
kylezs <kyle@chainflip.io>
2025-02-11 08:46:27 +0100

    chore: update session pallet benchmarks to v2 (#5623)
    chore: update session pallet migrations to v2

--- state-chain/cf-session-benchmarking/src/lib.rs
+++ state-chain/cf-session-benchmarking/src/lib.rs
@@ -2,14 +2,13 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use codec::Decode;
-use frame_benchmarking::{benchmarks, whitelisted_caller};
+use frame_benchmarking::v2::*;
 use frame_support::{assert_ok, sp_runtime::traits::Convert};
 use frame_system::RawOrigin;
 use pallet_session::*;
 use rand::{RngCore, SeedableRng};
 use sp_std::{prelude::*, vec};
 
-#[allow(dead_code)]
 pub struct Pallet<T: Config>(pallet_session::Pallet<T>);
 pub trait Config: pallet_session::Config + pallet_session::historical::Config {}
 
@@ -20,24 +19,34 @@ fn generate_key<T: Config>(seed: u64) -> T::Keys {
 	Decode::decode(&mut &key[..]).unwrap()
 }
 
-benchmarks! {
-	set_keys {
+#[benchmarks]
+mod benchmarks {
+	use super::*;
+
+	#[benchmark]
+	fn set_keys() {
 		let caller: T::AccountId = whitelisted_caller();
 		let validator_id = T::ValidatorIdOf::convert(caller.clone()).unwrap();
 		<NextKeys<T>>::insert(validator_id.clone(), generate_key::<T>(1));
 		frame_system::Pallet::<T>::inc_providers(&caller);
 		assert_ok!(frame_system::Pallet::<T>::inc_consumers(&caller));
 		let new_key = generate_key::<T>(0);
-	}: _(RawOrigin::Signed(caller), new_key.clone(), vec![])
-	verify {
+
+		#[extrinsic_call]
+		set_keys(RawOrigin::Signed(caller), new_key.clone(), vec![]);
+
 		assert_eq!(<NextKeys<T>>::get(validator_id).expect("No key for id"), new_key);
 	}
-	purge_keys {
+
+	#[benchmark]
+	fn purge_keys() {
 		let caller: T::AccountId = whitelisted_caller();
 		let validator_id = T::ValidatorIdOf::convert(caller.clone()).unwrap();
 		<NextKeys<T>>::insert(validator_id.clone(), generate_key::<T>(0));
-	}: _(RawOrigin::Signed(caller))
-	verify {
+
+		#[extrinsic_call]
+		purge_keys(RawOrigin::Signed(caller));
+
 		assert_eq!(<NextKeys<T>>::get(validator_id), None);
 	}
 }
