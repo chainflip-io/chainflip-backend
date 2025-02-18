120f3e5edce23aaf7a87dbff49e8d98500ee1335
Maxim Urschumzew <maxu@chainflip.io>
2025-02-18 17:27:00 +0100

    chore: removed unused type.

--- state-chain/runtime/src/chainflip/elections.rs
+++ state-chain/runtime/src/chainflip/elections.rs
@@ -3,18 +3,6 @@ use derive_where::derive_where;
 use scale_info::TypeInfo;
 use serde::{Deserialize, Serialize};
 
-/// Type which can be used to implement hooks. It implements
-/// all required bounds without requiring any bounds on the
-/// `Tag` type parameter.
-#[derive_where(Default, Debug, Clone, PartialEq, Eq;)]
-#[derive(Encode, Decode, TypeInfo, Deserialize, Serialize)]
-#[codec(encode_bound())]
-#[serde(bound = "")]
-#[scale_info(skip_type_params(Tag))]
-pub struct StatelessHookFor<Tag> {
-	_phantom: sp_std::marker::PhantomData<Tag>,
-}
-
 /// Type which can be used for implementing traits that
 /// contain only type definitions, as used in many parts of
 /// the state machine based electoral systems.
