5dd4e8bf6ad26f60b9aadc553fc5cad34d874c22
Assem Hasna <asem.hasna@gmail.com>
2025-02-27 12:16:01 +0100

    chore: simplify logic and update rpc endpoint 🐛

--- ci/scripts/check_runtime_spec_version.sh
+++ ci/scripts/check_runtime_spec_version.sh
@@ -5,9 +5,8 @@ network=$1
 runtime_spec_version_file="state-chain/runtime/src/lib.rs"
 
 # Extract the spec_version from the Rust file and convert it to a comparable number
-raw_version=$(grep -F '#[sp_version::runtime_version]' -A 10 $runtime_spec_version_file | grep 'spec_version' | awk '{print $2}' | tr -d ',')
-# Convert version like 1_07_10 to proper semver by removing leading zeros
-spec_version=$(echo $raw_version | sed 's/_\([0-9]\)/_\1/g' | tr -d '_')
+spec_version=$(grep -F '#[sp_version::runtime_version]' -A 10 $runtime_spec_version_file | grep 'spec_version' | awk '{print $2}' | tr -d ',' | tr -d '_')
+
 
 if [ -z $network ]; then
     echo "Network not specified"
@@ -15,7 +14,7 @@ if [ -z $network ]; then
 fi
 
 if [ $network == "berghain" ]; then
-    RPC_ENDPOINT="https://mainnet-archive.chainflip.io"
+    RPC_ENDPOINT="https://rpc.mainnet.chainflip.io"
 elif [ $network == "perseverance" ]; then
     RPC_ENDPOINT="https://archive.perseverance.chainflip.io"
 elif [ $network == "sisyphos" ]; then
@@ -27,7 +26,7 @@ fi
 
 live_runtime_version=$(curl -s -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "state_getRuntimeVersion", "params":[]}' $RPC_ENDPOINT | jq .result.specVersion)
 
-echo "Live runtime version: $live_runtime_version, Current Spec version: $spec_version (from $raw_version)"
+echo "Live runtime version: $live_runtime_version, Current Spec version: $spec_version"
 
 # Convert live_runtime_version to the same format for comparison
 live_runtime_version_comparable=$(printf "%d" $live_runtime_version)
