64c045abac504dd099f7ccc12118a272885dca0c
nmammeri <nmammeri@gmail.com>
2025-02-27 14:24:41 +0100

    fix: removed duplicate macro

--- api/cf-rpc-types/src/lib.rs
+++ api/cf-rpc-types/src/lib.rs
@@ -17,16 +17,3 @@ pub struct SwapChannelInfo<C: Chain> {
 	pub source_asset: Asset,
 	pub destination_asset: Asset,
 }
-
-#[macro_export]
-macro_rules! extract_event {
-    ($events:expr, $runtime_event_variant:path, $pallet_event_variant:path, $pattern:tt, $result:expr) => {
-        if let Some($runtime_event_variant($pallet_event_variant $pattern)) = $events.iter().find(|event| {
-            matches!(event, $runtime_event_variant($pallet_event_variant { .. }))
-        }) {
-        	Ok($result)
-        } else {
-            Err(anyhow!("No {}({}) event was found", stringify!($runtime_event_variant), stringify!($pallet_event_variant)))
-        }
-    };
-}
