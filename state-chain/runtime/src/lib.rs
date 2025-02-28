41e78c670e75d8690e13ccd6a6f926ae80fcc21f
Jan Börner <jan@chainflip.io>
2025-02-28 14:12:55 +0100

    chore: added basic migration

--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -1264,6 +1264,7 @@ type AllMigrations = (
 	migrations::solana_remove_unused_channels_state::SolanaRemoveUnusedChannelsState,
 	migrations::reap_old_accounts::Migration,
 	migrations::whitelist_brokers::Migration,
+	migrations::increase_arb_safety_margin::Migration,
 );
 
 /// All the pallet-specific migrations and migrations that depend on pallet migration order. Do not
