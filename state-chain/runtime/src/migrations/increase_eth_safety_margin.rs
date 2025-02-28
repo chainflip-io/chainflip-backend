3e10780f2867ac4ed42658e5b9dc1e8a9077c2ea
kylezs <zsembery.kyle@gmail.com>
2025-02-28 10:07:38 +0100

    increase arb safety margins

--- state-chain/runtime/src/migrations/increase_eth_safety_margin.rs
+++ state-chain/runtime/src/migrations/increase_eth_safety_margin.rs
@@ -1,5 +1,5 @@
 use crate::Runtime;
-use cf_chains::instances::EthereumInstance;
+use cf_chains::instances::{ArbitrumInstance, EthereumInstance};
 use cf_runtime_upgrade_utilities::genesis_hashes;
 use frame_support::{traits::OnRuntimeUpgrade, weights::Weight};
 use pallet_cf_ingress_egress::WitnessSafetyMargin;
@@ -14,16 +14,28 @@ pub struct Migration;
 
 const NEW_ETH_SAFETY_MARGIN: u64 = 12;
 
+// Keeping it a multiple of 24 to match the witness period of Arbitrum
+const NEW_ARB_SAFETY_MARGIN: u64 = 672;
+
 impl OnRuntimeUpgrade for Migration {
 	#[cfg(feature = "try-runtime")]
 	fn pre_upgrade() -> Result<Vec<u8>, sp_runtime::TryRuntimeError> {
-		Ok(WitnessSafetyMargin::<Runtime, EthereumInstance>::get().encode())
+		use cf_chains::instances::ArbitrumInstance;
+
+		let eth_margin = WitnessSafetyMargin::<Runtime, EthereumInstance>::get();
+		let arb_margin = WitnessSafetyMargin::<Runtime, ArbitrumInstance>::get();
+
+		let mut encoded = eth_margin.encode();
+		encoded.extend(arb_margin.encode());
+
+		Ok(encoded)
 	}
 
 	fn on_runtime_upgrade() -> Weight {
 		match genesis_hashes::genesis_hash::<Runtime>() {
 			genesis_hashes::BERGHAIN => {
 				WitnessSafetyMargin::<Runtime, EthereumInstance>::put(NEW_ETH_SAFETY_MARGIN);
+				WitnessSafetyMargin::<Runtime, ArbitrumInstance>::put(NEW_ARB_SAFETY_MARGIN);
 			},
 			genesis_hashes::PERSEVERANCE => {
 				// Nothing
@@ -39,15 +51,24 @@ impl OnRuntimeUpgrade for Migration {
 
 	#[cfg(feature = "try-runtime")]
 	fn post_upgrade(state: Vec<u8>) -> Result<(), DispatchError> {
-		let old_margin: Option<u64> = Decode::decode(&mut &state[..]).unwrap();
-		let new_margin = WitnessSafetyMargin::<Runtime, EthereumInstance>::get();
+		let (old_eth_margin, old_arb_margin): (Option<u64>, Option<u64>) =
+			Decode::decode(&mut &state[..])
+				.map_err(|_| DispatchError::Other("Failed to decode state"))?;
+
+		let new_eth_margin = WitnessSafetyMargin::<Runtime, EthereumInstance>::get();
+		let new_arb_margin = WitnessSafetyMargin::<Runtime, ArbitrumInstance>::get();
 		match genesis_hashes::genesis_hash::<Runtime>() {
-			genesis_hashes::BERGHAIN => assert_eq!(new_margin, Some(NEW_ETH_SAFETY_MARGIN)),
+			genesis_hashes::BERGHAIN => {
+				assert_eq!(new_eth_margin, Some(NEW_ETH_SAFETY_MARGIN));
+				assert_eq!(new_arb_margin, Some(NEW_ARB_SAFETY_MARGIN));
+			},
 			genesis_hashes::PERSEVERANCE => {
-				assert_eq!(new_margin, old_margin);
+				assert_eq!(new_eth_margin, old_eth_margin);
+				assert_eq!(new_arb_margin, old_arb_margin);
 			},
 			genesis_hashes::SISYPHOS => {
-				assert_eq!(new_margin, old_margin);
+				assert_eq!(new_eth_margin, old_eth_margin);
+				assert_eq!(new_arb_margin, old_arb_margin);
 			},
 			_ => {},
 		}
