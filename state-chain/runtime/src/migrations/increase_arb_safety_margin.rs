21b7e8bf3742f73966aa6d294f89581f90b22000
Jan Börner <jan@chainflip.io>
2025-03-03 17:16:41 +0100

    chore: updated migration

--- state-chain/runtime/src/migrations/increase_arb_safety_margin.rs
+++ state-chain/runtime/src/migrations/increase_arb_safety_margin.rs
@@ -18,27 +18,13 @@ const NEW_ARB_SAFETY_MARGIN: u64 = 672;
 impl OnRuntimeUpgrade for Migration {
 	#[cfg(feature = "try-runtime")]
 	fn pre_upgrade() -> Result<Vec<u8>, sp_runtime::TryRuntimeError> {
-		use cf_chains::instances::ArbitrumInstance;
 		log::info!("🚀 Increasing Arbitrum safety margin");
-
 		let arb_margin = WitnessSafetyMargin::<Runtime, ArbitrumInstance>::get();
-
 		Ok(arb_margin.encode())
 	}
 
 	fn on_runtime_upgrade() -> Weight {
-		match genesis_hashes::genesis_hash::<Runtime>() {
-			genesis_hashes::BERGHAIN => {
-				WitnessSafetyMargin::<Runtime, ArbitrumInstance>::put(NEW_ARB_SAFETY_MARGIN);
-			},
-			genesis_hashes::PERSEVERANCE => {
-				WitnessSafetyMargin::<Runtime, ArbitrumInstance>::put(NEW_ARB_SAFETY_MARGIN);
-			},
-			genesis_hashes::SISYPHOS => {
-				WitnessSafetyMargin::<Runtime, ArbitrumInstance>::put(NEW_ARB_SAFETY_MARGIN);
-			},
-			_ => {},
-		}
+		WitnessSafetyMargin::<Runtime, ArbitrumInstance>::put(NEW_ARB_SAFETY_MARGIN);
 		Weight::zero()
 	}
 
@@ -46,20 +32,8 @@ impl OnRuntimeUpgrade for Migration {
 	fn post_upgrade(state: Vec<u8>) -> Result<(), DispatchError> {
 		let (old_arb_margin): (Option<u64>) = Decode::decode(&mut &state[..])
 			.map_err(|_| DispatchError::Other("Failed to decode state"))?;
-
 		let new_arb_margin = WitnessSafetyMargin::<Runtime, ArbitrumInstance>::get();
-		match genesis_hashes::genesis_hash::<Runtime>() {
-			genesis_hashes::BERGHAIN => {
-				assert_eq!(new_arb_margin, Some(NEW_ARB_SAFETY_MARGIN));
-			},
-			genesis_hashes::PERSEVERANCE => {
-				assert_eq!(new_arb_margin, Some(NEW_ARB_SAFETY_MARGIN));
-			},
-			genesis_hashes::SISYPHOS => {
-				assert_eq!(new_arb_margin, Some(NEW_ARB_SAFETY_MARGIN));
-			},
-			_ => {},
-		}
+		assert_eq!(new_arb_margin, Some(NEW_ARB_SAFETY_MARGIN));
 		log::info!("✅ Successfully increased Arbitrum safety margin");
 		Ok(())
 	}
