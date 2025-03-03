02899a486f8d53922ed7b8a9044366e2ba580304
kylezs <zsembery.kyle@gmail.com>
2025-03-03 15:43:15 +0100

    fix: bouncer RPC retry logic for try_runtime_upgrade
    debug
    
    rebase with main
    
    bouncer logs
    
    re-enable tests
    
    reduce try_runtime iters
    
    chore: clean up

--- bouncer/shared/try_runtime_upgrade.ts
+++ bouncer/shared/try_runtime_upgrade.ts
@@ -2,17 +2,19 @@
 // https://github.com/paritytech/try-runtime-cli
 
 import path from 'path';
-import fs from 'fs';
+import { ApiPromise, HttpProvider } from '@polkadot/api';
 import { compileBinaries } from './utils/compile_binaries';
 import { mkTmpDir, execWithRustLog } from './utils/exec_with_log';
-import { getChainflipApi } from './utils/substrate';
+import { CHAINFLIP_HTTP_ENDPOINT } from './utils/substrate';
+import { retryRpcCall } from './utils';
+import { globalLogger as logger } from './utils/logger';
 
 async function createSnapshotFile(networkUrl: string, blockHash: string): Promise<boolean> {
   const blockParam = blockHash === 'latest' ? '' : `--at ${blockHash}`;
   const snapshotFolder = await mkTmpDir('chainflip/snapshots/');
   const snapshotOutputPath = path.join(snapshotFolder, `snapshot-at-${blockHash}.snap`);
 
-  console.log('Writing snapshot to: ', snapshotOutputPath);
+  logger.info('Writing snapshot to: ', snapshotOutputPath);
 
   return execWithRustLog(
     `try-runtime create-snapshot ${blockParam} --uri ${networkUrl} ${snapshotOutputPath}`,
@@ -53,44 +55,57 @@ export async function tryRuntimeUpgrade(
   block: number | 'latest' | 'all' | 'last-n',
   networkUrl: string,
   runtimePath: string,
-  lastN = 40,
+  lastN = 20,
 ) {
-  await using api = await getChainflipApi();
+  const httpApi = await ApiPromise.create({
+    provider: new HttpProvider(CHAINFLIP_HTTP_ENDPOINT),
+    noInitWarn: true,
+  });
 
   if (block === 'all') {
-    const latestBlock = await api.rpc.chain.getBlockHash();
+    const latestBlock = await httpApi.rpc.chain.getBlockHash();
 
-    console.log('Running migrations until we reach block with hash: ' + latestBlock);
+    logger.info('Running migrations until we reach block with hash: ' + latestBlock);
 
     let blockNumber = 1;
-    let blockHash = await api.rpc.chain.getBlockHash(blockNumber);
+    let blockHash = await httpApi.rpc.chain.getBlockHash(blockNumber);
     while (!blockHash.eq(latestBlock)) {
-      blockHash = await api.rpc.chain.getBlockHash(blockNumber);
+      blockHash = await httpApi.rpc.chain.getBlockHash(blockNumber);
       await tryRuntimeCommand(runtimePath, `${blockHash}`, networkUrl);
       blockNumber++;
     }
-    console.log(`Block ${latestBlock} has been reached, exiting.`);
+    logger.info(`Block ${latestBlock} has been reached, exiting.`);
   } else if (block === 'last-n') {
-    console.log(`Running migrations for the last ${lastN} blocks.`);
+    logger.info(`Running migrations for the last ${lastN} blocks.`);
     let blocksProcessed = 0;
 
-    let nextHash = await api.rpc.chain.getBlockHash();
+    let nextHash = await httpApi.rpc.chain.getBlockHash();
 
     while (blocksProcessed < lastN) {
+      logger.info('Running try-runtime for block: ', nextHash.toString());
       await tryRuntimeCommand(runtimePath, `${nextHash}`, networkUrl);
 
-      const currentBlockHeader = await api.rpc.chain.getHeader(nextHash);
+      const currentHash = nextHash;
+      const currentBlockHeader = await retryRpcCall(
+        () => httpApi.rpc.chain.getHeader(currentHash),
+        {
+          maxAttempts: 10,
+          timeoutMs: 20000,
+          operation: `get block header at ${currentHash}`,
+        },
+      );
       nextHash = currentBlockHeader.parentHash;
+
       blocksProcessed++;
     }
   } else if (block === 'latest') {
     await tryRuntimeCommand(runtimePath, 'latest', networkUrl);
   } else {
-    const blockHash = await api.rpc.chain.getBlockHash(block);
+    const blockHash = await httpApi.rpc.chain.getBlockHash(block);
     await tryRuntimeCommand(runtimePath, `${blockHash}`, networkUrl);
   }
 
-  console.log('try-runtime upgrade successful.');
+  logger.info('try-runtime upgrade successful.');
 }
 
 export async function tryRuntimeUpgradeWithCompileRuntime(
