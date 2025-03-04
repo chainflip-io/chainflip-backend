2c89355052f58a19022208e1fd32179bf851ae1b
Daniel <daniel@chainflip.io>
2025-03-04 17:23:05 +0100

    feat: ethereum broker level screening

--- state-chain/chains/src/lib.rs
+++ state-chain/chains/src/lib.rs
@@ -551,6 +551,15 @@ pub enum RejectError {
 	Other,
 }
 
+impl From<AllBatchError> for RejectError {
+	fn from(e: AllBatchError) -> Self {
+		match e {
+			AllBatchError::UnsupportedToken => RejectError::NotSupportedForAsset,
+			_ => RejectError::Other,
+		}
+	}
+}
+
 pub trait ConsolidateCall<C: Chain>: ApiCall<C::ChainCrypto> {
 	fn consolidate_utxos() -> Result<Self, ConsolidationError>;
 }
@@ -560,6 +569,8 @@ pub trait RejectCall<C: Chain>: ApiCall<C::ChainCrypto> {
 		_deposit_details: C::DepositDetails,
 		_refund_address: C::ChainAccount,
 		_refund_amount: C::ChainAmount,
+		_asset: C::ChainAsset,
+		_deposit_fetch_id: C::DepositFetchId,
 	) -> Result<Self, RejectError> {
 		Err(RejectError::NotSupportedForAsset)
 	}
@@ -1009,7 +1020,7 @@ pub enum RequiresSignatureRefresh<C: ChainCrypto, Api: ApiCall<C>> {
 }
 
 pub trait DepositDetailsToTransactionInId<C: ChainCrypto> {
-	fn deposit_id(&self) -> Option<C::TransactionInId> {
+	fn deposit_ids(&self) -> Option<Vec<C::TransactionInId>> {
 		None
 	}
 }
