470c29effc6e74ec435dfe76cb70a66d6593e93d
Daniel <daniel@chainflip.io>
2025-03-04 12:16:00 +0100

    feat: migration

--- state-chain/pallets/cf-ingress-egress/src/migrations.rs
+++ state-chain/pallets/cf-ingress-egress/src/migrations.rs
@@ -1,4 +1,10 @@
 use crate::Pallet;
 use cf_runtime_utilities::PlaceholderMigration;
+use frame_support::migrations::VersionedMigration;
 
-pub type PalletMigration<T, I> = (PlaceholderMigration<22, Pallet<T, I>>,);
+mod mandatory_refund_params;
+
+pub type PalletMigration<T, I> = (
+	VersionedMigration<22, 23, mandatory_refund_params::Migration<T, I>, Pallet<T, I>, ()>,
+	PlaceholderMigration<23, Pallet<T, I>>,
+);
