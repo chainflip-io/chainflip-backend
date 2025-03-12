5722b21732dd56fe03ba948f306fcb0816ef761c
Albert Llimos <53186777+albert-llimos@users.noreply.github.com>
2025-03-12 09:49:33 +0100

    feat: update ccm checker and test Solana CCM Versioned transactions (#5690)
    * chore: rename Egress witnessing -> witnessing something
    
    * chore: update limits
    
    * chore: implement logic, not testable yet
    
    * chore: nit
    
    * chore: test failing case - using alt
    
    * chore: run only Sol CCM swap
    
    * hcore: add TODO
    
    * feat: integration test
    
    * chore: cargo fmt
    
    * chore: cleanup
    
    * chore: tweak byte calculations
    
    * chore: remove old comment
    
    * chore: improve ccm checker
    
    * chore: restore ci
    
    * chore: add tests for Solana CCM MAX_BYTES and fix clippy
    
    * chore: update tests
    
    * chore: comments
    
    * Fixed some minor rust technicalities and typos
    
    * Fixed build and test and rebased onto the latest `base-branch`
    
    * chore: remove file from merge issue
    
    * chore: add clarification and rename
    
    ---------
    
    Co-authored-by: Ramiz Siddiqui <ramiz@chainflip.io>
    Co-authored-by: Roy Yang <roy@chainflip.io>

--- state-chain/chains/src/sol/api.rs
+++ state-chain/chains/src/sol/api.rs
@@ -730,7 +730,7 @@ impl<Env: 'static + SolanaEnvironment> ExecutexSwapAndCall<Solana> for SolanaApi
 			ccm_additional_data,
 		)
 		.map_err(|e| {
-			log::error!("Failed to construct Solana CCM transfer transaction! \nError: {}", e);
+			log::error!("Failed to construct Solana CCM transfer transaction! \nError: {:?}", e);
 			match e {
 				SolanaTransactionBuildingError::AltsNotYetWitnessed =>
 					ExecutexSwapAndCallError::TryAgainLater,
@@ -817,7 +817,17 @@ impl<Environment: SolanaEnvironment> SetGovKeyWithAggKey<SolanaCrypto> for Solan
 impl<Env: 'static> RejectCall<Solana> for SolanaApi<Env> {}
 
 #[derive(
-	Serialize, Deserialize, Debug, PartialEq, Eq, Clone, Encode, Decode, TypeInfo, Ord, PartialOrd,
+	Serialize,
+	Deserialize,
+	RuntimeDebug,
+	PartialEq,
+	Eq,
+	Clone,
+	Encode,
+	Decode,
+	TypeInfo,
+	Ord,
+	PartialOrd,
 )]
 pub enum AltConsensusResult<T> {
 	ValidConsensusAlts(T),
