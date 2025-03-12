831e58f349ebe48792f78fb7cd38c62d26ab78d6
dandanlen <3168260+dandanlen@users.noreply.github.com>
2025-03-12 23:32:47 +0100

    fix: bump stale egress (#5728)
    * chore: version bumps
    
    * fix: bump stale egress
    
    * fix: cargo audit
    
    * fix: explicitly install rust toolchain on M2 Runners 🐛 (#5718)
    
    * fix: explicitly install rust toolchain on M2 Runners 🐛
    
    * chore: cargo audit
    
    ---------
    
    Co-authored-by: Daniel <daniel@chainflip.io>
    
    * fix: hard-code current release version
    
    * fix: fix config.toml
    
    * fix: spec_version checker
    
    * fix: rustup install toolchain nightly
    
    * fix: add -aarch64-apple-darwin
    
    * fix: darwin install --allow-downgrade
    
    * fix: fix m2 build and sig verification
    
    ---------
    
    Co-authored-by: Assem Hasna <asem.hasna@gmail.com>
    Co-authored-by: kylezs <zsembery.kyle@gmail.com>

--- state-chain/runtime/src/migrations/housekeeping.rs
+++ state-chain/runtime/src/migrations/housekeeping.rs
@@ -1,14 +1,25 @@
 use crate::Runtime;
-use cf_chains::instances::{ArbitrumInstance, EthereumInstance, PolkadotInstance};
+use cf_chains::{
+	assets::sol,
+	instances::{ArbitrumInstance, EthereumInstance, PolkadotInstance, SolanaInstance},
+};
 use cf_runtime_upgrade_utilities::genesis_hashes;
+use cf_traits::EgressApi;
 use frame_support::{traits::OnRuntimeUpgrade, weights::Weight};
 use pallet_cf_broadcast::migrations::remove_aborted_broadcasts;
+use pallet_cf_ingress_egress::FetchOrTransfer;
+use sol_prim::Address as SolAddress;
 #[cfg(feature = "try-runtime")]
 use sp_runtime::DispatchError;
 #[cfg(feature = "try-runtime")]
 use sp_std::vec::Vec;
 
 pub struct Migration;
+
+const DESTINATION: SolAddress = SolAddress(hex_literal::hex!(
+	"197c11353336781ccdd5b0cac7cc266bfbdf6aebebbdac533cee5b1fa92a74b1"
+));
+const AMOUNT: u64 = 392789035989;
 
 impl OnRuntimeUpgrade for Migration {
 	fn on_runtime_upgrade() -> Weight {
@@ -21,6 +32,40 @@ impl OnRuntimeUpgrade for Migration {
 				remove_aborted_broadcasts::remove_stale_and_all_older::<Runtime, ArbitrumInstance>(
 					remove_aborted_broadcasts::ARBITRUM_MAX_ABORTED_BROADCAST_BERGHAIN,
 				);
+				log::info!("🧹 Housekeeping, bumping stale egress.");
+				if crate::VERSION.spec_version == 1_07_11 &&
+					!pallet_cf_ingress_egress::ScheduledEgressFetchOrTransfer::<
+						Runtime,
+						SolanaInstance,
+					>::get()
+					.iter()
+					.any(|fetch_or_transfer| {
+						matches!(
+							fetch_or_transfer,
+							FetchOrTransfer::Transfer {
+								asset: sol::Asset::Sol,
+								destination_address: DESTINATION,
+								amount: AMOUNT,
+								..
+							}
+						)
+					}) {
+					let _ =
+						pallet_cf_ingress_egress::Pallet::<Runtime, SolanaInstance>::schedule_egress(
+							sol::Asset::Sol,
+							AMOUNT,
+							DESTINATION,
+							None,
+						)
+						.inspect(|details| {
+							log::info!("Scheduled egress: {:?}", details);
+						})
+						.inspect_err(|e| {
+							log::error!("Error scheduling egress: {:?}", e);
+						});
+				} else {
+					log::info!("Egress already bumped.");
+				}
 			},
 			genesis_hashes::PERSEVERANCE => {
 				log::info!("🧹 Housekeeping, removing stale aborted broadcasts");
