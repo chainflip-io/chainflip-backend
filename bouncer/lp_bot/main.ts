183fbea870fd33eacadc2af7fab72d1b283708a0
Jan Börner <jan@chainflip.io>
2025-03-14 11:17:19 +0100

    chore: added current status

--- bouncer/lp_bot/main.ts
+++ bouncer/lp_bot/main.ts
@@ -1,12 +1,13 @@
 #!/usr/bin/env -S pnpm tsx
-import { initializeLiquidityProviderBot, depositUsdcLiquidity, cancelAllOrders } from './lp_bot';
+import { initializeLiquidityProviderBot, depositUsdcLiquidity, cancelAllOrders, manageRangeOrder } from './lp_bot';
 import { globalLogger as logger } from '../shared/utils/logger';
 import { startSwapSimulator } from './swap_simulator';
-
+import { Asset } from '../shared/utils';
 
 const main = async () => {
     await cancelAllOrders();
     await depositUsdcLiquidity('100000');
+    await manageRangeOrder('Usdt', 1, 10, 1000);
 
     const [stateChainWsConnection, lpWsConnection] = initializeLiquidityProviderBot();
 
