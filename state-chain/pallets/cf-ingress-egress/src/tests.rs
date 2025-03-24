35f9e628865d295b99a2b6a3f5f9b84158401145
Jan Börner <jan@chainflip.io>
2025-03-24 12:03:22 +0100

    chore: refactored tests

--- state-chain/pallets/cf-ingress-egress/src/tests.rs
+++ state-chain/pallets/cf-ingress-egress/src/tests.rs
@@ -2980,70 +2980,69 @@ mod evm_transaction_rejection {
 
 	#[test]
 	fn rejecting_vault_swap() {
-		new_test_ext().execute_with(|| {
-			let tx_id = H256::from_str(
-				"0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
-			)
-			.unwrap();
+		let tx_id =
+			H256::from_str("0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef")
+				.unwrap();
 
-			assert_ok!(IngressEgress::mark_transaction_for_rejection(
-				OriginTrait::signed(BROKER),
-				tx_id,
-			));
+		new_test_ext()
+			.then_execute_with(|_| {
+				assert_ok!(IngressEgress::mark_transaction_for_rejection(
+					OriginTrait::signed(BROKER),
+					tx_id,
+				));
+			})
+			.then_execute_at_next_block(|_| {
+				assert_ok!(submit_vault_swap_request(
+					Asset::Eth,
+					Asset::Flip,
+					DEFAULT_DEPOSIT_AMOUNT,
+					Default::default(),
+					MockAddressConverter::to_encoded_address(ForeignChainAddress::Eth(
+						ALICE_ETH_ADDRESS
+					)),
+					None,
+					tx_id,
+					DepositDetails { tx_hashes: Some(vec![tx_id]) },
+					Beneficiary { account: BROKER, bps: 0 },
+					bounded_vec![],
+					ETH_REFUND_PARAMS,
+					None,
+					0
+				));
+				assert!(MockSwapRequestHandler::<Test>::get_swap_requests().is_empty());
 
-			assert_ok!(submit_vault_swap_request(
-				Asset::Eth,
-				Asset::Flip,
-				DEFAULT_DEPOSIT_AMOUNT,
-				Default::default(),
-				MockAddressConverter::to_encoded_address(ForeignChainAddress::Eth(
-					ALICE_ETH_ADDRESS
-				)),
-				None,
-				tx_id,
-				DepositDetails { tx_hashes: Some(vec![tx_id]) },
-				Beneficiary { account: BROKER, bps: 0 },
-				bounded_vec![],
-				ETH_REFUND_PARAMS,
-				None,
-				0
-			));
+				let scheduled_txs = ScheduledTransactionsForRejection::<Test, ()>::get();
 
-			assert_ok!(IngressEgress::mark_transaction_for_rejection(
-				OriginTrait::signed(BROKER),
-				tx_id,
-			));
-
-			assert!(MockSwapRequestHandler::<Test>::get_swap_requests().is_empty());
-
-			let scheduled_txs = ScheduledTransactionsForRejection::<Test, ()>::get();
-
-			assert_eq!(scheduled_txs.len(), 1);
-			assert_eq!(scheduled_txs[0].deposit_details.deposit_ids().unwrap(), vec![tx_id]);
-
-			IngressEgress::on_finalize(1);
-
-			assert!(FailedRejections::<Test, ()>::get().is_empty());
-
-			assert_has_matching_event!(
-				Test,
-				RuntimeEvent::IngressEgress(
-					crate::Event::<Test, ()>::TransactionRejectedByBroker { tx_id, .. }
-				) if tx_id == tx_id
-			);
-
-			let pending_api_calls = MockEgressBroadcaster::get_pending_api_calls();
-
-			assert_eq!(pending_api_calls.len(), 1);
-			let api_call = pending_api_calls[0].clone();
-
-			match api_call {
-				MockEthereumApiCall::RejectCall { deposit_details, deposit_fetch_id, .. } => {
-					assert_eq!(deposit_details.deposit_ids().unwrap(), vec![tx_id]);
-					assert!(deposit_fetch_id.is_none());
+				assert_eq!(scheduled_txs.len(), 1);
+				assert_eq!(scheduled_txs[0].deposit_details.deposit_ids().unwrap(), vec![tx_id]);
+			})
+			.then_process_blocks(1)
+			.then_process_events(|_, event| match event {
+				RuntimeEvent::IngressEgress(PalletEvent::TransactionRejectedByBroker {
+					tx_id,
+					..
+				}) if tx_id == tx_id => Some(()),
+				_ => {
+					panic!("Expected a TransactionRejectedByBroker event");
 				},
-				_ => panic!("Expected a RejectCall"),
-			}
-		});
+			})
+			.then_execute_with(|_| {
+				assert!(FailedRejections::<Test, ()>::get().is_empty());
+
+				let pending_api_calls = MockEgressBroadcaster::get_pending_api_calls();
+
+				assert_eq!(pending_api_calls.len(), 1);
+				let api_call = pending_api_calls[0].clone();
+
+				match api_call {
+					MockEthereumApiCall::RejectCall {
+						deposit_details, deposit_fetch_id, ..
+					} => {
+						assert_eq!(deposit_details.deposit_ids().unwrap(), vec![tx_id]);
+						assert!(deposit_fetch_id.is_none());
+					},
+					_ => panic!("Expected a RejectCall"),
+				}
+			});
 	}
 }
