4d57c86149fb1694e83d55b9d85c6080335594c9
Jan Börner <jan@chainflip.io>
2025-03-27 10:00:59 +0100

    chore: readded migration since we need it

--- state-chain/pallets/cf-ingress-egress/src/migrations.rs
+++ state-chain/pallets/cf-ingress-egress/src/migrations.rs
@@ -16,11 +16,11 @@
 
 use crate::Pallet;
 use cf_runtime_utilities::PlaceholderMigration;
-// use frame_support::migrations::VersionedMigration;
+use frame_support::migrations::VersionedMigration;
 
-// mod mandatory_refund_params;
+mod mandatory_refund_params;
 
 pub type PalletMigration<T, I> = (
-	// VersionedMigration<22, 23, mandatory_refund_params::Migration<T, I>, Pallet<T, I>, ()>,
+	VersionedMigration<22, 23, mandatory_refund_params::Migration<T, I>, Pallet<T, I>, ()>,
 	PlaceholderMigration<23, Pallet<T, I>>,
 );
