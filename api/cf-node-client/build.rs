1ebdfb9b6536d0ed855c84c60ab4e63be3d80bce
Daniel <daniel@chainflip.io>
2025-04-04 14:41:23 +0200

    chore: tabs -> spaces

--- api/cf-node-client/build.rs
+++ api/cf-node-client/build.rs
@@ -16,35 +16,35 @@ fn main() {
 	// include it verbatim instead.
 	let cf_static_runtime_content = format!(
 		r#"
-        #[subxt::subxt(
-            runtime_path = "{}",
-            substitute_type(
-                path = "primitive_types::U256",
-                with = "::subxt::utils::Static<sp_core::U256>"
-            ),
-            substitute_type(
-                path = "cf_chains::address::EncodedAddress",
-                with = "::subxt::utils::Static<cf_chains::address::EncodedAddress>"
-            ),
-            substitute_type(
-                path = "cf_primitives::chains::assets::any::Asset",
-                with = "::subxt::utils::Static<cf_primitives::chains::assets::any::Asset>"
-            ),
-            substitute_type(
-                path = "cf_primitives::chains::ForeignChain",
-                with = "::subxt::utils::Static<cf_primitives::chains::ForeignChain>"
-            ),
-            substitute_type(
+		#[subxt::subxt(
+			runtime_path = "{}",
+			substitute_type(
+				path = "primitive_types::U256",
+				with = "::subxt::utils::Static<sp_core::U256>"
+			),
+			substitute_type(
+				path = "cf_chains::address::EncodedAddress",
+				with = "::subxt::utils::Static<cf_chains::address::EncodedAddress>"
+			),
+			substitute_type(
+				path = "cf_primitives::chains::assets::any::Asset",
+				with = "::subxt::utils::Static<cf_primitives::chains::assets::any::Asset>"
+			),
+			substitute_type(
+				path = "cf_primitives::chains::ForeignChain",
+				with = "::subxt::utils::Static<cf_primitives::chains::ForeignChain>"
+			),
+			substitute_type(
 				path = "cf_primitives::SwapRequestId",
 				with = "::subxt::utils::Static<cf_primitives::SwapRequestId>"
 			),
-            substitute_type(
-                path = "cf_amm::common::Side",
-                with = "::subxt::utils::Static<cf_amm::common::Side>"
-            )
-        )]
-        pub mod cf_static_runtime {{}}
-    "#,
+			substitute_type(
+				path = "cf_amm::common::Side",
+				with = "::subxt::utils::Static<cf_amm::common::Side>"
+			)
+		)]
+		pub mod cf_static_runtime {{}}
+	"#,
 		wasm_path.to_str().expect("Path to wasm should be stringifiable")
 	);
 	let cf_static_runtime_path = Path::new(&out_dir).join("cf_static_runtime.rs");
