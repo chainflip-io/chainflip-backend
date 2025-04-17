0c7b132df477ac3194fbfba4b50906d40a656c6f
Daniel <daniel@chainflip.io>
2025-04-17 14:20:13 +0200

    chore: bump version to 1.10 and delete 1.9 migrations

--- state-chain/pallets/cf-ingress-egress/src/migrations.rs
+++ state-chain/pallets/cf-ingress-egress/src/migrations.rs
@@ -16,11 +16,5 @@
 
 use crate::Pallet;
 use cf_runtime_utilities::PlaceholderMigration;
-use frame_support::migrations::VersionedMigration;
 
-mod mandatory_refund_params;
-
-pub type PalletMigration<T, I> = (
-	VersionedMigration<22, 23, mandatory_refund_params::Migration<T, I>, Pallet<T, I>, ()>,
-	PlaceholderMigration<23, Pallet<T, I>>,
-);
+pub type PalletMigration<T, I> = (PlaceholderMigration<23, Pallet<T, I>>,);
