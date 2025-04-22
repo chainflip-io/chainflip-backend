2d6b67f6d7766da225e55469341b421bbd833b07
albert <allimos3@gmail.com>
2025-04-22 09:51:30 +0200

    chore: rebase and fix accordingly

--- state-chain/pallets/cf-ingress-egress/src/migrations/deposit_channel_details_migration.rs
+++ state-chain/pallets/cf-ingress-egress/src/migrations/deposit_channel_details_migration.rs
@@ -41,7 +41,7 @@ pub mod old {
 		pub expires_at: TargetChainBlockNumber<T, I>,
 		pub action: ChannelAction<T::AccountId>,
 		pub boost_fee: BasisPoints,
-		pub boost_status: BoostStatus<TargetChainAmount<T, I>>,
+		pub boost_status: BoostStatus<TargetChainAmount<T, I>, BlockNumberFor<T>>,
 	}
 
 	#[derive(Clone, PartialEq, Eq, Encode, Decode)]
