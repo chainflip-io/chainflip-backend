e93a8e58f9b1786204a62d8ef33cefd60c2f3647
albert <allimos3@gmail.com>
2025-04-22 09:57:34 +0200

    chore: fix swapping pallet version

--- state-chain/pallets/cf-swapping/src/migrations.rs
+++ state-chain/pallets/cf-swapping/src/migrations.rs
@@ -32,11 +32,11 @@ pub type PalletMigration<T> = (
 		<T as frame_system::Config>::DbWeight,
 	>,
 	VersionedMigration<
+		9,
 		10,
-		11,
 		swap_request_migration::Migration<T>,
 		Pallet<T>,
 		<T as frame_system::Config>::DbWeight,
 	>,
-	PlaceholderMigration<11, Pallet<T>>,
+	PlaceholderMigration<10, Pallet<T>>,
 );
