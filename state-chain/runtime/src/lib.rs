c889e25db7bd15e5f0f0de7a8a6cdb2af5f75da4
Martin Rieke <martin@chainflip.io>
2025-05-16 11:29:45 +0200

    bump version

--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -233,7 +233,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("chainflip-node"),
 	impl_name: create_runtime_str!("chainflip-node"),
 	authoring_version: 1,
-	spec_version: 1_09_02,
+	spec_version: 1_09_03,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 13,
