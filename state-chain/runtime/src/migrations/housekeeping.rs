7e3d1ff7e6a7a69a4e2237571d5a586c82a9d8fa
kylezs <zsembery.kyle@gmail.com>
2025-05-20 10:50:12 +0200

    chore: fetch and egress from channel

--- state-chain/runtime/src/migrations/housekeeping.rs
+++ state-chain/runtime/src/migrations/housekeeping.rs
@@ -14,9 +14,19 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::Runtime;
+use crate::{EthereumBroadcaster, Runtime};
 use cf_runtime_utilities::genesis_hashes;
 use frame_support::{traits::OnRuntimeUpgrade, weights::Weight};
+
+pub struct FetchAndEgressFromChannel;
+use crate::*;
+use cf_chains::{
+	assets::eth::Asset,
+	evm::{self, EvmFetchId, H256},
+	RejectCall,
+};
+use core::str::FromStr;
+
 #[cfg(feature = "try-runtime")]
 use sp_runtime::DispatchError;
 #[cfg(feature = "try-runtime")]
@@ -24,6 +34,7 @@ use sp_std::vec::Vec;
 
 pub mod reap_old_accounts;
 pub mod solana_remove_unused_channels_state;
+use pallet_cf_cfe_interface::{CfeEvents, RuntimeUpgradeEvents};
 
 pub type Migration = (
 	NetworkSpecificHousekeeping,
@@ -32,14 +43,53 @@ pub type Migration = (
 	solana_remove_unused_channels_state::SolanaRemoveUnusedChannelsState,
 );
 
+const REFUND_ADDRESS: &str = "0x39296AF2e64065D99Aaf57E03FF4d80eB6368A5b";
+const REFUND_AMOUNT: u128 = 308498000000;
+const CHANNEL_ADDRESS: &str = "0x52aa289646e5068185a30f79e95175f10ce6c48f";
+
 pub struct NetworkSpecificHousekeeping;
 
 impl OnRuntimeUpgrade for NetworkSpecificHousekeeping {
 	fn on_runtime_upgrade() -> Weight {
 		match genesis_hashes::genesis_hash::<Runtime>() {
-			genesis_hashes::BERGHAIN => {
-				log::info!("🧹 No housekeeping required for Berghain.");
-			},
+			genesis_hashes::BERGHAIN =>
+				if crate::VERSION.spec_version == 1_09_05 {
+					match <EthereumApi<EvmEnvironment> as RejectCall<Ethereum>>::new_unsigned(
+						evm::DepositDetails {
+							tx_hashes: Some(vec![H256::from_str(
+								"0x71ddf79e091b2af49bdd023a014d33038d71cea051342aea0dd714a7efccc375",
+							)
+							.unwrap()]),
+						},
+						EthereumAddress::from_str(REFUND_ADDRESS).unwrap(),
+						REFUND_AMOUNT,
+						Asset::Usdt,
+						Some(EvmFetchId::Fetch(
+							EthereumAddress::from_str(CHANNEL_ADDRESS).unwrap(),
+						)),
+					) {
+						Ok(reject_transaction) => {
+							let broadcast_id = EthereumBroadcaster::threshold_sign_and_broadcast(
+								reject_transaction,
+								None,
+								|_| None,
+							);
+							log::info!(
+								"Rejected transaction successfully broadcasted with id: {:?}",
+								broadcast_id
+							);
+
+							// Without doing this the events are cleared on_initialise and so the
+							// engine will never see them.
+							RuntimeUpgradeEvents::<Runtime>::put(CfeEvents::<Runtime>::take());
+						},
+						Err(e) => {
+							log::error!("Failed to reject transaction: {:?}", e);
+						},
+					}
+				} else {
+					log::info!("Runtime version is not 1.9.5, skipping migration.");
+				},
 			genesis_hashes::PERSEVERANCE => {
 				log::info!("🧹 No housekeeping required for Perseverance.");
 			},
