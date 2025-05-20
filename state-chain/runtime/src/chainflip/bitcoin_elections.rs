1231115d1ad6683d743ef4c63b4b4ed366d8c5c6
marcellorigotti <marcello@chainflip.io>
2025-05-20 11:01:08 +0200

    remove unnecessary pub visibility

--- state-chain/runtime/src/chainflip/bitcoin_elections.rs
+++ state-chain/runtime/src/chainflip/bitcoin_elections.rs
@@ -576,7 +576,7 @@ impl
 	}
 }
 
-pub(crate) const LIVENESS_CHECK_DURATION: BlockNumberFor<Runtime> = 10;
+const LIVENESS_CHECK_DURATION: BlockNumberFor<Runtime> = 10;
 
 // Channel expiry:
 // We need to process elections in order, even after a safe mode pause. This is to ensure channel
