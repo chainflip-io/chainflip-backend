3af919093b780f190afed1eb995effb99676699d
albert <allimos3@gmail.com>
2025-05-22 14:15:29 +0200

    chore: update Solana image and add feed addresses

--- bouncer/shared/utils.ts
+++ bouncer/shared/utils.ts
@@ -184,6 +184,10 @@ export function getContractAddress(chain: Chain, contract: string): string {
           return 'EWaGcrFXhf9Zq8yxSdpAa75kZmDXkRxaP17sYiL6UpZN';
         case 'USER_ADDRESS_LOOKUP_TABLE':
           return '4eUGPyr3krnw8tD3rL3UBXXdy1cx8Sf9HKVESZUAatqv';
+        case 'PRICE_FEED_BTC':
+          return 'GRZmvuxuxCXyrabSuMdqwbn53Bht9wDRMqitgL49nNFK';
+        case 'PRICE_FEED_ETH':
+          return '3Tnp6ZdsxcJzrG7W3xCcVxB8QhL2HXYwrFgeWe5vQ5Dp';
         default:
           throw new Error(`Unsupported contract: ${contract}`);
       }
