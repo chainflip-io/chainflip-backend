c5591f7699b10d54972616bad4d3f83ed24f7831
albert <allimos3@gmail.com>
2025-06-03 15:03:32 +0200

    chore: add Solana price feed setup

--- bouncer/shared/update_price_feed.ts
+++ bouncer/shared/update_price_feed.ts
@@ -1,21 +1,58 @@
-import { Asset } from '@chainflip/cli';
+import { BN } from '@polkadot/util';
+import { Asset, Chain } from '@chainflip/cli';
 import Web3 from 'web3';
 import { signAndSendTxEvm } from '../shared/send_evm';
-import { getContractAddress, getEvmEndpoint } from '../shared/utils';
-import { globalLogger } from '../shared/utils/logger';
+import { amountToFineAmount, getContractAddress, getEvmEndpoint } from '../shared/utils';
+import { Logger } from '../shared/utils/logger';
+import { PublicKey, Transaction, TransactionInstruction } from '@solana/web3.js';
+import { signAndSendTxSol } from './send_sol';
 
-// For now only using Ethereum price feeds
-export async function updateEvmPriceFeed(asset: Asset, price: string) {
+// All price feeds are using 8 decimals
+const PRICE_FEED_DECIMALS = 8;
+
+export async function updatePriceFeed(logger: Logger, chain: Chain, asset: Asset, price: string) {
   if (asset !== 'BTC' && asset !== 'ETH') {
     throw new Error(`Unsupported price feed asset: ${asset}`);
   }
 
-  const evmClient = new Web3(getEvmEndpoint('Ethereum'));
+  // For now only using Ethereum price feeds
+  switch (chain) {
+    case 'Ethereum':
+      await updateEvmPriceFeed(logger, 'Ethereum', asset, price);
+    case 'Solana':
+      return updateSolanaPriceFeed(logger, asset, price);
+    default:
+      throw new Error(`Unsupported chain for price feed update: ${chain}`);
+  }
+}
 
-  const priceFeedAddress = getContractAddress('Ethereum', `PRICE_FEED_${asset}`);
+async function updateSolanaPriceFeed(logger: Logger, asset: Asset, price: string) {
+  const finePrice = amountToFineAmount(price, PRICE_FEED_DECIMALS);
+  const priceFeedMockAddress = getContractAddress('Solana', `PRICE_FEED_MOCK`);
+  const priceFeedAddress = new PublicKey(getContractAddress('Solana', `PRICE_FEED_${asset}`));
+
+  const updatePriceDiscriminator = Buffer.from([61, 34, 117, 155, 75, 34, 123, 208]);
+
+  const priceBN = new BN(finePrice);
+  const priceBuffer = priceBN.toBuffer('le', 16);
+
+  let tx = new Transaction().add(
+    new TransactionInstruction({
+      data: Buffer.concat([Buffer.from(updatePriceDiscriminator), priceBuffer]),
+      keys: [{ pubkey: priceFeedAddress, isSigner: false, isWritable: true }],
+      programId: new PublicKey(priceFeedMockAddress),
+    }),
+  );
+  await signAndSendTxSol(logger, tx);
+}
+
+async function updateEvmPriceFeed(logger: Logger, chain: Chain, asset: Asset, price: string) {
+  const evmClient = new Web3(getEvmEndpoint(chain));
+  const priceFeedAddress = getContractAddress(chain, `PRICE_FEED_${asset}`);
+  const finePrice = amountToFineAmount(price, PRICE_FEED_DECIMALS);
 
   // Not adding it in the contract interfaces folder because these are functions added in
-  // our mock, while that interface is the real one declared by Chainlink.
+  // our mock, while that interface is the real one.
   const PRICE_FEED_GOV_ABI = [
     {
       inputs: [
@@ -88,7 +125,6 @@ export async function updateEvmPriceFeed(asset: Asset, price: string) {
     PRICE_FEED_GOV_ABI as any,
     priceFeedAddress,
   );
-  console.log('Price', price);
-  const txData = priceFeedContract.methods.updatePrice(price).encodeABI();
-  await signAndSendTxEvm(globalLogger, 'Ethereum', priceFeedAddress, '0', txData);
+  const txData = priceFeedContract.methods.updatePrice(finePrice).encodeABI();
+  await signAndSendTxEvm(logger, chain, priceFeedAddress, '0', txData);
 }
