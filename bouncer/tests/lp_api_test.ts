6760873d3f9ca5bdbf049cc3d7844589a6048d57
nmammeri <nmammeri@gmail.com>
2025-06-16 13:39:49 +0200

    test: fix  testLiquidityDepositLegacy bouncer test

--- bouncer/tests/lp_api_test.ts
+++ bouncer/tests/lp_api_test.ts
@@ -97,13 +97,10 @@ async function testLiquidityDepositLegacy(logger: Logger) {
     },
   ).event;
 
-  // InBlock waiting is disabled for lp_request_liquidity_deposit_address, because of finality bug
   await assert.rejects(
     () => lpApiRpc(logger, `lp_request_liquidity_deposit_address`, [testRpcAsset, 'InBlock']),
-    (e: Error) =>
-      e.message ===
-      'InBlock waiting is not allowed for this method. Use request_liquidity_deposit_address_v2 instead.',
-    `Unexpected lp_request_liquidity_deposit_address result. Expected to return an error because InBlock waiting is not supported`,
+    (e: Error) => e.message.includes('InBlock waiting is not allowed for this method'),
+    `Unexpected lp_request_liquidity_deposit_address result. Expected to return an error because InBlock waiting is not allowed`,
   );
 
   const liquidityDepositAddress = (
