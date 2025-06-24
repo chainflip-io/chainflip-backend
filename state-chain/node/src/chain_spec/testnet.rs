50e4683cfb9075db494e2dc01dd2666a9b052910
Albert Llimos <53186777+albert-llimos@users.noreply.github.com>
2025-06-24 16:44:26 +0200

    chore: update chain specs' safety margins (#5951)
    * chore: update safety margins
    
    * chore: restore btc

--- state-chain/node/src/chain_spec/testnet.rs
+++ state-chain/node/src/chain_spec/testnet.rs
@@ -438,6 +438,6 @@ pub fn extra_accounts() -> Vec<(AccountId, AccountRole, FlipBalance, Option<Vec<
 }
 
 pub const BITCOIN_SAFETY_MARGIN: u64 = 2;
-pub const ETHEREUM_SAFETY_MARGIN: u64 = 6;
+pub const ETHEREUM_SAFETY_MARGIN: u64 = 2;
 pub const ARBITRUM_SAFETY_MARGIN: u64 = 1;
 pub const SOLANA_SAFETY_MARGIN: u64 = 1; //todo
