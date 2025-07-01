c51ed42d07c4865622e29169180454718760c20a
Maxim Urschumzew <maxu@chainflip.io>
2025-07-01 16:44:26 +0200

    chore: remove unused code.

--- state-chain/pallets/cf-elections/src/electoral_systems/block_witnesser/helpers.rs
+++ state-chain/pallets/cf-elections/src/electoral_systems/block_witnesser/helpers.rs
@@ -1,57 +1,6 @@
 use core::{iter::Step, ops::RangeInclusive};
 use sp_std::collections::{btree_map::BTreeMap, btree_set::BTreeSet};
 
-pub trait Implies {
-	fn implies(self, other: Self) -> Self;
-}
-impl Implies for bool {
-	fn implies(self, other: Self) -> Self {
-		// false implies false (false <= false)
-		// false implies true  (false <= true)
-		// true implies true   (true <= true)
-		// true !implies false (! true <= false)
-		self <= other
-	}
-}
-
-// ------------ my helpers ---------------
-pub trait KeySet<K> {
-	fn key_set(&self) -> BTreeSet<K>;
-}
-
-impl<K: Ord + Clone, V> KeySet<K> for BTreeMap<K, V> {
-	fn key_set(&self) -> BTreeSet<K> {
-		self.keys().map(Clone::clone).collect()
-	}
-}
-
-pub trait With<K> {
-	fn with(self, k: K) -> Self;
-	fn without(self, k: K) -> Self;
-}
-
-impl<K: Ord> With<K> for BTreeSet<K> {
-	fn with(mut self, k: K) -> Self {
-		self.insert(k);
-		self
-	}
-
-	fn without(mut self, k: K) -> Self {
-		self.remove(&k);
-		self
-	}
-}
-
-pub trait IntoSet<X> {
-	fn into_set(self) -> BTreeSet<X>;
-}
-
-impl<N: Step + Ord> IntoSet<N> for RangeInclusive<N> {
-	fn into_set(self) -> BTreeSet<N> {
-		BTreeSet::from_iter(self)
-	}
-}
-
 #[macro_export]
 macro_rules! prop_do {
     (let $var:pat in $expr:expr; $($expr2:tt)+) => {
