e9f95fb74e64a6f42745e86cfb3ef3834c440d61
Maxim Urschumzew <maxu@chainflip.io>
2025-07-03 15:49:35 +0200

    fix: use correct solana addresses.

--- state-chain/runtime/src/lib.rs
+++ state-chain/runtime/src/lib.rs
@@ -1166,6 +1166,7 @@ impl pallet_cf_elections::Config for Runtime {
 	type ElectoralSystemRunner = chainflip::generic_elections::GenericElectoralSystemRunner;
 	type WeightInfo = pallet_cf_elections::weights::PalletWeight<Runtime>;
 	type CreateGovernanceElectionHook = chainflip::generic_elections::GenericElectionHooks;
+	type ElectoralEvents = ();
 }
 
 impl pallet_cf_trading_strategy::Config for Runtime {
