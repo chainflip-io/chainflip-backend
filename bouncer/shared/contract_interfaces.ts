d7e8aac8750a30c3f50fce003e727fb90848fa9d
albert <allimos3@gmail.com>
2025-07-07 16:43:03 +0200

    chore: update to EVM image 1.3.0

--- bouncer/shared/contract_interfaces.ts
+++ bouncer/shared/contract_interfaces.ts
@@ -12,7 +12,7 @@ function loadContractCached(abiPath: string) {
     return cached;
   };
 }
-const CF_ETH_CONTRACT_ABI_TAG = 'v1.3.0-test3';
+const CF_ETH_CONTRACT_ABI_TAG = 'v1.3.0';
 const CF_SOL_PROGRAM_IDL_TAG = 'v1.2.2';
 export const getErc20abi = loadContractCached(
   '../contract-interfaces/eth-contract-abis/IERC20.json',
