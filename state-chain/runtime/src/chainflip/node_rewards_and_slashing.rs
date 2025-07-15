9b29e082493e7d8755196063c0fcef7da764f880
Roy Yang <roy@chainflip.io>
2025-07-15 21:38:34 +1200

    fixed unit tests in Funding pallet

--- state-chain/runtime/src/chainflip/node_rewards_and_slashing.rs
+++ state-chain/runtime/src/chainflip/node_rewards_and_slashing.rs
@@ -1,20 +1,20 @@
-use crate::Flip;
-/// Copyright 2025 Chainflip Labs GmbH
-///
-/// Licensed under the Apache License, Version 2.0 (the "License");
-/// you may not use this file except in compliance with the License.
-/// You may obtain a copy of the License at
-///
-///     http://www.apache.org/licenses/LICENSE-2.0
-///
-/// Unless required by applicable law or agreed to in writing, software
-/// distributed under the License is distributed on an "AS IS" BASIS,
-/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-/// See the License for the specific language governing permissions and
-/// limitations under the License.
-///
-/// SPDX-License-Identifier: Apache-2.0
-use crate::{AccountId, Authorship, BlockNumber, Emissions, Runtime, System, Validator};
+// Copyright 2025 Chainflip Labs GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::{AccountId, Authorship, BlockNumber, Emissions, Flip, Runtime, System, Validator};
 use cf_primitives::{AssetAmount, FlipBalance};
 use cf_traits::{Bid, EpochInfo, Issuance, RewardsDistribution, Slashing};
 use frame_support::{
