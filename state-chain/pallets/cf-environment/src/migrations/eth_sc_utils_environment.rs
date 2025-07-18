c18685365ddc3d79992edaee31417e45a9afd0aa
Albert Llimos <53186777+albert-llimos@users.noreply.github.com>
2025-07-18 11:33:31 +0200

    chore: insert Sc Utils addresses and image (#6010)
    * chore: update image to v1.3.1 like main, updating SC_UTILS address
    
    * chore: update to v1.3.2
    
    * chore: update eth contract-interfaces to v1.3.2
    
    * chore: insert deployed contract's addresses

--- state-chain/pallets/cf-environment/src/migrations/eth_sc_utils_environment.rs
+++ state-chain/pallets/cf-environment/src/migrations/eth_sc_utils_environment.rs
@@ -12,11 +12,11 @@ impl<T: Config<Hash = H256>> UncheckedOnRuntimeUpgrade for EthScUtilsEnvironment
 		let eth_sc_utils_address: EvmAddress =
 			match cf_runtime_utilities::genesis_hashes::genesis_hash::<T>() {
 				cf_runtime_utilities::genesis_hashes::BERGHAIN =>
-					hex_literal::hex!("0000000000000000000000000000000000000000").into(), /* TODO: To update */
+					hex_literal::hex!("13Ad793E7B75eaaCee34b69792552f086b301380").into(),
 				cf_runtime_utilities::genesis_hashes::PERSEVERANCE =>
-					hex_literal::hex!("0000000000000000000000000000000000000000").into(), /* TODO: To update */
+					hex_literal::hex!("C191c202fdc308F54fF815fb3309eCd052E75D73").into(),
 				cf_runtime_utilities::genesis_hashes::SISYPHOS =>
-					hex_literal::hex!("0000000000000000000000000000000000000000").into(), /* TODO: To update */
+					hex_literal::hex!("7c08ea651dA70239DA8cb87A5913c3579Ba9F6fE").into(),
 				_ => hex_literal::hex!("7a2088a1bFc9d81c55368AE168C2C02570cB814F").into(),
 			};
 		EthereumScUtilsAddress::<T>::put(eth_sc_utils_address);
@@ -32,19 +32,19 @@ impl<T: Config<Hash = H256>> UncheckedOnRuntimeUpgrade for EthScUtilsEnvironment
 			cf_runtime_utilities::genesis_hashes::BERGHAIN => {
 				assert_eq!(
 					EthereumScUtilsAddress::<T>::get(),
-					hex_literal::hex!("0000000000000000000000000000000000000000").into() /* TODO: To update */
+					hex_literal::hex!("13Ad793E7B75eaaCee34b69792552f086b301380").into()
 				);
 			},
 			cf_runtime_utilities::genesis_hashes::PERSEVERANCE => {
 				assert_eq!(
 					EthereumScUtilsAddress::<T>::get(),
-					hex_literal::hex!("0000000000000000000000000000000000000000").into() /* TODO: To update */
+					hex_literal::hex!("C191c202fdc308F54fF815fb3309eCd052E75D73").into()
 				);
 			},
 			cf_runtime_utilities::genesis_hashes::SISYPHOS => {
 				assert_eq!(
 					EthereumScUtilsAddress::<T>::get(),
-					hex_literal::hex!("0000000000000000000000000000000000000000").into() /* TODO: To update */
+					hex_literal::hex!("7c08ea651dA70239DA8cb87A5913c3579Ba9F6fE").into()
 				);
 			},
 			_ => {
