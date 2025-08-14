15926f8242ddf99298fccff785ddff6f073f50be
albert <allimos3@gmail.com>
2025-08-14 15:51:25 +0200

    chore: fix evm_deposits conflicts

--- bouncer/tests/evm_deposits.ts
+++ bouncer/tests/evm_deposits.ts
@@ -2,6 +2,7 @@ import Web3 from 'web3';
 import { InternalAsset as Asset } from '@chainflip/cli';
 import { doPerformSwap, requestNewSwap } from 'shared/perform_swap';
 import { prepareSwap, testSwap } from 'shared/swapping';
+import BigNumber from 'bignumber.js';
 import { Keyring } from '@polkadot/api';
 import {
   observeFetch,
@@ -18,6 +19,7 @@ import {
   TransactionOrigin,
   stateChainAssetFromAsset,
   amountToFineAmountBigInt,
+  createEvmWalletAndFund,
 } from 'shared/utils';
 import { signAndSendTxEvm } from 'shared/send_evm';
 import { getCFTesterAbi, getEvmVaultAbi } from 'shared/contract_interfaces';
@@ -181,23 +183,99 @@ async function testDoubleDeposit(parentLogger: Logger, sourceAsset: Asset, destA
       SwapRequestType.Regular,
     );
 
-    await send(logger, sourceAsset, swapParams.depositAddress, defaultAssetAmounts(sourceAsset));
+    await send(logger, sourceAsset, swapParams.depositAddress);
     await swapRequestedHandle;
   }
 
   // Do another deposit. Regardless of the fetch having been broadcasted or not, another swap
   // should be scheduled when we deposit again.
-  {
-    const swapRequestedHandle = observeSwapRequested(
-      logger,
-      sourceAsset,
-      destAsset,
-      { type: TransactionOrigin.DepositChannel, channelId: swapParams.channelId },
-      SwapRequestType.Regular,
-    );
+  const swapRequestedHandle = observeSwapRequested(
+    logger,
+    sourceAsset,
+    destAsset,
+    { type: TransactionOrigin.DepositChannel, channelId: swapParams.channelId },
+    SwapRequestType.Regular,
+  );
 
-    await send(logger, sourceAsset, swapParams.depositAddress, defaultAssetAmounts(sourceAsset));
-    await swapRequestedHandle;
+  await send(logger, sourceAsset, swapParams.depositAddress);
+  await swapRequestedHandle;
+}
+
+async function testEvmLegacyCfParametersVaultSwap(parentLogger: Logger) {
+  const logger = parentLogger.child({ tag: 'test' });
+
+  const sourceAsset = 'ArbEth';
+  const srcChain = 'Arbitrum';
+  const evmWallet = await createEvmWalletAndFund(logger, sourceAsset);
+  const web3 = new Web3(getEvmEndpoint(srcChain));
+
+  // Hardcoded payload obtained encoding a Vault Swap with the old Encoding
+  const vaultSwapDetailsArray = [
+    // https://scan.chainflip.io/swaps/583409
+    // https://etherscan.io/tx/0x5cbff756754d7c2b1c935609eb9ca9681480a50c55f946ef45006f89f885e5af#internal
+    {
+      chain: srcChain,
+      calldata:
+        '0xdd68734500000000000000000000000000000000000000000000000000000000000000050000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000900000000000000000000000000000000000000000000000000000000000000c00000000000000000000000000000000000000000000000000000000000000020edab591a28ba82b8d2e86c1e3e3548fe1007fb99ef8a46b490c728469cfe928d000000000000000000000000000000000000000000000000000000000000005e009600000094d29e656ad719b348488c516c5ef45a8bb894887cd2d9679dca31c70cb7f5a747000000000000000000000000000000000000000000e0a3208a8748c830691a567b66a9b1d93b93aba308af9004c0c4a20569f30d070000000000',
+      value: '0x2E4D8EE1F032000',
+      to: getContractAddress(srcChain, 'VAULT'),
+      broker: 'cFNx21kQWmr9wsqq29zWM7RpDBKv4bctudEUE6J22Hd4NUUHR',
+    },
+    // Example with CCM from https://docs.chainflip.io/swapping/integrations/advanced/vault-swaps/evm#1-request-the-encoded-parameters-via-rpc
+    {
+      chain: srcChain,
+      calldata:
+        '0x07933dd2000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000c00000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000003e800000000000000000000000000000000000000000000000000000000000001400000000000000000000000000000000000000000000000000000000000000014cf0871027a5f984403aefd2fb22831d4bebc11ef00000000000000000000000000000000000000000000000000000000000000000000000000000000000000080011223344556677000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000006000000a00000000cf0871027a5f984403aefd2fb22831d4bebc11ef000000000000000000000000000000000000000000000000000000000000000000009059e6d854b769a505d01148af212bf8cb7f8469a7153edce8dcaedd9d299125000000',
+      value: '0x3e8',
+      to: getContractAddress(srcChain, 'VAULT'),
+      broker: 'cFHtDi8T8QVBoYpxWYkB8up3XAyEyey5bAos3uBwMjz9TtZs1',
+    },
+    // Example encoded with previous version of localnet (1.9).
+    {
+      chain: srcChain,
+      calldata:
+        '0xdd68734500000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000000000000000000000000000000000000c00000000000000000000000000000000000000000000000000000000000000014ecae5ac7046ebd3d8c811f82dfcef449caf4df00000000000000000000000000000000000000000000000000000000000000000000000000000000000000005e00000000002988897cb44670c37fca6998849f884dfff8da6600009c584c491ff27f16d24b8a61616b1987cabb0d46d4859c4ac6347d0e000000009059e6d854b769a505d01148af212bf8cb7f8469a7153edce8dcaedd9d2991250100000000',
+      value: '0x3e8',
+      to: getContractAddress(srcChain, 'VAULT'),
+    },
+  ];
+
+  for (const vaultSwapDetails of vaultSwapDetailsArray) {
+    const tx = {
+      to: vaultSwapDetails.to,
+      data: vaultSwapDetails.calldata,
+      value: new BigNumber(vaultSwapDetails.value.slice(2), 16).toString(),
+      gas: srcChain === 'Arbitrum' ? 32000000 : 5000000,
+    };
+
+    const signedTx = await web3.eth.accounts.signTransaction(tx, evmWallet.privateKey);
+    const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction as string);
+
+    logger.info(`Vault swap transaction sent with hash: ${receipt.transactionHash}`);
+
+    const depositFinalisedEvent = observeEvent(
+      logger,
+      `${chainFromAsset(sourceAsset).toLowerCase()}IngressEgress:DepositFinalised`,
+      {
+        test: (event) =>
+          event.data.originType === 'Vault' &&
+          event.data.depositDetails.txHashes[0] === receipt.transactionHash,
+      },
+    ).event;
+
+    // The swap will be refunded because the mainnet broker doesn't match the testnet broker
+    // but the swap is observed correctly.
+    const unknownBrokerEvent = vaultSwapDetails.broker
+      ? observeEvent(
+          logger,
+          `${chainFromAsset(sourceAsset).toLowerCase()}IngressEgress:UnknownBroker`,
+          {
+            test: (event) => event.data.brokerId === vaultSwapDetails.broker,
+          },
+        ).event
+      : Promise.resolve();
+
+    await Promise.all([depositFinalisedEvent, unknownBrokerEvent]);
   }
 }
 
@@ -299,6 +377,7 @@ export async function testEvmDeposits(testContext: TestContext) {
     noDuplicatedWitnessingTest,
     multipleTxSwapsTest,
     doubleDepositTests,
+    testEvmLegacyCfParametersVaultSwap(testContext.logger),
     testEncodingCfParameters,
   ]);
 }
