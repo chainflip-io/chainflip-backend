101c773ba4940435936f1e82a2f64ff25b535af1
albert <allimos3@gmail.com>
2025-08-15 12:32:31 +0200

    chore: fix sed command

--- bouncer/tests/delegate_flip.ts
+++ bouncer/tests/delegate_flip.ts
@@ -92,7 +92,6 @@ async function testDelegate(parentLogger: Logger) {
       const txMatch = event.data.txHash === receipt.transactionHash;
       const amountMatch = event.data.fundsAdded.replace(/,/g, '') === amount.toString();
       const accountIdMatch = evmToScAddress(whalePubkey) === event.data.accountId;
-      logger.info("Fund event", event.data, "txMatch: " + txMatch + ", amountMatch: " + amountMatch + ", accountIdMatch: " + accountIdMatch);
       return txMatch && amountMatch && accountIdMatch;
     },
   }).event;
@@ -100,7 +99,6 @@ async function testDelegate(parentLogger: Logger) {
     test: (event) => {
       const txMatch = event.data.ethTxHash === receipt.transactionHash;
       const operatorMatch = event.data.scCall.Delegation.Delegate.operator === operator.address;
-      logger.info("SCCallExecuted event", event.data, "txMatch: " + txMatch + ", operatorMatch: " + operatorMatch);
       return txMatch && operatorMatch;
     },
   }).event;
@@ -108,7 +106,6 @@ async function testDelegate(parentLogger: Logger) {
     test: (event) => {
       const delegatorMatch = event.data.delegator === evmToScAddress(whalePubkey);
       const operatorMatch = event.data.operator === operator.address;
-      logger.info("Delegated event", event.data, "delegatorMatch: " + delegatorMatch + ", operatorMatch: " + operatorMatch);
       return delegatorMatch && operatorMatch;
     },
   }).event;
