39bf3acf884db103214b13bc7b14d03243240eb9
Daniel <daniel@chainflip.io>
2025-08-18 15:33:30 +0200

    feat: replace CurrentEpochDelegations with DelegationSnapshots storage

--- state-chain/pallets/cf-validator/src/delegation.rs
+++ state-chain/pallets/cf-validator/src/delegation.rs
@@ -1,6 +1,6 @@
-use crate::Config;
+use crate::{Config, OperatorSettingsLookup, ValidatorIdOf};
 use codec::{Decode, Encode, MaxEncodedLen};
-use frame_support::{sp_runtime::traits::Zero, RuntimeDebugNoBound};
+use frame_support::RuntimeDebugNoBound;
 use scale_info::TypeInfo;
 use serde::{Deserialize, Serialize};
 use sp_std::collections::btree_map::BTreeMap;
@@ -61,20 +61,45 @@ pub struct OperatorSettings {
 #[derive(Clone, PartialEq, Eq, Default, Encode, Decode, TypeInfo, RuntimeDebugNoBound)]
 #[scale_info(skip_type_params(T))]
 pub struct DelegationSnapshot<T: Config> {
+	/// Map of validator accounts to their bid amounts.
+	pub validators: BTreeMap<ValidatorIdOf<T>, T::Amount>,
 	/// Map of delegator accounts to their bid amounts.
 	pub delegators: BTreeMap<T::AccountId, T::Amount>,
-	/// Map of validator accounts to their bid amounts.
-	pub validators: BTreeMap<T::AccountId, T::Amount>,
 	/// Operator fee at time of snapshot creation.
 	pub delegation_fee_bps: u32,
 }
 
 impl<T: Config> DelegationSnapshot<T> {
-	pub fn average_validator_bid(&self) -> T::Amount {
-		if self.validators.is_empty() {
-			return T::Amount::zero();
+	pub fn init(operator: &T::AccountId) -> Self {
+		Self {
+			delegators: Default::default(),
+			validators: Default::default(),
+			delegation_fee_bps: OperatorSettingsLookup::<T>::get(operator)
+				.map(|settings| settings.fee_bps)
+				.unwrap_or(0),
 		}
-		let total: T::Amount = self.validators.values().copied().sum();
-		total / T::Amount::from(self.validators.len() as u32)
+	}
+
+	pub fn total_validator_bid(&self) -> T::Amount {
+		self.validators.values().copied().sum()
+	}
+
+	pub fn total_delegator_bid(&self) -> T::Amount {
+		self.delegators.values().copied().sum()
+	}
+
+	pub fn total_available_bid(&self) -> T::Amount {
+		self.total_validator_bid() + self.total_delegator_bid()
+	}
+
+	pub fn effective_validator_bids(&self) -> BTreeMap<ValidatorIdOf<T>, T::Amount> {
+		if self.validators.is_empty() {
+			return Default::default();
+		}
+		let avg_bid = self.total_available_bid() / T::Amount::from(self.validators.len() as u32);
+		self.validators
+			.iter()
+			.map(|(validator, _)| (validator.clone(), avg_bid))
+			.collect()
 	}
 }
