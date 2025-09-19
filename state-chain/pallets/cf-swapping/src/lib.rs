46a97fd02e8e56b96783d887bc4a460cab5530b1
marcellorigotti <marcello@chainflip.io>
2025-09-19 14:33:49 +0200

    use price of GAS_ASSET

--- state-chain/pallets/cf-swapping/src/lib.rs
+++ state-chain/pallets/cf-swapping/src/lib.rs
@@ -2773,7 +2773,7 @@ pub mod pallet {
 			.and_then(|amount| C::ChainAmount::try_from(amount).ok())
 			.unwrap_or_else(|| {
 				log::warn!("Unable to calculate input amount required for gas of {required_gas:?} for input asset ${input_asset:?}. Estimating the input amount based on either oracle price or reference price.");
-				let oracle_price = T::PriceFeedApi::get_price(input_asset.into());
+				let oracle_price = T::PriceFeedApi::get_price(C::GAS_ASSET.into());
 				let price = if oracle_price.as_ref().is_some_and(|price| !price.stale) {
 					Some(oracle_price.unwrap().price)
 				} else {
