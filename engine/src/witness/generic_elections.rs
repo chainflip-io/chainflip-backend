2cb539162677f0bed39b5f86bc68781c01504158
marcellorigotti <marcello@chainflip.io>
2025-09-30 17:25:37 +0200

    fix rebase

--- engine/src/witness/generic_elections.rs
+++ engine/src/witness/generic_elections.rs
@@ -20,13 +20,11 @@ use crate::{
 		cached_rpc::AddressCheckerRetryRpcApiWithResult,
 		rpc::{address_checker::PriceFeedData as EthPriceFeedData, EvmRpcSigningClient},
 	},
-	sol::retry_rpc::SolRetryRpcClient,
 	state_chain_observer::client::{
 		chain_api::ChainApi, electoral_api::ElectoralApi,
 		extrinsic_api::signed::SignedExtrinsicApi, storage_api::StorageApi,
 	},
-	witness::sol::oracle_witnessing::get_price_feeds,
-	EvmCachingClient,
+	EvmCachingClient, EvmRetryRpcClient,
 };
 use anyhow::{anyhow, Result};
 use cf_utilities::task_scope::{self, Scope};
@@ -40,22 +38,8 @@ use pallet_cf_elections::{
 	},
 	VoteOf,
 };
-use sol_prim::program_instructions::PriceFeedData as SolPriceFeedData;
 use sp_core::U256;
 use state_chain_runtime::chainflip::generic_elections::*;
-
-use crate::{
-	elections::voter_api::{CompositeVoter, VoterApi},
-	evm::{
-		retry_rpc::{address_checker::AddressCheckerRetryRpcApi, EvmRetryRpcClient},
-		rpc::{address_checker::PriceFeedData as EthPriceFeedData, EvmRpcSigningClient},
-	},
-	state_chain_observer::client::{
-		chain_api::ChainApi, electoral_api::ElectoralApi,
-		extrinsic_api::signed::SignedExtrinsicApi, storage_api::StorageApi,
-	},
-};
-use anyhow::{anyhow, Result};
 
 /// IMPORTANT: These strings have to match with the price feed "description" as returned by
 /// chainlink.
@@ -74,7 +58,7 @@ pub fn asset_pair_from_description(description: String) -> Option<ChainlinkAsset
 #[derive(Clone)]
 struct OraclePriceVoter {
 	arb_client: EvmRetryRpcClient<EvmRpcSigningClient>,
-	eth_client: EvmRetryRpcClient<EvmRpcSigningClient>,
+	eth_client: EvmCachingClient<EvmRpcSigningClient>,
 }
 
 #[derive(Debug, Clone)]
@@ -219,7 +203,7 @@ use std::{collections::BTreeMap, sync::Arc};
 pub async fn start<StateChainClient>(
 	scope: &Scope<'_, anyhow::Error>,
 	arb_client: EvmRetryRpcClient<EvmRpcSigningClient>,
-	eth_client: EvmRetryRpcClient<EvmRpcSigningClient>,
+	eth_client: EvmCachingClient<EvmRpcSigningClient>,
 	state_chain_client: Arc<StateChainClient>,
 ) -> Result<()>
 where
