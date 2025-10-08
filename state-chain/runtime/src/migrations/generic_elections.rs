3aae4e092574849566bb4bbb5cdc6f258b90806a
Maxim Urschumzew <maxu@chainflip.io>
2025-10-08 13:01:10 +0200

    fix: initialize generic_elections if they're not initialized.

--- state-chain/runtime/src/migrations/generic_elections.rs
+++ state-chain/runtime/src/migrations/generic_elections.rs
@@ -31,6 +31,35 @@ impl OnRuntimeUpgrade for Migration {
 	}
 
 	fn on_runtime_upgrade() -> Weight {
+		// we have to initialize the elections pallet if we're upgrading from version 1.10.x (this
+		// only happens in the upgrade-test in CI).
+		if pallet_cf_elections::Status::<Runtime, ()>::get().is_none() {
+			let chainlink_oracle_price_settings = ChainlinkOraclePriceSettings {
+				arb_address_checker: hex_literal::hex!("9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0")
+					.into(),
+				arb_oracle_feeds: vec![
+					hex_literal::hex!("a85233C63b9Ee964Add6F2cffe00Fd84eb32338f").into(),
+					hex_literal::hex!("4A679253410272dd5232B3Ff7cF5dbB88f295319").into(),
+					hex_literal::hex!("7a2088a1bFc9d81c55368AE168C2C02570cB814F").into(),
+					hex_literal::hex!("09635F643e140090A9A8Dcd712eD6285858ceBef").into(),
+					hex_literal::hex!("c5a5C42992dECbae36851359345FE25997F5C42d").into(),
+				],
+				eth_address_checker: hex_literal::hex!("e7f1725E7734CE288F8367e1Bb143E90bb3F0512")
+					.into(),
+				eth_oracle_feeds: vec![
+					hex_literal::hex!("322813Fd9A801c5507c9de605d63CEA4f2CE6c44").into(),
+					hex_literal::hex!("a85233C63b9Ee964Add6F2cffe00Fd84eb32338f").into(),
+					hex_literal::hex!("4A679253410272dd5232B3Ff7cF5dbB88f295319").into(),
+					hex_literal::hex!("7a2088a1bFc9d81c55368AE168C2C02570cB814F").into(),
+					hex_literal::hex!("09635F643e140090A9A8Dcd712eD6285858ceBef").into(),
+				],
+			};
+
+			let _result = pallet_cf_elections::Pallet::<Runtime, ()>::internally_initialize(
+				generic_elections::initial_state(chainlink_oracle_price_settings),
+			);
+		}
+
 		if crate::VERSION.spec_version == 1_11_11 {
 			let result = pallet_cf_elections::Pallet::<Runtime, ()>::update_settings(
 				pallet_cf_governance::RawOrigin::GovernanceApproval.into(),
