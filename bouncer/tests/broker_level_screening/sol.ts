311814268cf0bd7915cad2e482127220cca6b18c
Maxim Urschumzew <maxu@chainflip.io>
2025-10-17 18:17:16 +0700

    feat: add BLS tests for all solana assets and swap types.

--- bouncer/tests/broker_level_screening/sol.ts
+++ bouncer/tests/broker_level_screening/sol.ts
@@ -1,44 +1,24 @@
-import axios from 'axios';
-import { Chain, Chains, InternalAsset } from '@chainflip/cli';
-import Web3 from 'web3';
-import { btcClient, sendBtc, sendBtcTransactionWithParent } from 'shared/send_btc';
+import { Chains, InternalAsset } from '@chainflip/cli';
 import {
   newAssetAddress,
   sleep,
-  handleSubstrateError,
-  chainGasAsset,
-  lpMutex,
   createStateChainKeypair,
-  isWithinOnePercent,
-  amountToFineAmountBigInt,
-  getEvmEndpoint,
-  chainContractId,
   chainFromAsset,
   ingressEgressPalletForChain,
   observeBalanceIncrease,
   observeCcmReceived,
   observeFetch,
-  btcClientMutex,
-  getBtcClient,
 } from 'shared/utils';
-import { getChainflipApi, observeEvent } from 'shared/utils/substrate';
-import Keyring from 'polkadot/keyring';
+import { observeEvent } from 'shared/utils/substrate';
 import { requestNewSwap } from 'shared/perform_swap';
 import { FillOrKillParamsX128 } from 'shared/new_swap';
-import { getBtcBalance } from 'shared/get_btc_balance';
 import { TestContext } from 'shared/utils/test_context';
-import { getIsoTime, Logger } from 'shared/utils/logger';
 import { getBalance } from 'shared/get_balance';
 import { send } from 'shared/send';
-import { submitGovernanceExtrinsic } from 'shared/cf_governance';
-import { buildAndSendBtcVaultSwap } from 'shared/btc_vault_swap';
-import { executeEvmVaultSwap } from 'shared/evm_vault_swap';
 import { newCcmMetadata } from 'shared/swapping';
+import { executeSolVaultSwap } from 'shared/sol_vault_swap';
 
-const keyring = new Keyring({ type: 'sr25519' });
 const brokerUri = '//BROKER_1';
-const broker = keyring.createFromUri(brokerUri);
-
 
 export async function testSol(
   testContext: TestContext,
@@ -109,4 +89,75 @@ export async function testSol(
   ]);
 
   logger.info(`Marked ${sourceAsset} transaction was rejected and refunded 👍.`);
+}
+
+export async function testSolVaultSwap(
+  testContext: TestContext,
+  sourceAsset: InternalAsset,
+  reportFunction: (txId: string) => Promise<void>,
+) {
+  const logger = testContext.logger;
+
+  const chain = chainFromAsset(sourceAsset);
+  if (chain !== Chains.Solana) {
+    // This should always be Sol
+    throw new Error('Expected chain to be Solana');
+  }
+
+  logger.info(`Testing broker level screening for ${chain} ${sourceAsset} vault swap...`);
+  const MAX_RETRIES = 120;
+
+  const destinationAddressForBtc = await newAssetAddress('Btc');
+  const solanaRefundAddress = await newAssetAddress('Sol');
+
+  logger.debug(`Refund address for ${sourceAsset} is ${solanaRefundAddress}...`);
+
+  logger.debug(`Sending ${sourceAsset} (vault swap) tx to reject...`);
+
+  const receipt = await executeSolVaultSwap(
+    logger,
+    sourceAsset,
+    'Btc',
+    destinationAddressForBtc,
+    {
+      account: createStateChainKeypair(brokerUri).address,
+      commissionBps: 0.0,
+    },
+    undefined,
+    undefined,
+    undefined,
+    {
+      retryDurationBlocks: 0,
+      refundAddress: solanaRefundAddress,
+      minPriceX128: '0x0',
+    },
+    undefined,
+    [],
+  );
+  const txHash = receipt.txHash;
+  logger.debug(`Sent ${sourceAsset} (vault swap) tx...`);
+
+  await reportFunction(`ActualSignature(S(${txHash}))`);
+  logger.debug(`Marked ${sourceAsset} (vault swap) ${txHash} for rejection. Awaiting refund.`);
+
+  // Currently this event cannot be decoded correctly, so we don't wait for it,
+  // just wait for the funds to arrive at the refund address
+  // await observeEvent(`${ingressEgressPallet}:TransactionRejectedByBroker`).event;
+
+  let receivedRefund = false;
+  for (let i = 0; i < MAX_RETRIES; i++) {
+    const refundBalance = await getBalance(sourceAsset, solanaRefundAddress);
+    if (refundBalance !== '0') {
+      receivedRefund = true;
+      break;
+    }
+    await sleep(6000);
+  }
+
+  if (!receivedRefund) {
+    throw new Error(
+      `Didn't receive funds refund to address ${solanaRefundAddress} within timeout!`,
+    );
+  }
+  logger.info(`Marked ${sourceAsset} vault swap was rejected and refunded 👍.`);
 }
