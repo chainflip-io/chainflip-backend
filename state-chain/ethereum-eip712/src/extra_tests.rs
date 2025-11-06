59419eabb42cefe01650adc48c045c967eef29f1
Ramiz Siddiqui <ramiz@chainflip.io>
2025-11-06 12:58:11 +0100

    feat: test cases, add back u128 variant

--- state-chain/ethereum-eip712/src/extra_tests.rs
+++ state-chain/ethereum-eip712/src/extra_tests.rs
@@ -2,12 +2,16 @@ use crate::{eip712::Eip712, *};
 
 use core::marker::PhantomData;
 
-use ethers_core::types::transaction::eip712::{
-	Eip712 as EthersEip712, Eip712DomainType as EthersType, TypedData as EthersTypedData,
+use ethers_core::types::{
+	transaction::eip712::{
+		Eip712 as EthersEip712, Eip712DomainType as EthersType, TypedData as EthersTypedData,
+	},
+	H256, U128, U256,
 };
 use scale_info::prelude::string::String;
 
 mod test_types {
+
 	use super::*;
 	#[derive(TypeInfo, Encode)]
 	pub enum TestEnum<T, S> {
@@ -21,8 +25,8 @@ mod test_types {
 	#[derive(TypeInfo, Encode)]
 	pub struct TestEmptyNested(pub TestEmpty);
 
-	#[derive(TypeInfo, serde::Serialize, Encode)]
-	pub struct Test1<T, S> {
+	#[derive(TypeInfo, Encode)]
+	pub struct TestCompact<T, S> {
 		#[codec(compact)]
 		pub c: u128,
 		pub a: T,
@@ -43,12 +47,24 @@ mod test_types {
 
 	#[derive(TypeInfo, Encode)]
 	pub struct Abs(pub (u8, u128));
-	#[derive(serde::Serialize, Encode, TypeInfo)]
+	#[derive(Encode, TypeInfo)]
 	pub struct Mail {
 		pub from: String,
 		pub to: String,
 		pub message: String,
 	}
+
+	#[derive(Encode, TypeInfo)]
+	pub struct TestVec<T>(pub Vec<T>);
+	#[derive(Encode, TypeInfo)]
+	pub struct TestArray(pub [u16; 10]);
+
+	#[derive(Encode, TypeInfo)]
+	pub struct TetsPrimitiveTypeU256(pub U256);
+	#[derive(Encode, TypeInfo)]
+	pub struct TetsPrimitiveTypeU128(pub U128);
+	#[derive(Encode, TypeInfo)]
+	pub struct TetsPrimitiveTypeH256(pub H256);
 }
 
 fn eip712_hash_matches_ethers<T: TypeInfo + Encode + 'static>(test_value: T) {
@@ -123,11 +139,21 @@ fn test_eip_encoding_matches_ethers() {
 	eip712_hash_matches_ethers(test_types::TestEnum::<u32, u8>::D { dd: 8 });
 	eip712_hash_matches_ethers(test_types::TestWrappedEmptyEnum(test_types::TestEmptyEnum::Aaaaa));
 	eip712_hash_matches_ethers(test_types::TestWrappedEmptyEnum(test_types::TestEmptyEnum::Bbbbb));
-	eip712_hash_matches_ethers(test_types::Test1 { a: 5u8, b: 6u32, c: 7 });
+	eip712_hash_matches_ethers(test_types::TestCompact { a: 5u8, b: 6u32, c: 7 });
 	eip712_hash_matches_ethers(test_types::TestEmpty);
 	eip712_hash_matches_ethers(test_types::TestEmptyNested(test_types::TestEmpty));
 	eip712_hash_matches_ethers(test_types::Abs((8, 9)));
 	eip712_hash_matches_ethers(
 		test_types::TestEmptyGeneric::<test_types::Mail>(Default::default()),
 	);
+	eip712_hash_matches_ethers(test_types::TestVec::<u8>(vec![]));
+	eip712_hash_matches_ethers(test_types::TestVec::<u128>(vec![]));
+	eip712_hash_matches_ethers(test_types::TestVec(vec![5u128, 6u128]));
+	eip712_hash_matches_ethers(test_types::TestVec(vec![5u8, 6u8]));
+
+	eip712_hash_matches_ethers(test_types::TestArray([5u16; 10]));
+	eip712_hash_matches_ethers(test_types::TetsPrimitiveTypeU256(U256([1, 2, 3, 4])));
+	eip712_hash_matches_ethers(test_types::TetsPrimitiveTypeU128(U128([1, 2])));
+	eip712_hash_matches_ethers(test_types::TetsPrimitiveTypeH256(H256([5u8; 32])));
+	eip712_hash_matches_ethers((12u128, test_types::Abs((8, 10))));
 }
