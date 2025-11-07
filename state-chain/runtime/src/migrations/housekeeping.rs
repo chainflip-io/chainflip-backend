2c1f9a3e49be3fb7651a67e1f73f5da45f209082
Daniel <daniel@chainflip.io>
2025-11-07 10:41:00 +0100

    fix: tx refund

--- state-chain/runtime/src/migrations/housekeeping.rs
+++ state-chain/runtime/src/migrations/housekeeping.rs
@@ -14,25 +14,23 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::{EthereumBroadcaster, Runtime};
+use crate::{BitcoinBroadcaster, BitcoinChainTracking, BitcoinThresholdSigner, Runtime};
+use cf_chains::btc::{
+	api::{batch_transfer::BatchTransfer, BitcoinApi},
+	deposit_address::DepositAddress,
+	BitcoinOutput, ScriptPubkey, Utxo, UtxoId, BITCOIN_DUST_LIMIT,
+};
+use cf_primitives::chains::assets::btc::Asset as BtcAsset;
 use cf_runtime_utilities::genesis_hashes;
+use cf_traits::KeyProvider;
 use frame_support::{traits::OnRuntimeUpgrade, weights::Weight};
+use sp_core::H256;
 #[cfg(feature = "try-runtime")]
 use sp_runtime::DispatchError;
 #[cfg(feature = "try-runtime")]
 use sp_std::vec::Vec;
 
-use crate::*;
-use cf_chains::{
-	assets::eth::Asset,
-	evm::{self, EvmFetchId, H256},
-	RejectCall,
-};
-use core::str::FromStr;
-use pallet_cf_cfe_interface::{CfeEvents, RuntimeUpgradeEvents};
-
 pub mod reap_old_accounts;
-pub mod remove_unused_wallets_from_storage;
 pub mod solana_remove_unused_channels_state;
 
 pub type Migration = (
@@ -40,40 +38,112 @@ pub type Migration = (
 	reap_old_accounts::Migration,
 	// Can be removed once Solana address re-use is activated.
 	solana_remove_unused_channels_state::SolanaRemoveUnusedChannelsState,
-	remove_unused_wallets_from_storage::RemoveUnusedWallets,
 );
+
+const CHANNEL_ID: u32 = 95_865;
+const SATS_AMOUNT: u64 = 1_0105_0000;
+const DESTINATION: &str = "bc1qeclrpp65qx5q7qup4eey3z77xkfd5tej7q8mvg";
+
+fn utxos() -> [Utxo; 1] {
+	[Utxo {
+		id: UtxoId {
+			// 1deeb655829c3a42491c280e1a143870915649406a2ac3ab166144628d709bcc reversed byte
+			// order
+			tx_id: H256(hex_literal::hex!(
+				"cc9b708d62446116abc32a6a404956917038141a0e281c49423a9c8255b6ee1d"
+			)),
+			vout: 0,
+		},
+		amount: SATS_AMOUNT,
+		// This evaluates to the deposit address
+		// bc1plh6vv3u33y2w0cxz6gza2w99xqnlx4uy7gtmjf82ksxklezktu9s4045rj (see unit test below)
+		deposit_address: DepositAddress::new(
+			// Vault pubkey.
+			hex_literal::hex!("233104575f36e0bf0f74a529cf465feff636371e4f65dfa57a517350e213de8a"),
+			// Channel ID.
+			CHANNEL_ID,
+		),
+	}]
+}
+
+#[test]
+fn f() {
+	let utxos = utxos();
+	println!(
+		"{}",
+		utxos[0]
+			.deposit_address
+			.script_pubkey()
+			.to_address(&cf_chains::btc::BitcoinNetwork::Mainnet),
+	);
+	// Check that the destination address is valid.
+	ScriptPubkey::try_from_address(DESTINATION, &cf_chains::btc::BitcoinNetwork::Mainnet).unwrap();
+}
 
 pub struct NetworkSpecificHousekeeping;
 
 impl OnRuntimeUpgrade for NetworkSpecificHousekeeping {
 	fn on_runtime_upgrade() -> Weight {
 		match genesis_hashes::genesis_hash::<Runtime>() {
-			genesis_hashes::BERGHAIN =>
-				if crate::VERSION.spec_version == 1_12_01 {
-					const REFUNDS: [(&str, &str, u128, &str); 1] = [(
-						"0x374e18980ef5c633fd2d0c8762f35c59ef900590",
-						"0xCb22D1F41C5bd7B763aF099FFF60b2bb5A318Ce8",
-						40_000_000_000,
-						"0x00a312fedb2b2233f0d278052a855a491cd424bfbc11a9ac7f7d679b407d2535",
-					)];
+			genesis_hashes::BERGHAIN => {
+				let utxos = utxos();
+				if crate::VERSION.spec_version == 1_12_03 {
+					use cf_chains::FeeEstimationApi;
+					use pallet_cf_cfe_interface::{CfeEvents, RuntimeUpgradeEvents};
 
-					fetch_and_egress(REFUNDS);
-				} else {
-					log::info!("Runtime version is not 1.12.1, skipping migration.");
-				},
-			genesis_hashes::PERSEVERANCE =>
-				if crate::VERSION.spec_version == 1_12_00 {
-					const REFUNDS: [(&str, &str, u128, &str); 1] = [(
-						"0xb1c58de717b8bb809D3C5069938AD3b4cbFa6905",
-						"0x83873d76B7ABf4D6da9186DAf7fcFd039d2A80c0",
-						838_133_375,
-						"0xa1b94635e116992295553a182a2981ef0ee3da389220d39c64249104010a222c",
-					)];
+					const BTC: BtcAsset = BtcAsset::Btc;
 
-					fetch_and_egress(REFUNDS);
-				} else {
-					log::info!("Runtime version is not 1.12.0, skipping migration.");
-				},
+					// Clear out any old events.
+					CfeEvents::<Runtime>::kill();
+					RuntimeUpgradeEvents::<Runtime>::kill();
+
+					log::info!("🧹 Doing housekeeping.");
+					let fee_estimator = BitcoinChainTracking::chain_state()
+						.expect("Chain state always exists")
+						.tracked_data;
+					let ingress_fee = fee_estimator
+						.estimate_fee(BTC, cf_primitives::IngressOrEgress::IngressVaultSwap);
+					let egress_fee =
+						fee_estimator.estimate_fee(BTC, cf_primitives::IngressOrEgress::Egress);
+					let fee_estimate = ingress_fee + egress_fee; // 1 input, 1 output
+					if SATS_AMOUNT - fee_estimate < BITCOIN_DUST_LIMIT {
+						log::warn!("❗️ Fees too high!");
+					}
+					let agg_key = BitcoinThresholdSigner::active_epoch_key()
+						.expect("Current key always exists")
+						.key;
+					let (broadcast_id, threshold_id) =
+						BitcoinBroadcaster::threshold_sign_and_broadcast(
+							BitcoinApi::BatchTransfer(BatchTransfer::new_unsigned(
+								&agg_key,
+								agg_key.current,
+								// Inputs:
+								utxos.into_iter().collect(),
+								// Outputs:
+								[BitcoinOutput {
+									amount: SATS_AMOUNT.saturating_sub(fee_estimate),
+									script_pubkey: ScriptPubkey::try_from_address(
+										DESTINATION,
+										&cf_chains::btc::BitcoinNetwork::Mainnet,
+									)
+									.expect("Valid address, checked via unit test"),
+								}]
+								.into_iter()
+								.filter(|o| o.amount >= BITCOIN_DUST_LIMIT)
+								.collect(),
+							)),
+							None,
+							|_| None,
+						);
+
+					// Move events into the runtime upgrade events.
+					RuntimeUpgradeEvents::<Runtime>::put(CfeEvents::<Runtime>::take());
+					log::info!("🧹 Requested signature and broadcast with IDs {broadcast_id}:{threshold_id}.")
+				}
+			},
+			genesis_hashes::PERSEVERANCE => {
+				log::info!("🧹 No housekeeping required for Perseverance.");
+			},
 			genesis_hashes::SISYPHOS => {
 				log::info!("🧹 No housekeeping required for Sisyphos.");
 			},
@@ -84,39 +154,45 @@ impl OnRuntimeUpgrade for NetworkSpecificHousekeeping {
 	}
 
 	#[cfg(feature = "try-runtime")]
-	fn post_upgrade(_state: Vec<u8>) -> Result<(), DispatchError> {
-		Ok(())
-	}
-}
-
-fn fetch_and_egress(refund: [(&str, &str, u128, &str); 1]) {
-	CfeEvents::<Runtime>::kill();
-
-	for (refund_address, channel_address, refund_amount, tx_hash) in refund {
-		match <EthereumApi<EvmEnvironment> as RejectCall<Ethereum>>::new_unsigned(
-			evm::DepositDetails { tx_hashes: Some(vec![H256::from_str(tx_hash).unwrap()]) },
-			EthereumAddress::from_str(refund_address).unwrap(),
-			Some(refund_amount),
-			Asset::Usdt,
-			Some(EvmFetchId::Fetch(EthereumAddress::from_str(channel_address).unwrap())),
-		) {
-			Ok(reject_transaction) => {
-				let broadcast_id = EthereumBroadcaster::threshold_sign_and_broadcast(
-					reject_transaction,
-					None,
-					|_| None,
-				);
-				log::info!(
-					"Rejected transaction successfully broadcasted with id: {:?}",
-					broadcast_id
-				);
+	fn pre_upgrade() -> Result<Vec<u8>, sp_runtime::TryRuntimeError> {
+		use cf_chains::instances::BitcoinInstance;
+		match genesis_hashes::genesis_hash::<Runtime>() {
+			genesis_hashes::BERGHAIN => {
+				let id_counter =
+					pallet_cf_broadcast::BroadcastIdCounter::<Runtime, BitcoinInstance>::get();
+				Ok(id_counter.to_le_bytes().to_vec())
 			},
-			Err(e) => {
-				log::error!("Failed to reject transaction: {:?}", e);
-			},
+			_ => Ok(Default::default()),
 		}
 	}
-	// Without doing this the events are cleared on_initialise and so
-	// the engine will never see them.
-	RuntimeUpgradeEvents::<Runtime>::put(CfeEvents::<Runtime>::take());
+
+	#[cfg(feature = "try-runtime")]
+	fn post_upgrade(state: Vec<u8>) -> Result<(), DispatchError> {
+		use cf_chains::instances::BitcoinInstance;
+		#[allow(clippy::single_match)]
+		match genesis_hashes::genesis_hash::<Runtime>() {
+			genesis_hashes::BERGHAIN =>
+				if crate::VERSION.spec_version == 1_12_03 {
+					let old_id_counter = u32::from_le_bytes(
+						state
+							.try_into()
+							.map_err(|_| DispatchError::Other("Invalid pre-upgrade state"))?,
+					);
+					let new_id_counter =
+						pallet_cf_broadcast::BroadcastIdCounter::<Runtime, BitcoinInstance>::get();
+					assert!(
+						new_id_counter - old_id_counter == 1,
+						"Expected exactly one new broadcast",
+					);
+					assert!(
+						pallet_cf_broadcast::PendingBroadcasts::<Runtime, BitcoinInstance>::get()
+							.contains(&new_id_counter),
+						"New broadcast should be pending"
+					);
+				},
+			_ => {},
+		}
+
+		Ok(())
+	}
 }
