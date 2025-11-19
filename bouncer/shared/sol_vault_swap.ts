3616fbb986792d0ea1847d3ae7030518654d5e0b
Albert Llimos <allimos3@gmail.com>
2025-11-19 14:37:22 +0100

    Revert "chore: use SDK for vault swap encoding"
    This reverts commit 0e1a8b240f4fa6f285e00beaed03d98614788cd8.

--- bouncer/shared/sol_vault_swap.ts
+++ bouncer/shared/sol_vault_swap.ts
@@ -1,34 +1,76 @@
 import assert from 'assert';
 import * as anchor from '@coral-xyz/anchor';
-import { InternalAsset as Asset, broker } from '@chainflip/cli';
-import { brokerApiEndpoint } from 'shared/json_rpc';
+import { InternalAsset as Asset } from '@chainflip/cli';
 import {
   PublicKey,
   sendAndConfirmTransaction,
   TransactionInstruction,
   Transaction,
+  AccountMeta,
 } from '@solana/web3.js';
+import BigNumber from 'bignumber.js';
 import { randomBytes } from 'crypto';
 import {
   getContractAddress,
   amountToFineAmount,
   defaultAssetAmounts,
+  chainFromAsset,
   assetDecimals,
   getSolWhaleKeyPair,
   getSolConnection,
   sleep,
   stateChainAssetFromAsset,
+  decodeSolAddress,
+  decodeDotAddressForContract,
   observeFetch,
+  Chains,
 } from 'shared/utils';
 import { CcmDepositMetadata, DcaParams, FillOrKillParamsX128 } from 'shared/new_swap';
 
 import { getSolanaSwapEndpointIdl } from 'shared/contract_interfaces';
+import { getChainflipApi } from 'shared/utils/substrate';
 import { getBalance } from 'shared/get_balance';
 import { TestContext } from 'shared/utils/test_context';
 import { Logger, throwError } from 'shared/utils/logger';
 import { SwapEndpoint } from '../../contract-interfaces/sol-program-idls/v1.2.3/swap_endpoint';
 
 const createdEventAccounts: [PublicKey, boolean][] = [];
+
+interface SolVaultSwapDetails {
+  chain: string;
+  program_id: string;
+  accounts: RpcAccountMeta[];
+  data: string;
+}
+
+type RpcAccountMeta = {
+  pubkey: string;
+  is_signer: boolean;
+  is_writable: boolean;
+};
+
+interface SolanaVaultSwapExtraParameters {
+  chain: 'Solana';
+  from: string;
+  seed: string;
+  input_amount: string;
+  refund_parameters: ChannelRefundParameters;
+  from_token_account?: string;
+}
+
+export type ChannelRefundParameters = {
+  retry_duration: number;
+  refund_address: string;
+  min_price: string;
+  refund_ccm_metadata:
+    | {
+        message: string;
+        gas_budget: string;
+        ccm_additional_data: string | undefined;
+      }
+    | undefined;
+  max_oracle_price_slippage: number | undefined;
+};
 
 export async function executeSolVaultSwap(
   logger: Logger,
@@ -65,59 +107,77 @@ export async function executeSolVaultSwap(
     assetDecimals(srcAsset),
   );
 
-  const vaultSwapDetails = await broker.requestSwapParameterEncoding(
-    {
-      srcAsset: stateChainAssetFromAsset(srcAsset),
-      srcAddress: whaleKeypair.publicKey.toBase58(),
-      destAsset: stateChainAssetFromAsset(destAsset),
-      destAddress,
-      commissionBps: brokerFees.commissionBps,
-      ccmParams: messageMetadata && {
-        message: messageMetadata.message,
-        gasBudget: messageMetadata.gasBudget,
-        ccmAdditionalData: messageMetadata.ccmAdditionalData,
-      },
-      fillOrKillParams: fillOrKillParams ?? {
-        retryDurationBlocks: 0,
-        refundAddress: whaleKeypair.publicKey.toBase58(),
-        minPriceX128: '0',
-      },
-      maxBoostFeeBps: boostFeeBps ?? 0,
-      amount: amountToSwap,
-      dcaParams: dcaParams && {
-        numberOfChunks: dcaParams.numberOfChunks,
-        chunkIntervalBlocks: dcaParams.chunkIntervalBlocks,
-      },
-      extraParams: {
-        seed: `0x${seed.toString('hex')}`,
-      },
-      affiliates: affiliateFees.map((fee) => ({
-        account: fee.account,
-        commissionBps: fee.bps,
-      })),
-    },
-    {
-      url: brokerApiEndpoint,
-    },
-    'backspin',
-  );
+  await using chainflip = await getChainflipApi();
+
+  // This will be replaced in PRO-2228 when the SDK is used
+  const refundParams: ChannelRefundParameters = {
+    retry_duration: fillOrKillParams?.retryDurationBlocks ?? 0,
+    refund_address: decodeSolAddress(
+      fillOrKillParams?.refundAddress ?? whaleKeypair.publicKey.toBase58(),
+    ),
+    min_price: fillOrKillParams?.minPriceX128 ?? '0x0',
+    refund_ccm_metadata: fillOrKillParams?.refundCcmMetadata
+      ? {
+          message: fillOrKillParams.refundCcmMetadata.message,
+          gas_budget: fillOrKillParams.refundCcmMetadata.gasBudget,
+          ccm_additional_data: fillOrKillParams.refundCcmMetadata.ccmAdditionalData,
+        }
+      : undefined,
+    max_oracle_price_slippage: undefined,
+  };
+  const extraParameters: SolanaVaultSwapExtraParameters = {
+    chain: 'Solana',
+    from: decodeSolAddress(whaleKeypair.publicKey.toBase58()),
+    seed: seed.toString('hex'),
+    input_amount: '0x' + new BigNumber(amountToSwap).toString(16),
+    refund_parameters: refundParams,
+    from_token_account: undefined,
+  };
 
   logger.trace('Requesting vault swap parameter encoding');
+  const vaultSwapDetails = (await chainflip.rpc(
+    `cf_request_swap_parameter_encoding`,
+    brokerFees.account,
+    stateChainAssetFromAsset(srcAsset),
+    stateChainAssetFromAsset(destAsset),
+    chainFromAsset(destAsset) === Chains.Polkadot
+      ? decodeDotAddressForContract(destAddress)
+      : destAddress,
+    brokerFees.commissionBps,
+    extraParameters,
+    messageMetadata && {
+      message: messageMetadata.message,
+      gas_budget: messageMetadata.gasBudget,
+      ccm_additional_data: messageMetadata.ccmAdditionalData,
+    },
+    boostFeeBps ?? 0,
+    affiliateFees,
+    dcaParams && {
+      number_of_chunks: dcaParams.numberOfChunks,
+      chunk_interval: dcaParams.chunkIntervalBlocks,
+    },
+  )) as unknown as SolVaultSwapDetails;
 
   assert.strictEqual(vaultSwapDetails.chain, 'Solana');
   assert.strictEqual(
-    new PublicKey(vaultSwapDetails.programId).toBase58(),
+    new PublicKey(vaultSwapDetails.program_id).toBase58(),
     getContractAddress('Solana', 'SWAP_ENDPOINT'),
   );
 
+  // Convert vaultSwapDetails.instruction.accounts into AccountMeta[]
+  const keys: AccountMeta[] = [];
+  for (const account of vaultSwapDetails.accounts) {
+    keys.push({
+      pubkey: new PublicKey(account.pubkey),
+      isSigner: account.is_signer,
+      isWritable: account.is_writable,
+    });
+  }
+
   const transaction = new Transaction();
   const instruction = new TransactionInstruction({
-    keys: vaultSwapDetails.accounts.map((account) => ({
-      pubkey: new PublicKey(account.pubkey),
-      isSigner: account.isSigner,
-      isWritable: account.isWritable,
-    })),
-    programId: new PublicKey(vaultSwapDetails.programId),
+    keys,
+    programId: new PublicKey(vaultSwapDetails.program_id),
     data: Buffer.from(vaultSwapDetails.data.slice(2), 'hex'),
   });
 
