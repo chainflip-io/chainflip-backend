b90da55295725b9fa6617d83a2664163600fe6c8
Albert Llimos <allimos3@gmail.com>
2025-11-19 14:37:50 +0100

    chore: small fix

--- bouncer/tests/evm_deposits.ts
+++ bouncer/tests/evm_deposits.ts
@@ -3,7 +3,6 @@ import { InternalAsset as Asset, broker } from '@chainflip/cli';
 import { doPerformSwap, requestNewSwap } from 'shared/perform_swap';
 import { prepareSwap, testSwap } from 'shared/swapping';
 import BigNumber from 'bignumber.js';
-import { Keyring } from '@polkadot/api';
 import {
   observeFetch,
   sleep,
