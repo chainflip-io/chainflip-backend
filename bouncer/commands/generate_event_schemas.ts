42efa4cab38717b5532467ff5dfb384de7fa1181
Maxim Urschumzew <maxu@chainflip.io>
2025-12-05 09:20:13 +0100

    feat: add script for generating schemas for runtime events.

---
+++ bouncer/commands/generate_event_schemas.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env -S pnpm tsx
+import generate from '@chainflip/processor/generate';
+import * as path from 'path';
+
+const generatedDir = path.join(import.meta.dirname, '..', 'generated', 'events');
+
+generate(generatedDir);
