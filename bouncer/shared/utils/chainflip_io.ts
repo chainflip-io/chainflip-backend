850945d8869698b6adc2b7039f65b415b7fe4e3e
Maxim Urschumzew <maxu@chainflip.io>
2025-12-05 14:52:19 +0100

    feat: add `ChainflipIO` and use boost as pilot test case.

---
+++ bouncer/shared/utils/chainflip_io.ts
@@ -0,0 +1,121 @@
+import {
+  ChainflipExtrinsicSubmitter,
+  extractExtrinsicResult,
+  lpMutex,
+} from 'shared/utils';
+import { z } from 'zod';
+import { DisposableApiPromise, getChainflipApi } from './substrate';
+import { KeyringPair } from '@polkadot/keyring/types';
+import { findEvent } from './indexer';
+
+export type Ok<T> = { ok: true; value: T; unwrap: () => T };
+export type Err<E> = { ok: false; error: E; unwrap: () => never };
+export type Result<T, E> = Ok<T> | Err<E>;
+export const Ok = <T>(value: T): Ok<T> => ({ ok: true, value, unwrap: () => value });
+export const Err = <E>(error: E): Err<E> => ({
+  ok: false,
+  error,
+  unwrap: () => {
+    throw new Error(`${error}`);
+  },
+});
+
+
+
+// ---------------------------------
+
+export type AccountType = 'Broker' | 'Lp';
+
+export type FullAccount<T extends AccountType> = {
+  uri: `//${string}`;
+  keypair: KeyringPair;
+  type: T;
+};
+
+export type WithAccount<T extends AccountType> = { account: FullAccount<T> };
+export type WithLpAccount = WithAccount<'Lp'>;
+
+export class ChainflipIO<Requirements> {
+  private lastIoBlockHeight: number;
+  readonly requirements: Requirements;
+
+  // state logging
+  private actions: string[];
+
+  constructor(requirements: Requirements) {
+    this.lastIoBlockHeight = 0;
+    this.requirements = requirements;
+    this.actions = [];
+  }
+
+  async submitExtrinsic<Data extends Requirements & { account: FullAccount<AccountType> }>(
+    this: ChainflipIO<Data>,
+    extrinsic: (api: DisposableApiPromise) => any,
+  ): Promise<Result<any, string>> {
+    await using chainflip = await getChainflipApi();
+    const extrinsicSubmitter = new ChainflipExtrinsicSubmitter(
+      this.requirements.account.keypair,
+      lpMutex.for(this.requirements.account.uri),
+    );
+    const ext = extrinsic(chainflip);
+
+    // generate readable description for logging
+    const human = ext.toHuman();
+    const section = human.method.section;
+    const method = human.method.method;
+    const args = human.method.args;
+    const readable = `${section}.${method}(${JSON.stringify(args)})`;
+
+    console.log(`Submitting extrinsic '${readable}' for ${this.requirements.account.uri}`);
+    this.actions.push(`Submitting extrinsic '${readable}' for ${this.requirements.account.uri}`);
+
+    // submit
+    const result = extractExtrinsicResult(chainflip, await extrinsicSubmitter.submit(ext, false));
+    if (result.ok) {
+      console.log(`Successfully submitted`);
+      this.actions.push(` => Done`);
+      this.lastIoBlockHeight = result.value.blockNumber.toNumber();
+    } else {
+      console.log(`Encountered error when submitting extrinsic: ${result.error}`);
+      this.actions.push(` => failed`);
+    }
+    return result;
+  }
+
+  async findEventInSameBlock<Z extends z.ZodTypeAny = z.ZodTypeAny>(
+    name: `${string}.${string}` | `.${string}`,
+    schema: Z,
+  ): Promise<z.infer<Z>> {
+    this.actions.push(`Waiting for event ${name}`);
+    const event = await findEvent(
+      name,
+      {
+        startFromBlock: this.lastIoBlockHeight,
+        endBeforeBlock: this.lastIoBlockHeight + 1,
+      },
+      {
+        schema,
+      },
+    );
+    this.lastIoBlockHeight = event.blockHeight;
+    this.actions.push(`Done`);
+
+    return event.args;
+  }
+
+  printActions() {
+    for (const action of this.actions) {
+      console.log(` - ${action}`);
+    }
+  }
+}
+
+// the following fixes the "TypeError: Do not know how to serialize a BigInt" error
+declare global {
+  interface BigInt {
+    toJSON(): string;
+  }
+}
+BigInt.prototype.toJSON = function () {
+  return this.toString();
+};
