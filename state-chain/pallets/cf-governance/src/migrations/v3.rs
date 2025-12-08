83984e025726c1184fc9044ed7151b126f694edd
Daniel <daniel@chainflip.io>
2025-12-08 17:02:00 +0100

    fix: governance thresholds (again)

--- state-chain/pallets/cf-governance/src/migrations/v3.rs
+++ state-chain/pallets/cf-governance/src/migrations/v3.rs
@@ -31,8 +31,7 @@ impl<T: Config> UncheckedOnRuntimeUpgrade for FixThresholdCalculation<T> {
 	fn on_runtime_upgrade() -> Weight {
 		let _ = crate::Members::<T>::translate::<BTreeSet<T::AccountId>, _>(|old| {
 			old.map(|members| {
-				// We want to change the threshold to 3 on mainnet (previously 4 of 6).
-				let threshold = (members.len() as u32).div_ceil(2);
+				let threshold = members.len() as u32 / 2 + 1;
 				log::info!(
 					"Upgrading governance council with members {:?}, setting threshold to {}",
 					members,
@@ -61,7 +60,7 @@ impl<T: Config> UncheckedOnRuntimeUpgrade for FixThresholdCalculation<T> {
 		let old_council = BTreeSet::<T::AccountId>::decode(&mut &state[..])
 			.map_err(|_| DispatchError::Other("Failed to decode member count"))?;
 		let council = Members::<T>::get();
-		let expected_threshold = (council.members.len() as u32).div_ceil(2);
+		let expected_threshold = members.len() as u32 / 2 + 1;
 		assert_eq!(
 			council.members, old_council,
 			"Members should be {:?} but are {:?}",
