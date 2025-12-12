884a775a895f790e48bdda4cbecbc8957dd15709
Maxim Urschumzew <maxu@chainflip.io>
2025-12-12 15:11:35 +0100

    temp: try to run block time check.

--- ci/scripts/check_blocktime.sh
+++ ci/scripts/check_blocktime.sh
@@ -10,7 +10,7 @@ sleep 10
 SLOT_DURATION="$(curl -H "Content-Type: application/json" -d '{ "jsonrpc":"2.0", "id":"1", "method":"cf_slot_duration", "params":{} }' http://localhost:9944 | jq '.result')"
 
 # we expect a block time of 6000ms on all live networks. ANYTHING ELSE WILL BREAK THE NETWORK.
-if [[ $SLOT_DURATION == 6000 ]]; then
+if [[ $SLOT_DURATION == 5000 ]]; then
   echo -e "Slot duration is correct (6000ms)."
 else
   echo -e "ERROR: Wrong slot duration: ${SLOT_DURATION}ms. Expected it to be 6000ms"
