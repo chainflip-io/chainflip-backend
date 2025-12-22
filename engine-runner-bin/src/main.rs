e4be3c3b93e6866b0ebc26e0cb2b2a3baae572bd
Daniel <daniel@chainflip.io>
2025-12-22 10:27:17 +0100

    core: version bump & changelog 2.0.5

--- engine-runner-bin/src/main.rs
+++ engine-runner-bin/src/main.rs
@@ -29,7 +29,7 @@ mod old {
 }
 
 mod new {
-	#[engine_proc_macros::link_engine_library_version("2.0.3")]
+	#[engine_proc_macros::link_engine_library_version("2.0.5")]
 	extern "C" {
 		fn cfe_entrypoint(
 			c_args: engine_upgrade_utils::CStrArray,
