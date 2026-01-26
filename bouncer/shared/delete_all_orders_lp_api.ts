a6e0294d90bfdf7cf1ce57caf80d7bdda78eeebe
nmammeri <nmammeri@gmail.com>
2026-01-26 13:50:24 +0100

    feat: clean up delete_all_orders_lp_api bouncer command

--- bouncer/shared/delete_all_orders_lp_api.ts
+++ bouncer/shared/delete_all_orders_lp_api.ts
@@ -1,106 +1,30 @@
 import { lpApiRpc } from 'shared/json_rpc';
-import { createStateChainKeypair } from 'shared/utils';
+import { Assets, createStateChainKeypair, stateChainAssetFromAsset } from 'shared/utils';
 import { Logger } from 'shared/utils/logger';
 import { getChainflipApi } from 'shared/utils/substrate';
 
-export async function DeleteAllOrdersLpApi(logger: Logger) {
+export async function logLpPoolOrders(logger: Logger, lpAddress: string) {
   await using chainflip = await getChainflipApi();
 
-  const lp = createStateChainKeypair(process.env.LP_URI || '//LP_1');
+  const allPoolAssets = Object.values(Assets).filter(
+    (asset) => asset !== 'Usdc' && asset !== 'Dot',
+  );
 
-  let orders = await chainflip.rpc('cf_pool_orders', 'BTC', 'USDC', lp.address);
-  logger.info(`BTC pool: ${JSON.stringify(orders)}`);
-  orders = await chainflip.rpc('cf_pool_orders', 'ETH', 'USDC', lp.address);
-  logger.info(`ETH pool: ${JSON.stringify(orders)}`);
-  orders = await chainflip.rpc(
-    'cf_pool_orders',
-    { chain: 'Arbitrum', asset: 'ETH' },
-    'USDC',
-    lp.address,
-  );
-  logger.info(`ARBETH pool: ${JSON.stringify(orders)}`);
-  orders = await chainflip.rpc(
-    'cf_pool_orders',
-    { chain: 'Arbitrum', asset: 'USDC' },
-    'USDC',
-    lp.address,
-  );
-  logger.info(`ARBUSDC pool: ${JSON.stringify(orders)}`);
-  orders = await chainflip.rpc(
-    'cf_pool_orders',
-    { chain: 'Arbitrum', asset: 'USDT' },
-    'USDT',
-    lp.address,
-  );
-  logger.info(`ARBUSDT pool: ${JSON.stringify(orders)}`);
-  orders = await chainflip.rpc(
-    'cf_pool_orders',
-    { chain: 'Ethereum', asset: 'USDT' },
-    'USDC',
-    lp.address,
-  );
-  logger.info(`USDT pool: ${JSON.stringify(orders)}`);
-  orders = await chainflip.rpc(
-    'cf_pool_orders',
-    { chain: 'Ethereum', asset: 'FLIP' },
-    'USDC',
-    lp.address,
-  );
-  logger.info(`FLIP pool: ${JSON.stringify(orders)}`);
-  orders = await chainflip.rpc(
-    'cf_pool_orders',
-    { chain: 'Ethereum', asset: 'WBTC' },
-    'WBTC',
-    lp.address,
-  );
-  logger.info(`WBTC pool: ${JSON.stringify(orders)}`);
+  for (const asset of allPoolAssets) {
+    const assetAndChain = stateChainAssetFromAsset(asset);
 
+    const orders = await chainflip.rpc('cf_pool_orders', assetAndChain, 'USDC', lpAddress);
+    logger.info(`${asset} pool: ${JSON.stringify(orders)}`);
+  }
+}
+
+export async function DeleteAllOrdersLpApi(logger: Logger) {
+  const lp = createStateChainKeypair(process.env.LP_URI || '//LP_API');
+
+  await logLpPoolOrders(logger, lp.address);
+
+  logger.info(`Cancelling all lp orders for '//LP_API' ${lp.address} \n`);
   await lpApiRpc(logger, `lp_cancel_all_orders`, []);
 
-  orders = await chainflip.rpc('cf_pool_orders', 'BTC', 'USDC', lp.address);
-  logger.info(`BTC pool: ${JSON.stringify(orders)}`);
-  orders = await chainflip.rpc('cf_pool_orders', 'ETH', 'USDC', lp.address);
-  logger.info(`ETH pool: ${JSON.stringify(orders)}`);
-  orders = await chainflip.rpc(
-    'cf_pool_orders',
-    { chain: 'Arbitrum', asset: 'ETH' },
-    'USDC',
-    lp.address,
-  );
-  logger.info(`ARBETH pool: ${JSON.stringify(orders)}`);
-  orders = await chainflip.rpc(
-    'cf_pool_orders',
-    { chain: 'Arbitrum', asset: 'USDC' },
-    'USDC',
-    lp.address,
-  );
-  logger.info(`ARBUSDC pool: ${JSON.stringify(orders)}`);
-  orders = await chainflip.rpc(
-    'cf_pool_orders',
-    { chain: 'Arbitrum', asset: 'USDT' },
-    'USDT',
-    lp.address,
-  );
-  logger.info(`ARBUSDT pool: ${JSON.stringify(orders)}`);
-  orders = await chainflip.rpc(
-    'cf_pool_orders',
-    { chain: 'Ethereum', asset: 'USDT' },
-    'USDC',
-    lp.address,
-  );
-  logger.info(`USDT pool: ${JSON.stringify(orders)}`);
-  orders = await chainflip.rpc(
-    'cf_pool_orders',
-    { chain: 'Ethereum', asset: 'FLIP' },
-    'USDC',
-    lp.address,
-  );
-  logger.info(`FLIP pool: ${JSON.stringify(orders)}`);
-  orders = await chainflip.rpc(
-    'cf_pool_orders',
-    { chain: 'Ethereum', asset: 'WBTC' },
-    'WBTC',
-    lp.address,
-  );
-  logger.info(`WBTC pool: ${JSON.stringify(orders)}`);
+  await logLpPoolOrders(logger, lp.address);
 }
