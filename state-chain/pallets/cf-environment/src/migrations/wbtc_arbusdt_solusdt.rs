5614b12c5c4efee8812ff89dd82cf828f2276343
nmammeri <nmammeri@gmail.com>
2026-02-03 14:10:19 +0100

    feat: environment pallet migration fix

--- state-chain/pallets/cf-environment/src/migrations/wbtc_arbusdt_solusdt.rs
+++ state-chain/pallets/cf-environment/src/migrations/wbtc_arbusdt_solusdt.rs
@@ -4,6 +4,42 @@ use cf_chains::{eth::Address as EthereumAddress, evm::H256};
 use cf_utilities::bs58_array;
 use frame_support::{pallet_prelude::Weight, traits::UncheckedOnRuntimeUpgrade};
 
+mod old {
+	use crate::{pallet::Config, Pallet};
+	use cf_chains::sol::{AddressLookupTableAccount, SolAddress};
+	use codec::{Decode, Encode};
+	use frame_support::{
+		pallet_prelude::{TypeInfo, ValueQuery},
+		Deserialize, Serialize,
+	};
+
+	#[derive(
+		Encode, Decode, TypeInfo, Default, Clone, PartialEq, Eq, Debug, Serialize, Deserialize,
+	)]
+	pub struct OldSolApiEnvironment {
+		// For native Sol API calls.
+		pub vault_program: SolAddress,
+		pub vault_program_data_account: SolAddress,
+
+		// For token API calls.
+		pub token_vault_pda_account: SolAddress,
+
+		// For Usdc token
+		pub usdc_token_mint_pubkey: SolAddress,
+		pub usdc_token_vault_ata: SolAddress,
+
+		// For program swaps API calls.
+		pub swap_endpoint_program: SolAddress,
+		pub swap_endpoint_program_data_account: SolAddress,
+		pub alt_manager_program: SolAddress,
+		pub address_lookup_table_account: AddressLookupTableAccount,
+	}
+
+	#[frame_support::storage_alias]
+	pub type SolanaApiEnvironment<T: Config> =
+		StorageValue<Pallet<T>, OldSolApiEnvironment, ValueQuery>;
+}
+
 pub struct NewAssetsMigration<T>(PhantomData<T>);
 
 impl<T: Config<Hash = H256>> UncheckedOnRuntimeUpgrade for NewAssetsMigration<T> {
@@ -46,9 +82,20 @@ impl<T: Config<Hash = H256>> UncheckedOnRuntimeUpgrade for NewAssetsMigration<T>
 			),
 		};
 
-		SolanaApiEnvironment::<T>::mutate(|sol_api_environment| {
-			sol_api_environment.usdt_token_mint_pubkey = solusdt_pubkey;
-			sol_api_environment.usdt_token_vault_ata = solusdt_ata;
+		let old_sol_api_environment = old::SolanaApiEnvironment::<T>::take();
+		SolanaApiEnvironment::<T>::set(SolApiEnvironment {
+			vault_program: old_sol_api_environment.vault_program,
+			vault_program_data_account: old_sol_api_environment.vault_program_data_account,
+			token_vault_pda_account: old_sol_api_environment.token_vault_pda_account,
+			usdc_token_mint_pubkey: old_sol_api_environment.usdc_token_mint_pubkey,
+			usdc_token_vault_ata: old_sol_api_environment.usdc_token_vault_ata,
+			usdt_token_mint_pubkey: solusdt_pubkey,
+			usdt_token_vault_ata: solusdt_ata,
+			swap_endpoint_program: old_sol_api_environment.swap_endpoint_program,
+			swap_endpoint_program_data_account: old_sol_api_environment
+				.swap_endpoint_program_data_account,
+			alt_manager_program: old_sol_api_environment.alt_manager_program,
+			address_lookup_table_account: old_sol_api_environment.address_lookup_table_account,
 		});
 
 		log::info!("🌮 Environment pallet migration completed: Added Wbtc, ArbUsdt and SolUsdt");
@@ -58,11 +105,12 @@ impl<T: Config<Hash = H256>> UncheckedOnRuntimeUpgrade for NewAssetsMigration<T>
 
 	#[cfg(feature = "try-runtime")]
 	fn pre_upgrade() -> Result<Vec<u8>, DispatchError> {
-		Ok(vec![])
+		let old_sol_api_environment = old::SolanaApiEnvironment::<T>::get();
+		Ok(old_sol_api_environment.encode())
 	}
 
 	#[cfg(feature = "try-runtime")]
-	fn post_upgrade(_state: Vec<u8>) -> Result<(), DispatchError> {
+	fn post_upgrade(state: Vec<u8>) -> Result<(), DispatchError> {
 		let wbtc_address = EthereumSupportedAssets::<T>::get(EthAsset::Wbtc)
 			.ok_or(DispatchError::from("Failed to fetch WBTC address"))?;
 
@@ -99,8 +147,6 @@ impl<T: Config<Hash = H256>> UncheckedOnRuntimeUpgrade for NewAssetsMigration<T>
 			),
 		};
 
-		let solana_api_environment = SolanaApiEnvironment::<T>::get();
-
 		let (expected_solusdt_pubkey, expected_solusdt_ata) =
 			match ChainflipNetworkEnvironment::<T>::get() {
 				NetworkEnvironment::Mainnet => (
@@ -117,8 +163,46 @@ impl<T: Config<Hash = H256>> UncheckedOnRuntimeUpgrade for NewAssetsMigration<T>
 				),
 			};
 
+		let old_sol_api_environment = old::OldSolApiEnvironment::decode(&mut state.as_slice())
+			.map_err(|_| DispatchError::from("Failed to decode state"))?;
+		let solana_api_environment = SolanaApiEnvironment::<T>::get();
+
 		assert_eq!(solana_api_environment.usdt_token_mint_pubkey, expected_solusdt_pubkey);
 		assert_eq!(solana_api_environment.usdt_token_vault_ata, expected_solusdt_ata);
+
+		assert_eq!(solana_api_environment.vault_program, old_sol_api_environment.vault_program);
+		assert_eq!(
+			solana_api_environment.vault_program_data_account,
+			old_sol_api_environment.vault_program_data_account
+		);
+		assert_eq!(
+			solana_api_environment.token_vault_pda_account,
+			old_sol_api_environment.token_vault_pda_account
+		);
+		assert_eq!(
+			solana_api_environment.usdc_token_mint_pubkey,
+			old_sol_api_environment.usdc_token_mint_pubkey
+		);
+		assert_eq!(
+			solana_api_environment.usdc_token_vault_ata,
+			old_sol_api_environment.usdc_token_vault_ata
+		);
+		assert_eq!(
+			solana_api_environment.swap_endpoint_program,
+			old_sol_api_environment.swap_endpoint_program
+		);
+		assert_eq!(
+			solana_api_environment.swap_endpoint_program_data_account,
+			old_sol_api_environment.swap_endpoint_program_data_account
+		);
+		assert_eq!(
+			solana_api_environment.alt_manager_program,
+			old_sol_api_environment.alt_manager_program
+		);
+		assert_eq!(
+			solana_api_environment.address_lookup_table_account,
+			old_sol_api_environment.address_lookup_table_account
+		);
 
 		Ok(())
 	}
