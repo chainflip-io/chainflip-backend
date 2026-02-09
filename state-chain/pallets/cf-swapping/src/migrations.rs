f8a3e84abb710c15fb156014120a79dd843e0414
Maxim Shishmarev <msgmaxim@gmail.com>
2026-02-09 12:21:54 +1100

    chore: Manually reschedule stuck fee swaps (#6378)
    * chore: manually reschedule stuck fee swaps
    
    * fix: remove fees field after merge main
    
    ---------
    
    Co-authored-by: Jamie Ford <Jamie@chainflip.io>

--- state-chain/pallets/cf-swapping/src/migrations.rs
+++ state-chain/pallets/cf-swapping/src/migrations.rs
@@ -19,6 +19,7 @@ use crate::Pallet;
 use cf_runtime_utilities::PlaceholderMigration;
 use frame_support::migrations::VersionedMigration;
 
+mod reschedule_stuck_swaps;
 pub mod swap_fee_migration;
 
 pub type PalletMigration<T> = (
@@ -29,5 +30,12 @@ pub type PalletMigration<T> = (
 		Pallet<T>,
 		<T as frame_system::Config>::DbWeight,
 	>,
-	PlaceholderMigration<14, Pallet<T>>,
+	VersionedMigration<
+		14,
+		15,
+		reschedule_stuck_swaps::RescheduleStuckSwaps<T>,
+		Pallet<T>,
+		<T as frame_system::Config>::DbWeight,
+	>,
+	PlaceholderMigration<15, Pallet<T>>,
 );
