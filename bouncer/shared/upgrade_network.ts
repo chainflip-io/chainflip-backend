0334de0d7450005d34ed6550fe9bde090d324956
Daniel <daniel@chainflip.io>
2026-02-12 11:18:32 +0100

    fix: regenerate Event schemas during upgrade test

--- bouncer/shared/upgrade_network.ts
+++ bouncer/shared/upgrade_network.ts
@@ -13,6 +13,7 @@ import { submitGovernanceExtrinsic } from 'shared/cf_governance';
 import { globalLogger as logger } from 'shared/utils/logger';
 import { clearChainflipApiCache, clearSubscribeHeadsCache } from 'shared/utils/substrate';
 import { AccountRole, setupAccount } from 'shared/setup_account';
+import generate from '@chainflip/processor/generate';
 
 async function readPackageTomlVersion(projectRoot: string): Promise<string> {
   const data = await fs.readFile(path.join(projectRoot, '/state-chain/runtime/Cargo.toml'), 'utf8');
@@ -273,6 +274,10 @@ async function incompatibleUpgradeNoBuild(
   await sleep(20000);
 
   logger.info('Setting missed authorship suspension back to 100/150 after nodes back up.');
+
+  // Regenerate event schemas, since they may have changed with the new runtime.
+  const generatedDir = path.join(import.meta.dirname, '..', 'generated', 'events');
+  await generate(generatedDir);
 
   // clear api caches (this seems to be required starting for node.js version >= 22.0)
   clearChainflipApiCache();
