cb72d2674a05525125cb70ef15f8acb4dd5bc78d
Maxim Urschumzew <maxu@chainflip.io>
2026-02-19 14:11:44 +0100

    chore: allow meta items to be forwarded throughout `define_empty_struct!` macro invocation.

--- utilities/src/macros.rs
+++ utilities/src/macros.rs
@@ -45,12 +45,14 @@ macro_rules! define_empty_struct {
 		[$name:ident $(: $path:path)?, $($rest:tt)*]
 		[$($names:tt)*]
 		[$($names_and_bounds:tt)*]
+		$(#[$meta:meta])*
 		$vis:vis struct $struct_name:ident
 	) => {
 		cf_utilities::define_empty_struct!{
 			[$($rest)*]
 			[$($names)* $name, ]
 			[$($names_and_bounds)* $name $(:$path)?, ]
+			$(#[$meta])*
 			$vis struct $struct_name
 		}
 	};
@@ -58,12 +60,14 @@ macro_rules! define_empty_struct {
 		[$name:ident: $l:lifetime, $($rest:tt)*]
 		[$($names:tt)*]
 		[$($names_and_bounds:tt)*]
+		$(#[$meta:meta])*
 		$vis:vis struct $struct_name:ident
 	) => {
 		cf_utilities::define_empty_struct!{
 			[$($rest)*]
 			[$($names)* $name, ]
 			[$($names_and_bounds)* $name:$l, ]
+			$(#[$meta])*
 			$vis struct $struct_name
 		}
 	};
@@ -77,11 +81,13 @@ macro_rules! define_empty_struct {
 		[>;]
 		[$($names:tt)*]
 		[$($names_and_bounds:tt)*]
+		$(#[$meta:meta])*
 		$vis:vis struct $struct_name:ident
 	) => {
 		cf_utilities::derive_common_traits!{
 			#[derive(scale_info::TypeInfo, frame_support::DefaultNoBound)]
 			#[scale_info(skip_type_params(T, I))]
+			$(#[$meta])*
 			$vis struct $struct_name<$($names_and_bounds)*>
 			(
 				sp_std::marker::PhantomData
@@ -101,11 +107,12 @@ macro_rules! define_empty_struct {
 	};
 	// This is a special case handling structs without type parameters
 	(
-		$vis:vis struct $struct_name:ident;
+		$(#[$meta:meta])* $vis:vis struct $struct_name:ident;
 	) => {
 		cf_utilities::derive_common_traits!{
 			#[derive(scale_info::TypeInfo, frame_support::DefaultNoBound)]
 			#[derive(PartialOrd, Ord)]
+			$(#[$meta])*
 			$vis struct $struct_name {}
 		}
 		#[cfg(test)]
@@ -125,12 +132,13 @@ macro_rules! define_empty_struct {
 	};
 	// This is the entry point for structs with type parameters
 	(
-		$vis:vis struct $struct_name:ident<$($rest:tt)*
+		$(#[$meta:meta])* $vis:vis struct $struct_name:ident<$($rest:tt)*
 	) => {
 		cf_utilities::define_empty_struct!{
 			[$($rest)*]
 			[]
 			[]
+			$(#[$meta])*
 			$vis struct $struct_name
 		}
 	};
