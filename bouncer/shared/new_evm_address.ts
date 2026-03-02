03d73107c309ea81e37900ca1ffcfa9f8e33b8a5
nmammeri <nmammeri@gmail.com>
2026-03-02 12:49:31 +0100

    feat: make newEvmAddress use singleton Web3 client

--- bouncer/shared/new_evm_address.ts
+++ bouncer/shared/new_evm_address.ts
@@ -1,8 +1,7 @@
-import Web3 from 'web3';
-import { sha256 } from 'shared/utils';
+import { getWeb3, sha256 } from 'shared/utils';
 
 export function newEvmAddress(seed: string): string {
   const secret = sha256(seed).toString('hex');
-  const web3 = new Web3();
+  const web3 = getWeb3('Ethereum'); // any evm client works
   return web3.eth.accounts.privateKeyToAccount(secret).address;
 }
