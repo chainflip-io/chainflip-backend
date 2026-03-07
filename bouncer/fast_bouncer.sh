5f121fd1263c8550416a8182c66bb6ab847fdac4
nmammeri <nmammeri@gmail.com>
2026-03-07 17:06:04 +0100

    feat: reduce concurrency on ci only

--- bouncer/fast_bouncer.sh
+++ bouncer/fast_bouncer.sh
@@ -2,4 +2,7 @@
 set -e
 ./commands/observe_block.ts 5
 ./setup_for_test.sh
-NODE_COUNT=$1 LOCALNET=$LOCALNET pnpm vitest --maxConcurrency=100 --hideSkippedTests run -t "ConcurrentTests"
+# Lower concurrency in slow CI nodes to avoid resource contention; higher locally for speed.
+VITEST_CONCURRENCY=$([[ -n "$GITHUB_ACTIONS" ]] && echo 100 || echo 1000)
+echo "Running ConcurrentTests with concurrency: $VITEST_CONCURRENCY"
+NODE_COUNT=$1 LOCALNET=$LOCALNET pnpm vitest --maxConcurrency=$VITEST_CONCURRENCY --hideSkippedTests run -t "ConcurrentTests"
