37c8a47ccc3c08a95ee9abfc4cd3de89c21ef6b3
Maxim Urschumzew <maxu@chainflip.io>
2026-03-09 12:30:12 +0700

    temp: increase max-concurrency.

--- bouncer/fast_bouncer.sh
+++ bouncer/fast_bouncer.sh
@@ -2,6 +2,4 @@
 set -e
 ./commands/observe_block.ts 5
 ./setup_for_test.sh
-# Lower concurrency in slow CI nodes to avoid resource contention; higher locally for speed.
-VITEST_CONCURRENCY=$([[ -n "$GITHUB_ACTIONS" ]] && echo 100 || echo 1000)
-NODE_COUNT=$1 LOCALNET=$LOCALNET pnpm vitest --maxConcurrency=$VITEST_CONCURRENCY --hideSkippedTests run -t "ConcurrentTests"
+NODE_COUNT=$1 LOCALNET=$LOCALNET pnpm vitest --maxConcurrency=1000 --hideSkippedTests run -t "ConcurrentTests"
