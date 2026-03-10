5603d278b6d5549a9466c051f3ac66ea46388e93
Maxim Urschumzew <maxu@chainflip.io>
2026-03-10 15:57:21 +0700

    chore: rename BtcClient to BtcMutexClient.

--- bouncer/commands/create_raw_btc_tx.ts
+++ bouncer/commands/create_raw_btc_tx.ts
@@ -2,24 +2,26 @@
 // Constructs a very simple Raw BTC transaction. Can be used for manual testing a raw broadcast for example.
 // Usage: ./commands/create_raw_btc_tx.ts <bitcoin_address> <btc_amount>
 
-import { BTC_ENDPOINT, btcClient } from 'shared/send_btc';
+import { BTC_ENDPOINT, globalBtcWhaleMutexClient } from 'shared/send_btc';
 
 console.log(`Btc endpoint is set to '${BTC_ENDPOINT}'`);
 
 const createRawTransaction = async (toAddress: string, amountInBtc: number | string) => {
   try {
     // Create the raw transaction
-    const rawTx = await btcClient.createRawTransaction([], {
+    const rawTx = await globalBtcWhaleMutexClient.unsafe_getClient().createRawTransaction([], {
       [toAddress]: amountInBtc,
     });
-    const fundedTx = (await btcClient.fundRawTransaction(rawTx, {
-      changeAddress: await btcClient.getNewAddress(),
+    const fundedTx = (await globalBtcWhaleMutexClient.unsafe_getClient().fundRawTransaction(rawTx, {
+      changeAddress: await globalBtcWhaleMutexClient.getNewAddress(),
       feeRate: 0.00001,
       lockUnspents: true,
     })) as { hex: string };
 
     // Sign the raw transaction
-    const signedTx = await btcClient.signRawTransactionWithWallet(fundedTx);
+    const signedTx = await globalBtcWhaleMutexClient
+      .unsafe_getClient()
+      .signRawTransactionWithWallet(fundedTx);
 
     // Here's your raw signed transaction
     console.log('Raw signed transaction:', signedTx.hex);
