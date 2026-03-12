fb3271e3a08493336c80d1e8b0153cbc7863aad3
Daniel <daniel@chainflip.io>
2026-03-12 16:19:53 +0100

    fix: use call builder for evm vault transfer failed

--- engine/src/witness/evm/vault.rs
+++ engine/src/witness/evm/vault.rs
@@ -41,8 +41,7 @@ use cf_primitives::{Asset, AssetAmount, EpochIndex, ForeignChain};
 use ethers::prelude::*;
 use pallet_cf_ingress_egress::{TransferFailedWitness, VaultDepositWitness};
 use state_chain_runtime::{
-	chainflip::witnessing::pallet_hooks::EvmVaultContractEvent, EthereumInstance, Runtime,
-	RuntimeCall,
+	chainflip::witnessing::pallet_hooks::EvmVaultContractEvent, RuntimeCall,
 };
 
 abigen!(Vault, "$CF_ETH_CONTRACT_ABI_ROOT/$CF_ETH_CONTRACT_ABI_TAG/IVault.json");
@@ -232,16 +231,16 @@ where
 			amount,
 			token,
 			reason: _,
-		}) => Some(RuntimeCall::EthereumIngressEgress(pallet_cf_ingress_egress::Call::<
-			Runtime,
-			EthereumInstance,
-		>::vault_transfer_failed {
-			asset: (*(supported_assets.get(&token).ok_or(anyhow!("Asset {token:?} not found"))?))
+		}) => Some(CallBuilder::vault_transfer_failed(
+			(*supported_assets.get(&token).ok_or(anyhow!("Asset {token:?} not found"))?)
 				.try_into()
 				.expect("Asset translated from EthereumAddress must be supported by the chain."),
-			amount: try_into_primitive(amount)?,
-			destination_address: recipient.0.into(),
-		})),
+			try_into_primitive::<_, AssetAmount>(amount)?
+				.try_into()
+				.unwrap_or_else(|_| panic!("Amount must be supported by the chain.")),
+			recipient.0.into(),
+		)),
+
 		_ => None,
 	})
 }
