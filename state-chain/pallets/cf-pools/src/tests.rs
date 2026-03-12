ff1aa50a6caecbb0874bf06828a549aaba2c7156
Daniel <daniel@chainflip.io>
2026-03-12 16:22:12 +0100

    fix: stricter order scheduling limits.

--- state-chain/pallets/cf-pools/src/tests.rs
+++ state-chain/pallets/cf-pools/src/tests.rs
@@ -378,7 +378,7 @@ fn can_execute_scheduled_limit_order_updates() {
 	fn test_scheduled_limit_order_update(
 		update: impl Fn(OrderId, u64, AssetAmount) -> DispatchResult,
 	) {
-		const DISPATCH_AT: u64 = 6;
+		const DISPATCH_AT: u64 = SCHEDULE_OPEN_LIMIT_BLOCKS as u64;
 		const ORDER_ID: u64 = 0;
 		const AMOUNT: AssetAmount = 55;
 
@@ -478,7 +478,7 @@ fn test_dispatch_at_validation() {
 				Some(0),
 				IncreaseOrDecrease::Decrease(55),
 				// Too far in the future
-				Some(CURRENT_BLOCK + (SCHEDULE_UPDATE_LIMIT_BLOCKS as u64) + 1)
+				Some(CURRENT_BLOCK + (SCHEDULE_OPEN_LIMIT_BLOCKS as u64) + 1)
 			),
 			Error::<Test>::InvalidDispatchAt
 		);
@@ -492,7 +492,7 @@ fn test_dispatch_at_validation() {
 			Some(0),
 			IncreaseOrDecrease::Decrease(55),
 			// Valid dispatch at
-			Some(CURRENT_BLOCK + (SCHEDULE_UPDATE_LIMIT_BLOCKS as u64))
+			Some(CURRENT_BLOCK + (SCHEDULE_OPEN_LIMIT_BLOCKS as u64))
 		));
 	});
 }
@@ -1468,8 +1468,8 @@ fn test_sweeping_when_updating_range_order() {
 #[test]
 fn test_limit_order_auto_close() {
 	const ASSET: Asset = Asset::Flip;
-	const CLOSE_ORDER_AT: u64 = 10;
-	const DISPATCH_AT: u64 = 5;
+	const CLOSE_ORDER_AT: u64 = SCHEDULE_CLOSE_LIMIT_BLOCKS as u64;
+	const DISPATCH_AT: u64 = SCHEDULE_OPEN_LIMIT_BLOCKS as u64;
 	const AMOUNT: AssetAmount = 10_000;
 	const ORDER_ID: u64 = 1;
 
@@ -1513,8 +1513,8 @@ fn test_limit_order_auto_close() {
 					AMOUNT,
 					// Schedule the call for the same block as the close order, so it
 					// should be rejected
-					Some(CLOSE_ORDER_AT),
-					Some(CLOSE_ORDER_AT),
+					Some(DISPATCH_AT),
+					Some(DISPATCH_AT),
 				),
 				Error::<Test>::InvalidCloseOrderAt
 			);
@@ -1528,8 +1528,8 @@ fn test_limit_order_auto_close() {
 					Some(5),
 					AMOUNT,
 					// Schedule the call for after the close order, so it should be rejected
-					Some(CLOSE_ORDER_AT + 1),
-					Some(CLOSE_ORDER_AT),
+					Some(DISPATCH_AT),
+					Some(DISPATCH_AT - 1),
 				),
 				Error::<Test>::InvalidCloseOrderAt
 			);
@@ -1544,7 +1544,7 @@ fn test_limit_order_auto_close() {
 					AMOUNT,
 					Some(DISPATCH_AT),
 					// Schedule the close order for too far in the future
-					Some((SCHEDULE_UPDATE_LIMIT_BLOCKS as u64) + 2),
+					Some((SCHEDULE_CLOSE_LIMIT_BLOCKS as u64) + 2),
 				),
 				Error::<Test>::InvalidCloseOrderAt
 			);
