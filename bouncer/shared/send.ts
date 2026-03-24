ee7b7c687b1819e358f811bc33806b53acd01f35
Maxim Urschumzew <maxu@chainflip.io>
2026-03-24 11:56:15 +0100

    chore: use linearly the next btc wallet instead of randomly.

--- bouncer/shared/send.ts
+++ bouncer/shared/send.ts
@@ -1,4 +1,4 @@
-import { getRandomBtcClient, sendBtc } from 'shared/send_btc';
+import { getNextBtcClient, sendBtc } from 'shared/send_btc';
 import { sendErc20 } from 'shared/send_erc20';
 import { sendEvmNative, signAndSendTxEvm } from 'shared/send_evm';
 import {
@@ -30,7 +30,7 @@ export async function send(
   logger.debug(`Sending ${amount} ${asset} to ${address}`);
   switch (asset) {
     case 'Btc':
-      return sendBtc(logger, address, amount, 1, await getRandomBtcClient(logger));
+      return sendBtc(logger, address, amount, 1, await getNextBtcClient(logger));
     case 'Eth':
       return sendEvmNative(logger, 'Ethereum', address, amount);
     case 'ArbEth':
