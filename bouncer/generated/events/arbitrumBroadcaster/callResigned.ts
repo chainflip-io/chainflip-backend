b62560e617828ffeb1acd7e6aa9219669651ee3c
Marcello <marcello@chainflip.io>
2026-04-14 13:50:04 +0200

    feat: add signed payload to CallResigned event (#6520)
    add signed payload to CallResigned event

--- bouncer/generated/events/arbitrumBroadcaster/callResigned.ts
+++ bouncer/generated/events/arbitrumBroadcaster/callResigned.ts
@@ -1,3 +1,7 @@
 import { z } from 'zod';
+import { cfChainsEvmTransaction } from '../common';
 
-export const arbitrumBroadcasterCallResigned = z.object({ broadcastId: z.number() });
+export const arbitrumBroadcasterCallResigned = z.object({
+  broadcastId: z.number(),
+  transactionPayload: cfChainsEvmTransaction,
+});
