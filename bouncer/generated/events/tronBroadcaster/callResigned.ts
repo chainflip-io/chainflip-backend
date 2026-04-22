9152cb1d71a84ccd657b5fdec33339ddf2d3d3e1
Albert Llimos <allimos3@gmail.com>
2026-04-22 14:32:09 +0200

    chore: add newly generated events

--- bouncer/generated/events/tronBroadcaster/callResigned.ts
+++ bouncer/generated/events/tronBroadcaster/callResigned.ts
@@ -1,3 +1,7 @@
 import { z } from 'zod';
+import { cfChainsTronTronTransaction } from '../common';
 
-export const tronBroadcasterCallResigned = z.object({ broadcastId: z.number() });
+export const tronBroadcasterCallResigned = z.object({
+  broadcastId: z.number(),
+  transactionPayload: cfChainsTronTronTransaction,
+});
