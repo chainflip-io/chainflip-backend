d07b987b1fdc03a41b7e3726374d766e32d8ae87
Ramiz Siddiqui <ramiz@chainflip.io>
2026-04-30 17:41:51 +0200

    fix: ensure max_bid <= balance

--- state-chain/pallets/cf-validator/src/migrations/remove_underfunded_delegations.rs
+++ state-chain/pallets/cf-validator/src/migrations/remove_underfunded_delegations.rs
@@ -11,19 +11,29 @@ impl<T: Config> UncheckedOnRuntimeUpgrade for Migration<T> {
 		let mut total_read = 0u64;
 		let mut to_undelegate = Vec::new();
 
-		for (delegator, (_, max_bid)) in DelegationChoice::<T>::iter() {
+		for (delegator, (validator, mut max_bid)) in DelegationChoice::<T>::iter() {
 			total_read += 1;
+
+			let balance = T::FundingInfo::balance(&delegator);
+			if max_bid > balance {
+				log::info!(
+					target: "cf-validator",
+					"max_bid should be lower or equal to balance. violation found and so setting max_bid equal to balance"
+				);
+				max_bid = balance;
+				DelegationChoice::<T>::insert(&delegator, (validator, max_bid));
+			}
+
 			if max_bid.into() < min_funding {
-				to_undelegate.push(delegator);
+				to_undelegate.push(delegator.clone());
 			}
 		}
 
 		let removed_count = to_undelegate.len() as u64;
 		for delegator in to_undelegate {
-			if let Err(e) = Pallet::<T>::undelegate(
-				RawOrigin::Signed(delegator).into(),
-				DelegationAmount::Max,
-			) {
+			if let Err(e) =
+				Pallet::<T>::undelegate(RawOrigin::Signed(delegator).into(), DelegationAmount::Max)
+			{
 				log::error!(
 					target: "cf-validator",
 					"remove_underfunded_delegations: failed to undelegate: {:?}",
@@ -40,25 +50,47 @@ impl<T: Config> UncheckedOnRuntimeUpgrade for Migration<T> {
 	#[cfg(feature = "try-runtime")]
 	fn pre_upgrade() -> Result<Vec<u8>, DispatchError> {
 		let min_funding = T::MinimumFunding::get_min_funding_amount();
-		let count = DelegationChoice::<T>::iter()
-			.filter(|(_, (_, max_bid))| Into::<u128>::into(*max_bid) < min_funding)
+		let over_balance_count = DelegationChoice::<T>::iter()
+			.filter(|(delegator, (_, max_bid))| {
+				let balance = T::FundingInfo::balance(delegator);
+				*max_bid > balance
+			})
 			.count() as u32;
-		Ok(count.encode())
+		let underfunded_count = DelegationChoice::<T>::iter()
+			.filter(|(delegator, (_, max_bid))| {
+				let balance = T::FundingInfo::balance(delegator);
+				Into::<u128>::into(*max_bid) < min_funding || balance.into() < min_funding
+			})
+			.count() as u32;
+		Ok((over_balance_count, underfunded_count).encode())
 	}
 
 	#[cfg(feature = "try-runtime")]
 	fn post_upgrade(state: Vec<u8>) -> Result<(), DispatchError> {
-		let pre_count = u32::decode(&mut &state[..])
-			.map_err(|_| "Failed to decode pre_upgrade state")?;
+		let (pre_over_balance, pre_underfunded) =
+			<(u32, u32)>::decode(&mut &state[..]).map_err(|_| "Failed to decode pre_upgrade state")?;
 		let min_funding = T::MinimumFunding::get_min_funding_amount();
-		let remaining = DelegationChoice::<T>::iter()
-			.filter(|(_, (_, max_bid))| Into::<u128>::into(*max_bid) < min_funding)
+		let remaining_over_balance = DelegationChoice::<T>::iter()
+			.filter(|(delegator, (_, max_bid))| {
+				let balance = T::FundingInfo::balance(delegator);
+				*max_bid > balance
+			})
 			.count();
-		ensure!(remaining == 0, "Expected all underfunded delegations to be removed");
+		let remaining_underfunded = DelegationChoice::<T>::iter()
+			.filter(|(delegator, (_, max_bid))| {
+				let balance = T::FundingInfo::balance(delegator);
+				Into::<u128>::into(*max_bid) < min_funding || balance.into() < min_funding
+			})
+			.count();
+		ensure!(remaining_over_balance == 0, "Expected no max_bid values above balance after upgrade");
+		ensure!(remaining_underfunded == 0, "Expected no underfunded delegations after upgrade");
 		log::info!(
 			target: "cf-validator",
-			"remove_underfunded_delegations: removed {} underfunded delegations",
-			pre_count,
+			"remove_underfunded_delegations: pre-upgrade over_balance={}, underfunded={}; post-upgrade over_balance={}, underfunded={}",
+			pre_over_balance,
+			pre_underfunded,
+			remaining_over_balance,
+			remaining_underfunded,
 		);
 		Ok(())
 	}
