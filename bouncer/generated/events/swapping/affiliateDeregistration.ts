fce180fd88dce09245ef48af35d49bfaa272cf1b
Daniel <daniel@chainflip.io>
2026-05-06 20:11:08 +0200

    chore: update event schemas

--- bouncer/generated/events/swapping/affiliateDeregistration.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-import { accountId } from '../common';
-
-export const swappingAffiliateDeregistration = z.object({
-  brokerId: accountId,
-  shortId: z.number(),
-  affiliateAccountId: accountId,
-});
