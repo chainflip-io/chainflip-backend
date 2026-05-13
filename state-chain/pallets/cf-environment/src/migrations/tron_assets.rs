65092122087eac25ad61a79760fc49c062bbc445
Albert Llimos <allimos3@gmail.com>
2026-05-13 11:34:56 +0200

    chore: fix migration

--- state-chain/pallets/cf-environment/src/migrations/tron_assets.rs
+++ state-chain/pallets/cf-environment/src/migrations/tron_assets.rs
@@ -54,42 +54,68 @@ impl<T: Config<Hash = H256>> UncheckedOnRuntimeUpgrade for TronAssetsMigration<T
 				assert_eq!(TronChainId::<T>::get(), cf_chains::tron::CHAIN_ID_MAINNET);
 				assert_eq!(
 					TronSupportedAssets::<T>::get(TrxAsset::TrxUsdt),
-					Some(EvmAddress::default()) // TODO: update when real address is known
+					Some(EvmAddress::from(hex_literal::hex!(
+						"a614f803b6fd780986a42c78ec9c7f77e6ded13c"
+					)))
 				);
-				// TODO: update when real addresses are known
-				assert_eq!(TronKeyManagerAddress::<T>::get(), EvmAddress::default());
-				assert_eq!(TronVaultAddress::<T>::get(), EvmAddress::default());
+				assert_eq!(
+					TronKeyManagerAddress::<T>::get(),
+					EvmAddress::from(hex_literal::hex!("67d11affdff89ba412e60ecfc8db778950486039"))
+				);
+				assert_eq!(
+					TronVaultAddress::<T>::get(),
+					EvmAddress::from(hex_literal::hex!("24c97d696e0db753b229be451d1a50705a09566e"))
+				);
 			},
 			cf_runtime_utilities::genesis_hashes::PERSEVERANCE => {
 				assert_eq!(TronChainId::<T>::get(), cf_chains::tron::CHAIN_ID_NILE_TESTNET);
 				assert_eq!(
 					TronSupportedAssets::<T>::get(TrxAsset::TrxUsdt),
-					Some(EvmAddress::default()) // TODO: update when real address is known
+					Some(EvmAddress::from(hex_literal::hex!(
+						"eca9bc828a3005b9a3b909f2cc5c2a54794de05f"
+					)))
 				);
-				// TODO: update when real addresses are known
-				assert_eq!(TronKeyManagerAddress::<T>::get(), EvmAddress::default());
-				assert_eq!(TronVaultAddress::<T>::get(), EvmAddress::default());
+				assert_eq!(
+					TronKeyManagerAddress::<T>::get(),
+					EvmAddress::from(hex_literal::hex!("391362ed6f5f649868eede354d3e833d6165dd9a"))
+				);
+				assert_eq!(
+					TronVaultAddress::<T>::get(),
+					EvmAddress::from(hex_literal::hex!("2523ae929fecd9d665f472f59b99a8ce6b179510"))
+				);
 			},
 			cf_runtime_utilities::genesis_hashes::SISYPHOS => {
 				assert_eq!(TronChainId::<T>::get(), cf_chains::tron::CHAIN_ID_NILE_TESTNET);
 				assert_eq!(
 					TronSupportedAssets::<T>::get(TrxAsset::TrxUsdt),
-					Some(EvmAddress::default()) // TODO: update when real address is known
+					Some(EvmAddress::from(hex_literal::hex!(
+						"eca9bc828a3005b9a3b909f2cc5c2a54794de05f"
+					)))
 				);
-				// TODO: update when real addresses are known
-				assert_eq!(TronKeyManagerAddress::<T>::get(), EvmAddress::default());
-				assert_eq!(TronVaultAddress::<T>::get(), EvmAddress::default());
-			},
-			_ => {
-				assert_eq!(TronChainId::<T>::get(), 4271970548);
-				assert!(TronSupportedAssets::<T>::get(TrxAsset::TrxUsdt).is_some());
 				assert_eq!(
 					TronKeyManagerAddress::<T>::get(),
-					EvmAddress::from(hex_literal::hex!("3dc86c42d46ba03ab9563726fa63c22226e1ae11"))
+					EvmAddress::from(hex_literal::hex!("fa989a8351b3ada8f871d12a75635d1589cf74a0"))
 				);
 				assert_eq!(
 					TronVaultAddress::<T>::get(),
-					EvmAddress::from(hex_literal::hex!("6f07c012183ecf09c449d769a5a05427a6385503"))
+					EvmAddress::from(hex_literal::hex!("95b740ec67963cc17ef1931978639e320c0b4be5"))
+				);
+			},
+			_ => {
+				assert_eq!(TronChainId::<T>::get(), 4271970548);
+				assert_eq!(
+					TronSupportedAssets::<T>::get(TrxAsset::TrxUsdt),
+					Some(EvmAddress::from(hex_literal::hex!(
+						"a24205e83c1805fcdae7a93db837dc6a0b843340"
+					)))
+				);
+				assert_eq!(
+					TronKeyManagerAddress::<T>::get(),
+					EvmAddress::from(hex_literal::hex!("f7229affc93ee042e750ce66a062b381f393b25a"))
+				);
+				assert_eq!(
+					TronVaultAddress::<T>::get(),
+					EvmAddress::from(hex_literal::hex!("814f36c1bbfd8aabec86273f0d61521e0c2d5287"))
 				);
 			},
 		};
