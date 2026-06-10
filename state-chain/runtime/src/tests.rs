2cbe51ce24c72505db8f701bdfcaee2aef68d796
Maxim Urschumzew <maxu@chainflip.io>
2026-06-10 15:44:06 +0200

    chore: extract common code in runtime compat tests.

--- state-chain/runtime/src/tests.rs
+++ state-chain/runtime/src/tests.rs
@@ -13,39 +13,27 @@ use crate::runtime_apis::{
 
 #[test]
 pub fn offline_test_historical_compatibility_of_runtime_api() -> Result<(), String> {
-	let mut tester = OfflineMetadataTester::new();
-	let incompatibilities = test_all_historical_runtime_calls(&mut tester);
-
-	if incompatibilities.len() > 0 {
-		print_incompatibilities(incompatibilities);
-		Err("type schema incompatibilities found!".into())
-	} else {
-		Ok(())
-	}
+	check_incompatibilities(test_all_historical_runtime_calls(&mut OfflineMetadataTester::new()))
 }
 
 #[ignore = "requires access to archive node"]
 #[test]
 pub fn online_test_historical_compatibility_of_runtime_api() -> Result<(), String> {
-	let mut tester = OnlineNodeTester {
+	check_incompatibilities(test_all_historical_runtime_calls(&mut OnlineNodeTester {
 		get_blockhash_from_spec_version: Box::new(|spec_version| match spec_version {
 			20012 => Some("0xc2068ad859fc5c3b3c7c5ecb3bd84033f1b5a0ce60e8c3b52cab4d22840eec37"),
 			20119 => Some("0x2ad1dd83839b13039d1a4ee85932b439e041068bd3bb91acf43455db97d71bd0"),
 			_ => None,
 		}),
 		node_url: "https://mainnet-archive.chainflip.io",
-	};
-	let incompatibilities = test_all_historical_runtime_calls(&mut tester);
-
-	if incompatibilities.len() > 0 {
-		print_incompatibilities(incompatibilities);
-		Err("type schema incompatibilities found!".into())
-	} else {
-		Ok(())
-	}
+	}))
 }
 
-fn print_incompatibilities(incompatibilities: Vec<TypeIncompatibilityInfo>) {
+fn check_incompatibilities(incompatibilities: Vec<TypeIncompatibilityInfo>) -> Result<(), String> {
+	if incompatibilities.is_empty() {
+		return Ok(());
+	}
+
 	let mut types_and_locations: HashMap<(TypeName, TypeDiff), Vec<FullTypeLocation>> =
 		Default::default();
 
@@ -81,4 +69,6 @@ fn print_incompatibilities(incompatibilities: Vec<TypeIncompatibilityInfo>) {
 		println!("  ]");
 		println!("");
 	}
+
+	Err(format!("{} type schema incompatibilities found!", incompatibilities.len()))
 }
