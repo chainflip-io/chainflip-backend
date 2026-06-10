019a6ffc803ac6e0a3049948b416709119b021ab
Daniel <daniel@chainflip.io>
2026-06-10 08:41:36 +0200

    chore: add top-level LICENSE file and license headers where missing

--- state-chain/pallets/cf-elections/src/electoral_systems/tests/statemachine_witnessing_pipeline/chainstate_simulation.rs
+++ state-chain/pallets/cf-elections/src/electoral_systems/tests/statemachine_witnessing_pipeline/chainstate_simulation.rs
@@ -1,3 +1,18 @@
+// Copyright 2025 Chainflip Labs GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
 //! This file defines a proptest strategy for generating a sequence of states of a progressing
 //! blockchain. The blocks of the chain contain vectors of an arbitrary datatype. The generated
 //! sequence of states includes reorgs (block data may change within a given safety margin) and
