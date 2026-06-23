a69f57d4e32e9a7a08f47870ff8143207865569e
marcellorigotti <marcello@chainflip.io>
2026-06-23 17:11:08 +0200

    update event schema

--- bouncer/generated/events/liquidityPools/minimumLimitOrderAmountSet.ts
+++ bouncer/generated/events/liquidityPools/minimumLimitOrderAmountSet.ts
@@ -1,7 +1,13 @@
 import { z } from 'zod';
 import { cfPrimitivesChainsAssetsAnyAsset, numberOrHex } from '../common';
+import { defineEvent } from '@chainflip/processor/event';
 
 export const liquidityPoolsMinimumLimitOrderAmountSet = z.object({
   asset: cfPrimitivesChainsAssetsAnyAsset,
   amount: numberOrHex,
 });
+
+export const liquidityPoolsLimitOrderUpdatedEvent = defineEvent(
+  'LiquidityPools.liquidityPoolsMinimumLimitOrderAmountSetEvent',
+  liquidityPoolsMinimumLimitOrderAmountSet,
+);
