4838bb3fb978fe6f100ebd1287a154909e3a4dd2
Maxim Urschumzew <maxu@chainflip.io>
2026-07-02 10:24:48 +0200

    fix: properly use parenthesis when computing is_empty for enum.
    This also adds tests to check is_empty behaviour.

--- utilities/src/without_std/type_introspection.rs
+++ utilities/src/without_std/type_introspection.rs
@@ -29,51 +29,82 @@ impl<A> HasTypeIntrospection for sp_std::marker::PhantomData<A> {
 
 #[cfg(test)]
 mod tests {
+	#![allow(unused)]
+
 	use crate::{never::Never, type_introspection::HasTypeIntrospection};
 
-	#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+	// --------------- is_empty_type ----------------
+	#[test]
+	fn test_is_empty_type() {
+		#[derive(cf_proc_macros::HasTypeIntrospection)]
+		enum NotEmpty0 {
+			Var0 { a: bool },
+		}
+
+		#[derive(cf_proc_macros::HasTypeIntrospection)]
+		enum NotEmpty1 {
+			Var0,
+		}
+
+		#[derive(cf_proc_macros::HasTypeIntrospection)]
+		enum NotEmpty2 {
+			Other { a: u16 },
+			MyBranch { a: u8, b: Never },
+		}
+
+		assert_eq!(NotEmpty0::is_empty_type(), false);
+		assert_eq!(NotEmpty1::is_empty_type(), false);
+		assert_eq!(NotEmpty2::is_empty_type(), false);
+
+		#[derive(cf_proc_macros::HasTypeIntrospection)]
+		enum Empty0 {}
+
+		#[derive(cf_proc_macros::HasTypeIntrospection)]
+		enum Empty1 {
+			Var0 { a: Never },
+		}
+
+		#[derive(cf_proc_macros::HasTypeIntrospection)]
+		enum Empty2 {
+			Var0 { a: Never },
+			Var1 { a: u8, b: Never },
+		}
+
+		assert_eq!(Empty0::is_empty_type(), true);
+		assert_eq!(Empty1::is_empty_type(), true);
+		assert_eq!(Empty2::is_empty_type(), true);
+	}
+
+	// --------------- sample_all_shapes ----------------
+
+	#[derive(PartialEq, Debug, cf_proc_macros::HasTypeIntrospection)]
+	enum Empty {}
+
+	#[derive(PartialEq, Debug, cf_proc_macros::HasTypeIntrospection)]
+	struct Singleton {}
+
+	#[derive(PartialEq, Debug, cf_proc_macros::HasTypeIntrospection)]
 	enum BinaryShape {
 		First,
 		Second,
 	}
 
-	impl HasTypeIntrospection for BinaryShape {
-		fn is_empty_type() -> bool {
-			false
-		}
-
-		fn sample_all_shapes() -> Vec<Self> {
-			vec![Self::First, Self::Second]
-		}
-	}
-
-	#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+	#[derive(PartialEq, Debug, cf_proc_macros::HasTypeIntrospection)]
 	enum TernaryShape {
 		First,
 		Second,
 		Third,
 	}
-
-	impl HasTypeIntrospection for TernaryShape {
-		fn is_empty_type() -> bool {
-			false
-		}
-
-		fn sample_all_shapes() -> Vec<Self> {
-			vec![Self::First, Self::Second, Self::Third]
-		}
-	}
-
-	#[derive(Copy, Clone, Debug, PartialEq, Eq, cf_proc_macros::HasTypeIntrospection)]
+	#[derive(PartialEq, Debug, cf_proc_macros::HasTypeIntrospection)]
 	struct NamedProduct {
 		binary: BinaryShape,
 		ternary: TernaryShape,
 	}
 
-	#[derive(Copy, Clone, Debug, PartialEq, Eq, cf_proc_macros::HasTypeIntrospection)]
+	#[derive(PartialEq, Debug, cf_proc_macros::HasTypeIntrospection)]
 	struct TupleProduct(BinaryShape, TernaryShape);
 
-	#[derive(Copy, Clone, Debug, PartialEq, Eq, cf_proc_macros::HasTypeIntrospection)]
+	#[derive(PartialEq, Debug, cf_proc_macros::HasTypeIntrospection)]
 	enum SumShape {
 		Unit,
 		Tuple(BinaryShape, TernaryShape),
@@ -84,6 +115,7 @@ mod tests {
 
 	#[test]
 	fn structs_sample_the_cartesian_product_of_field_shapes() {
+		assert_eq!(Singleton::sample_all_shapes(), vec![Singleton {}]);
 		assert_eq!(
 			NamedProduct::sample_all_shapes(),
 			vec![
@@ -111,6 +143,8 @@ mod tests {
 
 	#[test]
 	fn enums_sample_the_sum_of_variant_shapes() {
+		assert_eq!(Empty::sample_all_shapes(), vec![]);
+
 		assert_eq!(
 			SumShape::sample_all_shapes(),
 			vec![
