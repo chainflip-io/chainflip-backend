7de7708fd28aaf76ec370490f86cdc4314dd9fab
Maxim Urschumzew <maxu@chainflip.io>
2026-07-07 11:59:51 +0200

    chore: clean up `generate_module!` test a bit.

--- utilities/src/without_std/tests/generate_module.rs
+++ utilities/src/without_std/tests/generate_module.rs
@@ -20,7 +20,7 @@ use crate::migrations::HasChangelog;
 
 #[cf_proc_macros::generate_module]
 pub struct MyS<T> {
-	pub a: T, // pub a: u8,
+	pub a: T,
 }
 
 impl<T: HasChangelog> HasChangelog for MyS<T> {
@@ -29,17 +29,6 @@ impl<T: HasChangelog> HasChangelog for MyS<T> {
 
 pub trait T1 {
 	type XY;
-	type GGG;
-	type HHH;
-	type Bla;
-	type XXX;
-	type YYY;
-	type FFF;
-	type EEE;
-	type AAA;
-	type BBB;
-	type CCC;
-	type ZZZ;
 }
 
 mod enum1 {
@@ -50,10 +39,6 @@ mod enum1 {
 			Variant1(_0: T::XY),
 			VariantNone{_0: u8,_1: u16, _2: u8,},
 			Variant3(_0: T::XY),
-			// Variant5 {
-			// 	myfield: u8,
-			// 	// field2: (T::XY, T::XY),
-			// },
 		}
 		mod _MyTestValuesWithoutChangelog { #![migrations] }
 	}
@@ -65,10 +50,8 @@ mod enum2 {
 	pub enum MyTestValuesWithoutChangelog2<T: T1> {
 		Variant1(_0: T::XY),
 		VariantNone{_0: u8,_1: u16, _2: u8,},
-		// Variant3(_0: T::XY),
 		Variant5 {
 			myfield: u8,
-			// field2: (T::XY, T::XY),
 		},
 	}
 		mod _MyTestValuesWithoutChangelog2 { #![migrations] }
@@ -81,10 +64,10 @@ mod enum3 {
 	pub enum MyTestValuesWithoutChangelog3<T: T1> {
 		Variant1(_0: T::XY),
 		VariantNone{_0: u8,_1: u16, _2: u8,},
-		// Variant3(_0: T::XY),
+		Variant3(_0: T::XY),
 		Variant5 {
 			myfield: u8,
-			// field2: (T::XY, T::XY),
+			field2: (T::XY, T::XY),
 		},
 	}
 		mod _MyTestValuesWithoutChangelog3 { #![migrations] }
@@ -96,19 +79,15 @@ mod enum4 {
 	cf_utilities::generate_module! {
 	pub enum MyTestValues<T: T1> {
 		Variant1(_0: T::XY),
-		// Variant2(_0: u8),
 		Variant2{_0: u8, _1: u16, _2: u8, _3: u8, _4: u8, _5: u8, _6: u16, _7: u8, },
-		// Variant2(_0: u8, _1: u16),
 		Variant3 {_0: T::XY, _1: T::XY,},
 		Variant4 {
-			// myfield: u8,
 			field2: T::XY,
 			field3: T::XY,
 			field4: T::XY,
 			field5: T::XY,
 		},
 		Variant5 (
-			// myfield: u8,
 			field2: u16,
 			field3: u8,
 			field4: u8,
