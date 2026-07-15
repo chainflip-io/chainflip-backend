dc4fa5d130e93dfc84de12764a80bb7b90a7f520
Maxim Urschumzew <maxu@chainflip.io>
2026-07-15 14:37:38 +0200

    chore: clippy fixes.

--- state-chain/runtime/src/runtime_apis/custom_api/types.rs
+++ state-chain/runtime/src/runtime_apis/custom_api/types.rs
@@ -39,9 +39,8 @@ use cf_primitives::{
 pub use cf_primitives::{AssetAmount, BasisPoints};
 use cf_utilities::migrations::{
 	basics::{
-		migrate_to_historical_type, vCurrent, GlobalMigrationFromGeneric, HasGenericVariant,
-		HasVersion, IdentityMigration, IsHistoricalType, Migration, NewFieldWithDefault,
-		OverrideMigrationWith,
+		vCurrent, GlobalMigrationFromGeneric, HasGenericVariant, HasVersion, IdentityMigration,
+		IsHistoricalType, Migration, NewFieldWithDefault, OverrideMigrationWith,
 	},
 	primitives::NewTypeWithDefault,
 	v20100, v20200, v20300, HasChangelog,
