7707eaa65e6f85be78d200228debbbc2f4d619db
Ramiz Siddiqui <ramiz@chainflip.io>
2026-07-21 14:57:57 +0200

    fix: event schemas

--- bouncer/generated/events/flip/flipDistributed.ts
+++ bouncer/generated/events/flip/flipDistributed.ts
@@ -2,6 +2,8 @@ import { z } from 'zod';
 import { accountId, numberOrHex } from '../common';
 import { defineEvent } from '@chainflip/processor/event';
 
-export const flipFlipDistributed = z.object({ amount: z.array(z.tuple([accountId, numberOrHex])) });
+export const flipFlipDistributed = z.object({
+  amounts: z.array(z.tuple([accountId, numberOrHex])),
+});
 
 export const flipFlipDistributedEvent = defineEvent('Flip.FlipDistributed', flipFlipDistributed);
