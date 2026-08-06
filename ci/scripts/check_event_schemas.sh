ecc04929dc57ac225feb1da8682df81f0ba6effe
Maxim Urschumzew <maxu@chainflip.io>
2026-08-06 12:18:45 +0200

    feat: update `check_event_schemas.sh` script to also check dedot chaintypes.

--- ci/scripts/check_event_schemas.sh
+++ ci/scripts/check_event_schemas.sh
@@ -6,14 +6,17 @@ set -euo pipefail
 
 # Wait for node to start
 echo -e "🚀 Starting chainflip-node..."
-sleep 10
+sleep 30
+
+#################################################
+# Subsquid ingester event schemas
 
 # Call script to generate events
 echo -e "Generating event schemas..."
 cd bouncer && ./commands/generate_event_schemas.ts
 cd ..
 
-# Check whether a specific subdirectory is dirty
+# Check whether a event subdirectory is dirty
 EVENTS_DIR="bouncer/generated/events"
 
 if [[ -n "$(git status --porcelain -- "$EVENTS_DIR")" ]]; then
@@ -25,4 +28,27 @@ if [[ -n "$(git status --porcelain -- "$EVENTS_DIR")" ]]; then
   exit 1
 fi
 
-echo -e "Events are up to date!"
+#################################################
+# Dedot substrate api chaintypes
+
+# Call script to generate chaintypes (dedot)
+echo -e "Generating chaintypes (dedot)..."
+cd bouncer && ./commands/generate_chaintypes.ts
+cd ..
+
+# Check whether a event subdirectory is dirty
+CHAINTYPES_DIR="bouncer/generated/chaintypes"
+
+if [[ -n "$(git status --porcelain -- "$CHAINTYPES_DIR")" ]]; then
+  echo "ERROR: Event schemas in '$CHAINTYPES_DIR' are not up to date! Please run ./commands/generate_chaintypes.ts to regenerate dedot chaintypes and commit them."
+  echo ""
+  echo "The following chaintype changes have not been comitted:"
+  echo ""
+  git status -- "$CHAINTYPES_DIR"
+  exit 1
+fi
+
+#################################################
+# Success
+
+echo -e "Events and chaintypes are up to date!"
