76a4e7d6a78951f9caef916fce885b867074f612
Ramiz Siddiqui <ramiz@chainflip.io>
2026-08-13 03:41:10 +0200

    chore: call clear_refund_addresses

--- state-chain/pallets/cf-asset-balances/src/lib.rs
+++ state-chain/pallets/cf-asset-balances/src/lib.rs
@@ -1020,7 +1020,7 @@ pub struct DeleteAccount<T: Config>(PhantomData<T>);
 impl<T: Config> OnKilledAccount<T::AccountId> for DeleteAccount<T> {
 	fn on_killed_account(who: &T::AccountId) {
 		let _ = FreeBalances::<T>::clear_prefix(who, u32::MAX, None);
-		let _ = RefundAddresses::<T>::clear_prefix(who, u32::MAX, None);
+		Pallet::<T>::clear_refund_addresses(who);
 		WithdrawalWhitelists::<T>::remove(who);
 		BoundBrokerWithdrawalAddress::<T>::remove(who);
 		Pallet::<T>::discard_pending_matching(who, |_| true);
