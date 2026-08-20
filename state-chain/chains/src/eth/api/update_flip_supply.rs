b24503366c7139f7706a6c73c374fdd4396a2716
Ramiz Siddiqui <ramiz@chainflip.io>
2026-08-20 16:13:33 +0200

    chore: re-add update_flip_supply apicall

---
+++ state-chain/chains/src/eth/api/update_flip_supply.rs
@@ -0,0 +1,137 @@
+// Copyright 2025 Chainflip Labs GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+use super::*;
+use codec::{Decode, DecodeWithMemTracking, Encode, MaxEncodedLen};
+use frame_support::pallet_prelude::RuntimeDebug;
+use scale_info::TypeInfo;
+use sp_core::U256;
+use sp_std::{vec, vec::Vec};
+
+#[derive(
+	Encode,
+	Decode,
+	DecodeWithMemTracking,
+	TypeInfo,
+	Clone,
+	RuntimeDebug,
+	PartialEq,
+	Eq,
+	Default,
+	MaxEncodedLen,
+)]
+pub struct UpdateFlipSupply {
+	/// The new total supply
+	pub new_total_supply: U256,
+	/// The current state chain block number
+	pub state_chain_block_number: U256,
+}
+
+impl UpdateFlipSupply {
+	pub fn new<TotalSupply: Into<U256> + Clone, BlockNumber: Into<U256> + Clone>(
+		new_total_supply: TotalSupply,
+		state_chain_block_number: BlockNumber,
+	) -> Self {
+		Self {
+			new_total_supply: new_total_supply.into(),
+			state_chain_block_number: state_chain_block_number.into(),
+		}
+	}
+}
+
+impl EvmCall for UpdateFlipSupply {
+	const FUNCTION_NAME: &'static str = "updateFlipSupply";
+
+	fn function_params() -> Vec<(&'static str, ethabi::ParamType)> {
+		vec![("newTotalSupply", U256::param_type()), ("stateChainBlockNumber", U256::param_type())]
+	}
+
+	fn function_call_args(&self) -> Vec<ethabi::Token> {
+		vec![self.new_total_supply.tokenize(), self.state_chain_block_number.tokenize()]
+	}
+}
+
+#[cfg(test)]
+mod test_update_flip_supply {
+	use crate::{
+		eth::api::{abi::load_abi, EvmReplayProtection, EvmTransactionBuilder},
+		evm::SchnorrVerificationComponents,
+	};
+
+	use super::*;
+
+	#[test]
+	fn test_update_flip_supply_payload() {
+		use crate::evm::tests::asymmetrise;
+		use ethabi::Token;
+		const FAKE_KEYMAN_ADDR: [u8; 20] = asymmetrise([0xcf; 20]);
+		const FAKE_STATE_CHAIN_GATEWAY_ADDRESS: [u8; 20] = asymmetrise([0xcd; 20]);
+		const CHAIN_ID: u64 = 1;
+		const NONCE: u64 = 6;
+		const NEW_TOTAL_SUPPLY: u64 = 10;
+		const STATE_CHAIN_BLOCK_NUMBER: u64 = 5;
+		const FAKE_NONCE_TIMES_G_ADDR: [u8; 20] = asymmetrise([0x7f; 20]);
+		const FAKE_SIG: [u8; 32] = asymmetrise([0xe1; 32]);
+
+		let flip_token = load_abi("IStateChainGateway");
+
+		let flip_token_reference = flip_token.function("updateFlipSupply").unwrap();
+
+		let update_flip_supply_runtime = EvmTransactionBuilder::new_unsigned(
+			EvmReplayProtection {
+				nonce: NONCE,
+				chain_id: CHAIN_ID,
+				key_manager_address: FAKE_KEYMAN_ADDR.into(),
+				contract_address: FAKE_STATE_CHAIN_GATEWAY_ADDRESS.into(),
+			},
+			super::UpdateFlipSupply::new(NEW_TOTAL_SUPPLY, STATE_CHAIN_BLOCK_NUMBER),
+		);
+
+		let expected_msg_hash = update_flip_supply_runtime.threshold_signature_payload();
+
+		let runtime_payload = update_flip_supply_runtime
+			.clone()
+			.signed(
+				&SchnorrVerificationComponents {
+					s: FAKE_SIG,
+					k_times_g_address: FAKE_NONCE_TIMES_G_ADDR,
+				},
+				Default::default(),
+			)
+			.chain_encoded();
+
+		// Ensure signing payload isn't modified by signature.
+		assert_eq!(update_flip_supply_runtime.threshold_signature_payload(), expected_msg_hash);
+
+		assert_eq!(
+			// Our encoding:
+			runtime_payload,
+			// "Canoncial" encoding based on the abi definition above and using the ethabi crate:
+			flip_token_reference
+				.encode_input(&[
+					// sigData: SigData(uint, uint, address)
+					Token::Tuple(vec![
+						Token::Uint(U256::from_big_endian(&FAKE_SIG)),
+						Token::Uint(NONCE.into()),
+						Token::Address(FAKE_NONCE_TIMES_G_ADDR.into()),
+					]),
+					Token::Uint(NEW_TOTAL_SUPPLY.into()),
+					Token::Uint(STATE_CHAIN_BLOCK_NUMBER.into()),
+				])
+				.unwrap()
+		);
+	}
+}
