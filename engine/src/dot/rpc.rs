cb54b1f3c3018a39805068c72c366e700199c404
Marcello <marcello@chainflip.io>
2026-09-15 12:20:14 +0000

    chore: remove ws endpoint (keep it optional) and unused code (#6872)
    * remove ws endpoint (keep it optional) and unused code
    
    * use `author_submit_extrinsic` directly
    
    * properly convert err from request_raw

--- engine/src/dot/rpc.rs
+++ engine/src/dot/rpc.rs
@@ -14,35 +14,17 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-use std::pin::Pin;
-
 use async_trait::async_trait;
 use cf_chains::dot::{PolkadotAccountId, RuntimeVersion};
 use cf_primitives::{chains::assets::hub::Asset as HubAsset, PolkadotBlockNumber};
-use cf_utilities::redact_endpoint_secret::SecretUrl;
-use futures::{Stream, StreamExt, TryStreamExt};
 use subxt::{
 	backend::legacy::rpc_methods::{BlockDetails, Bytes},
 	events::Events,
-	ext::subxt_rpcs::{self},
-	OnlineClient, PolkadotConfig,
+	PolkadotConfig,
 };
-use tracing::warn;
 
 use crate::dot::{PolkadotHash, PolkadotHeader};
-use anyhow::{anyhow, bail, Result};
-
-/// This trait defines any subscription interfaces to Polkadot.
-#[async_trait]
-pub trait DotSubscribeApi: Send + Sync {
-	async fn subscribe_best_heads(
-		&self,
-	) -> Result<Pin<Box<dyn Stream<Item = Result<(PolkadotHash, PolkadotHeader)>> + Send>>>;
-
-	async fn subscribe_finalized_heads(
-		&self,
-	) -> Result<Pin<Box<dyn Stream<Item = Result<(PolkadotHash, PolkadotHeader)>> + Send>>>;
-}
+use anyhow::Result;
 
 /// The trait that defines the stateless / non-subscription requests to Polkadot.
 #[async_trait]
@@ -74,75 +56,4 @@ pub trait DotRpcApi: Send + Sync {
 		asset: HubAsset,
 		block_hash: PolkadotHash,
 	) -> Result<u128>;
-}
-
-#[derive(Clone)]
-pub struct DotSubClient {
-	pub ws_endpoint: SecretUrl,
-	expected_genesis_hash: Option<PolkadotHash>,
-}
-
-impl DotSubClient {
-	pub fn new(ws_endpoint: SecretUrl, expected_genesis_hash: Option<PolkadotHash>) -> Self {
-		Self { ws_endpoint, expected_genesis_hash }
-	}
-}
-
-#[async_trait]
-impl DotSubscribeApi for DotSubClient {
-	#[expect(clippy::result_large_err)]
-	async fn subscribe_best_heads(
-		&self,
-	) -> Result<Pin<Box<dyn Stream<Item = Result<(PolkadotHash, PolkadotHeader)>> + Send>>> {
-		let client = create_online_client(&self.ws_endpoint, self.expected_genesis_hash).await?;
-
-		Ok(Box::pin(
-			client
-				.blocks()
-				.subscribe_best()
-				.await?
-				.map(|result| result.map(|block| (block.hash(), block.header().clone())))
-				.map_err(|e| anyhow!("Error in best head stream: {e}")),
-		))
-	}
-
-	#[expect(clippy::result_large_err)]
-	async fn subscribe_finalized_heads(
-		&self,
-	) -> Result<Pin<Box<dyn Stream<Item = Result<(PolkadotHash, PolkadotHeader)>> + Send>>> {
-		let client = create_online_client(&self.ws_endpoint, self.expected_genesis_hash).await?;
-
-		Ok(Box::pin(
-			client
-				.blocks()
-				.subscribe_finalized()
-				.await?
-				.map(|result| result.map(|block| (block.hash(), block.header().clone())))
-				.map_err(|e| anyhow!("Error in finalised head stream: {e}")),
-		))
-	}
-}
-
-/// Creates an OnlineClient from the given websocket endpoint and checks the genesis hash if
-/// provided.
-async fn create_online_client(
-	ws_endpoint: &SecretUrl,
-	expected_genesis_hash: Option<PolkadotHash>,
-) -> Result<OnlineClient<PolkadotConfig>> {
-	if subxt_rpcs::utils::validate_url_is_secure(ws_endpoint.as_ref()).is_err() {
-		warn!("Using insecure Polkadot websocket endpoint: {ws_endpoint}");
-	}
-
-	let client = OnlineClient::<PolkadotConfig>::from_insecure_url(ws_endpoint).await?;
-
-	if let Some(expected_genesis_hash) = expected_genesis_hash {
-		let genesis_hash = client.genesis_hash();
-		if genesis_hash != expected_genesis_hash {
-			bail!("Expected Polkadot genesis hash {expected_genesis_hash} but got {genesis_hash}");
-		}
-	} else {
-		warn!("Skipping Polkadot genesis hash check");
-	}
-
-	Ok(client)
 }
