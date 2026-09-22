2bd62a9ce838b2b15158eb5b1ec39d419c6300e6
Daniel <daniel@chainflip.io>
2026-09-22 13:57:45 +0200

    feat: enhance AffiliateDetails struct with account ID derivation method and add doc warnings

--- state-chain/pallets/cf-swapping/src/lib.rs
+++ state-chain/pallets/cf-swapping/src/lib.rs
@@ -122,12 +122,30 @@ enum EgressType {
 	Refund { refund_fee: AssetAmount },
 }
 
+/// AffiliateDetails are assumed to be immutable for the lifetime of the affiliate account. In
+/// particular, the withdrawal address cannot be updated. If an affiliate wishes to change the
+/// withdrawal address, they can create a new affiliate account with a new short id.
 #[derive(
 	Encode, Decode, DecodeWithMemTracking, TypeInfo, Serialize, Deserialize, Copy, Clone, Debug,
 )]
 pub struct AffiliateDetails {
 	pub short_id: AffiliateShortId,
+	/// NOTE: this address should never be updated, it is part of the Affiliate's identity.
 	pub withdrawal_address: EthereumAddress,
+}
+
+impl AffiliateDetails {
+	pub fn derive_account_id<T: Config>(
+		&self,
+		broker_id: T::AccountId,
+	) -> Result<T::AccountId, Error<T>> {
+		Decode::decode(&mut TrailingZeroInput::new(
+			(*b"chainflip/affiliate", broker_id.clone(), self.short_id, self.withdrawal_address)
+				.blake2_256()
+				.as_ref(),
+		))
+		.map_err(|_| Error::<T>::AffiliateAccountIdDerivationFailed)
+	}
 }
 
 /// Refund parameter used within the swapping pallet.
@@ -1606,12 +1624,8 @@ pub mod pallet {
 					.ok_or(Error::<T>::AffiliateShortIdOutOfBounds)?,
 			);
 
-			let affiliate_id = Decode::decode(&mut TrailingZeroInput::new(
-				(*b"chainflip/affiliate", broker_id.clone(), short_id, withdrawal_address)
-					.blake2_256()
-					.as_ref(),
-			))
-			.map_err(|_| Error::<T>::AffiliateAccountIdDerivationFailed)?;
+			let affiliate_details = AffiliateDetails { short_id, withdrawal_address };
+			let affiliate_id = affiliate_details.derive_account_id::<T>(broker_id.clone())?;
 
 			AffiliateIdMapping::<T>::insert(&broker_id, short_id, &affiliate_id);
 			if !frame_system::Pallet::<T>::account_exists(&affiliate_id) {
@@ -1619,11 +1633,7 @@ pub mod pallet {
 				let _ = frame_system::Provider::<T>::created(&affiliate_id);
 			}
 
-			AffiliateAccountDetails::<T>::insert(
-				&broker_id,
-				&affiliate_id,
-				AffiliateDetails { short_id, withdrawal_address },
-			);
+			AffiliateAccountDetails::<T>::insert(&broker_id, &affiliate_id, affiliate_details);
 
 			Self::deposit_event(Event::<T>::AffiliateRegistration {
 				broker_id,
